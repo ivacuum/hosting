59d49a33ef42d96b78c8b17e721a474436f5a0bc
Sergey Pankov <vacuum@ivacuum.ru>
2014-10-14 01:18:55 +0400

    В php 5.5.17 защищенные подключения к БД не работают

--- app/config/database.php
+++ app/config/database.php
@@ -7,19 +7,18 @@ return [
 
 	'connections' => [
 		'mysql' => [
-			'driver'      => 'mysql',
-			'host'        => getenv('MYSQL_HOST'),
-			'database'    => getenv('MYSQL_DB'),
-			'username'    => getenv('MYSQL_USER'),
-			'password'    => getenv('MYSQL_PASS'),
-			'charset'     => 'utf8',
-			'collation'   => 'utf8_unicode_ci',
-			'prefix'      => '',
-			'unix_socket' => '',
-			'options'     => [
-				PDO::MYSQL_ATTR_SSL_CERT => '/usr/local/etc/mysql/ssl/client-cert.pem',
-				PDO::MYSQL_ATTR_SSL_KEY  => '/usr/local/etc/mysql/ssl/client-key.pem',
-			],
+			'driver'    => 'mysql',
+			'host'      => getenv('MYSQL_HOST'),
+			'database'  => getenv('MYSQL_DB'),
+			'username'  => getenv('MYSQL_USER'),
+			'password'  => getenv('MYSQL_PASS'),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			// 'options'   => [
+			// 	PDO::MYSQL_ATTR_SSL_CERT => '/usr/local/etc/mysql/ssl/client-cert.pem',
+			// 	PDO::MYSQL_ATTR_SSL_KEY  => '/usr/local/etc/mysql/ssl/client-key.pem',
+			// ],
 		],
 	],
 
