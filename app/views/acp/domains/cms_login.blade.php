611d70645f3f3217c859e8cb867f3c89029be3b2
Sergey Pankov <vacuum@ivacuum.ru>
2014-10-22 12:55:53 +0400

    Поддержка входа в cms integrium

--- app/views/acp/domains/cms_login.blade.php
+++ app/views/acp/domains/cms_login.blade.php
@@ -1,9 +1,15 @@
 <form method="post" action="{{ $domain->cms_url }}" target="_blank" style="display: inline;">
-	@if ($domain->cms_type == 'korden.cms' || $domain->cms_type == 'simpla')
-		<input type="hidden" name="login" value="{{ $domain->cms_user }}">
-		<input type="hidden" name="password" value="{{ $domain->cms_pass }}">
-		<button type="submit" name="loginsubmit" class="{{ $cms_button_class or '' }}">
-			<span class="glyphicon glyphicon-log-in"></span>
-		</button>
-	@endif
+  @if ($domain->cms_type == 'korden.cms' || $domain->cms_type == 'simpla')
+    <input type="hidden" name="login" value="{{ $domain->cms_user }}">
+    <input type="hidden" name="password" value="{{ $domain->cms_pass }}">
+    <button type="submit" name="loginsubmit" class="{{ $cms_button_class or '' }}">
+      <span class="glyphicon glyphicon-log-in"></span>
+    </button>
+  @elseif ($domain->cms_type == 'integrium')
+    <input type="hidden" name="login" value="{{ $domain->cms_user }}">
+    <input type="hidden" name="pwd" value="{{ $domain->cms_pass }}">
+    <button type="submit" class="{{ $cms_button_class or '' }}">
+      <span class="glyphicon glyphicon-log-in"></span>
+    </button>
+  @endif
 </form>
