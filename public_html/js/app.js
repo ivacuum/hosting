1aabc3856a1e50636353837f87e21029f42c7898
Sergey Pankov <vacuum@ivacuum.ru>
2014-10-27 16:43:26 +0300

    Полоса загрузки ajax

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,10 +1,18 @@
+$(document).ajaxStart(function() {
+  NProgress.start();
+});
+
+$(document).ajaxStop(function() {
+  NProgress.done();
+});
+
 $(document).ready(function() {
   $('.tip').tooltip();
 
   $('.js-dblclick-edit').bind('dblclick', function() {
     document.location = $(this).data('dblclick-url');
   });
-
+  
   $('.js-deferred-load').each(function() {
     $(this).load($(this).data('deferred-url'));
   });
