4e504544fedb96eea466fb66f2e86d3ec624f12b
Sergey Pankov <vacuum@ivacuum.ru>
2015-01-23 02:38:59 +0300

    Упрощение кода парсера ВК

--- app/controllers/ParserVk.php
+++ app/controllers/ParserVk.php
@@ -5,7 +5,7 @@ use GuzzleHttp\Client;
 class ParserVk extends BaseController
 {
 	protected $client;
-	protected $page;
+	protected $vkpage;
 	
 	public function __construct()
 	{
@@ -14,9 +14,9 @@ class ParserVk extends BaseController
 		$this->client = new Client(['base_url' => 'https://api.vk.com/method/']);
 	}
 	
-	public function index($page = 'palnom6', $date = false)
+	public function index($vkpage = 'palnom6', $date = false)
 	{
-		$this->page = $page;
+		$this->vkpage = $vkpage;
 		$date = false === $date ? '-1 day' : $date;
 		$date = Carbon::parse($date);
 		
@@ -62,15 +62,15 @@ class ParserVk extends BaseController
 		
 		$posts = array_slice($posts, 0, 10);
 
-		return View::make('parser.vk.index', compact('posts', 'page', 'date'))
+		return View::make('parser.vk.index', compact('posts', 'vkpage', 'date'))
 			->with('previous', Carbon::parse($date)->subDay())
 			->with('next', Carbon::parse($date)->addDay());
 	}
 	
 	protected function getPosts($count = 100, $offset = 0)
 	{
-		$cache_entry = "vk_{$this->page}_{$count}_{$offset}";
-		$domain = $this->page;
+		$cache_entry = "vk_{$this->vkpage}_{$count}_{$offset}";
+		$domain = $this->vkpage;
 		$params = compact('domain', 'count', 'offset');
 		
 		return Cache::remember($cache_entry, 5, function() use ($params) {
