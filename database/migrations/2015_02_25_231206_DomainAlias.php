d1734b687825d3eb436355d2de3f259713bc977d
Sergey Pankov <vacuum@ivacuum.ru>
2015-02-25 23:28:04 +0300

    Исправление названия файла миграции

--- database/migrations/2015_02_25_231206_DomainAlias.php
+++ database/migrations/2015_02_25_231206_DomainAlias.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DomainCredentials extends Migration
+class DomainAlias extends Migration
 {
 	public function up()
 	{
