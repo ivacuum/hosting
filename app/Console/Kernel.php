257072f647955f89d7af5c495d5c0e1b0c0fde96
Sergey Pankov <vacuum@ivacuum.ru>
2015-03-12 23:31:35 +0300

    Универсальный планировщик заданий

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,9 +9,20 @@ class Kernel extends ConsoleKernel
 		'App\Console\Commands\WhoisUpdate',
 	];
 
+	/**
+	* * * * * * cd /srv/www/vhosts/vhost && php artisan schedule:run >> logs/cron
+	*
+	* field         allowed values
+	* -----         --------------
+	* 
+	* minute        0-59
+	* hour          0-23
+	* day of month  1-31
+	* month         1-12
+	* day of week   0-7 (0 or 7 is Sun)
+	*/
 	protected function schedule(Schedule $schedule)
 	{
-		// $schedule->command('inspire')
-		// 	->hourly();
+		$schedule->command('app:whois-update')->cron('0 */4 * * *'); // каждые 4 часа
 	}
 }
