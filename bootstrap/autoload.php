173ac3390521d9cf43bd6182cb13a3f64f66fb7f
Sergey Pankov <vacuum@ivacuum.ru>
2015-03-18 09:24:45 +0300

    Laravel 5.0.16

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,7 +4,7 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-$compiledPath = __DIR__.'/../storage/framework/compiled.php';
+$compiledPath = __DIR__.'/../vendor/compiled.php';
 
 if (file_exists($compiledPath)) {
 	require $compiledPath;
