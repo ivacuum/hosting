367ba8d96971c94a84bf59b4fc0299bbb4fd7cdb
Sergey Pankov <vacuum@ivacuum.ru>
2015-05-27 22:24:10 +0300

    Продвинутые заголовки страниц в разделе доменов

---
+++ resources/lang/ru/meta_title.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'acp.domains.default'   => 'Домены на мониторинге',
+	'acp.domains.orphan'    => 'Домены на продажу',
+	'acp.domains.external'  => 'Домены не в нашей панели reg.ru',
+	'acp.domains.no-server' => 'Домены без сервера',
+	'acp.domains.no-ns'     => 'Домены без NS',
+	'acp.domains.inactive'  => 'Неактивные домены',
+	'acp.domains.trashed'   => 'Удаленные домены',
+];
