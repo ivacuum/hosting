2262c6bb9a66747d07654778579186ea28fdec87
Sergey Pankov <vacuum@ivacuum.ru>
2015-06-10 11:08:05 +0300

    Laravel 5.1

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -4,7 +4,7 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-$compiledPath = __DIR__.'/../vendor/compiled.php';
+$compiledPath = __DIR__.'/cache/compiled.php';
 
 if (file_exists($compiledPath)) {
 	require $compiledPath;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,8 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
+	protected $baseUrl = 'http://localhost';
+	
 	public function createApplication()
 	{
 		$app = require __DIR__.'/../bootstrap/app.php';
