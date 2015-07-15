d0ec3132fe871cb97f08c5841cf9cef00706a3be
Sergey Pankov <vacuum@ivacuum.ru>
2015-07-15 19:59:54 +0300

    Починка отправки почты

--- config/mail.php
+++ config/mail.php
@@ -6,7 +6,7 @@ return [
 	'driver'     => env('MAIL_DRIVER', 'smtp'),
 	'host'       => env('MAIL_HOST', 'localhost'),
 	'port'       => env('MAIL_PORT', 25),
-	'from'       => ['address' => null, 'name' => null],
+	'from'       => ['address' => env('MAIL_USERNAME', null), 'name' => null],
 	'encryption' => env('MAIL_ENCRYPTION'),
 	'username'   => env('MAIL_USERNAME'),
 	'password'   => env('MAIL_PASSWORD'),
