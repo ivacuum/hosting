b8a1995e2f4a661747416d03c4b01d9596f7fde0
Sergey Pankov <vacuum@ivacuum.ru>
2015-11-05 00:45:23 +0300

    Сетлист Oomph 2015

--- resources/views/life/oomph_2015.blade.php
+++ resources/views/life/oomph_2015.blade.php
@@ -14,7 +14,33 @@
 <h2>Oomph! в Москве <small>4 ноября 2015</small></h2>
 <div class="row">
   <div class="col-md-7">
-    <p>Ждем-с.</p>
+    <p>Что играли:</p>
+    <ol>
+      <li>Alles aus Liebe</li>
+      <li>Labyrinth</li>
+      <li>Träumst Du</li>
+      <li>Mein Schatz</li>
+      <li>Das weisse Licht</li>
+      <li>Mein Herz</li>
+      <li>Der neue Gott</li>
+      <li>Unzerstörbar</li>
+      <li>Als wärs das letzte Mal</li>
+      <li>Wunschkind</li>
+      <li>Jede Reise hat ein Ende</li>
+      <li>Bis Der Spiegel Zerbricht</li>
+      <li>Jetzt oder nie</li>
+      <li>Niemand</li>
+      <li>Mitten ins Herz</li>
+      <li>Unter diesem Mond</li>
+      <li>Auf Kurs</li>
+      <li>Sandmann</li>
+      <li>Gekreuzigt</li>
+      <li>Augen auf!</li>
+      <li>Kleinstadtboy</li>
+      <li>Gott ist ein Popstar</li>
+      <li>Dankeschön</li>
+      <li>The Power of Love</li>
+    </ol>
   </div>
   <div class="col-md-5">
     <div class="img-container">
