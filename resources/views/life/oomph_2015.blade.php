dc9219ab416bc1a02bbb7477b5e518b9eaae484f
Sergey Pankov <vacuum@ivacuum.ru>
2015-11-06 22:29:42 +0300

    Дополнение про концерт

--- resources/views/life/oomph_2015.blade.php
+++ resources/views/life/oomph_2015.blade.php
@@ -14,6 +14,7 @@
 <h2>Oomph! в Москве <small>4 ноября 2015</small></h2>
 <div class="row">
   <div class="col-md-7">
+    <p>Если в <a class="link" href="/life/oomph.2012">2012 году</a> немцы начали тур с Москвы, то в этот раз они в столице его закончили, то есть могли свободно задержаться на афтепати. Впрочем, мы получили бонус и во время шоу — плюс две песни относительно других концертов тура, причем исполненные одним вокалистом. В самом конце забавно было наблюдать по мониторным наушникам его намерения — если он их надевал, то собирался начать следующую песню.</p>
     <p>Что играли:</p>
     <ol>
       <li>Alles aus Liebe</li>
@@ -39,7 +40,7 @@
       <li>Kleinstadtboy</li>
       <li>Gott ist ein Popstar</li>
       <li>Dankeschön</li>
-      <li>The Power of Love</li>
+      <li>The Power of Love / Das Letzte Streichholz / Fieber</li>
     </ol>
   </div>
   <div class="col-md-5">
@@ -47,6 +48,11 @@
       <img src="//life.ivacuum.ru/gigs/oomph.2015.11.04.jpg">
     </div>
   </div>
+</div>
+
+<p>Видеозапись выступления:</p>
+<div class="fotorama" data-width="1280" data-ratio="16/10">
+  <a href="http://www.youtube.com/watch?v=FfETlcHkmCU"></a>
 </div>
 
 @include('life.timeline.oomph')
