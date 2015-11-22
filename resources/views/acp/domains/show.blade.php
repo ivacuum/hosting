30eee2ca85dd4e0f4ca3b6ffb1bbe89825bc02b8
Sergey Pankov <vacuum@ivacuum.ru>
2015-11-22 10:37:32 +0300

    Редизайн списка полей домена

--- resources/views/acp/domains/show.blade.php
+++ resources/views/acp/domains/show.blade.php
@@ -9,62 +9,64 @@
 
   @if ($domain->isExpired())
     <div class="alert alert-danger">
-      Подошел срок оплаты домена — {{ $domain->paid_till->addMonth()->formatLocalized('%d %B') }} ({{ $domain->paid_till->addMonth()->diffForHumans() }}) он совсем пропадет
+      Подошел срок оплаты домена — {{ $domain->paid_till->addMonth()->formatLocalized('%e %B') }} ({{ $domain->paid_till->addMonth()->diffForHumans() }}) он совсем пропадет
     </div>
   @endif
 
   @if ($domain->isExpiringSoon())
     <div class="alert alert-warning">
-      {{ $domain->paid_till->formatLocalized('%d %B') }} ({{ $domain->paid_till->diffForHumans() }}) подходит срок оплаты домена
+      {{ $domain->paid_till->formatLocalized('%e %B') }} ({{ $domain->paid_till->diffForHumans() }}) подходит срок оплаты домена
     </div>
   @endif
 
-  @if ($total_aliases = sizeof($domain->aliases))
-    <p>
-      <strong>Алиасы</strong>:
-      @foreach ($domain->aliases as $alias)
-        <a class="link" href="{{ action("$self@show", $alias) }}">{{ $alias->domain }}</a>
-      @endforeach
-    </p>
-  @endif
-
-  <p>
-    <strong>Клиент</strong>:
-    <a class="link" href="{{ action('Acp\Clients@show', $domain->client) }}">
-      {{ $domain->client->name }}
-    </a>
-  </p>
-
-  @if ($domain->yandex_user_id)
-    <p>
-      <strong>Яндекс</strong>:
-      <a class="link" href="{{ action('Acp\Yandex\Users@show', $domain->yandexUser->id) }}">
-        {{ $domain->yandexUser->account }}
-      </a>
-    </p>
-  @endif
-
-  @if ($domain->ipv4)
-    <p>
-      <strong>IPv4</strong>:
-      {{ $domain->ipv4 }}
-    </p>
-  @endif
-
-  @if ($domain->ipv6)
-    <p>
-      <strong>IPv6</strong>:
-      {{ $domain->ipv6 }}
-    </p>
-  @endif
-
-  <p>
-    <strong>MX</strong>:
-    {{ $domain->mx }}
-  </p>
-
-  <p>
-    <strong>NS</strong>:
-    {{ $domain->ns }}
-  </p>
+  <table class="table-stats">
+    @if (sizeof($domain->aliases))
+      <tr>
+        <td><strong>Алиасы</strong></td>
+        <td>
+          @foreach ($domain->aliases as $alias)
+            <a class="link" href="{{ action("$self@show", $alias) }}">{{ $alias->domain }}</a>
+          @endforeach
+        </td>
+      </tr>
+    @endif
+    <tr>
+      <td><strong>Клиент</strong></td>
+      <td>
+        <a class="link" href="{{ action('Acp\Clients@show', $domain->client) }}">
+          {{ $domain->client->name }}
+        </a>
+      </td>
+    </tr>
+    @if ($domain->yandex_user_id)
+      <tr>
+        <td><strong>Яндекс</strong></td>
+        <td>
+          <a class="link" href="{{ action('Acp\Yandex\Users@show', $domain->yandexUser->id) }}">
+            {{ $domain->yandexUser->account }}
+          </a>
+        </td>
+      </tr>
+    @endif
+    @if ($domain->ipv4)
+      <tr>
+        <td><strong>IPv4</strong></td>
+        <td>{{ $domain->ipv4 }}</td>
+      </tr>
+    @endif
+    @if ($domain->ipv6)
+      <tr>
+        <td><strong>IPv6</strong></td>
+        <td>{{ $domain->ipv6 }}</td>
+      </tr>
+    @endif
+    <tr>
+      <td><strong>MX</strong></td>
+      <td>{{ $domain->mx }}</td>
+    </tr>
+    <tr>
+      <td><strong>NS</strong></td>
+      <td>{{ $domain->ns }}</td>
+    </tr>
+  </table>
 @endsection
