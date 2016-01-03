55450fea7484ecf29649cd68954d5038e3f85905
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-03 21:27:24 +0300

    Заготовка для тестирования

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,18 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-	protected $baseUrl = 'http://localhost';
-	
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    protected $baseUrl = 'http://localhost';
 
-		$app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		return $app;
-	}
+        // if (file_exists(dirname(__DIR__) . '/.env.test')) {
+        //     Dotenv::load(dirname(__DIR__), '.env.test');
+        // }
+
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
