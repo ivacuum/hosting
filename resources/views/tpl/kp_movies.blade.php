6983a2570a9503e16ff2828975cdf5f034b2aef7
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-06 05:41:26 +0300

    Оформление обложек фильмов

--- resources/views/tpl/kp_movies.blade.php
+++ resources/views/tpl/kp_movies.blade.php
@@ -1,5 +1,5 @@
 @foreach ($movies as $movie)
   <a class="tip" data-html="true" title="{{ $movie['title'] or '' }}" href="http://www.kinopoisk.ru/film/{{ $movie['id'] }}/">
-    <img src="http://st.kp.yandex.net/images/film_big/{{ $movie['id'] }}.jpg" height="250">
+    <img class="screenshot" src="http://st.kp.yandex.net/images/film_big/{{ $movie['id'] }}.jpg" height="250">
   </a>
 @endforeach
