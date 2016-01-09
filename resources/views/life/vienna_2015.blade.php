8c10c7e026d60aca1d8e6ccbd1f4552aa223c3a4
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-09 07:41:40 +0300

    Вена 2015

---
+++ resources/views/life/vienna_2015.blade.php
@@ -0,0 +1,126 @@
+@extends('life.base', [
+  'meta_title' => 'Вена &middot; 24 декабря 2015',
+  'meta_description' => 'Заметки о поездке. Солнечный городок с немыслимым количеством огромных площадей.',
+  'meta_image' => 'https://life.ivacuum.ru/vienna.2015/IMG_2203.jpg',
+
+  'breadcrumbs' => [
+    ['title' => 'Заметки', 'url' => 'life'],
+    ['title' => 'Вена', 'url' => 'life/vienna'],
+    ['title' => '24 декабря 2015']
+  ]
+])
+
+@section('content')
+  <h2>
+    <span class="emoji">&#x1f1e6;&#x1f1f9;</span>
+    Вена
+    <small>24 декабря 2015</small>
+  </h2>
+  <p>Вена — город громадных улиц и площадей.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2201.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2200.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2202.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2209.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2198.jpg" alt="">
+  </div>
+
+  <p>И не только.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2193.jpg" width="1000" height="750">
+  </div>
+
+  <p>Часто встречаются парки.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2192.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2196.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2218.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2217.jpg" alt="">
+  </div>
+
+  <p>Приятно затеряться среди красивых зданий.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2208.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2206.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2203.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2229.jpg" alt="">
+  </div>
+
+  <p>Улица сотен магазинов.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2204.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2207.jpg" alt="">
+  </div>
+
+  <p>Дунайский канал.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2210.jpg" width="1000" height="750">
+  </div>
+
+  <p>Автобусная остановка. Карманы на них не делают — автобус на узкой дороге останавливает все попутное направление, зато потом сразу мчится вперед, а не ждет, пока ему уступят дорогу при выезде. Да и садиться пассажиру удобнее без кармана — транспорт четко к бордюру подъезжает.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2205.jpg" width="1000" height="750">
+  </div>
+
+  <p>Карманы делают для парковок.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2212.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2216.jpg" alt="">
+  </div>
+
+  <p>Бросающаяся в глаза форма урны.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2211.jpg" width="1000" height="750">
+  </div>
+
+  <p>Пепельница на автовокзале.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2238.jpg" width="1000" height="750">
+  </div>
+
+  <p>Снаружи видны все перемещения между этажами дома.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2222.jpg" width="1000" height="750">
+  </div>
+
+  <p>Вена стремится стать солнечным городком.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2224.jpg" width="1000" height="750">
+  </div>
+
+  <p>Улочки. Да-да, вспоминая громадные площади и проспекты.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2215.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2219.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2220.jpg" alt="">
+  </div>
+
+  <p>Дороги.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2190.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2191.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2228.jpg" alt="">
+  </div>
+
+  <p>На разметку не скупятся.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2221.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2214.jpg" alt="">
+  </div>
+
+  <p>Деловой центр.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2226.jpg" width="1000" height="750">
+  </div>
+
+  <p>Стемнело.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2230.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2231.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2232.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2233.jpg" alt="">
+    <img src="//life.ivacuum.ru/vienna.2015/IMG_2234.jpg" alt="">
+  </div>
+
+  <p>Наступление Рождества привело к закрытию практически всего и вся, даже продуктов в супермаркете не купить. Самый настоящий семейный праздник. Если заранее не затариться на неделю вперед, то придется тяжеловато.</p>
+@endsection
