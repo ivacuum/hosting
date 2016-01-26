3b9850121ffcf8e080179a60b424e198ba5dabbc
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-26 22:54:34 +0300

    Заметка о Минске

--- resources/views/life/trips/minsk_2016.blade.php
+++ resources/views/life/trips/minsk_2016.blade.php
@@ -1,4 +1,49 @@
 @extends('life.trips.base')
 
 @section('content')
+  <p>Белоруссия — страна, в которой перед покупкой карты доступа к городскому вай-фаю просят паспорт. И переписывают его данные в журнал.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2271.jpg" width="1000" height="750">
+  </div>
+
+  <p>Здесь можно ненадолго почувствовать себя миллионером. Всего за 4 000 <span class="rur">руб.</span>.</p>
+  <p>Обменный курс 1 к 250 быстро учит встречаемые цены делить на 1 000 и умножать на 4. Какой большой купюрой не решишь расплатиться, так все равно получишь еще больше бумажек сдачи. Минимальная купюра 100 рублей, монет нет — будут после деноминации.</p>
+  <p>Больше всего в стране доставляет язык. Даже не столько буквальное написание слов вроде раён вместо район, сколько забавные слова. Например, в самолете: «пришпиливайте ремни», «не отшпиливайте, пожалуйста, ремни».</p>
+  <p>Афиша театра. Царевна и кот определенно удались.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2261.jpg" width="1000" height="750">
+  </div>
+
+  <p>Внезапно.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2253.jpg" width="1000" height="750">
+  </div>
+
+  <p>Улицы.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2254.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2257.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2258.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2262.jpg">
+  </div>
+
+  <p>Ширина вполне подходящая для проезда трактора, но вот лавки требуют ручной чистки.</p>
+  <div class="img-container">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2259.jpg" width="1000" height="750">
+  </div>
+
+  <p>Делать в Минске оказалось ровным счетом нечего. Ничего не привлекало глаз на улицах.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg">
+  </div>
+
+  <p>Хорошо, что в это время проходил крупнейший в СНГ киберспортивный турнир <a class="link" href="http://starladder.com/">Старладдер</a>. Всего за 4 года ребята достигли впечатляющего уровня развития: многотысячная Минск-арена, трансляция чемпионата в реальном времени, комментирование на 3 языках, сотни тысяч зрителей в интернете. Для сравнения — в этом зале в 2015 году выступала группа Линкин Парк.</p>
+  <div class="fotorama" data-width="1000">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg">
+  </div>
+
+  <p>Усё.</p>
 @endsection
