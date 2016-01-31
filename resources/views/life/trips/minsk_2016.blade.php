28174795570a6591730654dd36146ac5b5bc2f0f
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-31 04:14:48 +0300

    Минск еще оптимизированнее

--- resources/views/life/trips/minsk_2016.blade.php
+++ resources/views/life/trips/minsk_2016.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <p>Белоруссия — страна, в которой перед покупкой карты доступа к городскому вай-фаю просят паспорт. И переписывают его данные в журнал.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2271.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2271.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2271@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Здесь можно ненадолго почувствовать себя миллионером. Всего за 4 000 <span class="rur">руб.</span>.</p>
@@ -11,12 +11,12 @@
   <p>Больше всего в стране доставляет язык. Даже не столько буквальное написание слов вроде раён вместо район, сколько забавные слова. Например, в самолете: «пришпиливайте ремни», «не отшпиливайте, пожалуйста, ремни».</p>
   <p>Афиша театра. Царевна и кот определенно удались.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2261.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2261.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2261@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Внезапно.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2253.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2253.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2253@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Улицы.</p>
@@ -29,20 +29,20 @@
 
   <p>Ширина вполне подходящая для проезда трактора, но вот лавки требуют ручной чистки.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2259.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2259.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2259@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Делать в Минске оказалось ровным счетом нечего. Ничего не привлекало глаз на улицах.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2250.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2251.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2252.jpg">
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2250.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2251.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2252.jpg"></a>
   </div>
 
   <p>Хорошо, что в это время проходил крупнейший в СНГ киберспортивный турнир <a class="link" href="http://starladder.com/">Старладдер</a>. Всего за 4 года ребята достигли впечатляющего уровня развития: многотысячная Минск-арена, трансляция чемпионата в реальном времени, комментирование на 3 языках, сотни тысяч зрителей в интернете. Для сравнения — в этом зале в 2015 году выступала группа Линкин Парк.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2273.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2272.jpg">
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2273.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2272.jpg"></a>
   </div>
 
   <p>Усё.</p>
