c7650bd71c12ddaf6dd2d95b30412daeeb542e90
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-31 03:13:57 +0300

    Оптимизация загрузки заметки про Минск

--- resources/views/life/trips/minsk_2016.blade.php
+++ resources/views/life/trips/minsk_2016.blade.php
@@ -21,10 +21,10 @@
 
   <p>Улицы.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2254.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2257.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2258.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2262.jpg">
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2254.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2254.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2257.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2257.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2258.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2258.jpg"></a>
+    <a href="//life.ivacuum.ru/minsk.2016/IMG_2262.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2262.jpg"></a>
   </div>
 
   <p>Ширина вполне подходящая для проезда трактора, но вот лавки требуют ручной чистки.</p>
@@ -34,15 +34,15 @@
 
   <p>Делать в Минске оказалось ровным счетом нечего. Ничего не привлекало глаз на улицах.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2250.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2251.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2252.jpg">
   </div>
 
   <p>Хорошо, что в это время проходил крупнейший в СНГ киберспортивный турнир <a class="link" href="http://starladder.com/">Старладдер</a>. Всего за 4 года ребята достигли впечатляющего уровня развития: многотысячная Минск-арена, трансляция чемпионата в реальном времени, комментирование на 3 языках, сотни тысяч зрителей в интернете. Для сравнения — в этом зале в 2015 году выступала группа Линкин Парк.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg">
-    <img src="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2273.jpg">
+    <img src="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2272.jpg">
   </div>
 
   <p>Усё.</p>
