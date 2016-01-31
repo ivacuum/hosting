19b71d5b261c24d7493824de2668e05a4dcd6612
Sergey Pankov <vacuum@ivacuum.ru>
2016-01-31 14:12:31 +0300

    Оптимизация Дрездена

--- resources/views/life/trips/dresden_2015.blade.php
+++ resources/views/life/trips/dresden_2015.blade.php
@@ -5,141 +5,141 @@
 
   <p>Цвет велосипедиста в Германии — красный, пешехода — серый.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2075.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2073.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2149.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2075.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2075.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2073.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2073.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2149.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2149.jpg"></a>
   </div>
 
   <p>На малополосных дорогах не всегда можно встретить светофор или пешеходный переход — автомобилисты и так пропускают. Спокойствие на дорогах приятно поражает — за весь день прогулки лишь к вечеру услышал, чтобы водитель кому-то посигналил, причем в том случае сигнал предотвратил аварию.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2115.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2076.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2115.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2115.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2076.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2076.jpg"></a>
   </div>
 
   <p>Зебр вообще нет в привычном русскому понимании. Тут отмечают границы перехода, порой эти границы разные для велосипедистов и пешеходов.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2123.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2132.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2123.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2123.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2132.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2132.jpg"></a>
   </div>
 
   <p>Пешеход может запросить зеленый для перехода дороги, но не всегда это самый эффективный способ. Фазы достаточно долгие, движение в городе не такое быстрое. Если попалось место с малым количеством машин, то проще перейти после потока на красный, чем ждать минуту, поглядывая на пустую дорогу.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2133.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2133.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2133@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Светофор для перехода трамвайных путей. Два огня означают, что вот-вот пронесется трамвай. При одном он еще достаточно далеко, а при выключенных огнях можно переходить вовсе без опаски. Отдельно стоит заметить ограждения, не позволяющие пройти насквозь прямо. Так и представляю залипшего в мобилу человека, который утыкается в ограду.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2146.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2146.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2146@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>На улице +12&deg;C, какие-то дома красят, а этот сносят.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2113.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2113.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2113@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Терминал оплаты автомобильной парковки.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2080.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2080.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2080@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Зарядка для электроавтомобилей неподалеку от главного вокзала.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2155.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2155.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2155@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>На носу Рождество.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2142.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2086.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2142.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2142.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2086.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2086.jpg"></a>
   </div>
 
   <p>Устремив на стройке взгляд ввысь, заметил, что рабочие умудрились затащить елку на один из башенных кранов. Праздник даже на высоте.</p>
   <p>Исторический центр.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2088.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2092.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2093.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2094.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2095.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2088.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2088.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2092.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2092.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2093.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2093.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2094.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2094.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2095.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2095.jpg"></a>
   </div>
 
   <p>Проходя по мостам через реку Эльбу.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2100.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2126.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2127.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2128.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2100.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2100.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2126.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2126.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2127.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2127.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2128.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2128.jpg"></a>
   </div>
 
   <p>За день не попалось ни одного бесплатного вай-фая, даже в Макдоналдсе. Одни платные хотспоты Дойче телекома за 5 евро в сутки. При оплате банковской картой бывают ограничения по минимальной сумме платежа, мелкая наличка не будет лишней. Вообще монеты крайне ценны в Европе — именно они являются основным способом оплаты общественного транспорта.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2148.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2148.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2148@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Застекленные балконы — редкость.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2079.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2081.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2150.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2151.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2104.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2079.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2079.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2081.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2081.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2150.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2150.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2151.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2151.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2104.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2104.jpg"></a>
   </div>
 
   <p>В городе очень много велосипедистов и велопарковок. Можно встретить дома, где у каждого подъезда будет по небольшой парковке.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2083.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2084.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2118.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2136.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2083.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2083.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2084.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2084.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2118.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2118.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2136.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2136.jpg"></a>
   </div>
 
   <p>Два железнодорожных вокзала снаружи.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2071.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2117.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2071.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2071.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2117.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2117.jpg"></a>
   </div>
 
   <p>Крутой низкопольный трамвай.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2111.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2111.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2111@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Жилища студентов. Ночью субъективно крайне слабое освещение на улицах, причем свет используется теплый желтый.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2153.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2153.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2153@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Подъезды, однако, светлые. Более того — в части домов в этом районе с улицы видно лестничную клетку. И видно, что на свет включается автоматически по ходу движения этаж за этажом.</p>
   <div class="img-container">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2154.jpg" width="1000" height="750">
+    <img class="js-lazy" data-src="//life.ivacuum.ru/dresden.2015/IMG_2154.jpg" data-src-2x="//life.ivacuum.ru/dresden.2015/IMG_2154@2x.jpg" width="1000" height="750" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==">
   </div>
 
   <p>Бесчетное количество мест для прогулок.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2109.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2110.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2112.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2114.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2116.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2122.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2125.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2129.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2144.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2109.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2109.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2110.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2110.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2112.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2112.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2114.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2114.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2116.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2116.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2122.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2122.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2125.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2125.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2129.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2129.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2144.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2144.jpg"></a>
   </div>
 
   <p>В целом Дрезден произвел впечатление города будущего. В плане, что нам к такой красоте стремиться и стремиться, причем немцы за это время еще дальше от нас оторвутся.</p>
   <div class="fotorama" data-width="1000">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2087.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2082.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2085.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2105.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2106.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2107.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2130.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2139.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2140.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2141.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2143.jpg">
-    <img src="//life.ivacuum.ru/dresden.2015/IMG_2145.jpg">
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2087.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2087.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2082.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2082.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2085.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2085.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2105.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2105.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2106.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2106.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2107.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2107.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2130.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2130.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2139.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2139.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2140.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2140.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2141.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2141.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2143.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2143.jpg"></a>
+    <a href="//life.ivacuum.ru/dresden.2015/IMG_2145.jpg" data-thumb="//life.ivacuum.ru/dresden.2015/t/IMG_2145.jpg"></a>
   </div>
 @endsection
