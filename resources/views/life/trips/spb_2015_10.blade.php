f9d0eea8e35efbd04aa46f6ba98ae17c7da7bab2
Sergey Pankov <vacuum@ivacuum.ru>
2016-03-09 01:17:38 +0300

    Заметка про поездку в СПб осенью 2015 года

--- resources/views/life/trips/spb_2015_10.blade.php
+++ resources/views/life/trips/spb_2015_10.blade.php
@@ -2,6 +2,63 @@
 
 @section('content')
 <div class="shortcuts-item">
-  <p>Ы.</p>
+  <p>Перелет из открытого несколькими месяцами ранее крохотного аэропорта Грабцево в Калуге.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/IMG_1824.jpg" data-src-2x="https://life.ivacuum.ru/spb.2015.10/IMG_1824@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>Через два с лишним часа уже в центре Петербурга. Сам полет занимает немногим более часа.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/IMG_1811.jpg" data-src-2x="https://life.ivacuum.ru/spb.2015.10/IMG_1811@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>Раз в день в крупных сетях кинотеатров показывают кино в оригинале с субтитрами. Сеансы не самые удобные для гостей города — между 6 и 8 часами вечера. При покупке билета сервис удостоверяется в вашей уверенности смотреть фильм на иностранном языке.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/english.png" data-src-2x="https://life.ivacuum.ru/spb.2015.10/english.png" width="846" height="550" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>За оплату Мастеркардом дают скидку. Не узнал бы, если бы не нажал ради интереса на список вариантов оплаты.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/discount.png" data-src-2x="https://life.ivacuum.ru/spb.2015.10/discount.png" width="541" height="259" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>По итогу присылают код билета. Его достаточно поднести к терминалу Яндекса у входа в кинозал, к контролеру обращаться не нужно.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/qr.png" data-src-2x="https://life.ivacuum.ru/spb.2015.10/qr.png" width="607" height="220" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>В городе приятно.</p>
+  <div class="fotorama" data-width="1000">
+    <a href="https://life.ivacuum.ru/spb.2015.10/IMG_1816.jpg" data-thumb="https://life.ivacuum.ru/spb.2015.10/t/IMG_1816.jpg"></a>
+    <a href="https://life.ivacuum.ru/spb.2015.10/IMG_1817.jpg" data-thumb="https://life.ivacuum.ru/spb.2015.10/t/IMG_1817.jpg"></a>
+  </div>
+
+  <p>Несколько высотных видов.</p>
+  <div class="fotorama" data-width="1000">
+    <a href="https://life.ivacuum.ru/spb.2015.10/IMG_1823.jpg" data-thumb="https://life.ivacuum.ru/spb.2015.10/t/IMG_1823.jpg"></a>
+    <a href="https://life.ivacuum.ru/spb.2015.10/IMG_1813.jpg" data-thumb="https://life.ivacuum.ru/spb.2015.10/t/IMG_1813.jpg"></a>
+  </div>
+
+  <p>Безмятежно прогуливающаяся парочка.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/IMG_1818.jpg" data-src-2x="https://life.ivacuum.ru/spb.2015.10/IMG_1818@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>Балконы с возможностью зайти за угол.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/IMG_1821.jpg" data-src-2x="https://life.ivacuum.ru/spb.2015.10/IMG_1821@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>В вестибюле метро есть куда выбросить чеки. На самих подземных станциях урн не наблюдается с <a class="link" href="https://meduza.io/feature/2015/11/20/metro-bez-urn-samolety-bez-zhidkostey">1977 года</a>.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/IMG_1822.jpg" data-src-2x="https://life.ivacuum.ru/spb.2015.10/IMG_1822@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>Типичная картина списка доступных беспроводных сетей дома. Не всегда даже на экран ноутбука влезает.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2015.10/wi-fi.png" data-src-2x="https://life.ivacuum.ru/spb.2015.10/wi-fi.png" width="296" height="586" src="https://life.ivacuum.ru/0.gif">
+  </div>
+
+  <p>Рядом с домом сетей может быть в несколько раз больше. Почему? Близкое к окну расположение роутера, максимальная (100%) мощность сигнала в его настройках. В идеале поместить роутер в недра квартиры, снизить мощность до минимальной, при которой сохраняется стабильное подключение устройств. Так будет меньше помех и вам и соседям. Выставить стандарт связи вроде 802.11n — он выбирается по минимальному поддерживаемому среди устройств дома. Подробнее <a class="link" href="https://habrahabr.ru/post/149447/">о нюансах работы вай-фая</a>.</p>
 </div>
 @endsection
