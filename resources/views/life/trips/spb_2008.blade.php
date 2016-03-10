52f108e65e2ba53144bf36e635f25f70b0644ee1
Sergey Pankov <vacuum@ivacuum.ru>
2016-03-10 05:32:55 +0300

    Заметка о поездке в СПб в 2008 году

---
+++ resources/views/life/trips/spb_2008.blade.php
@@ -0,0 +1,8 @@
+@extends('life.trips.base')
+
+@section('content')
+  <p>Краткосрочный визит на <a class="link" href="/life/metallica.2008">концерт Металлики</a>. С тех пор понял, что в купе на поезде ехать стоит только в крайнем случае. В пересчете на доллары билет туда-обратно в то время стоил около $200. Даже билет на концерт в фан-зону обошелся почти вдвое дешевле.</p>
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/spb.2008/IMG_1144.jpg" data-src-2x="https://life.ivacuum.ru/spb.2008/IMG_1144.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
+@endsection
