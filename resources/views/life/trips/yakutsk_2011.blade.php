e0a21a917de4f40fc282ff374e3a9b600d952546
Sergey Pankov <vacuum@ivacuum.ru>
2016-03-10 05:33:18 +0300

    Заготовка заметки про Якутск 2011

--- resources/views/life/trips/yakutsk_2011.blade.php
+++ resources/views/life/trips/yakutsk_2011.blade.php
@@ -1,4 +1,7 @@
 @extends('life.trips.base')
 
 @section('content')
+  <div class="img-container">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/yakutsk.2011/IMG_1785.jpg" data-src-2x="https://life.ivacuum.ru/yakutsk.2011/IMG_1785.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
+  </div>
 @endsection
