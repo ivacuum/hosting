7a87afdd799d20ac81648e7a26d0b4937ab5f62b
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-04 18:35:27 +0300

    Калуга 19 марта 2016

---
+++ resources/views/life/trips/kaluga_2016_03_19.blade.php
@@ -0,0 +1,13 @@
+@extends('life.trips.base')
+
+@section('content')
+<p>День.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.19/IMG_0115.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.19/IMG_0115@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+
+<p>Вечер.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="//life.ivacuum.ru/kaluga.2016.03.19/IMG_0116.jpg" data-src-2x="//life.ivacuum.ru/kaluga.2016.03.19/IMG_0116@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+@endsection
