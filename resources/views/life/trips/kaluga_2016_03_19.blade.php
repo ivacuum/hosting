8fa8eaf06cab01ba113fc0b9cee4020287f9d756
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-04 19:00:51 +0300

    Калуга 24 марта 2016

--- resources/views/life/trips/kaluga_2016_03_19.blade.php
+++ resources/views/life/trips/kaluga_2016_03_19.blade.php
@@ -8,6 +8,6 @@
 
 <p>Вечер.</p>
 <div class="img-container">
-  <img class="js-lazy pic-1000x750" data-src="//life.ivacuum.ru/kaluga.2016.03.19/IMG_0116.jpg" data-src-2x="//life.ivacuum.ru/kaluga.2016.03.19/IMG_0116@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.19/IMG_0116.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.19/IMG_0116@2x.jpg" src="https://life.ivacuum.ru/0.gif">
 </div>
 @endsection
---
+++ resources/views/life/trips/kaluga_2016_03_24.blade.php
@@ -0,0 +1,23 @@
+@extends('life.trips.base')
+
+@section('content')
+<p>Два похожих состава. Где экспресс и где электричка?</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0123.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0123@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+
+<p>На маршрут выставили прокачанную электричку с кондиционерами, туалетами, местами для инвалидов и велосипедов. И да — на фото выше она справа.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0124.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0124@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+
+<p>Пассажирский салон.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0126.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0126@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+
+<p>В Москве уже поджидает аэроэкспресс. Благодаря четкому интервалу следования состава, до аэропорта можно приезжать на вокзал буквально за полтора часа до вылета.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0127.jpg" data-src-2x="https://life.ivacuum.ru/kaluga.2016.03.24/IMG_0127@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
+@endsection
