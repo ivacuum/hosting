a9b0b00b2db05b17a81585b106047b441d4f27d0
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-04 18:35:02 +0300

    Точный размер фото для поддержки ссылок-якорей

--- resources/views/life/trips/minsk_2016.blade.php
+++ resources/views/life/trips/minsk_2016.blade.php
@@ -1,49 +1,49 @@
 @extends('life.trips.base')
 
 @section('content')
-  <p>Белоруссия — страна, в которой перед покупкой карты доступа к городскому вай-фаю просят паспорт. И переписывают его данные в журнал.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2271.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2271@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Белоруссия — страна, в которой перед покупкой карты доступа к городскому вай-фаю просят паспорт. И переписывают его данные в журнал.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/minsk.2016/IMG_2271.jpg" data-src-2x="https://life.ivacuum.ru/minsk.2016/IMG_2271@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
 
-  <p>Здесь можно ненадолго почувствовать себя миллионером. Всего за 4 000 <span class="rur">руб.</span>.</p>
-  <p>Обменный курс 1 к 250 быстро учит встречаемые цены делить на 1 000 и умножать на 4. Какой большой купюрой не решишь расплатиться, так все равно получишь еще больше бумажек сдачи. Минимальная купюра 100 рублей, монет нет — будут после деноминации.</p>
-  <p>Больше всего в стране доставляет язык. Даже не столько буквальное написание слов вроде раён вместо район, сколько забавные слова. Например, в самолете: «пришпиливайте ремни», «не отшпиливайте, пожалуйста, ремни».</p>
-  <p>Афиша театра. Царевна и кот определенно удались.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2261.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2261@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Здесь можно ненадолго почувствовать себя миллионером. Всего за 4 000 <span class="rur">руб.</span>.</p>
+<p>Обменный курс 1 к 250 быстро учит встречаемые цены делить на 1 000 и умножать на 4. Какой большой купюрой не решишь расплатиться, так все равно получишь еще больше бумажек сдачи. Минимальная купюра 100 рублей, монет нет — будут после деноминации.</p>
+<p>Больше всего в стране доставляет язык. Даже не столько буквальное написание слов вроде раён вместо район, сколько забавные слова. Например, в самолете: «пришпиливайте ремни», «не отшпиливайте, пожалуйста, ремни».</p>
+<p>Афиша театра. Царевна и кот определенно удались.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/minsk.2016/IMG_2261.jpg" data-src-2x="https://life.ivacuum.ru/minsk.2016/IMG_2261@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
 
-  <p>Внезапно.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2253.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2253@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Внезапно.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/minsk.2016/IMG_2253.jpg" data-src-2x="https://life.ivacuum.ru/minsk.2016/IMG_2253@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
 
-  <p>Улицы.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2254.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2254.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2257.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2257.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2258.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2258.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2262.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2262.jpg"></a>
-  </div>
+<p>Улицы.</p>
+<div class="fotorama" data-width="1000">
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2254.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2254.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2257.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2257.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2258.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2258.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2262.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2262.jpg"></a>
+</div>
 
-  <p>Ширина вполне подходящая для проезда трактора, но вот лавки требуют ручной чистки.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/minsk.2016/IMG_2259.jpg" data-src-2x="//life.ivacuum.ru/minsk.2016/IMG_2259@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Ширина вполне подходящая для проезда трактора, но вот лавки требуют ручной чистки.</p>
+<div class="img-container">
+  <img class="js-lazy pic-1000x750" data-src="https://life.ivacuum.ru/minsk.2016/IMG_2259.jpg" data-src-2x="https://life.ivacuum.ru/minsk.2016/IMG_2259@2x.jpg" src="https://life.ivacuum.ru/0.gif">
+</div>
 
-  <p>Делать в Минске оказалось ровным счетом нечего. Ничего не привлекало глаз на улицах.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2250.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2250.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2251.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2251.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2252.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2252.jpg"></a>
-  </div>
+<p>Делать в Минске оказалось ровным счетом нечего. Ничего не привлекало глаз на улицах.</p>
+<div class="fotorama" data-width="1000">
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2250.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2250.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2251.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2251.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2252.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2252.jpg"></a>
+</div>
 
-  <p>Хорошо, что в это время проходил крупнейший в СНГ киберспортивный турнир <a class="link" href="http://starladder.com/">Старладдер</a>. Всего за 4 года ребята достигли впечатляющего уровня развития: многотысячная Минск-арена, трансляция чемпионата в реальном времени, комментирование на 3 языках, сотни тысяч зрителей в интернете. Для сравнения — в этом зале в 2015 году выступала группа Линкин Парк.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2273.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2273.jpg"></a>
-    <a href="//life.ivacuum.ru/minsk.2016/IMG_2272.jpg" data-thumb="//life.ivacuum.ru/minsk.2016/t/IMG_2272.jpg"></a>
-  </div>
+<p>Хорошо, что в это время проходил крупнейший в СНГ киберспортивный турнир <a class="link" href="http://starladder.com/">Старладдер</a>. Всего за 4 года ребята достигли впечатляющего уровня развития: многотысячная Минск-арена, трансляция чемпионата в реальном времени, комментирование на 3 языках, сотни тысяч зрителей в интернете. Для сравнения — в этом зале в 2015 году выступала группа Линкин Парк.</p>
+<div class="fotorama" data-width="1000">
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2273.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2273.jpg"></a>
+  <a href="https://life.ivacuum.ru/minsk.2016/IMG_2272.jpg" data-thumb="https://life.ivacuum.ru/minsk.2016/t/IMG_2272.jpg"></a>
+</div>
 
-  <p>Усё.</p>
+<p>Усё.</p>
 @endsection
