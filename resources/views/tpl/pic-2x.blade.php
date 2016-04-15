fac044a5cb9e096c814ace24533dc318d7590ef6
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-15 03:49:06 +0300

    Улучшенный способ вывода адаптивных фото в заметках

---
+++ resources/views/tpl/pic-2x.blade.php
@@ -0,0 +1,5 @@
+<div class="pic-container">
+  <div class="pic">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/{{ $trip->slug }}/{{ $pic }}" data-src-2x="https://life.ivacuum.ru/{{ $trip->slug }}/{{ pathinfo($pic)['filename'] }}@2x.{{ pathinfo($pic)['extension'] }}" src="https://life.ivacuum.ru/0.gif">
+  </div>
+</div>
---
+++ resources/views/tpl/pic.blade.php
@@ -0,0 +1,5 @@
+<div class="pic-container">
+  <div class="pic">
+    <img class="js-lazy" data-src="https://life.ivacuum.ru/{{ $trip->slug }}/{{ $pic }}" src="https://life.ivacuum.ru/0.gif">
+  </div>
+</div>
