34acea4cc7c450cc68e30e11726f4aeb2ebb9ff2
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-15 04:58:13 +0300

    Рефакторинг заметки о Вене

--- resources/views/life/trips/vienna_2015.blade.php
+++ resources/views/life/trips/vienna_2015.blade.php
@@ -1,111 +1,95 @@
 @extends('life.trips.base')
 
 @section('content')
-  <p>Вена — город громадных улиц и площадей.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2201.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2201.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2200.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2200.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2202.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2202.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2209.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2209.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2198.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2198.jpg" alt=""></a>
-  </div>
+<p>Вена — город громадных улиц и площадей.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2201.jpg',
+  'IMG_2200.jpg',
+  'IMG_2202.jpg',
+  'IMG_2209.jpg',
+  'IMG_2198.jpg',
+]])
 
-  <p>И не только.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2193.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2193@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>И не только.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2193.jpg'])
 
-  <p>Часто встречаются парки.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2192.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2192.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2196.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2196.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2218.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2218.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2217.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2217.jpg" alt=""></a>
-  </div>
+<p>Часто встречаются парки.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2192.jpg',
+  'IMG_2196.jpg',
+  'IMG_2218.jpg',
+  'IMG_2217.jpg',
+]])
 
-  <p>Приятно затеряться среди красивых зданий.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2208.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2208.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2206.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2206.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2203.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2203.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2229.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2229.jpg" alt=""></a>
-  </div>
+<p>Приятно затеряться среди красивых зданий.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2208.jpg',
+  'IMG_2206.jpg',
+  'IMG_2203.jpg',
+  'IMG_2229.jpg',
+]])
 
-  <p>Улица сотен магазинов.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2204.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2204.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2207.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2207.jpg" alt=""></a>
-  </div>
+<p>Улица сотен магазинов.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2204.jpg',
+  'IMG_2207.jpg',
+]])
 
-  <p>Дунайский канал.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2210.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2210@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Дунайский канал.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2210.jpg'])
 
-  <p>Автобусная остановка. Карманы на них не делают — автобус на узкой дороге останавливает все попутное направление, зато потом сразу мчится вперед, а не ждет, пока ему уступят дорогу при выезде. Да и садиться пассажиру удобнее без кармана — транспорт четко к бордюру подъезжает.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2205.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2205@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Автобусная остановка. Карманы на них не делают — автобус на узкой дороге останавливает все попутное направление, зато потом сразу мчится вперед, а не ждет, пока ему уступят дорогу при выезде. Да и садиться пассажиру удобнее без кармана — транспорт четко к бордюру подъезжает.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2205.jpg'])
 
-  <p>Карманы делают для парковок.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2212.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2212.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2216.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2216.jpg" alt=""></a>
-  </div>
+<p>Карманы делают для парковок.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2212.jpg',
+  'IMG_2216.jpg',
+]])
 
-  <p>Бросающаяся в глаза форма урны.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2211.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2211@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Бросающаяся в глаза форма урны.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2211.jpg'])
 
-  <p>Пепельница на автовокзале.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2238.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2238@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Пепельница на автовокзале.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2238.jpg'])
 
-  <p>Снаружи видны все перемещения между этажами дома.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2222.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2222@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Снаружи видны все перемещения между этажами дома.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2222.jpg'])
 
-  <p>Вена стремится стать солнечным городком.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2224.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2224@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Вена стремится стать солнечным городком.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2224.jpg'])
 
-  <p>Улочки. Да-да, вспоминая громадные площади и проспекты.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2215.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2215.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2219.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2219.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2220.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2220.jpg" alt=""></a>
-  </div>
+<p>Улочки. Да-да, вспоминая громадные площади и проспекты.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2215.jpg',
+  'IMG_2219.jpg',
+  'IMG_2220.jpg',
+]])
 
-  <p>Дороги.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2190.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2190.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2191.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2191.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2228.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2228.jpg" alt=""></a>
-  </div>
+<p>Дороги.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2190.jpg',
+  'IMG_2191.jpg',
+  'IMG_2228.jpg',
+]])
 
-  <p>На разметку не скупятся.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2221.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2221.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2214.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2214.jpg" alt=""></a>
-  </div>
+<p>На разметку не скупятся.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2221.jpg',
+  'IMG_2214.jpg',
+]])
 
-  <p>Деловой центр.</p>
-  <div class="img-container">
-    <img class="js-lazy" data-src="//life.ivacuum.ru/vienna.2015/IMG_2226.jpg" data-src-2x="//life.ivacuum.ru/vienna.2015/IMG_2226@2x.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+<p>Деловой центр.</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_2226.jpg'])
 
-  <p>Стемнело.</p>
-  <div class="fotorama" data-width="1000">
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2230.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2230.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2231.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2231.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2232.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2232.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2233.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2233.jpg" alt=""></a>
-    <a href="//life.ivacuum.ru/vienna.2015/IMG_2234.jpg" data-thumb="//life.ivacuum.ru/vienna.2015/t/IMG_2234.jpg" alt=""></a>
-  </div>
+<p>Стемнело.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_2230.jpg',
+  'IMG_2231.jpg',
+  'IMG_2232.jpg',
+  'IMG_2233.jpg',
+  'IMG_2234.jpg',
+]])
 
-  <p>Наступление Рождества привело к закрытию практически всего и вся, даже продуктов в супермаркете не купить. Самый настоящий семейный праздник. Если заранее не затариться на неделю вперед, то придется тяжеловато.</p>
+<p>Наступление Рождества привело к закрытию практически всего и вся, даже продуктов в супермаркете не купить. Самый настоящий семейный праздник. Если заранее не затариться на неделю вперед, то придется тяжеловато.</p>
 @endsection
