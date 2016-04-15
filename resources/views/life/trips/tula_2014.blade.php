e2bef96eaea2f1f03816391dd9aef13e1f1256ff
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-15 05:20:26 +0300

    Рефакторинг фото в заметках

--- resources/views/life/trips/tula_2014.blade.php
+++ resources/views/life/trips/tula_2014.blade.php
@@ -1,102 +1,68 @@
 @extends('life.trips.base')
 
 @section('content')
-  <div class="shortcuts-item">
-    <p>Исследование города на велосипеде.</p>
-    <div class="img-container">
-      <img class="js-lazy" data-src="https://life.ivacuum.ru/tula.2014/IMG_0630.jpg" data-src-2x="https://life.ivacuum.ru/tula.2014/IMG_0630.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-    </div>
-  </div>
+<p>Исследование города на велосипеде.</p>
+@include('tpl.pic', ['pic' => 'IMG_0630.jpg'])
 
-  <div class="shortcuts-item">
-    <p>Внезапно тупик у железной дороги.</p>
-    <div class="img-container">
-      <img class="js-lazy" data-src="https://life.ivacuum.ru/tula.2014/IMG_0631.jpg" data-src-2x="https://life.ivacuum.ru/tula.2014/IMG_0631.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-    </div>
-  </div>
+<p>Внезапно тупик у железной дороги.</p>
+@include('tpl.pic', ['pic' => 'IMG_0631.jpg'])
 
-  <div class="shortcuts-item">
-    <p>Местами кучкуются деревянные дома.</p>
-    <div class="img-container">
-      <img class="js-lazy" data-src="https://life.ivacuum.ru/tula.2014/IMG_0632.jpg" data-src-2x="https://life.ivacuum.ru/tula.2014/IMG_0632.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-    </div>
-  </div>
+<p>Местами кучкуются деревянные дома.</p>
+@include('tpl.pic', ['pic' => 'IMG_0632.jpg'])
 
-  <div class="shortcuts-item">
-    <p>В Туле очень много детских площадок во дворах. Вдобавок большинство дворов изобилует деревьями и теньком — детям в кайф проводить дни напролет на улице.</p>
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0638.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0638.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0646.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0646.jpg"></a>
-    </div>
-  </div>
+<p>В Туле очень много детских площадок во дворах. Вдобавок большинство дворов изобилует деревьями и теньком — детям в кайф проводить дни напролет на улице.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0638.jpg',
+  'IMG_0646.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Сам город очень рельефный.</p>
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0642.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0642.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0643.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0643.jpg"></a>
-    </div>
-  </div>
+<p>Сам город очень рельефный.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0642.jpg',
+  'IMG_0643.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Трамвайным путям не позавидуешь.</p>
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0649.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0649.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0650.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0650.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0651.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0651.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0652.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0652.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0673.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0673.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0674.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0674.jpg"></a>
-    </div>
-  </div>
+<p>Трамвайным путям не позавидуешь.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0649.jpg',
+  'IMG_0650.jpg',
+  'IMG_0651.jpg',
+  'IMG_0652.jpg',
+  'IMG_0673.jpg',
+  'IMG_0674.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Да и пешеходу тоже. Крайне редко встречаются места с тротуарной плиткой — в основном в центре.</p>
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0676.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0676.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0655.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0655.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0657.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0657.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0661.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0661.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0682.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0682.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0664.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0664.jpg"></a>
-    </div>
-  </div>
+<p>Да и пешеходу тоже. Крайне редко встречаются места с тротуарной плиткой — в основном в центре.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0676.jpg',
+  'IMG_0655.jpg',
+  'IMG_0657.jpg',
+  'IMG_0661.jpg',
+  'IMG_0682.jpg',
+  'IMG_0664.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Река Упа.</p>
-    <div class="img-container">
-      <img class="js-lazy" data-src="https://life.ivacuum.ru/tula.2014/IMG_0680.jpg" data-src-2x="https://life.ivacuum.ru/tula.2014/IMG_0680.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-    </div>
-  </div>
+<p>Река Упа.</p>
+@include('tpl.pic', ['pic' => 'IMG_0680.jpg'])
 
-  <div class="shortcuts-item">
-    <p>Укатив черт знает куда на восток Тулы, наткнулся на затяжной спуск вдоль гаражей по нелюбимой щебенке (даже по песку лучше ехать). После приятного спуска на велосипеде всегда следует тяжелый подъем. В этот раз это почему-то не смущало. В определенный момент слева обнаружился въезд на большую открытую зону, а там трасса для мотокросса.</p>
-  </div>
-  <div class="shortcuts-item">
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0666.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0666.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0668.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0668.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0669.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0669.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0671.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0671.jpg"></a>
-    </div>
-  </div>
+<p>Укатив черт знает куда на восток Тулы, наткнулся на затяжной спуск вдоль гаражей по нелюбимой щебенке (даже по песку лучше ехать). После приятного спуска на велосипеде всегда следует тяжелый подъем. В этот раз это почему-то не смущало. В определенный момент слева обнаружился въезд на большую открытую зону, а там трасса для мотокросса.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0666.jpg',
+  'IMG_0668.jpg',
+  'IMG_0669.jpg',
+  'IMG_0671.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Дороги.</p>
-    <div class="fotorama" data-width="1000">
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0635.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0635.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0640.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0640.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0644.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0644.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0659.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0659.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0662.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0662.jpg"></a>
-      <a href="https://life.ivacuum.ru/tula.2014/IMG_0683.jpg" data-thumb="https://life.ivacuum.ru/tula.2014/t/IMG_0683.jpg"></a>
-    </div>
-  </div>
+<p>Дороги.</p>
+@include('tpl.fotorama', ['pics' => [
+  'IMG_0635.jpg',
+  'IMG_0640.jpg',
+  'IMG_0644.jpg',
+  'IMG_0659.jpg',
+  'IMG_0662.jpg',
+  'IMG_0683.jpg',
+]])
 
-  <div class="shortcuts-item">
-    <p>Круг по городу сделан.</p>
-    <div class="img-container">
-      <img class="js-lazy" data-src="https://life.ivacuum.ru/tula.2014/IMG_0689.jpg" data-src-2x="https://life.ivacuum.ru/tula.2014/IMG_0689.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-    </div>
-  </div>
+<p>Круг по городу сделан.</p>
+@include('tpl.pic', ['pic' => 'IMG_0689.jpg'])
 @endsection
--- resources/views/life/trips/yakutsk_2011.blade.php
+++ resources/views/life/trips/yakutsk_2011.blade.php
@@ -1,7 +1,5 @@
 @extends('life.trips.base')
 
 @section('content')
-  <div class="img-container">
-    <img class="js-lazy" data-src="https://life.ivacuum.ru/yakutsk.2011/IMG_1785.jpg" data-src-2x="https://life.ivacuum.ru/yakutsk.2011/IMG_1785.jpg" width="1000" height="750" src="https://life.ivacuum.ru/0.gif">
-  </div>
+@include('tpl.pic', ['pic' => 'IMG_1785.jpg'])
 @endsection
