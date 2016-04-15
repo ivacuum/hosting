6701273deeaaa542a8d44c1fdfb9e92af84b19c1
Sergey Pankov <vacuum@ivacuum.ru>
2016-04-15 05:19:59 +0300

    Заготовки

---
+++ resources/views/life/trips/krasnodar_2016.blade.php
@@ -0,0 +1,6 @@
+@extends('life.trips.base')
+
+@section('content')
+<p>...</p>
+@include('tpl.pic-2x', ['pic' => 'IMG_0208.jpg'])
+@endsection
---
+++ resources/views/life/trips/spb_2016_03.blade.php
@@ -0,0 +1,5 @@
+@extends('life.trips.base')
+
+@section('content')
+<p>Парам-пам-пам</p>
+@endsection
