252a08d7ee9ed9190684a0e01eb398dbd0f92188
Sergey Pankov <vacuum@ivacuum.ru>
2016-06-22 14:19:03 +0300

    Верстка книг

--- resources/views/life/books.blade.php
+++ resources/views/life/books.blade.php
@@ -9,14 +9,14 @@
 
 @section('content')
 <h2>Понравившиеся книги</h2>
-<ul>
-  <li>
-    <a class="link" href="http://www.ozon.ru/context/detail/id/19728054/">11/22/63</a>
-    <span class="text-muted">[2013]</span>
-  </li>
-  <li>
-    <a class="link" href="http://www.ozon.ru/context/detail/id/28788268/">Марсианин</a>
-    <span class="text-muted">[2014]</span>
-  </li>
-</ul>
+
+<div class="travel-entry">
+  <span class="travel-year">2014</span>
+  <a class="link" href="http://www.ozon.ru/context/detail/id/28788268/">Марсианин</a>
+</div>
+
+<div class="travel-entry">
+  <span class="travel-year">2013</span>
+  <a class="link" href="http://www.ozon.ru/context/detail/id/19728054/">11/22/63</a>
+</div>
 @endsection
