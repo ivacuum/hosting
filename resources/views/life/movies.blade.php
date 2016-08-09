05d0424c0475caa6a0fe139f3f62739e5d744648
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-10 01:00:09 +0300

    Рефакторинг стилей

--- resources/views/life/movies.blade.php
+++ resources/views/life/movies.blade.php
@@ -9,34 +9,28 @@
 
 @section('content')
 <h2>Фильмы и сериалы, достойные многократного просмотра</h2>
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2016 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2016 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Хардкор<br>Hardcore Henry', 'id' => 778218],
       ['title' => 'Ночной администратор<br>The Night Manager', 'id' => 462649],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2015 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2015 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Головоломка<br>Inside Out', 'id' => 645118],
       ['title' => 'Kingsman: Секретная служба<br>Kingsman: The Secret Service', 'id' => 749540],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2014 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2014 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Бёрдмэн<br>Birdman', 'id' => 722827],
@@ -49,12 +43,10 @@
       ['title' => 'Волк с Уолл-стрит<br>Wolf of Wall Street', 'id' => 462682],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2013 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2013 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Далласский клуб покупателей<br>Dallas Buyers Club', 'id' => 260162],
@@ -63,12 +55,10 @@
       ['title' => 'Одержимость<br>Whiplash', 'id' => 725190],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2011 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2011 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Родина<br>Homeland', 'id' => 574688],
@@ -78,12 +68,10 @@
       ['title' => 'Однажды в Ирландии<br>The Guard', 'id' => 484474],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2010 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2010 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Экспорт Рэймонда<br>Exporting Raymond', 'id' => 581145],
@@ -91,23 +79,19 @@
       ['title' => 'Шерлок<br>Sherlock', 'id' => 502838],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2009 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2009 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Три идиота<br>3 Idiots', 'id' => 423210],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2008 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2008 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => "Бобро поржаловать!<br>Bienvenue chez les Ch'tis", 'id' => 391735],
@@ -116,12 +100,10 @@
       ['title' => 'ВАЛЛ·И<br>WALL·E', 'id' => 279102],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2007 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2007 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Залечь на дно в Брюгге<br>In Bruges', 'id' => 276295],
@@ -131,12 +113,10 @@
       ['title' => 'Теория большого взрыва<br>The Big Bang Theory', 'id' => 306084],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2006 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2006 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Жизнь других<br>Das Leben der Anderen', 'id' => 126196],
@@ -144,58 +124,48 @@
       ['title' => 'Престиж<br>The Prestige', 'id' => 195334],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2005 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2005 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Счастливое число Слевина<br>Lucky Number Slevin', 'id' => 86326],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2004 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2004 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Вечное сияние чистого разума<br>Eternal Sunshine of the Spotless Mind', 'id' => 5492],
       ['title' => 'Терминал<br>Terminal', 'id' => 6877],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2002 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2002 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Поймай меня, если сможешь<br>Catch Me If You Can', 'id' => 324],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">2000 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">2000 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Изгой<br>Cast Away', 'id' => 627],
       ['title' => 'Большой куш<br>Snatch', 'id' => 526],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1999 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1999 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Бойцовский клуб<br>Fight Club', 'id' => 361],
@@ -203,62 +173,52 @@
       ['title' => 'Шестое чувство<br>The Sixth Sense', 'id' => 395],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1998 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1998 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Карты, деньги, два ствола<br>Lock, Stock and Two Smoking Barrels', 'id' => 522],
       ['title' => 'Шоу Трумана<br>The Truman Show', 'id' => 4541],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1997 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1997 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Секреты Лос-Анджелеса<br>L.A. Confidential', 'id' => 363],
       ['title' => 'Игра<br>The Game', 'id' => 12198],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1996 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1996 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Заводила<br>Kingpin', 'id' => 4343],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1993 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1993 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'День сурка<br>Groundhog Day', 'id' => 527],
     ]
   ])
-</section>
+</div>
 
-<section class="movies-container">
-  <div class="page-header">
-    <div class="h2">1991 год</div>
-  </div>
+<div class="movies-container">
+  <div class="h2">1991 год</div>
   @include('tpl.kp_movies', [
     'movies' => [
       ['title' => 'Терминатор 2: Судный день<br>Terminator 2: Judgement Day', 'id' => 444],
     ]
   ])
-</section>
+</div>
 @endsection
