37fc31fe5a94212d3cd7c22cbe20cd4903e44a4a
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-18 08:54:11 +0300

    Правка отступов

--- gulpfile.js
+++ gulpfile.js
@@ -17,57 +17,57 @@ elixir.config.js.browserify.transformers.push({
 elixir(function(mix) {
   mix.sass('app.scss')
 
-      .styles([
-          'bower/nprogress/nprogress.css',
-          'bower/fotorama/fotorama.css',
-      ], 'public/build/css/vendor.css', './resources/assets')
+    .styles([
+      'bower/nprogress/nprogress.css',
+      'bower/fotorama/fotorama.css'
+    ], 'public/build/css/vendor.css', './resources/assets')
 
-      .styles([
-        'bower/fancytree/dist/skin-lion/ui.fancytree.min.css'
-      ], 'public/build/css/acp.css', './resources/assets')
+    .styles([
+      'bower/fancytree/dist/skin-lion/ui.fancytree.min.css'
+    ], 'public/build/css/acp.css', './resources/assets')
 
-      .browserify('app.js')
+    .browserify('app.js')
 
-      .copy('resources/assets/bower/fotorama/*.png', 'public/build/css')
-      .copy('resources/assets/bower/fancytree/dist/skin-lion/*.gif', 'public/build/css')
+    .copy('resources/assets/bower/fotorama/*.png', 'public/build/css')
+    .copy('resources/assets/bower/fancytree/dist/skin-lion/*.gif', 'public/build/css')
 
-      .scripts([
-          'bower/jquery/dist/jquery.min.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/modal.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
-          'bower/bootstrap-sass/assets/javascripts/bootstrap/affix.js',
-          'bower/nprogress/nprogress.js',
-          'js/fotorama-settings.js',
-          'bower/fotorama/fotorama.js',
-          'bower/jquery.scrollTo/jquery.scrollTo.min.js',
-          'bower/floatThead/dist/jquery.floatThead.min.js',
-          'bower/jquery-pjax/jquery.pjax.js'
-      ], 'public/build/js/vendor.js', './resources/assets')
+    .scripts([
+      'bower/jquery/dist/jquery.min.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/modal.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
+      'bower/bootstrap-sass/assets/javascripts/bootstrap/affix.js',
+      'bower/nprogress/nprogress.js',
+      'js/fotorama-settings.js',
+      'bower/fotorama/fotorama.js',
+      'bower/jquery.scrollTo/jquery.scrollTo.min.js',
+      'bower/floatThead/dist/jquery.floatThead.min.js',
+      'bower/jquery-pjax/jquery.pjax.js'
+    ], 'public/build/js/vendor.js', './resources/assets')
 
-      .scripts([
-          'bower/jquery-ui/ui/minified/core.js',
-          'bower/jquery-ui/ui/minified/widget.js',
-          'bower/jquery-ui/ui/widgets/mouse.js',
-          'bower/jquery-ui/ui/minified/position.js',
-          'bower/jquery-ui/ui/widgets/selectable.js',
-          'bower/jquery-ui/ui/widgets/draggable.js',
-          'bower/jquery-ui/ui/widgets/droppable.js',
-          'bower/jquery-ui/ui/widgets/sortable.js',
-          'bower/fancytree/dist/jquery.fancytree-all.min.js'
-      ], 'public/build/js/acp.js', './resources/assets');
+    .scripts([
+      'bower/jquery-ui/ui/minified/core.js',
+      'bower/jquery-ui/ui/minified/widget.js',
+      'bower/jquery-ui/ui/widgets/mouse.js',
+      'bower/jquery-ui/ui/minified/position.js',
+      'bower/jquery-ui/ui/widgets/selectable.js',
+      'bower/jquery-ui/ui/widgets/draggable.js',
+      'bower/jquery-ui/ui/widgets/droppable.js',
+      'bower/jquery-ui/ui/widgets/sortable.js',
+      'bower/fancytree/dist/jquery.fancytree-all.min.js'
+    ], 'public/build/js/acp.js', './resources/assets');
 
-      if (elixir.config.production) {
-        mix.version([
-          'css/acp.css',
-          'css/app.css',
-          'css/vendor.css',
-          'js/acp.js',
-          'js/app.js',
-          'js/vendor.js'
-        ]);
-      }
+  if (elixir.config.production) {
+    mix.version([
+      'css/acp.css',
+      'css/app.css',
+      'css/vendor.css',
+      'js/acp.js',
+      'js/app.js',
+      'js/vendor.js'
+    ]);
+  }
 });
