0e6f9cf0ed3b8311cff5121345e23b173fdee1a4
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-20 16:24:46 +0300

    Правка отступов

--- resources/views/life/trips/base.blade.php
+++ resources/views/life/trips/base.blade.php
@@ -5,80 +5,80 @@
 ])
 
 @section('content_header')
-  @parent
+@parent
+<h2>
+  <span class="emoji">{{ $trip->city->country->emoji }}</span>
+  {{ $trip->title }}
+</h2>
+{{--
+@if ($trip->meta_image)
+  <div class="cover">
+    <div class="cover-image" style="background-image: url({{ $trip->meta_image }});"></div>
+    <div class="cover-title">
+      <span class="emoji">{{ $trip->city->country->emoji }}</span>
+      {{ $trip->title }}
+      <div class="cover-description">{{ $trip->getMetaDescription() }}</div>
+      <div class="cover-meta">
+        {{ $trip->getLocalizedDate() }}
+        &nbsp;
+        <span class="emoji">{{ $trip->city->country->emoji }}</span>
+        {{ $trip->city->country->title }}
+      </div>
+    </div>
+  </div>
+@else
   <h2>
     <span class="emoji">{{ $trip->city->country->emoji }}</span>
     {{ $trip->title }}
   </h2>
-  {{--
-  @if ($trip->meta_image)
-    <div class="cover">
-      <div class="cover-image" style="background-image: url({{ $trip->meta_image }});"></div>
-      <div class="cover-title">
-        <span class="emoji">{{ $trip->city->country->emoji }}</span>
-        {{ $trip->title }}
-        <div class="cover-description">{{ $trip->getMetaDescription() }}</div>
-        <div class="cover-meta">
-          {{ $trip->getLocalizedDate() }}
-          &nbsp;
-          <span class="emoji">{{ $trip->city->country->emoji }}</span>
-          {{ $trip->city->country->title }}
-        </div>
-      </div>
-    </div>
-  @else
-    <h2>
-      <span class="emoji">{{ $trip->city->country->emoji }}</span>
-      {{ $trip->title }}
-    </h2>
-  @endif
-  --}}
-  <div class="trip-text js-trip-shortcuts">
+@endif
+--}}
+<div class="trip-text js-trip-shortcuts trip-lang-{{ $locale }}">
 @endsection
 
 @section('content_footer')
-  </div>
-  <div class="travel-timeline-container">
-    @if (sizeof($previous_trips))
-      @foreach ($previous_trips as $previous)
-        <div class="travel-timeline">
-          <div class="travel-timeline-date">
-            {{ $previous->period }}
-            @if ($previous->year !== $trip->year)
-              {{ $previous->year }}
-            @endif
-          </div>
-          <a class="link" href="/life/{{ $previous->slug }}">{{ $previous->title }}</a>
+</div>
+<div class="travel-timeline-container">
+  @if (sizeof($previous_trips))
+    @foreach ($previous_trips as $previous)
+      <div class="travel-timeline">
+        <div class="travel-timeline-date">
+          {{ $previous->period }}
+          @if ($previous->year !== $trip->year)
+            {{ $previous->year }}
+          @endif
         </div>
-      @endforeach
-    @endif
-    <div class="travel-timeline travel-timeline-current">
-      <div class="travel-timeline-date">{{ $trip->period }} {{ $trip->year }}</div>
-      <strong>{{ $trip->title }}</strong>
-      <div class="travel-timeline-arrows">
-        @if (sizeof($previous_trips))
-          <span class="horizontal">&larr;</span>
-          <span class="vertical">&darr;</span>
-        @endif
-        @if (sizeof($next_trips))
-          <span class="horizontal">&rarr;</span>
-          <span class="vertical">&uarr;</span>
-        @endif
+        <a class="link" href="/life/{{ $previous->slug }}">{{ $previous->title }}</a>
       </div>
+    @endforeach
+  @endif
+  <div class="travel-timeline travel-timeline-current">
+    <div class="travel-timeline-date">{{ $trip->period }} {{ $trip->year }}</div>
+    <strong>{{ $trip->title }}</strong>
+    <div class="travel-timeline-arrows">
+      @if (sizeof($previous_trips))
+        <span class="horizontal">&larr;</span>
+        <span class="vertical">&darr;</span>
+      @endif
+      @if (sizeof($next_trips))
+        <span class="horizontal">&rarr;</span>
+        <span class="vertical">&uarr;</span>
+      @endif
     </div>
-    @if (sizeof($next_trips))
-      @foreach ($next_trips as $next)
-        <div class="travel-timeline">
-          <div class="travel-timeline-date">
-            {{ $next->period }}
-            @if ($next->year !== $trip->year)
-              {{ $next->year }}
-            @endif
-          </div>
-          <a class="link" href="/life/{{ $next->slug }}">{{ $next->title }}</a>
-        </div>
-      @endforeach
-    @endif
   </div>
-  @parent
+  @if (sizeof($next_trips))
+    @foreach ($next_trips as $next)
+      <div class="travel-timeline">
+        <div class="travel-timeline-date">
+          {{ $next->period }}
+          @if ($next->year !== $trip->year)
+            {{ $next->year }}
+          @endif
+        </div>
+        <a class="link" href="/life/{{ $next->slug }}">{{ $next->title }}</a>
+      </div>
+    @endforeach
+  @endif
+</div>
+@parent
 @endsection
