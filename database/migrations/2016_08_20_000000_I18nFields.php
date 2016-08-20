003f43df4e4d1d6d8ae9fe8df34c1ff31bf17a2f
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-20 11:07:03 +0300

    Правка миграции

--- database/migrations/2016_08_20_000000_I18nFields.php
+++ database/migrations/2016_08_20_000000_I18nFields.php
@@ -8,17 +8,17 @@ class I18nFields extends Migration
 	public function up()
 	{
         Schema::table('cities', function (Blueprint $table) {
-            $table->string('title_en')->after('title');
+            $table->string('title_en')->after('title_ru');
         });
 
 		Schema::table('countries', function (Blueprint $table) {
-            $table->string('title_en')->after('title');
+            $table->string('title_en')->after('title_ru');
 		});
 
         Schema::table('trips', function (Blueprint $table) {
-            $table->string('title_en')->after('title');
-            $table->string('meta_title_en')->after('meta_title');
-            $table->string('meta_description_en')->after('meta_description');
+            $table->string('title_en')->after('title_ru');
+            $table->string('meta_title_en')->after('meta_title_ru');
+            $table->string('meta_description_en')->after('meta_description_ru');
         });
 	}
 
