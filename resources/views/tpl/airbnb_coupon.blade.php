34cfae0a13d0d2055a554f6ecb2c80f164aa75d5
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-21 06:12:57 +0300

    Локализация сообщения о купоне airbnb

--- resources/views/tpl/airbnb_coupon.blade.php
+++ resources/views/tpl/airbnb_coupon.blade.php
@@ -1,1 +1,2 @@
-<p>На сервисе <a class="link" href="https://www.airbnb.ru/c/spankov1?s=8">Airbnb</a> по купону <code>{{ $coupon }}</code> можно получить скидку в 10 евро при бронировании жилья в {{ $city }}.</p>
+<p lang="ru">На сервисе <a class="link" href="https://www.airbnb.ru/c/spankov1?s=8">Airbnb</a> по купону <code>{{ $coupon }}</code> можно получить скидку в 10 евро при бронировании жилья в {{ $city }}.</p>
+<p lang="en">You can get &euro;10 discount on <a class="link" href="https://www.airbnb.com/c/spankov1?s=8">Airbnb</a> with <code>{{ $coupon }}</code> coupon code.</p>
