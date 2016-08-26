81c0da52774bb9836b0f05596565e8fbf3abc22e
Sergey Pankov <vacuum@ivacuum.ru>
2016-08-26 10:57:58 +0300

    Ускорение тестов путем создания пользователей без пароля

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\User::class, function ($faker) {
     return [
         'name'           => $faker->name,
         'email'          => $faker->email,
-        'password'       => str_random(10),
+        // 'password'       => str_random(10),
         'remember_token' => str_random(10),
     ];
 });
