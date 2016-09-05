6f3826574a10ae5dd2626231a57aa1bd6ef8594f
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-05 10:22:06 +0300

    Новый внешний вид фильтров доменов

--- resources/views/acp/domains/index.blade.php
+++ resources/views/acp/domains/index.blade.php
@@ -3,56 +3,41 @@
 ])
 
 @section('content')
-<ul class="list-inline" style="margin: 0 0 0.5em;">
-  <li><h4>Домены:</h4></li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => '', 'page' => 1]) }}" class="link js-pjax {{ !$filter ? 'active' : '' }}">
-        На мониторинге
-      </a>
-    </h4>
+<ul class="nav nav-link-tabs">
+  <li class="{{ !$filter ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => '', 'page' => 1]) }}">
+      На мониторинге
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'orphan', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'orphan' ? 'active' : '' }}">
-        На продажу
-      </a>
-    </h4>
+  <li class="{{ $filter == 'orphan' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'orphan', 'page' => 1]) }}">
+      На продажу
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'external', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'external' ? 'active' : '' }}">
-        Не в нашей панели reg.ru
-      </a>
-    </h4>
+  <li class="{{ $filter == 'external' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'external', 'page' => 1]) }}">
+      Не в нашей панели reg.ru
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'no-server', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'no-server' ? 'active' : '' }}">
-        Без сервера
-      </a>
-    </h4>
+  <li class="{{ $filter == 'no-server' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'no-server', 'page' => 1]) }}">
+      Без сервера
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'no-ns', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'no-ns' ? 'active' : '' }}">
-        Без NS
-      </a>
-    </h4>
+  <li class="{{ $filter == 'no-ns' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'no-ns', 'page' => 1]) }}">
+      Без NS
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'inactive', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'inactive' ? 'active' : '' }}">
-        Неактивные
-      </a>
-    </h4>
+  <li class="{{ $filter == 'inactive' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'inactive', 'page' => 1]) }}">
+      Неактивные
+    </a>
   </li>
-  <li>
-    <h4>
-      <a href="{{ Request::fullUrlWithQuery(['filter' => 'trashed', 'page' => 1]) }}" class="link js-pjax {{ $filter == 'trashed' ? 'active' : '' }}">
-        Удаленные
-      </a>
-    </h4>
+  <li class="{{ $filter == 'trashed' ? 'active' : '' }}">
+    <a class="js-pjax" href="{{ Request::fullUrlWithQuery(['filter' => 'trashed', 'page' => 1]) }}">
+      Удаленные
+    </a>
   </li>
 </ul>
 
