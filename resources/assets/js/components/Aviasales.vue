e1d912e8ff8d2c9a3f5c8de4c2b6aefa624e69f5
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-05 10:27:52 +0300

    Как мог бы выглядеть компонент на vue.js

---
+++ resources/assets/js/components/Aviasales.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p class="lead">А еще лучше слетать <a class="link pseudo" @click.prevent="aviasales">повидать новый город</a>.</p>
+    <div id="aviasales_container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  ready() {
+    console.log('Aviasales ready.');
+  },
+
+  methods: {
+    aviasales(e) {
+      $(e.target).contents().unwrap();
+
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.src = 'https://www.travelpayouts.com/widgets/044c854e39d539701be0fa773757da42.js?v=443';
+      s.async = true;
+      document.getElementById('aviasales_container').appendChild(s);
+    }
+  }
+};
+</script>
