99570e2b6d48df38826e9fc156039fb1c4bb7709
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-11 00:21:06 +0300

    Рефакторинг вывода инфы о купоне airbnb

--- resources/views/tpl/airbnb_coupon.blade.php
+++ resources/views/tpl/airbnb_coupon.blade.php
@@ -1,2 +1,5 @@
-<p lang="ru">На сервисе <a class="link" href="https://www.airbnb.ru/c/spankov1?s=8">Airbnb</a> по купону <code>{{ $coupon }}</code> можно получить скидку в 10 евро при бронировании жилья в {{ $city }}.</p>
-<p lang="en">You can get &euro;10 discount on <a class="link" href="https://www.airbnb.com/c/spankov1?s=8">Airbnb</a> with <code>{{ $coupon }}</code> coupon code.</p>
+@ru
+  <p>На сервисе <a class="link" href="https://www.airbnb.ru/c/spankov1?s=8">Airbnb</a> по купону <code>{{ $coupon }}</code> можно получить скидку в 10 евро при бронировании жилья в {{ $city }}.</p>
+@en
+  <p>You can get &euro;10 discount on <a class="link" href="https://www.airbnb.com/c/spankov1?s=8">Airbnb</a> with <code>{{ $coupon }}</code> coupon code.</p>
+@endlang
