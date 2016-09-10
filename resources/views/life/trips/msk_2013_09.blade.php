ef06163f9aa03b46e082cf12c80514624240e4b7
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-11 02:22:33 +0300

    Полный перевод заметки на английский

--- resources/views/life/trips/msk_2013_09.blade.php
+++ resources/views/life/trips/msk_2013_09.blade.php
@@ -1,40 +1,60 @@
 @extends('life.trips.base')
 
 @section('content')
-<p lang="ru">Информативное объяснение стройки.</p>
-<div class="img-container">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0197.jpg" width="1000" height="750">
-</div>
+@ru
+  <p>Информативное объяснение стройки.</p>
+@en
+  <p>Good explanation of what's being constructed. "Subway is growing! The first train from Petrovsky park station is departing in late 2015."</p>
+@endlang
+@include('tpl.pic', ['pic' => 'IMG_0197.jpg'])
 
-<p lang="ru">Хитрец избегает оплаты за парковку.</p>
-<div class="img-container">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0198.jpg" width="1000" height="750">
-</div>
+@ru
+  <p>Хитрец избегает оплаты за парковку.</p>
+@en
+  <p>Sneaky driver avoids parking payment.</p>
+@endlang
+@include('tpl.pic', ['pic' => 'IMG_0198.jpg'])
 
-<p lang="ru">На пути в Государственный Кремлевский дворец — он справа от башни.</p>
-<div class="img-container">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0199.jpg" width="1000" height="750">
-</div>
+@ru
+  <p>На пути в Государственный Кремлевский дворец — он справа от башни.</p>
+@en
+  <p>On the way to the State Kremlin Palace, it's to the right of the tower.</p>
+@endlang
+@include('tpl.pic', ['pic' => 'IMG_0199.jpg'])
 
-<p lang="ru">Внутри <abbr title="Государственного Кремлевского дворца">ГКД</abbr>.</p>
-<div class="img-container">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0201.jpg" width="1000" height="750">
-</div>
+@ru
+  <p>Внутри <abbr title="Государственного Кремлевского дворца">ГКД</abbr>.</p>
+@en
+  <p>Inside the <abbr title="State Kremlin Palace">SKP</abbr>.</p>
+@endlang
+@include('tpl.pic', ['pic' => 'IMG_0201.jpg'])
 
-<p lang="ru">Кульминация вечера — предпоказ фильма Metallica: Through the Never. Приехали только ударник и басист. В лучших традициях знаменитостей <strike>опоздали</strike> задержались на пару часов. Красная ковровая дорожка, все как полагается. Ведущим был Иван Охлобыстин, слышно его было прекрасно во всем зале даже без микрофона. Зрители первых рядов получили возможность задать участникам группы вопросы, большинство из которых, впрочем, было ни о чем. Фильм показывали на английском языке с субтитрами, давно хотелось увидеть фильм в кино в таком формате. Во время самого показа по залу ходила охрана и препятствовала съемке. Звук в зале был потрясающий — очень близкий к <a href="/life/metallica.2008" class="link">концертному</a>. Довелось позднее сравнить со звуком в кинотеатре в Калуге. В последнем уши хотелось скорей заткнуть чем-нибудь или выбежать из зала — хорошо, что я на минуту всего в зал заглянул. Кстати, на предпоказе была 2D версия, а в широкий прокат фильм вышел в 3D.</p>
+@ru
+  <p>Кульминация вечера — предпоказ фильма Metallica: Through the Never. Приехали только ударник и басист. В лучших традициях знаменитостей <strike>опоздали</strike> задержались на пару часов. Красная ковровая дорожка, все как полагается. Ведущим был Иван Охлобыстин, слышно его было прекрасно во всем зале даже без микрофона. Зрители первых рядов получили возможность задать участникам группы вопросы, большинство из которых, впрочем, было ни о чем. Фильм показывали на английском языке с субтитрами, давно хотелось увидеть фильм в кино в таком формате. Во время самого показа по залу ходила охрана и препятствовала съемке. Звук в зале был потрясающий — очень близкий к <a class="link" href="/life/metallica.2008">концертному</a>. Довелось позднее сравнить со звуком в кинотеатре в Калуге. В последнем уши хотелось скорей заткнуть чем-нибудь или выбежать из зала — хорошо, что я на минуту всего в зал заглянул. Кстати, на предпоказе была 2D версия, а в широкий прокат фильм вышел в 3D.</p>
+@en
+  <p>The culmination of the evening was presentation of the movie Metallica: Through the Never. Just band's drummer and bassist were there. They were late for the start for few hours like most of the celebrities do. Red carpet — all as it should be. The movie was streamed in english with russian subtitles, I always wanted to watch a movie in this format in the cinema. Sound in Kremlin palace was outstanding, very close to the <a class="link" href="/en/life/metallica.2008">concert</a> one. By the way, the movie was presented in 2D that day, but later in the cinemas it was available only in 3D.</p>
+@endlang
 <div class="fotorama">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0203.jpg">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0204.jpg">
-  <img src="//life.ivacuum.ru/msk.2013.09/IMG_0205.jpg">
+  <img src="https://life.ivacuum.ru/msk.2013.09/IMG_0203.jpg">
+  <img src="https://life.ivacuum.ru/msk.2013.09/IMG_0204.jpg">
+  <img src="https://life.ivacuum.ru/msk.2013.09/IMG_0205.jpg">
 </div>
 
-<p lang="ru">В определенный момент у переводчика возникли трудности с объяснением сложных оборотов речи Охлобыстина, это можно увидеть в видео ниже.</p>
-<div class="fotorama" data-width="1280" data-ratio="16/10">
+@ru
+  <p>В определенный момент у переводчика возникли трудности с объяснением сложных оборотов речи Охлобыстина, это можно увидеть в видео ниже.</p>
+@en
+  <p>It was difficult for the translator to explain host's question to the band's members at some point. It can be seen in the video below.</p>
+@endlang
+<div class="fotorama" data-width="1000" data-ratio="1000/595">
   <a href="https://www.youtube.com/watch?v=FP28g9HijzQ"></a>
 </div>
 
-<p lang="ru">Песня One из фильма.</p>
-<div class="fotorama" data-width="1280" data-ratio="16/10">
+@ru
+  <p>Песня One из фильма.</p>
+@en
+  <p>Song "One" from the movie.</p>
+@endlang
+<div class="fotorama" data-width="1000" data-ratio="1000/450">
   <a href="https://www.youtube.com/watch?v=tbFZO1V5hrE"></a>
 </div>
 @endsection
