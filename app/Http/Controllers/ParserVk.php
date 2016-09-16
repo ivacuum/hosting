c46f4aa8185bd3c9b2e4e18b0ac071be220fd2b1
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-16 15:51:32 +0300

    Улучшение парсера ВК
    — Возможность запроса стены по owner_id
    — Скрытие ссылки на дату в будущем
    — Пропуск пустых дней при навигации назад
    — Возможность скрыть репосты
    — При пролистывании стены до начала времен скрипт не зависает

--- app/Http/Controllers/ParserVk.php
+++ app/Http/Controllers/ParserVk.php
@@ -1,6 +1,5 @@
 <?php namespace App\Http\Controllers;
 
-use Cache;
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 
@@ -19,25 +18,38 @@ class ParserVk extends Controller
     public function index($vkpage = 'pn6', $date = false)
     {
         $this->vkpage = $vkpage;
+        $own = $this->request->input('own');
         $date = false === $date ? '-1 day' : $date;
         $date = Carbon::parse($date);
 
         $count = 100;
         $offset = 0;
+        $total = 0;
         $parsed = false;
-        $posts = [];
+        $posts = collect();
 
         $date_start = Carbon::parse($date)->startOfDay()->timestamp;
         $date_end = Carbon::parse($date)->endofDay()->timestamp;
+        $previous = Carbon::parse($date)->subDay();
+        $next = Carbon::now()->startOfDay()->gt($date) ? Carbon::parse($date)->addDay() : null;
 
         while (false === $parsed) {
             $json = $this->getPosts($count, $offset);
 
+            if (isset($json->error)) {
+                dd($json->error);
+            }
+
+            $json = $json->response;
+
+            $total = $total ?: $json[0];
+
             // В первом элементе количество записей
-            for ($i = 1, $len = sizeof($json->response) - 1; $i < $len; $i++) {
-                $post = $json->response[$i];
+            for ($i = 1, $len = sizeof($json) - 1; $i < $len; $i++) {
+                $post = $json[$i];
 
                 if (@!$post->is_pinned && $post->date < $date_start) {
+                    $previous = Carbon::createFromTimestamp($post->date);
                     $parsed = true;
                     break 2;
                 }
@@ -56,27 +68,43 @@ class ParserVk extends Controller
                     }
                 }
 
-                $posts[] = [
+                $posts->push([
                     'likes'       => $post->likes->count,
                     'reposts'     => $post->reposts->count,
                     'url'         => "https://vk.com/wall{$post->to_id}_{$post->id}",
                     'text'        => $post->text,
+                    'type'        => $post->post_type,
                     'photos'      => $photos,
                     'attachment'  => @$post->attachment,
                     'attachments' => @$post->attachments,
-                ];
+                ]);
+            }
+
+            if ($offset + $i === $total) {
+                $previous = null;
+                $parsed = true;
+                break;
             }
 
             $offset += $count;
         }
 
-        rsort($posts);
+        if ($own) {
+            $posts = $posts->reject(function ($post) {
+                return $post['type'] === 'copy';
+            });
+        }
 
-        $posts = array_slice($posts, 0, 10);
+        $posts = $posts->sortByDesc('likes')->take(10);
 
-        return view('parser.vk', compact('posts', 'vkpage', 'date'))
-            ->withPrevious(Carbon::parse($date)->subDay())
-            ->withNext(Carbon::parse($date)->addDay());
+        return view('parser.vk', compact(
+            'date',
+            'next',
+            'own',
+            'posts',
+            'previous',
+            'vkpage'
+        ));
     }
 
     public function indexPost()
@@ -87,10 +115,16 @@ class ParserVk extends Controller
     protected function getPosts($count = 100, $offset = 0)
     {
         $cache_entry = "vk_{$this->vkpage}_{$count}_{$offset}";
-        $domain = $this->vkpage;
-        $params = compact('domain', 'count', 'offset');
+        $filter = 'owner';
+        $params = compact('count', 'filter', 'offset');
 
-        return Cache::remember($cache_entry, 15 + intval($offset / 100), function () use ($params) {
+        if (is_numeric($this->vkpage)) {
+            $params['owner_id'] = $this->vkpage;
+        } else {
+            $params['domain'] = $this->vkpage;
+        }
+
+        return \Cache::remember($cache_entry, 15 + intval($offset / 100), function () use ($params) {
             $response = $this->client->get('wall.get', ['query' => $params]);
 
             return json_decode($response->getBody());
