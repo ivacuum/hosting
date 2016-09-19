b3fdade601bd573c4c832b4c74d07dc818fa0cfd
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-19 11:42:22 +0300

    Возможность оставить отзыв к концертам

--- resources/views/life/trips/base.blade.php
+++ resources/views/life/trips/base.blade.php
@@ -91,30 +91,5 @@
 @endsection
 
 @push('js')
-<div class="modal js-modal-feedback" tabindex="-1">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form action="{{ action('Ajax@feedback') }}" method="post">
-        <input hidden type="text" name="mail" value="{{ old('mail') }}">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">
-            @svg (times)
-          </button>
-          <h4 class="modal-title">{{ trans('life.feedback.heading') }}</h4>
-        </div>
-        <div class="modal-body">
-          <div hidden class="h4 m-t-0 js-modal-question-text"></div>
-          <input hidden class="js-modal-question-input" type="text" name="question" value="">
-          <textarea required class="form-control textarea-autosized js-autosize-textarea" name="text" rows="5" placeholder="{{ trans('life.feedback.placeholder') }}"></textarea>
-          <p class="help-block">{{ trans('life.feedback.help') }}</p>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-primary">
-            {{ trans('life.feedback.submit') }}
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+@include('tpl.modal-feedback')
 @endpush
