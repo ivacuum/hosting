0a2a85fe614f5ceb7fbd2082600e444aa6e494e2
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-19 11:44:54 +0300

    Без ; в конце js тоже работает

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,131 +1,131 @@
-import Map from './map';
-import Pjax from './pjax';
+import Map from './map'
+import Pjax from './pjax'
 
-import './events';
-import './life';
+import './events'
+import './life'
 import './modal-feedback'
-import './shortcuts';
-import './yandex-dns';
+import './shortcuts'
+import './yandex-dns'
 
 class Application {
   constructor() {
     this.locale = window['AppOptions'].locale
-    this.map = new Map(this.locale);
-    this.pjax = new Pjax();
+    this.map = new Map(this.locale)
+    this.pjax = new Pjax()
 
-    this.ajaxProgress();
-    this.csrfToken();
-    this.onPjaxComplete();
-    this.onPjaxSend();
+    this.ajaxProgress()
+    this.csrfToken()
+    this.onPjaxComplete()
+    this.onPjaxSend()
 
     $(document).ready(() => {
-      this.lazyLoadImages();
-      this.initOnReadyAndPjax();
-    });
+      this.lazyLoadImages()
+      this.initOnReadyAndPjax()
+    })
   }
 
   ajaxProgress() {
-    $(document).ajaxStart(() => NProgress.start());
-    $(document).ajaxStop(() => NProgress.done());
+    $(document).ajaxStart(() => NProgress.start())
+    $(document).ajaxStop(() => NProgress.done())
   }
 
   autosizeTextareas() {
-    autosize($('.js-autosize-textarea'));
+    autosize($('.js-autosize-textarea'))
   }
 
   csrfToken() {
     $.ajaxSetup({
       headers: { 'X-CSRF-TOKEN': window['AppOptions'].csrfToken }
-    });
+    })
   }
 
   fotoramaInit(pjax = false) {
     if (pjax) {
-      $('.fotorama').fotorama();
+      $('.fotorama').fotorama()
     }
 
-    this.fotorama2x();
+    this.fotorama2x()
 
-    $('.js-fotorama-2x').fotorama();
+    $('.js-fotorama-2x').fotorama()
   }
 
   fotorama2x() {
-    const breakpoint = 1200;
+    const breakpoint = 1200
 
-    let width = $(window).width();
+    let width = $(window).width()
 
     if (width >= breakpoint) {
       $('.js-fotorama-2x a').each(function() {
-        $(this).attr('href', $(this).data('src2x'));
-      });
+        $(this).attr('href', $(this).data('src2x'))
+      })
     }
   }
 
   lazyLoadImages() {
-    const offset = 400;
-    const breakpoint = 1200;
+    const offset = 400
+    const breakpoint = 1200
 
     let $w = $(window),
         $body = $(document.body),
         images,
-        timer;
+        timer
 
-    const width = $w.width();
+    const width = $w.width()
 
     function initLazyLoad() {
-      images = $('.js-lazy');
+      images = $('.js-lazy')
 
-      $w.off('.js-lazy').on('scroll.js-lazy resize.js-lazy', () => timer || (timer = setTimeout(performLazyLoad, 150)));
+      $w.off('.js-lazy').on('scroll.js-lazy resize.js-lazy', () => timer || (timer = setTimeout(performLazyLoad, 150)))
 
-      performLazyLoad();
+      performLazyLoad()
     }
 
     function performLazyLoad() {
-      const scrolled = $w.scrollTop() + $w.height() + offset;
+      const scrolled = $w.scrollTop() + $w.height() + offset
 
       images = images.filter(function() {
-        let e = $(this);
-        const src = width > breakpoint ? (e.data('src2x') || e.data('src')) : e.data('src');
-        return scrolled > e.offset().top ? (e.attr("src", src).removeClass('js-lazy'), false) : true;
-      });
+        let e = $(this)
+        const src = width > breakpoint ? (e.data('src2x') || e.data('src')) : e.data('src')
+        return scrolled > e.offset().top ? (e.attr("src", src).removeClass('js-lazy'), false) : true
+      })
 
       if (!images.length) {
-        $w.off('.js-lazy');
+        $w.off('.js-lazy')
       }
 
-      clearTimeout(timer);
-      timer = 0;
+      clearTimeout(timer)
+      timer = 0
     }
 
-    $body.off('reset.js-lazy').on('reset.js-lazy', initLazyLoad);
+    $body.off('reset.js-lazy').on('reset.js-lazy', initLazyLoad)
 
-    initLazyLoad();
+    initLazyLoad()
   }
 
   initOnReadyAndPjax(pjax = false) {
     // Прилипшие заголовки таблиц
-    $('.js-float-thead').floatThead();
+    $('.js-float-thead').floatThead()
 
     // Подсказки
-    $('.tip').tooltip();
+    $('.tip').tooltip()
 
-    this.autosizeTextareas();
-    this.fotoramaInit(pjax);
+    this.autosizeTextareas()
+    this.fotoramaInit(pjax)
   }
 
   onPjaxComplete() {
     $(document).on('pjax:complete', () => {
-      this.pjax.onComplete();
+      this.pjax.onComplete()
 
-      $(document.body).trigger('reset.js-lazy');
+      $(document.body).trigger('reset.js-lazy')
 
-      this.initOnReadyAndPjax(true);
-    });
+      this.initOnReadyAndPjax(true)
+    })
   }
 
   onPjaxSend() {
-    $(document).on('pjax:send', () => this.pjax.onSend());
+    $(document).on('pjax:send', () => this.pjax.onSend())
   }
 }
 
-window.App = new Application;
+window.App = new Application
