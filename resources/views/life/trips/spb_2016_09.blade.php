a7e3afe552224d45c7916e1e81959115b0568b1d
Sergey Pankov <vacuum@ivacuum.ru>
2016-09-25 20:45:19 +0300

    Заготовки будущих историй

---
+++ resources/views/life/trips/spb_2016_09.blade.php
@@ -0,0 +1,10 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Вот уж не думал, что знание немецкого интерфейса стиральной машины пригодится в Питере.</p>
+@en
+  <p>Never thought that I would use knowledge of german washer ui in Saint Petersburg.</p>
+@endlang
+IMG_1378.jpg
+@endsection
