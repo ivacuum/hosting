ba718f2edd9e692014910edffaeed2eb985efbc8
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-11 20:41:06 +0300

    Заметка о Петербурге

--- resources/views/life/trips/spb_2016_09.blade.php
+++ resources/views/life/trips/spb_2016_09.blade.php
@@ -2,9 +2,60 @@
 
 @section('content')
 @ru
-  <p>Вот уж не думал, что знание немецкого интерфейса стиральной машины пригодится в Питере.</p>
+  <p>Шлагбаум перед эскалаторами в метро.</p>
 @en
-  <p>Never thought that I would use knowledge of german washer ui in Saint Petersburg.</p>
+  <p>Barrier in front of the escalator in the subway.</p>
 @endlang
-IMG_1378.jpg
+@include('tpl.pic-2x', ['pic' => 'IMG_1367.jpg'])
+
+@ru
+  <p>Не так давно открыли часть острова Новая Голландия.</p>
+@en
+  <p>New Holland island was opened recently.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1370.jpg'])
+
+@ru
+  <p>Осенние краски.</p>
+@en
+  <p>Fall colors.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1368.jpg'])
+
+@ru
+  <p>Правый дом выглядит плоским в таком ракурсе.</p>
+@en
+  <p>Building to the right looks flat from this angle.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1373.jpg'])
+
+@ru
+  <p>Река Фонтанка.</p>
+@en
+  <p>Fontanka river.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1375.jpg'])
+
+@ru
+  <p>Самое распространенное место угона велосипедов по моему опыту.</p>
+@en
+  <p>Most popular place to steal bicycles in my experience.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1377.jpg'])
+
+@ru
+  <p>Вот уж не думал, что знание <a class="link" href="/life/cologne.2016#washer">немецкого интерфейса стиральной машины</a> пригодится в Питере.</p>
+@en
+  <p>Never thought that I would use knowledge of <a class="link" href="/en/life/cologne.2016#washer">german washer user interface</a> in Saint Petersburg.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1378.jpg'])
+
+@ru
+  <p>Следующая остановка — Хельсинки.</p>
+@en
+  <p>Next stop is Helsinki.</p>
+@de
+  <p>Nächste Station ist Helsinki.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1380.jpg'])
 @endsection
