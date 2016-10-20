294c87c16db09cb9e86966fa978c714eb9c90243
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-20 04:35:10 +0300

    Минск в октябре 2016

---
+++ resources/views/life/trips/minsk_2016_10.blade.php
@@ -0,0 +1,136 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Проба нового московского аэропорта в городе Жуковский. Открылся в этом же 2016 году. Белавиа ежедневно летает в Минск.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2176.jpg'])
+
+@ru
+  <p>Листопад.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2183.jpg'])
+
+@ru
+  <p>Небольшой туман.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2184.jpg'])
+
+@ru
+  <p>Красивый очаг желтых листьев вокруг дерева.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2185.jpg'])
+
+@ru
+  <p>Знак «Гололедица».</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2187.jpg'])
+
+@ru
+  <p>Монструозный дом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2189.jpg'])
+
+@ru
+  <p>Плитка под прицелом x_X.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2197.jpg'])
+
+@ru
+  <p>Библиотека.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2202.jpg'])
+
+@ru
+  <p>Наверху смотровая площадка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2209.jpg'])
+
+@ru
+  <p>Ромовая <strike>баба</strike> бабища.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2193.jpg'])
+
+@ru
+  <p><strike>Бокал</strike> Бокальчик для винишка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2230.jpg'])
+
+@ru
+  <p>Никогда не обращал внимания на штрих-код, а по первым его цифрам легко определить страну-производитель товара. Россия 460–469, Белоруссия 481, Украина 482, Италия 80 и т.д.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2222.jpg'])
+
+@ru
+  <p>У второго и третьего сверху товаров код начинается на 460, а страна написана Польша. Расхожденьице. Код верный, а страну подписали неправильно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2227.jpg'])
+
+@ru
+  <p>Метка, чтобы стойку не угнали?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2226.jpg'])
+
+@ru
+  <p>И тут тоже?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2231.jpg'])
+
+@ru
+<p>Потеряли букву ю в алфавите.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2237.jpg'])
+
+@ru
+  <p>Ни много ни мало целый второй этаж посвящен мужской одежде! Женская расположилась на третьем.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2228.jpg'])
+
+@ru
+  <p>Делением на десять тысяч белорусы провели деноминацию национальной валюты, теперь в ходу новые купюры, а также вернулись в обиход монеты. Какую бумажку не возьмешь в руки, так та новенькая. И монеты снова обрели ценность — проезд в метро стоит 55 копеек.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2232.jpg'])
+
+@ru
+  <p>Универмаг «На Немиге» предоставляет возможность окунуться в советское время.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2233.jpg'])
+
+@ru
+  <p>Управление белорусской железной дороги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2235.jpg'])
+
+@ru
+<p>Проспект Независимости.</p>
+@en
+<p>Independence Avenue.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2236.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2188.jpg',
+  'IMG_2191.jpg',
+  'IMG_2194.jpg',
+  'IMG_2196.jpg',
+  'IMG_2203.jpg',
+  'IMG_2221.jpg',
+  'IMG_2242.jpg',
+  'IMG_2192.jpg',
+]])
+
+@ru
+<p>Оформление весов в аэропорту напоминает таковые в детстве на продуктовом рынке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2246.jpg'])
+
+@ru
+<p>Обратный полет на закате.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2247.jpg'])
+@endsection
