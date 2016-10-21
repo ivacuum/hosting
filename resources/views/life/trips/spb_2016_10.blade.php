55e7c4b948f9a269742035af1bcf9f5761025ae0
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-21 15:01:18 +0300

    Питер в октябре 2016

---
+++ resources/views/life/trips/spb_2016_10.blade.php
@@ -0,0 +1,45 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Река Фонтанка.</p>
+@en
+  <p>Fontanka river.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2166.jpg'])
+
+@ru
+  <p>Современный троллейбус.</p>
+@en
+  <p>Modern trolleybus.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2168.jpg'])
+
+@ru
+  <p>После Хельсинки недостает красного цвета листвы. Остается только смотреть на зеленую и желтую.</p>
+@en
+  <p>Miss trees with red leaves like in Helsinki. There are only green and yellow ones to look at.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2169.jpg'])
+
+@ru
+  <p>Рамы практически всех окон одного темного цвета, белый явно выделяется.</p>
+@en
+  <p>Window frames are mostly of dark color, the white ones are clearly stand out.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2173.jpg'])
+
+@ru
+  <p>Как и здесь.</p>
+@en
+  <p>Here too.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2170.jpg'])
+
+@ru
+  <p>Московский вокзал.</p>
+@en
+  <p>Moskovsky rail terminal.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2174.jpg'])
+@endsection
