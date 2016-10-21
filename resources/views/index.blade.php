342d05970de04737fcc5e1fad47a221f2c2d56d5
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-21 15:13:02 +0300

    Дата поездок на главной

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,10 @@
         <div class="page-block-cover">
           <div class="page-block-cover-image" style="background-image: linear-gradient(rgba(26, 26, 26, 0.1) 0%, rgba(26, 26, 26, 0.3) 50%), url({{ $trip->meta_image }});"></div>
           <div class="page-block-cover-info">
-            <div class="page-block-cover-title">{{ $trip->title }}</div>
+            <div class="page-block-cover-title">
+              {{ $trip->title }}
+              <span class="page-block-cover-date">{{ $trip->period }} {{ $trip->year }}</span>
+            </div>
             <div class="page-block-cover-description">{{ $trip->meta_description }}</div>
           </div>
           <a class="page-block-cover-link" href="{{ action('Life@page', $trip->slug) }}"><span></span></a>
