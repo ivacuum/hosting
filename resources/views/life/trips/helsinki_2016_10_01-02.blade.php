d89dde910aa15bd474f05533cfa8c3a0a4ebe5bf
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-23 20:30:17 +0300

    Заготовка для второго визита в Хельсинки

---
+++ resources/views/life/trips/helsinki_2016_10_01-02.blade.php
@@ -0,0 +1,7 @@
+@extends('life.trips.base')
+
+@section('content')
+<youtube title="Helsinki Railroad, October 2016" v="9m8GqENFUnA"></youtube>
+<youtube title="Helsinki Park, October 2016" v="-HysOT4rKk4"></youtube>
+<youtube title="Helsinki Pedestrian Bridge, October 2016" v="4ZNVmOUhfNY"></youtube>
+@endsection
