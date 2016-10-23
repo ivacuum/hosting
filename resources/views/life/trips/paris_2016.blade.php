aa5d6fbf80ea5f31ed6d3a6c39a8fca891cb0a50
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-23 20:29:41 +0300

    Обновление заготовки по Парижу

--- resources/views/life/trips/paris_2016.blade.php
+++ resources/views/life/trips/paris_2016.blade.php
@@ -1,4 +1,6 @@
 @extends('life.trips.base')
 
 @section('content')
+<youtube title="Paris Sunset, September 2016" v="txS910rrMQs"></youtube>
+<youtube title="Paris, Parc Clichy-Batignolles — Martin Luther King, September 2016" v="teU3i_W24R4"></youtube>
 @endsection
