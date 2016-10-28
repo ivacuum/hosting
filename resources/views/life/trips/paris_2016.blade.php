0baca2185b8a1bbf5afd34d81ff5989b9d1c88fb
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-28 23:47:25 +0300

    Париж в сентябре 2016

--- resources/views/life/trips/paris_2016.blade.php
+++ resources/views/life/trips/paris_2016.blade.php
@@ -1,6 +1,381 @@
 @extends('life.trips.base')
 
 @section('content')
-<youtube title="Paris Sunset, September 2016" v="txS910rrMQs"></youtube>
+@ru
+  <p>С вай-фаем в самолете время летит незаметно. На выходе из аэропорта Орли совсем неочевидно как добраться до нужной точки города, так как способов много и большинство из них с пересадками. За прямой <a class="link" href="http://en.lebusdirect.com/">автобус</a> до города стригут 12 евро (около 840 ₽) — сразу видно размах цен. Для сравнения в <a class="link" href="/life/msk">Москве</a> около 500 ₽, <a class="link" href="/life/minsk">Минске</a> — 100 ₽, <a class="link" href="/life/kaliningrad">Калининграде</a> — 70 ₽, <a class="link" href="/life/kazan">Казани</a> — 30 ₽, <a class="link" href="/life/spb">Питере</a> — 30 ₽, <a class="link" href="/life/kaluga">Калуге</a> — 16 ₽.</p>
+@endlang
+
+@ru
+  <p>Буква П какая-то. Поразительно подстрекает передавать послания преимущественно п-шками.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1494.jpg'])
+
+@ru
+  <p>Автобусная остановка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1497.jpg'])
+
+@ru
+  <p>Большинство зданий выполнено в едином стиле: серо-желтый цвет, черные чугунные ограды балконов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1508.jpg'])
+
+@ru
+  <p>Иногда попадаются гигантские балконы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1622.jpg'])
+
+@ru
+  <p>Хорошо окна моют.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1558.jpg'])
+
+@ru
+  <p>И тротуары тоже.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1574.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1679.jpg',
+  'IMG_1496.jpg',
+  'IMG_1533.jpg',
+  'IMG_1538.jpg',
+  'IMG_1554.jpg',
+  'IMG_1556.jpg',
+  'IMG_1557.jpg',
+  'IMG_1560.jpg',
+  'IMG_1607.jpg',
+  'IMG_1612.jpg',
+  'IMG_1627.jpg',
+  'IMG_1658.jpg',
+  'IMG_1659.jpg',
+  'IMG_1660.jpg',
+  'IMG_1661.jpg',
+  'IMG_1662.jpg',
+  'IMG_1651.jpg',
+]])
+
+@ru
+  <p>Места для прогулок.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1511.jpg',
+  'IMG_1521.jpg',
+  'IMG_1540.jpg',
+  'IMG_1567.jpg',
+  'IMG_1619.jpg',
+  'IMG_1623.jpg',
+]])
+
+@ru
+  <p>Подальше от центра дома меняют вид.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1550.jpg',
+  'IMG_1561.jpg',
+  'IMG_1630.jpg',
+  'IMG_1633.jpg',
+  'IMG_1634.jpg',
+  'IMG_1635.jpg',
+  'IMG_1637.jpg',
+]])
+
+@ru
+  <p>Шлагбаум нацелился и собирается выстрелить.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1512.jpg'])
+
+@ru
+  <p>Река Сена. На английском пишется Seine — читал на немецкий манер и недоумевал к чему это слово в предложениях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1516.jpg'])
+
+@ru
+  <p>В воде придерживаются левостороннего движения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1584.jpg'])
+
+@ru
+  <p>Раньше это была автомобильная набережная, теперь — пешеходная.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1587.jpg'])
+
+@ru
+  <p>Жара. Фонтан высох.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1523.jpg'])
+
+@ru
+  <p>Листьям тоже несладко приходится.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1526.jpg'])
+
+@ru
+  <p>В Калуге и Москве +7&deg;C, в Париже +25&deg;C.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1620.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1501.jpg',
+  'IMG_1569.jpg',
+  'IMG_1591.jpg',
+  'IMG_1599.jpg',
+  'IMG_1618.jpg',
+  'IMG_1626.jpg',
+  'IMG_1643.jpg',
+]])
+
+@ru
+  <p>Роскошные здания.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1505.jpg',
+  'IMG_1589.jpg',
+  'IMG_1608.jpg',
+  'IMG_1625.jpg',
+]])
+
+@ru
+  <p>Либхер не только холодильники делает. Самсунг вообще дома строит в Корее.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1536.jpg'])
+
+@ru
+  <p>Учебное заведение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1537.jpg'])
+
+@ru
+  <p>Безбарьерная среда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1541.jpg'])
+
+@ru
+  <p>Вид из темницы (на самом деле нет).</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1546.jpg'])
+
+@ru
+  <p>Зарядки для электромобилей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1555.jpg'])
+
+@ru
+  <p>Еще учебное заведение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1559.jpg'])
+
+@ru
+  <p>Чудо-куст.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1564.jpg'])
+
+@ru
+  <p>Строящийся дом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1570.jpg'])
+
+@ru
+  <p>Полицейский участок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1571.jpg'])
+
+@ru
+  <p>Переход дороги в два этапа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1572.jpg'])
+
+@ru
+  <p>Так много <a class="pseudo" data-toggle="feedback" data-target=".js-modal-feedback" data-question="Вытяжки на крыше?">вытяжек</a> на крыше?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1573.jpg'])
+
+@ru
+  <p>Уже знакомые после <a class="link" href="/life/cologne.2016#pacman">Кельна</a> персонажи из игры Пакман.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1575.jpg'])
+
+@ru
+  <p>Увлекательный многочасовой путь домой для парижан. Показательно, что в понедельник вечером автобус из аэропорта довез в город за сорок минут, а в пятницу вечером обратный путь занял более чем два с половиной часа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1577.jpg'])
+
+@ru
+  <p>Справа регулировщик движения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1621.jpg'])
+
+@ru
+  <p>Иногда дороги бывают относительно свободны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1578.jpg'])
+
+@ru
+  <p>Но чаще так.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1642.jpg'])
+
+@ru
+  <p>Где найти много граффити? На стенах вдоль железной дороги. Актуально для многих городов и стран.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1581.jpg'])
+
+@ru
+  <p>Можно попрактиковаться в переводе единиц измерения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1586.jpg'])
+
+@ru
+  <p>Зебру зажевало.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1597.jpg'])
+
+@ru
+  <p>Очередь в кинотеатр.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1600.jpg'])
+
+@ru
+  <p>Метро непривлекательное на вид, но в нем есть урны, в отличие от России. Еще нет предупреждения не опираться на двери в поезде, так как они полуавтоматические — нужно открывать самому.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1606.jpg'])
+
+@ru
+  <p>Зонтик на стене.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1609.jpg'])
+
+@ru
+  <p>Бумага сбоку выглядит позолоченной.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1613.jpg'])
+
+@ru
+  <p>Трапеза начинается с обеда в 12:00. Далее кафешки заполнены до самого вечера. Культура кушать вне дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1614.jpg'])
+
+@ru
+  <p>Завалялись дома громоздкие вещи, настала пора выбрасывать. Что делать? Пойти и отнести на помойку. Да, вариант. Но у парижан есть альтернативный выход: <a class="link" href="https://teleservices.paris.fr/sira/jsp/site/Portal.jsp?page=formengine&form=ramen">оставить заявку</a> на сайте мэрии, наклеить на вещи полученный номер и сложить их около дома. Городские службы уже на следующий день все заберут.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1615.jpg'])
+
+@ru
+  <p>В Париже из оборота выводят один и два цента, автоматы принимают не меньше пяти. В Германии постоянно в каждой сдаче есть цент, так как цены везде 1.99, 2.99 и т.п., что в итоге не знаешь куда их девать после десятка покупок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1616.jpg'])
+
+@ru
+  <ul class="list-unstyled">
+    <li>— На чем гоняешь?</li>
+    <li>— На бэхе.</li>
+  </ul>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1617.jpg'])
+
+@ru
+  <p>В единственном попавшемся супермаркете не было камер хранения. Люди просто оставляли габаритные вещи на полу рядом с кассами.</p>
+@endlang
+
+@ru
+  <p>7j/7 — открыто ежедневно, но необязательно 24 часа в сутки.</p>
+@endlang
+
+@ru
+  <p>Манго. Сначала прочитал происхождение Brest. Не мог поверить, что из Бреста привезли. И правильно не верил, потому что написано Brésil — Бразилия.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1629.jpg'])
+
+@ru
+  <p>Парижане охотно научат переходить дорогу на красный.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1631.jpg'])
+
+@ru
+  <p>Проход не закрыт, просто ворота нужно открывать и закрывать вручную.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1638.jpg'])
+
+@ru
+  <p>Столбики не позволяют машинам проехать. Однако, водитель общественного транспорта может опустить их удаленно прямо из салона.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1639.jpg'])
+
+@ru
+  <p>Футбольное поле.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1640.jpg'])
+
+@ru
+  <p>Интересная крыша дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1649.jpg'])
+
+@ru
+  <p>Путей всем хватит.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1652.jpg'])
+
+@ru
+  <p>Гулькин трехэтажный дом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1653.jpg'])
+
+@ru
+  <p>Парк.</p>
+@endlang
 <youtube title="Paris, Parc Clichy-Batignolles — Martin Luther King, September 2016" v="teU3i_W24R4"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_1655.jpg'])
+
+@ru
+  <p>Водоотвод в деле.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1657.jpg'])
+
+@ru
+  <p>Кладбище.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1664.jpg',
+  'IMG_1667.jpg',
+  'IMG_1668.jpg',
+  'IMG_1669.jpg',
+  'IMG_1670.jpg',
+  'IMG_1671.jpg',
+  'IMG_1672.jpg',
+  'IMG_1673.jpg',
+]])
+
+@ru
+  <p>Почта, телеграф, телефон.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1676.jpg'])
+
+@ru
+  <p>Закат в красных тонах.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1601.jpg'])
+
+@ru
+  <p>С примесью зеленого — все светофоры переключились одновременно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1603.jpg'])
+
+@ru
+  <p>Сена на прощание.</p>
+@endlang
+<youtube title="Paris Sunset, September 2016" v="txS910rrMQs"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_1594.jpg'])
+
+@ru
+  <p>На обратный рейс зачем-то понадобился паспорт — смотрели визу и ФИО. Для владельцев Galaxy Note 7 было специальное объявление, что телефон должен быть выключен во время нахождения в самолете. В октябре 2016 авиакомпания Norwegian Air и вовсе запретила эти аппараты на своих рейсах.</p>
+@endlang
 @endsection
