af4d596b329974761450d64b167bb41ad4b5600e
Sergey Pankov <vacuum@ivacuum.ru>
2016-10-31 07:16:09 +0300

    Хельсинки в октябре 2016

--- resources/views/life/trips/helsinki_2016_10_04-05.blade.php
+++ resources/views/life/trips/helsinki_2016_10_04-05.blade.php
@@ -1,5 +1,211 @@
 @extends('life.trips.base')
 
 @section('content')
+@ru
+  <p>Скоростные ограничения при въезде в страну.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2046.jpg'])
+
+@ru
+  <p>Осенние краски.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2059.jpg'])
+
+@ru
+  <p>Кладбище.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2061.jpg',
+  'IMG_2060.jpg',
+  'IMG_2058.jpg',
+  'IMG_2057.jpg',
+  'IMG_2056.jpg',
+  'IMG_2055.jpg',
+]])
+
+@ru
+  <p>Живописное расположение у кладбища — вода по соседству.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2063.jpg'])
+
+@ru
+  <p>Гигантский камыш.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2065.jpg'])
+
+@ru
+  <p>Гулять одно удовольствие.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2066.jpg'])
+
+@ru
+  <p>Снова камни, с которых открывается высотный вид.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2068.jpg'])
+
+@ru
+  <p>Вид в обратную сторону.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2070.jpg'])
+
+@ru
+  <p>Пляж.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2069.jpg'])
+
+@ru
+  <p>Контраст красок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2071.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2072.jpg',
+  'IMG_2073.jpg',
+  'IMG_2074.jpg',
+  'IMG_2075.jpg',
+  'IMG_2079.jpg',
+  'IMG_2081.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2047.jpg',
+  'IMG_2048.jpg',
+  'IMG_2051.jpg',
+  'IMG_2084.jpg',
+  'IMG_2087.jpg',
+  'IMG_2088.jpg',
+  'IMG_2089.jpg',
+  'IMG_2103.jpg',
+  'IMG_2105.jpg',
+  'IMG_2110.jpg',
+  'IMG_2117.jpg',
+  'IMG_2121.jpg',
+]])
+
+@ru
+  <p>Жилой район.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2076.jpg'])
+
+@ru
+  <p>Дворик внутри него.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2077.jpg'])
+
+@ru
+  <p>Лавочка снаружи.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2078.jpg'])
+
+@ru
+  <p>Тесла.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2083.jpg'])
+
+@ru
+  <p>Трамвай.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2085.jpg'])
+
+@ru
+  <p>Внезапно гора, а вокруг дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2091.jpg'])
+
+@ru
+  <p>Сверху хороший вид.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2092.jpg'])
+
+@ru
+  <p>Деревья добавляют красок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2095.jpg'])
+
+@ru
+  <p>Высота примерно третьего этажа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2098.jpg'])
+
+@ru
+  <p>Жирафы на балконе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2100.jpg'])
+
+@ru
+  <p>Дома.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2106.jpg',
+  'IMG_2108.jpg',
+  'IMG_2143.jpg',
+]])
+
+@ru
+  <p>Подъезд дома. В квартирах распространены смесители, у которых одна ручка регулирует температуру воды, а другая напор.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2149.jpg'])
+
+@ru
+  <p>Пустынный парк.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2111.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2112.jpg'])
+
+@ru
+  <p>Причал.</p>
+@endlang
 <youtube title="Helsinki Embankment, October 2016" v="JMtH89DsHEw"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_2119.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2124.jpg'])
+
+@ru
+  <p>Вид с набережной.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2126.jpg'])
+
+@ru
+  <p>Аллея молодых деревьев.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2138.jpg'])
+
+@ru
+  <p>Триколор.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2139.jpg'])
+
+@ru
+  <p>В супермаркетах встречаются терминалы самообслуживания и оплаты. Среди интерфейсов есть финский, шведский, английский и русский. Также при использовании карты в магазинах некоторые банковские терминалы автоматически выводят сообщения на экран на языке страны банка, выпустившего карту. А для бесконтактной оплаты обычно нужно подносить карту к терминалу слева, а не к самому экрану, как в России.</p>
+@endlang
+
+@ru
+  <p>Дать вручную вводить цифры на весах? Да зачем! Лучше кнопки под все возможные числа сделать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2146.jpg'])
+
+@ru
+  <p>Не только пятилитровки есть, но и десяти. Хм, проще из-под крана питьевой воды набрать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2147.jpg'])
+
+@ru
+  <p>Йогурт со вкусом кокоса. Похоже на рафаэлку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2148.jpg'])
+
+<p>Нужно быть внимательным, если хочется прикупить заграничный товар с собой. Все-таки Россия рядом, вполне можно встретить товары из нее. Так неглядя купил зубную щетку, которая оказалась импортирована из <a class="link" href="/life/spb">Санкт-Петербурга</a>. Даже на обратной стороне упаковки все на русском. Хотя произведена она оказалась в Швейцарии. Есть смысл обращать внимание на <a class="link" href="/life/minsk.2016.10#barcode">штрих-коды</a>.</p>
 @endsection
