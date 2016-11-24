87ca70d1e6502ff174acae795115bb4a7700f9b4
Sergey Pankov <vacuum@ivacuum.ru>
2016-11-24 03:21:21 +0300

    Рефакторинг

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,14 +1,13 @@
 <?php namespace App\Providers;
 
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [];
 
-    public function boot(GateContract $gate)
+    public function boot()
     {
-        parent::registerPolicies($gate);
+        parent::registerPolicies();
     }
 }
