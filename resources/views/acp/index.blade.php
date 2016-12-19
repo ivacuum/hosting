82f6fd50c43945aa87340218c223645deab4e741
Sergey Pankov <vacuum@ivacuum.ru>
2016-12-19 12:16:17 +0800

    Правка оформления

--- resources/views/acp/index.blade.php
+++ resources/views/acp/index.blade.php
@@ -1,15 +1,13 @@
 @extends('acp.base')
 
 @section('content')
-<h2>Разделы админки</h2>
-<div class="lead">
-  <ul>
-    <li><a class="link" href="/acp/clients">Клиенты</a></li>
-    <li><a class="link" href="/acp/domains">Домены</a></li>
-    <li><a class="link" href="/acp/servers">Серверы</a></li>
-    <li><a class="link" href="/acp/pages">Страницы</a></li>
-    <li><a class="link" href="/acp/users">Пользователи</a></li>
-    <li><a class="link" href="/acp/yandex/users">Пользователи Яндекс API</a></li>
-  </ul>
-</div>
+<h3>Хостинг</h3>
+<ul>
+  <li><a class="link" href="/acp/clients">Клиенты</a></li>
+  <li><a class="link" href="/acp/domains">Домены</a></li>
+  <li><a class="link" href="/acp/servers">Серверы</a></li>
+  <li><a class="link" href="/acp/pages">Страницы</a></li>
+  <li><a class="link" href="/acp/users">Пользователи</a></li>
+  <li><a class="link" href="/acp/yandex/users">Пользователи Яндекс API</a></li>
+</ul>
 @endsection
