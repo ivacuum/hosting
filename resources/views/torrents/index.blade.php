7c0390e6534c2ff3206109721a580bc32615098b
Sergey Pankov <vacuum@ivacuum.ru>
2016-12-19 12:34:42 +0800

    Сообщение при отсутствии раздач

--- resources/views/torrents/index.blade.php
+++ resources/views/torrents/index.blade.php
@@ -1,36 +1,40 @@
 @extends('torrents.base')
 
 @section('content')
-<table class="table-stats">
-  <thead>
-    <tr>
-      <td>Дата</td>
-      <td></td>
-      <td>Раздача</td>
-      <td>Размер</td>
-      <td>Сиды</td>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($torrents as $torrent)
+@if (sizeof($torrents))
+  <table class="table-stats">
+    <thead>
       <tr>
-        <td>{{ $torrent->shortDate() }}</td>
-        <td>
-          <a class="link" href="{{ $torrent->magnet() }}">
-            @svg (magnet)
-          </a>
-        </td>
-        <td>
-          <a class="link" href="{{ action("{$self}@torrent", $torrent) }}">
-            {{ $torrent->title }}
-          </a>
-        </td>
-        <td>
-          {{ $torrent->localizedSize() }}
-        </td>
-        <td class="text-success">{{ $torrent->seeders }}</td>
+        <td>Дата</td>
+        <td></td>
+        <td>Раздача</td>
+        <td>Размер</td>
+        <td>Сиды</td>
       </tr>
-    @endforeach
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      @foreach ($torrents as $torrent)
+        <tr>
+          <td>{{ $torrent->shortDate() }}</td>
+          <td>
+            <a class="link" href="{{ $torrent->magnet() }}">
+              @svg (magnet)
+            </a>
+          </td>
+          <td>
+            <a class="link" href="{{ action("{$self}@torrent", $torrent) }}">
+              {{ $torrent->title }}
+            </a>
+          </td>
+          <td>
+            {{ $torrent->localizedSize() }}
+          </td>
+          <td class="text-success">{{ $torrent->seeders }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@else
+  <p>Подходящих раздач не найдено.</p>
+@endif
 @endsection
