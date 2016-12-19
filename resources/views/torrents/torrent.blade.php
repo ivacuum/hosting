1e7851d7ed5546a854ac27a0f8f2f2031fcd0300
Sergey Pankov <vacuum@ivacuum.ru>
2016-12-19 22:57:13 +0800

    Оформление шапки раздачи

--- resources/views/torrents/torrent.blade.php
+++ resources/views/torrents/torrent.blade.php
@@ -1,13 +1,22 @@
 @extends('torrents.base')
 
 @section('content')
-<p class="text-center">
-  {{ $torrent->localizedSize() }}
-  &middot;
-  <span class="text-success">{{ $torrent->seeders }} сидов</span>
-  &middot;
-  <a class="link" href="https://rutracker.org/forum/viewtopic.php?t={{ $torrent->rto_id }}">первоисточник</a>
-</p>
+<div class="panel panel-default text-center center-block torrent-stats-container">
+  <div class="panel-heading">
+    Статистика раздачи
+    <a href="https://rutracker.org/forum/viewtopic.php?t={{ $torrent->rto_id }}">
+      @svg (external-link)
+    </a>
+  </div>
+  <div class="panel-body">
+    Размер: <strong>{{ $torrent->localizedSize() }}</strong>
+    <span class="torrent-stats-separator">&middot;</span>
+    Активна: <strong>{{ $torrent->registered_at->diffForHumans(null, true) }}</strong>
+    <span class="torrent-stats-separator">&middot;</span>
+    <span class="text-success">{{ $torrent->seeders }} сидов</span>
+  </div>
+  <div class="panel-footer"><a class="btn btn-success" href="{{ $torrent->magnet() }}">Скачать</a></div>
+</div>
 <div class="rutracker-post">
   {!! $torrent->text !!}
 </div>
