4d07cd31380e59e16dc630100b4e76482fa0a215
Sergey Pankov <vacuum@ivacuum.ru>
2016-12-31 22:30:46 +0300

    Обновление формы входа на сайт

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,17 @@
 @section('content')
 @include('tpl.form_errors')
 
-<div class="form-signin">
+<div class="form-signin text-center">
   <h3>Вход на сайт</h3>
-  <form action="/auth/login" method="post">
+  <form action="{{ action('Auth@login') }}" method="post">
     <input hidden type="text" name="mail" value="{{ old('mail') }}">
 
     <div class="form-group">
-      <label>Электронная почта:</label>
-      <input autofocus required type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email">
+      <input autofocus required type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Электронная почта">
     </div>
 
     <div class="form-group has-feedback">
-      <label>Пароль:</label>
-      <a href="/auth/password/remind" class="link">забыли пароль?</a>
-      <input required type="password" class="form-control" name="password">
+      <input required type="password" class="form-control" name="password" placeholder="Пароль">
       <span class="form-control-feedback form-control-feedback-password js-password-eye">
         <span class="js-password-eye-show" title="Показать пароль">
           @svg (eye)
@@ -27,19 +24,14 @@
       </span>
     </div>
 
-    <div class="checkbox">
-      <label><input type="checkbox" name="foreign" {{ old('foreign') ? 'checked' : '' }}> Чужой компьютер?</label>
+    <div class="m-y-1 clearfix">
+      <div class="pull-left"><a class="link" href="/auth/password/remind">Забыли пароль?</a></div>
+      {{--<div class="pull-right"><a class="link" href="/auth/register">Регистрация</a></div>--}}
     </div>
 
     <button type="submit" class="btn btn-primary btn-lg btn-block">
       Войти
     </button>
-
-    <p class="lead text-center" style="margin: 1em 0;">или</p>
-
-    <a href="/auth/register" class="btn btn-default btn-lg btn-block">
-      Зарегистрироваться
-    </a>
 
     {{ csrf_field() }}
   </form>
