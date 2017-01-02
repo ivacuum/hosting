a91256a63723e415db861e0ac4506fd6a3e01e7e
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-02 09:37:00 +0300

    Перевод инфы о восстановлении пароля

--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user"     => "We can't find a user with that e-mail address.",
-	"token"    => "This password reset token is invalid.",
-	"sent"     => "We have e-mailed your password reset link!",
-	"reset"    => "Your password has been reset!",
+    'sent' => 'Ссылка на восстановление пароля отправлена на указанную почту',
+    'user' => 'Пользователь с указанной почтой не найден',
+    'token' => 'Эта ссылка для восстановления более не действительна',
+    'reset' => 'Новый пароль установлен',
+    'password' => 'Пароль должен содержать не менее 6 символов, а также совпадать с таковым во втором поле',
 ];
