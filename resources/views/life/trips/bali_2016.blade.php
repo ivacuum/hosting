4074b4efa127c0d25cd9e36dd00672d95d6e84c3
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-02 09:06:15 +0300

    Заготовки по поездкам

---
+++ resources/views/life/trips/bali_2016.blade.php
@@ -0,0 +1,10 @@
+@extends('life.trips.base')
+
+@section('content')
+{{--
+Иммиграционную карту по прибытии пограничник смотрит:
+— CTO. Chief Technical..
+— Officer, yea
+— Where? Gazprom?
+--}}
+@endsection
