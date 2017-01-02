175cb99771a2fc59799b970456b02bc351151c7c
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-02 09:34:09 +0300

    Рефакторинг

--- resources/lang/en/life.php
+++ resources/lang/en/life.php
@@ -1,32 +1,32 @@
 <?php
 
 return [
+    'intro_text' => 'All the content I post in one place.',
     'intro_title' => 'Life notes',
-    'intro_text'  => 'All the content I post in one place.',
 
-    'feedback.heading'     => 'Feedback',
-    'feedback.help'        => 'Leave your contact information if you want to get a reply',
-    'feedback.leave'       => 'Leave a feedback',
+    'feedback.help' => 'Leave your contact information if you want to get a reply',
+    'feedback.leave' => 'Leave a feedback',
+    'feedback.submit' => 'Submit',
+    'feedback.heading' => 'Feedback',
     'feedback.placeholder' => 'Write a message...',
-    'feedback.submit'      => 'Submit',
 
+    'gigs_intro_text' => "It's all started with a simple plan of visiting one show per year, but something went wrong in 2014...",
     'gigs_intro_title' => 'Attended and incoming concerts',
-    'gigs_intro_text'  => 'It\'s all started with a simple plan of visiting one show per year, but something went wrong in 2014...',
 
-    'by_city'    => 'by city',
+    'by_city' => 'by city',
+    'by_year' => 'by year',
     'by_country' => 'by country',
-    'by_year'    => 'by year',
 
     'on_map' => 'on the map',
 
-    'visited_cities'    => 'Visited cities',
+    'visited_cities' => 'Visited cities',
     'visited_countries' => 'Visited countries',
 
+    'trips' => 'Trips',
     'favorites' => 'Favorites',
-    'trips'     => 'Trips',
 
-    'date.gig_short'  => '%B %e', // April 21
-    'date.months'     => '%2$s %d – %4$s %3$d, %5$d', // May 27 – June 3, 2016
-    'date.same_day'   => '%B %e, %Y', // June 1, 2016
+    'date.months' => '%2$s %d – %4$s %3$d, %5$d', // May 27 – June 3, 2016
+    'date.same_day' => '%B %e, %Y', // June 1, 2016
+    'date.gig_short' => '%B %e', // April 21
     'date.same_month' => '%3$s %d–%d, %4$d', // June 1–3, 2016
 ];
--- resources/lang/ru/meta_title.php
+++ resources/lang/ru/meta_title.php
@@ -1,33 +1,13 @@
 <?php
 
 return [
-	'acp.domains.default'   => 'Домены на мониторинге',
-	'acp.domains.orphan'    => 'Домены на продажу',
-	'acp.domains.external'  => 'Домены не в нашей панели reg.ru',
+	'acp.domains.default' => 'Домены на мониторинге',
+	'acp.domains.orphan' => 'Домены на продажу',
+	'acp.domains.external' => 'Домены не в нашей панели reg.ru',
 	'acp.domains.no-server' => 'Домены без сервера',
-	'acp.domains.no-ns'     => 'Домены без NS',
-	'acp.domains.inactive'  => 'Неактивные домены',
-	'acp.domains.trashed'   => 'Удаленные домены',
-
-    'acp.cities.index'  => 'Города',
-    'acp.cities.create' => 'Новый город',
-    'acp.cities.edit'   => 'Редактирование города',
-    'acp.cities.show'   => 'Город',
-
-    'acp.countries.index'  => 'Страны',
-    'acp.countries.create' => 'Новая страна',
-    'acp.countries.edit'   => 'Редактирование страны',
-    'acp.countries.show'   => 'Страна',
-
-    'acp.gigs.index'  => 'Концерты',
-    'acp.gigs.create' => 'Новый концерт',
-    'acp.gigs.edit'   => 'Редактирование концерта',
-    'acp.gigs.show'   => 'Концерт',
-
-    'acp.trips.index'  => 'Поездки',
-    'acp.trips.create' => 'Новая поездка',
-    'acp.trips.edit'   => 'Редактирование поездки',
-    'acp.trips.show'   => 'Поездка',
+	'acp.domains.no-ns' => 'Домены без NS',
+	'acp.domains.inactive' => 'Неактивные домены',
+	'acp.domains.trashed' => 'Удаленные домены',
 
     'dcpp.faq' => 'FAQ (ЧаВо) по DC++ | Решение проблем с DC++',
     'dcpp.dcpp' => 'Скачать DC++ 0.782',
