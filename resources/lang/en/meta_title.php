bd49a0e91104d97b3a73af10568f18aa86842ccc
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-03 14:48:27 +0300

    Правка названия ОС

--- resources/lang/en/meta_title.php
+++ resources/lang/en/meta_title.php
@@ -4,10 +4,10 @@ return [
     'dcpp.dcpp' => 'Download DC++ 0.782',
     'dcpp.airdc' => 'Download AirDC++ 2.09',
     'dcpp.apexdc' => 'Download ApexDC++ 1.4.3',
-    'dcpp.jucydc' => 'Download Jucy DC++ 0.85 for Windows, Linux and Mac OS X',
+    'dcpp.jucydc' => 'Download Jucy DC++ 0.85 for Windows, Linux and macOS',
     'dcpp.pelinkdc' => 'Download PeLinkDC++ 5.95',
     'dcpp.strongdc' => 'Download StrongDC++ 2.42',
     'dcpp.flylinkdc' => 'Download FlyLinkDC++ r500',
     'dcpp.greylinkdc' => 'Download GreyLinkDC++ 0.49',
-    'dcpp.shakespeer' => 'Download ShakesPeer 0.9.2 for Mac OS X',
+    'dcpp.shakespeer' => 'Download ShakesPeer 0.9.2 for macOS',
 ];
