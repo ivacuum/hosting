e0b3bc8202655aaf7a80f75a1eac9ef0d7e07067
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-14 21:02:55 +0300

    Заметка о Генте

--- resources/views/life/trips/ghent_2016.blade.php
+++ resources/views/life/trips/ghent_2016.blade.php
@@ -2,42 +2,107 @@
 
 @section('content')
 @ru
-  <p></p>
+  <p>Ох, уж это чувство, когда покупаешь билеты, а их никто не проверяет. Ни туда, ни обратно. Хоть печатать их не понадобилось — достаточно хранить на телефоне полученный после оплаты через интернет файл.</p>
 @endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2391.jpg'])
 
-IMG_2391.jpg
-IMG_2395.jpg
-IMG_2398.jpg
-IMG_2399.jpg
-IMG_2400.jpg
-IMG_2402.jpg
-IMG_2404.jpg
-IMG_2406.jpg
-IMG_2408.jpg
-IMG_2409.jpg
-IMG_2410.jpg
-IMG_2411.jpg
-IMG_2412.jpg
-IMG_2413.jpg
-IMG_2414.jpg
-IMG_2415.jpg
-IMG_2416.jpg
-IMG_2417.jpg
-IMG_2419.jpg
-IMG_2420.jpg
-IMG_2421.jpg
-IMG_2422.jpg
-IMG_2423.jpg
-IMG_2426.jpg
-IMG_2427.jpg
-IMG_2428.jpg
-IMG_2430.jpg
-IMG_2431.jpg
-IMG_2432.jpg
-IMG_2433.jpg
-IMG_2434.jpg
-IMG_2436.jpg
-IMG_2440.jpg
-IMG_2441.jpg
-IMG_2442.jpg
+@ru
+  <p>Приятная картина снаружи вокзала.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2395.jpg'])
+
+@ru
+  <p>И по мере отдаления она становится еще лучше.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2400.jpg',
+  'IMG_2398.jpg',
+  'IMG_2399.jpg',
+  'IMG_2402.jpg',
+]])
+
+@ru
+  <p>В парке.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2404.jpg',
+  'IMG_2406.jpg',
+]])
+
+@ru
+  <p>За парком. Отдельная дорога есть у всех. Велосипедисты то отжимают полосу у автомобилистов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2408.jpg'])
+
+@ru
+  <p>То у пешеходов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2409.jpg'])
+
+@ru
+  <p>Настольная лампа на улице.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2412.jpg'])
+
+@ru
+  <p>Красота вокруг.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2410.jpg',
+  'IMG_2411.jpg',
+  'IMG_2413.jpg',
+  'IMG_2414.jpg',
+  'IMG_2415.jpg',
+  'IMG_2417.jpg',
+  'IMG_2419.jpg',
+  'IMG_2420.jpg',
+  'IMG_2427.jpg',
+  'IMG_2428.jpg',
+  'IMG_2430.jpg',
+  'IMG_2431.jpg',
+  'IMG_2432.jpg',
+  'IMG_2433.jpg',
+  'IMG_2434.jpg',
+  'IMG_2436.jpg',
+  'IMG_2440.jpg',
+]])
+
+@ru
+  <p>Указатели на столбе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2416.jpg'])
+
+@ru
+  <p>Фасад под старину, а бок новый.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2421.jpg'])
+
+@ru
+  <p>Замок прямо в городе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2422.jpg'])
+
+@ru
+  <p>И трамваи рядом ходят.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2423.jpg'])
+
+@ru
+  <p>Этот даже прямиком до Москвы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2426.jpg'])
+
+@ru
+  <p>Пора обратно в <a class="link" href="/life/brussels.2016">Брюссель</a> на концерт группы <a class="link" href="/life/opeth.2016">Opeth</a>. Железнодорожная платформа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2441.jpg'])
+
+@ru
+  <p>Обратно подъехал состав не первой свежести.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2442.jpg'])
+
+@ru
+  <p>Гент — отличный выбор для однодневной вылазки.</p>
+@endlang
 @endsection
