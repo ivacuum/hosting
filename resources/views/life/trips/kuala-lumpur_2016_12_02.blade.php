ac38f8346aaea41714868a83485aa63c72194837
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-16 23:15:03 +0300

    Заметка о Куала-Лумпуре

---
+++ resources/views/life/trips/kuala-lumpur_2016_12_02.blade.php
@@ -0,0 +1,168 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Куала-Лумпур — огромный пересадочный узел в Азии. Настолько огромный, что внутри первого терминала ходит бесплатный поезд до зоны паспортного контроля. А еще есть второй терминал для лоукост-компаний. В общей сложности улететь можно чуть ли не в любую точку мира.</p>
+  <p>Визу для выхода в город можно получить по прибытии в аэропорт. Каждый раз это будет штамп на 30 дней. До города ходит экспресс-поезд и в 5 раз более дешевый автобус с морознейшим кондиционером. С приемом карт беда, лучше наменять местных ринггитов. Купюры в Малайзии полимерные (пластиковые) — не впитывают влагу.</p>
+@endlang
+
+@ru
+  <p>Скромное шоссе и огромной высоты дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2633.jpg'])
+
+@ru
+  <p>Еще более скромный тротуар.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2634.jpg'])
+
+@ru
+  <p>Поезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2636.jpg'])
+
+@ru
+  <p>Под мостом можно переждать ежедневный ливень. Влажно тут всегда. И тепло тоже — не стоит даже рассчитывать застать +20&deg;C.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2637.jpg'])
+
+@ru
+  <p>Очень хорошо от дождя спасают навесы. Их длина порой составляет сотни метров.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2690.jpg'])
+
+@ru
+  <p>Надземный переход.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2638.jpg'])
+
+@ru
+  <p>Вид с перехода.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2639.jpg'])
+
+@ru
+  <p>Левостороннее движение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2640.jpg'])
+
+@ru
+  <p>Рисунки на домах.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2642.jpg',
+  'IMG_2651.jpg',
+]])
+
+@ru
+  <p>Перекресток не занимать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2643.jpg'])
+
+@ru
+  <p>Знак детально объясняет принцип работы разметки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2647.jpg'])
+
+@ru
+  <p>Разрыв шаблона из-за высоты здания слева. Привычнее видеть строения в подобном стиле вдвое-втрое ниже.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2648.jpg'])
+
+@ru
+  <p>Jalan — улица. В Индонезии этот перевод тоже справедлив.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2649.jpg'])
+
+@ru
+  <p>Парковка в доме.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2652.jpg'])
+
+@ru
+  <p>Парковка на парковке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2679.jpg'])
+
+@ru
+  <p>Остановка со схемой движения общественного транспорта.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2657.jpg'])
+
+@ru
+  <p>Пополнение коллекции флагов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2658.jpg'])
+
+@ru
+  <p>К встрече Нового года готовы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2660.jpg'])
+
+@ru
+  <p>Монорельс.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2662.jpg'])
+
+@ru
+  <p>Высокие не только здания, но и деревья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2663.jpg'])
+
+@ru
+  <p>Парк.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2664.jpg',
+  'IMG_2677.jpg',
+  'IMG_2678.jpg',
+]])
+
+@ru
+  <p>Переулки.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2641.jpg',
+  'IMG_2680.jpg',
+  'IMG_2682.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+<youtube v="a4ehGZmRAcg" title="Kuala Lumpur street, December 2016"></youtube>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2632.jpg',
+  'IMG_2635.jpg',
+  'IMG_2650.jpg',
+  'IMG_2653.jpg',
+  'IMG_2654.jpg',
+  'IMG_2655.jpg',
+  'IMG_2659.jpg',
+  'IMG_2683.jpg',
+  'IMG_2684.jpg',
+  'IMG_2685.jpg',
+  'IMG_2686.jpg',
+  'IMG_2688.jpg',
+  'IMG_2689.jpg',
+]])
+
+@ru
+  <p>Второй этаж.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2687.jpg'])
+
+@ru
+  <p>Башни Петронас видел, да.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2691.jpg'])
+
+@ru
+  <p>Лифт — бомба! Как попасть на третий этаж аэропорта?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2693.jpg'])
+
+@ru
+  <p>Для пешехода далеко не самый приятный город, поскольку тротуары узкие, иногда и вовсе внезапно заканчиваются. Светофоры работают самым загадочным образом, а для пешеходов могут даже не включаться. Даже после нажатия кнопки на перекрестке.</p>
+@endlang
+@endsection
