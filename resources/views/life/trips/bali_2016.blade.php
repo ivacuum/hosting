ef6cb30f8ceabe18d69b1398f99bdbb045c4d47d
Sergey Pankov <vacuum@ivacuum.ru>
2017-01-21 03:52:11 +0300

    Заметка о Бали

--- resources/views/life/trips/bali_2016.blade.php
+++ resources/views/life/trips/bali_2016.blade.php
@@ -1,10 +1,303 @@
 @extends('life.trips.base')
 
 @section('content')
-{{--
-Иммиграционную карту по прибытии пограничник смотрит:
-— CTO. Chief Technical..
-— Officer, yea
-— Where? Gazprom?
---}}
+@ru
+  <p>Популярное среди начинающих серферов дешевое место с круглогодичным летом. Отрицательная широта -8,4&deg; подсказывает, что Бали находится в южном полушарии, совсем недалеко от экватора.</p>
+  <p>Также в относительной близости находится Австралия — до города Перт лететь около трех часов.</p>
+@endlang
+
+@ru
+  <p>Пополнение коллекции флагов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2703.jpg'])
+
+@ru
+  <p>Беседа на берегу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2704.jpg'])
+
+@ru
+  <p>Скутеров великое множество.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2705.jpg'])
+
+@ru
+  <p>Несметное множество. Для аренды скутера даже не требуется соответствующая открытая категория прав. Да и штрафы не только дешевле обучения, но и дешевле получения водительского удостоверения международного образца.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2765.jpg'])
+
+@ru
+  <p>Торговый центр.</p>
+@en
+  <p>Mall.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2697.jpg'])
+
+@ru
+  <p>Пляж.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2698.jpg'])
+
+@ru
+  <p>Улица вдоль пляжа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2700.jpg'])
+
+@ru
+  <p>Территория отеля.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2701.jpg'])
+
+@ru
+  <p>Мусорный ящик. Мусора внутри не было.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2702.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2706.jpg'])
+
+@ru
+  <p>Шахматы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2707.jpg'])
+
+@ru
+  <p>Редкий общественный транспорт — автобус.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2709.jpg'])
+
+@ru
+  <p>Если присмотреться, то можно увидеть меню.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2710.jpg'])
+
+@ru
+  <p>Свастика имеет положительное значение в Индонезии.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2739.jpg'])
+
+@ru
+  <p>Вид на пляж.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2787.jpg'])
+
+@ru
+  <p>Вид с пляжа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2792.jpg'])
+
+@ru
+  <p>Редкие пальмы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2793.jpg'])
+
+@ru
+  <p>Перед этим азиатским путешествием успел завершить прохождение игры Анчартед 4. Впоследствии встреченные пейзажи не раз напоминали игровые локации.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2742.jpg',
+  'IMG_2941.jpg',
+  'IMG_3006.jpg',
+  'IMG_3007.jpg',
+  'IMG_3008.jpg',
+  'IMG_3009.jpg',
+  'IMG_3020.jpg',
+  'IMG_3036.jpg',
+  'IMG_3035.jpg',
+]])
+
+@ru
+  <p>Закат.</p>
+@endlang
+<youtube v="2xUnV3yLl9Q" title="Bali Sea, December 2016"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_2796.jpg'])
+
+@ru
+  <p>Пейзажи.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2718.jpg',
+  'IMG_2724.jpg',
+  'IMG_2782.jpg',
+  'IMG_2835.jpg',
+  'IMG_2860.jpg',
+  'IMG_2903.jpg',
+  'IMG_2964.jpg',
+  'IMG_2990.jpg',
+  'IMG_3001.jpg',
+  'IMG_3054.jpg',
+  'IMG_3057.jpg',
+  'IMG_3059.jpg',
+]])
+
+@ru
+  <p>У заведений зачастую одно из меню выставлено на улицу. Легко без посадки ознакомиться с ценами. И это лучше делать, так как у заведений они могут отличаться в несколько раз. Норма пройти пять дорогих мест, пока найдется одно дешевое. Цена сока или жареного риса с курицей — неплохой ориентир.</p>
+  <p>Как можно поесть на 270 ₽. По возвращении в Россию определенно недостает свежевыжатого сока по 50 ₽ за стакан и разнообразных фруктовых тарелок за те же 50 ₽.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2801.jpg'])
+
+@ru
+  <p>Этот товарищ лучше любого будильника.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2804.jpg'])
+
+@ru
+  <p>Кажется, дождь надвигается.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2830.jpg'])
+
+@ru
+  <p>Узор напольной плитки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2834.jpg'])
+
+@ru
+  <p>За холмами это вглубь острова.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2843.jpg'])
+
+@ru
+  <p>Вход в продуктовый магазин.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2844.jpg'])
+
+@ru
+  <p>Редкий «зимний» день обходится без дождя. Иногда затяжного и проливного.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2888.jpg'])
+
+@ru
+  <p>Запросто затапливающего территорию жилья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2889.jpg'])
+
+@ru
+  <p>Хорошо, что байку потоп не помеха. Плюс дождя в отсутствии пыли на улицах. Открывать автомойку на Бали явно невыгодно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2891.jpg'])
+
+@ru
+  <p>Продуктовый рынок. Стоит быть готовым к самым разнообразным запахам.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2893.jpg'])
+
+@ru
+  <p>Вулкан на горизонте. Их в Индонезии огромное количество.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2906.jpg'])
+
+@ru
+  <p>По острову разбросаны не только вулканы, но и храмы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3051.jpg'])
+
+@ru
+  <p>Одинокий рыбак.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2931.jpg'])
+
+@ru
+  <p>Территория отеля.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2961.jpg'])
+
+@ru
+  <p>Пешеходный переход поднят на уровень тротуара.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2965.jpg'])
+
+@ru
+  <p>Вход в автобус на высоте второго этажа и с левой стороны. Детская загадка «в какую сторону едет автобус» здесь получит противоположный нашему ответ.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2975.jpg'])
+
+@ru
+  <p>Фасовка конфет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2977.jpg'])
+
+@ru
+  <p>Фасовка печенья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2978.jpg'])
+
+@ru
+  <p><a class="pseudo" data-toggle="feedback" data-target=".js-modal-feedback" data-question="Придумайте подпись к фотографии">Придумайте подпись к фотографии.</a></p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2989.jpg'])
+
+@ru
+  <p>Загорает. Даже в облачную погоду редко выглядывающие солнечные лучи подпекают.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2994.jpg'])
+
+@ru
+  <p>С этими товарищами важно научиться жить в гармонии, ибо редкое жилище обходится без них.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2908.jpg'])
+
+@ru
+  <p>Вараны на вывеске.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2999.jpg'])
+
+@ru
+  <p>Варанище. Маленький для сравнения в левом верхнем углу вывески.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3067.jpg'])
+
+@ru
+  <p>Собаку на дроне не покатать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3046.jpg'])
+
+@ru
+  <p>Порой даже на улице можно подзарядиться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3064.jpg'])
+
+@ru
+  <p>Вечерний пит-стоп. Замена камеры для скутера за смешные 250 ₽.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3066.jpg'])
+
+@ru
+  <p>Сложно понять как вообще на узких улицах городов Бали ездят на машинах. Нерегулируемые перекрестки определенно проезжаются по ощущениям. Скутеры часто берут количеством, тогда одно направление мирно ждет, пока ожидающих на нем не наберется критическое количество.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3068.jpg'])
+
+@ru
+  <p>Рисовые поля.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3073.jpg',
+  'IMG_3080.jpg',
+  'IMG_3082.jpg',
+]])
+
+@ru
+  <p>Фрукты в супермаркете.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3095.jpg',
+  'IMG_3096.jpg',
+]])
+
+@ru
+  <p>Бонусные видео.</p>
+@endlang
+<youtube v="dftzWN4at2Q" title="Bali Crabs, December 2016"></youtube>
+<youtube v="52QBBWT_yBc" title="Bali Fog, December 2016"></youtube>
+<youtube v="qS2dpzOIOSQ" title="Too hot for rain, December 2016"></youtube>
+
+@ru
+  <p>Еще бонус. Пограничник по прибытии на Бали смотрит иммиграционную карту:</p>
+  <ul class="list-unstyled">
+    <li>— CTO. Chief Technical..</li>
+    <li>— Officer, yea</li>
+    <li>— Where? Gazprom?</li>
+  </ul>
+@endlang
 @endsection
