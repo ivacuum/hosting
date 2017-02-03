f719c5805651fa31bcc9d5ce677090c6b906daf4
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-03 22:48:30 +0300

    Правка вывода контента в хроме

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -18,101 +18,102 @@
   @stack('head')
 </head>
 <body>
-  <div class="navbar navbar-default {{ App::environment('local') ? 'navbar-inverse' : '' }}">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        @section('brand')
-          <a class="navbar-brand" href="{{ action('Home@index') }}">{{ config('cfg.sitename') }}</a>
-        @show
-      </div>
-      <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          @section('global_menu')
-            <li>
-              <a class="{{ $self == 'Life' ? 'navbar-selected' : '' }}" href="{{ action('Life@index') }}">
-                {{ trans('menu.life') }}
-              </a>
-            </li>
-            <li>
-              <a class="{{ $self == 'News' ? 'navbar-selected' : '' }}" href="{{ action('News@index') }}">
-                {{ trans('news.index') }}
-              </a>
-            </li>
-            <li>
-              <a class="{{ $self == 'Torrents' ? 'navbar-selected' : '' }}" href="{{ action('Torrents@index') }}">
-                {{ trans('menu.torrents') }}
-              </a>
-            </li>
-          @show
-        </ul>
-        @yield('header_form')
-        <ul class="nav navbar-nav navbar-right">
+<div class="navbar navbar-default {{ App::environment('local') ? 'navbar-inverse' : '' }}">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      @section('brand')
+        <a class="navbar-brand" href="{{ action('Home@index') }}">{{ config('cfg.sitename') }}</a>
+      @show
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        @section('global_menu')
           <li>
-            @ru
-              <a href="{{ url("en/{$request_uri}") }}" lang="en">In english</a>
-            @en
-              <a href="{{ url($request_uri) }}" lang="ru">По-русски</a>
-            @endlang
-          </li>
-          {{--
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              {{ trans('menu.language') }}
-              <b class="caret"></b>
+            <a class="{{ $self == 'Life' ? 'navbar-selected' : '' }}" href="{{ action('Life@index') }}">
+              {{ trans('menu.life') }}
             </a>
-            <ul class="dropdown-menu">
-              <li><a href="{{ url("en/{$request_uri}") }}" lang="en">English</a></li>
-              <li><a href="{{ url($request_uri) }}" lang="ru">Русский</a></li>
-            </ul>
           </li>
-          --}}
-          @section('header_user')
-            @if (Auth::check())
-              <li>
-                <a class="{{ $self == 'Notifications' ? 'navbar-selected' : '' }}" href="{{ action('Notifications@index') }}">
-                  @svg (bell)
-                  <span class="counter-label">{{ !is_null(Auth::user()->unreadNotifications()->first()) ? '!' : '' }}</span>
-                </a>
-              </li>
-              @if (!starts_with($self, 'Acp\\'))
-                @if (Auth::user()->isAdmin())
-                  <li>
-                    <a href="{{ App::environment('local') ? "{$locale_uri}/acp/dev/templates" : "{$locale_uri}/acp/trips" }}">
-                      @svg (dashboard)
-                    </a>
-                  </li>
-                @endif
-                <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->displayName() }} <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a href="{{ action('Auth@logout') }}">
-                        {{ trans('menu.logout') }}
-                      </a>
-                    </li>
-                  </ul>
+          <li>
+            <a class="{{ $self == 'News' ? 'navbar-selected' : '' }}" href="{{ action('News@index') }}">
+              {{ trans('news.index') }}
+            </a>
+          </li>
+          <li>
+            <a class="{{ $self == 'Torrents' ? 'navbar-selected' : '' }}" href="{{ action('Torrents@index') }}">
+              {{ trans('menu.torrents') }}
+            </a>
+          </li>
+        @show
+      </ul>
+      @yield('header_form')
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          @ru
+            <a href="{{ url("en/{$request_uri}") }}" lang="en">In english</a>
+          @en
+            <a href="{{ url($request_uri) }}" lang="ru">По-русски</a>
+          @endlang
+        </li>
+        {{--
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            {{ trans('menu.language') }}
+            <b class="caret"></b>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a href="{{ url("en/{$request_uri}") }}" lang="en">English</a></li>
+            <li><a href="{{ url($request_uri) }}" lang="ru">Русский</a></li>
+          </ul>
+        </li>
+        --}}
+        @section('header_user')
+          @if (Auth::check())
+            <li>
+              <a class="{{ $self == 'Notifications' ? 'navbar-selected' : '' }}" href="{{ action('Notifications@index') }}">
+                @svg (bell)
+                <span class="counter-label">{{ !is_null(Auth::user()->unreadNotifications()->first()) ? '!' : '' }}</span>
+              </a>
+            </li>
+            @if (!starts_with($self, 'Acp\\'))
+              @if (Auth::user()->isAdmin())
+                <li>
+                  <a href="{{ App::environment('local') ? "{$locale_uri}/acp/dev/templates" : "{$locale_uri}/acp/trips" }}">
+                    @svg (dashboard)
+                  </a>
                 </li>
               @endif
-            @else
-              <li>
-                <a href="{{ action('Auth@login') }}">{{ trans('auth.signin') }}</a>
+              <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->displayName() }} <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a href="{{ action('Auth@logout') }}">
+                      {{ trans('menu.logout') }}
+                    </a>
+                  </li>
+                </ul>
               </li>
-              {{--
-              <form class="navbar-form navbar-right">
-                <a class="btn btn-default" href="{{ action('Auth@login') }}">{{ trans('auth.signin') }}</a>
-              </form>
-              --}}
             @endif
-          @show
-        </ul>
-      </div>
+          @else
+            <li>
+              <a href="{{ action('Auth@login') }}">{{ trans('auth.signin') }}</a>
+            </li>
+            {{--
+            <form class="navbar-form navbar-right">
+              <a class="btn btn-default" href="{{ action('Auth@login') }}">{{ trans('auth.signin') }}</a>
+            </form>
+            --}}
+          @endif
+        @show
+      </ul>
     </div>
   </div>
-  <div class="container container-full">
+</div>
+<div class="container-full">
+<div class="container">
 @section('breadcrumbs')
 @include('tpl.breadcrumbs', ['breadcrumbs' => $breadcrumbs ?? Breadcrumbs::get()])
 @show
@@ -127,6 +128,7 @@
 @yield('content')
 @yield('content_footer')
 @if (!Request::pjax())
+</div>
 </div>
 </div>
 <footer>
@@ -156,9 +158,9 @@
 </footer>
 <script>
 <?php echo 'window.AppOptions = ' . json_encode([
-  'csrfToken' => csrf_token(),
   'locale' => $locale,
   'loggedIn' => Auth::check(),
+  'csrfToken' => csrf_token(),
   'yandexMetrikaId' => 5266444,
 ]); ?>
 </script>
