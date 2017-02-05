ae1bed8bf6348b432867c54a46f56ae67d442259
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-05 14:00:00 +0300

    Может эта строчка нужна?

--- resources/views/dcpp/software.blade.php
+++ resources/views/dcpp/software.blade.php
@@ -67,6 +67,7 @@
          data-ad-client="ca-pub-7802683087624570"
          data-ad-slot="1858304644"
          data-ad-format="auto"></ins>
+    <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
   </div>
 @elseif (App::environment('local'))
   <div class="mt-3 banner-local google-b-horizontal"></div>
