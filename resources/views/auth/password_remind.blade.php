9e9a11ab72ab0887cd8922a0e39a5557d1becdca
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-12 21:37:15 +0300

    Подсказка рядом с полем

--- resources/views/auth/password_remind.blade.php
+++ resources/views/auth/password_remind.blade.php
@@ -1,20 +1,23 @@
 @extends('base')
 
 @section('content')
-@include('tpl.form_errors')
-
-<div class="form-signin text-center">
-  <h3 class="mb-4">{{ trans('auth.password_remind_title') }}</h3>
+<div class="form-signin">
+  <h3 class="mb-4 text-center">{{ trans('auth.password_remind_title') }}</h3>
   <form action="{{ action('Auth@passwordRemindPost') }}" method="post">
     {{ ViewHelper::inputHiddenMail() }}
 
     <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
       <input autofocus required type="email" class="form-control" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="{{ trans('auth.email') }}">
+      @if ($errors->has('email'))
+        <span class="help-block">{{ $errors->first('email') }}</span>
+      @endif
     </div>
 
-    <button type="submit" class="btn btn-primary btn-lg">
-      {{ trans('auth.password_remind') }}
-    </button>
+    <div class="text-center">
+      <button type="submit" class="btn btn-primary btn-lg">
+        {{ trans('auth.password_remind') }}
+      </button>
+    </div>
 
     {{ csrf_field() }}
   </form>
