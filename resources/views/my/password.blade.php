f276b50388f4863947ab57b8e10b69405ef719f0
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-13 08:32:21 +0300

    Правка оформления

--- resources/views/my/password.blade.php
+++ resources/views/my/password.blade.php
@@ -3,52 +3,48 @@
 @section('content')
 <h3 class="mt-2 mb-3">{{ trans('my.password') }}</h3>
 
-<form action="{{ action("$self@passwordPut") }}" class="form-horizontal" method="post">
-  {{ ViewHelper::inputHiddenMail() }}
+<div class="row">
+  <div class="col-md-6">
+    <form action="{{ action("$self@passwordPut") }}" method="post">
+      {{ ViewHelper::inputHiddenMail() }}
 
-  @if ($has_password)
-    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
-      <div class="col-md-6">
-        <label>{{ trans('my.old_password') }}</label>
-        <input required type="password" class="form-control" name="password">
-        @if ($errors->has('password'))
-          <span class="help-block">{{ $errors->first('password') }}</span>
+      @if ($has_password)
+        <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+          <label>{{ trans('my.old_password') }}</label>
+          <input required type="password" class="form-control" name="password">
+          @if ($errors->has('password'))
+            <span class="help-block">{{ $errors->first('password') }}</span>
+          @endif
+        </div>
+      @endif
+
+      <div class="form-group {{ $errors->has('new_password') ? 'has-error' : '' }}">
+        <label>{{ trans('my.new_password') }}</label>
+        <input required type="password" class="form-control" name="new_password">
+        @if ($errors->has('new_password'))
+          <span class="help-block">{{ $errors->first('new_password') }}</span>
+        @else
+          @ru
+            <span class="help-block">Не менее 6 символов</span>
+          @en
+            <span class="help-block">Minimum length is 6 characters</span>
+          @endlang
         @endif
       </div>
-    </div>
-  @endif
 
-  <div class="form-group {{ $errors->has('new_password') ? 'has-error' : '' }}">
-    <div class="col-md-6">
-      <label>{{ trans('my.new_password') }}</label>
-      <input required type="password" class="form-control" name="new_password">
-      @if ($errors->has('new_password'))
-        <span class="help-block">{{ $errors->first('new_password') }}</span>
-      @else
-        @ru
-          <span class="help-block">Не менее 6 символов</span>
-        @en
-          <span class="help-block">Minimum length is 6 characters</span>
-        @endlang
-      @endif
-    </div>
-  </div>
-
-  <div class="form-group">
-    <div class="col-md-6">
       <button type="submit" class="btn btn-primary">
         {{ trans("$tpl.save") }}
       </button>
-    </div>
-  </div>
 
-  {{ method_field('put') }}
-  {{ csrf_field() }}
-</form>
+      {{ method_field('put') }}
+      {{ csrf_field() }}
+    </form>
+  </div>
+</div>
 
 @if ($has_password)
   <h3 class="mt-5">{{ trans('auth.forgot_password') }}</h3>
-  <form action="{{ action('Auth@passwordRemindPost') }}" class="form-horizontal" method="post">
+  <form action="{{ action('Auth@passwordRemindPost') }}" method="post">
     {{ ViewHelper::inputHiddenMail() }}
 
     @ru
--- resources/views/my/profile.blade.php
+++ resources/views/my/profile.blade.php
@@ -3,44 +3,40 @@
 @section('content')
 <h3 class="mt-2 mb-3">{{ trans('my.profile') }}</h3>
 
-<form action="{{ action("$self@profilePut") }}" class="form-horizontal" method="post">
-  {{ ViewHelper::inputHiddenMail() }}
+<div class="row">
+  <div class="col-md-6">
+    <form action="{{ action("$self@profilePut") }}" method="post">
+      {{ ViewHelper::inputHiddenMail() }}
 
-  <div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
-    <div class="col-md-6">
-      <label>{{ trans('my.username') }}</label>
-      <input type="text" class="form-control" name="username" value="{{ old('username', Auth::user()->login) }}">
-      @if ($errors->has('username'))
-        <span class="help-block">{{ $errors->first('username') }}</span>
-      @else
-        @ru
-          <span class="help-block">От 2 до 32 символов. Первые два символа логина будут использованы для аватарки. В случае его отсутствия — первые два символа адреса электронной почты</span>
-        @en
-          <span class="help-block">From 2 to 32 characters. First two characters would be used as an avatar. In case of empty username, first two characters of email would be used.</span>
-        @endlang
-      @endif
-    </div>
-  </div>
+      <div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
+        <label>{{ trans('my.username') }}</label>
+        <input type="text" class="form-control" name="username" value="{{ old('username', Auth::user()->login) }}">
+        @if ($errors->has('username'))
+          <span class="help-block">{{ $errors->first('username') }}</span>
+        @else
+          @ru
+            <span class="help-block">От 2 до 32 символов. Первые два символа логина будут использованы для аватарки. В случае его отсутствия — первые два символа адреса электронной почты</span>
+          @en
+            <span class="help-block">From 2 to 32 characters. First two characters would be used as an avatar. In case of empty username, first two characters of email would be used.</span>
+          @endlang
+        @endif
+      </div>
 
-  <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-    <div class="col-md-6">
-      <label>{{ trans('auth.email') }}</label>
-      <input required type="text" class="form-control" name="email" value="{{ old('email', Auth::user()->email) }}">
-      @if ($errors->has('email'))
-        <span class="help-block">{{ $errors->first('email') }}</span>
-      @endif
-    </div>
-  </div>
+      <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+        <label>{{ trans('auth.email') }}</label>
+        <input required type="text" class="form-control" name="email" value="{{ old('email', Auth::user()->email) }}">
+        @if ($errors->has('email'))
+          <span class="help-block">{{ $errors->first('email') }}</span>
+        @endif
+      </div>
 
-  <div class="form-group mt-4">
-    <div class="col-md-6">
       <button type="submit" class="btn btn-primary">
         {{ trans("$tpl.save") }}
       </button>
-    </div>
-  </div>
 
-  {{ method_field('put') }}
-  {{ csrf_field() }}
-</form>
+      {{ method_field('put') }}
+      {{ csrf_field() }}
+    </form>
+  </div>
+</div>
 @endsection
