63d65ab4ee996be683d82e627270b7b58cd04c62
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-16 04:18:44 +0300

    Правка вывода подсказок

--- resources/views/tpl/kp_movies.blade.php
+++ resources/views/tpl/kp_movies.blade.php
@@ -1,5 +1,9 @@
 @foreach ($movies as $movie)
-  <a class="tooltipped tooltipped-s" aria-label="{{ $movie['title'] or '' }}" href="https://www.kinopoisk.ru/film/{{ $movie['id'] }}/">
-    <img class="screenshot" src="https://st.kp.yandex.net/images/film_big/{{ $movie['id'] }}.jpg" height="250">
+  <a class="d-inline-block screenshot-link tooltipped tooltipped-n"
+     aria-label="{{ $movie['title'] ?? '' }}"
+     href="https://www.kinopoisk.ru/film/{{ $movie['id'] }}/">
+    <img class="screenshot"
+         src="https://st.kp.yandex.net/images/film_big/{{ $movie['id'] }}.jpg"
+         height="250">
   </a>
 @endforeach
