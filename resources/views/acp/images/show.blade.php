99bea6a5e70844c0892e0f4ded914e32b046b88c
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-22 01:43:55 +0300

    Рамка у изображения

--- resources/views/acp/images/show.blade.php
+++ resources/views/acp/images/show.blade.php
@@ -1,7 +1,7 @@
 @extends("$tpl.base")
 
 @section('content')
-<p><img class="gallery-photo" src="{{ $model->originalSecretUrl() }}"></p>
+<p><img class="screenshot" src="{{ $model->originalSecretUrl() }}"></p>
 @if (Auth::user()->isRoot())
   <pre class="json-model">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
 @endif
