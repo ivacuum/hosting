082ed64e879e405475c593aebf3847bfa989395f
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-24 06:08:20 +0300

    Ссылка на комментарий в уведомлениях

--- resources/views/notifications/torrent_commented.blade.php
+++ resources/views/notifications/torrent_commented.blade.php
@@ -1,5 +1,5 @@
 {{-- Комментарий к раздаче --}}
 <strong>{{ $notification->data['comment']['user']['name'] }}</strong>
 {{ trans("notifications.{$class_basename}") }}
-<a class="link" href="{{ action('Torrents@torrent', $notification->data['id']) }}">{{ str_limit($notification->data['title'], 100) }}</a>
+<a class="link" href="{{ action('Torrents@torrent', $notification->data['id']) }}#comment-{{ $notification->data['comment']['id'] }}">{{ str_limit($notification->data['title'], 100) }}</a>
 <div class="my-1">{!! $notification->data['comment']['html'] !!}</div>
--- resources/views/notifications/trip_commented.blade.php
+++ resources/views/notifications/trip_commented.blade.php
@@ -1,5 +1,5 @@
 {{-- Комментарий к поездке --}}
 <strong>{{ $notification->data['comment']['user']['name'] }}</strong>
 {{ trans("notifications.{$class_basename}") }}
-<a class="link" href="{{ action('Life@page', $notification->data['slug']) }}">{{ str_limit($notification->data['title'], 100) }}</a>
+<a class="link" href="{{ action('Life@page', $notification->data['slug']) }}#comment-{{ $notification->data['comment']['id'] }}">{{ str_limit($notification->data['title'], 100) }}</a>
 <div class="my-1">{!! $notification->data['comment']['html'] !!}</div>
