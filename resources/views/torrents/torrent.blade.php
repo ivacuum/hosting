af5bfa68fa8b9715a4ba4dfa3cbbcd1ed60a6dbd
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-24 13:31:35 +0300

    Краткая шапка раздачи

--- resources/views/torrents/torrent.blade.php
+++ resources/views/torrents/torrent.blade.php
@@ -1,28 +1,25 @@
 @extends('torrents.base')
 
 @section('content')
-<div class="panel panel-default text-center center-block torrent-stats-container">
-  <div class="panel-heading">
-    Статистика раздачи
-    <a href="{{ $torrent->externalLink() }}">
-      @svg (external-link)
-    </a>
-  </div>
-  <div class="panel-body">
-    Зарегистрирована: <strong>{{ $torrent->registered_at->diffForHumans(null, true) }}</strong>
-    <span class="torrent-stats-separator">&middot;</span>
-    <span class="text-success">{{ $torrent->seeders }} {{ trans_choice('plural.seeders', $torrent->seeders) }}</span>
-  </div>
-  <div class="panel-footer">
-    <a class="btn btn-success js-magnet" href="{{ $torrent->magnet() }}" data-action="{{ action('Torrents@magnet', $torrent) }}">
-      {{ trans('torrents.download') }} &middot; {{ ViewHelper::size($torrent->size) }}
-    </a>
-  </div>
+<div class="my-3 text-center">
+  <a class="btn btn-success js-magnet" href="{{ $torrent->magnet() }}" data-action="{{ action('Torrents@magnet', $torrent) }}">
+    <span class="mr-1">
+      @svg (magnet)
+    </span>
+    {{ trans('torrents.download') }}
+    <span class="mx-1">&middot;</span>
+    {{ ViewHelper::size($torrent->size) }}
+  </a>
 </div>
 <rutracker-post>{!! $torrent->html !!}</rutracker-post>
 <div class="my-3 text-center">
   <a class="btn btn-success js-magnet" href="{{ $torrent->magnet() }}" data-action="{{ action('Torrents@magnet', $torrent) }}">
-    {{ trans('torrents.download') }} &middot; {{ ViewHelper::size($torrent->size) }}
+    <span class="mr-1">
+      @svg (magnet)
+    </span>
+    {{ trans('torrents.download') }}
+    <span class="mx-1">&middot;</span>
+    {{ ViewHelper::size($torrent->size) }}
   </a>
 </div>
 
