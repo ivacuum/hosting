06061a3fbe5eb4b84ed26f57895b41c29f2d886e
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-24 06:08:03 +0300

    Улучшение вывода мета-информации новостей

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -6,29 +6,39 @@
     <div class="col-md-8">
       @foreach ($news as $model)
         <h3 class="mt-0"><a class="link" href="{{ action("$self@show", $model) }}">{{ $model->title }}</a></h3>
-        <p class="text-muted">
-          <span class="mr-1 svg-muted">
-            @svg (calendar-o)
+        <div class="text-muted f14 mb-3">
+          <span class="text-nowrap mr-3">
+            <span class="mr-1 svg-muted">
+              @svg (calendar-o)
+            </span>
+            {{ $model->created_at->formatLocalized('%e %B %Y') }}
           </span>
-          {{ $model->created_at->formatLocalized('%e %B %Y') }}
           @if (!is_null($model->user) && $model->user->login)
-            <span class="ml-3 mr-1 svg-muted">
-              @svg (pencil)
+            <span class="text-nowrap mr-3">
+              <span class="mr-1 svg-muted">
+                @svg (pencil)
+              </span>
+              {{ $model->user->login }}
             </span>
-            {{ $model->user->login }}
           @endif
-          <span class="ml-3 mr-1 svg-muted">
-            @svg (eye)
+          <span class="text-nowrap mr-3">
+            <span class="mr-1 svg-muted">
+              @svg (eye)
+            </span>
+            {{ ViewHelper::number($model->views) }}
           </span>
-          {{ ViewHelper::number($model->views) }}
           @if ($model->comments_count)
-            <span class="ml-3 mr-1 svg-muted">
-              @svg (comment-o)
+            <span class="text-nowrap mr-3">
+              <span class="mr-1 svg-muted">
+                @svg (comment-o)
+              </span>
+              {{ ViewHelper::number($model->comments_count) }}
             </span>
-            {{ ViewHelper::number($model->comments_count) }}
           @endif
-        </p>
-        <div class="mb-5 hidden-xs">{!! $model->html !!}</div>
+        </div>
+        <div class="mb-5">
+          <div class="hidden-xs">{!! $model->html !!}</div>
+        </div>
       @endforeach
     </div>
   </div>
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -2,22 +2,28 @@
 
 @section('content')
 <h1 class="h3 mt-0">{{ $news->title }}</h1>
-<p class="text-muted">
-  <span class="svg-muted mr-1">
-    @svg (calendar-o)
-  </span>
-  {{ $news->created_at->formatLocalized('%e %B %Y') }}
-  @if ($news->user->login)
-    <span class="ml-3 mr-1 svg-muted">
-      @svg (pencil)
+<div class="text-muted f14 mb-3">
+  <span class="text-nowrap mr-3">
+    <span class="svg-muted mr-1">
+      @svg (calendar-o)
     </span>
-    {{ $news->user->login }}
-  @endif
-  <span class="ml-3 mr-1 svg-muted">
-    @svg (eye)
+    {{ $news->created_at->formatLocalized('%e %B %Y') }}
   </span>
-  {{ ViewHelper::number($news->views) }}
-</p>
+  @if ($news->user->login)
+    <span class="text-nowrap mr-3">
+      <span class="mr-1 svg-muted">
+        @svg (pencil)
+      </span>
+      {{ $news->user->login }}
+    </span>
+  @endif
+  <span class="text-nowrap mr-3">
+    <span class="mr-1 svg-muted">
+      @svg (eye)
+    </span>
+    {{ ViewHelper::number($news->views) }}
+  </span>
+</div>
 <div>{!! $news->html !!}</div>
 
 @include('tpl.comments-list')
