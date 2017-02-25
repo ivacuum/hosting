b014c07bb014233f5ecd9fdbda42e5cb1ee055d3
Sergey Pankov <vacuum@ivacuum.ru>
2017-02-25 12:20:53 +0300

    Возможность убрать рамку у поля при написании поста

--- resources/views/acp/news/form.blade.php
+++ resources/views/acp/news/form.blade.php
@@ -20,10 +20,9 @@
   </div>
 </div>
 
-<div class="form-group {{ $errors->has('html') ? 'has-error' : '' }}">
-  <label class="col-md-3 control-label required">HTML:</label>
-  <div class="col-md-9">
-    <textarea required class="form-control textarea-autosized js-autosize-textarea" name="html">{{ old('html', @$model->html) }}</textarea>
+<div class="form-group mt-4 {{ $errors->has('html') ? 'has-error' : '' }}">
+  <div class="col-xs-12">
+    <textarea required class="form-control textarea-autosized textarea-borderless-focus js-autosize-textarea" name="html">{{ old('html', @$model->html) }}</textarea>
   </div>
 </div>
 
