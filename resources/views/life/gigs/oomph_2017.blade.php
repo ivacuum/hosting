85cd10676e4ef037a1a02f25feb2b560513e1cc0
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-03 12:20:14 +0300

    Инфа о предстоящем концерте Oomph

--- resources/views/life/gigs/oomph_2017.blade.php
+++ resources/views/life/gigs/oomph_2017.blade.php
@@ -5,10 +5,22 @@
 @section('content')
 <div class="row">
   <div class="col-md-7">
+    @ru
+      <p>Новый тур группы Oomph! Это будет уже шестое выступление по счету в Москве. Сетлист будет известен вместе с первым концертом в Санкт-Петербурге 24 марта.</p>
+      <p>В своем Фэйсбуке участники группы <a class="link" href="https://www.facebook.com/oomphband/videos/10154365026933803/">пообещали</a> исполнить одну песню, которая никогда не звучала на просторах СНГ. Также будет несколько песен в новой обработке. Без хитов публика тоже не останется.</p>
+      <p>Ждем-с.</p>
+      <p>
+        <a class="btn btn-primary" href="https://ponominalu.ru/event/oomph">Купить билет</a>
+        <a class="btn btn-default" href="https://vk.com/oomph_msk">Группа ВК</a>
+        <a class="btn btn-default" href="https://www.treehouse-ticketing.com/cat/index/sCategory/95">Meet&Greet</a>
+      </p>
+    @endlang
+    {{--
     @include('tpl.setlist-title')
     <ol>
       <li>—</li>
     </ol>
+    --}}
   </div>
   <div class="col-md-5">
     <div class="img-container">
