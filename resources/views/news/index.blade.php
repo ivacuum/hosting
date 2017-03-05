72b1562c41375eff10273b8ffcb29580d670405b
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-05 09:53:25 +0300

    Микроразметка новостей

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,47 +5,57 @@
   <div class="row">
     <div class="col-md-8">
       @foreach ($news as $model)
-        <h3 class="mt-0"><a class="link" href="{{ action("$self@show", $model) }}">{{ $model->title }}</a></h3>
-        <div class="text-muted f14 mb-3">
-          <span class="text-nowrap mr-3">
-            <span class="mr-1 svg-muted">
-              @svg (calendar-o)
-            </span>
-            {{ $model->created_at->formatLocalized('%e %B %Y') }}
-          </span>
-          @if (!is_null($model->user) && $model->user->login)
-            <span class="text-nowrap mr-3">
-              <span class="mr-1 svg-muted">
-                @svg (pencil)
+        <article itemscope itemtype="http://schema.org/BlogPosting">
+          <header>
+            <h3 class="mt-0" itemprop="headline"><a class="link" href="{{ action("$self@show", $model) }}">{{ $model->title }}</a></h3>
+            <div class="text-muted f14 mb-3">
+              <span class="text-nowrap mr-3">
+                <span class="mr-1 svg-muted">
+                  @svg (calendar-o)
+                </span>
+                <time itemprop="datePublished" datetime="{{ $model->created_at->toDateString() }}">
+                  {{ $model->created_at->formatLocalized('%e %B %Y') }}
+                </time>
               </span>
-              {{ $model->user->login }}
-            </span>
-          @endif
-          <span class="text-nowrap mr-3">
-            <span class="mr-1 svg-muted">
-              @svg (eye)
-            </span>
-            {{ ViewHelper::number($model->views) }}
-          </span>
-          @if ($model->comments_count)
-            <span class="text-nowrap mr-3">
-              <span class="mr-1 svg-muted">
-                @svg (comment-o)
+              @if (!is_null($model->user) && $model->user->login)
+                <span class="text-nowrap mr-3">
+                  <span class="mr-1 svg-muted">
+                    @svg (pencil)
+                  </span>
+                  {{ $model->user->login }}
+                </span>
+              @endif
+              <span class="text-nowrap mr-3">
+                <span class="mr-1 svg-muted">
+                  @svg (eye)
+                </span>
+                {{ ViewHelper::number($model->views) }}
               </span>
-              {{ ViewHelper::number($model->comments_count) }}
-            </span>
-          @endif
-        </div>
-        <div class="mb-5">
-          <div class="hidden-xs">{!! $model->html !!}</div>
-        </div>
+              @if ($model->comments_count)
+                <span class="text-nowrap mr-3">
+                  <span class="mr-1 svg-muted">
+                    @svg (comment-o)
+                  </span>
+                  <span itemprop="commentCount">{{ ViewHelper::number($model->comments_count) }}</span>
+                </span>
+              @endif
+            </div>
+          </header>
+          <div class="mb-5">
+            <div class="hidden-xs">{!! $model->html !!}</div>
+          </div>
+        </article>
       @endforeach
     </div>
   </div>
 
   @include('tpl.paginator', ['class' => 'mt-3 text-center', 'paginator' => $news])
 @else
-  <p>По данным критериям новости не найдены.</p>
+  @ru
+    <p>По данным критериям новости не найдены.</p>
+  @en
+    <p>No news found.</p>
+  @endlang
 @endif
 
 @endsection
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,32 +1,36 @@
 @extends('base')
 
 @section('content')
-<h1 class="h3 mt-0">{{ $news->title }}</h1>
-<div class="text-muted f14 mb-3">
-  <span class="text-nowrap mr-3">
-    <span class="svg-muted mr-1">
-      @svg (calendar-o)
-    </span>
-    <time datetime="{{ $news->created_at->toDateString() }}">
-      {{ $news->created_at->formatLocalized('%e %B %Y') }}
-    </time>
-  </span>
-  @if ($news->user->login)
-    <span class="text-nowrap mr-3">
-      <span class="mr-1 svg-muted">
-        @svg (pencil)
+<article itemscope itemtype="http://schema.org/BlogPosting">
+  <header>
+    <h1 class="h3 mt-0" itemprop="headline">{{ $news->title }}</h1>
+    <div class="text-muted f14 mb-3">
+      <span class="text-nowrap mr-3">
+        <span class="svg-muted mr-1">
+          @svg (calendar-o)
+        </span>
+        <time itemprop="datePublished" datetime="{{ $news->created_at->toDateString() }}">
+          {{ $news->created_at->formatLocalized('%e %B %Y') }}
+        </time>
       </span>
-      {{ $news->user->login }}
-    </span>
-  @endif
-  <span class="text-nowrap mr-3">
-    <span class="mr-1 svg-muted">
-      @svg (eye)
-    </span>
-    {{ ViewHelper::number($news->views) }}
-  </span>
-</div>
-<div>{!! $news->html !!}</div>
+      @if ($news->user->login)
+        <span class="text-nowrap mr-3">
+          <span class="mr-1 svg-muted">
+            @svg (pencil)
+          </span>
+          {{ $news->user->login }}
+        </span>
+      @endif
+      <span class="text-nowrap mr-3">
+        <span class="mr-1 svg-muted">
+          @svg (eye)
+        </span>
+        {{ ViewHelper::number($news->views) }}
+      </span>
+    </div>
+  </header>
+  <div>{!! $news->html !!}</div>
+</article>
 
 @include('tpl.comments-list')
 @include('tpl.comment-add', ['params' => ['news', $news->id]])
