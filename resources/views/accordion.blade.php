26d1d114ee77b119790a949a52a2995b24a971e1
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-05 13:08:54 +0300

    Вопрос-ответ спойлерами

---
+++ resources/views/accordion.blade.php
@@ -0,0 +1,11 @@
+@php ($id = $id ?? 'accordion_'.str_random(6))
+<div class="panel panel-default mb-2">
+  <div class="panel-heading">
+    <h4 class="panel-title">
+      <a href="#{{ $id }}" data-toggle="collapse">{{ $title }}</a>
+    </h4>
+  </div>
+  <div id="{{ $id }}" class="panel-collapse collapse">
+    <div class="panel-body">{{ $slot }}</div>
+  </div>
+</div>
