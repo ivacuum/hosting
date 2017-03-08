0eda6c458b61ef13e19afadb7192a038a38ff5fa
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-08 12:35:50 +0300

    Удаление неиспользуемого функционала

--- app/Http/Controllers/Ajax.php
+++ app/Http/Controllers/Ajax.php
@@ -38,29 +38,6 @@ class Ajax extends Controller
         return back()->with('message', trans('comments.posted'));
     }
 
-    /*
-    public function feedback()
-    {
-        $this->validate($this->request, [
-            'mail' => 'empty',
-            'text' => 'required',
-        ]);
-
-        $text = $this->request->input('text');
-        $referer = $this->request->server('HTTP_REFERER');
-        $question = $this->request->input('question');
-
-        register_shutdown_function(function () use ($question, $referer, $text) {
-            \Mail::send(new Feedback($referer, $question, $text));
-        });
-
-        return [
-            'message' => trans('ajax.feedback.ok'),
-            'status'  => 'OK',
-        ];
-    }
-    */
-
     protected function notifiableModel($type, $id)
     {
         if ($type === 'news') {
