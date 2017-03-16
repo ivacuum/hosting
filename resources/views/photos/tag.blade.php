e6efa17d948d26257d9b74fb453ec2c3b3860750
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-16 23:27:48 +0300

    Красивый вывод фоток по тэгу

--- resources/views/photos/tag.blade.php
+++ resources/views/photos/tag.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
 <h3 class="mt-0">#{{ $tag->title }}</h3>
-<div>
+<div class="gallery-flex">
   @foreach ($tag->photos as $photo)
-    <a class="screenshot-link" href="{{ action("$self@show", [$photo, 'tag_id' => $tag]) }}">
-      <img class="image-200 screenshot" src="{{ $photo->thumbnailUrl() }}">
-    </a>
+    <div class="gallery-image mb-4">
+      <a class="screenshot-link" href="{{ action("$self@show", [$photo, 'tag_id' => $tag]) }}">
+        <img class="image-200 screenshot" src="{{ $photo->thumbnailUrl() }}">
+      </a>
+    </div>
   @endforeach
 </div>
 @endsection
