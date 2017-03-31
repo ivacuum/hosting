6f058452c4b8d8255a2e952caff745718b37098a
Sergey Pankov <vacuum@ivacuum.ru>
2017-03-31 06:39:43 +0300

    Правильные шаблоны листалок страниц

--- resources/views/tpl/paginator.blade.php
+++ resources/views/tpl/paginator.blade.php
@@ -1,5 +1,9 @@
 @if ($paginator->hasPages())
   <div class="{{ $class ?? 'mt-3 text-center' }}">
-    {{ $paginator->links() }}
+    @if ($paginator instanceof Illuminate\Pagination\LengthAwarePaginator)
+      {{ $paginator->links('tpl.pagination') }}
+    @else
+      {{ $paginator->links('tpl.pagination-simple') }}
+    @endif
   </div>
 @endif
