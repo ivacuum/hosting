084705b82120a57f899e3f76c0b652e979c9b181
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-01 19:03:54 +0300

    Якутск

---
+++ resources/views/life/trips/yakutsk_2012_09.blade.php
@@ -0,0 +1,25 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Осенью (очень короткой) повсюду грязь, как и на всей территории России.</p>
+@en
+  <p>Fall is very dirty, as well as throughout Russia. But here fall is short.</p>
+@endlang
+
+@ru
+  <p>2 октября уже вовсю бушует снег. Температура днем +18&deg;C, ночью -4&deg;C. Высокие колебания температур свойственны практически на протяжении всего года.</p>
+@endlang
+
+@ru
+  <p>Огромное разнообразие имен. Например, Спартак, Ким, Феликс.</p>
+@en
+  <p>A great variety of names can be heard like Spartacus, Kim, Felix.</p>
+@endlang
+
+@ru
+  <p>Некоторые телевизионные каналы показывают по часовому поясу Владивостока.</p>
+@en
+  <p>Some TV channels are being transmitted by Vladivostok time zone.</p>
+@endlang
+@endsection
