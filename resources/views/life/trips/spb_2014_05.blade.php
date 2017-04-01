feca74a9f56a8346893d4a28862a4c64ad5a4ca6
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-01 05:36:19 +0300

    Петербург

--- resources/views/life/trips/spb_2014_05.blade.php
+++ resources/views/life/trips/spb_2014_05.blade.php
@@ -2,16 +2,188 @@
 
 @section('content')
 @ru
-  <p>Реально ли отправиться в другой город с велосипедом? Разумеется! Берется билет на плацкарт, пустой чехол или сумка помещается в рюкзак с прочими вещами, на вокзале с велосипеда снимается одно или два колеса в зависимости от его размера и набитая сумка закидывается на третью полку купе. Доступных 36 кг бесплатного багажа хватит с лихвой.</p>
+  <p>Реально ли отправиться в другой город с велосипедом? Разумеется! Берется билет на плацкарт, пустой чехол или сумка, помещается в рюкзак с прочими вещами, на вокзале с велосипеда снимается одно или два колеса — в зависимости от его размера — и набитая сумка закидывается на третью полку купе. Доступных 36 кг бесплатного багажа хватит с лихвой.</p>
+  <p>1) пустая сумка в рюкзаке с личными вещами; 2) и 3) велосипед в сумке. Колеса сверху тоже помещаются.</p>
 @endlang
-<div class="js-lazy" data-lazy-type="fotorama" data-height="750" data-width="563">
-  <img src="https://life.ivacuum.ru/spb.2014.05/IMG_0522.jpg">
-  <img src="https://life.ivacuum.ru/spb.2014.05/IMG_0523.jpg">
-  <img src="https://life.ivacuum.ru/spb.2014.05/IMG_0524.jpg">
-</div>
+@include('tpl.fotorama-2x', ['w' => 563, 'h' => 750, 'pics' => [
+  'IMG_0522.jpg',
+  'IMG_0523.jpg',
+  'IMG_0524.jpg',
+]])
+
+@ru
+  <p>Здорово, что у Калуги есть прямое сообщение с Питером. До вокзала Калуга-2 из города на велосипеде реально добраться за 30-40 минут.</p>
+@endlang
+
+@ru
+  <p>Во всей красе московская чума красить заборы и бордюры в желто-зеленый цвет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0525.jpg'])
+
+@ru
+  <p>Путь на вокзал через водохранилище.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0526.jpg'])
+
+@ru
+  <p>Около 12 часов в пути, и вы в Петербурге. Рейс в ночь. Обычно это благодатное время для сна, но в этот раз не сложилось. Прибытие в половине шестого утра на Московский вокзал. Обратная сборка велосипеда, а затем исследование города. Нет, сначала, конечно же, еда, а затем уже исследование города.</p>
+@endlang
+
+@ru
+  <p>Лохматый песик. Нравятся песики?</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0537.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Время.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0539.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Заселение в Приморском районе. До него еще предстояло добраться. По мостам.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0540.jpg'])
+
+@ru
+  <p>По тротуарам.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0542.jpg'])
+
+@ru
+  <p>Снова по мостам.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0544.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Вот и Приморский район.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0545.jpg'])
+
+@ru
+  <p>Между прочим, 9 мая на дворе. РЖД дает скидку 50% на поездки 8–10 мая, поэтому добраться до Санкт-Петербурга стоило всего 666,6 ₽.</p>
+@endlang
+
+@ru
+  <p>Навигация в метро. Выход переведен не как exit, а way out.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0547.jpg'])
+
+@ru
+  <p>Туалет.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0548.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Обилие окон.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0601.jpg'])
+
+@ru
+  <p>Удачный снимок улицы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0603.jpg'])
+
+@ru
+  <p>В другую сторону.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0604.jpg'])
+
+@ru
+  <p>Грандиозное празднование, парад, все дела.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0556.jpg'])
+
+@ru
+  <p>Упс, на самом деле все люди на Невском. Вот где движуха.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0571.jpg'])
+
+@ru
+  <p>Поддержка с воздуха.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0558.jpg'])
+
+@ru
+  <p>Колокол.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0559.jpg', 'w' => 563, 'h' => 750])
 
 <a name="exhibition_photo"></a>
-<div class="img-container">
-  <img src="https://life.ivacuum.ru/spb.2014.05/IMG_0551.jpg" width="563" height="750">
-</div>
+@ru
+  <p>Аллея.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0551.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Дождь сменялся солнцем со страшной скоростью и периодичностью.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0563.jpg'])
+
+@ru
+  <p>Подземное такси.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0569.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Вернемся к велосипеду. Покататься в городе есть где.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0573.jpg',
+  'IMG_0574.jpg',
+  'IMG_0577.jpg',
+  'IMG_0579.jpg',
+  'IMG_0580.jpg',
+  'IMG_0590.jpg',
+  'IMG_0591.jpg',
+  'IMG_0593.jpg',
+  'IMG_0594.jpg',
+]])
+
+@ru
+  <p>Внезапная смена местности. И бордюр для бодрости.</p>
+@endlang
+@include('tpl.pic-arbitrary', ['pic' => 'IMG_0596.jpg', 'w' => 563, 'h' => 750])
+
+@ru
+  <p>Рыбак.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0575.jpg'])
+
+@ru
+  <p>Гребцы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0578.jpg'])
+
+@ru
+  <p>Яхт-клуб.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0584.jpg',
+  'IMG_0585.jpg',
+]])
+
+@ru
+  <p>Строится стадион.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0588.jpg'])
+
+@ru
+  <p>Обратный путь после дождя.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0611.jpg'])
+
+@ru
+  <p>Минутная остановка у Невы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0612.jpg'])
+
+@ru
+  <p>Снова около 11 часов на поезде. Вот и Калуга-2 в половине третьего ночи.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0616.jpg'])
+
+@ru
+  <p>Путь в город. Ночью дорога менее привлекательна, но однозначно быстрее по пустынным дорогам.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0621.jpg'])
 @endsection
