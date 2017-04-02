c36d6dbddced782dd3fae28ba8e08fb5a6af2606
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-02 21:03:50 +0300

    Якутск

--- resources/views/life/trips/yakutsk_2011.blade.php
+++ resources/views/life/trips/yakutsk_2011.blade.php
@@ -1,5 +1,242 @@
 @extends('life.trips.base')
 
 @section('content')
-@include('tpl.pic', ['pic' => 'IMG_1785.jpg'])
+@ru
+  <p>Театр.</p>
+@en
+  <p>Theater.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1668.jpg'])
+
+@ru
+  <p>Балкон.</p>
+@en
+  <p>Balcony.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1673.jpg'])
+
+@ru
+  <p>Жилой дом и убитые тротуары.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1675.jpg'])
+
+@ru
+  <p>Дом не на сваях, поэтому постепенно уходит под землю по мере прогрева мерзлоты под ним.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1679.jpg'])
+
+@ru
+  <p>Под землей коммуникации не пустишь из-за той же вечной мерзлоты, поэтому все наверху. И сзади дом на сваях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1689.jpg'])
+
+@ru
+  <p>Высотка.</p>
+@en
+  <p>Skyscraper.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1681.jpg'])
+
+@ru
+  <p>Нет, не трава. Такая вода.</p>
+@en
+  <p>No, not the grass. It's water.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1687.jpg'])
+
+@ru
+  <p>Здание цирка за рекламой.</p>
+@en
+  <p>Circus behind the advertisement.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1690.jpg'])
+
+@ru
+  <p>Лошадки.</p>
+@en
+  <p>Horses.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1693.jpg'])
+
+@ru
+  <p>Небо и облака выглядят гораздо дальше, чем в центральной России. После возвращения в Москву кажется, что сверху на тебя все давит. А тут все разреженно и спокойно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1697.jpg'])
+
+@ru
+  <p>За окном во время движения.</p>
+@en
+  <p>Outside the window while driving.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1704.jpg',
+  'IMG_1717.jpg',
+  'IMG_1721.jpg',
+  'IMG_1726.jpg',
+  'IMG_1727.jpg',
+  'IMG_1801.jpg',
+  'IMG_1802.jpg',
+]])
+
+@ru
+  <p>Евражка.</p>
+@en
+  <p>Arctic ground squirrel.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1724.jpg',
+  'IMG_1725.jpg',
+]])
+
+@ru
+  <p>Дальше перемещение на лодках по реке Лене к Ленским столбам.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1729.jpg',
+  'IMG_1730.jpg',
+  'IMG_1736.jpg',
+  'IMG_1737.jpg',
+]])
+
+@ru
+  <p>Могучие Ленские столбы.</p>
+@en
+  <p>Mighty Lena Pillars.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1731.jpg',
+  'IMG_1732.jpg',
+  'IMG_1734.jpg',
+  'IMG_1735.jpg',
+  'IMG_1742.jpg',
+  'IMG_1743.jpg',
+  'IMG_1744.jpg',
+  'IMG_1745.jpg',
+  'IMG_1746.jpg',
+  'IMG_1750.jpg',
+  'IMG_1753.jpg',
+  'IMG_1754.jpg',
+  'IMG_1761.jpg',
+  'IMG_1762.jpg',
+  'IMG_1763.jpg',
+  'IMG_1765.jpg',
+  'IMG_1766.jpg',
+]])
+
+@ru
+  <p>Захватывающие панорамы.</p>
+@en
+  <p>Spectacular panoramas.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1772.jpg',
+  'IMG_1773.jpg',
+  'IMG_1774.jpg',
+  'IMG_1776.jpg',
+  'IMG_1778.jpg',
+  'IMG_1780.jpg',
+  'IMG_1785.jpg',
+]])
+
+@ru
+  <p>Подъем занимает около получаса.</p>
+@en
+  <p>Ascending takes about half an hour.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1791.jpg',
+  'IMG_1795.jpg',
+]])
+
+@ru
+  <p>Не то находка, не то потеря.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1782.jpg',
+  'IMG_1783.jpg',
+  'IMG_1784.jpg',
+]])
+
+@ru
+  <p>Гигантский муравей.</p>
+@en
+  <p>Giant ant.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1786.jpg'])
+
+@ru
+  <p>Местная традиция оставлять ткани на деревьях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1790.jpg'])
+
+@ru
+  <p>Пещера с ледяными скульптурами. Снаружи около +30&deg;C, внутри -5&deg;C. Лето в Якутске очень жаркое, словно находишься на курорте.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1804.jpg',
+  'IMG_1814.jpg',
+  'IMG_1816.jpg',
+  'IMG_1819.jpg',
+  'IMG_1831.jpg',
+  'IMG_1834.jpg',
+  'IMG_1835.jpg',
+  'IMG_1837.jpg',
+  'IMG_1840.jpg',
+  'IMG_1856.jpg',
+  'IMG_1857.jpg',
+  'IMG_1858.jpg',
+  'IMG_1861.jpg',
+  'IMG_1862.jpg',
+  'IMG_1864.jpg',
+  'IMG_1867.jpg',
+  'IMG_1868.jpg',
+  'IMG_1869.jpg',
+  'IMG_1871.jpg',
+  'IMG_1872.jpg',
+]])
+
+@ru
+  <p>Еще панорамы.</p>
+@en
+  <p>More panoramas.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1881.jpg',
+  'IMG_1884.jpg',
+]])
+
+@ru
+  <p>Судно на воздушной подушке.</p>
+@en
+  <p>Hovercraft.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1885.jpg'])
+
+@ru
+  <p>Лесок.</p>
+@en
+  <p>Forest.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1888.jpg'])
+
+@ru
+  <p>Вечный огонь.</p>
+@en
+  <p>Eternal flame.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2064.jpg'])
+
+@ru
+  <p>Композиция.</p>
+@en
+  <p>Composition.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2065.jpg'])
+
+@ru
+  <p>Выезд в аэропорт на рассвете.</p>
+@en
+  <p>Departure to the airport at sunrise.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_2257.jpg'])
 @endsection
