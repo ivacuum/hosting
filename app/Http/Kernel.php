8b4829509348fd56f2d3b122e987a926fa07f817
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-26 17:07:08 +0300

    Отключение кэширования страниц

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,21 +9,18 @@ use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
-use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Foundation\Http\Middleware\ValidatePostSize;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Ivacuum\Generic\Middleware\NoCacheHeaders;
 
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        // CheckForMaintenanceMode::class,
         ValidatePostSize::class,
         TrimStrings::class,
-        // ConvertEmptyStringsToNull::class,
     ];
 
     protected $middlewareGroups = [
@@ -34,6 +31,7 @@ class Kernel extends HttpKernel
             ShareErrorsFromSession::class,
             VerifyCsrfToken::class,
             SubstituteBindings::class,
+            NoCacheHeaders::class,
         ],
 
         'api' => [
