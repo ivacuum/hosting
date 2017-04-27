f821822324c226fe646cefaff11be70e3129cbab
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-27 11:31:12 +0300

    Рефакторинг

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,24 @@
+<?php namespace Tests;
+
+use Illuminate\Contracts\Console\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        // if (file_exists(dirname(__DIR__) . '/.env.test')) {
+        //     Dotenv::load(dirname(__DIR__), '.env.test');
+        // }
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,17 +1,6 @@
 <?php namespace Tests;
 
-class TestCase extends \Illuminate\Foundation\Testing\TestCase
+abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        // if (file_exists(dirname(__DIR__) . '/.env.test')) {
-        //     Dotenv::load(dirname(__DIR__), '.env.test');
-        // }
-
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
+    use CreatesApplication;
 }
