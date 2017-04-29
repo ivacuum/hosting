5048353108901ea42a452c11cd393533f2c193f7
Sergey Pankov <vacuum@ivacuum.ru>
2017-04-29 14:07:02 +0300

    Рефакторинг

--- resources/assets/js/pwa/service-worker-installer.js
+++ resources/assets/js/pwa/service-worker-installer.js
@@ -1,15 +1,14 @@
 (function() {
-  'use strict';
+  'use strict'
 
   if ('serviceWorker' in navigator) {
     // Service worker можно установить только с https либо localhost
     // http://www.w3.org/TR/2015/WD-service-workers-20150625/#security-considerations
     if (location.protocol === 'https:' || location.hostname === 'localhost') {
-
       navigator.serviceWorker.register('/assets/service-worker.js', { scope: '/' })
         .catch(function(e) {
-          console.error('service worker не зарегистрировался: ', e);
-        });
+          console.error('service worker не зарегистрировался: ', e)
+        })
     }
   }
-})();
+})()
