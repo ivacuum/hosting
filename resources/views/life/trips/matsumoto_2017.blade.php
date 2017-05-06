d09e47343cd9387c8c1c3c53a25cee8e243ac5bd
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-06 20:17:21 +0300

    Мацумото

---
+++ resources/views/life/trips/matsumoto_2017.blade.php
@@ -0,0 +1,123 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>За окном во время поездки на автобусе из Такаямы в Мацумото.</p>
+@endlang
+<youtube title="Matsumoto Bus, April 2017" v="neekGArH7pU"></youtube>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7744.jpg',
+  'IMG_7746.jpg',
+  'IMG_7754.jpg',
+  'IMG_7763.jpg',
+  'IMG_7771.jpg',
+  'IMG_7781.jpg',
+  'IMG_7820.jpg',
+  'IMG_7832.jpg',
+  'IMG_7845.jpg',
+]])
+
+@ru
+  <p>В горах полно снега.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7789.jpg'])
+
+@ru
+  <p>По пути много тоннелей.</p>
+@endlang
+<youtube title="Matsumoto Bus in Tunnel, April 2017" v="87rCmd8QWuk"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_7793.jpg'])
+
+@ru
+  <p>И даже дамба.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7806.jpg'])
+
+@ru
+  <p>На табло слева цены за проезд от определенных участков пути. Полный путь в 91 км из Такаямы стоит 3 190 иен (около 1650 ₽ на момент поездки).</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7846.jpg'])
+
+@ru
+  <p>Разноцветные люки.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7850.jpg',
+  'IMG_7851.jpg',
+]])
+
+@ru
+  <p>Около писсуаров крючки для зонтов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7887.jpg'])
+
+@ru
+  <p>Цветы перед домом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7860.jpg'])
+
+@ru
+  <p>Потрясающий столб освещения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7861.jpg'])
+
+@ru
+  <p>Речка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7859.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7847.jpg',
+  'IMG_7848.jpg',
+  'IMG_7849.jpg',
+  'IMG_7852.jpg',
+  'IMG_7855.jpg',
+  'IMG_7885.jpg',
+]])
+
+@ru
+  <p>Дороги.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7853.jpg',
+  'IMG_7862.jpg',
+  'IMG_7866.jpg',
+]])
+
+@ru
+  <p>Лавочка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7868.jpg'])
+
+@ru
+  <p>Парк — эвакуационная точка при чрезвычайных ситуациях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7870.jpg'])
+
+@ru
+  <p>Сам парк прекрасен.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7881.jpg'])
+
+@ru
+  <p>Черный замок, красный мост, горы на фоне. Красота!</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7884.jpg'])
+
+@ru
+  <p>Здесь остановится второй вагон восьми- и десятивагонных составов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7890.jpg'])
+
+@ru
+  <p>Железнодорожная станция. На немецкий манер платформы внизу, а переход с помощью эскалатора по верху.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7894.jpg',
+  'IMG_7889.jpg',
+  'IMG_7892.jpg',
+]])
+@endsection
