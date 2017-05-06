881c4fb0c30657d416e4a2b6380d37653d5ddf49
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-06 21:01:11 +0300

    Нара

---
+++ resources/views/life/trips/nara_2017.blade.php
@@ -0,0 +1,137 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Пустой путь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4596.jpg'])
+
+@ru
+  <p>Путь с составом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4597.jpg'])
+
+@ru
+  <p>Около железнодорожной станции. Рекламы на домах, конечно, много. Но она почти не бросается в глаза, когда не можешь ее прочитать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4599.jpg'])
+
+@ru
+  <p>Автобусная остановка. </p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4600.jpg'])
+
+@ru
+  <p>Табло парковок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4601.jpg'])
+
+@ru
+  <p>Разноцветная листва.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4602.jpg'])
+
+@ru
+  <p>Осторожно, олени.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5202.jpg'])
+
+@ru
+  <p>Их тут много, и они свободно ходят по улицам. По зеленым зонтом рядом с картой продают печеньки для их кормления. Кстати, карты местности на улицах Японии встречаются регулярно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4607.jpg'])
+
+@ru
+  <p>Если бы олени могли, то смело покупали бы себе разнообразные напитки в автомате.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5415.jpg'])
+
+@ru
+  <p>Уверен, что уже натренированный глаз читателя без особых усилий найдет здесь четырехлапого товарища.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5228.jpg'])
+
+@ru
+  <p>Японским домам шарм придает своеобразная форма и отделка крыш.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5194.jpg',
+  'IMG_5195.jpg',
+]])
+
+@ru
+  <p>Прекрасный сад. Вход платный, как и почти во все остальные парки, сады и замки.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5211.jpg',
+  'IMG_5212.jpg',
+  'IMG_5213.jpg',
+  'IMG_5214.jpg',
+  'IMG_5215.jpg',
+  'IMG_5216.jpg',
+  'IMG_5218.jpg',
+  'IMG_5220.jpg',
+  'IMG_5221.jpg',
+]])
+
+@ru
+  <p>Улитка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5226.jpg'])
+
+@ru
+  <p>Первый встреченный случай парковки на земле. Безобразие.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5227.jpg'])
+
+@ru
+  <p>В Наре отличный гигантский парк с разнообразными видами и сооружениями.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5418.jpg',
+  'IMG_5420.jpg',
+  'IMG_5423.jpg',
+  'IMG_5421.jpg',
+  'IMG_5422.jpg',
+]])
+
+@ru
+  <p>Под ногами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5208.jpg'])
+
+@ru
+  <p>Камни застревают в решетке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5424.jpg'])
+
+@ru
+  <p>Атмосферная туманная картина, да еще и с зеленоватым цветом фонаря.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5427.jpg'])
+
+@ru
+  <p>У оленя есть квест для меня.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5430.jpg'])
+
+@ru
+  <p>Это улица в семь вечера.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5431.jpg'])
+
+@ru
+  <p>Японские банкоматы. Наши карты не принимают.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5432.jpg'])
+
+@ru
+  <p>Железнодорожная станция.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5434.jpg'])
+
+@ru
+  <p>Навигация и табло. Как сесть на нужный поезд? Все поезда отходят в разные минуты — уже однозначный ориентир. А также есть буквы, цвета и названия направлений движения. Запутаться очень сложно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5435.jpg'])
+@endsection
