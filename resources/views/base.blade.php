329908b21e285f986a1f2f12c9c679abe3ef9693
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-26 21:13:35 +0300

    Версии-то обновились

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -175,13 +175,13 @@
 ]); ?>
 </script>
 <script src="/assets/polyfills.js"></script>
-<script src="/assets/jquery.js"></script>
+<script src="/assets/jquery.js?3.2.1"></script>
 <script src="/assets/jquery.pjax.js?2.0.1"></script>
 <script src="/assets/jquery.scrollto.js"></script>
 <script src="/assets/autosize.js?3.0.21"></script>
 <script src="/assets/bootstrap.js"></script>
 <script src="/assets/vue.js?2.3.3"></script>
-<script src="/assets/axios.js"></script>
+<script src="/assets/axios.js?0.16.1"></script>
 <script src="/assets/fotorama-settings.js"></script>
 <script src="/assets/fotorama.js"></script>
 <script src="{{ mix('/assets/app.js') }}"></script>
