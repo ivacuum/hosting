705e697b7eb527bcc676ef52abfeb72826d868c8
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-27 12:45:25 +0300

    Калуга

---
+++ resources/views/life/trips/kaluga_2017_04.blade.php
@@ -0,0 +1,73 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Дело к дождю.</p>
+@en
+  <p>It's going to rain.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8783.jpg',
+  'IMG_8784.jpg',
+  'IMG_8785.jpg',
+  'IMG_8786.jpg',
+  'IMG_8787.jpg',
+  'IMG_8788.jpg',
+  'IMG_8789.jpg',
+]])
+
+@ru
+  <p>Дело к закату.</p>
+@en
+  <p>It's going to be a sunset.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8791.jpg',
+  'IMG_8792.jpg',
+  'IMG_8793.jpg',
+  'IMG_8794.jpg',
+  'IMG_8795.jpg',
+  'IMG_8798.jpg',
+  'IMG_8800.jpg',
+  'IMG_8803.jpg',
+]])
+
+@ru
+  <p>Дело к стройке.</p>
+@en
+  <p>It's going to be a construction.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8808.jpg',
+  'IMG_8809.jpg',
+  'IMG_8810.jpg',
+]])
+
+@ru
+  <p>Тоннель.</p>
+@en
+  <p>Tunnel.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8790.jpg'])
+
+@ru
+  <p>Женщина с ребенком ловко переходит широкую дорогу.</p>
+@en
+  <p>A woman with a child skillfully crosses a wide road.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8804.jpg'])
+
+@ru
+  <p>Тропинка вдоль забора.</p>
+@en
+  <p>Path along the fence.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8807.jpg'])
+
+@ru
+  <p>В Калуге много железнодорожных путей прямо в городе.</p>
+@en
+  <p>There are many railroads right in the city.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8811.jpg'])
+@endsection
