3c5e1f5f8e079e023d544731831c2c913353edad
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-27 14:27:01 +0300

    Пардубице

--- resources/views/life/trips/pardubice_2017.blade.php
+++ resources/views/life/trips/pardubice_2017.blade.php
@@ -2,46 +2,70 @@
 
 @section('content')
 @ru
-  <p>Удачная возможность 9 мая долететь до Чехии чартерным рейсом всего за 2 500 ₽. За эти деньги даже кормят. Полет проходил на Ту-204.</p>
-  <p>Дешевле (за 2 000 ₽) удавалось добраться до Европы только через <a class="link" href="/life/spb.2016.09">Питер</a> автобусом в <a class="link" href="/life/helsinki.2016.09">Хельсинки</a>.</p>
+  <p>Удачная возможность 9 мая долететь до Чехии чартерным рейсом всего за 2 500 ₽. За эти деньги даже кормят. Полет проходил на Ту-204. Дешевле (за 2 000 ₽) удавалось добраться до Европы только через <a class="link" href="/life/spb.2016.09">Питер</a> автобусом в <a class="link" href="/life/helsinki.2016.09">Хельсинки</a>.</p>
+ <p>Перед вылетом снова была бессонная ночь, так как нужно было подготовить транспорт на всю поездку. Еще никогда разом не брал 10 билетов на автобус. Кайф, что <a class="link" href="https://www.studentagency.eu/en/">Student Agency</a> дает один уникальный номер, по которому можно сесть во все забронированные автобусы. Нет нужды в бумажных билетах.</p>
 @endlang
+
 @ru
   <p>Аэропорт Пардубице напоминает здание детского сада.</p>
+@en
+  <p>Pardubice airport reminds the kindergarten building.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8813.jpg'])
 
 @ru
   <p>Вид подальше.</p>
+@en
+  <p>A view little bit farther.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8814.jpg'])
 
 @ru
   <p>Улица в городе.</p>
+@en
+  <p>Street in the city.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8815.jpg'])
 
 @ru
   <p>Встает вопрос как добраться до Праги из столь отдаленного (100 км) города? Автобусы 8, 23, 24 и 88 с остановки недалеко от аэропорта довезут до центра города. Оттуда с ж/д вокзала каждые 20-30 минут что-нибудь увезет в столицу. По железной дороге в среднем вдвое быстрее, нежели на автобусе.</p>
   <p><a class="link" href="https://idos.cz/">idos.cz</a> — поисковик билетов на чешский транспорт.</p>
+@en
+  <p><a class="link" href="https://idos.cz/">idos.cz</a> is a Czech public transport search engine.</p>
 @endlang
 
 @ru
   <p>Просьба ставить чемоданы между сиденьями.</p>
+@en
+  <p>The request to put suitcases between the seats.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8823.jpg'])
 
 @ru
   <p>Вот сюда.</p>
+@en
+  <p>Right here.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8824.jpg'])
 
 @ru
   <p>Так выглядят самые дешевые места.</p>
+@en
+  <p>These are the cheapest seats.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8825.jpg'])
 
 @ru
+  <p>Вид из окна во время пути.</p>
+@en
+  <p>A view during the ride.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8817.jpg'])
+
+@ru
   <p>Прибытие в Прагу на закате, который в мае только в девятом часу вечера. После <a class="link" href="/life/japan">Японии</a> настоящая сказка, что так поздно.</p>
+@en
+  <p>Arrival to Prague at sunset (around 9 PM in May). It's like miracle it is so late after <a class="link" href="/en/life/japan">Japan</a>.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_8821.jpg'])
 @endsection
