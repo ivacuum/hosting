b91f9e51513082797b7b27845a715f0be3144b45
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-29 01:07:56 +0300

    Москва

---
+++ resources/views/life/trips/msk_2017_03_26.blade.php
@@ -0,0 +1,122 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Выезд на рассвете.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3764.jpg'])
+
+@ru
+  <p>Поезд Стриж до Берлина по чумовым ценам. Уж лучше самолет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3766.jpg'])
+
+@ru
+  <p>Метро.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3767.jpg'])
+
+@ru
+  <p>Кунцево.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3768.jpg'])
+
+@ru
+  <p>Океанариум в РИО.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3778.jpg',
+  'IMG_3782.jpg',
+]])
+
+@ru
+  <p>Фасады.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3785.jpg',
+  'IMG_3787.jpg',
+  'IMG_3794.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3786.jpg',
+  'IMG_3789.jpg',
+  'IMG_3790.jpg',
+]])
+
+@ru
+  <p>Стекляшки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3788.jpg'])
+
+@ru
+  <p>Высотка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3792.jpg'])
+
+@ru
+  <p>Внезапно — митинг. Было не по себе, когда толпа во всю ширину улицы побежала мимо меня назад, а за ними омон. Относительно безопасное место оказалось между машин, откуда и сделан снимок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3793.jpg'])
+
+@ru
+  <p>Сопутствующие нынче митингу аксессуары.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3797.jpg'])
+
+@ru
+  <p>Менее распространенный аксессуар на той стороне дороги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3798.jpg'])
+
+@ru
+  <p>Откусанная навигация.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3800.jpg'])
+
+@ru
+  <p>Здания.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3796.jpg',
+  'IMG_3801.jpg',
+]])
+
+@ru
+  <p>Остановка напротив Кремля и выделенная полоса для автобуса.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3802.jpg'])
+
+@ru
+  <p>На мосту редко попадающийся знак скользкой дороги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3803.jpg'])
+
+@ru
+  <p>Москва-река.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3805.jpg'])
+
+@ru
+  <p>Цивильный велопрокат.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3806.jpg'])
+
+@ru
+  <p>Дело к вечеру. А вечером <a class="link" href="/life/oomph.2017">концерт Oomph</a>.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3807.jpg'])
+
+@ru
+  <p>Возвращение в метель. Не знаю как люди водят в таких условиях. 40 км/ч даже стремно ехать. Если машин не видно, то и вовсе непонятно по какой части дороги едешь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3811.jpg'])
+
+@ru
+  <p>После сухой и теплой Москвы дома снег.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3814.jpg'])
+@endsection
