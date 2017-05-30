de6c75c4cd8ac443f3ba69f7d2b730e0dbed2394
Sergey Pankov <vacuum@ivacuum.ru>
2017-05-30 22:12:29 +0300

    Осака

---
+++ resources/views/life/trips/osaka_2017.blade.php
@@ -0,0 +1,217 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Вагоны каждого типа шинкансена отличаются. Теперь достался попроще.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4500.jpg'])
+
+@ru
+  <p>Табло просит быть осторожнее с играми на телефоне на ходу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4504.jpg'])
+
+@ru
+  <p>Разметка как формировать очередь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4505.jpg'])
+
+@ru
+  <p>Телефоны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4506.jpg'])
+
+@ru
+  <p>Вечерние улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4507.jpg',
+  'IMG_4512.jpg',
+  'IMG_4537.jpg',
+  'IMG_4539.jpg',
+]])
+
+@ru
+  <p>Макет высотного здания.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4515.jpg'])
+
+@ru
+  <p>Панорамы с этого здания.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4516.jpg',
+  'IMG_4519.jpg',
+  'IMG_4527.jpg',
+]])
+
+@ru
+  <p>Если вдруг умеете ходить по стенам, то сможете присесть в красный автомобиль.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4534.jpg'])
+
+@ru
+  <p>Напольная информация. Слева выходят, справа входим в две линии.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4536.jpg'])
+
+@ru
+  <p>Вид из обеденной зоны капсульного отеля. В таких отелях почти всегда есть разделение по полу — отдельный этаж для женщин и отдельный для мужчин. Стоит быть готовым, что часов с семи утра будильники будут включаться у людей каждые пять минут, так что спокойно поспать до обеда в капсуле не вариант.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4544.jpg'])
+
+@ru
+  <p>Дневные улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4545.jpg',
+  'IMG_4546.jpg',
+]])
+
+@ru
+  <p>Стройные очереди как часть культуры.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4547.jpg'])
+
+@ru
+  <p>&lt;3</p>
+@en
+  <p>&lt;3</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4548.jpg'])
+
+@ru
+  <p>Растения на продажу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4549.jpg'])
+
+@ru
+  <p>Причесанные деревья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4550.jpg'])
+
+@ru
+  <p>Оголенное дерево.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4555.jpg'])
+
+@ru
+  <p>Выбор напитков и мороженого. Есть даже горячие напитки. Иногда попадаются автоматы с алкоголем, который в Японии продается круглосуточно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4565.jpg'])
+
+@ru
+  <p>Навигация.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4569.jpg'])
+
+@ru
+  <p>Территория около замка.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4551.jpg',
+  'IMG_4553.jpg',
+  'IMG_4554.jpg',
+  'IMG_4556.jpg',
+  'IMG_4557.jpg',
+  'IMG_4558.jpg',
+  'IMG_4559.jpg',
+  'IMG_4563.jpg',
+  'IMG_4566.jpg',
+  'IMG_4567.jpg',
+  'IMG_4568.jpg',
+  'IMG_4570.jpg',
+]])
+
+@ru
+  <p>Зарастающий пешеходный мост.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4571.jpg'])
+
+@ru
+  <p>Автомобильный мост.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4572.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4573.jpg',
+  'IMG_4574.jpg',
+]])
+
+@ru
+  <p>Дядя в белом направляет людей непонятным образом. Японский знак стоп треугольной формы с местной надписью. Указатель к ж/д станции компании JR. Сакура. Прекрасное и полное деталей место.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4575.jpg'])
+
+@ru
+  <p>А сзади небольшой храм.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4576.jpg'])
+
+@ru
+  <p>Пешеходный мост с чайками.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4577.jpg',
+  'IMG_4580.jpg',
+]])
+
+@ru
+  <p>Проход к ярмарке, а затем и сквозь нее.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4578.jpg',
+  'IMG_4579.jpg',
+  'IMG_4581.jpg',
+  'IMG_4582.jpg',
+  'IMG_4588.jpg',
+]])
+
+@ru
+  <p>Рыбки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4583.jpg'])
+
+@ru
+  <p>Бананы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4584.jpg',
+  'IMG_4585.jpg',
+]])
+
+@ru
+  <p>Над головой летают самолеты.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4586.jpg'])
+
+@ru
+  <p>А внизу вкусняшки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4587.jpg'])
+
+@ru
+  <p>Подъем наверх.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4589.jpg'])
+
+@ru
+  <p>В магазинах разметка начала очереди в кассу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4591.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4592.jpg'])
+
+@ru
+  <p>Внезапно — пути поперек станции. На такой важно в телефоне не залипнуть во время ходьбы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_4593.jpg'])
+@endsection
