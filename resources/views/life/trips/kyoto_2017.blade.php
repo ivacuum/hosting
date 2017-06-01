a3291a9958bfb8c9131cf291c1105614adba08e9
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-01 21:53:36 +0300

    Киото

---
+++ resources/views/life/trips/kyoto_2017.blade.php
@@ -0,0 +1,635 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Улицы Киото.</p>
+@en
+  <p>Kyoto streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5446.jpg',
+  'IMG_5447.jpg',
+  'IMG_5448.jpg',
+  'IMG_5451.jpg',
+  'IMG_5456.jpg',
+  'IMG_5490.jpg',
+  'IMG_5493.jpg',
+]])
+
+@ru
+  <p>Поезда.</p>
+@en
+  <p>Trains.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5436.jpg',
+  'IMG_5437.jpg',
+  'IMG_5438.jpg',
+]])
+
+@ru
+  <p>Железная дорога.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5450.jpg'])
+
+@ru
+  <p>Автобусная остановка. Под маршрутами кругляши под иконку. Чем иконка левее, тем ближе автобус. 207-й еще далеко, 202-й уже на подходе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5449.jpg'])
+
+@ru
+  <p>Подробное расписание каждого маршрута по дням недели, часам и минутам.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5452.jpg'])
+
+@ru
+  <p>Внутри автобуса.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5453.jpg'])
+
+@ru
+  <p>Низкопольная только передняя половина — в хвосте ступеньки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5607.jpg'])
+
+@ru
+  <p>Рядом с каждым сиденьем имеется кнопка стоп. 25 сидений? Будет 25 кнопок. Если ее не нажать, то водитель может пропустить остановку. Вставать и выходить просят только после полной остановки транспорта и открытия дверей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5774.jpg'])
+
+@ru
+  <p>Нетипичные балконы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5455.jpg'])
+
+@ru
+  <p>Желтая машина спряталась во дворе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5457.jpg'])
+
+@ru
+  <p>Панорама.</p>
+@en
+  <p>Panorama.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5458.jpg'])
+
+@ru
+  <p>Более высокая панорама.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6661.jpg'])
+
+@ru
+  <p>Велопарковка перед входом в здание. Разметка для слепых — ее вообще немыслимое количество.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5488.jpg'])
+
+@ru
+  <p>Открытый переулок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5489.jpg'])
+
+@ru
+  <p>Временно закрытый переулок. Товарищи с флажками все время присутствуют при каких-либо транспортных, ремонтных или строительных работах. Регулируют движение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5491.jpg'])
+
+@ru
+  <p>Вход в автобус через среднюю дверь, выход через переднюю. Если есть транспортная карта, то достаточно приложить ее на входе и выходе — сумма рассчитается и спишется автоматически. При отсутствии карты надо взять на входе билет, а на выходе билет вместе с деньгами за проезд кинуть в автомат около водителя (потому и выход через переднюю дверь). Сдачу автомат не дает, поэтому рядом же можно разменять деньги на мелочь. Как узнать стоимость проезда? Либо в приложении, которое построит вам маршрут, либо смотреть на табло стоимость напротив места посадки со входного билета. Еще можно купить безлимитный дневной проездной у водителя, который затем достаточно просто показывать другим водителям при выходе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5492.jpg'])
+
+@ru
+  <p>Цветы перед домом. Счетчики на улице.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5494.jpg'])
+
+@ru
+  <p>Ворота под названием тории.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5495.jpg',
+  'IMG_6258.jpg',
+  'IMG_6655.jpg',
+]])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5496.jpg',
+  'IMG_5497.jpg',
+]])
+
+@ru
+  <p>Парковка, терминал оплаты и стоимость.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5498.jpg'])
+
+@ru
+  <p>Еще парковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5508.jpg'])
+
+@ru
+  <p>И еще парковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5570.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@en
+  <p>Pedestrian cross.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5502.jpg'])
+
+@ru
+  <p>Набережные.</p>
+@en
+  <p>Embankments.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5504.jpg',
+  'IMG_5529.jpg',
+  'IMG_5534.jpg',
+  'IMG_5539.jpg',
+  'IMG_5540.jpg',
+  'IMG_5542.jpg',
+  'IMG_5545.jpg',
+  'IMG_5547.jpg',
+  'IMG_5548.jpg',
+  'IMG_5551.jpg',
+]])
+
+@ru
+  <p>Река Камо.</p>
+@en
+  <p>Kamo river.</p>
+@endlang
+<youtube title="Kyoto Bridge, April 2017" v="Bar7HkxaYuc"></youtube>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5505.jpg',
+  'IMG_5528.jpg',
+  'IMG_5538.jpg',
+  'IMG_5552.jpg',
+]])
+
+@ru
+  <p>Мост через нее.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5537.jpg'])
+
+@ru
+  <p>Слева пешеходы, справа велосипеды. Немного далее слева заправка и стоимость бензина.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5506.jpg'])
+
+@ru
+  <p>Приятная улица.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5507.jpg',
+  'IMG_5509.jpg',
+  'IMG_5511.jpg',
+  'IMG_5516.jpg',
+]])
+
+@ru
+  <p>Сакура опадает.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5514.jpg'])
+
+@ru
+  <p>Заведенная машина с множеством громкоговорителей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5517.jpg'])
+
+@ru
+  <p>Еще улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5519.jpg',
+  'IMG_5522.jpg',
+  'IMG_5525.jpg',
+  'IMG_5526.jpg',
+]])
+
+@ru
+  <p>Не только счетчики, но и колонка на улице.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5520.jpg'])
+
+@ru
+  <p>Сушка обуви на улице.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5524.jpg'])
+
+@ru
+  <p>Велопарковка и велопрокат на набережной.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5530.jpg',
+  'IMG_5531.jpg',
+  'IMG_5532.jpg',
+]])
+
+@ru
+  <p>Парковка для скутеров.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5533.jpg'])
+
+@ru
+  <p>Небольшие домики.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5536.jpg'])
+
+@ru
+  <p>Домики в зелени.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6668.jpg'])
+
+@ru
+  <p>Строящийся мост.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5554.jpg'])
+
+@ru
+  <p>На функционирующем мосту.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5556.jpg'])
+
+@ru
+  <p>Трубы-трубы-трубы. Много труб.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5557.jpg',
+  'IMG_5558.jpg',
+  'IMG_5559.jpg',
+]])
+
+@ru
+  <p>На переезде чаще можно встретить людей перед шлагбаумами, нежели автомобили.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5560.jpg'])
+
+@ru
+  <p>Железнодорожная станция.</p>
+@en
+  <p>Railway station.</p>
+@endlang
+<youtube title="Kyoto Bridge, Railway Station, April 2017" v="_8gvomz8uxY"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_5561.jpg'])
+
+@ru
+  <p>Вид снизу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6649.jpg'])
+
+@ru
+  <p>Улочка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5563.jpg'])
+
+@ru
+  <p>Крыша дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5564.jpg'])
+
+@ru
+  <p>Зелень за заборами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5565.jpg'])
+
+@ru
+  <p>Сиденья на остановке спиной к дороге.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5567.jpg'])
+
+@ru
+  <p>Большинство дверей в Японии либо автоматические, либо полуавтоматически — открываются по нажатию кнопки, как эти. К хорошему быстро привыкаешь. По возвращении негодуешь, что надо тягать двери.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5568.jpg'])
+
+@ru
+  <p>И еще улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5566.jpg',
+  'IMG_5569.jpg',
+  'IMG_5571.jpg',
+  'IMG_5576.jpg',
+  'IMG_6030.jpg',
+  'IMG_6031.jpg',
+  'IMG_6396.jpg',
+  'IMG_6397.jpg',
+  'IMG_6399.jpg',
+  'IMG_6400.jpg',
+  'IMG_6402.jpg',
+  'IMG_6407.jpg',
+  'IMG_6408.jpg',
+  'IMG_6409.jpg',
+  'IMG_6410.jpg',
+  'IMG_6411.jpg',
+  'IMG_6412.jpg',
+  'IMG_6413.jpg',
+]])
+
+@ru
+  <p>Школьный двор.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5574.jpg',
+  'IMG_5603.jpg',
+]])
+
+@ru
+  <p>Улица около школы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5575.jpg'])
+
+@ru
+  <p>Лифт.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5577.jpg'])
+
+@ru
+  <p>Кодовая панель перед номером в отеле. Отсчет каждый раз начинается с новой цифры (сейчас с 0), чтобы нельзя было жать те же самые кнопки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5580.jpg'])
+
+@ru
+  <p>Стиральная машина.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5582.jpg'])
+
+@ru
+  <p>Автобус для детей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5612.jpg'])
+
+@ru
+  <p>Внезапно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5613.jpg'])
+
+@ru
+  <p>Щит о ремонтных работах на той стороне дороги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5614.jpg'])
+
+@ru
+  <p>Красивое ограждение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5617.jpg'])
+
+@ru
+  <p>Ограждение попроще.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5616.jpg'])
+
+@ru
+  <p>Парковка для зонтов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5618.jpg'])
+
+@ru
+  <p>Зонт чаще всего оставляют на улице. В крайнем случае надевают на него <a class="link" href="/life/tokyo.2017.04.08-10#umbrella-cover">чехол</a>, если надо зайти в здание.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6406.jpg'])
+
+@ru
+  <p>Водосток интересной формы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5619.jpg'])
+
+@ru
+  <p>Школьники в форме изучают красоты.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_5915.jpg'])
+
+@ru
+  <p>Смайлик на крышке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6023.jpg'])
+
+@ru
+  <p>Телефон с монетоприемником.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6032.jpg'])
+
+@ru
+  <p>Пятиуровневая пагода.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6046.jpg',
+  'IMG_6254.jpg',
+  'IMG_6259.jpg',
+]])
+
+@ru
+  <p>Переезд. Поезда ходят очень близко к домам.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6403.jpg',
+  'IMG_6404.jpg',
+]])
+
+@ru
+  <p>Железная дорога.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6405.jpg'])
+
+@ru
+  <p>Бамбуковая роща.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6415.jpg',
+  'IMG_6420.jpg',
+  'IMG_6422.jpg',
+]])
+
+@ru
+  <p>Рядом кладбище.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6417.jpg'])
+
+@ru
+  <p>Все японские монеты.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6558.jpg',
+  'IMG_6556.jpg',
+]])
+
+@ru
+  <p>Под лавкой тарелки для окурков.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6560.jpg'])
+
+@ru
+  <p>Иногда для детей иероглифы расшифровывают символами азбуки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6566.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5621.jpg',
+  'IMG_5743.jpg',
+  'IMG_5744.jpg',
+  'IMG_5772.jpg',
+  'IMG_5773.jpg',
+  'IMG_6020.jpg',
+  'IMG_6543.jpg',
+  'IMG_6544.jpg',
+  'IMG_6546.jpg',
+  'IMG_6549.jpg',
+  'IMG_6550.jpg',
+  'IMG_6564.jpg',
+  'IMG_6567.jpg',
+  'IMG_6570.jpg',
+  'IMG_6572.jpg',
+  'IMG_6586.jpg',
+  'IMG_6670.jpg',
+]])
+
+@ru
+  <p>Места не для прогулок. Любоваться можно только на расстоянии.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6034.jpg',
+  'IMG_6037.jpg',
+  'IMG_6039.jpg',
+  'IMG_6042.jpg',
+]])
+
+@ru
+  <p>Лавочка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6575.jpg'])
+
+@ru
+  <p>Еще лавочка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6660.jpg'])
+
+@ru
+  <p>Растут апельсины.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6589.jpg'])
+
+@ru
+  <p>Добро пожаловать к столу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6595.jpg'])
+
+@ru
+  <p>Кладбища очень компактные. В среднем по площади как один российский многоквартирный дом в регионе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6590.jpg'])
+
+@ru
+  <p>Домик рядом с кладбищем.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6593.jpg'])
+
+@ru
+  <p>Растения растут в подушках.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6601.jpg'])
+
+@ru
+  <p>Приготовьтесь по пять раз в день разуваться и обуваться. Лучше выбрать для этого подходящую обувь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6606.jpg'])
+
+@ru
+  <p>В супермаркете. Немного видно интерфейс кассира. Ручной сканер они прикладывают к товару вплотную, в отличие от нашего способа поднесения товара к статичному сканеру. При оплате наличными касса сама выдает кассиру сдачу, ему остается только передать ее покупателю. Двумя руками и с небольшим поклоном. При покупке алкоголя надо подтвердить касанием экрана, что тебе исполнилось 20 лет.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6610.jpg',
+  'IMG_6611.jpg',
+]])
+
+@ru
+  <p>Немного продаваемых товаров. Фруктов мало, и они очень дорогие: один банан около 90 ₽, одно яблоко 150 ₽. Можно смело брать йогурты, желе, суши, какой-нибудь рис с рыбой и тому подобное, так как на кассе выдадут бесплатные приборы согласно вашим покупкам. Почему у нас такого нет? Сколько раз отсутствие приборов останавливало от покупки йогурта! Еще горячие и холодные покупки разложат по разным пакетам. Горячие? Да, помимо изначально теплых товаров еду можно попросить разогреть на кассе, либо сделать это самому — в зале бывают микроволновки. Можно даже автомат с кипятком найти для лапши.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6612.jpg',
+  'IMG_6613.jpg',
+  'IMG_6614.jpg',
+  'IMG_6615.jpg',
+  'IMG_6616.jpg',
+  'IMG_6617.jpg',
+  'IMG_6618.jpg',
+  'IMG_6619.jpg',
+  'IMG_6622.jpg',
+  'IMG_6624.jpg',
+  'IMG_6625.jpg',
+  'IMG_6626.jpg',
+  'IMG_6627.jpg',
+  'IMG_6628.jpg',
+  'IMG_6629.jpg',
+  'IMG_6630.jpg',
+  'IMG_6631.jpg',
+  'IMG_6632.jpg',
+]])
+
+@ru
+  <p>Ночная башня.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6637.jpg'])
+
+@ru
+  <p>В этот поезд двери открывать и закрывать надо самому.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6641.jpg'])
+
+@ru
+  <p>Традиционные японские крыши.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6646.jpg'])
+
+@ru
+  <p>Комната для курения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6647.jpg'])
+
+@ru
+  <p>Турникеты на станции. Обладателям проездного JR Pass проходить вдоль окошка с человеком.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6648.jpg'])
+
+@ru
+  <p>Многие станции не прямые.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6650.jpg'])
+
+@ru
+  <p>К повороту налево готов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6651.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6652.jpg'])
+
+@ru
+  <p>Много где можно найти дефибрилляторы. На станции.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6674.jpg'])
+
+@ru
+  <p>Или даже перед домом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6669.jpg'])
+
+@ru
+  <p>Навигация на станции. Поначалу казалось, что путь 0 в описании маршрута — это ошибка, и придется искать информацию на табло. Но нет, нулевой путь действительно есть прямо по курсу. Поезд оттуда доставит в Канадзаву.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6673.jpg'])
+@endsection
