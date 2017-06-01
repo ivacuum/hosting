3e86b436da382540340f348cafd0fc80a28a5fdc
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-01 23:01:31 +0300

    Канадзава

---
+++ resources/views/life/trips/kanazawa_2017.blade.php
@@ -0,0 +1,203 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Вид из окна поезда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6687.jpg'])
+
+@ru
+  <p>Люди сушат вещи на крыше дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6680.jpg'])
+
+@ru
+  <p>Короткая остановка. Иероглифы пишут как горизонтально, так и вертикально.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6689.jpg'])
+
+@ru
+  <p>Похоже на истребитель в воздухе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6701.jpg'])
+
+@ru
+  <p>Главный вокзал Канадзавы.</p>
+@en
+  <p>Kanazawa central station.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6745.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6747.jpg',
+  'IMG_6748.jpg',
+  'IMG_6753.jpg',
+  'IMG_6761.jpg',
+  'IMG_6762.jpg',
+]])
+
+@ru
+  <p>Выделяется красный знак пожарного гидранта.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6756.jpg'])
+
+@ru
+  <p>Переход во все стороны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6757.jpg'])
+
+@ru
+  <p>Предлагается идти по дороге.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6758.jpg'])
+
+@ru
+  <p>Разметка подтверждает. Место есть для всех.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6759.jpg'])
+
+@ru
+  <p>Свой стиль знака пешеходного перехода.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6760.jpg'])
+
+@ru
+  <p>Просторы и замки в первом парке напоминают пейзажи из игры Варкрафт 2.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6763.jpg',
+  'IMG_6766.jpg',
+  'IMG_6767.jpg',
+  'IMG_6768.jpg',
+  'IMG_6769.jpg',
+  'IMG_6770.jpg',
+  'IMG_6771.jpg',
+  'IMG_6773.jpg',
+  'IMG_6778.jpg',
+  'IMG_6779.jpg',
+  'IMG_6783.jpg',
+]])
+
+@ru
+  <p>Можно ходить и собирать заставки на рабочий стол.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_6764.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6784.jpg',
+  'IMG_6785.jpg',
+]])
+
+@ru
+  <p>Чудесной красоты следующий парк. Преимущественно за ним и едут в Канадзаву. В Японии вход почти во все стоящие храмы и парки платный. Иногда попадаются дни, когда вход свободный. В среднем стоимость от ¥400 до ¥1000. На данный момент в рубли цены переводятся делением пополам.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6787.jpg',
+  'IMG_6789.jpg',
+  'IMG_6791.jpg',
+  'IMG_6792.jpg',
+  'IMG_6795.jpg',
+  'IMG_6796.jpg',
+  'IMG_6798.jpg',
+  'IMG_6799.jpg',
+  'IMG_6800.jpg',
+  'IMG_6801.jpg',
+  'IMG_6802.jpg',
+  'IMG_6803.jpg',
+  'IMG_6804.jpg',
+  'IMG_6805.jpg',
+  'IMG_6806.jpg',
+  'IMG_6808.jpg',
+  'IMG_6809.jpg',
+  'IMG_6810.jpg',
+  'IMG_6812.jpg',
+  'IMG_6814.jpg',
+  'IMG_6816.jpg',
+  'IMG_6817.jpg',
+  'IMG_6818.jpg',
+  'IMG_6819.jpg',
+  'IMG_6820.jpg',
+  'IMG_6823.jpg',
+  'IMG_6825.jpg',
+  'IMG_6867.jpg',
+  'IMG_6871.jpg',
+  'IMG_6875.jpg',
+  'IMG_6886.jpg',
+  'IMG_6892.jpg',
+  'IMG_7238.jpg',
+  'IMG_7240.jpg',
+  'IMG_7243.jpg',
+  'IMG_7245.jpg',
+  'IMG_7255.jpg',
+  'IMG_7257.jpg',
+  'IMG_7259.jpg',
+]])
+
+@ru
+  <p>Камень говорит, что проход закрыт.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6878.jpg',
+  'IMG_6877.jpg',
+]])
+
+@ru
+  <p>Аллея фонарей.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7264.jpg',
+  'IMG_7265.jpg',
+  'IMG_7266.jpg',
+  'IMG_7268.jpg',
+]])
+
+@ru
+  <p>Широченная зебра. Даже автобус боком перейдет по такой дорогу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7270.jpg'])
+
+@ru
+  <p>Частые карты местности никогда не дадут потеряться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7275.jpg'])
+
+@ru
+  <p>Куда идет автобус? При построении маршрутов по регионам Японии названия остановок приложение показывало иероглифами. Проще было по местоположению на карте посмотреть доехал или нет, чем сопоставлять символы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7277.jpg'])
+
+@ru
+  <p>Вечерние улицы.</p>
+@en
+  <p>Street in the evening.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7279.jpg',
+  'IMG_7283.jpg',
+  'IMG_7285.jpg',
+  'IMG_7287.jpg',
+  'IMG_7289.jpg',
+  'IMG_7291.jpg',
+  'IMG_7295.jpg',
+]])
+
+@ru
+  <p>У входа в магазины неизменно продаются журналы и книги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7293.jpg'])
+
+@ru
+  <p>Тьма способов оплаты, многие из них бесконтактные.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7308.jpg'])
+@endsection
