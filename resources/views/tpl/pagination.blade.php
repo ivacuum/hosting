5a3340351c6aa865127d5a2f70067d3963281221
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-01 08:05:39 +0300

    Правки в постраничной навигации

--- resources/views/tpl/pagination.blade.php
+++ resources/views/tpl/pagination.blade.php
@@ -2,11 +2,11 @@
   @if ($paginator->onFirstPage())
     <li class="disabled"><span>@svg (chevron-left)</span></li>
   @else
-    <li><a class="js-pjax tooltipped tooltipped-n" href="{{ $paginator->previousPageUrl() }}" id="previous_page" rel="prev" aria-label="{{ trans('pagination.previous') }}">@svg (chevron-left)</a></li>
+    <li><a class="js-pjax" href="{{ $paginator->previousPageUrl() }}" rel="prev">@svg (chevron-left)</a></li>
   @endif
 
   @if ($paginator->hasMorePages())
-    <li><a class="js-pjax tooltipped tooltipped-n" href="{{ $paginator->nextPageUrl() }}" id="next_page" rel="next" aria-label="{{ trans('pagination.next') }}">@svg (chevron-right)</a></li>
+    <li><a class="js-pjax" href="{{ $paginator->nextPageUrl() }}" rel="next">@svg (chevron-right)</a></li>
   @else
     <li class="disabled"><span>@svg (chevron-right)</span></li>
   @endif
