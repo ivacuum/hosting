1187f9f88a1003c4572fd41b0995a143189caabe
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-02 15:44:44 +0300

    Такаяма

---
+++ resources/views/life/trips/takayama_2017.blade.php
@@ -0,0 +1,419 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Воспользуемся обнаруженной машиной времени и переместимся на несколько десятилетий назад.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7533.jpg'])
+
+@ru
+  <p>Маленькие телевизоры на любой вкус. Самое то в гараж поставить.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7530.jpg'])
+
+@ru
+  <p>Уголок фотолюбителей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7534.jpg'])
+
+@ru
+  <p>Неделя начинается с воскресенья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7535.jpg'])
+
+@ru
+  <p>Ребята играют в настолку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7539.jpg'])
+
+@ru
+  <p>Заглянем в кабинет доктора, пока его нет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7540.jpg'])
+
+@ru
+  <p>Закажем песню, что не было так беспокойно его ждать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7541.jpg'])
+
+@ru
+  <p>Если вы вдруг не разглядели названия. Тут все куда ближе и понятнее.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7556.jpg'])
+
+@ru
+  <p>Игрушки детства.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7542.jpg',
+  'IMG_7543.jpg',
+  'IMG_7545.jpg',
+  'IMG_7547.jpg',
+  'IMG_7550.jpg',
+]])
+
+@ru
+  <p>Куда же без Годзиллы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7546.jpg'])
+
+@ru
+  <p>Маски.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7548.jpg'])
+
+@ru
+  <p>Давайте все пересчитаем.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7551.jpg'])
+
+@ru
+  <p>В кабинете парикмахера. Ловкая попытка не отразиться в зеркалах.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7552.jpg'])
+
+@ru
+  <p>Вид сбоку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7554.jpg'])
+
+@ru
+  <p>Вид сверху.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7562.jpg'])
+
+@ru
+  <p>Колонка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7555.jpg'])
+
+@ru
+  <p>Что тут можно добавить? Только ААА в конце.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7557.jpg'])
+
+@ru
+  <p>Сигареты.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7559.jpg'])
+
+@ru
+  <p>Свежие журналы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7563.jpg'])
+
+@ru
+  <p>Игровые новинки.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7565.jpg',
+  'IMG_7568.jpg',
+]])
+
+@ru
+  <p>Можно сразу опробовать с друзьями.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7567.jpg',
+  'IMG_7569.jpg',
+  'IMG_7597.jpg',
+  'IMG_7598.jpg',
+  'IMG_7599.jpg',
+]])
+
+@ru
+  <p>Пеналы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7571.jpg'])
+
+@ru
+  <p>Калькуляторы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7572.jpg'])
+
+@ru
+  <p>В классе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7573.jpg'])
+
+@ru
+  <p>Строение приборов, в том числе утюга.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7574.jpg'])
+
+@ru
+  <p>Печатная машинка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7575.jpg'])
+
+@ru
+  <p>Таблица умножения. Тут без сюрпризов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7590.jpg'])
+
+@ru
+  <p>Термометр в Цельсиях и Фаренгейтах.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7591.jpg'])
+
+@ru
+  <p>Все готово к игре.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7592.jpg'])
+
+@ru
+  <p>Нас уже ждут в гости.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7596.jpg'])
+
+@ru
+  <p>Массажное кресло.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7601.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7602.jpg'])
+
+@ru
+  <p>Время забора корреспонденции.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7603.jpg'])
+
+@ru
+  <p>Все средства передвижения на ходу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7604.jpg'])
+
+@ru
+  <p>Еще всякая всячина.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7536.jpg',
+  'IMG_7537.jpg',
+  'IMG_7538.jpg',
+  'IMG_7553.jpg',
+  'IMG_7605.jpg',
+  'IMG_7606.jpg',
+  'IMG_7607.jpg',
+  'IMG_7609.jpg',
+  'IMG_7610.jpg',
+  'IMG_7611.jpg',
+]])
+
+@ru
+  <p>Кинотеатр. Тише, идет сеанс.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7613.jpg'])
+
+@ru
+  <p>Новинки проката.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7615.jpg'])
+
+@ru
+  <p>Туалет в японском стиле.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7617.jpg'])
+
+@ru
+  <p>Телефонная будка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7620.jpg'])
+
+@ru
+  <p>Пинбол.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7622.jpg',
+  'IMG_7623.jpg',
+  'IMG_7624.jpg',
+]])
+
+<p style="height: 15em;"></p>
+<p class="text-center">* * * * *</p>
+<p style="height: 15em;"></p>
+
+@ru
+  <p>Вернемся в наше время. Новый день — новые поезда. У этого широкие панорамные окна.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7321.jpg'])
+
+@ru
+  <p>Есть смысл обращать внимание на информацию на спинке кресла. Здесь информация, что можно регулировать высоту подголовника.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7318.jpg'])
+
+@ru
+  <p>Виды из окна.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7334.jpg',
+  'IMG_7338.jpg',
+  'IMG_7351.jpg',
+  'IMG_7361.jpg',
+]])
+
+@ru
+  <p>Пешеходный светофор. Нет, он не показывает 11. Деления исчезают сверху вниз. Когда все погаснут, то загорится зеленый.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7362.jpg'])
+
+@ru
+  <p>Кегля.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7365.jpg'])
+
+@ru
+  <p>Почтовый ящик, велосипедист, гидрант, уже знакомый светофор, дорожные маршруты на столбе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7368.jpg'])
+
+@ru
+  <p>Во дворе храм.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7366.jpg'])
+
+@ru
+  <p>Лавочка у дороги. Снова спиной, как и остановки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7369.jpg'])
+
+@ru
+  <p>Велосипеды.</p>
+@en
+  <p>Bicycles.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7370.jpg'])
+
+@ru
+  <p>Переулок закрыли для проезда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7371.jpg'])
+
+@ru
+  <p>Река Миягава.</p>
+@en
+  <p>Miyagawa river.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7372.jpg',
+  'IMG_7375.jpg',
+  'IMG_7666.jpg',
+  'IMG_7667.jpg',
+]])
+
+@ru
+  <p>Окошко на входе в один из музеев.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7642.jpg'])
+
+@ru
+  <p>Солнечный вид на улицу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7645.jpg'])
+
+@ru
+  <p>Во время ежегодного фестиваля такие штуки катают по городу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7653.jpg'])
+
+@ru
+  <p>Возможно, этот щит подсказывает, что тут проходит ее маршрут.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7689.jpg'])
+
+@ru
+  <p>Одинаковые машины.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7680.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7683.jpg'])
+
+@ru
+  <p>Цветы перед домом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7685.jpg'])
+
+@ru
+  <p>Разменник денег.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7693.jpg'])
+
+@ru
+  <p>Какой можно ожидать унитаз в туалете с «советским» интерьером?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7694.jpg'])
+
+@ru
+  <p>Конечно, самый современный! Воткнутый в розетку и с кучей кнопок. Некоторые люди сравнивают его панель управления с таковой у космического корабля. На самом деле полезно изучить его возможности и потом ими пользоваться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7695.jpg'])
+
+@ru
+  <p>Снаружи узнается традиционное японское жилье.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7364.jpg'])
+
+@ru
+  <p>Внутри жилья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7696.jpg'])
+
+@ru
+  <p>Длинный умывальник на всех.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7697.jpg'])
+
+@ru
+  <p>Отдельно краны с кипятком и ледяной водой. Без тазика для смешивания не обойтись.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7734.jpg'])
+
+@ru
+  <p>По телевизору боевик «<a class="link" href="https://www.kinopoisk.ru/film/4053/">В осаде</a>» на английском с субтитрами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7698.jpg'])
+
+@ru
+  <p>Ограждения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7735.jpg'])
+
+@ru
+  <p>Улицы Такаямы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7626.jpg',
+  'IMG_7628.jpg',
+  'IMG_7646.jpg',
+  'IMG_7649.jpg',
+  'IMG_7656.jpg',
+  'IMG_7659.jpg',
+  'IMG_7669.jpg',
+  'IMG_7671.jpg',
+  'IMG_7672.jpg',
+  'IMG_7673.jpg',
+  'IMG_7675.jpg',
+  'IMG_7691.jpg',
+  'IMG_7741.jpg',
+]])
+
+@ru
+  <p>Поезда.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7737.jpg',
+  'IMG_7738.jpg',
+  'IMG_7739.jpg',
+]])
+
+@ru
+  <p>Парковка для конусов, парковка для зонтов, вход в дом, парковка для велосипедов. Надпись AED подсказывает, что внутри есть дефибриллятор.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7740.jpg'])
+@endsection
