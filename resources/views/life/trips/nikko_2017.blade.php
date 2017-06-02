f4ae72f84b13786124f342b0924f448bab5fae0e
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-03 02:41:13 +0300

    Никко

---
+++ resources/views/life/trips/nikko_2017.blade.php
@@ -0,0 +1,319 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Из горной <a class="link" href="/life/takayama.2017">Такаямы</a> оказалось непросто добраться до Никко. Одними поездами выходило около шести часов с несколькими пересадками, причем сначала предстояло ехать назад по уже увиденным местам. Если же сначала доехать на автобусе до <a class="link" href="/life/matsumoto.2017">Мацумото</a>, а оттуда поездами, то в итоге те же шесть часов, но без повторений пути и плюс один город на осмотр.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7907.jpg'])
+
+@ru
+  <p>Сначала на экспрессе до Нагано.</p>
+@endlang
+<youtube title="Nagano Train, April 2017" v="sp1NyMHw39Y"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_7934.jpg'])
+
+@ru
+  <p>Поворот налево. Всегда такие снимки сперва хочется выровнять, но потом мозг парирует, что не настолько же кривые руки, чтобы так изначально снимать. Действительно поворот налево.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7935.jpg'])
+
+@ru
+  <p>Крохотное кладбище по пути.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7983.jpg'])
+
+@ru
+  <p>Затем пересадка на шинкансен. Зеленый указатель хорошо выделяется.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7992.jpg'])
+
+@ru
+  <p>Какие поезда только не встретишь. Нужно только научиться снимать объекты в движении.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7993.jpg',
+  'IMG_8020.jpg',
+  'IMG_8039.jpg',
+]])
+
+@ru
+  <p>Платформы.</p>
+@en
+  <p>Platforms.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7994.jpg'])
+
+@ru
+  <p>Классный электрический кабель между вагонами. Шестой вагон только по предварительному бронированию мест. Остановка четко напротив стрелочки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_7995.jpg'])
+
+@ru
+  <p>Очередь на посадку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8038.jpg'])
+
+@ru
+  <p>Затем еще одна пересадка на другой шинкансен на станции Омия.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8052.jpg'])
+
+@ru
+  <p>Интерьер вагона.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8054.jpg'])
+
+@ru
+  <p>Присутствует туалет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8055.jpg'])
+
+@ru
+  <p>Следующая пересадка на станции Утсономия. Уже видно, что отсюда, наконец-то, можно добраться до Никко.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8057.jpg'])
+
+@ru
+  <p>Платформа. Поезда по-прежнему проще всего опознавать по времени отправления.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8058.jpg'])
+
+@ru
+  <p>Хитрая кодовая панель на двери.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8061.jpg'])
+
+@ru
+  <p>В электричке. На местном языке Японию называют либо Ниппон, как на плакате, либо Нихон. Стоит отметить как мало места отведено машинисту.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8066.jpg'])
+
+@ru
+  <p>Можно подойти и посмотреть за движением.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8071.jpg'])
+
+@ru
+  <p>Прибыли.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8079.jpg'])
+
+@ru
+  <p>Подтверждение.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8080.jpg'])
+
+@ru
+  <p>Если необходимо пополнить транспортную карту, то можно сразу вставлять ее в терминал. Помимо пополнения, можно распечатать историю трат по карте. Слева от экрана классные кнопки-заготовки для выбора количества пассажиров.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8081.jpg'])
+
+@ru
+  <p>Пора уже выйти в город. Некоторые автобусы развозят прямо по отелям — так расставлены остановки на их маршрутах.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8083.jpg'])
+
+@ru
+  <p>Телефонная будка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8086.jpg'])
+
+@ru
+  <p>По случаю проведения фестиваля на улице регулировщики движения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8087.jpg'])
+
+@ru
+  <p>Ужин. Суп и рис остались в стороне.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8094.jpg'])
+
+@ru
+  <p>Завтрак. Рис снова остался в стороне.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8097.jpg'])
+
+@ru
+  <p>Пополнение коллекции флагов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8104.jpg'])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8105.jpg'])
+
+@ru
+  <p>Стена из камней.</p>
+@en
+  <p>Wall of stones.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8108.jpg'])
+
+@ru
+  <p>Автобусная остановка.</p>
+@en
+  <p>Bus stop.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8113.jpg'])
+
+@ru
+  <p>На обратной стороне расписание автобуса.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8114.jpg'])
+
+@ru
+  <p>Бамбук.</p>
+@en
+  <p>Bamboo.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8116.jpg'])
+
+@ru
+  <p>Улицы Никко.</p>
+@en
+  <p>Nikko streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8118.jpg',
+  'IMG_8119.jpg',
+  'IMG_8121.jpg',
+  'IMG_8122.jpg',
+  'IMG_8123.jpg',
+  'IMG_8162.jpg',
+  'IMG_8164.jpg',
+  'IMG_8177.jpg',
+  'IMG_8188.jpg',
+  'IMG_8189.jpg',
+]])
+
+@ru
+  <p>Урна. Она же пепельница.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8120.jpg'])
+
+@ru
+  <p>Мост и вид с него.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8126.jpg',
+  'IMG_8124.jpg',
+  'IMG_8125.jpg',
+]])
+
+@ru
+  <p>Каменный парк.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8128.jpg'])
+
+@ru
+  <p>Чутье и беспорядочное брожение не впервые заводят в интересное место. Указатель на обратном пути сообщал, что это некая бездна.</p>
+@endlang
+<youtube title="Nikko Abyss, April 2017" v="mNwzCvQDQ1w"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_8134.jpg'])
+
+@ru
+  <p>Отличное место для привала.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8143.jpg'])
+
+@ru
+  <p>Ребята в красном.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8147.jpg'])
+
+@ru
+  <p>Их очень много, но один, похоже, сдулся.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8133.jpg'])
+
+@ru
+  <p>Обратный путь.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8153.jpg',
+  'IMG_8157.jpg',
+]])
+
+@ru
+  <p>Очевидно, сюда просят не ходить.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8159.jpg'])
+
+@ru
+  <p>Запас дров. Да, без центрального отопления зимой каждый согревается как может.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8161.jpg'])
+
+@ru
+  <p>Вот и указатель.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8165.jpg'])
+
+@ru
+  <p>Пейзажи вокруг.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8166.jpg',
+  'IMG_8167.jpg',
+  'IMG_8169.jpg',
+  'IMG_8170.jpg',
+  'IMG_8176.jpg',
+]])
+
+@ru
+  <p>Скайлайн в домашней среде.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8172.jpg',
+  'IMG_8173.jpg',
+  'IMG_8174.jpg',
+]])
+
+@ru
+  <p>Парковка с видом на горы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8181.jpg'])
+
+@ru
+  <p>Вид наверх.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8183.jpg'])
+
+@ru
+  <p>Вид вниз.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8102.jpg'])
+
+@ru
+  <p>Мост.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8185.jpg'])
+
+@ru
+  <p>Карты на любой вкус.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8190.jpg'])
+
+@ru
+  <p>Пора отправляться в Токио. Поезд уже поджидает.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8191.jpg'])
+
+@ru
+  <p>Состав всего из двух вагонов. Всего через пару станций предстоит сделать первую пересадку. Планировщик маршрута указывает, что следующий поезд отправляется в ту же минуту, когда прибывает этот. Менее минуты на пересадку! Реально? Да. Когда в таком случае приезжаешь на нужную станцию, то напротив уже стоит состав с открытыми дверьми, остается только перейти в него. Конечно, большинство бежит, так как боится его упустить. Когда планировщик пишет, что две минуты на пересадку, то состав аналогично придет на соседний путь. Нереальные варианты не предлагаются.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8193.jpg'])
+
+@ru
+  <p>Бригада на путях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8196.jpg'])
+
+@ru
+  <p>Железный человек в транспортном обличии.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8194.jpg'])
+@endsection
