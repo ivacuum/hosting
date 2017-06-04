566318a96bd85e7ca71a36998613563cbaa36022
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-04 13:46:07 +0300

    Калуга

---
+++ resources/views/life/trips/kaluga_2013_10.blade.php
@@ -0,0 +1,73 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Двор детства.</p>
+@en
+  <p>The yard of my childhood.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0267.jpg'])
+
+@ru
+  <p>Только в детстве в нем почти не было машин. Было относительно безопасно пинать мяч.</p>
+@en
+  <p>Except there were almost no cars. It was relatively safe to kick the ball there.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0269.jpg'])
+
+@ru
+  <p>По праздникам улица Кирова на несколько часов становится пешеходной.</p>
+@en
+  <p>During the holidays Kirov street becomes pedestrian street for a few hours.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0270.jpg'])
+
+@ru
+  <p>Или наполовину пешеходной.</p>
+@en
+  <p>Or half pedestrian.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0276.jpg'])
+
+@ru
+  <p>Все дружно ждут.</p>
+@en
+  <p>People are amicably waiting.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0272.jpg'])
+
+@ru
+  <p>И ждут.</p>
+@en
+  <p>And waiting.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0274.jpg'])
+
+@ru
+  <p>Ждут олимпийский огонь.</p>
+@en
+  <p>Waiting for the Olympic fire.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0289.jpg'])
+
+@ru
+  <p>Как обойти лужу?</p>
+@en
+  <p>How to get around the puddle?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0295.jpg'])
+
+@ru
+  <p>Так и живем. Песок дальше по улице все же подсказывает, что ведется подготовка к укладке тротуарной плитки.</p>
+@en
+  <p>That's how we live. The sand further down the street says that preparations are being made for the new sidewalk tile instead of asphalt.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0319.jpg'])
+
+@ru
+  <p>Прям как тут будет.</p>
+@en
+  <p>It will be just like here.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0323.jpg'])
+@endsection
