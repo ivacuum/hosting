2aa3d29ea5a5bbd36263f569fe61cc1028bc0984
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-12 03:02:04 +0300

    Удаление упоминания несуществующего файла

--- config/compile.php
+++ config/compile.php
@@ -6,7 +6,6 @@ return [
 
         realpath(__DIR__.'/../app/Providers/AppServiceProvider.php'),
         realpath(__DIR__.'/../app/Providers/AuthServiceProvider.php'),
-        realpath(__DIR__.'/../app/Providers/BreadcrumbsServiceProvider.php'),
         realpath(__DIR__.'/../app/Providers/EventServiceProvider.php'),
         realpath(__DIR__.'/../app/Providers/RouteServiceProvider.php'),
         realpath(__DIR__.'/../app/Providers/ValidatorServiceProvider.php'),
