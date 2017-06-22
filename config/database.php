24bc81680784b8e89f422bdb8c56ecefcc7662f2
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-23 00:07:37 +0300

    Подготовка к событиям в реальном времени

--- config/database.php
+++ config/database.php
@@ -2,7 +2,6 @@
 
 return [
 
-    'fetch' => PDO::FETCH_OBJ,
     'default' => env('DB_CONNECTION', 'mysql'),
 
     'connections' => [
@@ -62,10 +61,10 @@ return [
 
     'redis' => [
 
-        'cluster' => false,
+        'client' => 'predis',
 
         'default' => [
-            'host'     => env('REDIS_HOST', 'localhost'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
             'port'     => env('REDIS_PORT', 6379),
             'password' => env('REDIS_PASSWORD', null),
             'database' => 0,
