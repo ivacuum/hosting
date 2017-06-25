775335bba5f7f3414c571a7c1f1e7cda74f6ceb2
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-25 12:53:38 +0300

    Брянск

---
+++ resources/views/life/trips/bryansk_2017.blade.php
@@ -0,0 +1,175 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Из Калуги до Брянска два часа по железной дороге или пять часов на автобусе. Выбор очевиден.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1785.jpg'])
+
+@ru
+  <p>Отсюда уже можно доехать до белорусов — в пяти часах езды город Гомель.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1846.jpg'])
+
+@ru
+  <p>Вокзал находится на удалении от города, без транспорта не обойтись. Для вызова такси пункт отправления можно назвать Брянск первый или Орловский.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1845.jpg'])
+
+@ru
+  <p>Районы тоже достаточно удалены друг от друга. На остановках подсказка до каких из них можно добраться. Центр города — Советский район. Маршрутки вроде газелей оккупировали город. Проезд в них стоит 20 ₽ днем и 25 ₽ с 22:00 до 06:00.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1786.jpg'])
+
+@ru
+  <p>Понтонный мост.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1789.jpg'])
+
+@ru
+  <p>Набережная. Строительные работы еще ведутся.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1791.jpg'])
+
+@ru
+  <p>По следам минувшего выпускного.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1792.jpg'])
+
+@ru
+  <p>Красивый дом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1793.jpg'])
+
+@ru
+  <p>Город рельефный.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1795.jpg',
+  'IMG_1800.jpg',
+  'IMG_1822.jpg',
+  'IMG_1823.jpg',
+  'IMG_1838.jpg',
+  'IMG_1825.jpg',
+]])
+
+@ru
+  <p>Заброшка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1796.jpg'])
+
+@ru
+  <p>Указатели улиц.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1798.jpg',
+  'IMG_1813.jpg',
+  'IMG_1824.jpg',
+]])
+
+@ru
+  <p>Фонтаны с самыми тонкими струями.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1799.jpg',
+  'IMG_1809.jpg',
+]])
+
+@ru
+  <p>Дорогу от тротуара отделяет лишь желтая полоса разметки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1802.jpg'])
+
+@ru
+  <p>Пешеходная улица Гагарина.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1804.jpg'])
+
+@ru
+  <p>С растяжками как рекламного характера, так и светового.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1806.jpg'])
+
+@ru
+  <p>Мозаичные панно.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1808.jpg',
+  'IMG_1816.jpg',
+  'IMG_1835.jpg',
+]])
+
+@ru
+  <p>Так вот какой хип-хоп на самом деле.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1810.jpg'])
+
+@ru
+  <p>На что похожа стена?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1814.jpg'])
+
+@ru
+  <p>Пожарная часть.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1817.jpg'])
+
+@ru
+  <p>Запрещающие знаки не с красным, а с белым контуром. Не единичное явление.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1818.jpg'])
+
+@ru
+  <p>Лавочек со спинками попадается немало. И это прекрасно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1819.jpg'])
+
+@ru
+  <p>У корня деревья красят белым, а столбы — голубым.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1820.jpg'])
+
+@ru
+  <p>Проспект.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1821.jpg',
+  'IMG_1833.jpg',
+  'IMG_1834.jpg',
+]])
+
+@ru
+  <p>Переходить его предлагается без светофора.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1841.jpg'])
+
+@ru
+  <p>Впрочем, местные уже приловчились переходить дороги пошире и без зебр.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1849.jpg'])
+
+@ru
+  <p>Армия на «побелевшей» крыше.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1826.jpg'])
+
+@ru
+  <p>Местное ограждение порой включает в себя урну, как справа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1829.jpg'])
+
+@ru
+  <p>Рекламный баннер утратил физический адрес.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1844.jpg'])
+
+@ru
+  <p>Расписание автобуса написано от руки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1843.jpg'])
+
+@ru
+  <p>Среди других преимуществ брянского поезда можно отметить прекрасные <a class="link" href="/life/msk.2016.11#sitting-car">сидячие вагоны</a> с мягкими подушками под голову и откидывающимися спинками кресел. И это при двух часах езды! Экспрессу Калуга–Москва бы такие.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1850.jpg'])
+@endsection
