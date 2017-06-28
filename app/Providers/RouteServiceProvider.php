dee558417f9efb1b99ec3f609448fe9aee2ee6b7
Sergey Pankov <vacuum@ivacuum.ru>
2017-06-28 18:28:10 +0300

    Удаление неиспользуемых в маршрутах моделей

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,23 +1,11 @@
 <?php namespace App\Providers;
 
-use App\Artist;
-use App\City;
-use App\Client;
-use App\Comment;
-use App\Country;
-use App\Domain;
 use App\File;
-use App\Gig;
 use App\Image;
-use App\News;
-use App\Page;
-use App\Photo;
-use App\Server;
 use App\Tag;
 use App\Torrent;
 use App\Trip;
 use App\User;
-use App\YandexUser;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
@@ -30,24 +18,11 @@ class RouteServiceProvider extends ServiceProvider
 
         parent::boot();
 
-        \Route::model('Artist', Artist::class);
-        \Route::model('City', City::class);
-        \Route::model('Client', Client::class);
-        \Route::model('Comment', Comment::class);
-        \Route::model('Country', Country::class);
-        \Route::model('Domain', Domain::class);
         \Route::model('File', File::class);
-        \Route::model('Gig', Gig::class);
         \Route::model('Image', Image::class);
-        \Route::model('News', News::class);
-        \Route::model('Page', Page::class);
-        \Route::model('Photo', Photo::class);
-        \Route::model('Server', Server::class);
         \Route::model('Tag', Tag::class);
         \Route::model('Torrent', Torrent::class);
         \Route::model('Trip', Trip::class);
-        \Route::model('User', User::class);
-        \Route::model('YandexUser', YandexUser::class);
 
         \Route::bind('UserWithCounts', function ($id) {
             return User::withCount('comments', 'images', 'torrents')->findOrFail($id);
