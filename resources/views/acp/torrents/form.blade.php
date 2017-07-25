b3086cf3988ac6754978e890d3ecd075a27bae22
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-25 08:11:50 +0300

    Удаление неактуальных полей

--- resources/views/acp/torrents/form.blade.php
+++ resources/views/acp/torrents/form.blade.php
@@ -23,23 +23,10 @@
   </div>
 </div>
 
-{!! Form::text('title')->required()->html() !!}
-
 {!! Form::text('rto_id')->required()->html() !!}
-
-{!! Form::text('info_hash')->required()->html() !!}
-
-{!! Form::text('announcer')->html() !!}
 
 {!! Form::radio('status')->required()->values([
   App\Torrent::STATUS_HIDDEN => 'Скрыт',
   App\Torrent::STATUS_PUBLISHED => 'Опубликован',
   App\Torrent::STATUS_DELETED => 'Удален',
 ])->html() !!}
-
-<div class="form-group {{ $errors->has('html') ? 'has-error' : '' }}">
-  <label class="col-md-3 control-label required">HTML:</label>
-  <div class="col-md-9">
-    <textarea required class="form-control textarea-autosized js-autosize-textarea" name="html">{{ old('html', @$model->html) }}</textarea>
-  </div>
-</div>
