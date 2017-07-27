bc9762467bc2e191263bcf8858d891cf1679b004
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-27 17:02:05 +0300

    Ошмяны

---
+++ resources/views/life/trips/ashmyany_2017.blade.php
@@ -0,0 +1,50 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Автовокзал.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3092.jpg'])
+
+@ru
+  <p>Ведущая к нему улица с низкоэтажной застройкой.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3101.jpg'])
+
+@ru
+  <p>Контейнер для бутылок рядом с остановкой.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3088.jpg'])
+
+@ru
+  <p>Почтовые ящики с прорезями по диагонали.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3094.jpg'])
+
+@ru
+  <p>Рассвет.</p>
+@en
+  <p>Sunrise.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3096.jpg'])
+
+@ru
+  <p>Возвращающий в детство пейзаж.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3102.jpg'])
+
+@ru
+  <p>Каково идти с местным жителем по городку с населением менее 20 тысяч человек? С ним каждые пять минут кто-нибудь будет здороваться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3091.jpg'])
+
+@ru
+  <p>Режим работы на местном языке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3104.jpg'])
+
+@ru
+  <p>Для каждого сотового оператора свой номер для связи.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3107.jpg'])
+@endsection
