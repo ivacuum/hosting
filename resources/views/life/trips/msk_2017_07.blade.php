bc65a7bc30f470c89846f2dec51b56bed5bd3a69
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-27 22:33:10 +0300

    Москва

---
+++ resources/views/life/trips/msk_2017_07.blade.php
@@ -0,0 +1,91 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3038.jpg'])
+
+@ru
+  <p>Противоположную ее сторону еще доделывают.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3040.jpg'])
+
+@ru
+  <p>Министерство иностранных дел.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3041.jpg'])
+
+@ru
+  <p>Столько раз ходил возле Арбата и впервые добрался именно до него. Уступает по красоте новым набережным — той же Крымской, например.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3044.jpg',
+  'IMG_3045.jpg',
+]])
+
+@ru
+  <p>Троллейбус.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3047.jpg',
+  'IMG_3046.jpg',
+]])
+
+@ru
+  <p>Готовый газон. Только недавно попадался о нем <a class="link" href="https://www.youtube.com/watch?v=eG0boEHvlBc">сюжет в программе Галилео</a>.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3048.jpg'])
+
+@ru
+  <p>Переулок. Городская навигация стремительно хорошеет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3051.jpg'])
+
+@ru
+  <p>Логотипы метро разных времен в одном месте.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3052.jpg'])
+
+@ru
+  <p>Александровский парк.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3054.jpg'])
+
+@ru
+  <p>Красная площадь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3057.jpg'])
+
+@ru
+  <p>Ветошный переулок.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3059.jpg'])
+
+@ru
+  <p>В обратную сторону.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3062.jpg'])
+
+@ru
+  <p>Реклама Эйрбнб — сервиса по поиску прекрасного и недорогого жилья у местных. На специальной странице собрал <a class="link" href="/promocodes-coupons/airbnb">купоны и прочие способы</a> для получения скидки на бронирование.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3064.jpg'])
+
+@ru
+  <p>Дело к закату.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3065.jpg'])
+
+@ru
+  <p>Под логотипом метро подсветка обозначает цвета веток, которые здесь проходят.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3066.jpg'])
+
+@ru
+  <p>Далее ночной автобус в Минск. Ежедневные рейсы с Теплого стана — самый дешевый выбор летом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_3069.jpg'])
+@endsection
