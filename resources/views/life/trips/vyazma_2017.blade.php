46805bcb04b2fd9f18790744a5de9b11de242cc9
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-28 22:14:29 +0300

    Вязьма

---
+++ resources/views/life/trips/vyazma_2017.blade.php
@@ -0,0 +1,30 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Калуга—Брюгге. Реально? Да! Поехали!</p>
+@en
+  <p>Kaluga—Bruges. Is it real? Yes! Let's go!</p>
+@endlang
+
+@ru
+  <p>Остановка в форме зонтика.</p>
+@en
+  <p>Bus stop has an umbrella as a roof.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1883.jpg'])
+
+@ru
+  <p>Вид поближе.</p>
+@en
+  <p>A closer look.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1884.jpg'])
+
+@ru
+  <p>Не задерживаемся, продолжаем путь на запад.</p>
+@en
+  <p>We don't slow down. Moving on to the west.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_1886.jpg'])
+@endsection
