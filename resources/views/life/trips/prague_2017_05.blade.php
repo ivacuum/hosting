9771eaf175d7ded039f00723421fb9231af9a7d8
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-29 13:37:40 +0300

    Прага

---
+++ resources/views/life/trips/prague_2017_05.blade.php
@@ -0,0 +1,596 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Прага — отличная точка для совершения однодневных вылазок по стране. Часа за четыре можно добраться до самых дальних городов Чехии. Множество хороших мест и вовсе в 1-2 часах езды. <a class="link" href="https://www.regiojet.com/en/our-lines/map/">Карта направлений RegioJet</a>.</p>
+  <p>При снятии наличных банкомат Райффайзена предлагает сразу пересчитать сумму в рубли, тогда не будет колебания курса. Но эта операция жутко невыгодная. За 2 000 крон в рублях он просил 5 300 ₽, когда при проведении операции в кронах банк снял всего 4 800 ₽. Правильная кнопка экономит 10%!</p>
+  <p>Еще банкомат спрашивает какие купюры хочется получить: 200, 1 000 и 2 000 крон. Можно составить любую комбинацию.</p>
+  <p>По слухам на подходе закон о запрете курения в заведениях, поэтому некоторые запрещают уже сейчас. Теперь и в Чехии становится легче дышать.</p>
+@endlang
+
+@ru
+  <p>Понятно же пишут.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8828.jpg'])
+
+@ru
+  <p>В подъезде.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8829.jpg'])
+
+@ru
+  <p>Лифт в жилом доме.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0131.jpg'])
+
+@ru
+  <p>Вход на зеленую ветку метро.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0197.jpg'])
+
+@ru
+  <p>Метро. На преодоление всей красной ветки необходимо всего 40 минут — из центра до любой конечной всего 20 минут. Желтая подлиннее будет.</p>
+@en
+  <p>Metro.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8834.jpg',
+  'IMG_8976.jpg',
+  'IMG_8977.jpg',
+  'IMG_8978.jpg',
+  'IMG_0128.jpg',
+]])
+
+@ru
+  <p>Названия станций метро.</p>
+@en
+  <p>Metro stations' names.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0129.jpg',
+  'IMG_0130.jpg',
+  'IMG_9827.jpg',
+]])
+
+@ru
+  <p>Автобусный вокзал. Вам сюда, если хотите отправиться на восток страны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8835.jpg'])
+
+@ru
+  <p>Автобусный вокзал на западе Праги. Далековато находится, стоит около часа закладывать при выезде из центра.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9733.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8826.jpg',
+  'IMG_8827.jpg',
+  'IMG_8980.jpg',
+  'IMG_8981.jpg',
+  'IMG_9025.jpg',
+  'IMG_9026.jpg',
+  'IMG_9027.jpg',
+  'IMG_9028.jpg',
+  'IMG_9054.jpg',
+  'IMG_9055.jpg',
+  'IMG_9101.jpg',
+  'IMG_9823.jpg',
+  'IMG_0116.jpg',
+  'IMG_0123.jpg',
+  'IMG_0124.jpg',
+  'IMG_0160.jpg',
+  'IMG_0181.jpg',
+  'IMG_0189.jpg',
+  'IMG_0190.jpg',
+  'IMG_0191.jpg',
+  'IMG_0192.jpg',
+  'IMG_0216.jpg',
+  'IMG_0245.jpg',
+]])
+
+@ru
+  <p>Характерный дизайн пражской тротуарной плитки узнается издалека.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8988.jpg'])
+
+@ru
+  <p>Узоры поближе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0119.jpg'])
+
+@ru
+  <p>В разобранном виде.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0185.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8989.jpg',
+  'IMG_8993.jpg',
+  'IMG_8997.jpg',
+  'IMG_9012.jpg',
+  'IMG_9018.jpg',
+  'IMG_9021.jpg',
+  'IMG_9024.jpg',
+  'IMG_9096.jpg',
+  'IMG_0117.jpg',
+  'IMG_0122.jpg',
+  'IMG_0214.jpg',
+  'IMG_0221.jpg',
+  'IMG_0239.jpg',
+]])
+
+@ru
+  <p>Камни.</p>
+@en
+  <p>Rocks.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9011.jpg'])
+
+@ru
+  <p>Выключенный фонтан.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8995.jpg'])
+
+@ru
+  <p>Дверь с объемными фигурами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8996.jpg'])
+
+@ru
+  <p>Разминка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9013.jpg'])
+
+@ru
+  <p>Вид на поезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9020.jpg'])
+
+@ru
+  <p>Вид из поезда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0105.jpg'])
+
+@ru
+  <p>Двухэтажный поезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9023.jpg'])
+
+@ru
+  <p>Еще один.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9037.jpg'])
+
+@ru
+  <p>Крытая велопарковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9029.jpg'])
+
+@ru
+  <p>Лавочки перед стеклянным зданием.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9030.jpg'])
+
+@ru
+  <p>Складной шлагбаум в верхнем положении.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9031.jpg'])
+
+@ru
+  <p>И в нижнем.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9032.jpg'])
+
+@ru
+  <p>Арка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9033.jpg'])
+
+@ru
+  <p>Тротуар внезапно заканчивается.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9034.jpg'])
+
+@ru
+  <p>Стеклянные здания.</p>
+@en
+  <p>Glass buildings.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9035.jpg',
+  'IMG_9036.jpg',
+  'IMG_0244.jpg',
+]])
+
+@ru
+  <p>Мусор в воде.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9043.jpg'])
+
+@ru
+  <p>Шлюз.</p>
+@en
+  <p>Lock.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9046.jpg'])
+
+@ru
+  <p>Под рекламными щитами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9049.jpg'])
+
+@ru
+  <p>Велосипедная разметка на перекрестке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9056.jpg'])
+
+@ru
+  <p>Так поезда попадают на небеса.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9059.jpg'])
+
+@ru
+  <p>А по таким путям ходят ежедневно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0107.jpg'])
+
+@ru
+  <p>А по таким лишь изредка.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9062.jpg',
+  'IMG_9061.jpg',
+]])
+
+@ru
+  <p>Рекламный щит: чистая водка, чистая забава.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9066.jpg'])
+
+@ru
+  <p>Зеленая стена над парковкой.</p>
+@en
+  <p>Green wall above the parking.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9068.jpg'])
+
+@ru
+  <p>Промзона.</p>
+@en
+  <p>Industrial zone.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9071.jpg'])
+
+@ru
+  <p>Стройки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0219.jpg'])
+
+@ru
+  <p>Ветра нет.</p>
+@en
+  <p>There is no wind.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9072.jpg'])
+
+@ru
+  <p>Тропинка.</p>
+@en
+  <p>Pathway.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9076.jpg'])
+
+@ru
+  <p>Мост.</p>
+@en
+  <p>Bridge.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9081.jpg'])
+
+@ru
+  <p>Под мостом.</p>
+@en
+  <p>Under the bridge.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9082.jpg'])
+
+@ru
+  <p>На мосту.</p>
+@en
+  <p>On the bridge.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9085.jpg'])
+
+@ru
+  <p>Другой мост.</p>
+@en
+  <p>Another bridge.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9047.jpg'])
+
+@ru
+  <p>Район Троя останется для исследования на следующий визит.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9083.jpg'])
+
+@ru
+  <p>Канализационный люк.</p>
+@en
+  <p>Sewer hatch.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9084.jpg'])
+
+@ru
+  <p>Голубь.</p>
+@en
+  <p>Pigeon.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9093.jpg'])
+
+@ru
+  <p>Оса.</p>
+@en
+  <p>Wasp.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9094.jpg'])
+
+@ru
+  <p>Вода настолько чистая, что местами кажется, что ее вовсе нет.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9097.jpg',
+  'IMG_9098.jpg',
+  'IMG_9099.jpg',
+  'IMG_9100.jpg',
+]])
+
+@ru
+  <p>Вот тут действительно нет воды.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9824.jpg'])
+
+@ru
+  <p>Главный вокзал.</p>
+@en
+  <p>Central station.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0109.jpg',
+  'IMG_0110.jpg',
+]])
+
+@ru
+  <p>Гигантский знак пешеходного перехода.</p>
+@en
+  <p>Giant road sign of a pedestrian crossing.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0111.jpg'])
+
+@ru
+  <p>Тоннель.</p>
+@en
+  <p>Tunnel.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8982.jpg',
+  'IMG_0112.jpg',
+  'IMG_8985.jpg',
+  'IMG_8986.jpg',
+]])
+
+@ru
+  <p>Тоннель поменьше.</p>
+@en
+  <p>A smaller tunnel.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0224.jpg'])
+
+@ru
+  <p>Карты maps.me в Чехии далеко не раз показывали проходы в тоннелях и подобных местах. И действительно всегда можно было пройти. Не без посторонних мыслей, но можно.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9050.jpg'])
+
+@ru
+  <p>Все бутылки не влезли в приемник. Коробка заботливо принимает дополнительную тару.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0118.jpg'])
+
+@ru
+  <p>Трамвайные пути.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0125.jpg',
+  'IMG_0127.jpg',
+  'IMG_9052.jpg',
+]])
+
+@ru
+  <p>Стадион, где неделей спустя играли Депеш мод и Раммштайн.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0133.jpg'])
+
+@ru
+  <p>Внутрь не пройти, но через ограждения немного видно поле и трибуны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0135.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8994.jpg',
+  'IMG_9042.jpg',
+  'IMG_9048.jpg',
+  'IMG_9089.jpg',
+  'IMG_0136.jpg',
+  'IMG_0137.jpg',
+]])
+
+@ru
+  <p>Круговое движение.</p>
+@en
+  <p>Roundabout.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0242.jpg'])
+
+@ru
+  <p>Улица Владивосточная.</p>
+@en
+  <p>Vladivostochnaya street.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0134.jpg'])
+
+@ru
+  <p>Улица Челябинская.</p>
+@en
+  <p>Chelyabinskaya street.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0138.jpg'])
+
+@ru
+  <p>Улицы Грузинская и Байкальская.</p>
+@en
+  <p>Gruzinskaya and Baikalskaya streets.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0139.jpg'])
+
+@ru
+  <p>Улица Якутская.</p>
+@en
+  <p>Yakutskaya street.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0157.jpg'])
+
+@ru
+  <p>Улица Тульская.</p>
+@en
+  <p>Tulskaya street.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0158.jpg'])
+
+@ru
+  <p>Улица Русская.</p>
+@en
+  <p>Russkaya street.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0182.jpg'])
+
+@ru
+  <p>На остановке стрелки над маршрутами указывают куда продолжится движение.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0145.jpg',
+  'IMG_0196.jpg',
+]])
+
+@ru
+  <p>Парковка в жилом районе.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0146.jpg'])
+
+@ru
+  <p>Фасады.</p>
+@en
+  <p>Facades.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8987.jpg',
+  'IMG_0148.jpg',
+  'IMG_0159.jpg',
+]])
+
+@ru
+  <p>Котейка.</p>
+@en
+  <p>A cat.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0161.jpg'])
+
+@ru
+  <p>По нраву яркие цвета автомобилей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0163.jpg'])
+
+@ru
+  <p>Миниатюрная заправка. Достаточно припарковаться рядом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0179.jpg'])
+
+@ru
+  <p>Домик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0180.jpg'])
+
+@ru
+  <p>Еще домик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0184.jpg'])
+
+@ru
+  <p>Из окна трамвая.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0204.jpg'])
+
+@ru
+  <p>Водители трамвая могут меняться прямо в городе. Было неожиданно увидеть как машинист на остановке оделся, собрал вещи и ушел. Только секунд через 10 на его место пришла замена.</p>
+@endlang
+
+@ru
+  <p>Внутри трамвая. В салоне добрый пяток компостеров для билетов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0212.jpg'])
+
+@ru
+  <p>Так как нет копеек, то цены в магазине округляют до ближайшего целого числа. В том числе в большую сторону.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0213.jpg'])
+
+@ru
+  <p>Посиделки у воды.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0218.jpg'])
+
+@ru
+  <p>Посиделки с более сложным и крутым спуском.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9038.jpg'])
+
+@ru
+  <p>Рыбалка.</p>
+@en
+  <p>Angling.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0220.jpg'])
+
+@ru
+  <p>Однажды вечером тучи сгустились.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0229.jpg',
+  'IMG_0231.jpg',
+  'IMG_0232.jpg',
+  'IMG_0233.jpg',
+  'IMG_0234.jpg',
+  'IMG_0235.jpg',
+  'IMG_0236.jpg',
+  'IMG_0237.jpg',
+  'IMG_0238.jpg',
+  'IMG_0241.jpg',
+  'IMG_0243.jpg',
+]])
+
+@ru
+  <p>И перешли в закат.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0246.jpg'])
+@endsection
