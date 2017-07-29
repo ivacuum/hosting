44f721363bfa6e37af3167c5e9f34a040313d01c
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-29 16:36:33 +0300

    Чески-Крумлов

---
+++ resources/views/life/trips/cesky-krumlov_2017.blade.php
@@ -0,0 +1,185 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Подвох чувствовался еще при покупке билетов на автобус — оставалось всего несколько свободных мест из 60+. Направление дико популярное у туристов.</p>
+@endlang
+
+@ru
+  <p>За окном автобуса разноцветные поля и одинокий поезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9104.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9140.jpg',
+  'IMG_9141.jpg',
+]])
+
+@ru
+  <p>Улицы вне центра.</p>
+@en
+  <p>Streets outside of the city center.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9131.jpg',
+  'IMG_9132.jpg',
+]])
+
+@ru
+  <p>Улицы в центре.</p>
+@en
+  <p>Streets in the city center.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9142.jpg',
+  'IMG_9145.jpg',
+  'IMG_9147.jpg',
+  'IMG_9155.jpg',
+]])
+
+@ru
+  <p>Город очень рельефный.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9148.jpg',
+  'IMG_9144.jpg',
+  'IMG_9154.jpg',
+  'IMG_9157.jpg',
+  'IMG_9161.jpg',
+]])
+
+@ru
+  <p>Панорамы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9136.jpg',
+  'IMG_9169.jpg',
+  'IMG_9170.jpg',
+  'IMG_9173.jpg',
+  'IMG_9194.jpg',
+]])
+
+@ru
+  <p>Места для прогулок.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9138.jpg',
+  'IMG_9143.jpg',
+  'IMG_9191.jpg',
+  'IMG_9212.jpg',
+]])
+
+@ru
+  <p>Река Влтава.</p>
+@en
+  <p>Vltava river.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9149.jpg',
+  'IMG_9151.jpg',
+  'IMG_9152.jpg',
+  'IMG_9158.jpg',
+  'IMG_9159.jpg',
+  'IMG_9164.jpg',
+  'IMG_9180.jpg',
+  'IMG_9186.jpg',
+]])
+
+@ru
+  <p>Зеленый дом.</p>
+@en
+  <p>Green house.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9156.jpg'])
+
+@ru
+  <p>Потравины — продукты. Первый попавшийся супермаркет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9166.jpg'])
+
+@ru
+  <p>Лавочка.</p>
+@en
+  <p>Bench.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9168.jpg'])
+
+@ru
+  <p>Еще лавочка.</p>
+@en
+  <p>Another bench.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9185.jpg'])
+
+@ru
+  <p>Красочная и объемная городская навигация.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9171.jpg',
+  'IMG_9192.jpg',
+]])
+
+@ru
+  <p>Замок.</p>
+@en
+  <p>Castle.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9175.jpg'])
+
+@ru
+  <p>Сбоку от моста к замку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9176.jpg'])
+
+@ru
+  <p>Люк.</p>
+@en
+  <p>Manhole.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9189.jpg'])
+
+@ru
+  <p>Отличное место, чтобы прилечь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9190.jpg'])
+
+@ru
+  <p>Солнечные часы не перевели на летнее время.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9193.jpg'])
+
+@ru
+  <p>Окно в Европу.</p>
+@en
+  <p>Window to Europe.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9195.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9206.jpg',
+  'IMG_9208.jpg',
+  'IMG_9209.jpg',
+]])
+
+@ru
+  <p>Луг.</p>
+@en
+  <p>Meadow.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9210.jpg'])
+
+@ru
+  <p>ДТП. Автобус въехал в машину на перекрестке. Просидели 20 минут, проводник сообщил о задержке рейса и предложил скоротать время за развлекаловом на планшетах. В это время подогнали новый автобус, всех попросили пересесть в него на те же места. Вместе с переносом багажа всех пассажиров пересадка заняла всего 3 минуты. Продолжили путь. В ближайший пункт назначения опоздали всего на 15 минут.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9213.jpg'])
+@endsection
