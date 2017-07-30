24f1bfe3eccf62bb77c629a0d7bb384f73094c63
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-30 11:11:37 +0300

    Градец-Кралове

---
+++ resources/views/life/trips/hradec-kralove_2017.blade.php
@@ -0,0 +1,277 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Место водителя.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8970.jpg'])
+
+@ru
+  <p>Главный автовокзал.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8838.jpg',
+  'IMG_8839.jpg',
+]])
+
+@ru
+  <p>Многоэтажная велосипедная парковка.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8840.jpg',
+  'IMG_8841.jpg',
+  'IMG_8842.jpg',
+]])
+
+@ru
+  <p>Фонтан перед главным железнодорожным вокзалом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8843.jpg'])
+
+@ru
+  <p>Подъезд жилого дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8845.jpg'])
+
+@ru
+  <p>Велодорожка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8846.jpg'])
+
+@ru
+  <p>Футуристическая конструкция с подвешенными троллейбусными проводами на круговом перекрестке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8847.jpg'])
+
+@ru
+  <p>И в центре фонтан.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8848.jpg'])
+
+@ru
+  <p>Лавочки. Особенный кайф, когда они посреди зелени.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8849.jpg',
+  'IMG_8850.jpg',
+  'IMG_8851.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8852.jpg',
+  'IMG_8853.jpg',
+  'IMG_8854.jpg',
+  'IMG_8855.jpg',
+  'IMG_8856.jpg',
+  'IMG_8866.jpg',
+  'IMG_8868.jpg',
+  'IMG_8907.jpg',
+  'IMG_8938.jpg',
+  'IMG_8917.jpg',
+  'IMG_8918.jpg',
+  'IMG_8921.jpg',
+  'IMG_8944.jpg',
+  'IMG_8945.jpg',
+  'IMG_8950.jpg',
+  'IMG_8951.jpg',
+  'IMG_8952.jpg',
+  'IMG_8955.jpg',
+]])
+
+@ru
+  <p>Площадь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8916.jpg'])
+
+@ru
+  <p>Аллея деревьев, как в <a class="link" href="/life/kirov-klg.2016#trees">Кирове</a>.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8857.jpg'])
+
+@ru
+  <p>Разноцветные дома смотрятся лучше монотонных.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8858.jpg'])
+
+@ru
+  <p>И вашему самокату найдется место на парковке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8859.jpg'])
+
+@ru
+  <p>Жилой район.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8860.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8869.jpg',
+  'IMG_8870.jpg',
+  'IMG_8871.jpg',
+  'IMG_8873.jpg',
+  'IMG_8874.jpg',
+  'IMG_8875.jpg',
+  'IMG_8876.jpg',
+  'IMG_8878.jpg',
+  'IMG_8879.jpg',
+  'IMG_8880.jpg',
+]])
+
+@ru
+  <p>Еще парки.</p>
+@en
+  <p>More parks.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8922.jpg',
+  'IMG_8923.jpg',
+  'IMG_8924.jpg',
+  'IMG_8928.jpg',
+  'IMG_8929.jpg',
+  'IMG_8930.jpg',
+  'IMG_8931.jpg',
+  'IMG_8937.jpg',
+]])
+
+@ru
+  <p>Урна с пепельницей.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8934.jpg'])
+
+@ru
+  <p>Удобства для небольшого застолья.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8936.jpg'])
+
+@ru
+  <p>Мосты.</p>
+@en
+  <p>Bridges.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8862.jpg',
+  'IMG_8864.jpg',
+  'IMG_8865.jpg',
+  'IMG_8883.jpg',
+  'IMG_8885.jpg',
+  'IMG_8886.jpg',
+  'IMG_8910.jpg',
+  'IMG_8911.jpg',
+  'IMG_8942.jpg',
+  'IMG_8949.jpg',
+  'IMG_8959.jpg',
+]])
+
+@ru
+  <p>Что же это за здание?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8901.jpg'])
+
+@ru
+  <p>Возле которого пешеходы идут наверху, а внизу парковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8903.jpg'])
+
+@ru
+  <p>И вокруг красота.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8904.jpg'])
+
+@ru
+  <p>Это библиотека.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8905.jpg'])
+
+@ru
+  <p>Или книговня, если на местном.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8908.jpg'])
+
+@ru
+  <p>Вход в библиотеку.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8906.jpg'])
+
+@ru
+  <p>Троллейбус с гармошкой.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8909.jpg'])
+
+@ru
+  <p>Остановка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8912.jpg'])
+
+@ru
+  <p>Здания.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8913.jpg',
+  'IMG_8914.jpg',
+  'IMG_8915.jpg',
+  'IMG_8956.jpg',
+  'IMG_8957.jpg',
+]])
+
+@ru
+  <p>Дороги.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8925.jpg',
+  'IMG_8926.jpg',
+  'IMG_8927.jpg',
+  'IMG_8958.jpg',
+]])
+
+@ru
+  <p>Многоэтажная парковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8968.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8940.jpg',
+  'IMG_8947.jpg',
+]])
+
+@ru
+  <p>Пешеходный переход из плитки на одном уровне с тротуаром.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8946.jpg'])
+
+@ru
+  <p>Лавка двигается к дереву.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8954.jpg'])
+
+@ru
+  <p>Пешеходная зона.</p>
+@en
+  <p>Pedestrian zone.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8960.jpg',
+  'IMG_8962.jpg',
+  'IMG_8963.jpg',
+  'IMG_8964.jpg',
+  'IMG_8965.jpg',
+]])
+
+@ru
+  <p>Окно в Европу.</p>
+@en
+  <p>Window to Europe.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_8973.jpg'])
+@endsection
