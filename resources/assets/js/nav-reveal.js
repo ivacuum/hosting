d501f61ff6e3cc6833b5d967b5ffdadf3bfc4a54
Sergey Pankov <vacuum@ivacuum.ru>
2017-07-31 11:59:06 +0300

    Обработчик прокрутки только для мобил

--- resources/assets/js/nav-reveal.js
+++ resources/assets/js/nav-reveal.js
@@ -1,34 +1,37 @@
 let throttle = require('lodash/throttle')
 
-let last_offset = window.pageYOffset
-let $tabbar = $('.bottom-tabbar-container')
-const tabbar_height = $tabbar.height()
-const threshold = 77
+if (window.matchMedia('(max-width: 768px)').matches) {
+  let last_offset = window.pageYOffset
+  let $tabbar = $('.bottom-tabbar-container')
+  const tabbar_height = $tabbar.height()
+  const threshold = 77
 
-$(window).on('scroll.js-nav-reveal', throttle(function () {
-  let offset = window.pageYOffset
-  const window_height = window.innerHeight
-  const doc_height = $(document).height()
+  $(window).on('scroll.js-nav-reveal', throttle(function () {
+    let offset = window.pageYOffset
+    const window_height = window.innerHeight
+    const doc_height = $(document).height()
+    console.log('yes')
 
-  // Домотали до конца
-  if (offset + window_height >= doc_height - tabbar_height) {
-    $tabbar.addClass('revealed')
+    // Домотали до конца
+    if (offset + window_height >= doc_height - tabbar_height) {
+      $tabbar.addClass('revealed')
+      last_offset = offset
+      return
+    }
+
+    // Промотали мало
+    if (Math.abs(last_offset - offset) <= threshold) {
+      return
+    }
+
+    if (offset > last_offset) {
+      // Промотали вниз достаточно для скрытия
+      $tabbar.removeClass('revealed')
+    } else {
+      // Промотали вверх достаточно для показа
+      $tabbar.addClass('revealed')
+    }
+
     last_offset = offset
-    return
-  }
-
-  // Промотали мало
-  if (Math.abs(last_offset - offset) <= threshold) {
-    return
-  }
-
-  if (offset > last_offset) {
-    // Промотали вниз достаточно для скрытия
-    $tabbar.removeClass('revealed')
-  } else {
-    // Промотали вверх достаточно для показа
-    $tabbar.addClass('revealed')
-  }
-
-  last_offset = offset
-}, 250))
+  }, 250))
+}
