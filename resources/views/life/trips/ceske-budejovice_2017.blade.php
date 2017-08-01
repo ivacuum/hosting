7663b7da45ee708e436d68d3a5bed969b10968c1
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-01 09:31:39 +0300

    Ческе-Будеёвице

---
+++ resources/views/life/trips/ceske-budejovice_2017.blade.php
@@ -0,0 +1,358 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Пейзаж из автобуса напоминает наши спальные районы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9109.jpg'])
+
+@ru
+  <p>Водичка с потолка по 24,90.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9215.jpg'])
+
+@ru
+  <p>Пешеходная зона.</p>
+@en
+  <p>Pedestrian zone.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9217.jpg'])
+
+@ru
+  <p>Объемный пакет завлекает в супермаркет Билла.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9218.jpg'])
+
+@ru
+  <p>Плиточный узор расширяется.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9220.jpg'])
+
+@ru
+  <p>Двусторонние лавки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9225.jpg'])
+
+@ru
+  <p>Каменные ребята.</p>
+@en
+  <p>Stone guys.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9227.jpg'])
+
+@ru
+  <p>Уличная навигация.</p>
+@en
+  <p>Street navigation.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9229.jpg'])
+
+@ru
+  <p>Еще уличная навигация.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9253.jpg'])
+
+@ru
+  <p>Лавочки в парке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9230.jpg'])
+
+@ru
+  <p>Лавочки за парком.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9235.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9232.jpg'])
+
+@ru
+  <p>Чем дальше от столицы, тем дешевле транспорт.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9233.jpg'])
+
+@ru
+  <p>Водосток в лучших российских традициях.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9234.jpg'])
+
+@ru
+  <p>Вариант получше.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9289.jpg'])
+
+@ru
+  <p>Фасады.</p>
+@en
+  <p>Facades.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9236.jpg',
+  'IMG_9263.jpg',
+  'IMG_9268.jpg',
+  'IMG_9270.jpg',
+  'IMG_9272.jpg',
+  'IMG_9275.jpg',
+]])
+
+@ru
+  <p>Понятно же пишут.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9237.jpg'])
+
+@ru
+  <p>Основная видимая грязь в Европе — граффити (скорее даже мазки) на первых этажах домов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9238.jpg'])
+
+@ru
+  <p>Канализационный люк.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9239.jpg'])
+
+@ru
+  <p>Набережные.</p>
+@en
+  <p>Embankments.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9240.jpg',
+  'IMG_9242.jpg',
+  'IMG_9243.jpg',
+  'IMG_9244.jpg',
+  'IMG_9261.jpg',
+  'IMG_9264.jpg',
+  'IMG_9267.jpg',
+]])
+
+@ru
+  <p>Поразительной красоты ворота.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9241.jpg'])
+
+@ru
+  <p>Люди проводят время на улице.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9245.jpg',
+  'IMG_9247.jpg',
+  'IMG_9248.jpg',
+]])
+
+@ru
+  <p>Мосты.</p>
+@en
+  <p>Bridges.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9246.jpg',
+  'IMG_9265.jpg',
+]])
+
+@ru
+  <p>Пополнение коллекции кандидатов в аватарки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9249.jpg'])
+
+@ru
+  <p>Домик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9254.jpg'])
+
+@ru
+  <p>Река Малше.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9255.jpg'])
+
+@ru
+  <p>Пополнение коллекции...</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9256.jpg'])
+
+@ru
+  <p>...флагов!</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9257.jpg'])
+
+@ru
+  <p>Зверокруги, они же знаки зодиака.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9258.jpg'])
+
+@ru
+  <p>Беседка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9259.jpg'])
+
+@ru
+  <p>Гребцы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9260.jpg'])
+
+@ru
+  <p>Арки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9271.jpg'])
+
+@ru
+  <p>Площадь.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9276.jpg'])
+
+@ru
+  <p>Уличные кафе на площади.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9274.jpg'])
+
+@ru
+  <p>Солнце спряталось в башне.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9278.jpg'])
+
+@ru
+  <p>Указатель улицы.</p>
+@en
+  <p>Street sign.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9280.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9216.jpg',
+  'IMG_9273.jpg',
+  'IMG_9281.jpg',
+  'IMG_9294.jpg',
+  'IMG_9303.jpg',
+]])
+
+@ru
+  <p>Навигация на автобусной остановке. На заднем плане дико завешенное рекламой здание.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9282.jpg'])
+
+@ru
+  <p>Впервые попался футбайк — самокат с запчастями от велосипеда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9284.jpg'])
+
+@ru
+  <p>Жилой район.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9298.jpg'])
+
+@ru
+  <p>Прямо через него проходит канал.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9285.jpg',
+  'IMG_9295.jpg',
+  'IMG_9297.jpg',
+]])
+
+@ru
+  <p>Матершска школа — детский сад.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9287.jpg',
+  'IMG_9288.jpg',
+]])
+
+@ru
+  <p>Совершенно разный вид у соседних подъездов.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9290.jpg'])
+
+@ru
+  <p>Балкон с миниатюрными деревьями.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9291.jpg'])
+
+@ru
+  <p>Часы на углу дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9293.jpg'])
+
+@ru
+  <p>Ящик Владимира Петровича.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9299.jpg'])
+
+@ru
+  <p>Детские рисунки мелом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9300.jpg'])
+
+@ru
+  <p>Электрощит.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9302.jpg'])
+
+@ru
+  <p>Пожарная лестница.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9304.jpg'])
+
+@ru
+  <p>Тем временем на железных дорогах.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9306.jpg',
+  'IMG_9307.jpg',
+  'IMG_9315.jpg',
+]])
+
+@ru
+  <p>И около них.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9308.jpg'])
+
+@ru
+  <p>Автомобили везут кому-то.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9309.jpg'])
+
+@ru
+  <p>Локомотив.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9311.jpg'])
+
+@ru
+  <p>С линейкой на крыше.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9312.jpg'])
+
+@ru
+  <p>Мост через железную дорогу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9313.jpg'])
+
+@ru
+  <p>Тягают один вагон туда-сюда. Похоже на испытания.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9314.jpg'])
+
+@ru
+  <p>Главный железнодорожный вокзал.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9316.jpg'])
+
+@ru
+  <p>Автовокзал на втором этаже торгового центра. Город настолько приятен, что из него никто не хочет уезжать.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9317.jpg'])
+
+@ru
+  <p>Любители закатов могут подобрать билеты на вечерний рейс с окнами на запад.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9320.jpg'])
+
+@ru
+  <p>И любоваться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9322.jpg'])
+@endsection
