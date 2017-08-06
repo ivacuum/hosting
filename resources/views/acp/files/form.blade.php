69fbf7bfc58ccc6a18ba28b65e5537e6a6e14ae4
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-06 13:13:58 +0300

    Поля для редактирования файла

--- resources/views/acp/files/form.blade.php
+++ resources/views/acp/files/form.blade.php
@@ -1,22 +1,6 @@
 @include('tpl.form_errors')
 
-<div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
-  <label class="col-md-3 control-label required">Название:</label>
-  <div class="col-md-6">
-    <input required type="text" class="form-control" name="title" value="{{ old('title', @$model->title) }}">
-  </div>
-</div>
-
-<div class="form-group {{ $errors->has('folder') ? 'has-error' : '' }}">
-  <label class="col-md-3 control-label required">Папка:</label>
-  <div class="col-md-6">
-    <input required type="text" class="form-control" name="folder" value="{{ old('folder', @$model->folder) }}">
-  </div>
-</div>
-
-<div class="form-group {{ $errors->has('slug') ? 'has-error' : '' }}">
-  <label class="col-md-3 control-label required">URL:</label>
-  <div class="col-md-6">
-    <input required type="text" class="form-control" name="slug" value="{{ old('slug', @$model->slug) }}">
-  </div>
-</div>
+{!! Form::text('title')->required()->html() !!}
+{!! Form::text('slug')->required()->html() !!}
+{!! Form::text('project')->html() !!}
+{!! Form::text('folder')->html() !!}
