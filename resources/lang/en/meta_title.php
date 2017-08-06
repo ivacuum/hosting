cd6816c3cda6b2b23558011b38b943793e17a1cd
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-06 15:20:04 +0300

    Обновление заголовков

--- resources/lang/en/meta_title.php
+++ resources/lang/en/meta_title.php
@@ -2,12 +2,12 @@
 
 return [
     'dcpp.dcpp' => 'Download DC++ 0.782',
-    'dcpp.airdc' => 'Download AirDC++ 2.09',
-    'dcpp.apexdc' => 'Download ApexDC++ 1.4.3',
+    'dcpp.airdc' => 'Download AirDC++ 3.41',
+    'dcpp.apexdc' => 'Download ApexDC++ 1.6.4',
     'dcpp.jucydc' => 'Download Jucy DC++ 0.85 for Windows, Linux and macOS',
     'dcpp.pelinkdc' => 'Download PeLinkDC++ 5.95',
     'dcpp.strongdc' => 'Download StrongDC++ 2.42',
-    'dcpp.flylinkdc' => 'Download FlyLinkDC++ r500',
+    'dcpp.flylinkdc' => 'Download FlyLinkDC++ r504',
     'dcpp.greylinkdc' => 'Download GreyLinkDC++ 0.49',
     'dcpp.shakespeer' => 'Download ShakesPeer 0.9.2 for macOS',
 ];
