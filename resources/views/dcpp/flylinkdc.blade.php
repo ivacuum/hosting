c8a3c9c0840838d06effe74912d875c23f9e3183
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-06 14:57:39 +0300

    Обновление клиента

--- resources/views/dcpp/flylinkdc.blade.php
+++ resources/views/dcpp/flylinkdc.blade.php
@@ -2,7 +2,8 @@
   'meta_title' => trans('meta_title.dcpp.flylinkdc'),
   'software_title' => trans('dcpp.flylinkdc'),
   'software' => [
-    ['version' => 'r500', 'id' => 140, 'size' => 17611794, 'dl_suffix' => ' 32+64bit'],
+    ['version' => 'r504', 'id' => 153, 'size' => 14172792, 'dl_suffix' => ''],
+    ['version' => 'r500', 'id' => 140, 'size' => 17611794, 'dl_suffix' => ''],
   ],
   'developer_site' => 'http://www.flylinkdc.ru/',
 ])
