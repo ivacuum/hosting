248d661da4b998dc1e68852bb3ab3d541304e26a
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-10 00:14:27 +0300

    Оломоуц

---
+++ resources/views/life/trips/olomouc_2017.blade.php
@@ -0,0 +1,260 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Раз уж добрался до Брно, то дальше стоял выбор какой еще город зацепить на востоке Чехии в тот же день. Достойными кандидатами выглядели Оломоуц и Острава. Выбор в пользу первого был сделан из-за меньшего расстояния.</p>
+@endlang
+
+@ru
+  <p>Серая плитка для пешеходов, красная — для велосипедистов. Ни одного кондиционера на фасаде дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9561.jpg'])
+
+@ru
+  <p>Узор плитки меняется, но разделение по цветам остается.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9569.jpg'])
+
+@ru
+  <p>Прозрачный подъезд. Почтовые ящики рядом. Домофон на удивление далеко от двери.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9562.jpg'])
+
+@ru
+  <p>Вид крыши выделяется на фоне остальных этажей. Не будет удивлением, если по ней можно ходить.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9563.jpg'])
+
+@ru
+  <p>Самые ходовые марки машин в Чехии: Шкода, Пежо, Форд и Фиат.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9564.jpg'])
+
+@ru
+  <p>Трамвай ходит посередине дороги.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9565.jpg'])
+
+@ru
+  <p>Вот так.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9570.jpg'])
+
+@ru
+  <p>Уличная навигация.</p>
+@en
+  <p>Street navigation.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9567.jpg'])
+
+@ru
+  <p>Поле не простаивает — используется для игр с мячом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9568.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9571.jpg',
+  'IMG_9586.jpg',
+  'IMG_9642.jpg',
+  'IMG_9643.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9589.jpg',
+  'IMG_9592.jpg',
+  'IMG_9595.jpg',
+  'IMG_9638.jpg',
+  'IMG_9639.jpg',
+]])
+
+@ru
+  <p>По мосту можно зайти в торговый центр прямо на второй этаж. Рядом остановка, около которой трамваю приходится немало вилять.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9575.jpg'])
+
+@ru
+  <p>Домики.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9578.jpg'])
+
+@ru
+  <p>Развлекалово с утками.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9580.jpg'])
+
+@ru
+  <p>На удивление в парке есть мост-переход.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9585.jpg'])
+
+@ru
+  <p>Фасад.</p>
+@en
+  <p>Facade.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9588.jpg'])
+
+@ru
+  <p>К концу рабочего дня площади в центре вымирают.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9594.jpg'])
+
+@ru
+  <p>В коллекцию к проходам, которые показывали карты maps.me.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9596.jpg'])
+
+@ru
+  <p>Проход был в этом здании. Открыт и полезен.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9598.jpg'])
+
+@ru
+  <p>Велкопоповицкий Козел и козловня.</p>
+@en
+  <p>Velkopopovický Kozel.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9602.jpg'])
+
+@ru
+  <p>Первые нотки заката в отражении от стеклянного здания.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9603.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9607.jpg'])
+
+@ru
+  <p>Обратный путь к вокзалу через жилые районы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9619.jpg'])
+
+@ru
+  <p>Совсем рядом можно искупнуться.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9616.jpg'])
+
+@ru
+  <p>Дома простые и с чистыми фасадами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9620.jpg'])
+
+@ru
+  <p>И разноцветные.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9621.jpg'])
+
+@ru
+  <p>Еще цветные дома.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9632.jpg'])
+
+@ru
+  <p>Дом с балконами только по центру. Перед главным входом баскетбольная площадка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9623.jpg'])
+
+@ru
+  <p>Дома на расстоянии друг от друга, присутствует зелень, но мало тени.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9625.jpg'])
+
+@ru
+  <p>Зато есть вода.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9636.jpg',
+  'IMG_9628.jpg',
+  'IMG_9618.jpg',
+]])
+
+@ru
+  <p>Детская площадка в зелени.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9641.jpg'])
+
+@ru
+  <p>Улица напоминает Питер.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9629.jpg'])
+
+@ru
+  <p>Главный железнодорожный вокзал днем.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9557.jpg'])
+
+@ru
+  <p>Он же вечером.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9644.jpg'])
+
+@ru
+  <p>Около него высотка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9645.jpg'])
+
+@ru
+  <p>И многоэтажки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9648.jpg'])
+
+@ru
+  <p>Трамвайные пути в зелени.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9646.jpg'])
+
+@ru
+  <p>Совсем стемнело. Тут начинается самое интересное.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9649.jpg'])
+
+@ru
+  <p>Пора двигать на платформу на поздний поезд до Праги. Но он опаздывает, поэтому появляется время осмотреться. Современный вагон.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9651.jpg'])
+
+@ru
+  <p>У машиниста совсем маленькая кабинка. И входит он через вагон, а не с платформы.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9655.jpg'])
+
+@ru
+  <p>Поезда до столицы все еще нет. Так-так-так, а что это слева?</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9659.jpg'])
+
+@ru
+  <p>Паровоз в естественной среде!</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9665.jpg'])
+
+@ru
+  <p>Он же сбоку. Дымил он долго, вокруг него колдовало не менее пяти человек.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_9664.jpg'])
+
+@ru
+  <p>Вагоны к нему прицеплены соответствующей эпохи.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9666.jpg',
+  'IMG_9667.jpg',
+  'IMG_9668.jpg',
+]])
+
+@ru
+  <p>Но тут подошел мой поезд. В Чехии есть частные ж/д компании с привлекательными тарифами и услугами. ЛеоЭкспресс как раз относится к одной из них. В вагоне бесплатно давали некий газированный апельсиновый напиток, а за деньги можно было перекусить снэками и получить другие горячие и прохладительные напитки. В тоннелях в поезде закладывает уши, как в <a class="link" href="/life/countries/japan">Японии</a>. Причем даже при движении всего 100 км/ч. Сам ЛеоЭкспресс до Праги приличную часть пути держит 160 км/ч.</p>
+@endlang
+@endsection
