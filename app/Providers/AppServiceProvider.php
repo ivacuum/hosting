552ad37a2603bd0d758aa8e3fc8258c84d5ec786
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-13 15:12:31 +0300

    Все возможные классы

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,5 @@
 <?php namespace App\Providers;
 
-use App\News;
-use App\Photo;
-use App\Torrent;
-use App\Trip;
-use App\User;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,11 +8,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Relation::morphMap([
-            'News' => News::class,
-            'Trip' => Trip::class,
-            'User' => User::class,
-            'Photo' => Photo::class,
-            'Torrent' => Torrent::class,
+            'Gig' => \App\Gig::class,
+            'Tag' => \App\Tag::class,
+            'City' => \App\City::class,
+            'File' => \App\File::class,
+            'News' => \App\News::class,
+            'Page' => \App\Page::class,
+            'Trip' => \App\Trip::class,
+            'User' => \App\User::class,
+            'Image' => \App\Image::class,
+            'Photo' => \App\Photo::class,
+            'Artist' => \App\Artist::class,
+            'Client' => \App\Client::class,
+            'Domain' => \App\Domain::class,
+            'Server' => \App\Server::class,
+            'Comment' => \App\Comment::class,
+            'Country' => \App\Country::class,
+            'Torrent' => \App\Torrent::class,
+            'YandexUser' => \App\YandexUser::class,
+            'ChatMessage' => \App\ChatMessage::class,
+            'ExternalIdentity' => \App\ExternalIdentity::class,
         ]);
     }
 }
