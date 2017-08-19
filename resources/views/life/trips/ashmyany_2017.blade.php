e42d3af4f6422cc155db014d2ceffd34e4bd87fd
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-19 16:05:35 +0300

    Перевод заметки

--- resources/views/life/trips/ashmyany_2017.blade.php
+++ resources/views/life/trips/ashmyany_2017.blade.php
@@ -2,22 +2,36 @@
 
 @section('content')
 @ru
+  <p>Небольшой городок практически на границе с Литвой. Близкое расположение к федеральной трассе позволяет поймать попутку как в Минск, так и в Вильнюс.</p>
+@en
+  <p>A small town near the border with Lithuania. Close location to the federal highway makes it easy to catch a ride to both Minsk and Vilnius.</p>
+@endlang
+
+@ru
   <p>Автовокзал.</p>
+@en
+  <p>Bus terminal.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3092.jpg'])
 
 @ru
   <p>Ведущая к нему улица с низкоэтажной застройкой.</p>
+@en
+  <p>The street leading to terminal is full of low-rise buildings.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3101.jpg'])
 
 @ru
   <p>Контейнер для бутылок рядом с остановкой.</p>
+@en
+  <p>A bin for bottles near the bus stop.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3088.jpg'])
 
 @ru
   <p>Почтовые ящики с прорезями по диагонали.</p>
+@en
+  <p>Mailboxes with diagonal slots.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3094.jpg'])
 
@@ -30,21 +44,29 @@
 
 @ru
   <p>Возвращающий в детство пейзаж.</p>
+@en
+  <p>This building reminds me the childhood time.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3102.jpg'])
 
 @ru
   <p>Каково идти с местным жителем по городку с населением менее 20 тысяч человек? С ним каждые пять минут кто-нибудь будет здороваться.</p>
+@en
+  <p>What is it like to go with a local in a town with a population of less than 20 thousand people? Every five minutes someone greets him.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3091.jpg'])
 
 @ru
   <p>Режим работы на местном языке.</p>
+@en
+  <p>Opening hours in belarusian.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3104.jpg'])
 
 @ru
   <p>Для каждого сотового оператора свой номер для связи.</p>
+@en
+  <p>There is dedicated phone number for each mobile operator to reduce the cost of the call.</p>
 @endlang
 @include('tpl.pic-2x', ['pic' => 'IMG_3107.jpg'])
 @endsection
