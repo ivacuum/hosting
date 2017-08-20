d78b94cd41ce10c21c3d7d492daba914695e1adb
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-21 00:37:43 +0300

    Берлин

---
+++ resources/views/life/trips/berlin_2017_05.blade.php
@@ -0,0 +1,472 @@
+@extends('life.trips.base')
+
+{{--
+В Германии тоже желтые цветы вдоль трасс, как в Чехии.
+Под урны-пепельницы подставляют пакет и открывают дно для опустошения.
+--}}
+
+@section('content')
+@ru
+  <p>Надвигался первый концерт нового тура Раммштайна — 20 мая в Исландии. Очередная попытка выловить их перед репетицией. Предыдущие репетиции были в перед туром 2016 года в мае — совпали с датами моего исследования <a class="link" href="/life/barcelona.2016">Барселоны</a>. Тогда выиграть в розыгрыше билетов не удалось.</p>
+  <p>Что из себя представляет розыгрыш? Примерно за неделю до первой репетиции для членов фан-клуба на закрытом форуме делается объявление. Желающие пишут, что хотят попасть. За пару дней до шоу озвучивают случайных победителей. Им разрешается провести с собой на мероприятие любого компаньона (+1).</p>
+  <p>Так как собственный евротур начался с Чехии 9 мая, то визит в Берлин был спланирован заранее при неизвестной ситуации с репетицией. Отрезка с 16 по 19 мая должно было быть достаточно. Но группа решила свалить в Исландию за недельку до первого шоу и обойтись без репетиций.</p>
+@endlang
+
+@ru
+  <p>Автобус из Праги делает остановку на станции Зюдкройц. Конечная на главном вокзале, но если нужно не непосредственно к нему, то логичней как можно скорей пересесть на с-бан (электричку).</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0283.jpg'])
+
+@ru
+  <p>Плакат о строительных работах между Бернау и Берлином.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0287.jpg'])
+
+@ru
+  <p>Железнодорожные станции.</p>
+@en
+  <p>Railway stations.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0288.jpg',
+  'IMG_0481.jpg',
+  'IMG_0483.jpg',
+  'IMG_0484.jpg',
+]])
+
+@ru
+  <p>Свеженький Скайлайн.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0308.jpg',
+  'IMG_0309.jpg',
+]])
+
+@ru
+  <p>Футбольное поле, окруженное домами.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0311.jpg'])
+
+@ru
+  <p>Катеринка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0312.jpg'])
+
+@ru
+  <p>Внутренний двор арендованного жилья. С хозяином находились в разных комнатах одной квартиры, но за три дня ни разу не пересеклись вживую. Хотя по шуму ходьбы и воды по вечерам было ясно, что мы оба дома. Днем же он, очевидно, был на работе. То есть в немалой части случаев аренды одной лишь комнаты можно долго находиться в квартире одному. В той же <a class="link" href="/life/prague.2017.05">Праге</a> (за неделю до Берлина) хозяин появился дома только на четвертый день моего пребывания.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0313.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0314.jpg'])
+
+@ru
+  <p>Балконы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0315.jpg',
+  'IMG_0316.jpg',
+  'IMG_0351.jpg',
+  'IMG_0295.jpg',
+  'IMG_0339.jpg',
+  'IMG_0341.jpg',
+  'IMG_0373.jpg',
+  'IMG_0477.jpg',
+]])
+
+@ru
+  <p>Жилые районы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0317.jpg',
+  'IMG_0318.jpg',
+  'IMG_0319.jpg',
+  'IMG_0320.jpg',
+  'IMG_0322.jpg',
+  'IMG_0323.jpg',
+  'IMG_0336.jpg',
+  'IMG_0338.jpg',
+  'IMG_0348.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0296.jpg',
+  'IMG_0343.jpg',
+  'IMG_0346.jpg',
+  'IMG_0361.jpg',
+  'IMG_0372.jpg',
+  'IMG_0493.jpg',
+  'IMG_0510.jpg',
+  'IMG_0517.jpg',
+  'IMG_0522.jpg',
+  'IMG_0547.jpg',
+  'IMG_0558.jpg',
+]])
+
+@ru
+  <p>Арена Велодром.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0325.jpg'])
+
+@ru
+  <p>Она так ловко расположена в углублении, что ее не видно, пока не подойдешь совсем близко.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0326.jpg'])
+
+@ru
+  <p>Зигзагообразный подъем. Школьники практикуют на нем подтягивания в +28&deg;C.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0328.jpg'])
+
+@ru
+  <p>Трамвайные пути.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0330.jpg'])
+
+@ru
+  <p>Трамвайная остановка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0335.jpg'])
+
+@ru
+  <p>В Берлине среда.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0331.jpg',
+  'IMG_0334.jpg',
+  'IMG_0350.jpg',
+  'IMG_0376.jpg',
+  'IMG_0377.jpg',
+  'IMG_0378.jpg',
+  'IMG_0379.jpg',
+  'IMG_0381.jpg',
+  'IMG_0383.jpg',
+  'IMG_0390.jpg',
+  'IMG_0398.jpg',
+]])
+
+@ru
+  <p>Подъезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0337.jpg'])
+
+@ru
+  <p>Клумба в лавке.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0347.jpg'])
+
+@ru
+  <p>Железная дорога и поезда.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0353.jpg',
+  'IMG_0354.jpg',
+  'IMG_0355.jpg',
+  'IMG_0530.jpg',
+  'IMG_0292.jpg',
+]])
+
+@ru
+  <p>Стройка. Интересно как все тут будет выглядеть через пару лет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0356.jpg'])
+
+@ru
+  <p>Масштабы велодвижения.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0357.jpg'])
+
+@ru
+  <p>А наверху идет поезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0358.jpg'])
+
+@ru
+  <p>Периодически на перекрестках больше велосипедистов, нежели машин.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0365.jpg'])
+
+@ru
+  <p>Иногда меньше. Но в целом велосипеды очень популярны.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0305.jpg'])
+
+@ru
+  <p>Логово Юниверсал.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0362.jpg'])
+
+@ru
+  <p>Путь под железной дорогой.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0367.jpg'])
+
+@ru
+  <p>Здания вдоль набережной.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0368.jpg'])
+
+@ru
+  <p>Дом с огромными окнами. И в основном занавешенными.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0374.jpg'])
+
+@ru
+  <p>Еще жилой район.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0380.jpg',
+  'IMG_0382.jpg',
+  'IMG_0386.jpg',
+  'IMG_0388.jpg',
+  'IMG_0402.jpg',
+  'IMG_0403.jpg',
+  'IMG_0404.jpg',
+  'IMG_0405.jpg',
+]])
+
+@ru
+  <p>Товарищ рисует на берегу.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0394.jpg'])
+
+@ru
+  <p>Скамейки на набережных.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0389.jpg',
+  'IMG_0395.jpg',
+]])
+
+@ru
+  <p>Слегка закругленный дом.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0399.jpg'])
+
+@ru
+  <p>Потерялся кот. Вы знаете куда звонить.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0401.jpg'])
+
+@ru
+  <p>По-разному выпирающие балконы и хитрый водосток.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0471.jpg'])
+
+@ru
+  <p>На домах нет подписей улиц, только номера. Названия улиц на табличках на перекрестках.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0474.jpg',
+  'IMG_0475.jpg',
+  'IMG_0521.jpg',
+]])
+
+@ru
+  <p>В электричке элементарно провезти велик.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0482.jpg'])
+
+@ru
+  <p>Коих тут достаточно много.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0487.jpg'])
+
+@ru
+  <p>Особенно у железнодорожных станций.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0489.jpg'])
+
+@ru
+  <p>Как описать Берлин одним словом? Лес! Даже в самый жаркий день по теньку легко преодолеть больше половины пути.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0494.jpg',
+  'IMG_0498.jpg',
+  'IMG_0500.jpg',
+  'IMG_0534.jpg',
+  'IMG_0476.jpg',
+  'IMG_0531.jpg',
+  'IMG_0537.jpg',
+  'IMG_0538.jpg',
+  'IMG_0539.jpg',
+  'IMG_0552.jpg',
+  'IMG_0564.jpg',
+]])
+
+@ru
+  <p>Трамвай в стиле Депеш Мод.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0495.jpg'])
+
+@ru
+  <p>Прямо перед подъездом растет крыжовник.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0496.jpg'])
+
+@ru
+  <p>Причем растет в немалых количествах.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0497.jpg'])
+
+@ru
+  <p>Подъезд.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0501.jpg'])
+
+@ru
+  <p>Самолеты летают.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0503.jpg'])
+
+@ru
+  <p>До обоих аэропортов можно добраться на электричке. До Тегеля и вовсе за 20 минут на автобусе из центра.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0540.jpg'])
+
+@ru
+  <p>Нашелся свой район. Тут и вокзал, и суши, и аптека. Даже номера у машины ВАК.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0514.jpg',
+  'IMG_0508.jpg',
+  'IMG_0515.jpg',
+  'IMG_0516.jpg',
+]])
+
+@ru
+  <p>Двухэтажная велопарковка.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0511.jpg'])
+
+@ru
+  <p>В продаже молочный рис.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0512.jpg'])
+
+@ru
+  <p>Природоохранная зона.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0523.jpg'])
+
+@ru
+  <p>Футбольные ворота на колесиках. Легко можно поставить в любое место поля.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0536.jpg'])
+
+@ru
+  <p>В Берлине четверг.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0542.jpg',
+  'IMG_0543.jpg',
+  'IMG_0544.jpg',
+]])
+
+@ru
+  <p>Поезд дальше не идет.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0545.jpg'])
+
+@ru
+  <p>Пример приятного граффити.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0548.jpg'])
+
+@ru
+  <p>Пример неприятного граффити.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0407.jpg'])
+
+@ru
+  <p>Еще один.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0352.jpg'])
+
+@ru
+  <p>Автобусная остановка.</p>
+@en
+  <p>Bus stop.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0549.jpg'])
+
+@ru
+  <p>Дворы.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0550.jpg',
+  'IMG_0551.jpg',
+  'IMG_0553.jpg',
+]])
+
+@ru
+  <p>Трамваи по маршруту M10 работают 24/7.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0557.jpg'])
+
+@ru
+  <p>По субботам метро работает круглосуточно, по пятницам перерыв всего полтора часа.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0344.jpg'])
+
+@ru
+  <p>Автомат по продаже билетов есть прямо в трамвае. Удобно, что можно купить после посадки.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0560.jpg'])
+
+@ru
+  <p>Сам трамвай низкопольный. Закатывать коляски проще некуда.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0561.jpg'])
+
+@ru
+  <p>Автомат по продаже билетов на станции с-бана.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0527.jpg'])
+
+@ru
+  <p>Straßenschäden — осторожно, поврежденная дорога. Gehwegschäden — поврежденный тротуар.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0565.jpg'])
+
+@ru
+  <p>Трептов-парк, аллея Пушкина и советский военный мемориал.</p>
+@endlang
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0566.jpg',
+  'IMG_0567.jpg',
+  'IMG_0569.jpg',
+  'IMG_0570.jpg',
+  'IMG_0571.jpg',
+  'IMG_0573.jpg',
+]])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0568.jpg'])
+
+@ru
+  <p>Напольная навигация. Желтая линия — в аэропорт, красная — на поезд, зеленая — на с-бан.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0576.jpg'])
+
+@ru
+  <p>Аэропорт Шёнефельд. Ехать на с-бане до него оказалось дольше, чем лететь до Копенгагена. Есть даже смысл рассмотреть автобус в качестве альтернативы до столицы Дании.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0577.jpg'])
+
+@ru
+  <p>В туалете аэропорта можно купить презервативы и не только.</p>
+@endlang
+@include('tpl.pic-2x', ['pic' => 'IMG_0579.jpg'])
+@endsection
