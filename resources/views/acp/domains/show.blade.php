004aae982ab95d4accac5afcaa6526c204b6cf74
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-24 02:05:47 +0300

    Внешний вид таблицы

--- resources/views/acp/domains/show.blade.php
+++ resources/views/acp/domains/show.blade.php
@@ -28,7 +28,7 @@
 <table class="table-stats">
   @if ($model->alias)
     <tr>
-      <td><strong>Алиас</strong></td>
+      <td class="text-right font-bold">Алиас</td>
       <td>
         <a href="{{ path("$self@show", $model->alias) }}">{{ $model->alias->domain }}</a>
       </td>
@@ -36,7 +36,7 @@
   @endif
   @if (sizeof($model->aliases))
     <tr>
-      <td><strong>Алиасы</strong></td>
+      <td class="text-right font-bold">Алиасы</td>
       <td>
         @foreach ($model->aliases as $alias)
           <a href="{{ path("$self@show", $alias) }}">{{ $alias->domain }}</a>
@@ -45,7 +45,7 @@
     </tr>
   @endif
   <tr>
-    <td><strong>Клиент</strong></td>
+    <td class="text-right font-bold">Клиент</td>
     <td>
       <a href="{{ path('Acp\Clients@show', $model->client) }}">
         {{ $model->client->name }}
@@ -54,7 +54,7 @@
   </tr>
   @if ($model->yandex_user_id)
     <tr>
-      <td><strong>Яндекс</strong></td>
+      <td class="text-right font-bold">Яндекс</td>
       <td>
         <a href="{{ path('Acp\YandexUsers@show', $model->yandexUser->id) }}">
           {{ $model->yandexUser->account }}
@@ -64,22 +64,22 @@
   @endif
   @if ($model->ipv4)
     <tr>
-      <td><strong>IPv4</strong></td>
+      <td class="text-right font-bold">IPv4</td>
       <td>{{ $model->ipv4 }}</td>
     </tr>
   @endif
   @if ($model->ipv6)
     <tr>
-      <td><strong>IPv6</strong></td>
+      <td class="text-right font-bold">IPv6</td>
       <td>{{ $model->ipv6 }}</td>
     </tr>
   @endif
   <tr>
-    <td><strong>MX</strong></td>
+    <td class="text-right font-bold">MX</td>
     <td>{{ $model->mx }}</td>
   </tr>
   <tr>
-    <td><strong>NS</strong></td>
+    <td class="text-right font-bold">NS</td>
     <td>{{ $model->ns }}</td>
   </tr>
 </table>
