f828a033934f27feb299c7de55b4107fd570f54d
Sergey Pankov <vacuum@ivacuum.ru>
2017-08-24 14:03:25 +0300

    Правка отступов

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="gallery-flex">
   @foreach ($photos as $photo)
-    <div class="gallery-image mb-4">
+    <div class="gallery-image mb-1 mb-sm-4 mb-md-0 mb-lg-4">
       <a class="screenshot-link" href="{{ $photo->www() }}">
         <img class="image-200 screenshot" src="{{ $photo->thumbnailUrl() }}">
       </a>
