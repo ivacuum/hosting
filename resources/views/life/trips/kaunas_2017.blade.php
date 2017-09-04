88d65e1c2656a4746dcebc2044da8513f91a5b53
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-05 02:16:49 +0300

    Каунас

---
+++ resources/views/life/trips/kaunas_2017.blade.php
@@ -0,0 +1,280 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Долететь до Каунаса из Копенгагена можно <a class="link" href="copenhagen.2017#pricy-meal">по цене обеда</a> в столице Дании. А из Литвы уже недорого добраться до России, особенно нестеровским поездом.</p>
+@endru
+
+@ru
+  <p>Посадка в аэропорту Каунаса.</p>
+@en
+  <p>Landing.</p>
+@endru
+<youtube title="Landing in Kaunas" v="cC1J1EKUHDo"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_1223.jpg'])
+
+@ru
+  <p>Ангар РайанЭйр.</p>
+@en
+  <p>RyanAir's hangar.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1226.jpg'])
+
+@ru
+  <p>По прилету автобуса до терминала тоже не было, как и при <a class="link" href="copenhagen.2017#no-bus">посадке в Копенгагене</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1225.jpg'])
+
+@ru
+  <p>Шли пешком прямо по аэродрому.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1227.jpg'])
+
+@ru
+  <p>Уличная табличка.</p>
+@en
+  <p>Street sign.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1229.jpg'])
+
+@ru
+  <p>Знакомые по России пейзажи — дворы. В одном из трех довелось пожить. Большая угловая ванна позволила почувствовать себя как дома. Плюс русский язык в ходу. Плюс знакомый подход к питанию. Если остаться, то можно и не заметить разницы с Калугой.</p>
+@en
+  <p>Yards.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1234.jpg',
+  'IMG_1235.jpg',
+  'IMG_1259.jpg',
+]])
+
+@ru
+  <p>Газель подсказывает, что родина уже рядом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1338.jpg'])
+
+@ru
+  <p>Горшки перед торговым центром Акрополис.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1244.jpg'])
+
+@ru
+  <p>Внутри торгового центра около ресторанного дворика расположился каток.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1238.jpg'])
+
+@ru
+  <p>По расписанию площадку заняли хоккеисты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1241.jpg'])
+
+@ru
+  <p>Сам ресторанный дворик.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1336.jpg'])
+
+@ru
+  <p>Многоэтажная парковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1245.jpg'])
+
+@ru
+  <p>Снос здания похож на последствия боевых действий.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1246.jpg'])
+
+@ru
+  <p>Пешеходный мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1252.jpg'])
+
+@ru
+  <p>Под ним набережная.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1247.jpg'])
+
+@ru
+  <p>К набережной суровые спуски по плитам.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1253.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1230.jpg',
+  'IMG_1231.jpg',
+  'IMG_1232.jpg',
+  'IMG_1233.jpg',
+  'IMG_1317.jpg',
+  'IMG_1318.jpg',
+  'IMG_1322.jpg',
+  'IMG_1320.jpg',
+]])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1228.jpg',
+  'IMG_1256.jpg',
+  'IMG_1312.jpg',
+  'IMG_1327.jpg',
+  'IMG_1330.jpg',
+]])
+
+@ru
+  <p>Пешеходные зоны.</p>
+@en
+  <p>Pedestrian zones.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1258.jpg',
+  'IMG_1260.jpg',
+  'IMG_1262.jpg',
+  'IMG_1263.jpg',
+  'IMG_1267.jpg',
+  'IMG_1276.jpg',
+]])
+
+@ru
+  <p>Найдено сердце.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1268.jpg'])
+
+@ru
+  <p>Никто не терял?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1269.jpg'])
+
+@ru
+  <p>Фасад.</p>
+@en
+  <p>Facade.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1270.jpg'])
+
+@ru
+  <p>Почта.</p>
+@en
+  <p>Post office.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1272.jpg'])
+
+@ru
+  <p>Троллейбус спереди, сбоку и сзади.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1279.jpg',
+  'IMG_1274.jpg',
+  'IMG_1275.jpg',
+]])
+
+@ru
+  <p>Йога или йода? Слева фасад избавили от грязных надписей, справа только предстоит это сделать.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1277.jpg'])
+
+@ru
+  <p>Подземный переход к старому городу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1278.jpg'])
+
+@ru
+  <p>Напитки в магазинчике.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1281.jpg'])
+
+@ru
+  <p>Люк.</p>
+@en
+  <p>Manhole.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1282.jpg'])
+
+@ru
+  <p>Городской велопрокат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1287.jpg'])
+
+@ru
+  <p>В старом городе.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1286.jpg',
+  'IMG_1289.jpg',
+  'IMG_1293.jpg',
+  'IMG_1309.jpg',
+  'IMG_1313.jpg',
+]])
+
+@ru
+  <p>Рядом с рекой.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1288.jpg',
+  'IMG_1291.jpg',
+  'IMG_1294.jpg',
+  'IMG_1303.jpg',
+  'IMG_1306.jpg',
+  'IMG_1307.jpg',
+]])
+
+@ru
+  <p>Красивое граффити на стене и QR-код на колонне.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1314.jpg'])
+
+@ru
+  <p>Подушки в цвет литовского флага.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1316.jpg'])
+
+@ru
+  <p>Номер дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1319.jpg'])
+
+@ru
+  <p>Паркомат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1321.jpg'])
+
+@ru
+  <p>Котейка задумался и взгрустнул.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1323.jpg',
+  'IMG_1325.jpg',
+]])
+
+@ru
+  <p>Ножка у двери в подъезд. Органичнее традиционного валяющегося кирпича.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1329.jpg'])
+
+@ru
+  <p>Еще котейка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1331.jpg'])
+
+@ru
+  <p>Где настоящее окно и где нарисованное?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1333.jpg'])
+
+@ru
+  <p>В лучших традициях добрался до вокзала за 15 минут до отправления поезда. Предстояло еще отстоять длинную очередь в одну кассу. Оказалось, что в поезде тоже можно купить билет, но тогда он будет на 1,45 евро дороже.</p>
+@endru
+
+@ru
+  <p>Следующая остановка — Вильнюс.</p>
+@en
+  <p>Next stop — Vilnius.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1342.jpg'])
+@endsection
