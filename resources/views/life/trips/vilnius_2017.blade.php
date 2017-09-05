43f6079c192b7734b3ee3e52d4ed0c56a593fac0
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-05 04:22:33 +0300

    Вильнюс

---
+++ resources/views/life/trips/vilnius_2017.blade.php
@@ -0,0 +1,228 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Из Каунаса в Вильнюс за час доставляет двухэтажный экспресс. Последний город майской вылазки на Запад.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1346.jpg'])
+
+@ru
+  <p>Площадь главного вокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1347.jpg'])
+
+@ru
+  <p>У светофора для пешеходов есть желтый сигнал.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1350.jpg',
+  'IMG_1351.jpg',
+]])
+
+@ru
+  <p>Домофон.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1352.jpg'])
+
+@ru
+  <p>Коллекция балконов.</p>
+@en
+  <p>Balconies.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1353.jpg',
+  'IMG_1389.jpg',
+  'IMG_1403.jpg',
+  'IMG_1402.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1349.jpg',
+  'IMG_1354.jpg',
+  'IMG_1355.jpg',
+  'IMG_1367.jpg',
+  'IMG_1384.jpg',
+  'IMG_1386.jpg',
+  'IMG_1387.jpg',
+  'IMG_1393.jpg',
+  'IMG_1394.jpg',
+]])
+
+@ru
+  <p>Уличная табличка.</p>
+@en
+  <p>Street sign.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1356.jpg'])
+
+@ru
+  <p>Город довольно рельефный.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1357.jpg'])
+
+@ru
+  <p>Родные приемы парковки на тротуарах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1359.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1363.jpg',
+  'IMG_1364.jpg',
+  'IMG_1388.jpg',
+]])
+
+@ru
+  <p>Люк.</p>
+@en
+  <p>Manhole.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1366.jpg'])
+
+@ru
+  <p>Пешеходный переход вровень с проезжей частью.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1368.jpg'])
+
+@ru
+  <p>Множество кафе выставляет свои столики на улицу.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1371.jpg',
+  'IMG_1381.jpg',
+  'IMG_1390.jpg',
+]])
+
+@ru
+  <p>Здесь вовсе перекрывая проезжую часть.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1377.jpg'])
+
+@ru
+  <p>В режиме работы вместо дней недели — римские цифры.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1372.jpg'])
+
+@ru
+  <p>Они же в плане организации движения.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1396.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1374.jpg',
+  'IMG_1401.jpg',
+  'IMG_1410.jpg',
+]])
+
+@ru
+  <p>Улица ремонтируется, асфальт укладывается.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1376.jpg'])
+
+@ru
+  <p>Пример как в одном знаке уместить ограничения для трех типов транспортных средств: самоходных мотоциклов, мотоциклов с людьми и автомобилей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1378.jpg'])
+
+@ru
+  <p>Почта.</p>
+@en
+  <p>Post office.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1379.jpg'])
+
+@ru
+  <p>Городской велопрокат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1382.jpg'])
+
+@ru
+  <p>На знаке название остановки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1383.jpg'])
+
+@ru
+  <p>Воздушные шары украсят любой пейзаж.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1391.jpg'])
+
+@ru
+  <p>Элмо в магазине.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1392.jpg'])
+
+@ru
+  <p>Двор.</p>
+@en
+  <p>Yard.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1395.jpg'])
+
+@ru
+  <p>Длинный троллейбус и его младший брат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1398.jpg'])
+
+@ru
+  <p>Автобус с гармошкой. Проезд всего один евро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1399.jpg'])
+
+@ru
+  <p>Где настоящий дом номер шесть? Кто самозванец?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1400.jpg'])
+
+@ru
+  <p>Круглое расписание общественного транспорта на столбе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1406.jpg'])
+
+@ru
+  <p>Подъезд. При заселении не смог вспомнить что бронировал: комнату или целую квартиру. Немудрено запутаться на тринадцатом городе и пятом заселении в течение двух недель.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1407.jpg',
+  'IMG_1408.jpg',
+]])
+
+@ru
+  <p>Есть свет — и нет света.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1417.jpg',
+  'IMG_1418.jpg',
+]])
+
+@ru
+  <p>Пора заканчивать тур и возвращаться домой. Опробована халява в виде нестеровского поезда — можно брать билет не Вильнюс—Москва, а Нестеров—Москва и наоборот. Хоть это и дольше по расстоянию, но втрое дешевле — всего 1 200 ₽ с сентября по май.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1425.jpg'])
+
+@ru
+  <p>С нами в Москву ехал почтовый вагон.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1431.jpg'])
+
+@ru
+  <p>Электрички до Калуги постепенно улучшают.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1432.jpg'])
+
+@ru
+  <p>Дожили до хороших сидений.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1434.jpg'])
+@endsection
