3d3f1365c1a94a35d38594d5f9f8b535341f2905
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-10 21:11:40 +0300

    Кельн

---
+++ resources/views/life/trips/cologne_2017.blade.php
@@ -0,0 +1,22 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Подъезжаем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2196.jpg'])
+
+@ru
+  <p>Проезжаем.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2198.jpg',
+  'IMG_2203.jpg',
+  'IMG_2206.jpg',
+  'IMG_2208.jpg',
+  'IMG_2209.jpg',
+  'IMG_2211.jpg',
+  'IMG_2215.jpg',
+  'IMG_2218.jpg',
+]])
+@endsection
