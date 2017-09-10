05ef01d92e4399a2cd3b66655a1cf672567e6354
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-11 01:33:47 +0300

    Дрезден

---
+++ resources/views/life/trips/dresden_2017_07.blade.php
@@ -0,0 +1,189 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Едва заметная скульптура на балконе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1966.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@en
+  <p>Pedestrian cross.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1968.jpg'])
+
+@ru
+  <p>Туристов подвезли.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1969.jpg'])
+
+@ru
+  <p>Улицы старого города.</p>
+@en
+  <p>Streets of the old city.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1974.jpg',
+  'IMG_1973.jpg',
+]])
+
+@ru
+  <p>Дворец.</p>
+@en
+  <p>Palace.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1977.jpg'])
+
+@ru
+  <p>Цвингер.</p>
+@en
+  <p>Zwinger.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1975.jpg',
+  'IMG_1979.jpg',
+  'IMG_1981.jpg',
+]])
+
+@ru
+  <p>Денек жаркий. Есть где охладиться в тени деревьев.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1983.jpg'])
+
+@ru
+  <p>Есть где охладиться в воде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1984.jpg'])
+
+@ru
+  <p>Есть где попечься.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1986.jpg'])
+
+@ru
+  <p>И снова передохнуть в тени.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2021.jpg'])
+
+@ru
+  <p>Дорожный знак жилой зоны.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1985.jpg'])
+
+@ru
+  <p>Арки.</p>
+@en
+  <p>Archs.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1988.jpg'])
+
+@ru
+  <p>Уличный туалет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1989.jpg'])
+
+@ru
+  <p>Трамвайные пути посередине улицы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1996.jpg',
+  'IMG_1997.jpg',
+]])
+
+@ru
+  <p>Трамвай.</p>
+@en
+  <p>Tram.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1967.jpg'])
+
+@ru
+  <p>Приятное для посиделок место.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1998.jpg'])
+
+@ru
+  <p>Площадь с множеством уличных кафе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2004.jpg'])
+
+@ru
+  <p>Внутри церкви.</p>
+@en
+  <p>Inside the church.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2006.jpg'])
+
+@ru
+  <p>Напольная карта.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2009.jpg'])
+
+@ru
+  <p>Пешеходная зона.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2010.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2011.jpg'])
+
+@ru
+  <p>Рисунок во всю стену.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2013.jpg'])
+
+@ru
+  <p>Уличные таблички.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2014.jpg'])
+
+@ru
+  <p>Прямо за лошадьми мешок для какашек.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2017.jpg'])
+
+@ru
+  <p>Часы на фасаде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1976.jpg'])
+
+@ru
+  <p>Часы рядом с вывеской магазина.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2012.jpg'])
+
+@ru
+  <p>Еще часы на фасаде. Плотное по застройке место, которое сложно уместить даже в вертикальный кадр.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2018.jpg'])
+
+@ru
+  <p>Люк канализационный.</p>
+@en
+  <p>Manhole.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1970.jpg'])
+
+@ru
+  <p>Люк Сатурна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2022.jpg'])
+
+@ru
+  <p>Люк Юпитера.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2023.jpg'])
+
+@ru
+  <p>Лавочки с видом на набережную.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2024.jpg',
+  'IMG_2025.jpg',
+]])
+@endsection
