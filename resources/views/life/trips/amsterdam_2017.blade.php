1405be0496cbe2f74aa0864b562f431b114798e3
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-13 06:01:29 +0300

    Амстердам

---
+++ resources/views/life/trips/amsterdam_2017.blade.php
@@ -0,0 +1,190 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Так крыше недолго стать белой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2630.jpg'])
+
+@ru
+  <p>У всех подряд траву не берем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2632.jpg'])
+
+@ru
+  <p>Люк.</p>
+@en
+  <p>Manhole.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2634.jpg'])
+
+@ru
+  <p>В поиске парковки. Следует обращать внимание не только на ее цену, но и интервал начисления этой суммы. Потому что могут написать парковка всего 2 евро, но их будут начислять, например, каждые 17 минут.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2639.jpg'])
+
+@ru
+  <p>Собак не выгуливать.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2690.jpg'])
+
+@ru
+  <p>По велодорожкам гоняют на скутерах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2692.jpg'])
+
+@ru
+  <p>И в колясках. Вот это отличное применение!</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2693.jpg'])
+
+@ru
+  <p>Зеленые пространства.</p>
+@en
+  <p>Green spaces.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2689.jpg',
+  'IMG_2691.jpg',
+  'IMG_2695.jpg',
+  'IMG_2696.jpg',
+]])
+
+@ru
+  <p>Улицы и каналы. Летом центр Амстердама жутко переполнен. Логичнее наведаться весной-осенью.</p>
+@en
+  <p>Streets and canals. Amsterdam's city center is overcrowded in summer. It's better to visit in spring or fall.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2697.jpg',
+  'IMG_2698.jpg',
+  'IMG_2700.jpg',
+  'IMG_2701.jpg',
+  'IMG_2702.jpg',
+  'IMG_2703.jpg',
+  'IMG_2704.jpg',
+  'IMG_2705.jpg',
+  'IMG_2706.jpg',
+  'IMG_2707.jpg',
+  'IMG_2708.jpg',
+  'IMG_2709.jpg',
+  'IMG_2710.jpg',
+  'IMG_2712.jpg',
+  'IMG_2732.jpg',
+]])
+
+@ru
+  <p>Велопарковка около вокзала.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2713.jpg',
+  'IMG_2714.jpg',
+  'IMG_2716.jpg',
+]])
+
+@ru
+  <p>Скоростные поезда на вокзале.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2718.jpg',
+  'IMG_2719.jpg',
+  'IMG_2720.jpg',
+]])
+
+@ru
+  <p>Интерьер главного железнодорожного вокзала.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2722.jpg',
+  'IMG_2723.jpg',
+]])
+
+@ru
+  <p>Ожидание водного такси.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2724.jpg'])
+
+@ru
+  <p>Масштабы велодвижения.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2726.jpg',
+  'IMG_2711.jpg',
+]])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2727.jpg'])
+
+@ru
+  <p>Разметка.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2728.jpg',
+  'IMG_2730.jpg',
+]])
+
+@ru
+  <p>Парковка не более двух недель.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2729.jpg'])
+
+@ru
+  <p>Афиша музыкальных фестивалей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2731.jpg'])
+
+@ru
+  <p>Район красных фонарей утром. На уборку и мытье улиц отправляется целая армия дворников с техникой. Уже к полудню все чистенькое.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2736.jpg',
+  'IMG_2737.jpg',
+  'IMG_2739.jpg',
+]])
+
+@ru
+  <p>До автовокзала всего одна станция на поезде за 2,30 евро через приложение, 3,30 евро наличными и 3,80 евро картой. Пешком далековато, а вот велик можно быстро отбить, если за счет него экономить на общественном транспорте. Еще в Голландии беда с приемом российских банковских карт — часто либо не принимают, либо берут дополнительную комиссию.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2742.jpg'])
+
+@ru
+  <p>Турникеты все время открыты. На них обладатели транспортных карт делают чек-ин и чек-аут. Остальные просто проходят.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2743.jpg'])
+
+@ru
+  <p>Платформа.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2747.jpg',
+  'IMG_2748.jpg',
+]])
+
+@ru
+  <p>Поезд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2749.jpg'])
+
+@ru
+  <p>Лилии у автовокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2751.jpg'])
+
+@ru
+  <p>Ограждение, чтобы ничего не падало на пути и поезда.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2752.jpg'])
+
+@ru
+  <p>Велопарковка у автовокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2753.jpg'])
+
+@ru
+  <p>Автобус в Ганновер после въезда в Германию полицейские тщательно осматривали на предмет перевозки запрещенки. Водитель Денис попался забавный, он увлекательно рассказывал типичные вещи о безопасности и устройстве автобуса. Время от времени пассажиры ему аплодировали и благодарили. Часть процесса удалось <a class="link" href="https://life.ivacuum.ru/audio/20170709.m4a">записать на аудио</a>.</p>
+@endru
+@endsection
