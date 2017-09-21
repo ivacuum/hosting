71555fb85fa3718f305ef72d997753e95c125334
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-21 08:13:23 +0300

    Прогресс по переводу

--- resources/views/life/trips/tokyo_2017_04_08-10.blade.php
+++ resources/views/life/trips/tokyo_2017_04_08-10.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 @ru
   <p>Рейс японской авиакомпанией JAL. Билеты удалось с горем пополам — с пятого раза — урвать по очень привлекательной цене (даже ниже Аэрофлота) еще во время нахождения на <a class="link" href="bali.2016">Бали</a>. С этого момента началась подготовка к поездке. Можно смело сказать, что к настоящему моменту приготовления были самые масштабные. Японцы хотят быть в курсе каждого дня пребывания в стране вплоть до контактов отеля.</p>
+@en
+  <p>Flight by Japanese airline—JAL. Tickets were bought while in <a class="link" href="bali.2016">Bali</a>. Preparation for the trip began from that moment. I can surely say by now that the preparations were the most comprehensive. The japanese want to be aware of every day of stay in the country—even accomodations' contacts and things you want to do in cities.</p>
+@endru
+
+@ru
   <p>Отдельное спасибо стоит сказать человеку, вдохновившему на поездку и устроившему большую ее часть. Спасибо!</p>
+@en
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3838.jpg'])
 
 @ru
   <p>Перелет на Боинге 787 (Дримлайнер). Затемнение окон. Всего пять положений. Слева самое темное, справа самое светлое. Снимки с разным уровнем яркости, чтобы можно было оценить разницу.</p>
+@en
+  <p>Flight on a Boeing 787 (Dreamliner). Windows use smart glass allowing to adjust five levels of sunlight and visibility. Level one is on the left, and level five is on the right. Pictures are with different exposure level, so it's easier to notice the difference.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_3840.jpg',
@@ -18,18 +26,34 @@
 
 @ru
   <p>Перелет около 10 часов, за это время кормят дважды. Но эти два раза так растянуты, что ощущение, что ешь чуть ли не весь полет. Поспать дольше пары часов не удалось. По предыдущему положительному опыту было вновь заказано кошерное питание. И здесь оно не подкачало. Однако, удивило, что бортпроводники спрашивали можно ли им самим разогреть кошерную пищу или я схожу и проконтролирую процесс. А также просили вскрыть упаковку с едой и убедиться, что там все ОК, что никаких неожиданностей среди блюд. Ага, понимать бы еще что там внутри!</p>
+@en
+  <p>Flight is about 10 hours long, you get two meals during it. But meals are so big, that it feels like the only thing you do the entire flight is eat. I couldn't sleep for more than a couple of hours. Kosher meal was ordered given the previous positive experience. And it was good once again. However, it was surprising that flight attendant asked if she could warm up my kosher meal by herself or I would go with her and control the process. I was also asked to open the meal box and make sure everything is OK inside. Yea, I wish I knew what's inside!</p>
+@endru
+
+@ru
   <p>За час-два до посадки всю еду повторили. Это снова, грубо говоря, пять блюд.</p>
+@en
+  <p>The second meal was given few hours before landing. It's roughly five courses once again.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3850.jpg'])
 
 @ru
   <p>На табло пишут, что перелет в Токио, хотя на самом деле он в город Нарита к востоку от столицы. До Токио еще ехать на общественном транспорте. Уже тут уйма вариантов: автобус или поезд, поезд JR или поезд Keisei, поезд-экспресс или электричка.</p>
+@en
+  <p>Scoreboard reports the flight is to Tokyo, but in fact it is to Narita—city to the east of the capital. There are lots of options how to get to Tokyo by public transport: bus or train, JR train or Keisei train, express train or regular train.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3852.jpg'])
 
 @ru
   <p>По счастливому стечению обстоятельств с начала 2017 года стало возможным подавать на туристическую визу самостоятельно. Если это делать в консульстве, то процедура бесплатная. Разве что нужно самому привозить и забирать паспорт. Если подавать через Пони Экспресс, то можно заказать доставку домой.</p>
+@en
+  <p>Russians need to get visa in advance. Prior to 2017 it was not possible without japanese travel agency. Fortunately, since the beginning of 2017 we can apply for a visa by ourselves. If you apply in japanese consulate in Moscow, visa costs you nothing. The only caveat is that you can't get your passport delivered to your home then.</p>
+@endru
+
+@ru
   <p>Набор документов стал повторять таковой для Шенгенской визы, но с парой отличий: дополнение в виде плана поездки и необязательность наличия медицинской страховки. В таблице ниже пример двух дней плана поездки. Да, заполнять все документы нужно на английском.</p>
+@en
+  <p>The application is now almost the same as for Schengen visa, but with a couple of differences: schedule of stay is mandatory, and medical insurance is optional. You can find an example of two days of schedule of stay in a table below.</p>
 @endru
 <table class="table-stats mb-3">
   <thead>
@@ -42,7 +66,7 @@
   </thead>
   <tbody>
   <tr>
-    <td>08 / 04 / 2017</td>
+    <td>08/04/2017</td>
     <td>
       <div>Flight JL 422 from Moscow.</div>
       <div>Arrival to Narita airport at 08:35</div>
@@ -58,7 +82,7 @@
     </td>
   </tr>
   <tr>
-    <td>09 / 04 / 2017</td>
+    <td>09/04/2017</td>
     <td>Tours in Tokyo, train to Nikko</td>
     <td>—</td>
     <td>—</td>
@@ -69,61 +93,91 @@
 
 @ru
   <p>Добро пожаловать в Японию — страну восходящего солнца! И рано заходящего. Серьезно — в семь вечера темно круглый год.</p>
+@en
+  <p>Welcome to Japan—the land of the rising sun. And early setting sun. Seriously, it's dark at 7PM all year round.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3856.jpg'])
 
 @ru
   <p>Аэропорт Нарита.</p>
+@en
+  <p>Narita airport.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3857.jpg'])
 
 @ru
   <p>Экспресс до Токио ¥3000. На момент написания для перевода в рубли цены удобно делились пополам.</p>
+@en
+  <p>Express train to Tokyo costs ¥3000.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3863.jpg'])
 
 @ru
   <p>Электричка до Токио ¥1000.</p>
+@en
+  <p>The regular train to Tokyo costs ¥1000.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3865.jpg'])
 
 @ru
   <p>Висящие поручни четко на своих местах. Верхние полки часто используются японцами, чтобы не держать сумки даже несколько станций.</p>
+@en
+  <p>Grab handles are locked to their places, so they look symmetrical. The upper shelves see an often use by japanese even if they are in for a few stops.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3867.jpg'])
 
 @ru
-  <p>Раз была выбрана дешевая электричка, то предстоит сделать две пересадки. Если есть интернет, то проблем с навигацией совсем не возникнет. Если нету, то желательно знать время отправления поездов или хотя бы нужное направление движения. Неважно на каком поезде доберетесь и по какому маршруту. Важно лишь где вы зашли и на какой станции вышли — между ними будет подсчитан оптимальный маршрут и списана соответствующая дальности сумма. Если недостаточно денег на транспортной карте или билете, то можно доплатить разницу сотруднику на выходе. Если денег совсем нет (забыли), то можно вернуться на исходную станцию и выйти там словно никуда и не ездили. Также можно пополнить транспортную карту вплоть до ¥20 000 в терминале на станции. Продуманы, что тут скажешь. У пассажира есть право на ошибку, и это радует.</p>
+  <p>Раз была выбрана дешевая электричка, то предстояло сделать две пересадки. Если есть интернет, то проблем с навигацией совсем не возникнет. Если нету, то желательно знать время отправления поездов или хотя бы нужное направление движения.</p>
+@en
+  <p>Since the regular train was picked, there are two changes en route to the hotel. If you have internet, there will be no problems with nagivation at all. If you don't have it, it's best to know the time of the departure or at least the direction.</p>
+@endru
+
+@ru
+  <p>Неважно на каком поезде доберетесь и по какому маршруту. Важно лишь где вы зашли и на какой станции вышли — между ними будет подсчитан оптимальный маршрут и списана соответствующая дальности сумма. Если недостаточно денег на транспортной карте или билете, то можно доплатить разницу сотруднику на выходе. Если денег совсем нет (забыли), то можно вернуться на исходную станцию и выйти там словно никуда и не ездили. Также можно пополнить транспортную карту вплоть до ¥20 000 в терминале на станции. Продуманы, что тут скажешь. У пассажира есть право на ошибку, и это радует.</p>
+@en
+  <p>In the end it doesn't even matter what train and what route you would take. There are two things that make up the cost of your ride: entrance and exit points. So you gonna be charged by the most optimal and the shortest route. If you have not enough money on your ticket or transport card, you can pay the difference to a man at the exit point. If you have no money at all (left a wallet at home, for example), you can ride back to the entrance point and exit like you didn't go anywhere. By the way transport card can be funded up to ¥20 000 right there at the station. Geniuses, what else can be said? Passenger has got a right to be wrong.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3868.jpg'])
 
 @ru
   <p>Все места входа в вагоны подписаны. Этот для состава из восьми вагонов.</p>
+@en
+  <p>All the boarding points to the cars are signed. This one is for a train of eight cars.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3869.jpg'])
 
 @ru
   <p>Рядом синий для другого состава.</p>
+@en
+  <p>Blue sign is for a different train.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3870.jpg'])
 
 @ru
   <p>Следующая пересадка. На столбе две схемы. На верхней можно заметить, что станции имеют оригинальное название, на латинице, а также простое вида А09. Нижняя схема — метро. Да, веток много. Еще и присутствуют разные ж/д компании. Ехать лучше какой-то одной, тогда будет минимальная стоимость проезда. Потому что один вход и выход. Если переходить между компаниями, то каждый раз это новый вход и выход, соответственно и новая плата. Если строить маршрут через тот же Гугл мэпс, то он предложит как самый дешевый вариант, так и самый быстрый.</p>
+@en
+  <p>The next change. There are two schemes on the column. The stations on the upper one have hieroglyphic, latin and symbolic names. The lower one is metro map. Yea, there lots of lines. There are different railway companies as well. It is cheaper to pick just one for a single ride, because it's one entrance and one exit this way. You are charged when you change railway companies. When you use apps like Apple or Google maps for directions, they suggest cheapest and fastest options mixed, so double-check.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3871.jpg'])
 
 @ru
   <p>Станция.</p>
+@en
+  <p>Railway station.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3872.jpg'])
 
 @ru
   <p>Входы в вагоны действительно напротив надписей на полу.</p>
+@en
+  <p>Boarding points to the cars are really opposite the signs on the floor.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3874.jpg'])
 
 @ru
   <p>На столбе название станции. Можно попрактиковаться в <a class="link" href="japanese">иероглифах</a>.</p>
+@en
+  <p>Name of the station on the column. It is time practice <a class="link" href="japanese">japanese</a>.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3876.jpg'])
 
@@ -139,11 +193,15 @@
 
 @ru
   <p>Пора уже, наконец, выйти на улицу.</p>
+@en
+  <p>It's time to finally take a look at the streets.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3883.jpg'])
 
 @ru
   <p>Посмотреть на дороги.</p>
+@en
+  <p>And the roads.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_3884.jpg',
@@ -157,6 +215,9 @@
     <li>в иенах</li>
   </ol>
   <p>Второй вариант заметно выгоднее. Было 16 800 ₽ против 15 800 ₽.</p>
+@endru
+
+@ru
   <p>Если втянуть живот, то можно протиснуться между домами.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3886.jpg'])
@@ -189,16 +250,22 @@
 
 @ru
   <p>Храм.</p>
+@en
+  <p>Temple.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3903.jpg'])
 
 @ru
   <p>Рядом с храмом.</p>
+@en
+  <p>Near the temple.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3905.jpg'])
 
 @ru
   <p>Указатель пожарного гидранта.</p>
+@en
+  <p>Fire hydrant sign.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3904.jpg'])
 
@@ -209,6 +276,8 @@
 
 @ru
   <p>Ворота под названием тории.</p>
+@en
+  <p>Gates like this one are called torii.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3916.jpg'])
 
@@ -224,6 +293,8 @@
 
 @ru
   <p>Автобус НАТО.</p>
+@en
+  <p>Nato (in russian) bus.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3925.jpg'])
 
@@ -347,6 +418,8 @@
 
 @ru
   <p>Пять пунктов номер один.</p>
+@en
+  <p>Five items number one.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3965.jpg'])
 
@@ -361,12 +434,16 @@
 ]])
 
 @ru
-  <p>Телефонный автомат.</p>
+  <p>Таксофон.</p>
+@en
+  <p>Payphone.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3970.jpg'])
 
 @ru
   <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3972.jpg'])
 
@@ -402,6 +479,8 @@
 
 @ru
   <p>В метро.</p>
+@en
+  <p>In the metro.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3984.jpg'])
 
@@ -417,31 +496,43 @@
 
 @ru
   <p>Состав без машиниста.</p>
+@en
+  <p>Driverless train.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3986.jpg'])
 
 @ru
   <p>Станция метро U08.</p>
+@en
+  <p>Metro station U08.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3988.jpg'])
 
 @ru
-  <p>За окном.</p>
+  <p>За окном станции.</p>
+@en
+  <p>Outside the station.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3989.jpg'])
 
 @ru
   <p>Туалет на МКС.</p>
+@en
+  <p>Space toilet on the ISS.</p>
 @endru
 @include('tpl.pic-arbitrary', ['pic' => 'IMG_3990.jpg', 'w' => 563, 'h' => 750])
 
 @ru
   <p>Земля.</p>
+@en
+  <p>Earth.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_3992.jpg'])
 
 @ru
   <p>Район Одайба.</p>
+@en
+  <p>Odaiba district.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_3997.jpg',
@@ -466,11 +557,15 @@
 
 @ru
   <p>Мост.</p>
+@en
+  <p>Bridge.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4005.jpg'])
 
 @ru
   <p>Американские машины.</p>
+@en
+  <p>American cars.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4006.jpg'])
 
@@ -483,21 +578,29 @@
 
 @ru
   <p>Здание.</p>
+@en
+  <p>Building.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4009.jpg'])
 
 @ru
   <p>Велопарковка.</p>
+@en
+  <p>Bicycle parking.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4010.jpg'])
 
 @ru
   <p>Колесо обозрения.</p>
+@en
+  <p>Ferris wheel.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4011.jpg'])
 
 @ru
   <p>Метро.</p>
+@en
+  <p>Metro.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4048.jpg'])
 
@@ -518,11 +621,15 @@
 
 @ru
   <p>Вид сверху.</p>
+@en
+  <p>Top view.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4056.jpg'])
 
 @ru
   <p>Вид снизу.</p>
+@en
+  <p>Bottom view.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4058.jpg'])
 
@@ -533,6 +640,8 @@
 
 @ru
   <p>Станция.</p>
+@en
+  <p>Railway station.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_4061.jpg',
@@ -541,11 +650,15 @@
 
 @ru
   <p>Район Шибуя.</p>
+@en
+  <p>Shibuya district.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4064.jpg'])
 
 @ru
-  <p>Тоже можно переходить дорогу в любом направлении.</p>
+  <p>Можно переходить дорогу в любом направлении.</p>
+@en
+  <p>You can cross the road in any direction.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4065.jpg'])
 
@@ -556,6 +669,8 @@
 
 @ru
   <p><s>Ночные</s> Вечерние улицы. Темнеет же рано.</p>
+@en
+  <p>Streets in the <s>night</s> evening. It gets dark early.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_4068.jpg',
@@ -565,6 +680,8 @@
 
 @ru
   <p>Еще ж/д станции.</p>
+@en
+  <p>More railway stations.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_4071.jpg',
@@ -578,11 +695,15 @@
 
 @ru
   <p>Жвачка с интересными названиями.</p>
+@en
+  <p>Chewing gum.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4073.jpg'])
 
 @ru
   <p>Прямо как скотч.</p>
+@en
+  <p>Just like an adhesive tape.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4074.jpg'])
 
@@ -593,11 +714,15 @@
 
 @ru
   <p>Микроскопические отверстия для воды в душе.</p>
+@en
+  <p>Microscopic holes for water in the shower.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4081.jpg'])
 
 @ru
   <p>Дожди закончились.</p>
+@en
+  <p>It's stopped raining.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4083.jpg'])
 @endsection
