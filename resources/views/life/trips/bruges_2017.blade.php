b91af4d0904e71700bd8905ea409e8361ef980af
Sergey Pankov <vacuum@ivacuum.ru>
2017-09-23 13:13:24 +0300

    Брюгге

---
+++ resources/views/life/trips/bruges_2017.blade.php
@@ -0,0 +1,330 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Дорога в Брюгге.</p>
+@en
+  <p>Road to Bruges.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2223.jpg',
+  'IMG_2230.jpg',
+  'IMG_2233.jpg',
+]])
+
+@ru
+  <p>Нет газетам и рекламе.</p>
+@en
+  <p>No to newspapers and ads.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2239.jpg'])
+
+@ru
+  <p>Другой вариант.</p>
+@en
+  <p>Another option.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2276.jpg'])
+
+@ru
+  <p>Информация о парковках.</p>
+@en
+  <p>Information about parkings.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2240.jpg'])
+
+@ru
+  <p>Круглые окна.</p>
+@en
+  <p>Round windows.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2243.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2244.jpg'])
+
+@ru
+  <p>Модель поновее.</p>
+@en
+  <p>Newer model.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2341.jpg'])
+
+@ru
+  <p>Редкая полицейская машина.</p>
+@en
+  <p>Rare police car.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2268.jpg'])
+
+@ru
+  <p>Вот так зайдешь в заведение интернет поискать и обломишься.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2273.jpg'])
+
+@ru
+  <p>Номера домов.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2278.jpg',
+  'IMG_2280.jpg',
+]])
+
+@ru
+  <p>Прокручивающееся полотенце в туалете. Мятый вид отталкивает пользоваться им за кем-то другим, хотя подразумевается, что аппарат постоянно его чистит за счет прокрутки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2281.jpg'])
+
+@ru
+  <p>Ночные улицы.</p>
+@en
+  <p>Night streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2241.jpg',
+  'IMG_2245.jpg',
+  'IMG_2249.jpg',
+  'IMG_2257.jpg',
+  'IMG_2260.jpg',
+  'IMG_2261.jpg',
+  'IMG_2271.jpg',
+  'IMG_2279.jpg',
+]])
+
+@ru
+  <p>Дневные улицы.</p>
+@en
+  <p>Daytime streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2283.jpg',
+  'IMG_2285.jpg',
+  'IMG_2288.jpg',
+  'IMG_2296.jpg',
+  'IMG_2307.jpg',
+  'IMG_2382.jpg',
+  'IMG_2383.jpg',
+  'IMG_2384.jpg',
+]])
+
+@ru
+  <p>Запарковался в три часа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2284.jpg'])
+
+@ru
+  <p>Высота двери соразмерима с шириной улицы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2287.jpg'])
+
+@ru
+  <p>Детали.</p>
+@en
+  <p>Details.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2295.jpg',
+  'IMG_2299.jpg',
+  'IMG_2302.jpg',
+]])
+
+@ru
+  <p>Ушко у входной двери.</p>
+@en
+  <p>Ear near the entrance door.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2298.jpg'])
+
+@ru
+  <p>Дворик.</p>
+@en
+  <p>Yard.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2301.jpg'])
+
+@ru
+  <p>Маршруты на автобусной остановке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2306.jpg'])
+
+@ru
+  <p>Автобусы действительно ходят по этому маленькому городу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2297.jpg'])
+
+@ru
+  <p>Панорамы.</p>
+@en
+  <p>Panoramas.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2318.jpg',
+  'IMG_2319.jpg',
+  'IMG_2324.jpg',
+  'IMG_2326.jpg',
+]])
+
+@ru
+  <p>Амстердам — одна из следующих целей.</p>
+@en
+  <p>Amsterdam is one of the next destinations.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2327.jpg'])
+
+@ru
+  <p>Колокола.</p>
+@en
+  <p>Bells.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2331.jpg'])
+
+@ru
+  <p>Внутренний дворик.</p>
+@en
+  <p>Courtyard.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2308.jpg'])
+
+@ru
+  <p>Чем известна Бельгия? Ответ на домах.</p>
+@en
+  <p>What is Belgium famous for? The answer is on the houses.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2340.jpg'])
+
+@ru
+  <p>Площадь.</p>
+@en
+  <p>Square.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2353.jpg'])
+
+@ru
+  <p>Здания.</p>
+@en
+  <p>Buildings.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2254.jpg',
+  'IMG_2251.jpg',
+  'IMG_2264.jpg',
+  'IMG_2248.jpg',
+  'IMG_2304.jpg',
+  'IMG_2380.jpg',
+  'IMG_2379.jpg',
+]])
+
+@ru
+  <p>Каналы.</p>
+@en
+  <p>Canals.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2373.jpg',
+  'IMG_2374.jpg',
+  'IMG_2375.jpg',
+]])
+
+@ru
+  <p>Лебеди.</p>
+@en
+  <p>Swans.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2376.jpg',
+  'IMG_2377.jpg',
+]])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2291.jpg'])
+
+@ru
+  <p>Паркомат.</p>
+@en
+  <p>Parking meter.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2386.jpg'])
+
+@ru
+  <p>От Брюгге до моря двадцать минут на машине.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2388.jpg'])
+
+@ru
+  <p>Что-то можно <s>подцепить</s> прицепить на парковке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2389.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2390.jpg'])
+
+@ru
+  <p>Временные ограничения.</p>
+@en
+  <p>Temporary restrictions.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2391.jpg'])
+
+@ru
+  <p>Пляж.</p>
+@en
+  <p>Beach.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2393.jpg'])
+
+@ru
+  <p>Душ на пляже.</p>
+@en
+  <p>Shower on the beach.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2395.jpg'])
+
+@ru
+  <p>Северное море.</p>
+@en
+  <p>North sea.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2398.jpg'])
+
+@ru
+  <p>Прикатили трейлер с аппаратурой на пляж и транслируют мультфильм.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2407.jpg'])
+
+@ru
+  <p>Можно не только валяясь в песке смотреть, но и кушая за столиком.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2409.jpg'])
+
+@ru
+  <p>Чем не лавочки? Пусть и низкие.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2410.jpg'])
+
+@ru
+  <p>Домик.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2411.jpg'])
+
+@ru
+  <p>Пиктограмма парковочного места для инвалидов выложена плиткой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2412.jpg'])
+
+@ru
+  <p>Как и весь перекресток.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2387.jpg'])
+@endsection
