2ac3a4ea5bd6a816ab1f63bc1a15e6f30c7d1a6b
Sergey Pankov <vacuum@ivacuum.ru>
2017-10-02 09:14:21 +0200

    Перевод

--- resources/views/life/trips/matsumoto_2017.blade.php
+++ resources/views/life/trips/matsumoto_2017.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 @ru
   <p>За окном во время поездки на автобусе из Такаямы в Мацумото.</p>
+@en
+  <p>On the way from Takayama to Matsumoto by bus.</p>
 @endru
 <youtube title="Matsumoto Bus, April 2017" v="neekGArH7pU"></youtube>
 @include('tpl.fotorama-2x', ['pics' => [
@@ -26,17 +28,23 @@
 
 @ru
   <p>По пути много тоннелей. При движении по ним в поездах закладывает уши, причем не только в шинкансенах.</p>
+@en
+  <p>There are many tunnels on the way. They make your ears really feel the pressure difference while in any fast train—not only shinkansen.</p>
 @endru
 <youtube title="Matsumoto Bus in Tunnel, April 2017" v="87rCmd8QWuk"></youtube>
 @include('tpl.pic-2x', ['pic' => 'IMG_7793.jpg'])
 
 @ru
   <p>И даже дамба.</p>
+@en
+  <p>There is even a levee.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7806.jpg'])
 
 @ru
   <p>На табло слева цены за проезд от определенных участков пути. Полный путь в 91 км из Такаямы стоит 3 190 иен (около 1650 ₽ на момент поездки).</p>
+@en
+  <p>Prices are on the display in the upper left corner of the photo. 91 km (57 mi) way from Takayama costs ¥3190 or $28.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7846.jpg'])
 
@@ -52,6 +60,8 @@
 
 @ru
   <p>Около писсуаров крючки для зонтов.</p>
+@en
+  <p>Holders for umbrellas next to urinals.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7887.jpg'])
 
@@ -64,6 +74,8 @@
 
 @ru
   <p>Потрясающий столб освещения.</p>
+@en
+  <p>Fabulous light pole.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7861.jpg'])
 
@@ -108,6 +120,8 @@
 
 @ru
   <p>Парк — эвакуационная точка при чрезвычайных ситуациях.</p>
+@en
+  <p>Park is an evacuation place in emergency situations.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7870.jpg'])
 
@@ -120,16 +134,22 @@
 
 @ru
   <p>Черный замок, красный мост, горы на фоне. Красота!</p>
+@en
+  <p>Black castle, red bridge, mountains in the background. Gorgeous!</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7884.jpg'])
 
 @ru
   <p>Здесь остановится второй вагон восьми- и десятивагонных составов.</p>
+@en
+  <p>Boarding point of the second car of eight and ten cars' trains.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7890.jpg'])
 
 @ru
   <p>Железнодорожная станция. На немецкий манер платформы внизу, а переход с помощью эскалатора по верху.</p>
+@en
+  <p>Railway station.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_7894.jpg',
