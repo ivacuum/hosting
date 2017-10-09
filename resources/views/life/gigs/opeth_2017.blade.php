2088c991300a9fee42e11ef1f563a89a411077bd
Sergey Pankov <vacuum@ivacuum.ru>
2017-10-10 02:33:10 +0300

    Заготовка для концерта

---
+++ resources/views/life/gigs/opeth_2017.blade.php
@@ -0,0 +1,19 @@
+@extends('life.gigs.base')
+
+{{-- ГлавClub --}}
+
+@section('content')
+<div class="row">
+  <div class="col-md-7">
+    @include('tpl.setlist-title')
+    <ol>
+    </ol>
+  </div>
+  <div class="col-md-5">
+    <div class="img-container">
+      <img src="https://life.ivacuum.ru/gigs/opeth.2017.11.10.jpg">
+    </div>
+  </div>
+</div>
+
+@endsection
