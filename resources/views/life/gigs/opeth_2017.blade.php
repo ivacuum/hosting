fc15faa1900abf26d0e41476b154e5b18eddd3f6
Sergey Pankov <vacuum@ivacuum.ru>
2017-10-11 03:52:38 +0300

    Ожидаемый сет

--- resources/views/life/gigs/opeth_2017.blade.php
+++ resources/views/life/gigs/opeth_2017.blade.php
@@ -7,6 +7,15 @@
   <div class="col-md-7">
     @include('tpl.setlist-title')
     <ol>
+      <li>Sorceress</li>
+      <li>Ghost of Perdition</li>
+      <li>Demon of the Fall</li>
+      <li>The Wilde Flowers</li>
+      <li>In My Time of Need</li>
+      <li>Cusp of Eternity</li>
+      <li>Heir Apparent</li>
+      <li>The Drapery Falls</li>
+      <li>Deliverance</li>
     </ol>
   </div>
   <div class="col-md-5">
