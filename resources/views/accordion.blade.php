3db4ae7de86eb1643ab42b4a516f8b301dedc391
Sergey Pankov <vacuum@ivacuum.ru>
2017-10-14 02:38:21 +0300

    Увеличение области клика у аккордеона

--- resources/views/accordion.blade.php
+++ resources/views/accordion.blade.php
@@ -2,7 +2,7 @@
 <div class="panel panel-default mb-2">
   <div class="panel-heading">
     <h4 class="panel-title">
-      <a href="#{{ $id }}" data-toggle="collapse">{{ $title }}</a>
+      <a class="d-block" href="#{{ $id }}" data-toggle="collapse">{{ $title }}</a>
     </h4>
   </div>
   <div id="{{ $id }}" class="panel-collapse collapse">
