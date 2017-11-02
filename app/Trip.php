7b05447f9d6ef59d475e6dcb0044e2bb67614f03
Sergey Pankov <vacuum@ivacuum.ru>
2017-11-02 21:27:21 +0300

    Заготовка для описания изображений в поездках

--- app/Trip.php
+++ app/Trip.php
@@ -184,6 +184,11 @@ class Trip extends Model
         return self::orderBy('date_start', 'desc')->get(['id', 'slug'])->pluck('slug', 'id');
     }
 
+    public function imgAltText()
+    {
+        return "{$this->city->country->emoji} {$this->title}, {$this->city->country->title}, {$this->timelinePeriod(true)}.";
+    }
+
     public function localizedDate()
     {
         if ($this->date_end->isSameDay($this->date_start)) {
