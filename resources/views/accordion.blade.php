63d8dab450269f64b4b378b2ef1f4b4c6d9eb70e
Sergey Pankov <vacuum@ivacuum.ru>
2017-11-05 23:55:35 +0300

    Микроразметка рубрик вопрос-ответ

--- resources/views/accordion.blade.php
+++ resources/views/accordion.blade.php
@@ -1,11 +1,11 @@
 @php ($id = $id ?? 'accordion_'.str_random(6))
-<div class="panel panel-default mb-2">
+<div class="panel panel-default mb-2" itemscope itemtype="http://schema.org/Question">
   <div class="panel-heading">
     <h4 class="panel-title">
-      <a class="d-block" href="#{{ $id }}" data-toggle="collapse">{{ $title }}</a>
+      <a class="d-block" href="#{{ $id }}" data-toggle="collapse" itemprop="name">{{ $title }}</a>
     </h4>
   </div>
-  <div id="{{ $id }}" class="panel-collapse collapse">
-    <div class="panel-body font-smooth">{{ $slot }}</div>
+  <div id="{{ $id }}" class="panel-collapse collapse" itemprop="acceptedAnswer" itemscope itemtype="http://schema.org/Answer">
+    <div class="panel-body font-smooth" itemprop="text">{{ $slot }}</div>
   </div>
 </div>
