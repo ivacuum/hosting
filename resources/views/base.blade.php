e90016c29b93e1862bed06cc23473e99f6ac568b
Sergey Pankov <vacuum@ivacuum.ru>
2017-11-08 22:28:18 +0300

    Обновление зависимостей

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -140,7 +140,7 @@
 <script src="/assets/autosize.js?4.0.0"></script>
 <script src="/assets/bootstrap.js"></script>
 <script src="/assets/vue.js?2.4.4"></script>
-<script src="/assets/axios.js?0.16.2"></script>
+<script src="/assets/axios.js?0.17.0"></script>
 @if (!empty($websockets))
   <script src="/assets/socket.io.js?2.0.4"></script>
 @endif
