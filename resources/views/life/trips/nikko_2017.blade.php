4ee6408127ea1e776f2599781d7a764f9b887eef
Sergey Pankov <vacuum@ivacuum.ru>
2017-11-18 14:47:10 +0300

    Перевод

--- resources/views/life/trips/nikko_2017.blade.php
+++ resources/views/life/trips/nikko_2017.blade.php
@@ -3,32 +3,44 @@
 @section('content')
 @ru
   <p>Из горной <a class="link" href="takayama.2017">Такаямы</a> оказалось непросто добраться до Никко. Одними поездами выходило около шести часов с несколькими пересадками, причем сначала предстояло ехать назад по уже увиденным местам. Если же сначала доехать на автобусе до <a class="link" href="matsumoto.2017">Мацумото</a>, а оттуда поездами, то в итоге те же шесть часов, но без повторений пути и плюс один город на осмотр.</p>
+@en
+  <p>There is no easy way to get to Nikko from nested among mountains <a class="link" href="takayama.2017">Takayama</a>. It's a six hours ride on trains with a few changes at cities I have been to. The other option is to take a bus to <a class="link" href="matsumoto.2017">Matsumoto</a> first, and then continue the journey on trains. Same six hours ride, but one new city to see and no repeats.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7907.jpg'])
 
 @ru
   <p>Сначала на экспрессе до Нагано.</p>
+@en
+  <p>Express from Matsumoto to Nagano.</p>
 @endru
 <youtube title="Nagano Train, April 2017" v="sp1NyMHw39Y"></youtube>
 @include('tpl.pic-2x', ['pic' => 'IMG_7934.jpg'])
 
 @ru
   <p>Поворот налево. Всегда такие снимки сперва хочется выровнять, но потом мозг парирует, что не настолько же кривые руки, чтобы так изначально снимать. Действительно поворот налево.</p>
+@en
+  <p>Turning left. Always find myself wanting to straighten the horizon in pics like this, but then brain says my hands can't be so terrible at shooting pics. We are turning left for real.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7935.jpg'])
 
 @ru
   <p>Крохотное кладбище по пути.</p>
+@en
+  <p>Tiny cemetery.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7983.jpg'])
 
 @ru
   <p>Затем пересадка на шинкансен. Зеленый указатель хорошо выделяется.</p>
+@en
+  <p>Transfer to shinkansen. Green sign is easy to spot.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7992.jpg'])
 
 @ru
   <p>Какие поезда только не встретишь. Нужно только научиться снимать объекты в движении.</p>
+@en
+  <p>Plenty of different locomotives to see. Just need to find a way to clearly capture moving trains.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_7993.jpg',
@@ -45,71 +57,99 @@
 
 @ru
   <p>Классный электрический кабель между вагонами. Шестой вагон только по предварительному бронированию мест. Остановка четко напротив стрелочки.</p>
+@en
+  <p>Nice eletric cable between cars. Car #6 requires reservation in advance. Arrow sign is precisely pointing at the door.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7995.jpg'])
 
 @ru
   <p>Очередь на посадку.</p>
+@en
+  <p>Boarding queue.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8038.jpg'])
 
 @ru
   <p>Затем еще одна пересадка на другой шинкансен на станции Омия.</p>
+@en
+  <p>Next transfer is at Omiya station.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8052.jpg'])
 
 @ru
   <p>Интерьер вагона.</p>
+@en
+  <p>Car's interior.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8054.jpg'])
 
 @ru
   <p>Присутствует туалет.</p>
+@en
+  <p>There is a toilet.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8055.jpg'])
 
 @ru
   <p>Следующая пересадка на станции Уцуномия. Уже видно, что отсюда, наконец-то, можно добраться до Никко.</p>
+@en
+  <p>Next transfer is at Utsunomiya station. It's obvious that it's Nikko direction.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8057.jpg'])
 
 @ru
   <p>Платформа. Поезда по-прежнему проще всего опознавать по времени отправления.</p>
+@en
+  <p>Platform. Departure time is still my favorite way to clearly distinguish trains.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8058.jpg'])
 
 @ru
   <p>Хитрая кодовая панель на двери.</p>
+@en
+  <p>Combination lock.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8061.jpg'])
 
 @ru
   <p>В электричке. На местном языке Японию называют либо Ниппон, как на плакате, либо Нихон. Стоит отметить как мало места отведено машинисту.</p>
+@en
+  <p>Suburban train. Japan is nippon (as on ad) or nihon in Japanese. Usually nihon. There is so little space for a driver.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8066.jpg'])
 
 @ru
   <p>Можно подойти и посмотреть за движением.</p>
+@en
+  <p>You can come up and watch the movement.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8071.jpg'])
 
 @ru
   <p>Прибыли.</p>
+@en
+  <p>We have arrived.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8079.jpg'])
 
 @ru
   <p>Подтверждение.</p>
+@en
+  <p>Confirmation.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8080.jpg'])
 
 @ru
   <p>Если необходимо пополнить транспортную карту, то можно сразу вставлять ее в терминал. Помимо пополнения, можно распечатать историю трат по карте. Слева от экрана классные кнопки-заготовки для выбора количества пассажиров.</p>
+@en
+  <p>If you need to charge your transport card, you can put it into terminal right away. There is also an option to print history of your expenses. There are nice buttons to the left of the display for number of persons you most likely need tickets for.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8081.jpg'])
 
 @ru
-  <p>Пора уже выйти в город. Некоторые автобусы развозят прямо по отелям — так расставлены остановки на их маршрутах.</p>
+  <p>Пора уже выйти в город. Некоторые автобусы развозят прямо по отелям — так расставлены остановки на их маршрутах. Очень удобно.</p>
+@en
+  <p>Finally, it is time to go out and see Nikko. Some bus routes have stops at many hotels, so there is a big chance that you can get to the entrance of the one you need. So convenient!</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8083.jpg'])
 
@@ -122,20 +162,28 @@
 
 @ru
   <p>По случаю проведения фестиваля на улице регулировщики движения.</p>
+@en
+  <p>Traffic guards are on streets, because some festival is going on right now.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8087.jpg'])
 
 @ru
   <p>Ужин. Суп и рис остались в стороне.</p>
+@en
+  <p>Dinner. Soup and rice didn't fit on the tray.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8094.jpg'])
 
 @ru
   <p>Еду в заведениях готовят голыми руками. После заказа приносят стакан воды со льдом, иногда целый кувшин. Существует интересное блюдо рамен, сочетающее в себе салат, суп и горячее, — его едят с двух рук: в левой ложка, в правой палочки. Чаевые отсутствует в принципе. Даже если при счете в ¥2 291 дать ¥2 300, то обязательно выдадут сдачу.</p>
+@en
+  <p>Dishes are made with bare hands. After your order is taken, you get a jug of cold water. Or at least glass. There is a dish called ramen. It feels like it combines salad, soup and main course in one plate. You get a spoon for soup and chopsticks for everything else. It's totally okay to slurp. Also, there are no tips—you'll get all the change to the last coin.</p>
 @endru
 
 @ru
   <p>Завтрак. Рис снова остался в стороне.</p>
+@en
+  <p>Breakfast. Rice didn't fit once again.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8097.jpg'])
 
@@ -169,6 +217,8 @@
 
 @ru
   <p>На обратной стороне расписание автобуса.</p>
+@en
+  <p>There is a schedule on its back.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8114.jpg'])
 
@@ -199,11 +249,15 @@
 
 @ru
   <p>Урна. Она же пепельница.</p>
+@en
+  <p>Trashcan. It's also an ashtray.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8120.jpg'])
 
 @ru
   <p>Мост и вид с него.</p>
+@en
+  <p>Bridge and view from it.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_8126.jpg',
@@ -220,22 +274,30 @@
 
 @ru
   <p>Чутье и беспорядочное брожение не впервые заводят в интересное место. Указатель на обратном пути сообщал, что это некая бездна.</p>
+@en
+  <p>Looks like I have a flair for finding interesting places while wandering. Sign on the way back said it's kind of an abyss.</p>
 @endru
 <youtube title="Nikko Abyss, April 2017" v="mNwzCvQDQ1w"></youtube>
 @include('tpl.pic-2x', ['pic' => 'IMG_8134.jpg'])
 
 @ru
   <p>Отличное место для привала.</p>
+@en
+  <p>Nice place for a halt.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8143.jpg'])
 
 @ru
   <p>Ребята в красном.</p>
+@en
+  <p>Buddies in red.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8147.jpg'])
 
 @ru
   <p>Их очень много, но один, похоже, сдулся.</p>
+@en
+  <p>There are many of them, but it seems like one is already blown away.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8133.jpg'])
 
@@ -251,16 +313,22 @@
 
 @ru
   <p>Очевидно, сюда просят не ходить.</p>
+@en
+  <p>Apparently, we are asked not to go here.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8159.jpg'])
 
 @ru
   <p>Запас дров. Да, без центрального отопления зимой каждый согревается как может.</p>
+@en
+  <p>Wood stock. Yeah, without central heating you need a way to survive winter.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8161.jpg'])
 
 @ru
   <p>Вот и указатель.</p>
+@en
+  <p>Here is the sign.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8165.jpg'])
 
@@ -279,6 +347,8 @@
 
 @ru
   <p>Скайлайн в домашней среде.</p>
+@en
+  <p>Skyline in a home environment.</p>
 @endru
 @include('tpl.fotorama-2x', ['pics' => [
   'IMG_8172.jpg',
@@ -288,16 +358,22 @@
 
 @ru
   <p>Парковка с видом на горы.</p>
+@en
+  <p>Parking with a mountain view.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8181.jpg'])
 
 @ru
   <p>Вид наверх.</p>
+@en
+  <p>View from bottom to top.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8183.jpg'])
 
 @ru
   <p>Вид вниз.</p>
+@en
+  <p>View from top to bottom.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8102.jpg'])
 
@@ -310,26 +386,36 @@
 
 @ru
   <p>Карты на любой вкус.</p>
+@en
+  <p>Maps for every taste.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8190.jpg'])
 
 @ru
   <p>Пора отправляться в Токио. Поезд уже поджидает.</p>
+@en
+  <p>Time to go to Tokyo. Train is already waiting.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8191.jpg'])
 
 @ru
   <p>Состав всего из двух вагонов. Всего через пару станций предстоит сделать первую пересадку. Планировщик маршрута указывает, что следующий поезд отправляется в ту же минуту, когда прибывает этот. Менее минуты на пересадку! Реально? Да. Когда в таком случае приезжаешь на нужную станцию, то напротив уже стоит состав с открытыми дверьми, остается только перейти в него. Конечно, большинство бежит, так как боится его упустить. Когда планировщик пишет, что две минуты на пересадку, то состав аналогично придет на соседний путь. Нереальные варианты не предлагаются.</p>
+@en
+  <p>Just two cars train. First change is only a few stops away from here. Navigation app says the train we need to make a change for departures at the same minute as this one arrives. Less than a minute for a change! Is it real? Yes. In this case you gonna see the next train on the opposite side of the platform with doors wide open waiting for you to get in. Most folks run for it, but it's really unnecessary. If there is two minutes change, then your next train should arrive to the adjacent track as well. No unreal options through navigation app.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8193.jpg'])
 
 @ru
   <p>Бригада на путях.</p>
+@en
+  <p>Crew is at railroad.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8196.jpg'])
 
 @ru
   <p>Железный человек в транспортном обличии.</p>
+@en
+  <p>Iron man is in transport form.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_8194.jpg'])
 @endsection
