9913cda8eed3a48976562f0c45f75759a8aa9bc0
Sergey Pankov <vacuum@ivacuum.ru>
2017-12-18 21:49:09 +0300

    Москва в ноябре

---
+++ resources/views/life/trips/msk_2017_11_10.blade.php
@@ -0,0 +1,8 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Любимое место ожидания на Киевском вокзале — зал аэроэкспресса.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4247.jpg'])
+@endsection
