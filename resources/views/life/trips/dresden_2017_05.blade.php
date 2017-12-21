0d0e5199f3bb1442d5a741469c78506fc248556e
Sergey Pankov <vacuum@ivacuum.ru>
2017-12-21 15:40:41 +0300

    Дрезден в мае

---
+++ resources/views/life/trips/dresden_2017_05.blade.php
@@ -0,0 +1,39 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Множество корявых фоток стен с классными иллюстрациями на тему общественного транспорта и не только. Сделаны на ходу через окно автобуса из Праги в Берлин.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0250.jpg',
+  'IMG_0251.jpg',
+  'IMG_0252.jpg',
+  'IMG_0253.jpg',
+  'IMG_0254.jpg',
+  'IMG_0255.jpg',
+  'IMG_0256.jpg',
+  'IMG_0257.jpg',
+  'IMG_0258.jpg',
+  'IMG_0259.jpg',
+  'IMG_0260.jpg',
+  'IMG_0261.jpg',
+  'IMG_0262.jpg',
+  'IMG_0263.jpg',
+  'IMG_0264.jpg',
+  'IMG_0265.jpg',
+  'IMG_0266.jpg',
+  'IMG_0267.jpg',
+  'IMG_0268.jpg',
+  'IMG_0269.jpg',
+  'IMG_0270.jpg',
+  'IMG_0271.jpg',
+  'IMG_0272.jpg',
+  'IMG_0273.jpg',
+  'IMG_0274.jpg',
+  'IMG_0275.jpg',
+  'IMG_0276.jpg',
+  'IMG_0277.jpg',
+  'IMG_0278.jpg',
+  'IMG_0279.jpg',
+]])
+@endsection
