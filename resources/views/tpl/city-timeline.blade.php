da0e41d7562aa733ea37f32bf6abdb95940585cd
Sergey Pankov <vacuum@ivacuum.ru>
2018-01-16 10:24:45 +0300

    Рефакторинг

--- resources/views/tpl/city-timeline.blade.php
+++ resources/views/tpl/city-timeline.blade.php
@@ -1,20 +1,24 @@
 @if (isset($timeline) && sizeof($timeline->flatten()) > 1)
-  <div class="city-timeline f14 mb-3">
-    @foreach ($timeline as $year => $rows)
-      <ul class="city-timeline-column">
-        <li class="font-weight-bold">{{ $year }}</li>
-        @foreach ($rows as $row)
-          <li class="city-timeline-trip">
-            @if ($row->id === $trip->id)
-              <mark>{{ $row->timelinePeriod() }}</mark>
-            @elseif ($row->status === App\Trip::STATUS_PUBLISHED)
-              <a class="link" href="{{ $row->www() }}">{{ $row->timelinePeriod() }}</a>
-            @else
-              {{ $row->timelinePeriod() }}
-            @endif
-          </li>
+  <div class="timeline-container mb-3">
+    <div class="timeline-scroll">
+      <div class="f14 d-flex">
+        @foreach ($timeline as $year => $rows)
+          <div class="d-flex flex-column timeline-column">
+            <div class="font-weight-bold">{{ $year }}</div>
+            @foreach ($rows as $row)
+              <div>
+                @if ($row->id === $trip->id)
+                  <mark>{{ $row->timelinePeriod() }}</mark>
+                @elseif ($row->status === App\Trip::STATUS_PUBLISHED)
+                  <a class="link" href="{{ $row->www() }}">{{ $row->timelinePeriod() }}</a>
+                @else
+                  {{ $row->timelinePeriod() }}
+                @endif
+              </div>
+            @endforeach
+          </div>
         @endforeach
-      </ul>
-    @endforeach
+      </div>
+    </div>
   </div>
 @endif
--- resources/views/tpl/gig-timeline.blade.php
+++ resources/views/tpl/gig-timeline.blade.php
@@ -1,20 +1,24 @@
 @if (sizeof($timeline) > 1)
-  <div class="city-timeline f14 mb-3">
-    @foreach ($timeline as $year => $rows)
-      <ul class="city-timeline-column">
-        <li class="font-weight-bold">{{ $year }}</li>
-        @foreach ($rows as $row)
-          <li class="city-timeline-trip">
-            @if ($row->id === $gig->id)
-              <mark>{{ $row->shortDate() }}</mark>
-            @elseif ($row->status === App\Gig::STATUS_PUBLISHED)
-              <a class="link" href="{{ $row->www() }}">{{ $row->shortDate() }}</a>
-            @else
-              {{ $row->shortDate() }}
-            @endif
-          </li>
+  <div class="timeline-container mb-3">
+    <div class="timeline-scroll">
+      <div class="f14 d-flex">
+        @foreach ($timeline as $year => $rows)
+          <div class="d-flex flex-column timeline-column">
+            <div class="font-weight-bold">{{ $year }}</div>
+            @foreach ($rows as $row)
+              <div>
+                @if ($row->id === $gig->id)
+                  <mark>{{ $row->shortDate() }}</mark>
+                @elseif ($row->status === App\Gig::STATUS_PUBLISHED)
+                  <a class="link" href="{{ $row->www() }}">{{ $row->shortDate() }}</a>
+                @else
+                  {{ $row->shortDate() }}
+                @endif
+              </div>
+            @endforeach
+          </div>
         @endforeach
-      </ul>
-    @endforeach
+      </div>
+    </div>
   </div>
 @endif
