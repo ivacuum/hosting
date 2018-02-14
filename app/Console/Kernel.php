a4b9d1a27f4bfd728637a6cffe69641f4074c1fe
Sergey Pankov <vacuum@ivacuum.ru>
2018-02-14 07:39:29 +0300

    Рефакторинг

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,20 +31,30 @@ class Kernel extends ConsoleKernel
     {
         $cron_output = config('cfg.cron_output');
 
-        $schedule->command('app:notifications-purge')->cron('0 2,14 * * *')
+        // ttl = a week
+        $schedule->command('app:metrics-init-today')
+            ->cron('0 0 * * *')
+            ->appendOutputTo($cron_output);
+
+        $schedule->command('app:notifications-purge')
+            ->cron('0 2,14 * * *')
             ->appendOutputTo($cron_output);
 
         // Ежедневное удаление старых заявок на восстановление пароля
-        $schedule->command('app:purge-password-reminders')->cron('0 5 * * *')
+        $schedule->command('app:purge-password-reminders')
+            ->cron('0 5 * * *')
             ->appendOutputTo($cron_output);
 
-        $schedule->command('app:rto-update')->cron('0 */6 * * *')
+        $schedule->command('app:rto-update')
+            ->cron('0 */6 * * *')
             ->appendOutputTo($cron_output);
 
-        $schedule->command('app:vk-likes-add pn6')->cron('5,25,45 * * * *')
+        $schedule->command('app:vk-likes-add pn6')
+            ->cron('5,25,45 * * * *')
             ->appendOutputTo($cron_output);
 
-        $schedule->command('app:vk-likes-delete pn6')->cron('15,35,55 * * * *')
+        $schedule->command('app:vk-likes-delete pn6')
+            ->cron('15,35,55 * * * *')
             ->appendOutputTo($cron_output);
 
         // $schedule->command('app:whois-update')->cron('0 */4 * * *'); // каждые 4 часа
