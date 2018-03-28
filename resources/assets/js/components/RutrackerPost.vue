c51a16fd6acabd866bed91c0976fdb5798fac30d
Sergey Pankov <vacuum@ivacuum.ru>
2018-03-28 17:36:48 +0300

    Попытка выводить в раздачах оригинальные скриншоты вместо миниатюр

--- resources/assets/js/components/RutrackerPost.vue
+++ resources/assets/js/components/RutrackerPost.vue
@@ -9,20 +9,61 @@ export default {
   data() {
     return {
       absolute_link_regexp: new RegExp("^https?://"),
-      image_max_width: 0,
       image_aligned_max_width: 0,
     }
   },
 
   mounted() {
-    this.image_max_width = document.documentElement.clientWidth - 202 - 30
     this.image_aligned_max_width = Math.round(document.documentElement.clientWidth / 3) + 50
     this.initPost()
   },
 
   methods: {
+    convertThumbSrcToOriginal(thumb) {
+      return thumb.replace(
+          /http(s?):\/\/i(\d+)\.imageban\.ru\/thumbs\/(\d+)\.(\d+)\.(\d+)\/([a-z\d]+)\.([a-z]+)/,
+          'http$1://i$2.imageban.ru/out/$3/$4/$5/$6.$7'
+      ).replace(
+          /https?:\/\/s(\d+)\.radikal\.ru\/i(\d+)\/(\d+)\/([a-z\d]+)\/([a-z\d]+)t\.([a-z]+)/,
+          'https://s$1.radikal.ru/i$2/$3/$4/$5.$6'
+      )
+    },
+
     fitImage(a, b) {
       return "undefined" == typeof a.naturalHeight && (a.naturalHeight = a.height, a.naturalWidth = a.width), a.width > b ? (a.height = Math.round(b / a.width * a.height), a.width = b, a.style.cursor = "move", !1) : !(a.width == b && a.width < a.naturalWidth) || (a.height = a.naturalHeight, a.width = a.naturalWidth, !1)
+    },
+
+    getOriginalImageFromLink($item) {
+      if (!$item.attr('title').search(/fastpic\.ru/)) {
+        return
+      }
+
+      const href = $item.parent('.postLink').attr('href')
+
+      if (!href || -1 === href.search(/fastpic\.ru/)) {
+        return
+      }
+
+      const src = href.replace(
+          /https?:\/\/fastpic\.ru\/view\/(\d+)\/(\d+)\/([a-z\d]+)\/((?:[a-z\d]+)([a-z\d]{2}))\.([a-z]+)\.html/,
+          'https://i$1.fastpic.ru/big/$2/$3/$5/$4.$6'
+      )
+
+      return src !== href ? src : ''
+    },
+
+    getOriginalSrc($item) {
+      const path = $item.attr('title')
+
+      let src = this.convertThumbSrcToOriginal(path)
+
+      if (src !== path) {
+        return src
+      }
+
+      let linkSrc = this.getOriginalImageFromLink($item)
+
+      return linkSrc ? linkSrc : path
     },
 
     initPost() {
@@ -50,20 +91,20 @@ export default {
 
       $('var.postImg', context).not($in_spoilers).each((index, item) => {
         let $item = $(item)
-        let src = $item.attr('title')
+        let src = this.getOriginalSrc($item)
         let cls = $item.attr('class')
 
         let $img = $(`<img src="${src}" class="${cls}" alt="pic">`)
 
-        let max_width = $item.hasClass('postImgAligned') ? this.image_aligned_max_width : this.image_max_width
+        if ($item.hasClass('postImgAligned')) {
+          $img.on('click', (e) => {
+            return this.fitImage(e.currentTarget, this.image_aligned_max_width)
+          })
 
-        $img.on('click', (e) => {
-          return this.fitImage(e.currentTarget, max_width)
-        })
-
-        $img.one('load', (e) => {
-          this.fitImage(e.currentTarget, max_width)
-        })
+          $img.one('load', (e) => {
+            this.fitImage(e.currentTarget, this.image_aligned_max_width)
+          })
+        }
 
         $item.empty().append($img)
       })
