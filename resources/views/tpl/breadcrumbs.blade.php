48baee6563fbcbc39b0ef4230c6a1c0ecb83f2ba
Sergey Pankov <vacuum@ivacuum.ru>
2018-04-16 13:27:10 +0300

    Микроразметка цепочки навигации

--- resources/views/tpl/breadcrumbs.blade.php
+++ resources/views/tpl/breadcrumbs.blade.php
@@ -1,22 +1,27 @@
 @if (!empty($breadcrumbs))
   <div class="breadcrumbs py-2 border-bottom">
-    <div class="container">
-      <nav>
-        <span class="{{ !starts_with($self, 'Acp\\') ? 'd-none d-sm-inline' : '' }}">
-          <a href="{{ $locale_uri ?: '/' }}">
-            @svg (home)
-          </a>
+    <nav class="container" itemscope itemtype="http://schema.org/BreadcrumbList">
+      <span class="{{ !starts_with($self, 'Acp\\') ? 'd-none d-sm-inline' : '' }}" itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement">
+        <a href="{{ $locale_uri ?: '/' }}" itemprop="item">
+          <meta itemprop="name" content="{{ trans('menu.home') }}">
+          <meta itemprop="position" content="1">
+          @svg (home)
+        </a>
+        @svg (angle-right)
+      </span>
+      @foreach ($breadcrumbs as $row)
+        @if (!$loop->last)
+          <span itemscope itemtype="http://schema.org/ListItem" itemprop="itemListElement">
+            <a href="{{ $locale_uri }}/{{ $row['url'] }}" itemprop="item">
+              <span itemprop="name">{{ $row['title'] }}</span>
+              <meta itemprop="position" content="{{ $loop->iteration + 1 }}">
+            </a>
+          </span>
           @svg (angle-right)
-        </span>
-        @foreach ($breadcrumbs as $row)
-          @if (!$loop->last)
-            <a href="{{ $locale_uri }}/{{ $row['url'] }}">{{ $row['title'] }}</a>
-            @svg (angle-right)
-          @else
-            {{ $row['title'] }}
-          @endif
-        @endforeach
-      </nav>
-    </div>
+        @else
+          {{ $row['title'] }}
+        @endif
+      @endforeach
+    </nav>
   </div>
 @endif
