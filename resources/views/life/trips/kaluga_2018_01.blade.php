c96cd54d4adb4f5decf3de021802c35aafa5b2b3
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-03 14:42:05 +0300

    Калуга в январе 2018

---
+++ resources/views/life/trips/kaluga_2018_01.blade.php
@@ -0,0 +1,24 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Январь. Яблоки и не думают падать.</p>
+@en
+  <p>January. Apples don't even think of falling.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5813.jpg'])
+
+@ru
+  <p>Вот почему обычно в России не тянет фотографировать даже красивые сами по себе люки. Потому что дрист.</p>
+@en
+  <p>That's why you usually don't want to take a picture of an even beautiful manhole in Russia. Because of shit.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5818.jpg'])
+
+@ru
+  <p>Сочувствие людям в очках холодной зимой.</p>
+@en
+  <p>Sympathy for people with glasses in cold winter.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5833.jpg'])
+@endsection
