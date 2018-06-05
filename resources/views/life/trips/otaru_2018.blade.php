8deed6379de0e553af35ebc27da57d4d861e917f
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-05 14:39:13 +0300

    Отару в мае 2018

---
+++ resources/views/life/trips/otaru_2018.blade.php
@@ -0,0 +1,182 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Железная дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7315.jpg'])
+
+@ru
+  <p>Железнодорожная станция.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7316.jpg'])
+
+@ru
+  <p>Двери поезда полуавтоматические (по нажатию на кнопку). Удивительно, что дверь целиковая, а не разделена на две. Открывается внутри вбок.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7318.jpg'])
+
+@ru
+  <p>Прибывает состав, который подбросит до портового города Отару「小樽」— 12:47  на табло.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7319.jpg'])
+
+@ru
+  <p>Поезд идет вдоль моря. Пейзаж за окном.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7330.jpg'])
+
+@ru
+  <p>Интерьер вагона.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7336.jpg'])
+
+@ru
+  <p>Прибываем на центральную станцию Отару.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7331.jpg'])
+
+@ru
+  <p>Автоматы с напитками часто располагаются прямо на платформах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7337.jpg'])
+
+@ru
+  <p>Железная дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7338.jpg'])
+
+@ru
+  <p>Рекламы на вокзале много, но в глаза не бросается из-за непонимания надписей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7339.jpg'])
+
+@ru
+  <p>На севере своя <a class="link" href="tokyo.2017.04.08-10#ic-card">карта широкого назначения</a> под названием Китака.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7340.jpg'])
+
+@ru
+  <p>Центральный автовокзал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7342.jpg'])
+
+@ru
+  <p>Он же снаружи.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7349.jpg'])
+
+@ru
+  <p>Проветриваемая сумка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7344.jpg'])
+
+@ru
+  <p>Обувь для примерочной кабинки в женском отделе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7346.jpg'])
+
+@ru
+  <p>Внутри платок повязать голову перед примеркой одежды.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7347.jpg'])
+
+@ru
+  <p>Температура и влажность в помещении.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7348.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7341.jpg',
+  'IMG_7350.jpg',
+  'IMG_7352.jpg',
+]])
+
+@ru
+  <p>Была линия — и нет линии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7356.jpg'])
+
+@ru
+  <p>А железная дорога осталась.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7357.jpg'])
+
+@ru
+  <p>Старые домики.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7365.jpg',
+  'IMG_7367.jpg',
+  'IMG_7368.jpg',
+]])
+
+@ru
+  <p>В городах северного острова Хоккайдо нередко встречается перевод табличек и навигации на несколько языков. Русский часто присутствует, как здесь, например.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7370.jpg'])
+
+@ru
+  <p>Мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7376.jpg'])
+
+@ru
+  <p>Надпись гласит: «10 мая (четверг) — выходной». Все прибитые деревяшки с посланиями гостей заведения.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7393.jpg'])
+
+@ru
+  <p>Красота портового города.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7394.jpg',
+  'IMG_7412.jpg',
+  'IMG_7448.jpg',
+  'IMG_7449.jpg',
+  'IMG_7450.jpg',
+]])
+
+@ru
+  <p>Подход к парку около воды. В ограде наверху якоря вместо столбиков.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7395.jpg'])
+
+@ru
+  <p>К самой воде не подойти — сейчас все ограждено забором.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7433.jpg'])
+
+@ru
+  <p>Камни в парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7443.jpg'])
+
+@ru
+  <p>Рядом футбольное поле.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7404.jpg'])
+
+@ru
+  <p>Скутер с лобовым стеклом — оно бы очень пригодилось во время катания на <a class="link" href="bali.2016">Бали</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7452.jpg'])
+
+@ru
+  <p>Больше всего в Отару манят прогуляться вдоль канала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7374.jpg'])
+
+@ru
+  <p>Пройти его можно минут за десять.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7375.jpg'])
+
+@ru
+  <p>Погуляли и хватит.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7454.jpg'])
+@endsection
