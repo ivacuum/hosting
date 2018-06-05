7fb06a5021e834e579156eca1fe49b2b8954e0cf
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-06 01:13:47 +0300

    Тояко в мае 2018

---
+++ resources/views/life/trips/toyako_2018.blade.php
@@ -0,0 +1,113 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Цветы возле входа на центральную станцию.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7636.jpg'])
+
+@ru
+  <p>Озеро.</p>
+@en
+  <p>Lake.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7644.jpg'])
+
+@ru
+  <p>Уточки.</p>
+@en
+  <p>Ducks.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7698.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7643.jpg'])
+
+@ru
+  <p>Знак велодорожки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7727.jpg'])
+
+@ru
+  <p>Есть вариант 40 минут поплавать по озеру на судне вокруг центрального озера. Последнее отправление в 16:30 — это даже раньше, чем закрываются парки и сады.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7700.jpg'])
+
+@ru
+  <p>Лавочка. Корабль завершает плавание вокруг острова.</p>
+@en
+  <p>Bench. Ship is finishing its ride around the island.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7686.jpg'])
+
+@ru
+  <p>Лавочка под деревом.</p>
+@en
+  <p>Bench under the tree.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7665.jpg'])
+
+@ru
+  <p>Последняя возможность застать сакуру в этом году.</p>
+@en
+  <p>Last chance to see cherry blossoms this year.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7704.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7637.jpg',
+  'IMG_7639.jpg',
+  'IMG_7642.jpg',
+  'IMG_7703.jpg',
+  'IMG_7738.jpg',
+]])
+
+@ru
+  <p>Надвигается красивое облако.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7730.jpg'])
+
+@ru
+  <p>Зал ожидания на автовокзале.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7735.jpg'])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7737.jpg'])
+
+@ru
+  <p>Создается ощущение, что мы едем по тротуару и скоро собьем мужика впереди.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7744.jpg'])
+
+@ru
+  <p>На экране расшифровка типов вагонов прибывающего поезда. Без предварительного бронирования мест можно направиться только в шестой и седьмой вагоны.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7769.jpg'])
+
+@ru
+  <p>Вот и поезд. Самый быстрый вариант на данном направлении. И самый дорогой. Но благодаря <a class="link" href="himeji.2017">проездному</a> о стоимости в ближайшую неделю можно не беспокоиться.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7776.jpg'])
+
+@ru
+  <p>Багажный отсек. Для открытия двери вагона достаточно ее слегка коснуться.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7780.jpg'])
+
+@ru
+  <p>Кнопка SOS расположена весьма высоко. Она выполняет роль стоп-крана.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7800.jpg'])
+@endsection
