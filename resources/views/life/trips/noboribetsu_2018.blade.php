b11ee3a093b42426141beb28937b68a4dac7099c
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-06 00:48:21 +0300

    Ноборибецу в мае 2018

---
+++ resources/views/life/trips/noboribetsu_2018.blade.php
@@ -0,0 +1,116 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Городок с одними из лучших горячих источников на острове Хоккайдо, то есть на севере Японии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7472.jpg'])
+
+@ru
+  <p>На каждом шагу отели с купальнями.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7473.jpg'])
+
+@ru
+  <p>Из главных сложностей сюда добраться — сервисы навигации не построят маршрут с помощью общественного транспорта. Максимум до одноименной железнодорожной станции, а дальше на месте находить расписание автобуса и на нем добираться до эпицентра горячих источников. Расписание лучше захватить с собой, ведь придется еще возвращаться обратно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7475.jpg'])
+
+@ru
+  <p>Подъем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7487.jpg'])
+
+@ru
+  <p>Эта шикарная долина — основа горячих источников города.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7489.jpg',
+  'IMG_7494.jpg',
+  'IMG_7529.jpg',
+  'IMG_7530.jpg',
+  'IMG_7532.jpg',
+  'IMG_7533.jpg',
+  'IMG_7537.jpg',
+]])
+
+@ru
+  <p>Так мог бы выглядеть Марс с водой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7540.jpg'])
+
+@ru
+  <p>Столбы пара. Запах сероводорода бы еще передать через экран.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7544.jpg'])
+
+@ru
+  <p>Монеты кидают, несмотря на просьбы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7545.jpg'])
+
+@ru
+  <p>Привал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7572.jpg'])
+
+@ru
+  <p>Снова подъем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7573.jpg'])
+
+@ru
+  <p>Куда на этот раз?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7578.jpg'])
+
+@ru
+  <p>На этот раз к пруду.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7579.jpg'])
+
+@ru
+  <p>Спуститься к нему тоже можно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7583.jpg'])
+
+@ru
+  <p>Рядом с прудом горячая ванна для ног.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7602.jpg'])
+
+@ru
+  <p>Все приглашаются опробовать.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7604.jpg'])
+
+@ru
+  <p>Снова осенние краски сбивают с толку, ведь на дворе весна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7616.jpg'])
+
+@ru
+  <p>Дубина с шипами.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7620.jpg'])
+
+@ru
+  <p>Столб освещения в виде березки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7621.jpg'])
+
+@ru
+  <p>Он же поближе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7625.jpg'])
+
+@ru
+  <p>Платформа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7628.jpg'])
+
+@ru
+  <p>По плану три новых города за день, поэтому логично подстраиваться под расписание самого быстрого поезда. Вот и он.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7633.jpg'])
+@endsection
