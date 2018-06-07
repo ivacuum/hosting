e01ed41bc78cb8537155efa44d2791a85b4696dd
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-07 15:49:57 +0300

    Киото в мае 2018

---
+++ resources/views/life/trips/kyoto_2018.blade.php
@@ -0,0 +1,43 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>На вокзале движуха — группы школьников то прибывают.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_8578.jpg'])
+
+@ru
+  <p>То убывают.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_8574.jpg'])
+
+@ru
+  <p>Вертикальное письмо не только для столбов хорошо подходит, но еще и для поручней эскалатора.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_8585.jpg'])
+
+@ru
+  <p>Западное письмо в горизонтальном виде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0166.jpg'])
+
+@ru
+  <p>В прошлом году правый поезд подвез в Нару.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0165.jpg'])
+
+@ru
+  <p>Полки выдвигаются вперед для удобства добавления товара.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0170.jpg'])
+
+@ru
+  <p>Центральная станция вечером.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0168.jpg'])
+
+@ru
+  <p>Педаль для разворота сидений в <a class="link" href="himeji.2017">шинкансене</a>. На каждой конечной станции уборщики разворачивают все сидения вперед по будущему направлению движения поезда.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0176.jpg'])
+@endsection
