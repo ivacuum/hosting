4e8185e4b0c530f47f5ff81f105f8a5e24be3562
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-08 01:55:27 +0300

    Иокогама в мае 2018

---
+++ resources/views/life/trips/yokohama_2018.blade.php
@@ -0,0 +1,293 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Поезд одноименной линии подбросит в портовую Иокогаму из Токио.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0186.jpg'])
+
+@ru
+  <p>Сразу же шокируют многоуровневые развязки. Пешеходу не позавидуешь искать проход среди них.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0199.jpg',
+  'IMG_0190.jpg',
+  'IMG_0187.jpg',
+]])
+
+@ru
+  <p>Привезли напитки для автоматов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0189.jpg'])
+
+@ru
+  <p>Напольная навигация в торговом центре у центральной железнодорожной станции.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0193.jpg'])
+
+@ru
+  <p>Содержимое этажей. Спасибо, лучше поищем более понятный английский вариант.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0195.jpg'])
+
+@ru
+  <p>Велопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0197.jpg'])
+
+@ru
+  <p>Автобусопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0200.jpg'])
+
+@ru
+  <p>Мотопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0425.jpg'])
+
+@ru
+  <p>В штабе автомобильной компании Ниссан.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0201.jpg',
+  'IMG_0202.jpg',
+  'IMG_0203.jpg',
+  'IMG_0204.jpg',
+  'IMG_0206.jpg',
+  'IMG_0207.jpg',
+  'IMG_0208.jpg',
+  'IMG_0209.jpg',
+  'IMG_0210.jpg',
+  'IMG_0211.jpg',
+  'IMG_0212.jpg',
+  'IMG_0213.jpg',
+]])
+
+@ru
+  <p>Люки.</p>
+@en
+  <p>Manholes.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0227.jpg',
+  'IMG_0421.jpg',
+  'IMG_0488.jpg',
+  'IMG_0476.jpg',
+]])
+
+@ru
+  <p>Панорама.</p>
+@en
+  <p>Panorama.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0232.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0250.jpg',
+  'IMG_0241.jpg',
+  'IMG_0256.jpg',
+  'IMG_0263.jpg',
+  'IMG_0266.jpg',
+  'IMG_0292.jpg',
+  'IMG_0294.jpg',
+  'IMG_0296.jpg',
+  'IMG_0323.jpg',
+  'IMG_0332.jpg',
+  'IMG_0325.jpg',
+  'IMG_0328.jpg',
+]])
+
+@ru
+  <p>Лавочка.</p>
+@en
+  <p>Bench.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0259.jpg'])
+
+@ru
+  <p>Семь ручек на двери.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0291.jpg'])
+
+@ru
+  <p>Тихий час.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0293.jpg'])
+
+@ru
+  <p>Плоский водопад, около которого купаются вороны.</p>
+@endru
+<youtube title="Yokohama Ravens, May 2018" v="3Q8x-YRPElg"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_0308.jpg'])
+
+@ru
+  <p>Карта окрестностей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0314.jpg'])
+
+@ru
+  <p>Напольная навигация.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0330.jpg'])
+
+@ru
+  <p>Красивая, разноцветная и разнообразная плитка.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0335.jpg',
+  'IMG_0489.jpg',
+  'IMG_0496.jpg',
+]])
+
+@ru
+  <p>Электровелопрокат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0350.jpg'])
+
+@ru
+  <p>Водонапорная башня.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0361.jpg'])
+
+@ru
+  <p>Масштабы прятания японцев от солнца за зонтами.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0363.jpg',
+  'IMG_0366.jpg',
+  'IMG_0368.jpg',
+  'IMG_0369.jpg',
+  'IMG_0381.jpg',
+  'IMG_0402.jpg',
+  'IMG_0409.jpg',
+  'IMG_0412.jpg',
+  'IMG_0313.jpg',
+  'IMG_0319.jpg',
+]])
+
+@ru
+  <p>Фонтаны.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0396.jpg',
+  'IMG_0397.jpg',
+  'IMG_0399.jpg',
+  'IMG_0452.jpg',
+  'IMG_0486.jpg',
+]])
+
+@ru
+  <p>Металлическая поверхность для колесиков ворот.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0427.jpg'])
+
+@ru
+  <p>Дощатая дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0431.jpg'])
+
+@ru
+  <p>Вершина парка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0432.jpg'])
+
+@ru
+  <p>Панорама.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0433.jpg'])
+
+@ru
+  <p>В парках все цветет.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0415.jpg',
+  'IMG_0437.jpg',
+  'IMG_0439.jpg',
+  'IMG_0454.jpg',
+  'IMG_0485.jpg',
+]])
+
+@ru
+  <p>Собаке проводят фотосессию.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0456.jpg'])
+
+@ru
+  <p>Не пожалели столбиков по обе стороны дороги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0458.jpg'])
+
+@ru
+  <p>Китайский квартал.</p>
+@en
+  <p>Chinatown.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0462.jpg',
+  'IMG_0465.jpg',
+  'IMG_0473.jpg',
+  'IMG_0475.jpg',
+  'IMG_0478.jpg',
+  'IMG_0479.jpg',
+]])
+
+@ru
+  <p>Дома в трех разных стилях соседствуют друг с другом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0480.jpg'])
+
+@ru
+  <p>Полицейский регулирует движение на перекрестке вместе со светофором.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0491.jpg'])
+
+@ru
+  <p>Жесткое наполнение сидений.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0495.jpg'])
+
+@ru
+  <p>Вход на железнодорожную станцию.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0498.jpg'])
+
+@ru
+  <p>Платформа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0499.jpg'])
+
+@ru
+  <p>Касса в магазине. Налоговый вычет (такс фри) можно было получить прямо за углом на этом же этаже торгового центра — после сканирования паспорта и чека автомат выплевывал положенную мелочь за вычетом пары процентов за оказание услуг. Никаких мучений в аэропорту.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0503.jpg'])
+
+@ru
+  <p>Манекен без головы. Интересная форма рук.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0506.jpg'])
+
+@ru
+  <p>Манекены в пижамах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0504.jpg'])
+
+@ru
+  <p>Количество аварийных выходов поражает.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0508.jpg'])
+
+@ru
+  <p>Масштабы упаковочного материала для покупок в магазине.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0510.jpg'])
+
+@ru
+  <p>Кина больше не будет, <s>расходимся</s> разъезжаемся.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0512.jpg'])
+@endsection
