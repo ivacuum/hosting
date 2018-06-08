e5ea87560de04d45dde8ca0efb639808d450da3c
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-08 23:37:56 +0300

    Нарва в мае 2018

---
+++ resources/views/life/trips/narva_2018.blade.php
@@ -0,0 +1,92 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Местный экспресс подвез из Таллинна в Нарву. Дешевле него можно добраться поездом РЖД, который каждый вечер делает здесь остановку.  Но сегодня задачи другие: осмотреть Нарву, перейти пешком в Россию, осмотреть Ивангород и подсесть на следующей остановке в вышеупомянутый поезд РЖД.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4286.jpg'])
+
+@ru
+  <p>Реклама встречается то на эстонском, то на русском.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4290.jpg'])
+
+@ru
+  <p>Подъезд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4291.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4292.jpg'])
+
+@ru
+  <p>Пополнение коллекции флагов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4293.jpg'])
+
+@ru
+  <p>Здание.</p>
+@en
+  <p>Building.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4297.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+<youtube title="Narva Embankment, May 2018" v="KX8hLxL3D3U"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_4300.jpg'])
+
+@ru
+  <p>Лавочка.</p>
+@en
+  <p>Bench.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4307.jpg'])
+
+@ru
+  <p>Лавочки в виде римских цифр.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4309.jpg'])
+
+@ru
+  <p>Ивангородская крепость.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4310.jpg'])
+
+@ru
+  <p>Нарвская крепость.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4316.jpg'])
+
+@ru
+  <p>Лавочка-качели.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4315.jpg'])
+
+@ru
+  <p>Модель корабля.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4320.jpg'])
+
+@ru
+  <p>Еще более внушительная и детальная модель корабля.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4322.jpg'])
+
+@ru
+  <p>Лестница наверх.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4327.jpg'])
+
+@ru
+  <p>Уличная навигация.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4337.jpg'])
+@endsection
