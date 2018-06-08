9d527c6af3d4b55abb043f98ff5f7e67b362bc7e
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-08 13:55:32 +0300

    Фудзиномия в мае 2018

---
+++ resources/views/life/trips/fujinomiya_2018.blade.php
@@ -0,0 +1,168 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Дела железнодорожные. От жилья в Токио до Фудзиномии было ехать всего-то с пятью пересадками.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0518.jpg',
+  'IMG_0522.jpg',
+  'IMG_2064.jpg',
+  'IMG_2067.jpg',
+  'IMG_2069.jpg',
+]])
+
+@ru
+  <p>Вода — жизнь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0523.jpg'])
+
+@ru
+  <p>Выход со станции. Проход слева от знаков и указателей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0525.jpg'])
+
+@ru
+  <p>Уведомление в четко размеченном месте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0526.jpg'])
+
+@ru
+  <p>Разноцветные цветы.</p>
+@en
+  <p>Colorful flowers.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0529.jpg'])
+
+@ru
+  <p>Прямая улица.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0530.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0531.jpg'])
+
+@ru
+  <p>На улице карта с подписями маркером.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0532.jpg'])
+
+@ru
+  <p>Карта по соседству выполнена в другом стиле.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0533.jpg'])
+
+@ru
+  <p>К тротуару приклеен свежий выпуск газеты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0535.jpg'])
+
+@ru
+  <p>Гора Фудзи.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1696.jpg'])
+
+@ru
+  <p>Стоп, не тот снимок. Вот же она.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0540.jpg'])
+
+@ru
+  <p>Этот товарищ умеет спугнуть клиента.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0541.jpg'])
+
+@ru
+  <p>И остаться довольным при этом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0543.jpg'])
+
+@ru
+  <p>Слева салатница под оливье.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0544.jpg'])
+
+@ru
+  <p>Кнопка переключения пешеходного светофора.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0546.jpg'])
+
+@ru
+  <p>Возможность очиститься.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0556.jpg'])
+
+@ru
+  <p>Пейзажи города.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0566.jpg',
+  'IMG_0567.jpg',
+  'IMG_1711.jpg',
+  'IMG_1716.jpg',
+  'IMG_1720.jpg',
+  'IMG_1722.jpg',
+]])
+
+@ru
+  <p>Чистая вода.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0577.jpg'])
+
+@ru
+  <p>Дом на воде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0579.jpg'])
+
+@ru
+  <p>Пачинко — так можно было бы назвать автосервис. Или это он и есть?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1721.jpg'])
+
+@ru
+  <p>Навигация.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1725.jpg'])
+
+@ru
+  <p>Велосипед.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1732.jpg'])
+
+@ru
+  <p>Тории.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1737.jpg'])
+
+@ru
+  <p>Обзорная площадка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1743.jpg'])
+
+@ru
+  <p>Автомобильные номера.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1748.jpg'])
+
+@ru
+  <p>Песики.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2045.jpg'])
+
+@ru
+  <p>Пересменка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2065.jpg'])
+
+@ru
+  <p>Курить нельзя, а болтать можно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2079.jpg'])
+
+@ru
+  <p>Закат. Время валить.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2076.jpg'])
+@endsection
