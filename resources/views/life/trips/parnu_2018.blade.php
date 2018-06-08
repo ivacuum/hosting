c9fc8e24144f7acba3c96f14add2220022ad1116
Sergey Pankov <vacuum@ivacuum.ru>
2018-06-08 23:32:15 +0300

    Пярну в мае 2018

---
+++ resources/views/life/trips/parnu_2018.blade.php
@@ -0,0 +1,187 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Неожиданное использование елок, а ведь они круглый год дают хорошую тень.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_3972.jpg'])
+
+@ru
+  <p>⌘ — знак достопримечательности.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4002.jpg'])
+
+@ru
+  <p>Судна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4014.jpg'])
+
+@ru
+  <p>Стулья над амфитеатром.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4028.jpg'])
+
+@ru
+  <p>Зарисовки на тему как себя вести в общественных местах.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4035.jpg',
+  'IMG_4042.jpg',
+  'IMG_4095.jpg',
+]])
+
+@ru
+  <p>Бетомешалка. Мешает бетон. Бригада строителей...</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4040.jpg'])
+
+@ru
+  <p>Рампы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4041.jpg'])
+
+@ru
+  <p>Качели.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4087.jpg'])
+
+@ru
+  <p>Двор.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4090.jpg'])
+
+@ru
+  <p>Аллеи.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4092.jpg',
+  'IMG_4114.jpg',
+  'IMG_4143.jpg',
+  'IMG_4144.jpg',
+  'IMG_4152.jpg',
+  'IMG_4030.jpg',
+]])
+
+@ru
+  <p>Арки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4093.jpg'])
+
+@ru
+  <p>Для нудисток отведена определенная часть пляжа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4097.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4100.jpg',
+  'IMG_4112.jpg',
+  'IMG_4113.jpg',
+]])
+
+@ru
+  <p>Гольф. В школьные годы похожие поля были в одной игре на телефон. Ходили по очереди, пытаясь загнать шар за минимальное количество ударов. Кайф.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4104.jpg'])
+
+@ru
+  <p>Коровки на лугу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4122.jpg'])
+
+@ru
+  <p>Дощатая дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4115.jpg'])
+
+@ru
+  <p>Привал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4141.jpg'])
+
+@ru
+  <p>Футбольная площадка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4148.jpg'])
+
+@ru
+  <p>Ворота выпирают за пределы поля.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4147.jpg'])
+
+@ru
+  <p>Ухоженные и разноцветные домики.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4003.jpg',
+  'IMG_4004.jpg',
+  'IMG_4005.jpg',
+  'IMG_4016.jpg',
+  'IMG_4153.jpg',
+  'IMG_4154.jpg',
+  'IMG_4155.jpg',
+  'IMG_4156.jpg',
+  'IMG_4158.jpg',
+  'IMG_4159.jpg',
+  'IMG_4165.jpg',
+]])
+
+@ru
+  <p>Забор бережно обходит дерево. Двухметровый забор едва ли найдешь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4160.jpg'])
+
+@ru
+  <p>Номер дома и год его постройки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4162.jpg'])
+
+@ru
+  <p>Автобусная остановка и лавочка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4168.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4163.jpg',
+  'IMG_4164.jpg',
+  'IMG_4178.jpg',
+  'IMG_4212.jpg',
+  'IMG_4213.jpg',
+  'IMG_4032.jpg',
+]])
+
+@ru
+  <p>Касса самообслуживания в супермаркете.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4195.jpg'])
+
+@ru
+  <p>Сквер.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4210.jpg'])
+
+@ru
+  <p>Пожарная лестница торгового центра.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4219.jpg'])
+
+@ru
+  <p>Переход между зданиями на втором этаже.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4216.jpg'])
+
+@ru
+  <p>И на третьем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4215.jpg'])
+
+@ru
+  <p>Вид на переходы с улицы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_3975.jpg'])
+@endsection
