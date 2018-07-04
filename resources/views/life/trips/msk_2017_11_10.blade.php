30acd652649952c796d8800f94fb0ffc82a98daa
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-05 00:59:42 +0300

    Перевод

--- resources/views/life/trips/msk_2017_11_10.blade.php
+++ resources/views/life/trips/msk_2017_11_10.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 @ru
   <p>Любимое место ожидания на Киевском вокзале — зал аэроэкспресса. В нем всегда есть свободные места, потому что его никто не догадывается использовать, кроме пассажиров аэроэкспресса. Из этого зала удобный проход (без подземных переходов) к платформам поездов дальнего следования.</p>
+@en
+  <p>My favorite waiting room at Kiyevsky railway station is Aeroexpress Terminal. Plenty of free seats are always in here, because mainly it is being used only by infrequent aeroexpress passengers. From here you can get to railways tracks of long distance trains without using an underpass.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4247.jpg'])
 @endsection
