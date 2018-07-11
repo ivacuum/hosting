4cac0043ca53eb2c3ecdf07713627587d153bb6b
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-11 06:05:18 +0300

    Перевод

--- resources/views/life/trips/msk_2017_11_16.blade.php
+++ resources/views/life/trips/msk_2017_11_16.blade.php
@@ -3,16 +3,22 @@
 @section('content')
 @ru
   <p>До аэропорта Внуково теперь ходит двухэтажный красавец аэроэкспресс.</p>
+@en
+  <p>New and beautiful two-storey aeroexpress to Vnukovo airport.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4260.jpg'])
 
 @ru
   <p>По сравнению с ним поезд дальнего следования на соседнем пути выглядит крохой.</p>
+@en
+  <p>Compared to it, a typical long distance train would look tiny.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4259.jpg'])
 
 @ru
   <p>Из обновок в столице еще заметны современные трамваи. На углу дома значок парикмахерской.</p>
+@en
+  <p>What's new in Moscow? Modern trams. There is a barbershop sign on the corner of the house.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_4262.jpg'])
 
