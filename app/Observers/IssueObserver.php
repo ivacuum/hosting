6e98c0bd564718ba85ce12d2bca1c26afccaf808
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-22 16:20:38 +0300

    Уведомление о новых сообщениях формы обратной связи

--- app/Observers/IssueObserver.php
+++ app/Observers/IssueObserver.php
@@ -1,7 +1,26 @@
 <?php namespace App\Observers;
 
 use App\Issue as Model;
+use Ivacuum\Generic\Services\Telegram;
 
 class IssueObserver
 {
+    protected $telegram;
+
+    public function __construct(Telegram $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+    public function created(Model $model)
+    {
+        $this->notify($model);
+    }
+
+    protected function notify(Model $model)
+    {
+        $text = "Обратная связь от {$model->email}\n{$model->title}\n".url($model->page)."\n\n".htmlspecialchars_decode($model->text, ENT_QUOTES);
+
+        $this->telegram->notifyAdmin($text);
+    }
 }
