24bc5775897137c2386abb5abd8a4c6c442a5f55
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-22 05:24:47 +0300

    Форма обратной связи на странице хабов

--- resources/views/dcpp/hubs.blade.php
+++ resources/views/dcpp/hubs.blade.php
@@ -9,28 +9,38 @@
     @en
       <p>So you have installed a DC++ client software. The next question is where to connect. To a hub! You can find our top-10 list of DC++ hubs below.</p>
     @endru
-  </div>
-</div>
 
-<ol>
-  @foreach ($hubs as $hub)
-    <li>
-      <a
-        class="link js-dcpp-hub"
-        href="{{ $hub->externalLink() }}"
-        data-action="{{ path('DcppHubClick@store', $hub->id) }}"
-      >{{ $hub->externalLink() }}</a>
-    </li>
-  @endforeach
-</ol>
+    <ol>
+      @foreach ($hubs as $hub)
+        <li>
+          <a
+            class="link js-dcpp-hub"
+            href="{{ $hub->externalLink() }}"
+            data-action="{{ path('DcppHubClick@store', $hub->id) }}"
+          >{{ $hub->externalLink() }}</a>
+        </li>
+      @endforeach
+    </ol>
 
-<div class="row">
-  <div class="col-md-6">
     @ru
       <p>Обычно клика по названию хаба достаточно, чтобы подключиться. Однако, если клик не сработал должным образом, то вы можете скопировать адрес и добавить хаб вручную в своем DC++ клиенте.</p>
     @en
       <p>Usually, it is just enough to click a link to connect to a hub. However, if it didn't work for you, you can manually copy-paste the address into your DC++ client.</p>
     @endru
+
+    <h3 class="mt-5">{{ trans('issues.create') }}</h3>
+    @ru
+      <p>Знаете хаб, достойный добавления в список? Пришлите его нам, чтобы мы пополнили страницу.</p>
+    @en
+      <p>Use the form below to send us new hubs or just to tell how to make this page better.</p>
+    @endru
+    <feedback-form
+      email="{{ Auth::user()->email ?? '' }}"
+      title="DC++ хабы"
+      action="{{ path('Issues@store') }}"
+      hide-name
+      hide-title
+    ></feedback-form>
   </div>
 </div>
 @endsection
