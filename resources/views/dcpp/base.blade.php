f1cdb203d07ba34f4e3a5961fad0558afc64b092
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-30 18:22:05 +0300

    Вроде этот код не нужен

--- resources/views/dcpp/base.blade.php
+++ resources/views/dcpp/base.blade.php
@@ -6,12 +6,6 @@
 @push('head')
 @if (App::environment() === 'production')
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-      google_ad_client: "ca-pub-7802683087624570",
-      enable_page_level_ads: true
-    });
-  </script>
 @endif
 @endpush
 
