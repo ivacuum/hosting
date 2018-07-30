59d827e8384dc73278d7b4d95582a57a8c3c2e93
Sergey Pankov <vacuum@ivacuum.ru>
2018-07-30 16:43:14 +0300

    Проба вертикального баннера

--- resources/views/dcpp/hubs.blade.php
+++ resources/views/dcpp/hubs.blade.php
@@ -42,5 +42,19 @@
       hide-title
     ></feedback-form>
   </div>
+  <div class="col-md-6">
+    @if (App::environment() === 'production')
+      <ins
+        class="adsbygoogle d-block"
+        data-ad-client="ca-pub-7802683087624570"
+        data-ad-slot="3039258747"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({})</script>
+    @elseif (App::isLocal())
+      <div class="bg-info text-white d-flex justify-content-center align-items-center">google-vertical</div>
+    @endif
+  </div>
 </div>
 @endsection
