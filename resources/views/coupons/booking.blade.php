48b28f6caa776bff7b0879ee9fb278a84673d2ad
Sergey Pankov <vacuum@ivacuum.ru>
2018-08-03 14:48:51 +0300

    Улучшение верстки страниц с купонами

--- resources/views/coupons/booking.blade.php
+++ resources/views/coupons/booking.blade.php
@@ -2,17 +2,15 @@
 
 @section('content')
 <h1>{{ $meta_title }}</h1>
-<div class="row">
-  <div class="col-md-6">
-    @ru
-      <p>Получите скидку около 1000 рублей на ваше следующее бронирование на <a class="link" href="{{ config('cfg.booking_link') }}">booking.com</a> с помощью кнопки ниже.</p>
-      <a class="btn btn-primary" href="{{ config('cfg.booking_link') }}">Получить скидку 1000 ₽</a>
-      <div class="form-help">После клика вы будете перемещены на сайт booking.com</div>
-    @en
-      <p>Cut €15 off your booking cost on <a class="link" href="{{ config('cfg.booking_link') }}">booking.com</a> just by clicking button below.</p>
-      <a class="btn btn-primary" href="{{ config('cfg.booking_link') }}">Get €15 discount</a>
-      <div class="form-help">After a click you will be redirected to booking.com</div>
-    @endru
-  </div>
+<div class="mw-600">
+  @ru
+    <p>Получите скидку около 1000 рублей на ваше следующее бронирование на <a class="link" href="{{ config('cfg.booking_link') }}">booking.com</a> с помощью кнопки ниже.</p>
+    <a class="btn btn-primary" href="{{ config('cfg.booking_link') }}">Получить скидку 1000 ₽</a>
+    <div class="form-help">После клика вы будете перемещены на сайт booking.com</div>
+  @en
+    <p>Cut €15 off your booking cost on <a class="link" href="{{ config('cfg.booking_link') }}">booking.com</a> just by clicking button below.</p>
+    <a class="btn btn-primary" href="{{ config('cfg.booking_link') }}">Get €15 discount</a>
+    <div class="form-help">After a click you will be redirected to booking.com</div>
+  @endru
 </div>
 @endsection
--- resources/views/coupons/digitalocean.blade.php
+++ resources/views/coupons/digitalocean.blade.php
@@ -2,60 +2,58 @@
 
 @section('content')
 <h1>{{ $meta_title }}</h1>
-<div class="row">
-  <div class="col-md-6">
-    @ru
-      <p>Вы когда-нибудь задумывались можно ли бесплатно попробовать услуги одного из лучших хостинг-провайдеров в мире совершенно бесплатно в течение нескольких месяцев? Да, это возможно с помощью промокодов ниже.</p>
-    @en
-      <p>Have you ever wondered if you could try one of the best web hosting for free for a few months? Yes, you can, thanks to the promo codes below.</p>
-    @endru
-    <table class="table-stats table-adaptive mb-3">
-      <thead>
-        <tr>
-          <th>@ru Промокод @en Promo Code @endru</th>
-          <th>@ru Выгода @en Benefits @endru</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><span class="badge badge-success">DO10</span> / <span class="badge badge-success">DROPLET10</span></td>
-          <td>@ru $10 для новых пользователей @en $10 credit for new users @endru</td>
-        </tr>
-        <tr>
-          <td><span class="badge badge-success">BITNAMI</span></td>
-          <td>@ru $10 на счет при регистрации @en $10 off for a new account @endru</td>
-        </tr>
-        <tr>
-          <td><span class="badge badge-success">ACTIVATE10</span></td>
-          <td>@ru $10 на счет для нынешних клиентов @en $10 credit for existing customers @endru</td>
-        </tr>
-        <tr>
-          <td><span class="badge badge-success">LOWENDBOX</span> / <span class="badge badge-success">DOPRODUCT</span></td>
-          <td>@ru $15 на счет @en $15 DigitalOcean credits @endru</td>
-        </tr>
-      </tbody>
-    </table>
-    @ru
-      <p>Пожалуйста, учтите, что DigitalOcean разрешает использовать лишь <span class="font-weight-bold">один промокод для каждой учетной записи</span>, поэтому, если вы уже использовали какой-либо промокод ранее, то новый может не сработать.</p>
+<div class="mw-600">
+  @ru
+    <p>Вы когда-нибудь задумывались можно ли бесплатно попробовать услуги одного из лучших хостинг-провайдеров в мире совершенно бесплатно в течение нескольких месяцев? Да, это возможно с помощью промокодов ниже.</p>
+  @en
+    <p>Have you ever wondered if you could try one of the best web hosting for free for a few months? Yes, you can, thanks to the promo codes below.</p>
+  @endru
+  <table class="table-stats table-adaptive mb-3">
+    <thead>
+      <tr>
+        <th>@ru Промокод @en Promo Code @endru</th>
+        <th>@ru Выгода @en Benefits @endru</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><span class="badge badge-success">DO10</span> / <span class="badge badge-success">DROPLET10</span></td>
+        <td>@ru $10 для новых пользователей @en $10 credit for new users @endru</td>
+      </tr>
+      <tr>
+        <td><span class="badge badge-success">BITNAMI</span></td>
+        <td>@ru $10 на счет при регистрации @en $10 off for a new account @endru</td>
+      </tr>
+      <tr>
+        <td><span class="badge badge-success">ACTIVATE10</span></td>
+        <td>@ru $10 на счет для нынешних клиентов @en $10 credit for existing customers @endru</td>
+      </tr>
+      <tr>
+        <td><span class="badge badge-success">LOWENDBOX</span> / <span class="badge badge-success">DOPRODUCT</span></td>
+        <td>@ru $15 на счет @en $15 DigitalOcean credits @endru</td>
+      </tr>
+    </tbody>
+  </table>
+  @ru
+    <p>Пожалуйста, учтите, что DigitalOcean разрешает использовать лишь <span class="font-weight-bold">один промокод для каждой учетной записи</span>, поэтому, если вы уже использовали какой-либо промокод ранее, то новый может не сработать.</p>
 
-      <h2>Как получить бесплатные месяцы хостинга</h2>
-      <ol>
-        <li><a class="link" href="{{ config('cfg.digitalocean_link') }}">Зарегистрируйтесь на сайте DigitalOcean</a>.</li>
-        <li>Подтвердите адрес вашей электронной почты.</li>
-        <li>Перейдите в «Billing» и примените промокод из списка выше.</li>
-        <li>Привяжите банковскую карту или заплатите $5 через PayPal для завершения процесса регистрации на DigitalOcean.</li>
-      </ol>
-    @en
-      <p>Remember that DigitalOcean only allow <span class="font-weight-bold">one promo code per account</span>, so if you have redeemed one in the past you may not be able to add another again.</p>
+    <h2>Как получить бесплатные месяцы хостинга</h2>
+    <ol>
+      <li><a class="link" href="{{ config('cfg.digitalocean_link') }}">Зарегистрируйтесь на сайте DigitalOcean</a>.</li>
+      <li>Подтвердите адрес вашей электронной почты.</li>
+      <li>Перейдите в «Billing» и примените промокод из списка выше.</li>
+      <li>Привяжите банковскую карту или заплатите $5 через PayPal для завершения процесса регистрации на DigitalOcean.</li>
+    </ol>
+  @en
+    <p>Remember that DigitalOcean only allow <span class="font-weight-bold">one promo code per account</span>, so if you have redeemed one in the past you may not be able to add another again.</p>
 
-      <h2>How to get free months of VPS</h2>
-      <ol>
-        <li><a class="link" href="{{ config('cfg.digitalocean_link') }}">Sign up at DigitalOcean</a>.</li>
-        <li>Verify your e-mail.</li>
-        <li>Move on to Billing and apply any promo code from the above.</li>
-        <li>Link your credit card or pay $5 via PayPal to complete the process.</li>
-      </ol>
-    @endru
-  </div>
+    <h2>How to get free months of VPS</h2>
+    <ol>
+      <li><a class="link" href="{{ config('cfg.digitalocean_link') }}">Sign up at DigitalOcean</a>.</li>
+      <li>Verify your e-mail.</li>
+      <li>Move on to Billing and apply any promo code from the above.</li>
+      <li>Link your credit card or pay $5 via PayPal to complete the process.</li>
+    </ol>
+  @endru
 </div>
 @endsection
