384707fbd2832c40a12a28b014d7cfbfdb14a674
Sergey Pankov <vacuum@ivacuum.ru>
2018-09-23 22:37:08 +0300

    Орел в сентябре 2018

---
+++ resources/views/life/trips/oryol_2018.blade.php
@@ -0,0 +1,208 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Сентябрь — отличная пора для поездок.</p>
+@en
+  <p>September is a great month to travel.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5278.jpg'])
+
+@ru
+  <p>Жара уже спала, краски становятся разнообразнее.</p>
+@en
+  <p>It's less hot, there are more colors.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5279.jpg'])
+
+@ru
+  <p>Дороги.</p>
+@en
+  <p>Roads.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5281.jpg',
+  'IMG_5336.jpg',
+]])
+
+@ru
+  <p>Вид на запад. Вечером можно смело приходить сюда за закатом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5282.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@en
+  <p>Places for a walk.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5284.jpg',
+  'IMG_5285.jpg',
+]])
+
+@ru
+  <p>Разноцветные гаражи.</p>
+@en
+  <p>Colorful garages.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5286.jpg'])
+
+@ru
+  <p>Дворы.</p>
+@en
+  <p>Yards.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5287.jpg',
+  'IMG_5295.jpg',
+]])
+
+@ru
+  <p>Окна разом поменяли как минимум целому подъезду.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5290.jpg'])
+
+@ru
+  <p>Кстати, о подъездах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5288.jpg'])
+
+@ru
+  <p>Целлофановые пакеты оккупировали газон.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5292.jpg'])
+
+@ru
+  <p>Дом зеленеет.</p>
+@en
+  <p>Building is getting greener.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5293.jpg'])
+
+@ru
+  <p>Лавочка у подъезда.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5294.jpg'])
+
+@ru
+  <p>Орел — рекордсмен по количеству лавок разных видов.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5297.jpg',
+  'IMG_5301.jpg',
+  'IMG_5314.jpg',
+  'IMG_5324.jpg',
+  'IMG_5329.jpg',
+]])
+
+@ru
+  <p>Высокая лавка — фаворит. Прямо трон. Если присмотреться, то девушка рядом сидит тоже на необычной формы лавке с углублением по центру.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5333.jpg'])
+
+@ru
+  <p>Ребята увлеченно рисуют.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5300.jpg'])
+
+@ru
+  <p>Зачем у окон рамки? Сосульки зимой руками собирать?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5303.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5305.jpg'])
+
+@ru
+  <p>Впереди дорожные работы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5306.jpg'])
+
+@ru
+  <p>Вот и они.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5308.jpg'])
+
+@ru
+  <p>Расписание на остановках.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5311.jpg',
+  'IMG_5331.jpg',
+]])
+
+@ru
+  <p>Высококачественный принт обложек книг. Со стороны кажется, что реально томик в стене спрятан.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5313.jpg'])
+
+@ru
+  <p>Пешеходная улица.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5315.jpg',
+  'IMG_5316.jpg',
+]])
+
+@ru
+  <p>Кинотеатр не в торговом центре. Таких по стране в процентном соотношении становится все меньше и меньше.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5317.jpg'])
+
+@ru
+  <p>Пополнение коллекции уличных часов. И флагов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5319.jpg'])
+
+@ru
+  <p>Сквер.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5320.jpg'])
+
+@ru
+  <p>Парк.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5325.jpg'])
+
+@ru
+  <p>Река Ока.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5321.jpg',
+  'IMG_5322.jpg',
+]])
+
+@ru
+  <p><s>Московская площадь</s> Пардон, мы же не в <a class="link" href="kaluga">Калуге</a>. Хотя место весьма похоже выглядит. Даже слева тоже расположена улица Московская.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5327.jpg'])
+
+@ru
+  <p>Круглосуточное разливное молоко.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5335.jpg'])
+
+@ru
+  <p>Перекус в летнем дворике.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5338.jpg'])
+
+@ru
+  <p>Уличная табличка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5341.jpg'])
+
+@ru
+  <p>Представьтесь, пожалуйста. Очень приятно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5342.jpg'])
+
+@ru
+  <p>Через кого идут все дороги? Орловцы знают ответ.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5346.jpg'])
+@endsection
