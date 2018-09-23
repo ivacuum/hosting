0688ae5cb990bf8b888f3c338ed33e63b82898a0
Sergey Pankov <vacuum@ivacuum.ru>
2018-09-23 21:45:06 +0300

    Москва в июне 2018

---
+++ resources/views/life/trips/msk_2018_06.blade.php
@@ -0,0 +1,130 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Внутренности эскалатора.</p>
+@en
+  <p>Inside the escalator.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4672.jpg',
+  'IMG_4674.jpg',
+]])
+
+@ru
+  <p>В подземном переходе продается мой любимый напиток — Милкис.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4677.jpg'])
+
+@ru
+  <p>Улицы. Кайф смотреть на дома с лаконичными вывесками — дизайн-код города работает на ура.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4682.jpg',
+  'IMG_4686.jpg',
+]])
+
+@ru
+  <p>Вход в метро.</p>
+@en
+  <p>Entrance to the metro.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4685.jpg'])
+
+@ru
+  <p>Планшет.</p>
+@en
+  <p>Tablet.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4690.jpg'])
+
+@ru
+  <p>Макет парка.</p>
+@en
+  <p>Park layout.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4704.jpg'])
+
+@ru
+  <p>Смайлик. Камера телефона сходила с ума при попытке сделать фотографию — постоянно выскакивала считанная с кодов информация.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4712.jpg'])
+
+@ru
+  <p>Россия в одной фотографии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4724.jpg'])
+
+@ru
+  <p>Зарядья много не бывает. Так я оправдывал два визита в него за один день.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4725.jpg',
+  'IMG_4735.jpg',
+  'IMG_4748.jpg',
+  'IMG_4769.jpg',
+]])
+
+@ru
+  <p><s>Распыление</s> Опрыскивание растений.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4754.jpg'])
+
+@ru
+  <p>Ночью и без метро можно попасть в нужную часть города. Правда, не так быстро и часто.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4756.jpg'])
+
+@ru
+  <p>Футбольная тематика по мере приближения к центру выражается все ярче.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4680.jpg'])
+
+@ru
+  <p>Эпицентр на улице Никольской. Очень «продумански» выбрал день для поездки в столицу — когда в ближайшие плюс-минус два дня в Москве не было матчей. В будущем перед выбором нужно консультироваться с расписанием игр.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4759.jpg'])
+
+@ru
+  <p>Более половины фасадов завесили тканью.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4761.jpg'])
+
+@ru
+  <p>Вывеска пролезла через занавес.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4764.jpg'])
+
+@ru
+  <p>Дело к закату.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4767.jpg',
+  'IMG_4772.jpg',
+  'IMG_4778.jpg',
+]])
+
+@ru
+  <p>Вечерние танцы.</p>
+@en
+  <p>Evening dances.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4781.jpg'])
+
+@ru
+  <p>Ночная подсветка.</p>
+@en
+  <p>Night illumination.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4789.jpg'])
+
+@ru
+  <p>Велопрокат на набережной.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4793.jpg'])
+
+@ru
+  <p>Два пешеходных светофора совсем меня запутали. По правому можно переходить только прямо, по левому — в любую сторону.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4795.jpg'])
+@endsection
