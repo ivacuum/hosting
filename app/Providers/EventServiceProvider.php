53b394a2211c4efa77237a491fbfbc8fc4c1fb63
Sergey Pankov <vacuum@ivacuum.ru>
2018-09-30 12:31:05 +0300

    Оформление кода

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,19 +1,21 @@
 <?php namespace App\Providers;
 
+use App\Events;
+use App\Listeners;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-        'App\Events\ChatMessageCreated' => ['App\Listeners\TelegramChatMessage'],
-        'App\Events\CommentPublished' => ['App\Listeners\NotifyUsersAboutComment'],
-        'App\Events\DomainWhoisUpdated' => ['App\Listeners\EmailWhoisChanges'],
-        'App\Events\IssueCreated' => ['App\Listeners\TelegramIssue'],
-        'App\Events\TypoReceived' => ['App\Listeners\TelegramTypo'],
+        Events\ChatMessageCreated::class => [Listeners\TelegramChatMessage::class],
+        Events\CommentPublished::class => [Listeners\NotifyUsersAboutComment::class],
+        Events\DomainWhoisUpdated::class => [Listeners\EmailWhoisChanges::class],
+        Events\IssueCreated::class => [Listeners\TelegramIssue::class],
+        Events\TypoReceived::class => [Listeners\TelegramTypo::class],
 
         'Illuminate\Auth\Events\Login' => [
-            'App\Listeners\UserEmptySalt',
-            'App\Listeners\LogUserLogin',
+            Listeners\UserEmptySalt::class,
+            Listeners\LogUserLogin::class,
         ],
 
         'Illuminate\Foundation\Events\LocaleUpdated' => [
