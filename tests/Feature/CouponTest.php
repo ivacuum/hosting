80eebd3806e1a7628e7eed07f64a6c0fb8c86778
Sergey Pankov <vacuum@ivacuum.ru>
2018-10-28 01:16:45 +0300

    Тесты страниц с промокодами

---
+++ tests/Feature/CouponTest.php
@@ -0,0 +1,47 @@
+<?php namespace Tests\Feature;
+
+use App\Mail\FirstvdsPromocode;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CouponTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testFirstvdsPost()
+    {
+        $email = 'mail@example.com';
+
+        \Mail::fake();
+
+        $this->post(action('Coupons@firstvdsPost'), compact('email'))
+            ->assertStatus(302);
+
+        \Mail::assertQueued(FirstvdsPromocode::class, function (FirstvdsPromocode $mail) use ($email) {
+            return $mail->hasTo($email);
+        });
+    }
+
+    /**
+     * @dataProvider pages
+     * @param string $url
+     */
+    public function testPages(string $url)
+    {
+        $this->get($url)
+            ->assertStatus(200);
+    }
+
+    public function pages()
+    {
+        return [
+            ['/promocodes-coupons'],
+            ['/promocodes-coupons/airbnb'],
+            ['/promocodes-coupons/booking'],
+            ['/promocodes-coupons/digitalocean'],
+            ['/promocodes-coupons/drimsim'],
+            ['/promocodes-coupons/firstvds'],
+            ['/promocodes-coupons/timeweb'],
+        ];
+    }
+}
