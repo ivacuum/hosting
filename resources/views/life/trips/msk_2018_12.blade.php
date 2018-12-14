cf1197b707d293dfa9336b83b664a8f1655da680
Sergey Pankov <vacuum@ivacuum.ru>
2018-12-15 02:16:25 +0300

    Москва в декабре 2018

---
+++ resources/views/life/trips/msk_2018_12.blade.php
@@ -0,0 +1,59 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Конечная. Поезд дальше не идет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5685.jpg'])
+
+@ru
+  <p>Новенький состав.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5687.jpg'])
+
+@ru
+  <p>Здорово для встречи выбирать выход по номеру. Еще бы навигаторы подсказывали какой выход быстрее приведет к цели.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5690.jpg'])
+
+@ru
+  <p>Будущие ларьки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5692.jpg'])
+
+@ru
+  <p>Где это мы?</p>
+@en
+  <p>Where are we?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5695.jpg'])
+
+@ru
+  <p>А вот где.</p>
+@en
+  <p>Here we are.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5697.jpg'])
+
+@ru
+  <p>Впервые вижу, чтобы у временного сооружения был адрес.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5699.jpg'])
+
+@ru
+  <p>Улица красных фонарей.</p>
+@en
+  <p>Red lights street.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5701.jpg'])
+
+@ru
+  <p>Дорожные знаки уменьшаются в размерах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5703.jpg'])
+
+@ru
+  <p>В лапшичной автомат по продаже японских напитков. Сразу манит взгляд полюбившийся виноград, но тут единственный виноградный напиток с комочками, которые любят перекрывать поток жидкости, когда пьешь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_5704.jpg'])
+@endsection
