d00522342497b914ae66d6d592e7648c4e8927b1
Sergey Pankov <vacuum@ivacuum.ru>
2019-02-03 14:46:56 +0300

    Калининград 2015 HD

--- resources/views/life/trips/kaliningrad_2015.blade.php
+++ resources/views/life/trips/kaliningrad_2015.blade.php
@@ -6,14 +6,14 @@
 @en
   <p>The city which rare sees snow.</p>
 @endru
-@include('tpl.pic', ['pic' => 'IMG_0076.jpg'])
+@include('tpl.pic-2x', ['pic' => 'IMG_0204.jpg'])
 
 @ru
   <p>В Калининграде полно мест для прогулок, набережных так и вовсе несколько.</p>
 @en
   <p>There are lots of places for a walk and embankments in Kaliningrad.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1446.jpg',
   'IMG_1454.jpg',
   'IMG_1448.jpg',
@@ -27,7 +27,7 @@
 @en
   <p>Streets.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1502.jpg',
   'IMG_1501.jpg',
   'IMG_1503.jpg',
@@ -42,14 +42,14 @@
 @ru
   <p>На Куршскую косу легко попасть, но непросто ее покинуть. Если вы, конечно, не на своей машине. Общественный транспорт отсюда ходит лишь несколько раз в день. По пути обратно на дорогу вообще выбежал кабан из леса, что, впрочем, не помешало водителю продолжить путь.</p>
 @endru
-@include('tpl.pic', ['pic' => 'IMG_1469.jpg'])
+@include('tpl.pic-2x', ['pic' => 'IMG_1469.jpg'])
 
 @ru
   <p>Дюны.</p>
 @en
   <p>Dunes.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1477.jpg',
   'IMG_1474.jpg',
   'IMG_1479.jpg',
@@ -60,7 +60,7 @@
 @en
   <p>Baltic sea.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1489.jpg',
   'IMG_1488.jpg',
   'IMG_1482.jpg',
@@ -74,19 +74,19 @@
 @ru
   <p>Неподалеку от японского консульства встретилась мемориальная скамейка.</p>
 @endru
-@include('tpl.pic', ['pic' => 'IMG_1508.jpg'])
+@include('tpl.pic-2x', ['pic' => 'IMG_1508.jpg'])
 
 <a id="house_number"></a>
 @ru
   <p>В Калининграде нумеруются подъезды, а не дома. <a class="link" href="msk.2014.12#house_number" class="link">Сравните с Москвой</a>. Номер дома лишь подсказывает какие подъезды можно в нем встретить. Нумерация квартир в каждом из них начинается заново с единицы. Поэтому на картах того же Яндекса можно видеть, что дом с картинки ниже <a href="http://maps.yandex.ru/-/CVGCzOK3" class="link">подписан несколько раз</a> — по количеству подъездов.</p>
 @endru
-@include('tpl.pic', ['pic' => 'IMG_1505.jpg'])
+@include('tpl.pic-2x', ['pic' => 'IMG_1505.jpg'])
 
 <a id="trees"></a>
 @ru
   <p>Что за очаги листьев на деревьях зимой?</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1500.jpg',
   'IMG_1468.jpg',
 ]])
@@ -94,7 +94,7 @@
 @ru
   <p>В той же Калуге Луна предстает совершенно иначе — она больше и ярко белая, а в Калининграде маленькая, с мягким желтым ореолом. На снимке же это просто банан, <a class="link" href="spb.2014.09#banana">как в Питере</a>.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1456.jpg',
   'IMG_1457.jpg',
 ]])
@@ -104,7 +104,7 @@
 @en
   <p>Roads.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1447.jpg',
   'IMG_1491.jpg',
   'IMG_1507.jpg',
@@ -117,7 +117,7 @@
 @ru
   <p>К отсутствию снега так привыкаешь, что потом тяжеловато возвращаться в заснеженную Москву. Или это в Калининграде так чисто?.. Ан, нет, показалось.</p>
 @endru
-@include('tpl.fotorama', ['pics' => [
+@include('tpl.fotorama-2x', ['pics' => [
   'IMG_1518.jpg',
   'IMG_1511.jpg',
 ]])
