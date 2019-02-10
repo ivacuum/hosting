d45eb26ff5169587f649d747e9e822ed7e0e1ac1
Sergey Pankov <vacuum@ivacuum.ru>
2019-02-10 03:32:22 +0300

    Пусан в январе 2019

---
+++ resources/views/life/trips/busan_2019.blade.php
@@ -0,0 +1,311 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Железнодорожный вокзал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6577.jpg'])
+
+@ru
+  <p>Вид из окна в пути.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6587.jpg'])
+
+@ru
+  <p>Если грамотно выбрать высокоскоростной поезд KTX с минимальным количеством остановок, то в Пусане можно оказаться всего через 2 часа 15 минут, а это, на минуточку, другой конец страны.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6589.jpg'])
+
+@ru
+  <p>Подсказка к какому вагону выйдешь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6592.jpg'])
+
+@ru
+  <p>Подъезд к платформам скромного автовокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6593.jpg'])
+
+@ru
+  <p><s>Китайский</s> Российский квартал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6594.jpg'])
+
+@ru
+  <p>Пусан — рельефный город. Крайне рельефный. Зато крутой подъем вознаграждается красивыми панорамными видами.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6600.jpg',
+  'IMG_6604.jpg',
+  'IMG_6605.jpg',
+  'IMG_6607.jpg',
+  'IMG_6612.jpg',
+  'IMG_6615.jpg',
+  'IMG_6617.jpg',
+  'IMG_6666.jpg',
+  'IMG_6619.jpg',
+  'IMG_6625.jpg',
+]])
+
+@ru
+  <p>Рисунки на стенах загляденье по всему городу.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6601.jpg',
+  'IMG_6603.jpg',
+  'IMG_6626.jpg',
+  'IMG_6628.jpg',
+  'IMG_6665.jpg',
+  'IMG_6669.jpg',
+  'IMG_6673.jpg',
+  'IMG_6679.jpg',
+  'IMG_6680.jpg',
+]])
+
+@ru
+  <p>Собака из корейского алфавита.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6668.jpg'])
+
+@ru
+  <p>Автобусная остановка.</p>
+@en
+  <p>Bus stop.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6623.jpg'])
+
+@ru
+  <p>Обзорная площадка.</p>
+@en
+  <p>Observation deck.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6630.jpg'])
+
+@ru
+  <p>Высотные виды.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6632.jpg',
+  'IMG_6636.jpg',
+]])
+
+@ru
+  <p>Парк наверху.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6638.jpg',
+  'IMG_6639.jpg',
+  'IMG_6643.jpg',
+  'IMG_6645.jpg',
+  'IMG_6648.jpg',
+  'IMG_6649.jpg',
+]])
+
+@ru
+  <p>Очень непривычно из лесной обстановки сквозь деревья видеть мегаполис.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6650.jpg',
+  'IMG_6651.jpg',
+]])
+
+@ru
+  <p>Крутая лестница вниз и вид напоминают <a class="link" href="barcelona.2016#stairs">Барселону</a>.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6654.jpg',
+  'IMG_6653.jpg',
+]])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6657.jpg'])
+
+@ru
+  <p>Стена перед жилым домом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6658.jpg'])
+
+@ru
+  <p>Объемная карта района.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6660.jpg'])
+
+@ru
+  <p>Штаб-квартира Твиттера.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6662.jpg'])
+
+@ru
+  <p>Столбы обшиты материалом с пейзажем вокруг.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6667.jpg'])
+
+@ru
+  <p>Загадочные нити.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6674.jpg'])
+
+@ru
+  <p>Соединительные автомобили.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6675.jpg'])
+
+@ru
+  <p>Жилой район.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6676.jpg',
+  'IMG_6677.jpg',
+]])
+
+@ru
+  <p>Площадь.</p>
+@en
+  <p>Square.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6683.jpg'])
+
+@ru
+  <p>Вроде Новый год, а украшений минимум. Был взят квест найти наряженные елки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6685.jpg'])
+
+@ru
+  <p>У туалета есть адрес. Удобно для заказа бумаги с доставкой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6686.jpg'])
+
+@ru
+  <p>На схеме в вагоне метро показано на каких станциях проще всего пересесть в поезд в обратном направлении, где авто и жд вокзалы, паромы и где можно пересесть на высокоскоростной поезд KTX и SRT.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6690.jpg'])
+
+@ru
+  <p>Внимательный читатель без труда найдет елку с крестом вместо звезды.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6693.jpg'])
+
+@ru
+  <p>Корейцы стремятся к небесам.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6694.jpg'])
+
+@ru
+  <p>Велодорожка и светофор для велосипедистов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6696.jpg'])
+
+@ru
+  <p>Вход в парк.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6697.jpg'])
+
+@ru
+  <p>Косяк рыб.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6699.jpg'])
+
+@ru
+  <p>Вода есть — просто она прозрачная.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6702.jpg'])
+
+@ru
+  <p>Во время прогулки часто можно заметить, что покрытие под ногами меняется с одного на другое. Это, например, прорезиненное и хорошо подходит для бега.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6705.jpg'])
+
+@ru
+  <p>Напольные указатели улиц и домов.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6707.jpg',
+  'IMG_6713.jpg',
+]])
+
+@ru
+  <p>Землю красиво прикрыли.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6709.jpg'])
+
+@ru
+  <p>Куб — вход в громадный парк, в котором можно не один час плутать в попытке обойти его весь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6710.jpg'])
+
+@ru
+  <p>Продолжая тему жизнерадостных граффити и рисунков.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6715.jpg'])
+
+@ru
+  <p>Сам парк. Ничтожно малая его часть.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6716.jpg',
+  'IMG_6734.jpg',
+]])
+
+@ru
+  <p>Совушки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6718.jpg'])
+
+@ru
+  <p>Фото на электрощите.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6720.jpg'])
+
+@ru
+  <p>Водопад у дамбы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6732.jpg'])
+
+@ru
+  <p>Центральный вокзал. И третья елка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6740.jpg'])
+
+@ru
+  <p>Закат.</p>
+@en
+  <p>Sunset.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6744.jpg',
+  'IMG_6746.jpg',
+]])
+
+@ru
+  <p>Каждый посетитель может подзарядиться.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6749.jpg'])
+
+@ru
+  <p>Реновация. Лайнер. Мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6754.jpg'])
+
+@ru
+  <p>Вагон поезда KTX.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6756.jpg'])
+
+@ru
+  <p>Можно подумать, что у мужчины в ряду справа экран в спинке сиденья, но это лишь его планшет, хитро зацепленный за сетку чехлом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6759.jpg'])
+
+@ru
+  <p>Подробный рассказ какие услуги в каких вагонах доступны. Огнетушитель присутствует в каждом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6761.jpg'])
+
+@ru
+  <p>Пусан — южный портовый город. В нем ощутимо теплее Сеула — можно было даже без шапки ходить в январе. Не раз задавался вопросом почему сразу после <a class="link" href="psy.2018">концерта</a> не двинул сюда. Работать было не принципиально откуда, поэтому северный Сеул можно было благополучно поменять на солнечный Пусан. Будет на заметку для будущих планирований.</p>
+@endru
+@endsection
