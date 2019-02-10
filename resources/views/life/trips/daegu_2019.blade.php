e012d7addf6f7e900a8f941f611eb2b86f4d1889
Sergey Pankov <vacuum@ivacuum.ru>
2019-02-10 03:32:45 +0300

    Тэгу в январе 2019

---
+++ resources/views/life/trips/daegu_2019.blade.php
@@ -0,0 +1,320 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Стрелки в предпоследней колонке на табло подсказывают в каком направлении двигаться к нужному поезду.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6770.jpg'])
+
+@ru
+  <p>Путь из Сеула до Тэгу на высокоскоростном поезде занимает около полутора часов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6771.jpg'])
+
+@ru
+  <p>Центральный вокзал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6775.jpg'])
+
+@ru
+  <p>Медицинский крест поместили внутрь символа ㅇ слова аптека 약국.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6780.jpg'])
+
+@ru
+  <p>Около беседки не курить.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6785.jpg'])
+
+@ru
+  <p>Длинная набережная.</p>
+@en
+  <p>Long embankment.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6787.jpg',
+  'IMG_6788.jpg',
+  'IMG_6794.jpg',
+  'IMG_6796.jpg',
+  'IMG_6799.jpg',
+  'IMG_6806.jpg',
+  'IMG_6814.jpg',
+]])
+
+@ru
+  <p>Река.</p>
+@en
+  <p>River.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6791.jpg'])
+
+@ru
+  <p>Пешеходный мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6792.jpg'])
+
+@ru
+  <p><s>Торпеды</s> лодки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6798.jpg'])
+
+@ru
+  <p>Жилые дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6800.jpg'])
+
+@ru
+  <p>Солнечные панели на крышах домов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6801.jpg'])
+
+@ru
+  <p>Растяжка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6802.jpg'])
+
+@ru
+  <p>Мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6805.jpg'])
+
+@ru
+  <p>Платный телефон. Принимает карту T-money.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6808.jpg'])
+
+@ru
+  <p>Рыбалка.</p>
+@en
+  <p>Fishing.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6811.jpg'])
+
+@ru
+  <p>Символическое ограждение.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6812.jpg'])
+
+@ru
+  <p>Лавка-качалка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6815.jpg'])
+
+@ru
+  <p>Присядем, насладимся.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6817.jpg'])
+
+@ru
+  <p>Велопарковка над станцией метро. Можно было подумать, что это велопрокат, но кто ж делает велопрокат с настолько разнообразными моделями велосипедов?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6820.jpg'])
+
+@ru
+  <p>Обилие автобусных маршрутов около станции метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6822.jpg'])
+
+@ru
+  <p>Велопрокат на станции метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6824.jpg'])
+
+@ru
+  <p>При ближайшем рассмотрении оказывается, что велопрокат бесплатный.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6826.jpg'])
+
+@ru
+  <p>В метро телефон для экстренных звонков.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6828.jpg'])
+
+@ru
+  <p>Нанесение разметки на перекрестке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6829.jpg'])
+
+@ru
+  <p>Забота о дверях автомобиля, чтобы не бились при открытии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6833.jpg'])
+
+@ru
+  <p>Полицейский участок через дорогу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6834.jpg'])
+
+@ru
+  <p>Переулок.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6836.jpg'])
+
+@ru
+  <p>Самый разгар зимы на дворе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6837.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6838.jpg',
+  'IMG_6850.jpg',
+  'IMG_6852.jpg',
+  'IMG_6853.jpg',
+  'IMG_6856.jpg',
+  'IMG_6860.jpg',
+  'IMG_6933.jpg',
+]])
+
+@ru
+  <p>Дороги. Боковые двери автомобилей корейцы поголовно тонируют.</p>
+@en
+  <p>Roads.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6839.jpg',
+  'IMG_6872.jpg',
+  'IMG_6783.jpg',
+]])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6841.jpg'])
+
+@ru
+  <p>Хрю.</p>
+@en
+  <p>Oink.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6843.jpg'])
+
+@ru
+  <p>Возможность поиграть в баскетбол в парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6848.jpg'])
+
+@ru
+  <p>Раскрашенный электрощиток.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6858.jpg'])
+
+@ru
+  <p><s>Галерея</s> просто подземный переход.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6862.jpg'])
+
+@ru
+  <p>Высотные виды.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6864.jpg',
+  'IMG_6866.jpg',
+]])
+
+@ru
+  <p>Электромобиль на зарядке.</p>
+@en
+  <p>Electric car is being charged.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6868.jpg'])
+
+@ru
+  <p>Город вокруг гор. Любители хайкинга в шоколаде. Понадобится далеко не один день, чтобы обойти все дорожки, обозначенные на карте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6876.jpg'])
+
+@ru
+  <p>У подножия гор.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6870.jpg',
+  'IMG_6874.jpg',
+  'IMG_6881.jpg',
+]])
+
+@ru
+  <p>Теннисные корты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6883.jpg'])
+
+@ru
+  <p>Канатная дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6887.jpg'])
+
+@ru
+  <p>Обзорная площадка и панорамы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6898.jpg',
+  'IMG_6900.jpg',
+  'IMG_6902.jpg',
+  'IMG_6904.jpg',
+  'IMG_6905.jpg',
+]])
+
+@ru
+  <p>Около нижней станции канатной дороги уличные тренажеры, пользующиеся спросом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6911.jpg'])
+
+@ru
+  <p>Какой автобус довезет до вокзала или хотя бы до метро? <a class="link" href="/news/271">Симка для путешествий</a> предоставила интернет, который помог выяснить.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6915.jpg'])
+
+@ru
+  <p>В данном случае выручил 750-й маршрут. На входе нужно либо оплатить проезд, либо сделать чек-ин с помощью карты T-money (с картой еще на выходе терминал для чек-аута). Водители автобусов ведут себя на дороге весьма агрессивно, сигналя всем задумавшимся и мчась между остановками. Если не нажать на сигнал стоп в салоне, то велика вероятность, что ваша остановка будет пропущена.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6931.jpg'])
+
+@ru
+  <p>Метро.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_6778.jpg',
+  'IMG_6934.jpg',
+]])
+
+@ru
+  <p>Схема красной ветки метро. Все пересадки в центре.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6776.jpg'])
+
+@ru
+  <p>Повторители 4G. Стоит отметить, что по всей стране редко когда прием сигнала сотовой связи не был максимальным. Особенно в метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6936.jpg'])
+
+@ru
+  <p>Вечером елка обратила на себя внимание.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6938.jpg'])
+
+@ru
+  <p>Вокзал вечером.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6941.jpg'])
+
+@ru
+  <p>Ожидание возвращения в <a class="link" href="seoul.2018">Сеул</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6942.jpg'])
+
+@ru
+  <p>Лавки на платформе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6944.jpg'])
+
+@ru
+  <p>Шестнадцатый вагон напротив соответствующего указателя. Подсказка 1A&nbsp;&rarr;&nbsp;15D гласит, что внутри вагона нумерация рядов идет по возрастанию.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_6948.jpg'])
+@endsection
