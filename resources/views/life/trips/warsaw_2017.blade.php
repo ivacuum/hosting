78658a6171bb914fedd4cdcedb49cb2e2bc8580a
Sergey Pankov <vacuum@ivacuum.ru>
2019-02-16 11:27:17 +0300

    Перевод

--- resources/views/life/trips/warsaw_2017.blade.php
+++ resources/views/life/trips/warsaw_2017.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 @ru
   <p>Трехчасовое ожидание на границе закончилось успешным ее прохождением.</p>
+@en
+  <p>Three-hour waiting at the border ended up with a successful crossing.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1926.jpg'])
 
@@ -15,51 +17,71 @@
 
 @ru
   <p>Часы перевелись на час назад, солнце уже зашло. Постоянно вдоль дороги встречались аисты с гнездами на столбах.</p>
+@en
+  <p>Clocks are set one hour back. The sun has set. Constantly there were storks with nests on the top of the poles along the road.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1929.jpg'])
 
 @ru
   <p>Проехать хотелось как можно больше — в идеале всю Польшу одним рывком.</p>
+@en
+  <p>The dream was to drive as much as possible—ideally the whole Poland in one go.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1935.jpg'])
 
 @ru
   <p>Вместо литров на колонке кубические дециметры. Бензин заметно дороже — есть резон по полной заправиться в Беларуси до границы.</p>
+@en
+  <p>Cubic decimeters instead of liters at the pump. The gas is considerably more expensive, so it makes sense to get full tank before the border in Belarus.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1936.jpg'])
 
 @ru
   <p>Одним рывком проскочить страну не хватило сил. Путь был продолжен после пятичасового перерыва в Варшаве на сон в машине.</p>
+@en
+  <p>We couldn't drive through the country in one go. Journey continued after a five-hour sleep right in the car in Warsaw.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1944.jpg'])
 
 @ru
   <p>Дорога в сторону Германии.</p>
+@en
+  <p>The road towards Germany.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1945.jpg'])
 
 @ru
   <p>Машины позади.</p>
+@en
+  <p>Cars behind.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1948.jpg'])
 
 @ru
   <p>Время от времени шел дождь.</p>
+@en
+  <p>It rained from time to time.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1951.jpg'])
 
 @ru
   <p>За окном поля и деревья.</p>
+@en
+  <p>Fields and trees outside the window.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1955.jpg'])
 
 @ru
-  <p>Место отдыха вдоль трассы в Польше.</p>
+  <p>Место для отдыха и перекуса вдоль трассы в Польше.</p>
+@en
+  <p>Place to rest and take food along the road in Poland.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1956.jpg'])
 
 @ru
   <p>В Германии более того каждое место отдыха имеет свое название.</p>
+@en
+  <p>Each place like that has its own name in Germany.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_1965.jpg'])
 @endsection
