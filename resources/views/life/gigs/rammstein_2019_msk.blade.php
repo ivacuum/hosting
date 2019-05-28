2e32d09d52d4cfe0b2b1d2361eec94466153937c
Sergey Pankov <vacuum@ivacuum.ru>
2019-05-28 12:48:47 +0300

    Актуальный сетлист

--- resources/views/life/gigs/rammstein_2019_msk.blade.php
+++ resources/views/life/gigs/rammstein_2019_msk.blade.php
@@ -1,30 +1,50 @@
 @extends('life.gigs.base')
 
 @section('content')
-<div class="row">
-  <div class="col-md-7">
-    @ru
-      <div class="mb-2">
-        @include('tpl.gig-countdown', ['show_datetime' => '2019-07-29 19:00:00'])
-      </div>
-      <p>Новый тур группы Rammstein. Это будет уже шестнадцатое выступление по счету в России. Сетлист будет известен вместе с первым концертом в Гельзенкирхене 27 мая. Незадолго до этого (17 мая) был выпущен новый студийный альбом под названием Rammstein.</p>
-      <p>Место проведения концерта: большая спортивная арена Лужники.</p>
-      <p>
-        Билеты в продаже с 8 ноября 10:00 по Москве.
-        @if (!now()->gte('2018-11-08 10:00:00'))
-          Цены стоит ожидать около 100 евро, а также сервисный сбор в размере 10%.
-        @endif
-      </p>
-      <p>Ждем-с.</p>
-      <p>
-        <a class="btn btn-primary" href="http://tci.ru/rammstein2019/">Купить билет</a>
-        <a class="btn btn-default" href="https://vk.com/rammstein_2019_msk">Группа ВК</a>
-      </p>
-    @endru
+@ru
+  <div class="mb-2">
+    @include('tpl.gig-countdown', ['show_datetime' => '2019-07-29 19:00:00'])
   </div>
-  <div class="col-md-5">
-    <div class="img-container">
-      <img src="https://life.ivacuum.org/gigs/rammstein.2019.07.29.png">
+  <p>Новый тур группы Rammstein. Это будет уже шестнадцатое выступление по счету в России. В этот раз концерт в поддержку нового студийного альбома под названием Rammstein, выпущенного 17 мая. Место проведения концерта: большая спортивная арена Лужники.</p>
+  <p>Ждем-с.</p>
+  <p>
+    <a class="btn btn-primary" href="http://tci.ru/rammstein2019/">Купить билет</a>
+    <a class="btn btn-default" href="https://vk.com/rammstein_2019_msk">Группа ВК</a>
+  </p>
+@endru
+  <div class="row">
+    <div class="col-md-7">
+      <div class="mb-1">@ru Ожидаемый сетлист: @endru</div>
+      <ol>
+        <li>Was ich liebe</li>
+        <li>Links 2-3-4</li>
+        <li>Sex</li>
+        <li>Tattoo</li>
+        <li>Sehnsucht</li>
+        <li>Zeig dich</li>
+        <li>Mein Herz brennt</li>
+        <li>Puppe</li>
+        <li>Heirate mich</li>
+        <li>Diamant</li>
+        <li>Deutschland (RMX by Richard Z. Kruspe)</li>
+        <li>Deutschland</li>
+        <li>Radio</li>
+        <li>Mein Teil</li>
+        <li>Du hast</li>
+        <li>Sonne</li>
+        <li>Ohne dich</li>
+        <li>Engel</li>
+        <li>Ausländer</li>
+        <li>Du riechst so gut</li>
+        <li>Pussy</li>
+        <li>Rammstein</li>
+        <li>Ich will</li>
+      </ol>
+    </div>
+    <div class="col-md-5">
+      <div class="img-container">
+        <img src="https://life.ivacuum.org/gigs/rammstein.2019.07.29.png">
+      </div>
     </div>
   </div>
 </div>
--- resources/views/life/gigs/rammstein_2019_spb.blade.php
+++ resources/views/life/gigs/rammstein_2019_spb.blade.php
@@ -1,26 +1,45 @@
 @extends('life.gigs.base')
 
 @section('content')
+@ru
+  <div class="mb-2">
+    @include('tpl.gig-countdown', ['show_datetime' => '2019-08-02 19:00:00'])
+  </div>
+  <p>Новый тур группы Rammstein. Это будет уже семнадцатое выступление по счету в России и шестое в СПб. В этот раз концерт в поддержку нового студийного альбома под названием Rammstein, выпущенного 17 мая. Место проведения концерта: стадион «Санкт-Петербург».</p>
+  <p>Ждем-с.</p>
+  <p>
+    <a class="btn btn-primary" href="http://tci.ru/rammstein2019/">Купить билет</a>
+    <a class="btn btn-default" href="https://vk.com/rammstein2019">Группа ВК</a>
+  </p>
+@endru
 <div class="row">
   <div class="col-md-7">
-    @ru
-      <div class="mb-2">
-        @include('tpl.gig-countdown', ['show_datetime' => '2019-08-02 19:00:00'])
-      </div>
-      <p>Новый тур группы Rammstein. Это будет уже семнадцатое выступление по счету в России и шестое в СПб. Сетлист будет известен вместе с первым концертом в Гельзенкирхене 27 мая. Незадолго до этого (17 мая) был выпущен новый студийный альбом под названием Rammstein.</p>
-      <p>Место проведения концерта: стадион «Санкт-Петербург».</p>
-      <p>
-        Билеты в продаже с 8 ноября 10:00 по Москве.
-        @if (!now()->gte('2018-11-08 10:00:00'))
-          Цены стоит ожидать около 100 евро, а также сервисный сбор в размере 10%.
-        @endif
-      </p>
-      <p>Ждем-с.</p>
-      <p>
-        <a class="btn btn-primary" href="http://tci.ru/rammstein2019/">Купить билет</a>
-        <a class="btn btn-default" href="https://vk.com/rammstein2019">Группа ВК</a>
-      </p>
-    @endru
+    <div class="mb-1">@ru Ожидаемый сетлист: @endru</div>
+    <ol>
+      <li>Was ich liebe</li>
+      <li>Links 2-3-4</li>
+      <li>Sex</li>
+      <li>Tattoo</li>
+      <li>Sehnsucht</li>
+      <li>Zeig dich</li>
+      <li>Mein Herz brennt</li>
+      <li>Puppe</li>
+      <li>Heirate mich</li>
+      <li>Diamant</li>
+      <li>Deutschland (RMX by Richard Z. Kruspe)</li>
+      <li>Deutschland</li>
+      <li>Radio</li>
+      <li>Mein Teil</li>
+      <li>Du hast</li>
+      <li>Sonne</li>
+      <li>Ohne dich</li>
+      <li>Engel</li>
+      <li>Ausländer</li>
+      <li>Du riechst so gut</li>
+      <li>Pussy</li>
+      <li>Rammstein</li>
+      <li>Ich will</li>
+    </ol>
   </div>
   <div class="col-md-5">
     <div class="img-container">
