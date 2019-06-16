a81f0ab41b5c8881ceb9a2767120875cd4fd6833
Sergey Pankov <vacuum@ivacuum.ru>
2019-06-16 19:22:35 +0300

    Калуга в марте 2019

---
+++ resources/views/life/trips/kaluga_2019_03.blade.php
@@ -0,0 +1,14 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Грязные сугробы отжимают парковочные места.</p>
+@en
+  <p>Muddy snowdrifts occupy parking spaces.</p>
+@endru
+
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7385.jpg',
+  'IMG_7386.jpg',
+]])
+@endsection
