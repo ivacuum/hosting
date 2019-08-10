d96bfc3d1cf87f4df11823c13b8afc7f411a3b5f
Sergey Pankov <vacuum@ivacuum.ru>
2019-08-10 13:05:19 +0300

    Цюрих в марте 2019

---
+++ resources/views/life/trips/zurich_2019.blade.php
@@ -0,0 +1,226 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Авиакомпания Swiss заманивает летать по Европе с пересадками в Швейцарии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7417.jpg'])
+
+@ru
+  <p>Аэропорт.</p>
+@en
+  <p>Airport.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7421.jpg'])
+
+@ru
+  <p>Остановка трамвая возле аэропорта. Можно поехать до центра Цюриха быстрым подземным маршрутом, а можно неторопливым и наземным маршрутом номер десять, попутно имея возможность посмотреть улицы и оценить обстановку вокруг.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7418.jpg'])
+
+@ru
+  <p>Экран в трамвае показывает маршрут, остановки, пересадки и время в пути до конечной.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7426.jpg'])
+
+@ru
+  <p>Остановка трамвая.</p>
+@en
+  <p>Tram stop.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7428.jpg'])
+
+@ru
+  <p>Хоккеист.</p>
+@en
+  <p>Hockey player.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7430.jpg'])
+
+@ru
+  <p>Бесплатная швейцарская газета двадцатиминутка. Попадается на остановках поездов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7440.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7441.jpg'])
+
+@ru
+  <p>Спуск к подземным железнодорожным маршрутам.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7444.jpg'])
+
+@ru
+  <p>Полиция может выбрасывать мусор прямо из автомобиля.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7445.jpg'])
+
+@ru
+  <p>Пейзажи города.</p>
+@en
+  <p>Overview of the city.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7447.jpg',
+  'IMG_7452.jpg',
+  'IMG_7455.jpg',
+  'IMG_7458.jpg',
+  'IMG_7464.jpg',
+  'IMG_7466.jpg',
+  'IMG_7470.jpg',
+  'IMG_7475.jpg',
+]])
+
+@ru
+  <p>Мост с замками.</p>
+@en
+  <p>Love padlocks.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7451.jpg'])
+
+@ru
+  <p>Велодорожка посередине проезжей части.</p>
+@en
+  <p>Cycle lane in the middle of the road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7453.jpg'])
+
+@ru
+  <p>Велодорожка на мосту.</p>
+@en
+  <p>Cycle lane on the bridge.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7504.jpg'])
+
+@ru
+  <p>Автомат по продаже билетов на общественный транспорт. Тут же карта тарифных зон.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7457.jpg'])
+
+@ru
+  <p>Трамваи.</p>
+@en
+  <p>Trams.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7461.jpg',
+  'IMG_7480.jpg',
+]])
+
+@ru
+  <p>Велопрокат.</p>
+@en
+  <p>Bicycle rental.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7468.jpg'])
+
+@ru
+  <p>Люди предпочитают есть на улице.</p>
+@en
+  <p>People prefer to eat outside.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7474.jpg'])
+
+@ru
+  <p>Питьевой фонтанчик. Самое время пополнить запасы воды.</p>
+@en
+  <p>Drinking fountain. It's time to refill the water supply.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7479.jpg'])
+
+@ru
+  <p>Телефонная будка. Таксофон принимает франки и евро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7482.jpg'])
+
+@ru
+  <p>Набережные.</p>
+@en
+  <p>Embankments.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7483.jpg',
+  'IMG_7471.jpg',
+]])
+
+@ru
+  <p>Кушают все.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7498.jpg'])
+
+@ru
+  <p>Фотосессия.</p>
+@en
+  <p>Photo session.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7507.jpg'])
+
+@ru
+  <p>Впереди Альпы, которым видно, что досталась солнечная погода.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7512.jpg'])
+
+@ru
+  <p>Лавочка у берега.</p>
+@en
+  <p>Bench by the shore.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7516.jpg'])
+
+@ru
+  <p>Еще фотосессия.</p>
+@en
+  <p>Another photo session.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7674.jpg'])
+
+@ru
+  <p>Здания.</p>
+@en
+  <p>Buildings.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7676.jpg',
+  'IMG_7677.jpg',
+]])
+
+@ru
+  <p>Лавочка без спинки. Для любования пейзажем тоже пойдет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7679.jpg'])
+
+@ru
+  <p>Писсуар.</p>
+@en
+  <p>Urinal.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7680.jpg'])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking lot.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7683.jpg'])
+
+@ru
+  <p>Аккуратность строительных работ. Но бережное отношение к окружению не везде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7684.jpg'])
+
+@ru
+  <p>Платформа центрального вокзала. До аэропорта можно доехать на поезде из центра Цюриха в пределах десяти минут.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7880.jpg'])
+
+@ru
+  <p>Летим дальше.</p>
+@en
+  <p>Let's move on.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7882.jpg'])
+@endsection
