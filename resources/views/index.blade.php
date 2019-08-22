cdb69848ccf06ca8a77aafea901eaee42670177e
Sergey Pankov <vacuum@ivacuum.ru>
2019-08-23 02:02:29 +0300

    Внедрение tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -125,13 +125,13 @@
         <div class="tw-relative tw-w-full tw-pb-3/4">
           <img class="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-object-cover tw-brightness-3/4 group-hover:tw-brightness-full" src="{{ $trip->metaImage(500, 375) }}">
         </div>
-        <div class="trip-cover-info tw-p-4">
+        <div class="tw-absolute tw-bottom-0 tw-left-0 tw-text-white trip-cover-info tw-p-4">
           <div class="tw-text-lg">
             <img class="flag-24 flag-shadow" src="{{ $trip->city->country->flagUrl() }}">
-            <span class="group-hover:tw-text-gray-100">{{ $trip->title }}</span>
-            <span class="trip-cover-date">{{ $trip->timelinePeriod(true) }}</span>
+            {{ $trip->title }}
+            <span class="tw-text-gray-400 tw-text-xs">{{ $trip->timelinePeriod(true) }}</span>
           </div>
-          <div class="trip-cover-description">{{ $trip->metaDescription() }}</div>
+          <div class="tw-text-xs md:tw-text-sm">{{ $trip->metaDescription() }}</div>
         </div>
       </a>
     </div>
