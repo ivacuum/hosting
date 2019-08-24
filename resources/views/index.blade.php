264cfbc74557428f542d8c440db9e9458391f8fe
Sergey Pankov <vacuum@ivacuum.ru>
2019-08-24 12:29:24 +0300

    Внедрение tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -122,10 +122,13 @@
     <?php $trip->loadCityAndCountry(); ?>
     <div class="tw-w-full sm:tw-w-1/2 lg:tw-w-1/3 tw-mx-auto md:tw-mx-0 tw-relative">
       <a class="tw-block group" href="{{ $trip->www() }}">
-        <div class="tw-relative tw-w-full tw-pb-3/4">
-          <img class="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-object-cover tw-brightness-3/4 group-hover:tw-brightness-full" src="{{ $trip->metaImage(500, 375) }}">
+        <div class="tw-relative tw-pb-3/4">
+          <img
+            class="tw-absolute tw-w-full tw-h-full tw-object-cover brightness-3/4 group-hover:brightness-full"
+            src="{{ $trip->metaImage(500, 375) }}"
+          >
         </div>
-        <div class="tw-absolute tw-bottom-0 tw-left-0 tw-text-white trip-cover-info tw-p-4">
+        <div class="tw-absolute tw-bottom-0 tw-text-white trip-cover-info tw-p-4">
           <div class="tw-text-lg">
             <img class="flag-24 flag-shadow" src="{{ $trip->city->country->flagUrl() }}">
             {{ $trip->title }}
