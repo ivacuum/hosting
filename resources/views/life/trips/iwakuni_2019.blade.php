704bb0f29bdc586626a99a3854ccdfe1ccb1c1cc
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-21 05:28:17 +0300

    Ивакуни в апреле 2019

---
+++ resources/views/life/trips/iwakuni_2019.blade.php
@@ -0,0 +1,160 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Автобус подвез до конечной остановки у железнодорожной станции на западе Хиросимы. Люди расходятся кто на работу, кто на учебу. Отсюда с пересадками можно добраться до Ивакуни на поездах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9397.jpg'])
+
+@ru
+  <p>Бейсбольное оформление поезда.</p>
+@en
+  <p>Baseball themed train.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9398.jpg'])
+
+@ru
+  <p>Платформа.</p>
+@en
+  <p>Platform.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9400.jpg'])
+
+@ru
+  <p>Машинист по имени Мишима. При пересменке он ставит свою табличку в левый нижний угол окна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9402.jpg'])
+
+@ru
+  <p>В каждом уголке страны свое оформление поездов. Любоваться можно бесконечно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9405.jpg'])
+
+@ru
+  <p>Между поездами можно затариться напитками в автомате.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9407.jpg'])
+
+@ru
+  <p>Железная дорога.</p>
+@en
+  <p>Railroad.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9410.jpg'])
+
+@ru
+  <p>Кроха автобус.</p>
+@en
+  <p>Tiny bus.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9413.jpg'])
+
+@ru
+  <p>В автобусе. Прямо перед носом одна из многочисленных кнопок с просьбой сделать остановку.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9412.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@en
+  <p>Pedestrian cross.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9415.jpg'])
+
+@ru
+  <p>Дома. Сотрудники магазинов на первых этажах даже пылесосят тротуар перед входом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9416.jpg'])
+
+@ru
+  <p>Парковка на щебенке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9417.jpg'])
+
+@ru
+  <p>Мост.</p>
+@en
+  <p>Bridge.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9418.jpg'])
+
+@ru
+  <p>Река.</p>
+@en
+  <p>River.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9421.jpg'])
+
+@ru
+  <p>Скамейка.</p>
+@en
+  <p>Bench.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9422.jpg'])
+
+@ru
+  <p>Канатная дорога.</p>
+@en
+  <p>Cable car.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9427.jpg'])
+
+@ru
+  <p>Улица вдоль парка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9429.jpg'])
+
+@ru
+  <p>Рельеф дороги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9430.jpg'])
+
+@ru
+  <p>Мост вдалеке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9433.jpg'])
+
+@ru
+  <p>Парковка на щебенке размечена.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9437.jpg'])
+
+@ru
+  <p>Надземные переходы дают обзор.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9438.jpg',
+  'IMG_9439.jpg',
+]])
+
+@ru
+  <p>Парковка строительной техники.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9440.jpg'])
+
+@ru
+  <p>Туалет и велопарковка за железнодорожной станцией.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9441.jpg'])
+
+@ru
+  <p>Состав из одного вагона. Ходит крайне редко — раз в час или еще реже.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9443.jpg',
+  'IMG_9444.jpg',
+]])
+
+@ru
+  <p>Автомат по продаже билетов на вышеупомянутый поезд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9448.jpg'])
+
+@ru
+  <p>Снаружи станции Шин-Ивакуни. Приставка шин в названии станции, как правило, означает, что через нее ходят шинкансены.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9459.jpg',
+  'IMG_9460.jpg',
+]])
+@endsection
