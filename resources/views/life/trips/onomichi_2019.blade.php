a1ec87d3f23d7be0c4545a06c8fd62dff36dedc6
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-21 17:04:44 +0300

    Ономити в апреле 2019

---
+++ resources/views/life/trips/onomichi_2019.blade.php
@@ -0,0 +1,198 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Пейзаж за окном поезда.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9621.jpg'])
+
+@ru
+  <p>Сиденья на платформе. Самое то с напитком подождать свой поезд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9625.jpg'])
+
+@ru
+  <p>Вот и поезд.</p>
+@en
+  <p>Here is the train.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9628.jpg'])
+
+@ru
+  <p>Сиденья в электричке и пейзаж за окном.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9630.jpg'])
+
+@ru
+  <p><s>Зал</s> Платформа ожидания.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9633.jpg'])
+
+@ru
+  <p>Карта велодорожки по островам на <span class="whitespace-no-wrap">70 километров</span>. В последний момент передумал по ней прокатиться. Идеальным планом виделось один день рулить до города Имабари в левом нижнем углу, там ночевать и ехать обратно на поезде или автобусе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9634.jpg'])
+
+@ru
+  <p>Часы и скульптура. На холме замок. Вывеска заведения с раменом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9636.jpg'])
+
+@ru
+  <p>Скамейка с видом на причал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9638.jpg'])
+
+@ru
+  <p>Перед скамейкой в месте для ног незаметное резиновое полотно с порами, чтобы земля не растопталась. Рядом раковины с кранами.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9639.jpg'])
+
+@ru
+  <p>Машины желают переправиться на другой берег.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9641.jpg'])
+
+@ru
+  <p>Всех желающих приняли на борт.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9643.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9644.jpg'])
+
+@ru
+  <p>Переулок.</p>
+@en
+  <p>Side street.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9645.jpg'])
+
+@ru
+  <p>Барьерами оградились от автомобилей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9646.jpg'])
+
+@ru
+  <p>На холме жилые дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9647.jpg'])
+
+@ru
+  <p>Проход к домам через железную дорогу. Сакура.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9650.jpg'])
+
+@ru
+  <p>Указатель канатной дороги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9684.jpg'])
+
+@ru
+  <p>Станция канатной дороги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9651.jpg'])
+
+@ru
+  <p>Кладбища.</p>
+@en
+  <p>Cemeteries.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9653.jpg'])
+
+@ru
+  <p>Высотные виды.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9657.jpg',
+  'IMG_9658.jpg',
+]])
+
+@ru
+  <p>Котики.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9659.jpg'])
+
+@ru
+  <p>Скамейка на горе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9660.jpg'])
+
+@ru
+  <p>Еще одна скамейка. Не самая приятная поверхность для сидения.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9665.jpg'])
+
+@ru
+  <p>Парк на горе.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9662.jpg',
+  'IMG_9663.jpg',
+  'IMG_9664.jpg',
+  'IMG_9667.jpg',
+  'IMG_9669.jpg',
+  'IMG_9677.jpg',
+  'IMG_9681.jpg',
+]])
+
+@ru
+  <p>Надписи вырезаны на камне.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9680.jpg'])
+
+@ru
+  <p>Письмо справа налево и сверху вниз.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9683.jpg'])
+
+@ru
+  <p>Железная дорога.</p>
+@en
+  <p>Railroad.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9685.jpg'])
+
+@ru
+  <p>И как только люди живут возле нее с постоянным шумом поездов?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9687.jpg'])
+
+@ru
+  <p>Узкий тротуар и товарный поезд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9686.jpg'])
+
+@ru
+  <p>Переезд закрыт. Надпись на шлагбауме: «Пожалуйста, подождите».</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9688.jpg'])
+
+@ru
+  <p>Карта организаций района. Печатный 2ГИС.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9691.jpg'])
+
+@ru
+  <p>Главный вокзал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9694.jpg'])
+
+@ru
+  <p>Пейзажи за окном поезда.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9699.jpg',
+  'IMG_9701.jpg',
+]])
+
+@ru
+  <p>Платформа. На этой станции останавливаются не все шинкансены, поэтому впервые с 2017 года удалось заснять один из экспрессов, проносящихся мимо за считанные секунды.</p>
+@endru
+<youtube title="Shinkansen Passing Mihara Station, Japan, April 2019" v="gtTEneP041c"></youtube>
+@include('tpl.pic-2x', ['pic' => 'IMG_9703.jpg'])
+@endsection
