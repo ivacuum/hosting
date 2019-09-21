89770261c37471434cfe847ab32fb1df05127c16
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-21 05:46:27 +0300

    Окаяма в апреле 2019

---
+++ resources/views/life/trips/okayama_2019.blade.php
@@ -0,0 +1,120 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Снаружи центрального вокзала.</p>
+@en
+  <p>Outside the central station.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9462.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9463.jpg'])
+
+@ru
+  <p>Трамвай.</p>
+@en
+  <p>Tram.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9466.jpg'])
+
+@ru
+  <p>Инструкция к автобусной остановке. Каждому маршруту соответствует своя платформа. Так водитель будет видеть надо ли останавливаться и кого подбирать или можно проехать мимо, если никто из пассажиров не нажал кнопку с просьбой остановки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9467.jpg'])
+
+@ru
+  <p><span class="whitespace-no-wrap">400 ¥</span> за разовый вход в сад и <span class="whitespace-no-wrap">2050 ¥</span> безлимит на год.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9470.jpg'])
+
+@ru
+  <p>Сад. Весьма пустынный по сравнению с увиденными в других уголках страны, например, в <a class="link" href="kanazawa.2017#garden">Канадзаве</a>.</p>
+@en
+  <p>Garden.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9471.jpg',
+  'IMG_9473.jpg',
+  'IMG_9474.jpg',
+  'IMG_9475.jpg',
+  'IMG_9476.jpg',
+  'IMG_9478.jpg',
+  'IMG_9479.jpg',
+  'IMG_9480.jpg',
+  'IMG_9482.jpg',
+  'IMG_9485.jpg',
+  'IMG_9486.jpg',
+]])
+
+@ru
+  <p>Свежее ограждение на мосту.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9487.jpg'])
+
+@ru
+  <p>Велодорожка.</p>
+@en
+  <p>Cycle lane.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9488.jpg'])
+
+@ru
+  <p>Люки. После цветных невозможно хотеть обычные.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9490.jpg',
+  'IMG_9494.jpg',
+]])
+
+@ru
+  <p>Место ожидания для пешеходов на перекрестке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9491.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9492.jpg',
+  'IMG_9496.jpg',
+]])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9498.jpg'])
+
+@ru
+  <p>Галерея.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9500.jpg',
+  'IMG_9502.jpg',
+]])
+
+@ru
+  <p>Трамвайная остановка. Левый трамвай идет до остановки «Восточная гора», правый — до остановки «перед вокзалом Окаямы». Здорово, когда попадаются все знакомые иероглифы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9507.jpg'])
+
+@ru
+  <p>В трамвае.</p>
+@en
+  <p>Inside the tram.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9509.jpg'])
+
+@ru
+  <p>Хм. Почему удон и буккаке на одной вывеске?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9461.jpg'])
+@endsection
