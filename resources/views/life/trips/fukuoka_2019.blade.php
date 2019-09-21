f1cd09520f438e77e328d288659d8aa4a61361a4
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-21 04:43:21 +0300

    Фукуока в апреле 2019

---
+++ resources/views/life/trips/fukuoka_2019.blade.php
@@ -0,0 +1,187 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Фукуока (станция Хаката) — самая дальняя точка, до которой можно добраться по проездному сан-ё.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9195.jpg'])
+
+@ru
+  <p>Табло на вокзале. Поезда дальше на юг острова.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9197.jpg'])
+
+@ru
+  <p>Снаружи вокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9199.jpg'])
+
+@ru
+  <p>Камеры хранения.</p>
+@en
+  <p>Lockers.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9200.jpg'])
+
+@ru
+  <p>Метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9201.jpg'])
+
+@ru
+  <p>Пешеходный переход.</p>
+@en
+  <p>Pedestrian cross.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9202.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9203.jpg',
+  'IMG_9210.jpg',
+  'IMG_9211.jpg',
+  'IMG_9212.jpg',
+]])
+
+@ru
+  <p>Бухта.</p>
+@en
+  <p>Bay.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9205.jpg',
+  'IMG_9207.jpg',
+  'IMG_9208.jpg',
+  'IMG_9209.jpg',
+]])
+
+@ru
+  <p>Контейнеры для раздельного сбора мусора возле жилого дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9213.jpg'])
+
+@ru
+  <p>Подъем в парк. Дорожные знаки ловко прикреплены к столбу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9260.jpg'])
+
+@ru
+  <p>Осторожно, котики.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9215.jpg'])
+
+@ru
+  <p>Вот и котик.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9218.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9217.jpg',
+  'IMG_9220.jpg',
+  'IMG_9221.jpg',
+  'IMG_9222.jpg',
+  'IMG_9223.jpg',
+  'IMG_9224.jpg',
+]])
+
+@ru
+  <p>Автомат с прохладительными напитками.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9227.jpg'])
+
+@ru
+  <p>Такси в арке под мостом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9233.jpg'])
+
+@ru
+  <p>Деревья подпилили.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9234.jpg'])
+
+@ru
+  <p>Высотный вид на шоссе и промышленную зону.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9237.jpg'])
+
+@ru
+  <p>На высоте еще один парк.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9241.jpg',
+  'IMG_9243.jpg',
+  'IMG_9255.jpg',
+]])
+
+@ru
+  <p>Обзорная площадка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9248.jpg'])
+
+@ru
+  <p>Самолеты летают.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9254.jpg'])
+
+@ru
+  <p>Велопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9261.jpg'])
+
+@ru
+  <p>Парк у озера.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9263.jpg',
+  'IMG_9265.jpg',
+  'IMG_9266.jpg',
+  'IMG_9268.jpg',
+  'IMG_9271.jpg',
+  'IMG_9274.jpg',
+  'IMG_9280.jpg',
+]])
+
+@ru
+  <p>Увлечение взрослого поколения — радиоуправляемые лодки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9279.jpg'])
+
+@ru
+  <p>Пик сакуры уже прошел.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9282.jpg'])
+
+@ru
+  <p>На картах Гугла неожиданно попалось место возле аэропорта с высоким рейтингом. Хвалили за хороший обзор аэродрома и заходящих на посадку самолетов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9284.jpg'])
+
+@ru
+  <p>Самолеты действительно хорошо видно даже без приближения. Как маленькие.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9317.jpg'])
+
+@ru
+  <p>Так и большие.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9329.jpg'])
+
+@ru
+  <p>Желающих пофотографировать весьма достаточно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9375.jpg'])
+
+@ru
+  <p>Самолеты заходят на посадку примерно каждые пять минут.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_9385.jpg'])
+@endsection
