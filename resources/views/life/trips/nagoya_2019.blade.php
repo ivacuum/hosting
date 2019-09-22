7811443610863f16f69292620ba69021ec8b3d3a
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-23 00:14:58 +0300

    Нагоя в мае 2019

---
+++ resources/views/life/trips/nagoya_2019.blade.php
@@ -0,0 +1,411 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Из Осаки в Нагою был выбран путь на электричках с несколькими пересадками. Стоимость вышла около <span class="whitespace-no-wrap">3350 ¥</span>. То ли повлияла разница регионов, то ли относительно высокая стоимость, но турникет не принял оплату транспортной картой, а отправил к сотруднику станции в окно по соседству. Тот переспросил действительно ли я из Осаки приехал, посмотрел тариф в книге, спросил списывать ли деньги за проезд и вручную провел операцию.</p>
+@endru
+
+@ru
+  <p>Все майские праздники транспорт ходит по расписанию выходного дня.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0003.jpg'])
+
+@ru
+  <p>Прачечная тоже недоступна, но хоть не всю неделю.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0006.jpg'])
+
+@ru
+  <p>Перекресток с возможностью перехода в любую сторону.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0007.jpg'])
+
+@ru
+  <p>Уличная табличка с небольшой картой домов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0012.jpg'])
+
+@ru
+  <p>Вот где мусор запрятался.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0015.jpg'])
+
+@ru
+  <p>Зеленый уголок.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0016.jpg'])
+
+@ru
+  <p>Места для такси.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0017.jpg'])
+
+@ru
+  <p>Люки.</p>
+@en
+  <p>Manholes.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0018.jpg',
+  'IMG_0077.jpg',
+]])
+
+@ru
+  <p>Гора в парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0020.jpg'])
+
+@ru
+  <p>Миниатюрные солнечные батареи для подсветки информации о дорожных работах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0023.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0024.jpg',
+  'IMG_0025.jpg',
+  'IMG_0026.jpg',
+  'IMG_0027.jpg',
+  'IMG_0028.jpg',
+]])
+
+@ru
+  <p>Пожарные лестницы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0029.jpg'])
+
+@ru
+  <p>Велопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0031.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0034.jpg'])
+
+@ru
+  <p>Еще велопарковка. Железнодорожная линия JR снова над землей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0038.jpg'])
+
+@ru
+  <p>Скамейки перед сценой в парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0039.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0040.jpg',
+  'IMG_0044.jpg',
+  'IMG_0045.jpg',
+  'IMG_0048.jpg',
+  'IMG_0049.jpg',
+  'IMG_0054.jpg',
+]])
+
+@ru
+  <p>Два напитка образовали затор во второй урне.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0053.jpg'])
+
+@ru
+  <p>Цирк.</p>
+@en
+  <p>Circus.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0055.jpg'])
+
+@ru
+  <p>Музей науки снаружи.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0059.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0061.jpg'])
+
+@ru
+  <p>Классная вывеска кассы из элементов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0063.jpg'])
+
+@ru
+  <p>Фестиваль бельгийского пива. Мальчик в середине баннера иллюстрирует, что пиво — ссанина?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0065.jpg'])
+
+@ru
+  <p>Обзор с моста.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0066.jpg',
+  'IMG_0068.jpg',
+]])
+
+@ru
+  <p>Складное ограждение.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0070.jpg'])
+
+@ru
+  <p>Курить здесь нельзя — штраф <span class="whitespace-no-wrap">2000 ¥</span>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0071.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0073.jpg'])
+
+@ru
+  <p>Такой заросший пейзаж можно найти в центре города.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0075.jpg'])
+
+@ru
+  <p>Здание.</p>
+@en
+  <p>Building.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0079.jpg'])
+
+@ru
+  <p>Места для прогулок.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0083.jpg',
+  'IMG_0086.jpg',
+  'IMG_0089.jpg',
+  'IMG_0090.jpg',
+  'IMG_0095.jpg',
+]])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0093.jpg'])
+
+@ru
+  <p>Мельница на голландский манер.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0094.jpg'])
+
+@ru
+  <p>Больница.</p>
+@en
+  <p>Hospital.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0097.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0098.jpg',
+  'IMG_0100.jpg',
+]])
+
+@ru
+  <p>Черепаха в парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0110.jpg'])
+
+@ru
+  <p>Отдельные очереди на каждый маршрут автобуса. Единый тариф на автобус в Нагое приводит к входу и чек-ину через водителя, а затем свободному выходу без чек-аута. Не только двигатель гибридных автомобилей выключается на красный свет светофора, но и водители некоторых автобусов тоже глушат свой транспорт. И наступает тишина.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0119.jpg'])
+
+@ru
+  <p>Оазис 21.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0117.jpg',
+  'IMG_0120.jpg',
+  'IMG_0122.jpg',
+  'IMG_0127.jpg',
+  'IMG_0130.jpg',
+  'IMG_0131.jpg',
+]])
+
+@ru
+  <p>И обзор с него.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0128.jpg',
+  'IMG_0134.jpg',
+]])
+
+@ru
+  <p>Перехватывающая велопарковка перед метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0137.jpg'])
+
+@ru
+  <p>Человек только ступает на пешеходный переход, а автомобили уже останавливаются.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0139.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0140.jpg',
+  'IMG_0142.jpg',
+  'IMG_0143.jpg',
+  'IMG_0145.jpg',
+  'IMG_0150.jpg',
+  'IMG_0151.jpg',
+  'IMG_0153.jpg',
+]])
+
+@ru
+  <p>На руле велосипеда теплые перчатки для рук и держатель для зонта.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0148.jpg'])
+
+@ru
+  <p>Неместный дизайн дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0154.jpg'])
+
+@ru
+  <p>Цветы на мосту.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0159.jpg',
+  'IMG_0158.jpg',
+  'IMG_0163.jpg',
+]])
+
+@ru
+  <p>Канал.</p>
+@en
+  <p>Canal.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0160.jpg'])
+
+@ru
+  <p>Цветы на перекрестках.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0165.jpg',
+  'IMG_0167.jpg',
+  'IMG_0030.jpg',
+]])
+
+@ru
+  <p>Высотное здание.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0172.jpg'])
+
+@ru
+  <p>Пожарные лестницы на каждом этаже.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0173.jpg'])
+
+@ru
+  <p>Улица. Не перестает радовать обилие стеклянных зданий.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0178.jpg'])
+
+@ru
+  <p>Если есть возможность, то на перекрестках японцы встают в тень.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0181.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0183.jpg'])
+
+@ru
+  <p>Перекресток у центральной станции.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0184.jpg'])
+
+@ru
+  <p>Оживленное движение.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0188.jpg'])
+
+@ru
+  <p>Пассажиропоток на центральной станции Нагои.</p>
+@endru
+<youtube title="Nagoya Station, Japan, May 2019" v="AOq7cGttWDU"></youtube>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0191.jpg',
+  'IMG_0192.jpg',
+]])
+
+@ru
+  <p>Хороший транспортный узел — станция Канаяма.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0199.jpg'])
+
+@ru
+  <p>Витая пара в ящичке номера в отеле.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0203.jpg'])
+
+@ru
+  <p>Разглаживатель брюк. Вешается одежда и закрывается отсек, чтобы прижать помятое к обогревателю. После прогрева складки и помятости должны разгладиться, но моим брюкам это не помогло.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0206.jpg'])
+
+@ru
+  <p>Предлагается отрывать наклейки, чтобы переместить их наружу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0209.jpg'])
+
+@ru
+  <p>До аэропорта меньше часа езды на не самом скором поезде из центра.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0210.jpg'])
+
+@ru
+  <p>Над стойками регистрации знакомый замок <a class="link" href="matsumoto.2017">Мацумото</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0212.jpg'])
+
+@ru
+  <p>Места для ожидания.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0215.jpg'])
+
+@ru
+  <p>Аэропорт весьма небольшой. Магазинов минимальное количество.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0216.jpg'])
+
+@ru
+  <p>Самолет корейских авиалиний.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0213.jpg'])
+
+@ru
+  <p>Но сегодня нужны финские, потому что лететь предстоит 10 + 5 часов в Португалию через Хельсинки. Да, так было дешевле, чем туда-обратно в Москву. Всего около <span class="whitespace-no-wrap">31 000 ₽</span> за сегменты Москва—Токио—Осака и Нагоя—Хельсинки—Лиссабон.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0218.jpg'])
+@endsection
