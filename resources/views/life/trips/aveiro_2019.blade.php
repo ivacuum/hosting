ad6bbc18b2a557f6afb6bd508dfb583056ea44a9
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 20:01:04 +0300

    Авейру в мае 2019

---
+++ resources/views/life/trips/aveiro_2019.blade.php
@@ -0,0 +1,268 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Сегодня поездка по желтому маршруту. Удобно, что все время достаточно стартовать с вокзала Кампанья в центре Порту.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0703.jpg'])
+
+@ru
+  <p>Зеленый и красный маршруты припасены на будущие дни.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0701.jpg'])
+
+@ru
+  <p>Место назначения — Авейру. Забыл сделать чек-ин до посадки в электричку. Вышел на одной из станций во время остановки и сходил к валидатору, успел вернуться до отправления поезда. Один из пассажиров чек-ин не делал, контроллер на него ругался, на общем языке они не говорили, хитрый пассажир включил дурачка, контроллер в итоге его пожалел.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0705.jpg'])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking lot.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0709.jpg'])
+
+@ru
+  <p>Канал.</p>
+@en
+  <p>Canal.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0711.jpg'])
+
+@ru
+  <p>Автомобилям путь на подземную парковку, пешеходам по набережной, лодкам по каналу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0712.jpg'])
+
+@ru
+  <p>Скамейки в тени деревьев.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0714.jpg'])
+
+@ru
+  <p>Расположение скамеек даже позволяет вытянуть ноги. И не надо беспокоиться, что кто-то перед тобой может пройти.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0718.jpg'])
+
+@ru
+  <p>Мастера оформления плитки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0720.jpg'])
+
+@ru
+  <p>Плиточные фасады.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0721.jpg',
+  'IMG_0722.jpg',
+  'IMG_0727.jpg',
+  'IMG_0804.jpg',
+  'IMG_0809.jpg',
+  'IMG_0810.jpg',
+]])
+
+@ru
+  <p>Плиточные уличные таблички.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0724.jpg',
+  'IMG_0771.jpg',
+]])
+
+@ru
+  <p>Оформление тротуарной плиткой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0731.jpg'])
+
+@ru
+  <p>Здание почты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0728.jpg'])
+
+@ru
+  <p>Отверстие для почты на входной двери жилого дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0732.jpg'])
+
+@ru
+  <p>Скамейка почтовой тематики.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0725.jpg'])
+
+@ru
+  <p>Скамейка морской тематики.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0730.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0734.jpg',
+  'IMG_0797.jpg',
+  'IMG_0800.jpg',
+  'IMG_0812.jpg',
+]])
+
+@ru
+  <p>Городская навигация.</p>
+@en
+  <p>City navigation.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0735.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0736.jpg',
+  'IMG_0737.jpg',
+  'IMG_0738.jpg',
+  'IMG_0740.jpg',
+]])
+
+@ru
+  <p>Лимоны растут у кого-то во дворе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0739.jpg'])
+
+@ru
+  <p>Лестница надземного перехода.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0741.jpg'])
+
+@ru
+  <p>Сам надземный переход.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0743.jpg',
+  'IMG_0745.jpg',
+]])
+
+@ru
+  <p>Парк через дорогу.</p>
+@en
+  <p>Park across the road.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0749.jpg',
+  'IMG_0750.jpg',
+  'IMG_0753.jpg',
+  'IMG_0754.jpg',
+  'IMG_0756.jpg',
+  'IMG_0760.jpg',
+  'IMG_0762.jpg',
+  'IMG_0763.jpg',
+]])
+
+@ru
+  <p>Надписи на бамбуке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0752.jpg'])
+
+@ru
+  <p>Вход на территорию стадиона.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0765.jpg'])
+
+@ru
+  <p>Таксофон.</p>
+@en
+  <p>Payphone.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0766.jpg'])
+
+@ru
+  <p>Территория учебного кампуса университета Авейру. Целый город в городе.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0772.jpg',
+  'IMG_0774.jpg',
+  'IMG_0775.jpg',
+  'IMG_0776.jpg',
+  'IMG_0777.jpg',
+  'IMG_0778.jpg',
+  'IMG_0780.jpg',
+  'IMG_0781.jpg',
+  'IMG_0783.jpg',
+  'IMG_0784.jpg',
+]])
+
+@ru
+  <p>Жилой район.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0787.jpg',
+  'IMG_0788.jpg',
+  'IMG_0789.jpg',
+  'IMG_0790.jpg',
+  'IMG_0793.jpg',
+  'IMG_0794.jpg',
+  'IMG_0795.jpg',
+  'IMG_0796.jpg',
+]])
+
+@ru
+  <p>Платная парковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0792.jpg'])
+
+@ru
+  <p>Почтовые ящики.</p>
+@en
+  <p>Postboxes.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0798.jpg'])
+
+@ru
+  <p>Поля.</p>
+@en
+  <p>Fields.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0803.jpg'])
+
+@ru
+  <p>Скамейка во весь берег.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0806.jpg'])
+
+@ru
+  <p>Мост через канал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0807.jpg'])
+
+@ru
+  <p>Цветы и скамейка около дома.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0808.jpg'])
+
+@ru
+  <p>Дверки для доступа к коммуникациям. Счетчик видно с улицы.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0811.jpg'])
+
+@ru
+  <p>Фасад.</p>
+@en
+  <p>Facade.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0813.jpg',
+  'IMG_0814.jpg',
+]])
+
+@ru
+  <p>Сига — карта для поездок на электричках. Еще бывают городские карты, на которые либо записываются поездки, либо дневной проездной. Причем сначала нужно израсходовать записанное, чтобы записать что-то новое другого типа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0817.jpg'])
+
+@ru
+  <p>Вот и валидатор, к которому надо прикладывать сигу перед поездкой. Контролеры потом проверяют факт чек-ина.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0815.jpg'])
+@endsection
