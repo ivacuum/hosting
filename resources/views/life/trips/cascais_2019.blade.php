1fe7fea5fa40dde55e5acaa3ba150edcffb41ec2
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 19:44:10 +0300

    Кашкайш в мае 2019

---
+++ resources/views/life/trips/cascais_2019.blade.php
@@ -0,0 +1,187 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Расписание движения поездов до Кашкайша.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0342.jpg'])
+
+@ru
+  <p>В салоне электрички.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0344.jpg'])
+
+@ru
+  <p>Для каждого маршрута в Лиссабоне свой вокзал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0345.jpg'])
+
+@ru
+  <p>Сегодня нужен левый нижний маршрут. Непонятно почему все зеленые.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0347.jpg'])
+
+@ru
+  <p>Конечная.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0427.jpg'])
+
+@ru
+  <p>Круговое движение.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0352.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0354.jpg',
+  'IMG_0362.jpg',
+  'IMG_0364.jpg',
+  'IMG_0365.jpg',
+  'IMG_0366.jpg',
+  'IMG_0368.jpg',
+  'IMG_0390.jpg',
+]])
+
+@ru
+  <p>Почтовые ящики и входные двери.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0356.jpg'])
+
+@ru
+  <p>Лампа над входной дверью.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0357.jpg'])
+
+@ru
+  <p>Парковка.</p>
+@en
+  <p>Parking lot.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0358.jpg'])
+
+@ru
+  <p>Окрестности пляжа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0361.jpg'])
+
+@ru
+  <p>Уличная табличка.</p>
+@en
+  <p>Street sign.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0367.jpg'])
+
+@ru
+  <p>Парк с петухами. Стоило залипнуть в телефоне, как один из них чуть не попал под ноги.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0369.jpg',
+  'IMG_0370.jpg',
+  'IMG_0371.jpg',
+  'IMG_0373.jpg',
+  'IMG_0374.jpg',
+  'IMG_0376.jpg',
+  'IMG_0377.jpg',
+  'IMG_0378.jpg',
+  'IMG_0379.jpg',
+  'IMG_0380.jpg',
+  'IMG_0381.jpg',
+  'IMG_0382.jpg',
+  'IMG_0383.jpg',
+  'IMG_0385.jpg',
+  'IMG_0386.jpg',
+]])
+
+@ru
+  <p>К выходу из парка провожают утки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0387.jpg'])
+
+@ru
+  <p>Флаг Португалии.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0389.jpg'])
+
+@ru
+  <p>Залив.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0393.jpg'])
+
+@ru
+  <p>Променад вдоль океана. Велодорожка размером с полосу для автомобилей.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0394.jpg',
+  'IMG_0397.jpg',
+  'IMG_0399.jpg',
+  'IMG_0401.jpg',
+  'IMG_0404.jpg',
+]])
+
+@ru
+  <p>Проход поближе к воде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0398.jpg'])
+
+@ru
+  <p>Скамейка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0400.jpg'])
+
+@ru
+  <p>Спуск.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0403.jpg'])
+
+@ru
+  <p>Подъем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0405.jpg'])
+
+@ru
+  <p>Внизу рыбачат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0408.jpg'])
+
+@ru
+  <p>Сбоку любуются.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0409.jpg'])
+
+@ru
+  <p>Шрамы камней.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0412.jpg'])
+
+@ru
+  <p>Люк.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0415.jpg'])
+
+@ru
+  <p>Запасы тротуарной плитки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0417.jpg'])
+
+@ru
+  <p>Городская навигация.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0418.jpg'])
+
+@ru
+  <p>Умная урна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0421.jpg'])
+
+@ru
+  <p>Крохотный дорожный знак автомобиля.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0422.jpg'])
+
+@ru
+  <p>Заинтересованная чайка. Нужно лишь просто ее найти.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0424.jpg'])
+@endsection
