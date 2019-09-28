86ce12a5fea33323cab39a80c69e569ed38f7bba
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 19:49:06 +0300

    Синтра в мае 2019

---
+++ resources/views/life/trips/sintra_2019.blade.php
@@ -0,0 +1,257 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Отправление с очередного вокзала в Лиссабоне.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0451.jpg'])
+
+@ru
+  <p>Интерьер электрички.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0565.jpg'])
+
+@ru
+  <p>Часы на вокзале в Синтре.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0453.jpg'])
+
+@ru
+  <p>Весьма узкий тротуар.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0456.jpg'])
+
+@ru
+  <p>Первые подсказки, что надо будет много идти в гору.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0459.jpg',
+  'IMG_0465.jpg',
+]])
+
+@ru
+  <p>Припаркованная карета с лошадьми.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0463.jpg'])
+
+@ru
+  <p>Вход в парк.</p>
+@en
+  <p>Park entrance.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0467.jpg'])
+
+@ru
+  <p>Подъем по дороге с огороженным участком для пешеходов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0468.jpg'])
+
+@ru
+  <p>Жилые дома.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0471.jpg',
+  'IMG_0475.jpg',
+  'IMG_0479.jpg',
+]])
+
+@ru
+  <p>На каждом втором доме информация о включенной сигнализации.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0477.jpg'])
+
+@ru
+  <p>Улица.</p>
+@en
+  <p>Street.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0480.jpg'])
+
+@ru
+  <p>Карта местности вокруг замка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0482.jpg'])
+
+@ru
+  <p>Скамейка на высоте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0484.jpg'])
+
+@ru
+  <p>Панорамы.</p>
+@en
+  <p>Panoramas.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0470.jpg',
+  'IMG_0485.jpg',
+  'IMG_0486.jpg',
+  'IMG_0487.jpg',
+]])
+
+@ru
+  <p>Подъем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0490.jpg'])
+
+@ru
+  <p>Оформление автобуса, следующего по маршруту 434.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0492.jpg',
+  'IMG_0495.jpg',
+]])
+
+@ru
+  <p>Лес.</p>
+@en
+  <p>Forest.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0497.jpg'])
+
+@ru
+  <p>Замок на вершине холма.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0500.jpg'])
+
+@ru
+  <p>Башня, окруженная водой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0501.jpg'])
+
+@ru
+  <p>Подсказка пешеходам держаться справа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0504.jpg'])
+
+@ru
+  <p>Серпантин.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0506.jpg'])
+
+@ru
+  <p>Односторонний подъем.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0509.jpg'])
+
+@ru
+  <p>Пешеходное ущелье.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0511.jpg'])
+
+@ru
+  <p>Частная территория.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0514.jpg'])
+
+@ru
+  <p>Улица перед ней.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0515.jpg'])
+
+@ru
+  <p>Замок по соседству.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0516.jpg',
+  'IMG_0518.jpg',
+  'IMG_0521.jpg',
+]])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0525.jpg',
+  'IMG_0528.jpg',
+  'IMG_0536.jpg',
+  'IMG_0537.jpg',
+  'IMG_0539.jpg',
+  'IMG_0547.jpg',
+  'IMG_0550.jpg',
+]])
+
+@ru
+  <p>Чей-то дворик.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0527.jpg'])
+
+@ru
+  <p>Переулок.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0530.jpg'])
+
+@ru
+  <p>Насколько комфортно на автомобиле по таким узким улицам ездить?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0533.jpg'])
+
+@ru
+  <p>Площадь.</p>
+@en
+  <p>Square.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0534.jpg'])
+
+@ru
+  <p>Панорамы.</p>
+@en
+  <p>Panoramas.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0541.jpg',
+  'IMG_0542.jpg',
+]])
+
+@ru
+  <p>Снова карета с лошадьми припаркована.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0544.jpg'])
+
+@ru
+  <p>А теперь она на ходу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0552.jpg'])
+
+@ru
+  <p>Вход есть, а дом не видно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0545.jpg'])
+
+@ru
+  <p>Дом просто ниже входа.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0543.jpg'])
+
+@ru
+  <p>Крыше кранты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0546.jpg'])
+
+@ru
+  <p>Движение транспорта прямо четырехкратно запрещено.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0554.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0557.jpg'])
+
+@ru
+  <p>Платформа центральной железнодорожной станции.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0561.jpg',
+  'IMG_0562.jpg',
+]])
+
+@ru
+  <p>С платформы виден мини-замок.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0559.jpg'])
+@endsection
