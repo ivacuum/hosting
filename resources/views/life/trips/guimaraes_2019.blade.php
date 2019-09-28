b1e396ca92937d75b33151bba3e36bbb886817dc
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 20:07:56 +0300

    Гимарайнш в мае 2019

---
+++ resources/views/life/trips/guimaraes_2019.blade.php
@@ -0,0 +1,130 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>На некоторых станциях проводник (он же контролер) выглядывает из состава, убеждается в завершении посадки, вставляет ключ в отверстие над синей кнопкой и сам закрывает двери всех вагонов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0884.jpg'])
+
+@ru
+  <p>Внезапно — пешеходам дальше нельзя. Только переходить дорогу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0888.jpg'])
+
+@ru
+  <p>Пейзажи города.</p>
+@en
+  <p>City landscapes.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0890.jpg',
+  'IMG_0894.jpg',
+  'IMG_0904.jpg',
+  'IMG_0910.jpg',
+  'IMG_0912.jpg',
+  'IMG_0913.jpg',
+  'IMG_0918.jpg',
+  'IMG_0933.jpg',
+]])
+
+@ru
+  <p>Городской автобус.</p>
+@en
+  <p>City bus.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0895.jpg'])
+
+@ru
+  <p>Фонтаны.</p>
+@en
+  <p>Fountains.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0899.jpg',
+  'IMG_0907.jpg',
+  'IMG_0923.jpg',
+]])
+
+@ru
+  <p>Плита слева словно отпечаток пальца.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0909.jpg'])
+
+@ru
+  <p>Заблокированная остановка автобуса.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0911.jpg'])
+
+@ru
+  <p>Незакрашенный кусок плитки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0919.jpg'])
+
+@ru
+  <p>Крепость с португальским флагом.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0925.jpg',
+  'IMG_0926.jpg',
+]])
+
+@ru
+  <p>У подножия крепости.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0928.jpg'])
+
+@ru
+  <p>Легкая живая изгородь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0930.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0931.jpg'])
+
+@ru
+  <p>Детская площадка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0934.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0937.jpg'])
+
+@ru
+  <p>Проход сквозь дом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0939.jpg'])
+
+@ru
+  <p>Объемы майской пыльцы напоминают снег.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0941.jpg',
+  'IMG_0945.jpg',
+  'IMG_0946.jpg',
+]])
+
+@ru
+  <p>Канатная дорога.</p>
+@en
+  <p>Cable car.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0954.jpg',
+  'IMG_0953.jpg',
+  'IMG_0951.jpg',
+  'IMG_0949.jpg',
+]])
+
+@ru
+  <p>Платформа на главном вокзале.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0958.jpg'])
+@endsection
