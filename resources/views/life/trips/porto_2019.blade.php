4d77fe6ca4a0617256b6cfd7a80304d067d8230e
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 19:57:09 +0300

    Порту в мае 2019

---
+++ resources/views/life/trips/porto_2019.blade.php
@@ -0,0 +1,158 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Встречает туманище.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0670.jpg',
+  'IMG_0674.jpg',
+  'IMG_0675.jpg',
+  'IMG_0678.jpg',
+  'IMG_0683.jpg',
+  'IMG_0684.jpg',
+  'IMG_0687.jpg',
+  'IMG_0690.jpg',
+  'IMG_0680.jpg',
+]])
+
+@ru
+  <p>То ли дело картина в ясную погоду.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0880.jpg'])
+
+@ru
+  <p>Покупка билета в терминале и тут же валидация по соседству.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0694.jpg'])
+
+@ru
+  <p>Метро. Движение не всегда под землей.</p>
+@en
+  <p>Metro.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0696.jpg'])
+
+@ru
+  <p>Фасады сохранили, а здания местами совсем новые — торцы выдают.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0700.jpg'])
+
+@ru
+  <p>Человек моет тротуар и дорогу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0821.jpg'])
+
+@ru
+  <p>Карта остановок автобусных маршрутов около кругового движения.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0961.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0965.jpg',
+  'IMG_1013.jpg',
+]])
+
+@ru
+  <p>Панорамы.</p>
+@en
+  <p>Panoramas.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0966.jpg',
+  'IMG_0970.jpg',
+  'IMG_0971.jpg',
+  'IMG_0973.jpg',
+  'IMG_0979.jpg',
+  'IMG_0994.jpg',
+  'IMG_0995.jpg',
+  'IMG_0996.jpg',
+  'IMG_0997.jpg',
+]])
+
+@ru
+  <p>Чайка копается в мусоре. Но делает вид, что просто мимо проходила.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0968.jpg'])
+
+@ru
+  <p>Спортивная площадка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0974.jpg'])
+
+@ru
+  <p>Парковка в жилом районе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0976.jpg'])
+
+@ru
+  <p>Непуганый павлин со всех сторон.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0983.jpg',
+  'IMG_0988.jpg',
+  'IMG_0991.jpg',
+]])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0993.jpg'])
+
+@ru
+  <p>Пещера.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1001.jpg'])
+
+@ru
+  <p>Павлины свободно разгуливают по парку и периодически устраивают перекличку.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1002.jpg',
+  'IMG_1003.jpg',
+]])
+
+@ru
+  <p>Велопарковка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1005.jpg'])
+
+@ru
+  <p>Думал, что хотя бы это знакомое слово правильно прочитаю с португальского, но нет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1009.jpg'])
+
+@ru
+  <p>Здание.</p>
+@en
+  <p>Building.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1017.jpg'])
+
+@ru
+  <p>Сиденья в вагоне метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1026.jpg'])
+
+@ru
+  <p>Британский шоколад.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1028.jpg'])
+
+@ru
+  <p>Когда разница в цене между первым и вторым классом около 10%, то грех не попробовать первый. Всего три места в ряду, причем есть одиночное. Проводник вместо билета спрашивает имя или сам его называет и просит подтвердить ты ли это. Если билет бумажный, то компостирует его.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1030.jpg'])
+
+@ru
+  <p>Широченный стол, вай-фай, все дела. Лепота.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_1032.jpg'])
+@endsection
