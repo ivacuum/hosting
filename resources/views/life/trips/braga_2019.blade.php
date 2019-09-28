4676937803a1cc7b5888c769cabf43d337b84491
Sergey Pankov <vacuum@ivacuum.ru>
2019-09-28 20:05:44 +0300

    Брага в мае 2019

---
+++ resources/views/life/trips/braga_2019.blade.php
@@ -0,0 +1,115 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Центральный вокзал снаружи.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0879.jpg'])
+
+@ru
+  <p>Свисающие буквы вывески железнодорожных касс.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0830.jpg'])
+
+@ru
+  <p>Оцеплено полицией.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0835.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0836.jpg',
+  'IMG_0838.jpg',
+  'IMG_0839.jpg',
+  'IMG_0842.jpg',
+  'IMG_0863.jpg',
+  'IMG_0875.jpg',
+  'IMG_0876.jpg',
+]])
+
+@ru
+  <p>Ярмарка в живописном месте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0841.jpg'])
+
+@ru
+  <p>Фонтан.</p>
+@en
+  <p>Fountain.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0853.jpg'])
+
+@ru
+  <p>Пешеходная зона.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0854.jpg',
+  'IMG_0856.jpg',
+]])
+
+@ru
+  <p>Туннель закончился, держать включенными габариты стало необязательно.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0857.jpg'])
+
+@ru
+  <p>Балконы.</p>
+@en
+  <p>Balconies.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0859.jpg'])
+
+@ru
+  <p>Сколько раз название отеля ибис подписано? Вижу как минимум восемь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0860.jpg'])
+
+@ru
+  <p>Набережная под мостом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0864.jpg'])
+
+@ru
+  <p>Парк.</p>
+@en
+  <p>Park.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0865.jpg',
+  'IMG_0867.jpg',
+  'IMG_0868.jpg',
+  'IMG_0870.jpg',
+  'IMG_0871.jpg',
+]])
+
+@ru
+  <p>Обилие бананов в супермаркете.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0872.jpg'])
+
+@ru
+  <p>Проход к фонтану.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0831.jpg'])
+
+@ru
+  <p>Скамейка.</p>
+@en
+  <p>Bench.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0833.jpg'])
+
+@ru
+  <p>Валидировать поездку нужно еще до выхода на платформу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0827.jpg'])
+
+@ru
+  <p>Платформа центрального вокзала.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0826.jpg'])
+@endsection
