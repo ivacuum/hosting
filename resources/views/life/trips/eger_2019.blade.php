6d50401905a5b689329c73adbabf1fffa253126c
Sergey Pankov <vacuum@ivacuum.ru>
2019-10-02 23:57:02 +0300

    Эгер в июне 2019

---
+++ resources/views/life/trips/eger_2019.blade.php
@@ -0,0 +1,128 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Люк.</p>
+@en
+  <p>Manhole.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2722.jpg'])
+
+@ru
+  <p>Улицы.</p>
+@en
+  <p>Streets.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2727.jpg',
+  'IMG_2741.jpg',
+  'IMG_2786.jpg',
+]])
+
+@ru
+  <p>Знатный на этой улице банкет можно устроить с таким количество столов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2728.jpg'])
+
+@ru
+  <p>Спуск к каналу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2730.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2731.jpg'])
+
+@ru
+  <p>Интересная растительность у дерева.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2732.jpg'])
+
+@ru
+  <p>Площадь.</p>
+@en
+  <p>Square.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2742.jpg'])
+
+@ru
+  <p>Фотосессия у букв с названием города.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2744.jpg'])
+
+@ru
+  <p>Мост.</p>
+@en
+  <p>Bridge.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2751.jpg'])
+
+@ru
+  <p>Спуск к воде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2753.jpg'])
+
+@ru
+  <p>Бочка.</p>
+@en
+  <p>Barrel.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2754.jpg'])
+
+@ru
+  <p>Еще площадь.</p>
+@en
+  <p>Another square.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2757.jpg'])
+
+@ru
+  <p>Крохотная уличная табличка. Классная вывеска-надпись.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2770.jpg'])
+
+@ru
+  <p>Почтовый ящик.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2771.jpg'])
+
+@ru
+  <p>Подъем к крепости. Наличие перечеркнутой уличной таблички подсказывает, что улицу недавно переименовали.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2774.jpg'])
+
+@ru
+  <p>Единорог.</p>
+@en
+  <p>Unicorn.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2784.jpg'])
+
+@ru
+  <p>Граница плитки зафиксирована, чтобы она не расползалась.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2787.jpg',
+  'IMG_2789.jpg',
+]])
+
+@ru
+  <p>Необходимый атрибут для занятия парковочных мест для инвалидов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2792.jpg'])
+
+@ru
+  <p>Черника или голубика? Слово кек часто использовали во время поездки на остров <a class="link" href="bali.2016">Бали</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2794.jpg'])
+
+@ru
+  <p>Касса самообслуживания. Стоило раз однажды попробовать, как теперь невозможно от них отказаться.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_2797.jpg'])
+@endsection
