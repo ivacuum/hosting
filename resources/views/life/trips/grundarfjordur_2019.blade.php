7f4d21f9098c732ac63b885720d5e70a296ede03
Sergey Pankov <vacuum@ivacuum.ru>
2019-10-24 09:07:37 +0300

    Заготовка истории

--- resources/views/life/trips/grundarfjordur_2019.blade.php
+++ resources/views/life/trips/grundarfjordur_2019.blade.php
@@ -1,28 +1,78 @@
 @extends('life.trips.base')
 
 @section('content')
-<youtube title="Grundarfjörður⁩, Iceland, June 2019" v="Msqf2ckMHhI"></youtube>
-<youtube title="Seljalandsfoss Waterfall, Iceland, June 2019" v="-L0mlFzaNdE"></youtube>
+@ru
+  <p>Новый день — новый тур. В этот раз маленький автобус, гидом является водитель. За счет маленькой группы и более маневренного транспорта маршрут содержит дополнительные точки в отличие от такового на большом автобусе.</p>
+@endru
 
+@ru
+  <p>Выезд из Рейкьявика на север.</p>
+@endru
 IMG_3520.jpeg
 IMG_3521.jpeg
 IMG_3525.jpeg
 IMG_3529.jpeg
 IMG_3531.jpeg
 IMG_3532.jpeg
+
+@ru
+  <p>Остановка на завтрак. Парковка грузовиков.</p>
+@endru
 IMG_3534.jpeg
+
+@ru
+  <p>Заправка.</p>
+@endru
 IMG_3536.jpeg
+
+@ru
+  <p>Прогулка по городу.</p>
+@endru
 IMG_3538.jpeg
 IMG_3540.jpeg
+
+@ru
+  <p>Островки безопасности на пешеходном переходе.</p>
+@endru
 IMG_3542.jpeg
+
+@ru
+  <p>Автобусная остановка.</p>
+@endru
 IMG_3544.jpeg
+
+@ru
+  <p>Лошадки.</p>
+@en
+  <p>Horses.</p>
+@endru
 IMG_3551.jpeg
+
+@ru
+  <p>По всему острову наблюдается обилие заборов. Возможно, они для овец, лошадей и прочих животных, чтобы те на дорогу не выходили.</p>
+@endru
 IMG_3552.jpeg
 IMG_3554.jpeg
+
+@ru
+  <p>Дорога из автобуса.</p>
+@endru
 IMG_3558.jpeg
+
+@ru
+  <p>Дорога снаружи.</p>
+@endru
 IMG_3562.jpeg
-IMG_3566.jpeg
+
+@ru
+  <p>Место для пикника.</p>
+@endru
 IMG_3568.jpeg
+
+@ru
+  <p>Остановка посмотреть небольшой водопад. Осмотримся.</p>
+@endru
+IMG_3566.jpeg
 IMG_3570.jpeg
 IMG_3572.jpeg
 IMG_3573.jpeg
@@ -34,14 +84,24 @@ IMG_3583.jpeg
 IMG_3586.jpeg
 IMG_3588.jpeg
 IMG_3590.jpeg
+
+@ru
+  <p>Водителя зовут Боги. Он даже знал как его имя переводится с русского.</p>
+@endru
 IMG_3600.jpeg
+
+@ru
+  <p>На пути к Грюндарфьодюру.</p>
+@endru
 IMG_3604.jpeg
 IMG_3606.jpeg
 IMG_3610.jpeg
 IMG_3615.jpeg
 IMG_3616.jpeg
-IMG_3621.jpeg
-IMG_3623.jpeg
+
+@ru
+  <p>Прибыли в Грюндарфьодюр. Есть время осмотреться. Город, окруженный горами.</p>
+@endru
 IMG_3624.jpeg
 IMG_3625.jpeg
 IMG_3628.jpeg
@@ -50,14 +110,40 @@ IMG_3631.jpeg
 IMG_3632.jpeg
 IMG_3633.jpeg
 IMG_3634.jpeg
-IMG_3635.jpeg
-IMG_3639.jpeg
-IMG_3642.jpeg
 IMG_3644.jpeg
 IMG_3645.jpeg
 IMG_3646.jpeg
-IMG_3647.jpeg
 IMG_3648.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_3635.jpeg
+
+@ru
+  <p>Чайки столпились у корабля.</p>
+@endru
+IMG_3639.jpeg
+
+@ru
+  <p>Виды рыб, птиц и прочих животных города.</p>
+@endru
+IMG_3642.jpeg
+
+@ru
+  <p>Пожарный гидрант.</p>
+@endru
+IMG_3647.jpeg
+
+@ru
+  <p>Перекус в заведении, где в соседней комнате небольшая библиотека. Часть книг можно взять совершенно свободно.</p>
+@endru
+IMG_3621.jpeg
+IMG_3623.jpeg
+
+@ru
+  <p>Следующая остановка у водопада в живописном окружении.</p>
+@endru
 IMG_3649.jpeg
 IMG_3651.jpeg
 IMG_3652.jpeg
@@ -68,33 +154,89 @@ IMG_3658.jpeg
 IMG_3660.jpeg
 IMG_3661.jpeg
 IMG_3664.jpeg
-IMG_3665.jpeg
 IMG_3671.jpeg
-IMG_3676.jpeg
-IMG_3677.jpeg
 IMG_3680.jpeg
 IMG_3681.jpeg
 IMG_3682.jpeg
+
+@ru
+  <p>Дронам по-прежнему не рады.</p>
+@endru
+IMG_3665.jpeg
+
+@ru
+  <p>Гора с открыток.</p>
+@endru
+IMG_3676.jpeg
+
+@ru
+  <p>Нескончаемые заборы.</p>
+@endru
+IMG_3677.jpeg
+
+@ru
+  <p>Прокатная контора делится наблюдением.</p>
+@endru
 IMG_3684.jpeg
-IMG_3687.jpeg
+
+@ru
+  <p>И вновь рывок к следующей природной точке.</p>
+@endru
 IMG_3690.jpeg
+IMG_3687.jpeg
+
+@ru
+  <p>Вышка.</p>
+@endru
 IMG_3693.jpeg
+
+@ru
+  <p>Глыбы — поля застывшей лавы.</p>
+@endru
 IMG_3695.jpeg
 IMG_3699.jpeg
 IMG_3702.jpeg
 IMG_3708.jpeg
+
+@ru
+  <p>Исландцев беспокоит, что этот ледник стремительно тает.</p>
+@endru
 IMG_3710.jpeg
+
+@ru
+  <p>Место для пикника для среди застывшей лавы.</p>
+@endru
 IMG_3712.jpeg
+
+@ru
+  <p>Это все вокруг застывшая лава.</p>
+@endru
 IMG_3713.jpeg
 IMG_3715.jpeg
 IMG_3716.jpeg
 IMG_3718.jpeg
+
+@ru
+  <p>Людей заинтересовала щель в скале.</p>
+@endru
 IMG_3719.jpeg
+
+@ru
+  <p>Вулканический песок на пляже. Даже галька.</p>
+@endru
 IMG_3720.jpeg
-IMG_3722.jpeg
 IMG_3726.jpeg
 IMG_3729.jpeg
 IMG_3734.jpeg
+
+@ru
+  <p>Лужа.</p>
+@endru
+IMG_3722.jpeg
+
+@ru
+  <p>Осмотримся.</p>
+@endru
 IMG_3735.jpeg
 IMG_3737.jpeg
 IMG_3738.jpeg
@@ -107,12 +249,22 @@ IMG_3750.jpeg
 IMG_3755.jpeg
 IMG_3757.jpeg
 IMG_3758.jpeg
-IMG_3762.jpeg
 IMG_3763.jpeg
 IMG_3766.jpeg
 IMG_3768.jpeg
-IMG_3771.jpeg
 IMG_3773.jpeg
+
+@ru
+  <p>Вовремя на ледник посмотрели — облака прилетели закрывать обзор.</p>
+@endru
+IMG_3762.jpeg
+IMG_3771.jpeg
+
+@ru
+  <p>Остановка в деревне Арнарстапи на ужин.</p>
+@endru
+<youtube title="Arnarstapi, Iceland, June 2019" v="Msqf2ckMHhI"></youtube>
+IMG_3835.jpeg
 IMG_3776.jpeg
 IMG_3777.jpeg
 IMG_3780.jpeg
@@ -124,24 +276,61 @@ IMG_3790.jpeg
 IMG_3794.jpeg
 IMG_3795.jpeg
 IMG_3796.jpeg
-IMG_3797.jpeg
 IMG_3801.jpeg
-IMG_3806.jpeg
-IMG_3808.jpeg
 IMG_3810.jpeg
 IMG_3814.jpeg
-IMG_3817.jpeg
 IMG_3819.jpeg
 IMG_3822.jpeg
 IMG_3825.jpeg
 IMG_3826.jpeg
 IMG_3828.jpeg
 IMG_3829.jpeg
-IMG_3835.jpeg
+
+@ru
+  <p>Туалет?</p>
+@endru
+IMG_3797.jpeg
+
+@ru
+  <p>Чайки.</p>
+@endru
+IMG_3806.jpeg
+IMG_3808.jpeg
+
+@ru
+  <p>Указатель.</p>
+@endru
+IMG_3817.jpeg
+
+@ru
+  <p>Дорога.</p>
+@en
+  <p>Road.</p>
+@endru
 IMG_3836.jpeg
+
+@ru
+  <p>Наша немногочисленная группа. Интересно, что много американцев, а гид называет единицы в метрической системе.</p>
+@endru
 IMG_3842.jpeg
+
+@ru
+  <p>Последняя остановка тура на пляже перед отправлением домой.</p>
+@endru
 IMG_3844.jpeg
+
+@ru
+  <p>Последний красочный снимок на сегодня.</p>
+@endru
 IMG_3847.jpeg
+
+@ru
+  <p>Последний снимок пейзажа за окном.</p>
+@endru
 IMG_3853.jpeg
+
+@ru
+  <p>В России попадалось как дети убирали упавшие на пол конфеты обратно в контейнеры. И вот в Исландии несколько конфет валяется на полу и неизвестно сколько из них вернули к остальным продаваемым.</p>
+@endru
 IMG_3858.jpeg
 @endsection
