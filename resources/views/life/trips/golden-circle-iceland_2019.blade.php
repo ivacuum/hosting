282e1b2e17fd6b8b1dc366b43cb3fc3f2f57e2bd
Sergey Pankov <vacuum@ivacuum.ru>
2019-10-25 08:47:15 +0400

    Заготовки историй

--- resources/views/life/trips/golden-circle-iceland_2019.blade.php
+++ resources/views/life/trips/golden-circle-iceland_2019.blade.php
@@ -2,14 +2,24 @@
 
 @section('content')
 @ru
-  <p></p>
+  <p>Самый короткий и простой ознакомительный тур. Оставлен на день вылета, чтобы было время добраться до аэропорта.</p>
 @endru
 
-IMG_3973.jpeg
-IMG_3964.jpeg
+@ru
+  <p>Расписание четырех автобусных туров одной из туристических компаний.</p>
+@endru
 IMG_3968.jpeg
+
+@ru
+  <p>Пейзажи за окном.</p>
+@endru
+IMG_3964.jpeg
 IMG_3969.jpeg
-IMG_3979.jpeg
+IMG_3973.jpeg
+
+@ru
+  <p>Остановка у кратера. Кто-то просто остается у лавочки. Но времени достаточно, чтобы обойти кратер по периметру.</p>
+@endru
 IMG_3981.jpeg
 IMG_3984.jpeg
 IMG_3986.jpeg
@@ -22,23 +32,64 @@ IMG_3998.jpeg
 IMG_4005.jpeg
 IMG_4006.jpeg
 IMG_4009.jpeg
-IMG_4012.jpeg
 IMG_4014.jpeg
+
+@ru
+  <p>Яркий красный гравий.</p>
+@endru
+IMG_4012.jpeg
+IMG_3979.jpeg
+
+@ru
+  <p>Подождите меня!</p>
+@endru
 IMG_4016.jpeg
+
+@ru
+  <p>Всякий день обед с холмистым видом. Цена бургера неизвестного местного заведения стремилась к 800 ₽, супа — к 1200 ₽.</p>
+@endru
 IMG_4028.jpeg
+
+@ru
+  <p>Гейзеры ограждены. Табличка предостерегает от горячей воды.</p>
+@endru
 IMG_4030.jpeg
-IMG_4031.jpeg
-IMG_4033.jpeg
-IMG_4036.jpeg
-IMG_4038.jpeg
 IMG_4042.jpeg
 IMG_4043.jpeg
 IMG_4046.jpeg
+
+@ru
+  <p>Малый гейзер уже на пенсии.</p>
+@endru
+IMG_4031.jpeg
+
+@ru
+  <p>Большой гейзер.</p>
+@endru
+IMG_4033.jpeg
+IMG_4036.jpeg
+
+@ru
+  <p>Есть время подняться на холм.</p>
+@endru
+IMG_4038.jpeg
+
+@ru
+  <p>Подъем сопровождают люпины.</p>
+@endru
 IMG_4050.jpeg
 IMG_4053.jpeg
 IMG_4054.jpeg
+
+@ru
+  <p>Гейзеры и тут.</p>
+@endru
 IMG_4055.jpeg
 IMG_4057.jpeg
+
+@ru
+  <p>Подниматься недолго, но точка весьма высока, что открывается хороший обзор местности.</p>
+@endru
 IMG_4061.jpeg
 IMG_4062.jpeg
 IMG_4065.jpeg
@@ -48,13 +99,25 @@ IMG_4074.jpeg
 IMG_4077.jpeg
 IMG_4079.jpeg
 IMG_4082.jpeg
+
+@ru
+  <p>Замечены лошадки.</p>
+@endru
+IMG_4094.jpeg
 IMG_4086.jpeg
 IMG_4087.jpeg
 IMG_4089.jpeg
 IMG_4091.jpeg
 IMG_4093.jpeg
-IMG_4094.jpeg
+
+@ru
+  <p>Опять дронам не рады.</p>
+@endru
 IMG_4100.jpeg
+
+@ru
+  <p>Двойной водопад.</p>
+@endru
 IMG_4102.jpeg
 IMG_4104.jpeg
 IMG_4106.jpeg
@@ -66,6 +129,10 @@ IMG_4126.jpeg
 IMG_4129.jpeg
 IMG_4131.jpeg
 IMG_4133.jpeg
+
+@ru
+  <p>Остановка на парковке. Выбор пойти до следующей точки пешком полчаса, либо остаться в автобусе, чтобы он довез. Конечно, пешком.</p>
+@endru
 IMG_4137.jpeg
 IMG_4138.jpeg
 IMG_4140.jpeg
@@ -77,9 +144,25 @@ IMG_4152.jpeg
 IMG_4154.jpeg
 IMG_4156.jpeg
 IMG_4157.jpeg
+
+@ru
+  <p>Солнечные часы и компас?</p>
+@endru
 IMG_4159.jpeg
+
+@ru
+  <p>Разлом.</p>
+@endru
 IMG_4163.jpeg
+
+@ru
+  <p>Автобус с чешскими номерами. Вполне мог на пароме из Европы переправиться.</p>
+@endru
 IMG_4166.jpeg
+
+@ru
+  <p>Финальные пейзажи за окном.</p>
+@endru
 IMG_4169.jpeg
 IMG_4174.jpeg
 @endsection
