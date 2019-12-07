2a4b8787d8bdd9200d9a7ec902e11cde88849ddd
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-07 13:39:55 +0300

    Оформление новы

--- resources/views/vendor/nova/partials/meta.blade.php
+++ resources/views/vendor/nova/partials/meta.blade.php
@@ -1,1 +1,12 @@
 {{-- <link rel="icon" type="image/png" href="{{ asset('/img/favicon.png') }} "> --}}
+<style>
+.px-view {
+  padding-left: 2.5rem;
+  padding-right: 2.5rem;
+}
+.py-view {
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+.table td { height: 2.8125rem; }
+</style>
