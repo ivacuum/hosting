851c6b8e05d6358e88959bf0e660539075530dbf
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-09 23:06:18 +0300

    Золотое кольцо в июне 2019

--- resources/views/life/trips/golden-circle-iceland_2019.blade.php
+++ resources/views/life/trips/golden-circle-iceland_2019.blade.php
@@ -2,167 +2,191 @@
 
 @section('content')
 @ru
-  <p>Самый короткий и простой ознакомительный тур. Оставлен на день вылета, чтобы было время добраться до аэропорта.</p>
+  <p>Самый короткий и простой ознакомительный тур. Оставлен на день вылета, чтобы вечером было достаточно времени добраться до аэропорта на рейс до Москвы.</p>
 @endru
 
 @ru
   <p>Расписание четырех автобусных туров одной из туристических компаний.</p>
 @endru
-IMG_3968.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3968.jpg'])
 
 @ru
   <p>Пейзажи за окном.</p>
 @endru
-IMG_3964.jpeg
-IMG_3969.jpeg
-IMG_3973.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3964.jpg',
+  'IMG_3969.jpg',
+  'IMG_3973.jpg',
+]])
 
 @ru
-  <p>Остановка у кратера. Кто-то просто остается у лавочки. Но времени достаточно, чтобы обойти кратер по периметру.</p>
+  <p>Остановка у кратера. Часть людей просто остается у лавочки. Но времени достаточно, чтобы обойти кратер по периметру.</p>
 @endru
-IMG_3981.jpeg
-IMG_3984.jpeg
-IMG_3986.jpeg
-IMG_3987.jpeg
-IMG_3989.jpeg
-IMG_3991.jpeg
-IMG_3993.jpeg
-IMG_3997.jpeg
-IMG_3998.jpeg
-IMG_4005.jpeg
-IMG_4006.jpeg
-IMG_4009.jpeg
-IMG_4014.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3981.jpg',
+  'IMG_3984.jpg',
+  'IMG_3986.jpg',
+  'IMG_3987.jpg',
+  'IMG_3989.jpg',
+  'IMG_3991.jpg',
+  'IMG_3993.jpg',
+  'IMG_3997.jpg',
+  'IMG_3998.jpg',
+  'IMG_4005.jpg',
+  'IMG_4006.jpg',
+  'IMG_4009.jpg',
+  'IMG_4014.jpg',
+]])
 
 @ru
   <p>Яркий красный гравий.</p>
 @endru
-IMG_4012.jpeg
-IMG_3979.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4012.jpg',
+  'IMG_3979.jpg',
+]])
 
 @ru
   <p>Подождите меня!</p>
 @endru
-IMG_4016.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4016.jpg'])
 
 @ru
   <p>Всякий день обед с холмистым видом. Цена бургера неизвестного местного заведения стремилась к 800 ₽, супа — к 1200 ₽.</p>
 @endru
-IMG_4028.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4028.jpg'])
 
 @ru
   <p>Гейзеры ограждены. Табличка предостерегает от горячей воды.</p>
 @endru
-IMG_4030.jpeg
-IMG_4042.jpeg
-IMG_4043.jpeg
-IMG_4046.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4030.jpg',
+  'IMG_4042.jpg',
+  'IMG_4043.jpg',
+  'IMG_4046.jpg',
+]])
 
 @ru
   <p>Малый гейзер уже на пенсии.</p>
 @endru
-IMG_4031.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4031.jpg'])
 
 @ru
   <p>Большой гейзер.</p>
 @endru
-IMG_4033.jpeg
-IMG_4036.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4033.jpg',
+  'IMG_4036.jpg',
+]])
 
 @ru
   <p>Есть время подняться на холм.</p>
 @endru
-IMG_4038.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4038.jpg'])
 
 @ru
   <p>Подъем сопровождают люпины.</p>
 @endru
-IMG_4050.jpeg
-IMG_4053.jpeg
-IMG_4054.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4050.jpg',
+  'IMG_4053.jpg',
+  'IMG_4054.jpg',
+]])
 
 @ru
   <p>Гейзеры и тут.</p>
 @endru
-IMG_4055.jpeg
-IMG_4057.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4055.jpg',
+  'IMG_4057.jpg',
+]])
 
 @ru
   <p>Подниматься недолго, но точка весьма высока, что открывается хороший обзор местности.</p>
 @endru
-IMG_4061.jpeg
-IMG_4062.jpeg
-IMG_4065.jpeg
-IMG_4069.jpeg
-IMG_4071.jpeg
-IMG_4074.jpeg
-IMG_4077.jpeg
-IMG_4079.jpeg
-IMG_4082.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4061.jpg',
+  'IMG_4062.jpg',
+  'IMG_4065.jpg',
+  'IMG_4069.jpg',
+  'IMG_4071.jpg',
+  'IMG_4074.jpg',
+  'IMG_4077.jpg',
+  'IMG_4079.jpg',
+  'IMG_4082.jpg',
+]])
 
 @ru
   <p>Замечены лошадки.</p>
 @endru
-IMG_4094.jpeg
-IMG_4086.jpeg
-IMG_4087.jpeg
-IMG_4089.jpeg
-IMG_4091.jpeg
-IMG_4093.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4094.jpg',
+  'IMG_4086.jpg',
+  'IMG_4087.jpg',
+  'IMG_4089.jpg',
+  'IMG_4091.jpg',
+  'IMG_4093.jpg',
+]])
 
 @ru
   <p>Опять дронам не рады.</p>
 @endru
-IMG_4100.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4100.jpg'])
 
 @ru
   <p>Двойной водопад.</p>
 @endru
-IMG_4102.jpeg
-IMG_4104.jpeg
-IMG_4106.jpeg
-IMG_4108.jpeg
-IMG_4111.jpeg
-IMG_4117.jpeg
-IMG_4119.jpeg
-IMG_4126.jpeg
-IMG_4129.jpeg
-IMG_4131.jpeg
-IMG_4133.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4102.jpg',
+  'IMG_4104.jpg',
+  'IMG_4106.jpg',
+  'IMG_4108.jpg',
+  'IMG_4111.jpg',
+  'IMG_4117.jpg',
+  'IMG_4119.jpg',
+  'IMG_4126.jpg',
+  'IMG_4129.jpg',
+  'IMG_4131.jpg',
+  'IMG_4133.jpg',
+]])
 
 @ru
   <p>Остановка на парковке. Выбор пойти до следующей точки пешком полчаса, либо остаться в автобусе, чтобы он довез. Конечно, пешком.</p>
 @endru
-IMG_4137.jpeg
-IMG_4138.jpeg
-IMG_4140.jpeg
-IMG_4143.jpeg
-IMG_4146.jpeg
-IMG_4149.jpeg
-IMG_4151.jpeg
-IMG_4152.jpeg
-IMG_4154.jpeg
-IMG_4156.jpeg
-IMG_4157.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4137.jpg',
+  'IMG_4138.jpg',
+  'IMG_4140.jpg',
+  'IMG_4143.jpg',
+  'IMG_4146.jpg',
+  'IMG_4149.jpg',
+  'IMG_4151.jpg',
+  'IMG_4152.jpg',
+  'IMG_4154.jpg',
+  'IMG_4156.jpg',
+  'IMG_4157.jpg',
+]])
 
 @ru
   <p>Солнечные часы и компас?</p>
 @endru
-IMG_4159.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4159.jpg'])
 
 @ru
   <p>Разлом.</p>
 @endru
-IMG_4163.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4163.jpg'])
 
 @ru
   <p>Автобус с чешскими номерами. Вполне мог на пароме из Европы переправиться.</p>
 @endru
-IMG_4166.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4166.jpg'])
 
 @ru
   <p>Финальные пейзажи за окном.</p>
 @endru
-IMG_4169.jpeg
-IMG_4174.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4169.jpg',
+  'IMG_4174.jpg',
+]])
 @endsection
