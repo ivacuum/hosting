77f9287869768c693e8f198e4d09bdd506272a6e
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-09 23:15:10 +0300

    Москва в сентябре 2019

--- resources/views/life/trips/msk_2019_09_29.blade.php
+++ resources/views/life/trips/msk_2019_09_29.blade.php
@@ -4,15 +4,15 @@
 @ru
   <p>Скамейки неподалеку от станции метро Белорусская оказались на удивление комфортными для сидения.</p>
 @endru
-IMG_1345.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1345.jpg'])
 
 @ru
   <p>Впервые в России попалась площадка для выгула собак.</p>
 @endru
-IMG_1346.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1346.jpg'])
 
 @ru
   <p>Детский мир.</p>
 @endru
-IMG_1347.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1347.jpg'])
 @endsection
