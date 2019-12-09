fd565444dc388283dd074622d32e40fc2fb4a05d
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-09 23:14:19 +0300

    Москва в июле 2019

--- resources/views/life/trips/msk_2019_07_21.blade.php
+++ resources/views/life/trips/msk_2019_07_21.blade.php
@@ -4,88 +4,90 @@
 @ru
   <p>По газонам не ходить, уточек не кормить, в воде не плавать, с деревьями не обниматься, со вспышкой не фотографировать, громко не разговаривать и желательно не дышать.</p>
 @endru
-IMG_4508.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4508.jpg'])
 
 @ru
   <p>Точно, и не курить алкоголь в парке.</p>
 @endru
-IMG_4517.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4517.jpg'])
 
 @ru
   <p>Ходить по плитке и смотреть пока можно.</p>
 @endru
-IMG_4514.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4514.jpg'])
 
 @ru
   <p>Въезд в подвал на автомойку.</p>
 @endru
-IMG_4519.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4519.jpg'])
 
 @ru
   <p>Класс энергоэффективности здания.</p>
 @endru
-IMG_4523.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4523.jpg'])
 
 @ru
   <p>Словно в Амстердаме оказался.</p>
 @endru
-IMG_4528.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4528.jpg'])
 
 @ru
   <p>Напротив еще идет стройка.</p>
 @endru
-IMG_4527.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4527.jpg'])
 
 @ru
   <p>Закругленное здание.</p>
 @endru
-IMG_4533.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4533.jpg'])
 
 @ru
   <p>Дворовый комод.</p>
 @endru
-IMG_4534.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4534.jpg'])
 
 @ru
   <p>На каждой улице машины каршеринга — проката автомобилей на манер городского проката велосипедов. Удобно, что для таких машин платная парковка бесплатна.</p>
 @endru
-IMG_4538.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4538.jpg'])
 
 @ru
   <p>Дворец гимназии.</p>
 @endru
-IMG_4546.jpeg
-IMG_4548.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4546.jpg',
+  'IMG_4548.jpg',
+]])
 
 @ru
   <p>Схема спортивного комплекса Лужники. Тут <a class="link" href="metallica.2019">Металлика сегодня играет</a>.</p>
 @endru
-IMG_4551.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4551.jpg'])
 
 @ru
   <p>Российский флаг и закрытые туалеты.</p>
 @endru
-IMG_4556.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4556.jpg'])
 
 @ru
   <p>Набережная.</p>
 @en
   <p>Embankment.</p>
 @endru
-IMG_4557.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4557.jpg'])
 
 @ru
   <p>Расценки проката на набережной.</p>
 @endru
-IMG_4560.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4560.jpg'])
 
 @ru
   <p>Канатная дорога.</p>
 @endru
-IMG_4561.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4561.jpg'])
 
 @ru
   <p>Нашелся красный брат любимой питьевой воды Арктик. Воду выбрал после разочарования в фильтрации водопроводной дома. Фильтры буквально за считанные месяцы приходили в негодность, что приходилось менять их за приличную сумму, чтобы вкус воды раздражал.</p>
 @endru
-IMG_4600.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4600.jpg'])
 @endsection
