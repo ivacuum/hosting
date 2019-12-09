2e69d431a496d3dd3997713ca333feaec7aede4f
Sergey Pankov <vacuum@ivacuum.ru>
2019-12-09 23:15:01 +0300

    Москва в сентябре 2019

--- resources/views/life/trips/msk_2019_09_15.blade.php
+++ resources/views/life/trips/msk_2019_09_15.blade.php
@@ -4,91 +4,93 @@
 @ru
   <p>Площадь Киевского вокзала преобразилась в лучшую сторону.</p>
 @endru
-IMG_1240.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1240.jpg'])
 
 @ru
   <p>Фонтан.</p>
 @endru
-IMG_1241.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1241.jpg'])
 
 @ru
   <p>Сквер.</p>
 @endru
-IMG_1242.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1242.jpg'])
 
 @ru
   <p>В Москве интересные указатели домов попадаются.</p>
 @endru
-IMG_1245.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1245.jpg'])
 
 @ru
   <p>Городская навигация.</p>
 @endru
-IMG_1246.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1246.jpg'])
 
 @ru
   <p>Тренажеры у дома в осенних красках.</p>
 @endru
-IMG_1247.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1247.jpg'])
 
 @ru
   <p>Храм.</p>
 @endru
-IMG_1248.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1248.jpg'])
 
 @ru
   <p>Велозаезд, по случаю которого временно перекрыли дорогу.</p>
 @endru
-IMG_1249.jpeg
-IMG_1251.jpeg
-IMG_1253.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1249.jpg',
+  'IMG_1251.jpg',
+  'IMG_1253.jpg',
+]])
 
 @ru
   <p>Трамвай говорит спасибо.</p>
 @en
   <p>Tram says thanks.</p>
 @endru
-IMG_1254.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1254.jpg'])
 
 @ru
   <p>Цветочная арка.</p>
 @endru
-IMG_1255.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1255.jpg'])
 
 @ru
   <p>За сценой.</p>
 @en
   <p>Behind the scene.</p>
 @endru
-IMG_1256.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1256.jpg'])
 
 @ru
   <p>Променад.</p>
 @endru
-IMG_1257.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1257.jpg'])
 
 @ru
   <p>Еще трамвай.</p>
 @endru
-IMG_1259.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1259.jpg'])
 
 @ru
   <p>Пчелы — удивительные существа.</p>
 @endru
-IMG_1261.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1261.jpg'])
 
 @ru
   <p>Развлекалово по случаю столетия государственного цирка. Сюда завел поиск свободных мест для сдачи прокатного велосипеда.</p>
 @endru
-IMG_1264.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1264.jpg'])
 
 @ru
   <p>Фонтан.</p>
 @endru
-IMG_1265.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1265.jpg'])
 
 @ru
   <p>Смена караула.</p>
 @endru
-IMG_1266.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1266.jpg'])
 @endsection
