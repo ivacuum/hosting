6fdaebb711d4c8d2babb4696383f04b4dd64a7f1
Sergey Pankov <vacuum@ivacuum.ru>
2020-02-09 05:38:54 +0300

    Обновление зависимостей

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,7 +24,7 @@ mix.js('resources/js/app.js', 'public/assets')
   .copy('resources/js/pwa/service-worker-installer.js', 'public/assets')
 
   .copy('node_modules/intersection-observer/intersection-observer.js', 'public/assets/intersection-observer.js')
-  .copy('node_modules/promise-polyfill/promise.min.js', 'public/assets/polyfills.js')
+  .copy('node_modules/promise-polyfill/dist/polyfill.min.js', 'public/assets/polyfills.js')
 
   .copy('node_modules/mousetrap/mousetrap.min.js', 'public/assets/mousetrap.js')
 
