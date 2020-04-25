a00a7d1994e2a325d240fb4f74ef023563539671
Sergey Pankov <vacuum@ivacuum.ru>
2020-04-25 14:36:36 +0300

    Описание для тренажера

--- resources/lang/en/meta_description.php
+++ resources/lang/en/meta_description.php
@@ -2,4 +2,6 @@
 
 return [
     'home.index' => 'Personal website of Sergey Pankov also known as vacuum. Useful resources and notes.',
+
+    'japanese-words-trainer' => 'Make sure you have mastered Japanese syllabaries. Six thousand words to type.'
 ];
