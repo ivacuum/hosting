50d4de3705ba4e48ec9bc4743bbdf0be5b1433a6
Sergey Pankov <vacuum@ivacuum.ru>
2020-04-27 05:31:39 +0300

    Тест двух страниц

---
+++ tests/Feature/KoreanPsyTest.php
@@ -0,0 +1,15 @@
+<?php namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class KoreanPsyTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testIndex()
+    {
+        $this->get('korean/psy')
+            ->assertStatus(200);
+    }
+}
---
+++ tests/Feature/KoreanTest.php
@@ -0,0 +1,15 @@
+<?php namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class KoreanTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testIndex()
+    {
+        $this->get('korean')
+            ->assertStatus(200);
+    }
+}
