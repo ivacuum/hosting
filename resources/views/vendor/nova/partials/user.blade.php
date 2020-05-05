f79f4027940774181638febeab5024b6175a0116
Sergey Pankov <vacuum@ivacuum.ru>
2020-05-05 22:13:12 +0300

    Оформление меню в админке

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -1,18 +1,23 @@
 <dropdown-trigger class="h-9 flex items-center">
-    @isset($user->email)
-        <img
-            src="https://secure.gravatar.com/avatar/{{ md5($user->email) }}?size=512"
-            class="rounded-full w-8 h-8 mr-3"
-        />
-    @endisset
+    @include('tpl.avatar', ['classes' => 'w-8 h-8 mr-3'])
 
     <span class="text-90">
-        {{ $user->name ?? $user->email ?? __('Nova User') }}
+        {{ $user->login ?? $user->email ?? __('Nova User') }}
     </span>
 </dropdown-trigger>
 
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
+        <li>
+          <a href="{{ path(App\Http\Controllers\HomeController::class) }}" class="block no-underline text-90 hover:bg-30 p-3">
+            {{ __('Home') }}
+          </a>
+        </li>
+        <li>
+          <a href="{{ path([App\Http\Controllers\Acp\Home::class, 'index']) }}" class="block no-underline text-90 hover:bg-30 p-3">
+            ACP
+          </a>
+        </li>
         <li>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
                 {{ __('Logout') }}
