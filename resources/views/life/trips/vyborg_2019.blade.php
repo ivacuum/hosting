5ca3672a3e18d98154ae34f80cfc498bd339f726
Sergey Pankov <vacuum@ivacuum.ru>
2020-05-14 11:18:18 +0300

    Выборг в августе 2019

--- resources/views/life/trips/vyborg_2019.blade.php
+++ resources/views/life/trips/vyborg_2019.blade.php
@@ -2,35 +2,161 @@
 
 @section('content')
 @ru
-  <p></p>
+  <p>У Питера с Выборгом отличное железнодорожное сообщение. Поезда Ласточка недорогие, частые и комфортные. Поспать в них в пути — благодать.</p>
 @endru
-IMG_0901.jpeg
-IMG_0902.jpeg
-IMG_0903.jpeg
-IMG_0905.jpeg
-IMG_0906.jpeg
-IMG_0908.jpeg
-IMG_0910.jpeg
-IMG_0911.jpeg
-IMG_0912.jpeg
-IMG_0913.jpeg
-IMG_0914.jpeg
-IMG_0915.jpeg
-IMG_0916.jpeg
-IMG_0917.jpeg
-IMG_0919.jpeg
-IMG_0920.jpeg
-IMG_0921.jpeg
-IMG_0922.jpeg
-IMG_0923.jpeg
-IMG_0924.jpeg
-IMG_0926.jpeg
-IMG_0928.jpeg
-IMG_0930.jpeg
-IMG_0931.jpeg
-IMG_0932.jpeg
-IMG_0934.jpeg
-IMG_0935.jpeg
-IMG_0937.jpeg
-IMG_0938.jpeg
+
+@ru
+  <p>С платформы видно подтверждение, что городом не ошибся.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0937.jpg'])
+
+@ru
+  <p>Здание железнодорожного вокзала снаружи.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0934.jpg'])
+
+@ru
+  <p>Набережная.</p>
+@en
+  <p>Embankment.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0902.jpg'])
+
+@ru
+  <p>Рисунки разноцветным мелом на асфальте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0903.jpg'])
+
+@ru
+  <p>Замок окружен водой.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0905.jpg'])
+
+@ru
+  <p>Почтовый ящик на жилом доме.</p>
+@en
+  <p>Postbox.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0906.jpg'])
+
+@ru
+  <p>Висящие посреди улицы фонари напоминают таковые в <a class="link" href="spb">Петербурге</a>. Ширина тротуаров — практически их отсутствие — напоминает <a class="link" href="countries/japan">Японию</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0908.jpg'])
+
+@ru
+  <p>Часовая башня выглядывает.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0910.jpg'])
+
+@ru
+  <p>Граффити.</p>
+@en
+  <p>Graffiti.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0911.jpg'])
+
+@ru
+  <p>Парковка для служебных машин.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0912.jpg'])
+
+@ru
+  <p>Репейник. В детстве часто попадался, а потом перестал.</p>
+@en
+  <p>Greater burdock.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0914.jpg'])
+
+@ru
+  <p>Вход в дом зарос.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0915.jpg'])
+
+@ru
+  <p>Закрыто. Совсем закрыто.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0916.jpg'])
+
+@ru
+  <p>И тут закрыто.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0913.jpg'])
+
+@ru
+  <p>Неприметный лозунг.</p>
+@en
+  <p>Discreet slogan.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0917.jpg'])
+
+@ru
+  <p>Башня.</p>
+@en
+  <p>Tower.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0919.jpg'])
+
+@ru
+  <p>Хорошее место обломилось детской площадке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0920.jpg'])
+
+@ru
+  <p>Собор.</p>
+@en
+  <p>Cathedral.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0921.jpg'])
+
+@ru
+  <p>Трамвай. Внутри наливают.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0922.jpg',
+  'IMG_0923.jpg',
+]])
+
+@ru
+  <p>Ярмарка на площади.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_0924.jpg',
+  'IMG_0928.jpg',
+]])
+
+@ru
+  <p>Погода в этот день была переменчивее, чем где-либо — даже славящейся этой особенностью <a class="link" href="countries/iceland">Исландией</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0926.jpg'])
+
+@ru
+  <p>Действительно, достаточно было подождать 5–10 минут, чтобы дождь сменился на солнце.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0930.jpg'])
+
+@ru
+  <p>Полукруглые лавочки для больших компаний.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0931.jpg'])
+
+@ru
+  <p>На календаре 3 августа, а по желтой листве ощущение, что уже как минимум 3 октября, а и то 3 ноября.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0932.jpg'])
+
+@ru
+  <p>В автомате билет на Ласточку купить быстрее и удобнее, чем в кассе. Нужно сделать буквально несколько касаний экрана.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0935.jpg'])
+
+@ru
+  <p>Ласточка. С вай-фаем, конечно. Всей стране бы такие электрички.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0901.jpg'])
+
+@ru
+  <p>Окно вагона с красным кругляшом походит на экран смартфона, готового к записи видео.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_0938.jpg'])
 @endsection
