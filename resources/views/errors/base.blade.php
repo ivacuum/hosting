576c3fc37c8ea3429f19cadbbc2931d73bc996ff
Sergey Pankov <vacuum@ivacuum.ru>
2020-08-10 21:31:24 +0300

    Темнее цвет сообщений на страницах об ошибках

--- resources/views/errors/base.blade.php
+++ resources/views/errors/base.blade.php
@@ -7,7 +7,7 @@ body {
   padding: 0;
   width: 100%;
   height: 100%;
-  color: #B0BEC5;
+  color: #a0aec0;
   display: table;
   font-weight: 100;
   font-family: ui-rounded, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
