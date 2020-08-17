6fa6c20c20298fb8f0a2bb14be7c1026eb7ce44b
Sergey Pankov <vacuum@ivacuum.ru>
2020-08-17 10:01:25 +0300

    Мета для главной

--- resources/lang/en/meta_description.php
+++ resources/lang/en/meta_description.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'home' => 'Personal website of Sergey Pankov also known as vacuum. Useful resources and notes.',
+    '/' => 'Personal website of Sergey Pankov also known as vacuum. Useful resources and notes.',
 
     'japanese/hiragana-katakana' => 'Learn Japanese syllabaries online column by column. We will make you recognize and type symbols fast.',
     'japanese/words-trainer' => 'Make sure you have mastered Japanese syllabaries. Six thousand words to type.'
