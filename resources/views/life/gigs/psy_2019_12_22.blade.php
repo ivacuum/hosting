2b4ae78094bda9dd78612b88bdd15cbad37831d8
Sergey Pankov <vacuum@ivacuum.ru>
2020-08-27 21:38:40 +0300

    Update psy_2019_12_22.blade.php

--- resources/views/life/gigs/psy_2019_12_22.blade.php
+++ resources/views/life/gigs/psy_2019_12_22.blade.php
@@ -4,34 +4,399 @@
 
 @section('content')
 @ru
-  <p></p>
+  <p>Обложка шоу.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4533.jpg'])
+
+@ru
+  <p>Каждую ночь всем желающим выдавали напитки для пополнения хп (здоровья).</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4537.jpg'])
+
+@ru
+  <p>Решив поесть за несколько часов до шоу в Бургер Кинге, попросил кассира сделать бургер не острым, так как днем ранее такой же бургер без пожеланий сделали неприятно острым. Кассир уточнил: «Что — совсем соуса не надо?». Да, попросил совсем без соуса. И все равно бургер вышел острым. Не знаю как они это делают. Что там острит?</p>
 @endru
 
 @ru
-  <p>Коллаж официальных фотографий концерта. С помощью свайпа влево можно посмотреть его полностью.</p>
-@en
-  <p>Official concert photos collage. Swipe left to see it entirely.</p>
+  <p>Все предыдущие разы было непонятно в какую очередь вставать для получения печатной копии билета. Теперь разглядел, что всем с фамилиями от A до Z — сюда. Людей, оформивших билеты по загранпаспорту с латинской фамилией, менее 1%.</p>
 @endru
-@include('tpl.pic-collage', ['pic' => 'collage.jpg', 'w' => 5400, 'h' => 540])
+@include('tpl.pic-2x', ['pic' => 'IMG_4542.jpg'])
+
+@ru
+  <p>Сегодня малиновый билет со входным номером 100 — это вход в первой группе людей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4857.jpg'])
+
+@ru
+  <p>Третьей ночью место в задней половине танцпола. По роликам в интернете было видно, что трибунам посередине открывается шикарное световое и лазерное шоу. Что ж, проверим в ближайшем к трибунам месте.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4545.jpg'])
+
+@ru
+  <p>Действительно, позади только звуковики и трибуны.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4544.jpg'])
+
+@ru
+  <p>Ступеньки хорошо подошли для хранения напитков. Также можно было повесить свои вещи на ограждение. Сами сотрудники предлагали вешать вещи, чтобы освободить руки и спины посетителей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4547.jpg'])
+
+@ru
+  <p>Обшивка поручней. На полу на фоне коробка бутилированного чая.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4553.jpg'])
+
+@ru
+  <p>Начинается. Потрясающий обзор с этого места.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4549.jpg'])
+
+@ru
+  <p>Правила поведения на концерте.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4554.jpg',
+  'IMG_4555.jpg',
+]])
+
+@ru
+  <p>Первая песня: <span class="font-bold">I LUV IT</span>. Даааа, сбоку в прошлые дни так свет не восхищал, как по центру теперь.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4556.jpg',
+  'IMG_4558.jpg',
+  'IMG_4559.jpg',
+]])
+
+@ru
+  <p>Свет за экранами.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4557.jpg'])
+
+@ru
+  <p>Про каждый второй снимок можно писать нечто вроде: вау, шикарно, потрясающе, как красочно!</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4560.jpg'])
+
+@ru
+  <p>Вторая песня: <span class="font-bold">All Night Long</span>. Начинаем прыгать.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — All Night Long (오늘밤새), All Night Stand (올나잇스탠드), Live 2019-12-22" v="OrCDjz1YNJI"/>
+@include('tpl.pic-2x', ['pic' => 'IMG_4563.jpg'])
+
+@ru
+  <p>Время выпить. С собой принес бутылку воды и лимонада. На концерте еще дали бутылку чая.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4567.jpg'])
+
+@ru
+  <p>Третья песня: <span class="font-bold">The End</span>. Записал ее целиком на видео, так как в предыдущие дни очень понравился свет на ней.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — The End (끝), All Night Stand (올나잇스탠드), Live 2019-12-22" v="z9uM7u2QUbE"/>
+
+@ru
+  <p>Четвертая песня: <span class="font-bold">In My Eyes</span>.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4574.jpg',
+  'IMG_4575.jpg',
+  'IMG_4576.jpg',
+  'IMG_4577.jpg',
+  'IMG_4579.jpg',
+  'IMG_4583.jpg',
+]])
+
+@ru
+  <p>Пятая песня: <span class="font-bold">New Face</span>. Глядя ровно по центру, гадал как он так высоко подпрыгнул между танцорами. А это изображение на экране так высоко — на самом деле Сай справа бежит.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4584.jpg',
+  'IMG_4585.jpg',
+  'IMG_4586.jpg',
+  'IMG_4590.jpg',
+]])
+
+@ru
+  <p>Шестая песня: <span class="font-bold">Bird</span>. Каждый год она получает какую-нибудь новую аранжировку. В момент кульминации на сцене было порядка тридцати танцоров.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4591.jpg',
+  'IMG_4593.jpg',
+  'IMG_4595.jpg',
+  'IMG_4597.jpg',
+  'IMG_4599.jpg',
+]])
+
+@ru
+  <p>Седьмая песня: <span class="font-bold">GENTLEMAN</span>. Перед припевами эффектные лазеры. В конце песни заготовка с приглашенным коллективом танцоров.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — GENTLEMAN (젠틀맨), All Night Stand (올나잇스탠드), Live 2019-12-22" v="AyKogyLA2WE"/>
+
+@ru
+  <p>Восьмая песня: <span class="font-bold">Champion</span>. Как же упарывались и синхронизировали музыку со светом!</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Champion (챔피언), All Night Stand (올나잇스탠드), Live 2019-12-22" v="PIxgQmd2K_w"/>
+
+@ru
+  <p>Девятая песня: <span class="font-bold">Napal Baji</span>. Второй сет, новые костюмы. Очень мягкий фиолетовый свет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4603.jpg'])
+
+@ru
+  <p>Между песнями, пока идет общение с публикой, сотрудники раздают чай в бутылках. Расходится целыми коробками. Люди даже в десяти метрах от ограждения свободно за ним подходят.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4611.jpg',
+  'IMG_4612.jpg',
+]])
+
+@ru
+  <p>Десятая песня: <span class="font-bold">What Would Have Been</span>. С лав камерой.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — What would have been (어땠을까), All Night Stand (올나잇스탠드), Live 2019-12-22" v="Kt-vVQqhkOk"/>
+@include('tpl.pic-2x', ['pic' => 'IMG_4616.jpg'])
+
+@ru
+  <p>Одиннадцатая песня: <span class="font-bold">DADDY</span>. Мало того, что тут снова эффектные лазеры перед припевами, так еще и на самих припевах такой трюк, что, когда его впервые увидел, у меня отвисла челюсть со словами: «Что происходит? Откуда столько людей на сцене?». Нравится туса в конце песни под конфетти, когда все вышли в середину зала.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — DADDY, All Night Stand (올나잇스탠드), Live 2019-12-22" v="g_w7GDR9XrM"/>
+
+@ru
+  <p>Двенадцатая песня: <span class="font-bold">I'm a Guy Like This</span>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4621.jpg'])
+
+@ru
+  <p>Тринадцатая песня: <span class="font-bold">White Night</span>. Снежная и рождественская.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4623.jpg',
+  'IMG_4627.jpg',
+]])
+
+@ru
+  <p>Количество вещей и бутылок на ограждениях заметно увеличилось. На сцене второй гость — Хейз.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4632.jpg'])
+
+@ru
+  <p>Четырнадцатая песня: <span class="font-bold">Right Now</span>. Третий сет.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4636.jpg',
+  'IMG_4639.jpg',
+  'IMG_4641.jpg',
+  'IMG_4642.jpg',
+  'IMG_4644.jpg',
+  'IMG_4646.jpg',
+]])
+
+@ru
+  <p>Пятнадцатая песня: <span class="font-bold">Father</span>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4649.jpg'])
+
+@ru
+  <p>Шестнадцатая песня: <span class="font-bold">Shake It</span>. Видео вступления и концовки. Вступлению старался подпевать шепотом, но все равно на записи слышно весьма громко.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Shake It (흔들어주세요) Intro, All Night Stand (올나잇스탠드), Live 2019-12-22" v="WqeqVO2Lf-g"/>
+<livewire:youtube title="191222 PSY (싸이) — Shake It (흔들어주세요) Ending, All Night Stand (올나잇스탠드), Live 2019-12-22" v="t7_tMuIObBk"/>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4651.jpg',
+  'IMG_4652.jpg',
+]])
+
+@ru
+  <p>Сосед потянулся за чаем. Под ступенькой для нас припасена еще одна коробка чая. Все усыпано нарезанным снежком. Голубое полотенце и синий рюкзак с <a class="link" href="psy.2019.07">летнего водного концерта</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4656.jpg'])
+
+@ru
+  <p>Семнадцатая песня: <span class="font-bold">Paradise</span>. Поздравление с Рождеством. Снова снег.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4657.jpg',
+  'IMG_4659.jpg',
+  'IMG_4661.jpg',
+  'IMG_4663.jpg',
+  'IMG_4667.jpg',
+  'IMG_4668.jpg',
+  'IMG_4670.jpg',
+  'IMG_4673.jpg',
+]])
+
+@ru
+  <p>Восемнадцатая песня: <span class="font-bold">Gangnam Style</span>. По совместительству — международный хит. Все же круче эта песня в исполнении зала, когда ее как караоке включают во второй половине концерта.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4674.jpg',
+  'IMG_4675.jpg',
+  'IMG_4677.jpg',
+  'IMG_4678.jpg',
+  'IMG_4680.jpg',
+  'IMG_4681.jpg',
+]])
+
+@ru
+  <p>Девятнадцатая песня: <span class="font-bold">We are the One</span>. Четвертый сет. Еще снег.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4685.jpg',
+  'IMG_4686.jpg',
+  'IMG_4687.jpg',
+  'IMG_4688.jpg',
+  'IMG_4689.jpg',
+  'IMG_4692.jpg',
+  'IMG_4693.jpg',
+  'IMG_4694.jpg',
+]])
+
+@ru
+  <p>Двадцатая песня: <span class="font-bold">Entertainer</span>. Публика на ней традиционно активнее всего. Заснял как это выглядит. Не перестает впечатлять движуха на трибунах.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Entertainer (연예인이), All Night Stand (올나잇스탠드), Live 2019-12-22" v="dQUNhQ0d02c"/>
+
+@ru
+  <p>Двадцать первая песня: <span class="font-bold">Sigh</span>. Внезапно на сцене более семидесяти человек.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Sigh (한숨), All Night Stand (올나잇스탠드), Live 2019-12-22" v="rEfWtitMDRg"/>
+@include('tpl.pic-2x', ['pic' => 'IMG_4696.jpg'])
+
+@ru
+  <p>Двадцать вторая песня: <span class="font-bold">It's Art</span>. Диско-шар-голова и лазерное шоу.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — It's Art (예술이야), All Night Stand (올나잇스탠드), Live 2019-12-22" v="Dyjtby7j-JA"/>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4701.jpg',
+  'IMG_4702.jpg',
+  'IMG_4704.jpg',
+  'IMG_4705.jpg',
+  'IMG_4706.jpg',
+  'IMG_4708.jpg',
+  'IMG_4709.jpg',
+  'IMG_4711.jpg',
+  'IMG_4712.jpg',
+  'IMG_4713.jpg',
+  'IMG_4714.jpg',
+  'IMG_4717.jpg',
+  'IMG_4718.jpg',
+  'IMG_4723.jpg',
+]])
+
+@ru
+  <p>Диско-сет с хитами восьмидесятых и девяностых. Новые костюмы.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4730.jpg',
+  'IMG_4731.jpg',
+  'IMG_4732.jpg',
+  'IMG_4733.jpg',
+  'IMG_4734.jpg',
+  'IMG_4735.jpg',
+  'IMG_4736.jpg',
+  'IMG_4739.jpg',
+  'IMG_4740.jpg',
+]])
+
+@ru
+  <p>Двадцать девятая песня: <span class="font-bold">Refuge</span>. Баллада.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Place to Lean On / Refuge (기댈곳), All Night Stand (올나잇스탠드), Live 2019-12-22" v="76PdBqO-KBQ"/>
+
+@ru
+  <p>Местные рок-хиты минувших десятилетий.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Rock Medley (락 메들리), All Night Stand (올나잇스탠드), Live 2019-12-22" v="KxtruC6NKv8"/>
+
+@ru
+  <p>Тридцать седьмая песня: <span class="font-bold">Last Scene</span>. Баллада.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) — Last Scene (마지막 장면), All Night Stand (올나잇스탠드), Live 2019-12-22" v="DMP3nPT8v6E"/>
+
+@ru
+  <p>Реклама инстаграма. Типа концерт закончился после тридцати семи песен.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4744.jpg'])
+
+@ru
+  <p>Прямое включение из-за кулис и объявление выхода на бис. Новые костюмы. Время четыре утра.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4745.jpg',
+  'IMG_4746.jpg',
+  'IMG_4748.jpg',
+]])
+
+@ru
+  <p>После тридцать восьмой песни. Упал и показывает, что устал.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4754.jpg'])
+
+@ru
+  <p>Но доволен. Зал скандирует «вста-вай». И он встает и продолжает.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4755.jpg'])
+
+@ru
+  <p>Сорок первая песня: <span class="font-bold">Someday</span>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4760.jpg'])
+
+@ru
+  <p>На ней классно показывают зал на экранах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4763.jpg'])
+
+@ru
+  <p>Снова заставка, мол, все закончилось. Но нет, дожидаемся еще одного выхода.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4765.jpg'])
+
+@ru
+  <p>Дискотека продолжается сорок второй песней: <span class="font-bold">It's Art</span>.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4766.jpg',
+  'IMG_4770.jpg',
+]])
+
+@ru
+  <p>Крик: «Прыгаем!»</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4767.jpg',
+  'IMG_4768.jpg',
+]])
+
+@ru
+  <p>Сорок третья песня: <span class="font-bold">As Time Goes By</span>. Завершается пением без микрофона — неожиданно и мощно.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) Ends a Show without a Mic, All Night Stand (올나잇스탠드), Live 2019-12-22" v="SFs1aNnjZIc"/>
+
+@ru
+  <p>Вот теперь закончилось. После сорока трех песен. В пять утра.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_4777.jpg'])
 
 @component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/psy.2019.12.20.jpg'])
   <h5 class="mb-1">@ru Сет @en Set @endru 1</h5>
   <ol class="list-inside pl-0">
     <li>I LUV IT</li>
-    <li>오늘밤 새 <span class="text-sm text-muted">All Night Long</span></li>
-    <li>끝 <span class="text-sm text-muted">The End</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=OrCDjz1YNJI">오늘밤 새</a> <span class="text-sm text-muted">All Night Long</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=z9uM7u2QUbE">끝</a> <span class="text-sm text-muted">The End</span></li>
     <li>내 눈에는 <span class="text-sm text-muted">In My Eyes</span></li>
     <li>New Face</li>
     <li>새 <span class="text-sm text-muted">Bird</span></li>
-    <li>GENTLEMAN</li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=AyKogyLA2WE">GENTLEMAN</a></li>
   </ol>
 
   <h5 class="mt-4 mb-1">@ru Сет @en Set @endru 2</h5>
   <ol class="list-inside pl-0" start="8">
-    <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=PIxgQmd2K_w">챔피언</a> <span class="text-sm text-muted">Champion</span></li>
     <li>나팔바지 <span class="text-sm text-muted">Napal Baji</span></li>
-    <li>어땠을까 <span class="text-sm text-muted">What Would Have Been</span></li>
-    <li>DADDY</li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=Kt-vVQqhkOk">어땠을까</a> <span class="text-sm text-muted">What Would Have Been</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=g_w7GDR9XrM">DADDY</a></li>
     <li>나 이런 사람이야 <span class="text-sm text-muted">I'm a Guy Like This</span></li>
     <li>White Night</li>
   </ol>
@@ -40,7 +405,7 @@
   <ol class="list-inside pl-0" start="14">
     <li>Right Now</li>
     <li>아버지 <span class="text-sm text-muted">Father</span></li>
-    <li>흔들어 주세요 <span class="text-sm text-muted">Shake It</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=WqeqVO2Lf-g">흔들어</a> <a class="link" href="https://www.youtube.com/watch?v=t7_tMuIObBk">주세요</a> <span class="text-sm text-muted">Shake It</span></li>
     <li>낙원 <span class="text-sm text-muted">Paradise</span></li>
     <li>강남스타일 <span class="text-sm text-muted">Gangnam Style</span></li>
   </ol>
@@ -48,38 +413,66 @@
   <h5 class="mt-4 mb-1">@ru Сет @en Set @endru 4</h5>
   <ol class="list-inside pl-0" start="19">
     <li>We are the One</li>
-    <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
-    <li>Sigh</li>
-    <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=dQUNhQ0d02c">연예인</a> <span class="text-sm text-muted">Entertainer</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=rEfWtitMDRg">Sigh</a></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=Dyjtby7j-JA">에술이야</a> <span class="text-sm text-muted">It's Art</span></li>
   </ol>
 
   <h5 class="mt-4 mb-1">Encore 1</h5>
   <ol class="list-inside pl-0" start="23">
     <li>순정</li>
-    <li>쿵따리 샤바라</li>
-    <li>Tears</li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=E8b5bDoTLC0">쿵따리 샤바라</a></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=rP1G80IH7DU">Tears</a></li>
     <li>맨발의 청춘</li>
     <li>와</li>
     <li>Run to you</li>
-    <li>기댈곳 <span class="text-sm text-muted">Refuge</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=76PdBqO-KBQ">기댈곳</a> <span class="text-sm text-muted">Refuge</span></li>
     <li>왼손잡이</li>
-    <li>붉은 노을</li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=KxtruC6NKv8">붉은 노을</a></li>
     <li>나는 나비</li>
     <li>아파트</li>
     <li>말달리자</li>
     <li>그대에게</li>
     <li>여행을 떠나요</li>
-    <li>마지막 장면 <span class="text-sm text-muted">Last Scene</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=DMP3nPT8v6E">마지막 장면</a> <span class="text-sm text-muted">Last Scene</span></li>
   </ol>
 
-  <h5 class="mt-4 mb-1">Encore 2</h5>
+  <h5 class="mt-4 mb-1"><a class="link" href="https://www.youtube.com/watch?v=R8CWLYfMnUE">Encore 2</a></h5>
   <ol class="list-inside pl-0" start="38">
     <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
     <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
     <li>강남스타일 <span class="text-sm text-muted">Gangnam Style</span></li>
     <li>언젠가는 <span class="text-sm text-muted">Someday</span></li>
     <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
-    <li>세월이 가면 <span class="text-sm text-muted">As Time Goes By</span></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=SFs1aNnjZIc">세월이 가면</a> <span class="text-sm text-muted">As Time Goes By</span></li>
   </ol>
 @endcomponent
+
+@ru
+  <p>Коллаж официальных фотографий концерта. С помощью свайпа влево можно посмотреть его полностью.</p>
+@en
+  <p>Official concert photos collage. Swipe left to see it entirely.</p>
+@endru
+@include('tpl.pic-collage', ['pic' => 'collage.jpg', 'w' => 5400, 'h' => 540])
+
+<h3 class="mt-12">@ru Бонусные материалы @en Bonus materials @endru</h3>
+@ru
+  <p>Общение с публикой.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) Talks with Audience, All Night Stand (올나잇스탠드), Live 2019-12-22" v="c6lHSZKfCzU"/>
+
+@ru
+  <p>Обращение по возрастам.</p>
+@endru
+<livewire:youtube title="191222 PSY (싸이) Refers to the Audience by Age, All Night Stand (올나잇스탠드), Live 2019-12-22" v="ev9x35h2XDQ"/>
+
+@ru
+  <ul>
+    <li>Фишка вечера — <a class="link" href="https://www.instagram.com/p/B6c_515DUxH/">движение рукой</a></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=J76cmY1QKaA&t=159">Чувак был гвоздем вечера</a></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=cCKflT4AaA0">Уставшее исполнение предпоследней песни</a></li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=5qwD6M2atoo">Последняя песня концерта</a> с завершением без микрофона</li>
+    <li><a class="link" href="https://www.youtube.com/watch?v=K6xV5jzm5IM">Dynamic Duo — Friday Night</a>, песня третьих гостей концерта. Немного <a class="link" href="https://www.youtube.com/watch?v=D9bPJRgVEgQ">заснял</a> ее сам.</li>
+  </ul>
+@endru
 @endsection
