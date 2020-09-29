348ad43f88c6155eb98f6dba39473c1e960b9c5c
Sergey Pankov <vacuum@ivacuum.ru>
2020-09-29 18:01:58 +0300

    Тесты обновления меты раздач

--- config/app.php
+++ config/app.php
@@ -55,6 +55,7 @@ return [
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
+        'Bus' => Illuminate\Support\Facades\Bus::class,
         'Cache' => Illuminate\Support\Facades\Cache::class,
         'Config' => Illuminate\Support\Facades\Config::class,
         'Cookie' => Illuminate\Support\Facades\Cookie::class,
---
+++ tests/Job/FetchTorrentMetaJobTest.php
@@ -0,0 +1,176 @@
+<?php namespace Tests\Job;
+
+use App\Factory\TorrentFactory;
+use App\Http\GuzzleClientFactory;
+use App\Jobs\FetchTorrentMetaJob;
+use App\Services\Rto;
+use App\Services\RtoTopicData;
+use GuzzleHttp\Psr7\Response;
+use Ivacuum\Generic\Services\Telegram;
+
+class FetchTorrentMetaJobTest extends \Tests\TestCase
+{
+    public function testBodyFetchQueued()
+    {
+        \Bus::fake();
+
+        $infoHash = 'updated-info-hash';
+        $torrent = TorrentFactory::new()->create();
+
+        $topicData = new RtoTopicData(
+            $torrent->rto_id,
+            $torrent->title,
+            $infoHash,
+            $torrent->registered_at,
+            RtoTopicData::STATUS_APPROVED,
+            $torrent->size,
+            3,
+            4,
+            5,
+            now()
+        );
+
+        $rto = new Rto($this->httpClientFactory($topicData));
+        $telegram = \Mockery::mock(Telegram::class);
+        $telegram->shouldReceive('notifyAdmin');
+
+        $job = new FetchTorrentMetaJob($torrent->rto_id);
+        $job->handle($rto, $telegram);
+
+        $torrent->refresh();
+
+        $this->assertSame($infoHash, $torrent->info_hash);
+    }
+
+    public function testDuplicateDeleted()
+    {
+        $torrent = TorrentFactory::new()->create();
+
+        $topicData = new RtoTopicData(
+            $torrent->rto_id,
+            $torrent->title,
+            $torrent->info_hash,
+            $torrent->registered_at,
+            RtoTopicData::STATUS_DUPLICATE,
+            $torrent->size,
+            3,
+            4,
+            5,
+            now()
+        );
+
+        $rto = new Rto($this->httpClientFactory($topicData));
+        $telegram = \Mockery::mock(Telegram::class);
+        $telegram->shouldReceive('notifyAdmin');
+
+        $this->expectsEvents(\App\Events\Stats\TorrentDuplicateDeleted::class);
+
+        $job = new FetchTorrentMetaJob($torrent->rto_id);
+        $job->handle($rto, $telegram);
+
+        $torrent->refresh();
+
+        $this->assertTrue($torrent->trashed());
+    }
+
+    public function testMetaUpdated()
+    {
+        $size = 1234567890;
+        $title = 'TITLE';
+        $torrent = TorrentFactory::new()->create();
+
+        $topicData = new RtoTopicData(
+            $torrent->rto_id,
+            $title,
+            $torrent->info_hash,
+            $torrent->registered_at,
+            RtoTopicData::STATUS_APPROVED,
+            $size,
+            3,
+            4,
+            5,
+            now()
+        );
+
+        $rto = new Rto($this->httpClientFactory($topicData));
+        $telegram = \Mockery::mock(Telegram::class);
+
+        $job = new FetchTorrentMetaJob($torrent->rto_id);
+        $job->handle($rto, $telegram);
+
+        $torrent->refresh();
+
+        $this->assertSame($size, $torrent->size);
+        $this->assertSame($title, $torrent->title);
+    }
+
+    public function testNotFoundAndDeleted()
+    {
+        $torrent = TorrentFactory::new()->create();
+
+        $httpClientFactory = (new GuzzleClientFactory)->forTest([
+            new Response(200, [], json_encode([
+                'result' => [
+                    $torrent->rto_id => null,
+                ],
+            ])),
+        ]);
+
+        $rto = new Rto($httpClientFactory);
+        $telegram = \Mockery::mock(Telegram::class);
+        $telegram->shouldReceive('notifyAdmin');
+
+        $this->expectsEvents(\App\Events\Stats\TorrentNotFoundDeleted::class);
+
+        $job = new FetchTorrentMetaJob($torrent->rto_id);
+        $job->handle($rto, $telegram);
+
+        $torrent->refresh();
+
+        $this->assertTrue($torrent->trashed());
+    }
+
+    public function testPremoderationLeavesTorrentMetaUntouched()
+    {
+        $torrent = TorrentFactory::new()->create();
+
+        $size = $torrent->size;
+        $title = $torrent->title;
+
+        $topicData = new RtoTopicData(
+            $torrent->rto_id,
+            'NEW TITLE',
+            $torrent->info_hash,
+            $torrent->registered_at,
+            RtoTopicData::STATUS_PREMODERATION,
+            1234567890,
+            3,
+            4,
+            5,
+            now()
+        );
+
+        $rto = new Rto($this->httpClientFactory($topicData));
+        $telegram = \Mockery::mock(Telegram::class);
+        $telegram->shouldReceive('notifyAdmin');
+
+        $job = new FetchTorrentMetaJob($torrent->rto_id);
+        $job->handle($rto, $telegram);
+
+        $torrent->refresh();
+
+        $this->assertSame($size, $torrent->size);
+        $this->assertSame($title, $torrent->title);
+    }
+
+    private function httpClientFactory(RtoTopicData $topicData)
+    {
+        return (new GuzzleClientFactory)->forTest([
+            new Response(200, [], json_encode([
+                'result' => [
+                    $topicData->id => $topicData->toJson(),
+                ],
+            ])),
+        ]);
+    }
+}
