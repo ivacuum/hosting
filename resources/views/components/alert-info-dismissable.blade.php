1577842bdfe3be70602b285844f89dfa94dcac9c
Sergey Pankov <vacuum@ivacuum.ru>
2021-01-10 10:35:26 +0300

    Ровнее информационные сообщения

--- resources/views/components/alert-info-dismissable.blade.php
+++ resources/views/components/alert-info-dismissable.blade.php
@@ -3,10 +3,10 @@
     <div class="mr-auto">
       {{ $slot }}
     </div>
-    <div class="leading-none">
+    <div>
       <button
         type="button"
-        class="appearance-none text-2xl opacity-50 hover:opacity-75"
+        class="appearance-none leading-none text-2xl opacity-50 hover:opacity-75"
         onclick="this.closest('.js-alert-dismiss').remove()"
       >&times;</button>
     </div>
