502a2486d435d16ab6df12610fa1659cf56176be
Sergey Pankov <vacuum@ivacuum.ru>
2021-08-05 20:39:10 +0300

    Удален telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,13 +18,6 @@ class AppServiceProvider extends ServiceProvider
         App\Utilities\CountryHelper::class => App\Utilities\CountryHelper::class,
     ];
 
-    public function register()
-    {
-        if ($this->app->isLocal()) {
-            $this->app->register(TelescopeServiceProvider::class);
-        }
-    }
-
     public function boot()
     {
         \Blade::withoutDoubleEncoding();
