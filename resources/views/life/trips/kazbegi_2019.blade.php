49d1f301880efc54aec838d89be4f6b95f49b340
Sergey Pankov <vacuum@ivacuum.ru>
2021-10-10 16:26:25 +0300

    Заготовка истории

---
+++ resources/views/life/trips/kazbegi_2019.blade.php
@@ -0,0 +1,228 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Отправление в 9 утра.</p>
+@endru
+IMG_1794.jpeg
+
+@ru
+  <p>Бронировка тура.</p>
+@endru
+IMG_1795.jpeg
+
+@ru
+  <p>Список туров.</p>
+@endru
+IMG_1790.jpeg
+
+@ru
+  <p>Жинвальское водохранилище.</p>
+@en
+  <p>Zhinvali reservoir.</p>
+@endru
+IMG_1797.jpeg
+IMG_1798.jpeg
+IMG_1813.jpeg
+IMG_1818.jpeg
+
+@ru
+  <p>В группе тура в основном иностранцы. Даже пара японцев есть.</p>
+@endru
+IMG_1799.jpeg
+
+@ru
+  <p>На живописных точках продают еду.</p>
+@endru
+IMG_1812.jpeg
+
+@ru
+  <p>Слои бетона, камня, земли и дерева.</p>
+@endru
+IMG_1814.jpeg
+
+@ru
+  <p>Следующая остановка возле крепости Ананури.</p>
+@endru
+IMG_1822.jpeg
+
+@ru
+  <p>Немало автобусов подвозит немало туристов.</p>
+@endru
+IMG_1821.jpeg
+
+@ru
+  <p>Окрестности.</p>
+@endru
+IMG_1819.jpeg
+IMG_1820.jpeg
+IMG_1823.jpeg
+IMG_1824.jpeg
+IMG_1826.jpeg
+IMG_1829.jpeg
+IMG_1830.jpeg
+IMG_1833.jpeg
+IMG_1844.jpeg
+
+@ru
+  <p>Мост.</p>
+@endru
+IMG_1825.jpeg
+IMG_1836.jpeg
+
+@ru
+  <p>Еще мост.</p>
+@endru
+IMG_1834.jpeg
+IMG_1837.jpeg
+
+@ru
+  <p>Пес прилег на дороге.</p>
+@endru
+IMG_1827.jpeg
+
+@ru
+  <p>Дороги.</p>
+@endru
+IMG_1828.jpeg
+IMG_1845.jpeg
+
+@ru
+  <p>Сама крепость.</p>
+@endru
+IMG_1846.jpeg
+
+@ru
+  <p>Следующая остановка — Гудаири. На обочине наш автобус.</p>
+@endru
+IMG_1847.jpeg
+
+@ru
+  <p>Осенний вид горнолыжного курорта.</p>
+@endru
+IMG_1848.jpeg
+IMG_1849.jpeg
+
+@ru
+  <p>Вокруг потрясающие горы.</p>
+@endru
+IMG_1896.jpeg
+IMG_1897.jpeg
+IMG_1900.jpeg
+IMG_1901.jpeg
+
+@ru
+  <p>Крохотное озеро.</p>
+@endru
+IMG_1902.jpeg
+
+@ru
+  <p>Лошадка.</p>
+@endru
+IMG_1898.jpeg
+
+@ru
+  <p>Оживленный пятачок.</p>
+@endru
+IMG_1899.jpeg
+
+@ru
+  <p>Смотровая площадка.</p>
+@endru
+IMG_1903.jpeg
+IMG_1904.jpeg
+
+@ru
+  <p>Любоваться и любоваться.</p>
+@endru
+IMG_1906.jpeg
+IMG_1923.jpeg
+IMG_1924.jpeg
+IMG_1925.jpeg
+IMG_1926.jpeg
+IMG_1927.jpeg
+IMG_1928.jpeg
+
+@ru
+  <p>Возможность перекуса.</p>
+@endru
+IMG_1937.jpeg
+
+@ru
+  <p>Вип-туалет в Казбеги.</p>
+@endru
+IMG_1939.jpeg
+
+@ru
+  <p>Казбеги.</p>
+@endru
+IMG_1940.jpeg
+
+@ru
+  <p>Пересадка в транспорт поменьше для финального подъема в горы.</p>
+@endru
+IMG_1941.jpeg
+
+@ru
+  <p>Подъема вот сюда.</p>
+@endru
+IMG_1956.jpeg
+IMG_1944.jpeg
+IMG_1943.jpeg
+
+@ru
+  <p>Через неделю здесь уже был снег.</p>
+@endru
+IMG_1957.jpeg
+
+@ru
+  <p>Фотосессии.</p>
+@endru
+IMG_1958.jpeg
+
+@ru
+  <p>Осмотр старины.</p>
+@endru
+IMG_1959.jpeg
+IMG_1960.jpeg
+
+@ru
+  <p>Финальное любование видами перед спуском.</p>
+@endru
+IMG_1967.jpeg
+IMG_1968.jpeg
+
+@ru
+  <p>К себе в автобус позвали тех самых двоих японцев. У них удалось уточнить интересный момент про звук фотовспышки.</p>
+@endru
+IMG_1970.jpeg
+
+@ru
+  <p>Эти два снимка мысленно перенесли в игру Need For Speed 3.</p>
+@endru
+IMG_1975.jpeg
+IMG_1978.jpeg
+
+@ru
+  <p>Вода.</p>
+@endru
+IMG_1976.jpeg
+
+@ru
+  <p>Дорога.</p>
+@endru
+IMG_1977.jpeg
+IMG_1979.jpeg
+
+@ru
+  <p>Жинвальское водохранилище на закате.</p>
+@en
+  <p>Zhinvali reservoir at sunset.</p>
+@endru
+IMG_1980.jpeg
+
+@ru
+  <p>Возвращение в Тбилиси.</p>
+@endru
+IMG_1982.jpeg
+@endsection
