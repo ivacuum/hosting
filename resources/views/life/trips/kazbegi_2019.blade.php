0a58e0a9d4bd0360b6027b7f2f89b26b8d880608
Sergey Pankov <vacuum@ivacuum.ru>
2022-02-20 20:11:12 +0300

    Казбеги в 2019

--- resources/views/life/trips/kazbegi_2019.blade.php
+++ resources/views/life/trips/kazbegi_2019.blade.php
@@ -4,225 +4,253 @@
 @ru
   <p>Отправление в 9 утра.</p>
 @endru
-IMG_1794.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1794.jpg'])
 
 @ru
-  <p>Бронировка тура.</p>
+  <p>Бронировка тура. Именно так назвали процедуру во время обращения в офис.</p>
 @endru
-IMG_1795.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1795.jpg'])
 
 @ru
   <p>Список туров.</p>
 @endru
-IMG_1790.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1790.jpg'])
 
 @ru
   <p>Жинвальское водохранилище.</p>
 @en
   <p>Zhinvali reservoir.</p>
 @endru
-IMG_1797.jpeg
-IMG_1798.jpeg
-IMG_1813.jpeg
-IMG_1818.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1797.jpg',
+  'IMG_1798.jpg',
+  'IMG_1813.jpg',
+  'IMG_1818.jpg',
+]])
 
 @ru
   <p>В группе тура в основном иностранцы. Даже пара японцев есть.</p>
 @endru
-IMG_1799.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1799.jpg'])
 
 @ru
   <p>На живописных точках продают еду.</p>
 @endru
-IMG_1812.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1812.jpg'])
 
 @ru
   <p>Слои бетона, камня, земли и дерева.</p>
 @endru
-IMG_1814.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1814.jpg'])
 
 @ru
   <p>Следующая остановка возле крепости Ананури.</p>
 @endru
-IMG_1822.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1822.jpg'])
 
 @ru
   <p>Немало автобусов подвозит немало туристов.</p>
 @endru
-IMG_1821.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1821.jpg'])
 
 @ru
   <p>Окрестности.</p>
 @endru
-IMG_1819.jpeg
-IMG_1820.jpeg
-IMG_1823.jpeg
-IMG_1824.jpeg
-IMG_1826.jpeg
-IMG_1829.jpeg
-IMG_1830.jpeg
-IMG_1833.jpeg
-IMG_1844.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1819.jpg',
+  'IMG_1820.jpg',
+  'IMG_1823.jpg',
+  'IMG_1824.jpg',
+  'IMG_1826.jpg',
+  'IMG_1829.jpg',
+  'IMG_1830.jpg',
+  'IMG_1833.jpg',
+  'IMG_1844.jpg',
+]])
 
 @ru
   <p>Мост.</p>
 @endru
-IMG_1825.jpeg
-IMG_1836.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1825.jpg',
+  'IMG_1836.jpg',
+]])
 
 @ru
   <p>Еще мост.</p>
 @endru
-IMG_1834.jpeg
-IMG_1837.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1834.jpg',
+  'IMG_1837.jpg',
+]])
 
 @ru
   <p>Пес прилег на дороге.</p>
 @endru
-IMG_1827.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1827.jpg'])
 
 @ru
   <p>Дороги.</p>
 @endru
-IMG_1828.jpeg
-IMG_1845.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1828.jpg',
+  'IMG_1845.jpg',
+]])
 
 @ru
   <p>Сама крепость.</p>
 @endru
-IMG_1846.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1846.jpg'])
 
 @ru
   <p>Следующая остановка — Гудаири. На обочине наш автобус.</p>
 @endru
-IMG_1847.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1847.jpg'])
 
 @ru
   <p>Осенний вид горнолыжного курорта.</p>
 @endru
-IMG_1848.jpeg
-IMG_1849.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1848.jpg',
+  'IMG_1849.jpg',
+]])
 
 @ru
   <p>Вокруг потрясающие горы.</p>
 @endru
-IMG_1896.jpeg
-IMG_1897.jpeg
-IMG_1900.jpeg
-IMG_1901.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1896.jpg',
+  'IMG_1897.jpg',
+  'IMG_1900.jpg',
+  'IMG_1901.jpg',
+]])
 
 @ru
   <p>Крохотное озеро.</p>
 @endru
-IMG_1902.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1902.jpg'])
 
 @ru
   <p>Лошадка.</p>
 @endru
-IMG_1898.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1898.jpg'])
 
 @ru
   <p>Оживленный пятачок.</p>
 @endru
-IMG_1899.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1899.jpg'])
 
 @ru
   <p>Смотровая площадка.</p>
 @endru
-IMG_1903.jpeg
-IMG_1904.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1903.jpg',
+  'IMG_1904.jpg',
+]])
 
 @ru
   <p>Любоваться и любоваться.</p>
 @endru
-IMG_1906.jpeg
-IMG_1923.jpeg
-IMG_1924.jpeg
-IMG_1925.jpeg
-IMG_1926.jpeg
-IMG_1927.jpeg
-IMG_1928.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1906.jpg',
+  'IMG_1923.jpg',
+  'IMG_1924.jpg',
+  'IMG_1925.jpg',
+  'IMG_1926.jpg',
+  'IMG_1927.jpg',
+  'IMG_1928.jpg',
+]])
 
 @ru
   <p>Возможность перекуса.</p>
 @endru
-IMG_1937.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1937.jpg'])
 
 @ru
   <p>Вип-туалет в Казбеги.</p>
 @endru
-IMG_1939.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1939.jpg'])
 
 @ru
   <p>Казбеги.</p>
 @endru
-IMG_1940.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1940.jpg'])
 
 @ru
   <p>Пересадка в транспорт поменьше для финального подъема в горы.</p>
 @endru
-IMG_1941.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1941.jpg'])
 
 @ru
   <p>Подъема вот сюда.</p>
 @endru
-IMG_1956.jpeg
-IMG_1944.jpeg
-IMG_1943.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1956.jpg',
+  'IMG_1944.jpg',
+  'IMG_1943.jpg',
+]])
 
 @ru
   <p>Через неделю здесь уже был снег.</p>
 @endru
-IMG_1957.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1957.jpg'])
 
 @ru
   <p>Фотосессии.</p>
 @endru
-IMG_1958.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1958.jpg'])
 
 @ru
   <p>Осмотр старины.</p>
 @endru
-IMG_1959.jpeg
-IMG_1960.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1959.jpg',
+  'IMG_1960.jpg',
+]])
 
 @ru
   <p>Финальное любование видами перед спуском.</p>
 @endru
-IMG_1967.jpeg
-IMG_1968.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1967.jpg',
+  'IMG_1968.jpg',
+]])
 
 @ru
-  <p>К себе в автобус позвали тех самых двоих японцев. У них удалось уточнить интересный момент про звук фотовспышки.</p>
+  <p>К себе в автобус позвали тех самых двоих японцев. У них удалось уточнить интересный момент про мобильный звук затвора. Дело в том, что в японских моделях телефонов неотключаемый звук затвора во время фотографирования, а в тех же европейских моделях бесшумный режим телефона отключает и звук затвора. Даже с подключенными наушниками их телефоны издавали шум затвора на весь автобус. Таков японский закон, чтоб не снимали без палева. Чудеса, что хватило знания японского, чтобы это у них спросить.</p>
 @endru
-IMG_1970.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1970.jpg'])
 
 @ru
   <p>Эти два снимка мысленно перенесли в игру Need For Speed 3.</p>
 @endru
-IMG_1975.jpeg
-IMG_1978.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1975.jpg',
+  'IMG_1978.jpg',
+]])
 
 @ru
   <p>Вода.</p>
 @endru
-IMG_1976.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1976.jpg'])
 
 @ru
   <p>Дорога.</p>
 @endru
-IMG_1977.jpeg
-IMG_1979.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_1977.jpg',
+  'IMG_1979.jpg',
+]])
 
 @ru
   <p>Жинвальское водохранилище на закате.</p>
 @en
   <p>Zhinvali reservoir at sunset.</p>
 @endru
-IMG_1980.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1980.jpg'])
 
 @ru
-  <p>Возвращение в Тбилиси.</p>
+  <p>Возвращение в Тбилиси. В таком антураже мы с японцами проверяли звук затвора.</p>
 @endru
-IMG_1982.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_1982.jpg'])
 @endsection
