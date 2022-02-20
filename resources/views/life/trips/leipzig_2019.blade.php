15d008baefb35d02d8fb2e2c46e5c29f1a74f1c5
Sergey Pankov <vacuum@ivacuum.ru>
2022-02-21 01:22:54 +0300

    Лейпциг в 2019

--- resources/views/life/trips/leipzig_2019.blade.php
+++ resources/views/life/trips/leipzig_2019.blade.php
@@ -4,435 +4,453 @@
 @ru
   <p>Туманное отправление с центрального вокзала в Берлине.</p>
 @endru
-IMG_2382.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2382.jpg'])
 
 @ru
   <p>В разгаре реновация платформ.</p>
 @endru
-IMG_2383.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2383.jpg'])
 
 @ru
   <p>В Лейпциге автовокзал соседствует с железнодорожным и располагается под парковкой.</p>
 @endru
-IMG_2507.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2507.jpg'])
 
 @ru
   <p>Автомобиль пожарных выглядит как у нас скорая.</p>
 @endru
-IMG_2385.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2385.jpg'])
 
 @ru
   <p>Вход в лифт. Табло отправлений поездов.</p>
 @endru
-IMG_2386.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2386.jpg'])
 
 @ru
   <p>Восточный вход на центральный вокзал. Трамвай.</p>
 @endru
-IMG_2389.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2389.jpg'])
 
 @ru
   <p>Автобусно-трамвайная остановка.</p>
 @endru
-IMG_2390.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2390.jpg'])
 
 @ru
   <p>Раскрашенный электрический щит.</p>
 @endru
-IMG_2392.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2392.jpg'])
 
 @ru
-  <p>Переходный переход. Зебра вдоль, а не поперек.</p>
+  <p>Пешеходный переход. Зебра вдоль, а не поперек.</p>
 @endru
-IMG_2393.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2393.jpg'])
 
 @ru
   <p>Трамваи разных поколений.</p>
 @endru
-IMG_2394.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2394.jpg'])
 
 @ru
   <p>Скамейка.</p>
 @endru
-IMG_2395.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2395.jpg'])
 
 @ru
   <p>Небольшой парк.</p>
 @endru
-IMG_2396.jpeg
-IMG_2398.jpeg
-IMG_2399.jpeg
-IMG_2400.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2396.jpg',
+  'IMG_2398.jpg',
+  'IMG_2399.jpg',
+  'IMG_2400.jpg',
+]])
 
 @ru
   <p>Канализационные люки.</p>
 @endru
-IMG_2401.jpeg
-IMG_2492.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2401.jpg',
+  'IMG_2492.jpg',
+]])
 
 @ru
   <p>Опера.</p>
 @endru
-IMG_2402.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2402.jpg'])
 
 @ru
   <p>Площадь Августа.</p>
 @endru
-IMG_2403.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2403.jpg'])
 
 @ru
   <p>Скамейка.</p>
 @endru
-IMG_2404.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2404.jpg'])
 
 @ru
   <p>На знаке написано Doppelhaltestelle, что дословно значит двойная остановка. И это не про несколько маршрутов. Это подсказка для жителей, что благодаря длине платформы здесь может остановиться сразу два состава. Будьте готовы бодро прошагать к нужному.</p>
 @endru
-IMG_2405.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2405.jpg'])
 
 @ru
   <p>Почтовый ящик.</p>
 @endru
-IMG_2406.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2406.jpg'])
 
 @ru
   <p>Паулинеркирхе.</p>
 @endru
-IMG_2408.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2408.jpg'])
 
 @ru
   <p>Знаки зодиака.</p>
 @endru
-IMG_2409.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2409.jpg'])
 
 @ru
   <p>Табло со временем отправления ближайших поездов со станции. Ниже наклейка с текстом «нацисты, прочь ... из голов».</p>
 @endru
-IMG_2411.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2411.jpg'])
 
 @ru
   <p>Открыта дверь в филиал немецкого банка.</p>
 @endru
-IMG_2412.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2412.jpg'])
 
 @ru
   <p>Защита стволов деревьев и земли.</p>
 @endru
-IMG_2413.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2413.jpg'])
 
 @ru
   <p>Здесь ствол уже окреп, а землю все равно надо защищать.</p>
 @endru
-IMG_2482.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2482.jpg'])
 
 @ru
   <p>Билеты на общественный транспорт можно оплатить картой, в том числе касанием.</p>
 @endru
-IMG_2414.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2414.jpg'])
 
 @ru
   <p>Душевые принадлежности в магазине.</p>
 @endru
-IMG_2415.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2415.jpg'])
 
 @ru
   <p>Фасад здания.</p>
 @endru
-IMG_2417.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2417.jpg'])
 
 @ru
   <p>Декоративные и функциональные бочки.</p>
 @endru
-IMG_2418.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2418.jpg'])
 
 @ru
   <p>Церковь Святого Фомы. Статуя Баха теряется на ее фоне.</p>
 @endru
-IMG_2419.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2419.jpg'])
 
 @ru
   <p>Фасад здания.</p>
 @endru
-IMG_2420.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2420.jpg'])
 
 @ru
   <p>Канал. Над ним мост соединяет парковку и дом.</p>
 @endru
-IMG_2421.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2421.jpg'])
 
 @ru
   <p>Жилой дом. Ни единого кондиционера на фасаде.</p>
 @endru
-IMG_2422.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2422.jpg'])
 
 @ru
   <p>Над серединой дороги светофор из четырех секций.</p>
 @endru
-IMG_2424.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2424.jpg'])
 
 @ru
   <p>Дворик.</p>
 @endru
-IMG_2425.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2425.jpg'])
 
 @ru
   <p>Светофор просит ожидать.</p>
 @endru
-IMG_2427.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2427.jpg'])
 
 @ru
   <p>Парк Иоганны.</p>
 @endru
-IMG_2428.jpeg
-IMG_2430.jpeg
-IMG_2431.jpeg
-IMG_2432.jpeg
-IMG_2433.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2428.jpg',
+  'IMG_2430.jpg',
+  'IMG_2431.jpg',
+  'IMG_2432.jpg',
+  'IMG_2433.jpg',
+]])
 
 @ru
   <p>Переход в парк имени Клары Цеткин.</p>
 @endru
-IMG_2434.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2434.jpg'])
 
 @ru
   <p>Дорога.</p>
 @endru
-IMG_2435.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2435.jpg'])
 
 @ru
   <p>Парк имены Клары Цеткин.</p>
 @endru
-IMG_2437.jpeg
-IMG_2438.jpeg
-IMG_2439.jpeg
-IMG_2440.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2437.jpg',
+  'IMG_2438.jpg',
+  'IMG_2439.jpg',
+  'IMG_2440.jpg',
+]])
 
 @ru
   <p>Трамвай.</p>
 @endru
-IMG_2441.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2441.jpg'])
 
 @ru
   <p>Не перестанут умиротворять железнодорожные пути, окруженные деревьями.</p>
 @endru
-IMG_2442.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2442.jpg'])
 
 @ru
   <p>Ремонт дороги и тротуаров.</p>
 @endru
-IMG_2444.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2444.jpg'])
 
 @ru
   <p>Инструмент лежит на деревянном поддоне.</p>
 @endru
-IMG_2445.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2445.jpg'])
 
 @ru
   <p>Временный обходной мост.</p>
 @endru
-IMG_2446.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2446.jpg'])
 
 @ru
   <p>Даже освещение присутствует. Определенно приятнее, чем ходить по прогибающимся деревяшкам.</p>
 @endru
-IMG_2447.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2447.jpg'])
 
 @ru
   <p>Показалось, что трамвай едет в Москву — на немецком она зовется Moskau.</p>
 @endru
-IMG_2448.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2448.jpg'])
 
 @ru
   <p>Следующий парк.</p>
 @endru
-IMG_2449.jpeg
-IMG_2450.jpeg
-IMG_2451.jpeg
-IMG_2452.jpeg
-IMG_2454.jpeg
-IMG_2456.jpeg
-IMG_2457.jpeg
-IMG_2458.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2449.jpg',
+  'IMG_2450.jpg',
+  'IMG_2451.jpg',
+  'IMG_2452.jpg',
+  'IMG_2454.jpg',
+  'IMG_2456.jpg',
+  'IMG_2457.jpg',
+  'IMG_2458.jpg',
+]])
 
 @ru
   <p>Письменные надругательства над корой дерева.</p>
 @endru
-IMG_2455.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2455.jpg'])
 
 @ru
   <p>Пешеходный переход на перекрестке.</p>
 @endru
-IMG_2459.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2459.jpg'])
 
 @ru
   <p>У кого-то взгляд зацепится за железнодорожный путь, у кого-то за автомобиль.</p>
 @endru
-IMG_2460.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2460.jpg'])
 
 @ru
-  <p>У города однобуквенный домен l.de</p>
+  <p>У города однобуквенный домен <a class="link" href="https://www.l.de/">l.de</a></p>
 @endru
-IMG_2462.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2462.jpg'])
 
 @ru
   <p>Длинный путь вперед.</p>
 @endru
-IMG_2463.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2463.jpg'])
 
 @ru
   <p>Автобусная остановка.</p>
 @endru
-IMG_2464.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2464.jpg'])
 
 @ru
-  <p>Нравится, что на дорожных знаках пишут названия улиц — не надо осматривать края домов. Тем более на углу дома в России может быть лишь номер дома.</p>
+  <p>Нравится, что на дорожных знаках пишут названия улиц — не надо осматривать края домов. Тем более на углу дома в России может быть лишь номер дома — без указания улицы.</p>
 @endru
-IMG_2465.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2465.jpg'])
 
 @ru
   <p>Современный трамвай.</p>
 @endru
-IMG_2466.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2466.jpg'])
 
 @ru
   <p>Редкий случай раскрашенного автомобиля.</p>
 @endru
-IMG_2467.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2467.jpg'])
 
 @ru
   <p>Почтовый ящик.</p>
 @endru
-IMG_2469.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2469.jpg'])
 
 @ru
   <p>Остановка забаррикадировалась.</p>
 @endru
-IMG_2470.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2470.jpg'])
 
 @ru
   <p>Совсем новенькая.</p>
 @endru
-IMG_2471.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2471.jpg'])
 
 @ru
   <p>Дрюкен — нажми.</p>
 @endru
-IMG_2472.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2472.jpg'])
 
 @ru
   <p>Продажа табака в автоматах.</p>
 @endru
-IMG_2473.jpeg
-IMG_2478.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2473.jpg',
+  'IMG_2478.jpg',
+]])
 
 @ru
   <p>Под табличкой с названием улицы иногда рассказывается в честь кого или чего она названа. В данном случае в честь определенного человека.</p>
 @endru
-IMG_2474.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2474.jpg'])
 
 @ru
   <p>Улица сороки названа в честь одноименной реки, один из рукавов которой пересекает эту дорогу.</p>
 @endru
-IMG_2483.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2483.jpg'])
 
 @ru
   <p>Красный — стой, зеленый — иди.</p>
 @endru
-IMG_2475.jpeg
-IMG_2489.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2475.jpg',
+  'IMG_2489.jpg',
+]])
 
 @ru
-  <p>Дорожный знак напомнил японский слог «со»: <span lang="ja">ソ</span>, только на знаке тогда он грустный.</p>
+  <p>Дорожный знак напомнил японский слог «со»: <span lang="ja">ソ</span>, только на знаке он грустный.</p>
 @endru
-IMG_2476.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2476.jpg'])
 
 @ru
-  <p>Расшифровка наклейки на столбе слева: <span class="line-through decoration-red-600">Мог бы</span>. <span class="line-through decoration-red-600">Надо было</span>. <span class="line-through decoration-red-600">Хотел</span>. Сделай!</p>
+  <p>Расшифровка мотивационной наклейки на столбе слева: <span class="line-through decoration-red-600">Мог бы</span>. <span class="line-through decoration-red-600">Надо было</span>. <span class="line-through decoration-red-600">Хотел</span>. Сделай!</p>
 @endru
-IMG_2479.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2479.jpg'])
 
 @ru
   <p>Велодорожка на плитке.</p>
 @endru
-IMG_2484.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2484.jpg'])
 
 @ru
   <p>Самая непротяженная размеченная велодорожка.</p>
 @endru
-IMG_2485.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2485.jpg'])
 
 @ru
   <p>Контейнер для органического мусора.</p>
 @endru
-IMG_2486.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2486.jpg'])
 
 @ru
   <p>Чистая вода в луже.</p>
 @endru
-IMG_2487.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2487.jpg'])
 
 @ru
   <p>Контейнеры для стекла разного цвета: белого, зеленого и коричневого. И оранжевые контейнеры для текстильной обуви.</p>
 @endru
-IMG_2488.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2488.jpg'])
 
 @ru
   <p>Дорога.</p>
 @endru
-IMG_2491.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2491.jpg'])
 
 @ru
   <p>Навигация по примечательным местам.</p>
 @endru
-IMG_2494.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2494.jpg'])
 
 @ru
   <p>Места на улице защищены от дождя зонтами и от холода обогревателями.</p>
 @endru
-IMG_2496.jpeg
-IMG_2498.jpeg
-IMG_2499.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2496.jpg',
+  'IMG_2498.jpg',
+  'IMG_2499.jpg',
+]])
 
 @ru
   <p>Столбики пускают только избранные автомобили.</p>
 @endru
-IMG_2497.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2497.jpg'])
 
 @ru
   <p>Навигация на площади.</p>
 @endru
-IMG_2500.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2500.jpg'])
 
 @ru
   <p>Спуск к электричкам.</p>
 @endru
-IMG_2501.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2501.jpg'])
 
 @ru
   <p>Средства для защиты от загара.</p>
 @endru
-IMG_2503.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2503.jpg'])
 
 @ru
-  <p>За кассой никого — об этом и гласит табличка.</p>
+  <p>За кассой никого — об этом и гласит табличка. Чудеса, но на соседней кассе можно оплатить покупку касанием телефона, в отличие от Берлина.</p>
 @endru
-IMG_2505.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2505.jpg'])
 
 @ru
   <p>Жвачка и батарейки.</p>
 @endru
-IMG_2506.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_2506.jpg'])
 
 @ru
   <p>Улицы.</p>
 @endru
-IMG_2384.jpeg
-IMG_2391.jpeg
-IMG_2407.jpeg
-IMG_2423.jpeg
-IMG_2461.jpeg
-IMG_2468.jpeg
-IMG_2481.jpeg
-IMG_2490.jpeg
-IMG_2493.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_2384.jpg',
+  'IMG_2391.jpg',
+  'IMG_2407.jpg',
+  'IMG_2423.jpg',
+  'IMG_2461.jpg',
+  'IMG_2468.jpg',
+  'IMG_2481.jpg',
+  'IMG_2490.jpg',
+  'IMG_2493.jpg',
+]])
 @endsection
