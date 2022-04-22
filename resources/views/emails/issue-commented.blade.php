0987c369da89234bbeb6dff037da275dbbceacc5
Sergey Pankov <vacuum@ivacuum.ru>
2022-04-22 16:58:22 +0300

    Уведомление на английском

--- resources/views/emails/issue-commented.blade.php
+++ resources/views/emails/issue-commented.blade.php
@@ -7,15 +7,31 @@
 
 @component('mail::message')
 
+@ru
 Здравствуйте!
+@en
+Hello
+@endru
 
+@ru
 {{ ViewHelper::dateShort($issue->created_at) }} вы оставили нам сообщение на тему **{{ $issue->title }}** со страницы {{ url($issue->page) }}.
+@en
+You left us a message titled **{{ $issue->title }}** on {{ $issue->created_at->isoFormat('LL') }} from {{ url($issue->page) }}.
+@endru
 
+@ru
 ## Наш комментарий
+@en
+## Our response
+@endru
 
 {{ $comment->html }}
 
+@ru
 ## Текст вашего обращения
+@en
+## Your message
+@endru
 
 {{ $issue->text }}
 
