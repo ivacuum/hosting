e52a2cacf362916028c629c43f85f4c0104047e7
Sergey Pankov <vacuum@ivacuum.ru>
2022-05-15 13:39:22 +0300

    Оформление кода

--- app/Http/Requests/LifeCalendarForm.php
+++ app/Http/Requests/LifeCalendarForm.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Requests;
 
+use App\Scope\TripNotToKalugaScope;
 use App\Scope\TripOfAdminScope;
 use App\Scope\TripVisibleScope;
 use App\Trip;
@@ -29,7 +30,7 @@ class LifeCalendarForm extends AbstractForm
     {
         return Trip::query()
             ->tap(new TripOfAdminScope)
-            ->where('city_id', '<>', 1)
+            ->tap(new TripNotToKalugaScope)
             ->when($this->includeOnlyVisibleTrips(), fn (Builder $query) => $query->tap(new TripVisibleScope))
             ->orderBy('date_start')
             ->get();
---
+++ app/Scope/TripNotToKalugaScope.php
@@ -0,0 +1,11 @@
+<?php namespace App\Scope;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class TripNotToKalugaScope
+{
+    public function __invoke(Builder $query)
+    {
+        $query->where('city_id', '<>', 1);
+    }
+}
