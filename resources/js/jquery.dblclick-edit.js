9a73aa623837e8187b6b495a2c997efdaa507f26
Sergey Pankov <vacuum@ivacuum.ru>
2022-05-23 10:36:12 +0300

    Постепенный отказ от jquery

--- resources/js/jquery.dblclick-edit.js
+++ resources/js/jquery.dblclick-edit.js
@@ -1,8 +1,8 @@
 // Редактирование по двойному клику
-$(document).on(
-  'dblclick',
-  '.js-dblclick-edit',
-  (e) => {
-    document.location = e.currentTarget.dataset.dblclickUrl
-  },
-)
+document.addEventListener('dblclick', (e) => {
+  const target = e.target.closest('.js-dblclick-edit')
+
+  if (target) {
+    document.location = target.dataset.dblclickUrl
+  }
+})
