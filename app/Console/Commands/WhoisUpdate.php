493e5fa9cc969ea6b5d0d5af1706a942791f22a2
Sergey Pankov <vacuum@ivacuum.ru>
2022-06-07 11:35:48 +0300

    Оформление кода

--- app/Console/Commands/WhoisUpdate.php
+++ app/Console/Commands/WhoisUpdate.php
@@ -1,6 +1,8 @@
 <?php namespace App\Console\Commands;
 
+use App\Action\GetWhoisDataAction;
 use App\Domain;
+use App\Events\DomainWhoisUpdated;
 use App\Scope\DomainWhoisReadyScope;
 use Ivacuum\Generic\Commands\Command;
 
@@ -9,15 +11,17 @@ class WhoisUpdate extends Command
     protected $signature = 'app:whois-update';
     protected $description = 'Update domains information';
 
-    public function handle()
+    public function handle(GetWhoisDataAction $getWhoisData)
     {
         foreach (Domain::tap(new DomainWhoisReadyScope)->orderBy('paid_till')->get() as $domain) {
-            $result = $domain->updateWhois();
-
-            if (false === $result) {
-                $this->error("{$domain->domain} failed");
+            if (empty($data = $getWhoisData->execute($domain->domain))) {
+                $this->error("Failed to update whois data for {$domain->domain}");
                 continue;
             }
+
+            event(new DomainWhoisUpdated($domain, $data));
+
+            $domain->update($data);
 
             $this->info($domain->domain);
         }
