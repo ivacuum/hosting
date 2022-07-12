d75a0e634caef4419fd6077b36009e7a31f7e238
Sergey Pankov <vacuum@ivacuum.ru>
2022-07-12 21:16:23 +0300

    Упоминание тренажера хангыля в списке тренажеров

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -1,7 +1,8 @@
-@extends('base')
+@extends('life.base')
 
 @section('content')
 <p><a href="{{ to('japanese/hiragana-katakana') }}">Тренажер хираганы и катаканы</a></p>
 <p><a href="{{ to('japanese/words-trainer') }}">Тренажер по набору слов хираганой и катаканой</a></p>
+<p><a href="{{ to('korean/hangul') }}">Тренажер хангыля</a></p>
 <p><a href="{{ to('trainers/numbers') }}">Тренажер чисел</a></p>
 @endsection
