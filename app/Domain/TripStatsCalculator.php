6d8aa55e40789eaad63a9c21a5281a6e0a473116
Sergey Pankov <vacuum@ivacuum.ru>
2022-07-14 13:18:47 +0300

    Попытка отловить ошибку в среде github actions

--- app/Domain/TripStatsCalculator.php
+++ app/Domain/TripStatsCalculator.php
@@ -91,6 +91,11 @@ class TripStatsCalculator
 
     public function lastDate(): CarbonImmutable
     {
+        if (app()->runningUnitTests() && $this->lastDate === null) {
+            // Попытка отловить ошибку в среде github actions
+            dd($this);
+        }
+
         return $this->lastDate;
     }
 
