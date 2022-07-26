71a80cde7300030ad6175a004c910d68470856d2
Sergey Pankov <vacuum@ivacuum.ru>
2022-07-26 19:45:32 +0300

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2021_04.blade.php
@@ -0,0 +1,53 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Новая Третьяковская галерея.</p>
+@endru
+IMG_7527.jpeg
+
+@ru
+  <p>Отверстия для глаз для просмотра экспозиции.</p>
+@endru
+IMG_7528.jpeg
+
+@ru
+  <p>Свободное падение брусков со столкновениями с кольями.</p>
+@endru
+IMG_7529.jpeg
+
+@ru
+  <p>Афиши семидесятых.</p>
+@endru
+IMG_7531.jpeg
+
+@ru
+  <p>Проект по борьбе с пластиковыми отходами.</p>
+@endru
+IMG_7533.jpeg
+
+@ru
+  <p>USB-зарядки в вагоне метро.</p>
+@endru
+IMG_7534.jpeg
+
+@ru
+  <p>Улица Бутырский Вал.</p>
+@endru
+IMG_7535.jpeg
+
+@ru
+  <p>Флакон. Рынок.</p>
+@endru
+IMG_7536.jpeg
+
+@ru
+  <p>Подсказка местоположения под схемой.</p>
+@endru
+IMG_7537.jpeg
+
+@ru
+  <p>Территория флакона.</p>
+@endru
+IMG_7538.jpeg
+@endsection
