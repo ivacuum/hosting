a7494e78914a4b3913b111d6a00d79330cf183d4
Sergey Pankov <vacuum@ivacuum.ru>
2022-07-27 15:29:13 +0300

    Заготовка о поездке в Сестрорецк

---
+++ resources/views/life/trips/sestroretsk_2021.blade.php
@@ -0,0 +1,128 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Отправление в Сестрорецк со станции Старая деревня в Питере.</p>
+@endru
+IMG_7937.jpeg
+
+@ru
+  <p>Схема движения поездов на финляндском направлении.</p>
+@endru
+IMG_7938.jpeg
+
+@ru
+  <p>Примерно каждый час можно добраться до Сестрорецка и обратно.</p>
+@endru
+IMG_7939.jpeg
+
+@ru
+  <p>Один путь на платформе обслуживает поезда в обе стороны.</p>
+@endru
+IMG_7940.jpeg
+
+@ru
+  <p>Вид из окна поезда во время пути.</p>
+@endru
+IMG_7941.jpeg
+
+@ru
+  <p>Поезд прибыл.</p>
+@endru
+IMG_7943.jpeg
+
+@ru
+  <p>Железнодорожный переезд.</p>
+@endru
+IMG_7944.jpeg
+
+@ru
+  <p>Инсталляции около платформы.</p>
+@endru
+IMG_7945.jpeg
+
+@ru
+  <p>Много зелени.</p>
+@endru
+IMG_7946.jpeg
+IMG_7950.jpeg
+
+@ru
+  <p>Канал.</p>
+@endru
+IMG_7947.jpeg
+IMG_7948.jpeg
+
+@ru
+  <p>Улица.</p>
+@endru
+IMG_7949.jpeg
+
+@ru
+  <p>Конюшня с форточками.</p>
+@endru
+IMG_7951.jpeg
+
+@ru
+  <p>Тропы местами заминированы.</p>
+@endru
+IMG_7952.jpeg
+
+@ru
+  <p>Лодки в канале.</p>
+@endru
+IMG_7953.jpeg
+
+@ru
+  <p>Продолжение пути вдоль канала к Финскому заливу.</p>
+@endru
+IMG_7954.jpeg
+
+@ru
+  <p>Скамейки.</p>
+@endru
+IMG_7955.jpeg
+IMG_7960.jpeg
+IMG_7961.jpeg
+IMG_7965.jpeg
+
+@ru
+  <p>Парк Дубки.</p>
+@endru
+IMG_7956.jpeg
+IMG_7957.jpeg
+IMG_7959.jpeg
+IMG_7967.jpeg
+
+@ru
+  <p>Указатели в парке.</p>
+@endru
+IMG_7958.jpeg
+
+@ru
+  <p>Финский залив.</p>
+@endru
+IMG_7962.jpeg
+IMG_7963.jpeg
+IMG_7964.jpeg
+
+@ru
+  <p>Аллея в парке.</p>
+@endru
+IMG_7966.jpeg
+
+@ru
+  <p>У входа в подъезд расписаны номера квартир на каждом этаже.</p>
+@endru
+IMG_7968.jpeg
+
+@ru
+  <p>Белорусские продукты в магазине.</p>
+@endru
+IMG_7969.jpeg
+
+@ru
+  <p>На платформе в ожидании обратного поезда в Санкт-Петербург.</p>
+@endru
+IMG_7970.jpeg
+@endsection
