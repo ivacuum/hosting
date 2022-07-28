c1a359241ed8a4f49403306ba048b819d2337723
Sergey Pankov <vacuum@ivacuum.ru>
2022-07-28 14:37:04 +0300

    Заготовка о поездке в Великий Новгород

---
+++ resources/views/life/trips/veliky-novgorod_2021.blade.php
@@ -0,0 +1,192 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Поезд Ласточка из Санкт-Петербурга прибыл на центральный вокзал Великого Новгорода.</p>
+@endru
+IMG_7801.jpeg
+
+@ru
+  <p>Карта местности на торце дома.</p>
+@endru
+IMG_7802.jpeg
+
+@ru
+  <p>Схема поближе.</p>
+@endru
+IMG_7803.jpeg
+
+@ru
+  <p>Аллея.</p>
+@endru
+IMG_7804.jpeg
+
+@ru
+  <p>Урна.</p>
+@endru
+IMG_7805.jpeg
+IMG_7806.jpeg
+
+@ru
+  <p>Здание правительства.</p>
+@endru
+IMG_7807.jpeg
+
+@ru
+  <p>Стены кремля.</p>
+@endru
+IMG_7808.jpeg
+IMG_7884.jpeg
+IMG_7885.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_7810.jpeg
+
+@ru
+  <p>Часы на башне показывают правильное время.</p>
+@endru
+IMG_7811.jpeg
+
+@ru
+  <p>Дверь с потрясающим количеством деталей.</p>
+@endru
+IMG_7813.jpeg
+IMG_7814.jpeg
+
+@ru
+  <p>Колокол.</p>
+@endru
+IMG_7815.jpeg
+
+@ru
+  <p>Пляж.</p>
+@endru
+IMG_7820.jpeg
+
+@ru
+  <p>Мост.</p>
+@endru
+IMG_7822.jpeg
+
+@ru
+  <p>Аллея.</p>
+@endru
+IMG_7823.jpeg
+
+@ru
+  <p>Расстояние до российских городов на указателях.</p>
+@endru
+IMG_7824.jpeg
+
+@ru
+  <p>Реклама швейной фабрики на улице.</p>
+@endru
+IMG_7825.jpeg
+
+@ru
+  <p>В парке.</p>
+@endru
+IMG_7826.jpeg
+
+@ru
+  <p>Церковь.</p>
+@endru
+IMG_7827.jpeg
+
+@ru
+  <p>Урна на остановке.</p>
+@endru
+IMG_7828.jpeg
+
+@ru
+  <p>Реновация улицы.</p>
+@endru
+IMG_7829.jpeg
+IMG_7830.jpeg
+IMG_7835.jpeg
+
+@ru
+  <p>Сад в жилом дворе.</p>
+@endru
+IMG_7831.jpeg
+
+@ru
+  <p>Ворота на цепи.</p>
+@endru
+IMG_7846.jpeg
+
+@ru
+  <p>Двор за воротами.</p>
+@endru
+IMG_7847.jpeg
+
+@ru
+  <p>Церковь.</p>
+@endru
+IMG_7851.jpeg
+
+@ru
+  <p>Внутри церкви.</p>
+@endru
+IMG_7852.jpeg
+IMG_7853.jpeg
+IMG_7854.jpeg
+
+@ru
+  <p>Купание запрещено, но для людей это не помеха.</p>
+@endru
+IMG_7856.jpeg
+
+@ru
+  <p>Перынский скит.</p>
+@endru
+IMG_7857.jpeg
+IMG_7858.jpeg
+IMG_7859.jpeg
+IMG_7860.jpeg
+IMG_7861.jpeg
+IMG_7862.jpeg
+
+@ru
+  <p>Обед на терассе в центре Великого Новгорода.</p>
+@endru
+IMG_7868.jpeg
+
+@ru
+  <p>Счет за обед в конверте.</p>
+@endru
+IMG_7871.jpeg
+
+@ru
+  <p>Береза в цветах.</p>
+@endru
+IMG_7883.jpeg
+
+@ru
+  <p>Одинокий шоппер.</p>
+@endru
+IMG_7887.jpeg
+
+@ru
+  <p>Подъем к обелиску.</p>
+@endru
+IMG_7888.jpeg
+
+@ru
+  <p>Спортивные площадки.</p>
+@endru
+IMG_7891.jpeg
+
+@ru
+  <p>Улицы.</p>
+@endru
+IMG_7892.jpeg
+IMG_7894.jpeg
+
+@ru
+  <p>Вертикальная надпись: полиция. Объяснение именования улицы Людогоща.</p>
+@endru
+IMG_7893.jpeg
+@endsection
