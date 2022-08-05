de7d472d51bff5e57f37092754eaaf85b8ae9807
Sergey Pankov <vacuum@ivacuum.ru>
2022-08-05 20:58:57 +0300

    Заготовка о поездке в Хаконэ

---
+++ resources/views/life/trips/hakone_2019.blade.php
@@ -0,0 +1,372 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Пересадка в Одаваре.</p>
+@endru
+IMG_3428.jpeg
+
+@ru
+  <p>Печать станции для коллекционеров.</p>
+@endru
+IMG_3429.jpeg
+
+@ru
+  <p>Поиск нужной остановки.</p>
+@endru
+IMG_3430.jpeg
+
+@ru
+  <p>Обилие рекламы на фасаде дома.</p>
+@endru
+IMG_3432.jpeg
+
+@ru
+  <p>Автобус до Хаконе. Чемодан едет на пассажирском месте.</p>
+@endru
+IMG_3434.jpeg
+
+@ru
+  <p>За час в пути стемнело.</p>
+@endru
+IMG_3436.jpeg
+
+@ru
+  <p>На месте туман в потемках.</p>
+@endru
+IMG_3440.jpeg
+IMG_3441.jpeg
+IMG_3452.jpeg
+IMG_3453.jpeg
+
+@ru
+  <p>Туманная заправка.</p>
+@endru
+IMG_3455.jpeg
+
+@ru
+  <p>Скромняга.</p>
+@endru
+IMG_3526.jpeg
+IMG_3527.jpeg
+
+@ru
+  <p>Проход к жилью. Внутри есть свои горячие источники.</p>
+@endru
+IMG_3530.jpeg
+
+@ru
+  <p>Парковка у входа.</p>
+@endru
+IMG_3528.jpeg
+
+@ru
+  <p>Спуск к озеру.</p>
+@endru
+IMG_3536.jpeg
+
+@ru
+  <p>Улица.</p>
+@endru
+IMG_3537.jpeg
+
+@ru
+  <p>Остановка водного транспорта.</p>
+@endru
+IMG_3538.jpeg
+
+@ru
+  <p>Неожиданное использование знака аварийной остановки.</p>
+@endru
+IMG_3539.jpeg
+
+@ru
+  <p>Осенние краски. Изобилие красного.</p>
+@endru
+IMG_3540.jpeg
+
+@ru
+  <p>Обзорная площадка.</p>
+@endru
+IMG_3541.jpeg
+
+@ru
+  <p>Подъем по канатной дороге.</p>
+@endru
+IMG_3544.jpeg
+
+@ru
+  <p>Облака над Фудзи.</p>
+@endru
+IMG_3550.jpeg
+
+@ru
+  <p>Бумажные полотенца на случай, если поплохело в пути.</p>
+@endru
+IMG_3551.jpeg
+
+Но не только полотенца.
+IMG_3668.jpeg
+
+@ru
+  <p>Проезд над дорогами.</p>
+@endru
+IMG_3554.jpeg
+IMG_3552.jpeg
+
+@ru
+  <p>Окрестности с канатной дороги.</p>
+@endru
+IMG_3553.jpeg
+IMG_3559.jpeg
+IMG_3569.jpeg
+
+@ru
+  <p>Выход на промежуточной остановке, чтобы посмотреть на гору Фудзи.</p>
+@endru
+IMG_3572.jpeg
+
+@ru
+  <p>Разные ракурсы горы Фудзи.</p>
+@endru
+IMG_3652.jpeg
+IMG_3590.jpeg
+IMG_3588.jpeg
+IMG_3654.jpeg
+
+@ru
+  <p>Сероводородный участок.</p>
+@endru
+IMG_3570.jpeg
+IMG_3589.jpeg
+
+@ru
+  <p>Бинокль за 100 иен.</p>
+@endru
+IMG_3571.jpeg
+
+@ru
+  <p>Автобусная остановка.</p>
+@endru
+IMG_3610.jpeg
+
+@ru
+  <p>Безлиственный уголок.</p>
+@endru
+IMG_3653.jpeg
+
+@ru
+  <p>Сувениры на стену.</p>
+@endru
+IMG_3655.jpeg
+
+@ru
+  <p>Рабочее место продавца-кассира в магазине сувениров.</p>
+@endru
+IMG_3660.jpeg
+
+@ru
+  <p>Массажный ролик.</p>
+@endru
+IMG_3661.jpeg
+
+@ru
+  <p>Способы применения массажного ролика.</p>
+@endru
+IMG_3662.jpeg
+
+@ru
+  <p>Пеньки-вулканы.</p>
+@endru
+IMG_3663.jpeg
+
+@ru
+  <p>Братюня скромняги.</p>
+@endru
+IMG_3664.jpeg
+
+@ru
+  <p>Продолжение пути по канатной дороге.</p>
+@endru
+IMG_3665.jpeg
+IMG_3667.jpeg
+
+@ru
+  <p>Осенние улицы.</p>
+@endru
+IMG_3669.jpeg
+IMG_3673.jpeg
+IMG_3680.jpeg
+IMG_3683.jpeg
+IMG_3685.jpeg
+
+@ru
+  <p>Снос дома специальной техникой.</p>
+@endru
+IMG_3686.jpeg
+
+@ru
+  <p>Долгий и крутой путь вниз по направлению к музею на открытом воздухе.</p>
+@endru
+IMG_3687.jpeg
+IMG_3689.jpeg
+IMG_3690.jpeg
+IMG_3691.jpeg
+IMG_3692.jpeg
+IMG_3694.jpeg
+IMG_3695.jpeg
+IMG_3696.jpeg
+IMG_3699.jpeg
+IMG_3707.jpeg
+
+@ru
+  <p>Ворота на колесах.</p>
+@endru
+IMG_3697.jpeg
+
+@ru
+  <p>Вход в платный сад.</p>
+@endru
+IMG_3698.jpeg
+
+@ru
+  <p>Перчатки сохнут на прищепках.</p>
+@endru
+IMG_3700.jpeg
+
+@ru
+  <p>Автомобильная дорога.</p>
+@endru
+IMG_3701.jpeg
+
+@ru
+  <p>Железная дорога.</p>
+@endru
+IMG_3706.jpeg
+
+@ru
+  <p>Гладкий каменистый холм.</p>
+@endru
+IMG_3708.jpeg
+
+@ru
+  <p>На железнодорожном мосту пронумерованы шпалы.</p>
+@endru
+IMG_3709.jpeg
+
+@ru
+  <p>Автомобиль проезжает железнодорожный переезд.</p>
+@endru
+IMG_3710.jpeg
+
+@ru
+  <p>Территория музея Хаконе на открытом воздухе.</p>
+@endru
+IMG_3712.jpeg
+IMG_3713.jpeg
+IMG_3714.jpeg
+IMG_3715.jpeg
+IMG_3733.jpeg
+IMG_3738.jpeg
+IMG_3739.jpeg
+IMG_3740.jpeg
+IMG_3746.jpeg
+IMG_3747.jpeg
+
+@ru
+  <p>Винтовая лестница и разноцветные витражи.</p>
+@endru
+IMG_3769.jpeg
+IMG_3771.jpeg
+IMG_3772.jpeg
+
+@ru
+  <p>Протоптанный подъем по винтовой лестнице.</p>
+@endru
+IMG_3783.jpeg
+
+@ru
+  <p>Панорама с башни.</p>
+@endru
+IMG_3784.jpeg
+
+@ru
+  <p>Протоптанный спуск по винтовой лестнице.</p>
+@endru
+IMG_3802.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_3807.jpeg
+
+@ru
+  <p>Еще инсталляции.</p>
+@endru
+IMG_3809.jpeg
+IMG_3836.jpeg
+IMG_3853.jpeg
+IMG_3854.jpeg
+IMG_3835.jpeg
+
+@ru
+  <p>Скамейка-яичница.</p>
+@endru
+IMG_3837.jpeg
+
+@ru
+  <p>Почувствуй себя скульптурой с помощью захвата и проекции позы.</p>
+@endru
+IMG_3861.jpeg
+IMG_3862.jpeg
+
+@ru
+  <p>Инструкция к становлению скульптурой.</p>
+@endru
+IMG_3867.jpeg
+
+@ru
+  <p>Сувенирные камни в честь месяцев лунного календаря.</p>
+@endru
+IMG_3873.jpeg
+
+@ru
+  <p>Музыкальные шкатулки.</p>
+@endru
+IMG_3874.jpeg
+
+@ru
+  <p>Улица.</p>
+@endru
+IMG_3875.jpeg
+
+@ru
+  <p>Пронумерованная скамейка. Табличка предлагает отдохнуть.</p>
+@endru
+IMG_3876.jpeg
+
+@ru
+  <p>Цветы в горшке.</p>
+@endru
+IMG_3877.jpeg
+
+@ru
+  <p>Дорога.</p>
+@endru
+IMG_3878.jpeg
+
+@ru
+  <p>Доступ к вай-фаю в автобусе.</p>
+@endru
+IMG_3879.jpeg
+
+@ru
+  <p>Ужин с инструкцией.</p>
+@endru
+IMG_3884.jpeg
+
+@ru
+  <p>В утро выезда снова туман.</p>
+@endru
+IMG_3888.jpeg
+@endsection
