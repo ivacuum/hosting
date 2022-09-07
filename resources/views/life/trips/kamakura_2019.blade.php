ecbe1755d4f0eb8a036219ac9f2bca5f02c0812e
Sergey Pankov <vacuum@ivacuum.ru>
2022-09-07 19:31:49 +0300

    Камакура в 2019

--- resources/views/life/trips/kamakura_2019.blade.php
+++ resources/views/life/trips/kamakura_2019.blade.php
@@ -2,249 +2,263 @@
 
 @section('content')
 @ru
-  <p>Место для колясок и туалет в электричке.</p>
+  <p>В вагоне электрички место для колясок и туалет.</p>
 @endru
-IMG_3895.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3895.jpg'])
 
 @ru
-  <p>Камеры хранения. Если на экране видна сумма, то ее надо заплатить, чтобы камера закрылась и удалось забрать ключ.</p>
+  <p>Камеры хранения. Если на экране видна сумма, то ее надо заплатить, чтобы камера закрылась и удалось забрать ключ. Если ноль, то камера уже занята.</p>
 @endru
-IMG_3896.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3896.jpg'])
 
 @ru
-  <p>Часы показывают правильное время.</p>
+  <p>Часы на перекрестке показывают правильное время.</p>
 @endru
-IMG_3897.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3897.jpg'])
 
 @ru
-  <p>Широченная зебра.</p>
+  <p>Широченная зебра. Люки не закрашивают белой краской.</p>
 @endru
-IMG_3898.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3898.jpg'])
 
 @ru
   <p>Велопарковка.</p>
 @endru
-IMG_3900.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3900.jpg'])
 
 @ru
-  <p>Пеньки вокруг столика под навесом.</p>
+  <p>Пеньки вокруг деревянного столика под навесом.</p>
 @endru
-IMG_3901.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3901.jpg'])
 
 @ru
-  <p>Дизайн домов.</p>
+  <p>Дизайн домов в округе.</p>
 @endru
-IMG_3902.jpeg
-IMG_3903.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3902.jpg',
+  'IMG_3903.jpg',
+]])
 
 @ru
   <p>Оборудование невероятной белизны на улице вровень с забором.</p>
 @endru
-IMG_3904.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3904.jpg'])
 
 @ru
   <p>Лимоны растут в конце ноября.</p>
 @endru
-IMG_3912.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3912.jpg'])
 
 @ru
-  <p>Улицы.</p>
+  <p>Улица.</p>
+@en
+  <p>Street.</p>
 @endru
-IMG_3913.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3913.jpg'])
 
 @ru
-  <p>Таксофон.</p>
+  <p>Таксофон. Принимает некоторые монеты и телефонные карты.</p>
+@en
+  <p>Payphone. Accepts some coins and phone cards.</p>
 @endru
-IMG_3914.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3914.jpg'])
 
 @ru
   <p>Территория возле буддистского храма Хаседера.</p>
 @endru
-IMG_3919.jpeg
-IMG_3920.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3919.jpg',
+  'IMG_3920.jpg',
+]])
 
 @ru
-  <p>Самостоятельная покупка билетов возможна за наличные и касанием транспортной картой.</p>
+  <p>Самостоятельная покупка билетов возможна за наличные и касанием терминала транспортной картой.</p>
 @endru
-IMG_3921.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3921.jpg'])
 
 @ru
   <p>Столбик навигации.</p>
 @endru
-IMG_3922.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3922.jpg'])
 
 @ru
   <p>Пруд с рыбами.</p>
 @endru
-IMG_3924.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3924.jpg'])
 
 @ru
   <p>Сотни фигурок.</p>
 @endru
-IMG_3927.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3927.jpg'])
 
 @ru
   <p>Табличка просит придерживаться левостороннего движения на ступеньках.</p>
 @endru
-IMG_3928.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3928.jpg'])
 
 @ru
   <p>Панорама побережья. Предупреждение о птицах.</p>
 @endru
-IMG_3929.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3929.jpg'])
 
 @ru
   <p>Ручей и осенние краски.</p>
 @endru
-IMG_3935.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3935.jpg'])
 
 @ru
   <p>Поручень и ограждение.</p>
 @endru
-IMG_3945.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3945.jpg'])
 
 @ru
   <p>Панорама побережья повыше.</p>
 @endru
-IMG_3947.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3947.jpg'])
 
 @ru
   <p>Тропы.</p>
 @endru
-IMG_3948.jpeg
-IMG_3949.jpeg
-IMG_3957.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3948.jpg',
+  'IMG_3949.jpg',
+  'IMG_3957.jpg',
+]])
 
 @ru
   <p>Один из храмов.</p>
 @endru
-IMG_3959.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3959.jpg'])
 
 @ru
   <p>Рыбы в пруду.</p>
 @endru
-IMG_3960.jpeg
-IMG_3961.jpeg
-IMG_3977.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3960.jpg',
+  'IMG_3961.jpg',
+  'IMG_3977.jpg',
+]])
 
 @ru
-  <p>Выход из пещеры.</p>
+  <p>Выход из пещеры направо.</p>
 @endru
-IMG_3975.jpeg
-IMG_3973.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_3975.jpg',
+  'IMG_3973.jpg',
+]])
 
 @ru
-  <p>Оформление земли, по которой нельзя ходить посетителям.</p>
+  <p>Узоры на земле, по которой нельзя ходить посетителям.</p>
 @endru
-IMG_3976.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3976.jpg'])
 
 @ru
   <p>Платформы железнодорожной станции.</p>
 @endru
-IMG_3983.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3983.jpg'])
 
 @ru
   <p>Железнодорожный переезд.</p>
 @endru
-IMG_3984.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3984.jpg'])
 
 @ru
   <p>Поезд до центра города прибывает.</p>
 @endru
-IMG_3985.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3985.jpg'])
 
 @ru
   <p>Навигация в вагоне. Сейчас поезд в пути между станциями Хасе и Юигахама.</p>
 @endru
-IMG_3987.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3987.jpg'])
 
 @ru
   <p>Теперь поезд прибыл на станцию Юигахама.</p>
 @endru
-IMG_3988.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3988.jpg'])
 
 @ru
   <p>Двухуровневая велопарковка.</p>
 @endru
-IMG_3989.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3989.jpg'])
 
 @ru
   <p>Пешеходная улица.</p>
 @endru
-IMG_3990.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3990.jpg'])
 
 @ru
   <p>Увлажнение зонта проявляет узор, поэтому для демонстрации дизайна зонт крутится под краном с водой.</p>
 @endru
-IMG_3991.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3991.jpg'])
 
 @ru
   <p>Улица.</p>
 @endru
-IMG_3992.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3992.jpg'])
 
 @ru
-  <p>Между двумя прудами.</p>
+  <p>Территория между двумя прудами.</p>
 @endru
-IMG_3993.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3993.jpg'])
 
 @ru
   <p>Застолье с видом за пруд.</p>
 @endru
-IMG_3994.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3994.jpg'])
 
 @ru
   <p>Уличный фонарь.</p>
 @endru
-IMG_3995.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3995.jpg'])
 
 @ru
   <p>Много записок от людей.</p>
 @endru
-IMG_3996.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3996.jpg'])
 
 @ru
   <p>Еще уличные фонари.</p>
 @endru
-IMG_3997.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3997.jpg'])
 
 @ru
   <p>Десятки красных врат с пожеланиями людей.</p>
 @endru
-IMG_3998.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3998.jpg'])
 
 @ru
   <p>Стемнело.</p>
 @endru
-IMG_3999.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_3999.jpg'])
 
 @ru
   <p>Аллея разделяет проезжую часть.</p>
 @endru
-IMG_4001.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4001.jpg'])
 
 @ru
   <p>Пешеходная улица на обратном пути.</p>
 @endru
-IMG_4002.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4002.jpg'])
 
 @ru
-  <p>В магазине залили пол прям поверх плитки, но кое-где красивая плитка выходит на свет.</p>
+  <p>В магазине залили пол прямо поверх плитки, но кое-где красивая плитка выходит на свет.</p>
 @endru
-IMG_4006.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4006.jpg'])
 
 @ru
   <p>Подушка. Ням.</p>
 @endru
-IMG_4007.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4007.jpg'])
 
 @ru
   <p>Конвейер суши.</p>
 @endru
-IMG_4008.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4008.jpg'])
 
 @ru
   <p>Большой скромняга не продается.</p>
 @endru
-IMG_4009.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4009.jpg'])
 @endsection
