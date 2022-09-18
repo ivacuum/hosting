2a3a82aa1f0843a7138710e8bf2ad66d1a0b4869
Sergey Pankov <vacuum@ivacuum.ru>
2022-09-18 11:44:44 +0300

    Хатиодзи в 2019

--- resources/views/life/trips/hachioji_2019.blade.php
+++ resources/views/life/trips/hachioji_2019.blade.php
@@ -4,302 +4,324 @@
 @ru
   <p>Поезд до горы Такао.</p>
 @endru
-IMG_4066.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4066.jpg'])
 
 @ru
   <p>В поезде информация какие вагоны и в какое время дня самые людные.</p>
 @endru
-IMG_4069.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4069.jpg'])
 
 @ru
   <p>Железнодорожная платформа по прибытии.</p>
 @endru
-IMG_4070.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4070.jpg'])
 
 @ru
   <p>Турникеты на выходе с железнодорожной станции.</p>
 @endru
-IMG_4071.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4071.jpg'])
 
 @ru
   <p>Невероятное количество красной листвы.</p>
 @endru
-IMG_4072.jpeg
-IMG_4073.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4072.jpg',
+  'IMG_4073.jpg',
+]])
 
 @ru
   <p>Улица с зеленой изгородью.</p>
 @endru
-IMG_4074.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4074.jpg'])
 
 @ru
   <p>Очередь в кассы фуникулера.</p>
 @endru
-IMG_4075.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4075.jpg'])
 
 @ru
   <p>Очередь на посадку.</p>
 @endru
-IMG_4076.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4076.jpg'])
 
 @ru
   <p>Вид в заднее окно фуникулера во время подъема в гору.</p>
 @endru
-IMG_4079.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4079.jpg'])
 
 @ru
   <p>Бинокль и скамейки наверху.</p>
 @endru
-IMG_4080.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4080.jpg'])
 
 @ru
   <p>Схема местности. Восемь маршрутов для прогулки с примерным временем на их прохождение вверх и вниз.</p>
 @endru
-IMG_4081.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4081.jpg'])
 
 @ru
   <p>Скамейки с панорамным видом.</p>
 @endru
-IMG_4082.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4082.jpg'])
 
 @ru
   <p>Панорама.</p>
 @endru
-IMG_4083.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4083.jpg'])
 
 @ru
   <p>Навигация.</p>
 @endru
-IMG_4084.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4084.jpg'])
 
 @ru
   <p>Столики для перекуса добавляют смысла носить с собой еду, чтобы подкрепиться в живописных местах.</p>
 @endru
-IMG_4085.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4085.jpg'])
 
 @ru
   <p>Днем фонари вдоль маршрута выключены.</p>
 @endru
-IMG_4086.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4086.jpg'])
 
 @ru
   <p>Стена вертикальных надписей.</p>
 @endru
-IMG_4087.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4087.jpg'])
 
 @ru
   <p>Храм среди осенних красок.</p>
 @endru
-IMG_4088.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4088.jpg'])
 
 @ru
   <p>Двойной забор.</p>
 @endru
-IMG_4089.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4089.jpg'])
 
 @ru
   <p>Какие-то деревья стремительно теряют листву.</p>
 @endru
-IMG_4090.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4090.jpg'])
 
 @ru
   <p>Какие-то словно сохранили всю.</p>
 @endru
-IMG_4091.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4091.jpg'])
 
 @ru
   <p>Терминал по продаже напитков под навесом.</p>
 @endru
-IMG_4092.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4092.jpg'])
 
 @ru
   <p>Желтые краски тоже присутствуют.</p>
 @endru
-IMG_4094.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4094.jpg'])
 
 @ru
   <p>И зеленые краски в наличии.</p>
 @endru
-IMG_4095.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4095.jpg'])
 
 @ru
   <p>Навигация. Тут пересекаются первый и третий маршруты.</p>
 @endru
-IMG_4096.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4096.jpg'])
 
 @ru
   <p>Масштаб уклона.</p>
 @endru
-IMG_4099.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4099.jpg'])
 
 @ru
   <p>Направо — вершина горы Такао.</p>
 @endru
-IMG_4100.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4100.jpg'])
 
 @ru
   <p>Путь к пику.</p>
 @endru
-IMG_4101.jpeg
-IMG_4102.jpeg
-IMG_4105.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4101.jpg',
+  'IMG_4102.jpg',
+  'IMG_4105.jpg',
+]])
 
 @ru
   <p>Вот и вершина.</p>
 @endru
-IMG_4131.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4131.jpg'])
 
 @ru
-  <p>Двойная упаковка чипсов. После двойной упаковки жевательных подушечек уже не удивительно.</p>
+  <p>Двойная упаковка чипсов. После двойной упаковки жевательных подушечек уже ожидаешь чего-то такого от японцев.</p>
 @endru
-IMG_4132.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4132.jpg'])
 
 @ru
   <p>На вершине немало людей.</p>
 @endru
-IMG_4134.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4134.jpg'])
 
 @ru
   <p>Где-то половина присутствующих отдыхает.</p>
 @endru
-IMG_4143.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4143.jpg'])
 
 @ru
   <p>Еще часть — трапезничает.</p>
 @endru
-IMG_4144.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4144.jpg'])
 
 @ru
   <p>Очень недостает в России красной листвы.</p>
 @endru
-IMG_4146.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4146.jpg'])
 
 @ru
   <p>Продолжение прогулки. Пожилое поколение нередко здоровается в пути, когда встречаетесь в противоположных направлениях.</p>
 @endru
-IMG_4148.jpeg
-IMG_4156.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4148.jpg',
+  'IMG_4156.jpg',
+]])
 
 @ru
   <p>Больше осенних красок.</p>
 @endru
-IMG_4157.jpeg
-IMG_4158.jpeg
-IMG_4159.jpeg
-IMG_4160.jpeg
-IMG_4161.jpeg
-IMG_4163.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4157.jpg',
+  'IMG_4158.jpg',
+  'IMG_4159.jpg',
+  'IMG_4160.jpg',
+  'IMG_4161.jpg',
+  'IMG_4163.jpg',
+]])
 
 @ru
   <p>Магазин на горе.</p>
 @endru
-IMG_4162.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4162.jpg'])
 
 @ru
   <p>Скамейки, с которых люди любуются заснеженной горой Фудзи прямо по курсу.</p>
 @endru
-IMG_4164.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4164.jpg'])
 
 @ru
   <p>Люди вокруг.</p>
 @endru
-IMG_4165.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4165.jpg'])
 
 @ru
   <p>Даже с собакой поднялись.</p>
 @endru
-IMG_4166.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4166.jpg'])
 
 @ru
   <p>Нередко встречаются люди в походной одежде.</p>
 @endru
-IMG_4167.jpeg
-IMG_4185.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4167.jpg',
+  'IMG_4185.jpg',
+]])
 
 @ru
   <p>Далее по маршруту по-прежнему видна гора Фудзи.</p>
 @endru
-IMG_4186.jpeg
-IMG_4187.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4186.jpg',
+  'IMG_4187.jpg',
+]])
 
 @ru
   <p>Спуск настораживает тем, что потом надо будет подниматься.</p>
 @endru
-IMG_4188.jpeg
-IMG_4195.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4188.jpg',
+  'IMG_4195.jpg',
+]])
 
 @ru
-  <p>Вот и подъемы подоспели.</p>
+  <p>Вот и затяжные подъемы подоспели.</p>
 @endru
-IMG_4204.jpeg
-IMG_4205.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4204.jpg',
+  'IMG_4205.jpg',
+]])
 
 @ru
   <p>Снова места для застольев.</p>
 @endru
-IMG_4206.jpeg
-IMG_4207.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4206.jpg',
+  'IMG_4207.jpg',
+]])
 
 @ru
   <p>Благодать вокруг.</p>
 @endru
-IMG_4209.jpeg
-IMG_4210.jpeg
-IMG_4211.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4209.jpg',
+  'IMG_4210.jpg',
+  'IMG_4211.jpg',
+]])
 
 @ru
   <p>Полное золотой листвы дерево.</p>
 @endru
-IMG_4212.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4212.jpg'])
 
 @ru
   <p>Пеньки.</p>
 @endru
-IMG_4230.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4230.jpg'])
 
 @ru
   <p>Текстура подъема. С камнями хорошее сцепление.</p>
 @endru
-IMG_4231.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4231.jpg'])
 
 @ru
   <p>Обзорная площадка. Фудзи возвышается прямо по центру.</p>
 @endru
-IMG_4233.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4233.jpg'])
 
 @ru
   <p>Возвращение и спуск.</p>
 @endru
-IMG_4249.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4249.jpg'])
 
 @ru
   <p>Магазинчики.</p>
 @endru
-IMG_4250.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4250.jpg'])
 
 @ru
   <p>Панорама.</p>
 @endru
-IMG_4252.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4252.jpg'])
 
 @ru
-  <p>Множество стрелок подсказывает, что пора слезать с подъемника, так как он не остановится для высадки, а развернется и поедет вниз.</p>
+  <p>Множество стрелок подсказывает, что пора слезать с подъемника, потому что он не остановится для высадки, а развернется и поедет вниз.</p>
 @endru
-IMG_4255.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4255.jpg'])
 
 @ru
   <p>Спуск с горы на подъемнике.</p>
 @endru
-IMG_4259.jpeg
-IMG_4260.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_4259.jpg',
+  'IMG_4260.jpg',
+]])
 
 @ru
   <p>Канал реки.</p>
 @endru
-IMG_4276.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4276.jpg'])
 
 @ru
   <p>Последнее фото улицы.</p>
 @endru
-IMG_4277.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_4277.jpg'])
 @endsection
