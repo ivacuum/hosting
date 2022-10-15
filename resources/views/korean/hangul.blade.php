cda5f42ad814ce70d811d7f59aa9bdea8eb46f33
Sergey Pankov <vacuum@ivacuum.ru>
2022-10-15 16:03:45 +0300

    Обратная связь на странице тренажера хангыля

--- resources/views/korean/hangul.blade.php
+++ resources/views/korean/hangul.blade.php
@@ -3,4 +3,17 @@
 
 @section('content')
 @livewire(App\Http\Livewire\HangulTrainer::class)
+
+<div class="mt-12 max-w-xl mx-auto">
+  <div class="font-medium text-2xl mb-2 mt-12">@lang('Обратная связь')</div>
+  @ru
+    <p>Поделитесь своим опытом использования тренажера или задайте вопрос. Мы постараемся обработать информацию и сделать тренажер еще лучше.</p>
+  @en
+    <p>Use the form below to ask a question or share your thoughts. We will use your feedback to make the trainer better. There are certainly things to improve.</p>
+  @endru
+  @livewire(App\Http\Livewire\FeedbackForm::class, [
+    'title' => 'Hangul Trainer',
+    'hideTitle' => true,
+  ])
+</div>
 @endsection
