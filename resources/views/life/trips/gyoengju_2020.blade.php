604577b528bd37ac7c0503dd70b39e0ba7415dad
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-06 09:44:37 +0100

    Заготовка о поездке в Кёнджу

---
+++ resources/views/life/trips/gyoengju_2020.blade.php
@@ -0,0 +1,351 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Билет на автобус до города Кёнджу.</p>
+@endru
+IMG_5096.jpeg
+
+@ru
+  <p>Расписание автобусов на вокзале.</p>
+@endru
+IMG_5097.jpeg
+
+@ru
+  <p>Издали холмы напоминают верблюдов.</p>
+@endru
+IMG_5098.jpeg
+
+@ru
+  <p>Вблизи уже не напоминают.</p>
+@endru
+IMG_5099.jpeg
+
+@ru
+  <p>Что тут написано?</p>
+@endru
+IMG_5100.jpeg
+
+@ru
+  <p>Тропинка между заборами.</p>
+@endru
+IMG_5101.jpeg
+
+@ru
+  <p>Огнетушители в поле.</p>
+@endru
+IMG_5102.jpeg
+
+@ru
+  <p>Романизация названия города на столбе. Согласитесь, что ничего общего с Кёнджу.</p>
+@endru
+IMG_5103.jpeg
+
+@ru
+  <p>Клумбы с цветами вокруг дерева. На ствол дерева натянута плетенка с табличкой.</p>
+@endru
+IMG_5104.jpeg
+
+@ru
+  <p>Плетеные узоры разные.</p>
+@endru
+IMG_5105.jpeg
+IMG_5106.jpeg
+
+@ru
+  <p>Сорока на дереве без листвы.</p>
+@endru
+IMG_5107.jpeg
+
+@ru
+  <p>Скамейка вокруг дерева.</p>
+@endru
+IMG_5108.jpeg
+
+@ru
+  <p>Тротуар.</p>
+@endru
+IMG_5109.jpeg
+
+@ru
+  <p>Общественный транспорт в этом районе. Защищен от ветра.</p>
+@endru
+IMG_5110.jpeg
+
+@ru
+  <p>Цветы, скамейка и старина.</p>
+@endru
+IMG_5111.jpeg
+
+@ru
+  <p>Фотозона.</p>
+@endru
+IMG_5112.jpeg
+
+@ru
+  <p>Непонятно от чего столбики защищают, когда их сбоку обойти и объехать можно.</p>
+@endru
+IMG_5113.jpeg
+
+@ru
+  <p>Сиденье формы кролика.</p>
+@endru
+IMG_5116.jpeg
+
+@ru
+  <p>Кёнджу весной. На самом деле, это в упор сфотографированная в общественном туалете малюсенькая плитка на стене. Но нет причин сомневаться, что город так и выглядит весной.</p>
+@endru
+IMG_5117.jpeg
+
+@ru
+  <p>Кёнджу зимой.</p>
+@endru
+IMG_5144.jpeg
+
+@ru
+  <p>Дорога.</p>
+@endru
+IMG_5118.jpeg
+IMG_5142.jpeg
+
+@ru
+  <p>Крыльцо.</p>
+@endru
+IMG_5119.jpeg
+
+@ru
+  <p>Вид противоположного берега.</p>
+@endru
+IMG_5120.jpeg
+
+@ru
+  <p>Особенно удивительно одинокое дерево на островке.</p>
+@endru
+IMG_5123.jpeg
+
+@ru
+  <p>Макет местности.</p>
+@endru
+IMG_5121.jpeg
+
+@ru
+  <p>Национальное оформление.</p>
+@endru
+IMG_5124.jpeg
+
+@ru
+  <p>Парк.</p>
+@endru
+IMG_5125.jpeg
+IMG_5127.jpeg
+IMG_5128.jpeg
+IMG_5131.jpeg
+IMG_5132.jpeg
+IMG_5139.jpeg
+
+@ru
+  <p>Круговая скамейка вокруг дерева.</p>
+@endru
+IMG_5126.jpeg
+
+@ru
+  <p>Прямая скамейка.</p>
+@endru
+IMG_5133.jpeg
+
+@ru
+  <p>Вид из рощи.</p>
+@endru
+IMG_5137.jpeg
+
+@ru
+  <p>Демонстрация автоматической ночной подсветки на щите.</p>
+@endru
+IMG_5140.jpeg
+
+@ru
+  <p>Щит под крышей.</p>
+@endru
+IMG_5141.jpeg
+
+@ru
+  <p>Тротуар в цветах светофора.</p>
+@endru
+IMG_5143.jpeg
+
+@ru
+  <p>Гуляешь себе, а вокруг природа.</p>
+@endru
+IMG_5145.jpeg
+IMG_5146.jpeg
+IMG_5147.jpeg
+IMG_5148.jpeg
+IMG_5149.jpeg
+IMG_5150.jpeg
+IMG_5153.jpeg
+IMG_5154.jpeg
+IMG_5155.jpeg
+IMG_5156.jpeg
+
+@ru
+  <p>Дятел. Плюс видео.</p>
+@endru
+IMG_5152.jpeg
+
+@ru
+  <p>П-образная скамейка.</p>
+@endru
+IMG_5157.jpeg
+
+@ru
+  <p>Приближение к мосту.</p>
+@endru
+IMG_5158.jpeg
+IMG_5159.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_5160.jpeg
+
+@ru
+  <p>Вход на мост.</p>
+@endru
+IMG_5161.jpeg
+
+@ru
+  <p>На мосту.</p>
+@endru
+IMG_5162.jpeg
+
+@ru
+  <p>Навигация и предостерегающие таблички.</p>
+@endru
+IMG_5164.jpeg
+IMG_5165.jpeg
+IMG_5166.jpeg
+
+@ru
+  <p>Вид с моста.</p>
+@endru
+IMG_5163.jpeg
+IMG_5168.jpeg
+IMG_5169.jpeg
+
+@ru
+  <p>Почтовый ящик у входа во двор дома.</p>
+@endru
+IMG_5170.jpeg
+
+@ru
+  <p>Дворики.</p>
+@endru
+IMG_5172.jpeg
+
+@ru
+  <p>Холмы.</p>
+@en
+  <p>Hills.</p>
+@endru
+IMG_5176.jpeg
+
+@ru
+  <p>Таракан.</p>
+@en
+  <p>Cockroach.</p>
+@endru
+IMG_5177.jpeg
+
+@ru
+  <p>Одинокий диванчик.</p>
+@endru
+IMG_5178.jpeg
+
+@ru
+  <p>Старбакс снаружи.</p>
+@endru
+IMG_5179.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_5184.jpeg
+
+@ru
+  <p>Дворик.</p>
+@endru
+IMG_5185.jpeg
+
+@ru
+  <p>Прям очень много крыш в едином стиле в городе.</p>
+@endru
+IMG_5187.jpeg
+
+@ru
+  <p>Улицы.</p>
+@endru
+IMG_5188.jpeg
+IMG_5190.jpeg
+IMG_5195.jpeg
+IMG_5202.jpeg
+
+@ru
+  <p>Идет отделка.</p>
+@endru
+IMG_5189.jpeg
+
+@ru
+  <p>Тоторо на стене дома.</p>
+@endru
+IMG_5191.jpeg
+
+@ru
+  <p>Входная дверь.</p>
+@endru
+IMG_5192.jpeg
+
+@ru
+  <p>Деревянный почтовый ящик.</p>
+@endru
+IMG_5193.jpeg
+
+@ru
+  <p>Подготовка к строительству.</p>
+@endru
+IMG_5194.jpeg
+
+@ru
+  <p>Еще наведут красоту.</p>
+@endru
+IMG_5196.jpeg
+
+@ru
+  <p>Необычный забор из кирпичей.</p>
+@endru
+IMG_5197.jpeg
+
+@ru
+  <p>Котик обедает у входной двери.</p>
+@endru
+IMG_5198.jpeg
+
+@ru
+  <p>Счетчик на улице.</p>
+@endru
+IMG_5199.jpeg
+
+@ru
+  <p>Опасно входить.</p>
+@endru
+IMG_5200.jpeg
+
+@ru
+  <p>Протекторы для размашистого открытия дверей.</p>
+@endru
+IMG_5201.jpeg
+
+@ru
+  <p>Городской почтовый ящик.</p>
+@endru
+IMG_5203.jpeg
+@endsection
