d600e2f798bbbbe384a9815cb8e79f9870f1861d
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-07 19:34:26 +0100

    Кёнджу в 2020

--- resources/views/life/trips/gyoengju_2020.blade.php
+++ resources/views/life/trips/gyoengju_2020.blade.php
@@ -4,348 +4,363 @@
 @ru
   <p>Билет на автобус до города Кёнджу.</p>
 @endru
-IMG_5096.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5096.jpg'])
 
 @ru
   <p>Расписание автобусов на вокзале.</p>
 @endru
-IMG_5097.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5097.jpg'])
 
 @ru
   <p>Издали холмы напоминают верблюдов.</p>
 @endru
-IMG_5098.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5098.jpg'])
 
 @ru
   <p>Вблизи уже не напоминают.</p>
 @endru
-IMG_5099.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5099.jpg'])
 
 @ru
-  <p>Что тут написано?</p>
+  <p>Нужно немало тренироваться, чтобы бегло читать корейский курсив.</p>
 @endru
-IMG_5100.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5100.jpg'])
 
 @ru
   <p>Тропинка между заборами.</p>
 @endru
-IMG_5101.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5101.jpg'])
 
 @ru
   <p>Огнетушители в поле.</p>
 @endru
-IMG_5102.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5102.jpg'])
 
 @ru
   <p>Романизация названия города на столбе. Согласитесь, что ничего общего с Кёнджу.</p>
 @endru
-IMG_5103.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5103.jpg'])
 
 @ru
   <p>Клумбы с цветами вокруг дерева. На ствол дерева натянута плетенка с табличкой.</p>
 @endru
-IMG_5104.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5104.jpg'])
 
 @ru
   <p>Плетеные узоры разные.</p>
 @endru
-IMG_5105.jpeg
-IMG_5106.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5105.jpg',
+  'IMG_5106.jpg',
+]])
 
 @ru
   <p>Сорока на дереве без листвы.</p>
 @endru
-IMG_5107.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5107.jpg'])
 
 @ru
   <p>Скамейка вокруг дерева.</p>
 @endru
-IMG_5108.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5108.jpg'])
 
 @ru
   <p>Тротуар.</p>
 @endru
-IMG_5109.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5109.jpg'])
 
 @ru
   <p>Общественный транспорт в этом районе. Защищен от ветра.</p>
 @endru
-IMG_5110.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5110.jpg'])
 
 @ru
   <p>Цветы, скамейка и старина.</p>
 @endru
-IMG_5111.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5111.jpg'])
 
 @ru
-  <p>Фотозона.</p>
+  <p>Тараканья фотозона.</p>
 @endru
-IMG_5112.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5112.jpg'])
 
 @ru
-  <p>Непонятно от чего столбики защищают, когда их сбоку обойти и объехать можно.</p>
+  <p>Непонятно от чего столбики защищают, когда их сбоку и обойти, и объехать можно.</p>
 @endru
-IMG_5113.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5113.jpg'])
 
 @ru
   <p>Сиденье формы кролика.</p>
 @endru
-IMG_5116.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5116.jpg'])
 
 @ru
   <p>Кёнджу весной. На самом деле, это в упор сфотографированная в общественном туалете малюсенькая плитка на стене. Но нет причин сомневаться, что город так и выглядит весной.</p>
 @endru
-IMG_5117.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5117.jpg'])
 
 @ru
   <p>Кёнджу зимой.</p>
 @endru
-IMG_5144.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5144.jpg'])
 
 @ru
   <p>Дорога.</p>
 @endru
-IMG_5118.jpeg
-IMG_5142.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5118.jpg',
+  'IMG_5142.jpg',
+]])
 
 @ru
   <p>Крыльцо.</p>
 @endru
-IMG_5119.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5119.jpg'])
 
 @ru
-  <p>Вид противоположного берега.</p>
+  <p>Вид с крыльца на противоположный берег.</p>
 @endru
-IMG_5120.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5120.jpg'])
 
 @ru
   <p>Особенно удивительно одинокое дерево на островке.</p>
 @endru
-IMG_5123.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5123.jpg'])
 
 @ru
   <p>Макет местности.</p>
 @endru
-IMG_5121.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5121.jpg'])
 
 @ru
   <p>Национальное оформление.</p>
 @endru
-IMG_5124.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5124.jpg'])
 
 @ru
   <p>Парк.</p>
 @endru
-IMG_5125.jpeg
-IMG_5127.jpeg
-IMG_5128.jpeg
-IMG_5131.jpeg
-IMG_5132.jpeg
-IMG_5139.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5125.jpg',
+  'IMG_5127.jpg',
+  'IMG_5128.jpg',
+  'IMG_5131.jpg',
+  'IMG_5132.jpg',
+  'IMG_5139.jpg',
+]])
 
 @ru
   <p>Круговая скамейка вокруг дерева.</p>
 @endru
-IMG_5126.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5126.jpg'])
 
 @ru
   <p>Прямая скамейка.</p>
 @endru
-IMG_5133.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5133.jpg'])
 
 @ru
   <p>Вид из рощи.</p>
 @endru
-IMG_5137.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5137.jpg'])
 
 @ru
   <p>Демонстрация автоматической ночной подсветки на щите.</p>
 @endru
-IMG_5140.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5140.jpg'])
 
 @ru
-  <p>Щит под крышей.</p>
+  <p>Щит под традиционной крышей.</p>
 @endru
-IMG_5141.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5141.jpg'])
 
 @ru
   <p>Тротуар в цветах светофора.</p>
 @endru
-IMG_5143.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5143.jpg'])
 
 @ru
   <p>Гуляешь себе, а вокруг природа.</p>
 @endru
-IMG_5145.jpeg
-IMG_5146.jpeg
-IMG_5147.jpeg
-IMG_5148.jpeg
-IMG_5149.jpeg
-IMG_5150.jpeg
-IMG_5153.jpeg
-IMG_5154.jpeg
-IMG_5155.jpeg
-IMG_5156.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5145.jpg',
+  'IMG_5146.jpg',
+  'IMG_5147.jpg',
+  'IMG_5148.jpg',
+  'IMG_5149.jpg',
+  'IMG_5150.jpg',
+  'IMG_5153.jpg',
+  'IMG_5154.jpg',
+  'IMG_5155.jpg',
+  'IMG_5156.jpg',
+]])
 
 @ru
-  <p>Дятел. Плюс видео.</p>
+  <p>Дятел. На видео слышно как стучит.</p>
 @endru
-IMG_5152.jpeg
+<livewire:youtube title="Woodpecker at Gyoengju, Korea, January 2020" v="I1Fb1Jhakhc"/>
+@include('tpl.pic-2x', ['pic' => 'IMG_5152.jpg'])
 
 @ru
   <p>П-образная скамейка.</p>
 @endru
-IMG_5157.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5157.jpg'])
 
 @ru
   <p>Приближение к мосту.</p>
 @endru
-IMG_5158.jpeg
-IMG_5159.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5158.jpg',
+  'IMG_5159.jpg',
+]])
 
 @ru
   <p>Скамейка.</p>
 @endru
-IMG_5160.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5160.jpg'])
 
 @ru
   <p>Вход на мост.</p>
 @endru
-IMG_5161.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5161.jpg'])
 
 @ru
   <p>На мосту.</p>
 @endru
-IMG_5162.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5162.jpg'])
 
 @ru
   <p>Навигация и предостерегающие таблички.</p>
 @endru
-IMG_5164.jpeg
-IMG_5165.jpeg
-IMG_5166.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5164.jpg',
+  'IMG_5165.jpg',
+  'IMG_5166.jpg',
+]])
 
 @ru
   <p>Вид с моста.</p>
 @endru
-IMG_5163.jpeg
-IMG_5168.jpeg
-IMG_5169.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5163.jpg',
+  'IMG_5168.jpg',
+  'IMG_5169.jpg',
+]])
 
 @ru
   <p>Почтовый ящик у входа во двор дома.</p>
 @endru
-IMG_5170.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5170.jpg'])
 
 @ru
   <p>Дворики.</p>
 @endru
-IMG_5172.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5172.jpg'])
 
 @ru
   <p>Холмы.</p>
 @en
   <p>Hills.</p>
 @endru
-IMG_5176.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5176.jpg'])
 
 @ru
   <p>Таракан.</p>
 @en
   <p>Cockroach.</p>
 @endru
-IMG_5177.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5177.jpg'])
 
 @ru
-  <p>Одинокий диванчик.</p>
+  <p>Одинокий диванчик на тротуаре.</p>
 @endru
-IMG_5178.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5178.jpg'])
 
 @ru
   <p>Старбакс снаружи.</p>
 @endru
-IMG_5179.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5179.jpg'])
 
 @ru
   <p>Скамейка.</p>
 @endru
-IMG_5184.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5184.jpg'])
 
 @ru
   <p>Дворик.</p>
 @endru
-IMG_5185.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5185.jpg'])
 
 @ru
   <p>Прям очень много крыш в едином стиле в городе.</p>
 @endru
-IMG_5187.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5187.jpg'])
 
 @ru
-  <p>Улицы.</p>
+  <p>Улицы с малоэтажными домами.</p>
 @endru
-IMG_5188.jpeg
-IMG_5190.jpeg
-IMG_5195.jpeg
-IMG_5202.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5188.jpg',
+  'IMG_5190.jpg',
+  'IMG_5195.jpg',
+  'IMG_5202.jpg',
+]])
 
 @ru
-  <p>Идет отделка.</p>
+  <p>Идет отделка нового здания.</p>
 @endru
-IMG_5189.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5189.jpg'])
 
 @ru
-  <p>Тоторо на стене дома.</p>
+  <p>Японский Тоторо на стене дома.</p>
 @endru
-IMG_5191.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5191.jpg'])
 
 @ru
-  <p>Входная дверь.</p>
+  <p>Входные двери и деревянные почтовые ящики.</p>
 @endru
-IMG_5192.jpeg
-
-@ru
-  <p>Деревянный почтовый ящик.</p>
-@endru
-IMG_5193.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5192.jpg',
+  'IMG_5193.jpg',
+]])
 
 @ru
   <p>Подготовка к строительству.</p>
 @endru
-IMG_5194.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5194.jpg'])
 
 @ru
   <p>Еще наведут красоту.</p>
 @endru
-IMG_5196.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5196.jpg'])
 
 @ru
   <p>Необычный забор из кирпичей.</p>
 @endru
-IMG_5197.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5197.jpg'])
 
 @ru
-  <p>Котик обедает у входной двери.</p>
+  <p>Котик обедает у входной двери, а на почтовый ящик повесили замок.</p>
 @endru
-IMG_5198.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5198.jpg'])
 
 @ru
   <p>Счетчик на улице.</p>
 @endru
-IMG_5199.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5199.jpg'])
 
 @ru
-  <p>Опасно входить.</p>
+  <p>Пугают, что опасно входить.</p>
 @endru
-IMG_5200.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5200.jpg'])
 
 @ru
-  <p>Протекторы для размашистого открытия дверей.</p>
+  <p>Протекторы для размашистого открытия дверей автомобиля.</p>
 @endru
-IMG_5201.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5201.jpg'])
 
 @ru
   <p>Городской почтовый ящик.</p>
 @endru
-IMG_5203.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5203.jpg'])
 @endsection
