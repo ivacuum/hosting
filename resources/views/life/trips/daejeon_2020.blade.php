1cecf30fa5b06e20addf99e3228f3353139e6461
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-08 10:41:42 +0100

    Тэджон в 2020

--- resources/views/life/trips/daejeon_2020.blade.php
+++ resources/views/life/trips/daejeon_2020.blade.php
@@ -4,300 +4,314 @@
 @ru
   <p>Поезд в Тэджон.</p>
 @endru
-IMG_5350.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5350.jpg'])
 
 @ru
-  <p>Постер.</p>
+  <p>Постер на заборе стройки.</p>
 @endru
-IMG_5362.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5362.jpg'])
 
 @ru
-  <p>Железнодорожные платформы.</p>
+  <p>Железнодорожные платформы на вокзале, на который прибывает поезд KTX.</p>
 @endru
-IMG_5351.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5351.jpg'])
 
 @ru
   <p>Холодные и горячие напитки в автомате.</p>
 @endru
-IMG_5352.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5352.jpg'])
 
 @ru
   <p>Подземный переход, который немало людей игнорирует и перебегает дорогу по земле.</p>
 @endru
-IMG_5353.jpeg
-IMG_5354.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5353.jpg',
+  'IMG_5354.jpg',
+]])
 
 @ru
-  <p>Вот так выбираешь заведение поесть на картах, идешь туда, а оно закрыто с запиской.</p>
+  <p>Вот так выбираешь на картах заведение поесть, идешь туда, а оно закрыто с запиской. Квест начинается сначала.</p>
 @endru
-IMG_5360.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5360.jpg'])
 
 @ru
   <p>Приходится идти в другое, выбирая его на ходу.</p>
 @endru
-IMG_5361.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5361.jpg'])
 
 @ru
   <p>Люк.</p>
 @en
   <p>Manhole.</p>
 @endru
-IMG_5365.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5365.jpg'])
 
 @ru
   <p>Как хорошо столбы подходят для вертикальных надписей.</p>
 @endru
-IMG_5366.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5366.jpg'])
 
 @ru
   <p>Реклама на бордюрах.</p>
 @endru
-IMG_5367.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5367.jpg'])
 
 @ru
   <p>Гигантское круглое окно.</p>
 @endru
-IMG_5368.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5368.jpg'])
 
 @ru
   <p>Чудеса — банковская карта сработала. До этого везде наличными пользовался.</p>
 @endru
-IMG_5370.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5370.jpg'])
 
 @ru
   <p>Здание с детской площадкой.</p>
 @endru
-IMG_5371.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5371.jpg'])
 
 @ru
   <p>Парк.</p>
 @endru
-IMG_5372.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5372.jpg'])
 
 @ru
-  <p>Дополнения к скамейке в виде связок стульев.</p>
+  <p>Связки стульев по обе стороны скамейки.</p>
 @endru
-IMG_5374.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5374.jpg'])
 
 @ru
   <p>Школа неподалеку — держим детей за руку.</p>
 @endru
-IMG_5375.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5375.jpg'])
 
 @ru
-  <p>Дорога.</p>
+  <p>Многополосная дорога.</p>
 @endru
-IMG_5376.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5376.jpg'])
 
 @ru
-  <p>Временные домики.</p>
+  <p>Один из временных домиков больно роскошен снаружи.</p>
 @endru
-IMG_5377.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5377.jpg'])
 
 @ru
   <p>Улица. В продаже пуховики.</p>
 @endru
-IMG_5378.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5378.jpg'])
 
 @ru
   <p>Объявления на столбе.</p>
 @endru
-IMG_5379.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5379.jpg'])
 
 @ru
   <p>Улицы.</p>
 @endru
-IMG_5380.jpeg
-IMG_5381.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5380.jpg',
+  'IMG_5381.jpg',
+]])
 
 @ru
   <p>Остановка автобуса и стоянка такси.</p>
 @endru
-IMG_5382.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5382.jpg'])
 
 @ru
   <p>Прокат велосипедов.</p>
 @endru
-IMG_5383.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5383.jpg'])
 
 @ru
   <p>Дороги.</p>
 @endru
-IMG_5384.jpeg
-IMG_5385.jpeg
-IMG_5392.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5384.jpg',
+  'IMG_5385.jpg',
+  'IMG_5392.jpg',
+]])
 
 @ru
-  <p>Лифт.</p>
+  <p>Лифт в жилом доме.</p>
 @endru
-IMG_5386.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5386.jpg'])
 
 @ru
-  <p>Автоматический замок внутри квартиры. Снаружи — кодовая панель к нему.</p>
+  <p>Автоматический замок внутри квартиры. Снаружи — кодовая панель к нему. Изнутри открывается по нажатию единственной кнопки.</p>
 @endru
-IMG_5387.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5387.jpg'])
 
 @ru
-  <p>Импровизированная урна для окурков.</p>
+  <p>Импровизированная урна для окурков с видом на мост.</p>
 @endru
-IMG_5388.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5388.jpg'])
 
 @ru
-  <p>На мосту.</p>
+  <p>Посередине моста.</p>
 @endru
-IMG_5389.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5389.jpg'])
 
 @ru
   <p>Обзор набережной с моста.</p>
 @endru
-IMG_5390.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5390.jpg'])
 
 @ru
-  <p>The North Face — часто встречающаяся марка одежды на корейцах.</p>
+  <p>На корейцах часто можно заметить одежду марки The North Face.</p>
 @endru
-IMG_5391.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5391.jpg'])
 
 @ru
   <p>Карта парка.</p>
 @endru
-IMG_5393.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5393.jpg'])
 
 @ru
   <p>Парк.</p>
 @endru
-IMG_5394.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5394.jpg'])
 
 @ru
   <p>Ступеньки наверх.</p>
 @endru
-IMG_5396.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5396.jpg'])
 
 @ru
   <p>Обзор сверху.</p>
 @endru
-IMG_5397.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5397.jpg'])
 
 @ru
   <p>Навигация.</p>
 @endru
-IMG_5398.jpeg
-IMG_5399.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5398.jpg',
+  'IMG_5399.jpg',
+]])
 
 @ru
-  <p>Почитать что тут у нас.</p>
+  <p>Описание процесса очистки для подачи питьевой воды в жилые дома.</p>
 @endru
-IMG_5400.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5400.jpg'])
 
 @ru
   <p>Ливневая канализация.</p>
 @endru
-IMG_5401.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5401.jpg'])
 
 @ru
   <p>Узкий проход между заборами.</p>
 @endru
-IMG_5402.jpeg
-IMG_5403.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5402.jpg',
+  'IMG_5403.jpg',
+]])
 
 @ru
   <p>Еще больше заборов.</p>
 @endru
-IMG_5404.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5404.jpg'])
 
 @ru
   <p>Спуск вниз.</p>
 @endru
-IMG_5405.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5405.jpg'])
 
 @ru
   <p>Обзорная площадка.</p>
 @endru
-IMG_5406.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5406.jpg'])
 
 @ru
   <p>Узкие улицы.</p>
 @endru
-IMG_5407.jpeg
-IMG_5409.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5407.jpg',
+  'IMG_5409.jpg',
+]])
 
 @ru
   <p>Уличная табличка и почтовый ящик.</p>
 @endru
-IMG_5408.jpeg
-IMG_5410.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_5408.jpg',
+  'IMG_5410.jpg',
+]])
 
 @ru
   <p>Многоэтажная парковка.</p>
 @endru
-IMG_5411.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5411.jpg'])
 
 @ru
   <p>Грузовик парит.</p>
 @endru
-IMG_5412.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5412.jpg'])
 
 @ru
-  <p>Пешеходный переход и раскладной зонт.</p>
+  <p>Пешеходный переход и раскладной зонт, неактуальный зимой.</p>
 @endru
-IMG_5413.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5413.jpg'])
 
 @ru
   <p>Фасад здания.</p>
 @endru
-IMG_5415.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5415.jpg'])
 
 @ru
   <p>Обеденная зона в круглосуточном магазине.</p>
 @endru
-IMG_5417.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5417.jpg'])
 
 @ru
   <p>Напольная карта местности.</p>
 @endru
-IMG_5418.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5418.jpg'])
 
 @ru
   <p>Книжный магазин.</p>
 @endru
-IMG_5424.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5424.jpg'])
 
 @ru
-  <p>Ручки в продаже. Как их расписывают корейцы.</p>
+  <p>Ручки в продаже. И как их расписывают корейцы.</p>
 @endru
-IMG_5421.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5421.jpg'])
 
 @ru
   <p>Карандаши.</p>
 @endru
-IMG_5422.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5422.jpg'])
 
 @ru
   <p>Фломастеры.</p>
 @endru
-IMG_5423.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5423.jpg'])
 
 @ru
   <p>Вечерняя улица.</p>
 @endru
-IMG_5538.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5538.jpg'])
 
 @ru
   <p>Цветы в обеденной зоне круглосуточного магазина.</p>
 @endru
-IMG_5539.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5539.jpg'])
 
 @ru
-  <p>Офис мобильного оператора.</p>
+  <p>Офис местного мобильного оператора.</p>
 @endru
-IMG_5540.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5540.jpg'])
 
 @ru
   <p>Юрта в аэропорту.</p>
 @endru
-IMG_5544.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5544.jpg'])
 
 @ru
   <p>Зрители наблюдают в аэропорту традиционное корейское представление.</p>
 @endru
-IMG_5545.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_5545.jpg'])
 @endsection
