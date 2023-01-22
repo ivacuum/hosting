dcda34c6c7c5f4adc8061b90ed113f71ec1b91aa
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-22 17:22:54 +0100

    Санкт-Петербург в апреле 2021

--- resources/views/life/trips/spb_2021_04.blade.php
+++ resources/views/life/trips/spb_2021_04.blade.php
@@ -2,6 +2,355 @@
 
 @section('content')
 @ru
-  <p>Онлайн-регистрация у Азимута платная.</p>
+  <p>Онлайн-регистрация у Азимута платная, а в аэропорту — бесплатная. Таким образом теперь продают удобство и скорость. А появлялась изначально онлайн-регистрация для ускорения прохождения регистрации всеми пассажирами и, как следствие, разгрузки сотрудников в аэропорту.</p>
 @endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7542.jpg'])
+
+@ru
+  <p>Стилизованная скамейка возле аэропорта Пулково.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7543.jpg'])
+
+@ru
+  <p>Парадная.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7544.jpg'])
+
+@ru
+  <p>Коридор на этаже.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7545.jpg'])
+
+@ru
+  <p>Подсветка фасада здания.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7547.jpg'])
+
+@ru
+  <p>Подсветка верхнего контура здания.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7546.jpg'])
+
+@ru
+  <p>Диагональная посадка за большим столом в пиццерии в эпоху пандемии.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7548.jpg',
+  'IMG_7549.jpg',
+]])
+
+@ru
+  <p>Одиноко стоит на тротуаре прокатный самокат.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7550.jpg'])
+
+@ru
+  <p>Плитка никак до некоторых сквериков не дойдет — приходится ходить по земле.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7551.jpg'])
+
+@ru
+  <p>Вот здесь в любую погоду комфортно ходить по плитке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7554.jpg'])
+
+@ru
+  <p>Заросший фасад театра на Фонтанке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7552.jpg'])
+
+@ru
+  <p>Театральная урна.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7553.jpg'])
+
+@ru
+  <p>Сад закрыт на просушку.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7555.jpg'])
+
+@ru
+  <p>Дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7556.jpg'])
+
+@ru
+  <p>Фасад здания. Улыбка радуги — сеть магазинов бытовой химии. Каждый в своем городе привыкает к своим сетям подобных магазинов, а потом немного теряется в поездках по стране, когда привычные названия отсутствуют.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7557.jpg'])
+
+@ru
+  <p>Троице-Измайловский собор.</p>
+@en
+  <p>Trinity Cathedral.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7558.jpg'])
+
+@ru
+  <p>Измайловский мост через реку Фонтанку.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7559.jpg'])
+
+@ru
+  <p>Набережная реки Фонтанки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7560.jpg'])
+
+@ru
+  <p>Дорожный знак пробуждает интерес: есть ли статистика по затонувшим в каналах автомобилям?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7561.jpg'])
+
+@ru
+  <p>Льдины в канале похожи на плоты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7562.jpg'])
+
+@ru
+  <p>Никольские ряды.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7563.jpg'])
+
+@ru
+  <p>Канал Грибоедова.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7564.jpg'])
+
+@ru
+  <p>Пикалов мост.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7565.jpg'])
+
+@ru
+  <p>Табличка Красногвардейского моста.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7566.jpg'])
+
+@ru
+  <p>Детская площадка перед Никольским морским собором.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7567.jpg'])
+
+@ru
+  <p>Еще один сад закрыт на просушку.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7568.jpg'])
+
+@ru
+  <p>Скамейки с навесом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7569.jpg'])
+
+@ru
+  <p>Возрастное предназначение детского оборудования в Никольском парке.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7570.jpg'])
+
+@ru
+  <p>Развитие технологий и сотовых сетей поколения 4G позволили сделать прокат самокатов без док-станций. Скачай приложение, привяжи банковскую карту — можно арендовать и ехать.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7571.jpg'])
+
+@ru
+  <p>Фасады домов.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7573.jpg',
+  'IMG_7576.jpg',
+]])
+
+@ru
+  <p>Каменный мост. Такой есть и в <a class="link" href="kaluga">Калуге</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7574.jpg'])
+
+@ru
+  <p>Троллейбус едет по мосту без подключения к контактной сети, потому что опустил рога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7575.jpg'])
+
+@ru
+  <p>Оплата за городской автобус на выходе. Остановить надо просить голосом.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7578.jpg'])
+
+@ru
+  <p>Гранд Макет — миниатюра России от запада до востока. На осмотр всего представленного запросто уходит три часа и больше. Можно взять бинокль и аудиогида, чтобы познать больше деталей.</p>
+@endru
+<livewire:youtube title="Grand Maket, Saint Petersburg, Russia 2021" v="fr7sIYFTCK4"/>
+@include('tpl.pic-2x', ['pic' => 'IMG_7664.jpg'])
+
+@ru
+  <p>Посетителей просят соблюдать масочный режим. Иногда по громкой связи обращаются к конкретным людям, называя во что они одеты.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7586.jpg'])
+
+@ru
+  <p>Смена дня и ночи на примере окрестностей киностудии.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7590.jpg',
+  'IMG_7591.jpg',
+]])
+
+@ru
+  <p>Вывод локомотивов из депо на железнодорожные пути.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7599.jpg'])
+
+@ru
+  <p>Под поверхностью макета еще много служебных уровней.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7600.jpg'])
+
+@ru
+  <p>В детстве в семье была коллекция подобных локомотивов.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7602.jpg'])
+
+@ru
+  <p>На сваях дома строят в том же <a class="link" href="yakutsk">Якутске</a>, потому что вечная мерзлота вынуждает.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7610.jpg'])
+
+@ru
+  <p>Промышленность при ночном освещении.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7613.jpg'])
+
+@ru
+  <p>Питер навсегда.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7614.jpg'])
+
+@ru
+  <p>Сотрудники присматривают за макетом по камерам и мониторингам.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7619.jpg'])
+
+@ru
+  <p>Еще смена ночи и дня.</p>
+@endru
+<livewire:youtube title="Night/Day change at Grand Maket, Saint Petersburg, Russia 2021" v="z9rv8YXI5fY"/>
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7621.jpg',
+  'IMG_7623.jpg',
+  'IMG_7624.jpg',
+]])
+
+@ru
+  <p>Снова уже встреченный в городе магазин Улыбка радуги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7625.jpg'])
+
+@ru
+  <p>Резные здания на фоне Москва-сити.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7630.jpg'])
+
+@ru
+  <p>Пожар в деревне анимирован.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7632.jpg'])
+
+@ru
+  <p>Ночная Москва.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7633.jpg'])
+
+@ru
+  <p>Дирижабль редко где-либо увидишь.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7636.jpg'])
+
+@ru
+  <p>Якоря не бро. Кто же наш бро?</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7637.jpg'])
+
+@ru
+  <p>Парад автомобилей у театра.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7640.jpg'])
+
+@ru
+  <p>Двухэтажный самолет.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7643.jpg'])
+
+@ru
+  <p>Смена дня и ночи в парке развлечений.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7644.jpg',
+  'IMG_7645.jpg',
+]])
+
+@ru
+  <p>Многоэтажный круизный лайнер.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7648.jpg'])
+
+@ru
+  <p>Лежаки на пляже.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7651.jpg'])
+
+@ru
+  <p>Зимний склон для лыжников.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7653.jpg'])
+
+@ru
+  <p>Важная просьба от создателей — не трогать макет, чтобы он дольше радовал посетителей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7659.jpg'])
+
+@ru
+  <p>Часть перечисленных городов уже удалось посетить: <a class="link" href="kaliningrad">Калининград</a>, <a class="link" href="kazan">Казань</a> и <a class="link" href="kaluga">Калугу</a>.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7660.jpg'])
+
+@ru
+  <p>К закрытию ажиотаж спадает и становится свободно. Видны ступеньки для удобства детей и низких людей.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7663.jpg'])
+
+@ru
+  <p>Московское шоссе.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7665.jpg'])
+
+@ru
+  <p>Река Фонтанка на закате.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7666.jpg'])
+
+@ru
+  <p>Ожидание еды по заказу номер двенадцать.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7667.jpg'])
+
+@ru
+  <p>Кодовый замок на двери для прохода в тамбур.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7668.jpg'])
+
+@ru
+  <p>Прекрасный солнечный день.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7669.jpg',
+  'IMG_7670.jpg',
+]])
+
+@ru
+  <p>Выглядывание на Невский проспект через арку.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7672.jpg'])
+
+@ru
+  <p>Зона вылетов аэропорта Пулково производит впечатление рынка.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7673.jpg'])
 @endsection
