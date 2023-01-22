908c1f56bc006c404db159d153702580fad1785b
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-22 17:59:23 +0100

    Москва в мае 2021

--- resources/views/life/trips/msk_2021_05.blade.php
+++ resources/views/life/trips/msk_2021_05.blade.php
@@ -4,9 +4,11 @@
 @ru
   <p>Станция метро Мичуринский проспект.</p>
 @endru
-IMG_7697.jpeg
-IMG_7698.jpeg
-IMG_7699.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7697.jpg',
+  'IMG_7698.jpg',
+  'IMG_7699.jpg',
+]])
 
 @ru
   <p>Опробован каршеринг. Если автомобиль не заводится, то может помочь ребут — приостановить поездку, подождать десять секунд, затем продолжить поездку. Цена на двоих сопоставима с такси. А по пробкам ехать вообще непривлекательно по цене выходит. Стоит пользоваться навигатором автомобиля, нежели свой телефон разряжать.</p>
@@ -15,37 +17,41 @@ IMG_7699.jpeg
 @ru
   <p>Царицыно.</p>
 @endru
-IMG_7702.jpeg
-IMG_7701.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7702.jpg',
+  'IMG_7701.jpg',
+]])
 
 @ru
   <p>Фонтан.</p>
 @endru
-IMG_7700.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7700.jpg'])
 
 @ru
   <p>Много желающих сфотографироваться на фоне цветущей сакуры.</p>
 @endru
-IMG_7703.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7703.jpg'])
 
 @ru
   <p>Пруд.</p>
 @endru
-IMG_7704.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7704.jpg'])
 
 @ru
   <p>Лежаки перед прудом.</p>
 @endru
-IMG_7705.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7705.jpg'])
 
 @ru
   <p>Аллеи.</p>
 @endru
-IMG_7706.jpeg
-IMG_7707.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7706.jpg',
+  'IMG_7707.jpg',
+]])
 
 @ru
   <p>Вход на станцию метро Тверская.</p>
 @endru
-IMG_7708.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7708.jpg'])
 @endsection
