b4abea349fc5e3de9b423669d8e84c15647ab131
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-27 18:14:18 +0100

    Санкт-Петербург в июле 2021

---
+++ resources/views/life/trips/spb_2021_07.blade.php
@@ -0,0 +1,203 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Мансарды приковали взгляд.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7783.jpg'])
+
+@ru
+  <p>Аномальное лето 2021 года — все дни в городе температура выше +30&deg;C. Достижение разблокировано: загореть в Питере.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7784.jpg'])
+
+@ru
+  <p>Крохотный сквер на пути к арендованному жилью.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7785.jpg'])
+
+@ru
+  <p>Тематическая навигация возле станции метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7791.jpg'])
+
+@ru
+  <p>Изогнутая скамейка между клумбой и урной.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7792.jpg'])
+
+@ru
+  <p>Односторонняя дорога.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7793.jpg'])
+
+@ru
+  <p>Зелень и дома тенью спасают от жаркого солнца.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7794.jpg'])
+
+@ru
+  <p>Настоящий автомобиль и нарисованный схожих размеров.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7895.jpg'])
+
+@ru
+  <p>Поезд на станции метро готовится к отправлению.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7896.jpg'])
+
+@ru
+  <p>Карты окрестностей северного полюса и созвездий южного полушария на торце здания.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7897.jpg'])
+
+@ru
+  <p>Самокаты компактно и вертикально размещены в док-станции.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7898.jpg'])
+
+@ru
+  <p>Камеры хранения на Московском вокзале общей высотой с человеческий рост.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7899.jpg',
+  'IMG_7900.jpg',
+]])
+
+@ru
+  <p>Масочный режим в метро.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7902.jpg'])
+
+@ru
+  <p>Два вида душа в арендованной квартире.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7905.jpg'])
+
+@ru
+  <p>Бойлер с рекламными наклейками производителя.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7906.jpg'])
+
+@ru
+  <p>Окно общепита в цветах.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7908.jpg'])
+
+@ru
+  <p>Фасады зданий.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7909.jpg',
+  'IMG_7915.jpg',
+]])
+
+@ru
+  <p>Тонкий светодиодный пешеходный светофор.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7912.jpg'])
+
+@ru
+  <p>Улица Бармалеева.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7911.jpg'])
+
+@ru
+  <p>Разноцветные дома на Большой Пушкарской улице.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7913.jpg'])
+
+@ru
+  <p>Столики общепита на улице.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7914.jpg'])
+
+@ru
+  <p>Территория больницы имени Павлова.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7916.jpg'])
+
+@ru
+  <p>Огромный навес.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7918.jpg'])
+
+@ru
+  <p>Здесь МРТ. Ковида здесь нет, ковид — туда (налево).</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7919.jpg'])
+
+@ru
+  <p>Кнопка вызова полиции на стене общежития.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7920.jpg'])
+
+@ru
+  <p>Общественное пространство на набережной реки Карповки.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7923.jpg',
+  'IMG_7924.jpg',
+  'IMG_7925.jpg',
+  'IMG_7927.jpg',
+]])
+
+@ru
+  <p>Качели.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7921.jpg'])
+
+@ru
+  <p>Молодые деревья.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7922.jpg'])
+
+@ru
+  <p>Спуск к воде.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7926.jpg'])
+
+@ru
+  <p>Севкабель Порт.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7928.jpg',
+  'IMG_7929.jpg',
+  'IMG_7930.jpg',
+  'IMG_7931.jpg',
+  'IMG_7933.jpg',
+]])
+
+@ru
+  <p>Крытый переход между зданиями.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7932.jpg'])
+
+@ru
+  <p>Салон троллейбуса.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7934.jpg'])
+
+@ru
+  <p>Станция метро пятой ветки.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7936.jpg'])
+
+@ru
+  <p>Аэропорт Пулково.</p>
+@endru
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7977.jpg',
+  'IMG_7978.jpg',
+]])
+
+@ru
+  <p>Много свободных сидений в неприметном месте этажом ниже.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7980.jpg'])
+
+@ru
+  <p>Самолет до Калуги.</p>
+@endru
+@include('tpl.pic-2x', ['pic' => 'IMG_7981.jpg'])
+@endsection
