a90ff927f2943c07cbb84a7dae6a24fd23a17e34
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-27 18:44:29 +0100

    Сестрорецк в 2021

--- resources/views/life/trips/sestroretsk_2021.blade.php
+++ resources/views/life/trips/sestroretsk_2021.blade.php
@@ -4,125 +4,135 @@
 @ru
   <p>Отправление в Сестрорецк со станции Старая деревня в Питере.</p>
 @endru
-IMG_7937.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7937.jpg'])
 
 @ru
   <p>Схема движения поездов на финляндском направлении.</p>
 @endru
-IMG_7938.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7938.jpg'])
 
 @ru
   <p>Примерно каждый час можно добраться до Сестрорецка и обратно.</p>
 @endru
-IMG_7939.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7939.jpg'])
 
 @ru
   <p>Один путь на платформе обслуживает поезда в обе стороны.</p>
 @endru
-IMG_7940.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7940.jpg'])
 
 @ru
   <p>Вид из окна поезда во время пути.</p>
 @endru
-IMG_7941.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7941.jpg'])
 
 @ru
   <p>Поезд прибыл.</p>
 @endru
-IMG_7943.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7943.jpg'])
 
 @ru
   <p>Железнодорожный переезд.</p>
 @endru
-IMG_7944.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7944.jpg'])
 
 @ru
   <p>Инсталляции около платформы.</p>
 @endru
-IMG_7945.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7945.jpg'])
 
 @ru
   <p>Много зелени.</p>
 @endru
-IMG_7946.jpeg
-IMG_7950.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7946.jpg',
+  'IMG_7950.jpg',
+]])
 
 @ru
   <p>Канал.</p>
 @endru
-IMG_7947.jpeg
-IMG_7948.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7947.jpg',
+  'IMG_7948.jpg',
+]])
 
 @ru
   <p>Улица.</p>
 @endru
-IMG_7949.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7949.jpg'])
 
 @ru
   <p>Конюшня с форточками.</p>
 @endru
-IMG_7951.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7951.jpg'])
 
 @ru
   <p>Тропы местами заминированы.</p>
 @endru
-IMG_7952.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7952.jpg'])
 
 @ru
   <p>Лодки в канале.</p>
 @endru
-IMG_7953.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7953.jpg'])
 
 @ru
   <p>Продолжение пути вдоль канала к Финскому заливу.</p>
 @endru
-IMG_7954.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7954.jpg'])
 
 @ru
   <p>Скамейки.</p>
 @endru
-IMG_7955.jpeg
-IMG_7960.jpeg
-IMG_7961.jpeg
-IMG_7965.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7955.jpg',
+  'IMG_7960.jpg',
+  'IMG_7961.jpg',
+  'IMG_7965.jpg',
+]])
 
 @ru
   <p>Парк Дубки.</p>
 @endru
-IMG_7956.jpeg
-IMG_7957.jpeg
-IMG_7959.jpeg
-IMG_7967.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7956.jpg',
+  'IMG_7957.jpg',
+  'IMG_7959.jpg',
+  'IMG_7967.jpg',
+]])
 
 @ru
   <p>Указатели в парке.</p>
 @endru
-IMG_7958.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7958.jpg'])
 
 @ru
   <p>Финский залив.</p>
 @endru
-IMG_7962.jpeg
-IMG_7963.jpeg
-IMG_7964.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_7962.jpg',
+  'IMG_7963.jpg',
+  'IMG_7964.jpg',
+]])
 
 @ru
   <p>Аллея в парке.</p>
 @endru
-IMG_7966.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7966.jpg'])
 
 @ru
   <p>У входа в подъезд расписаны номера квартир на каждом этаже.</p>
 @endru
-IMG_7968.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7968.jpg'])
 
 @ru
   <p>Белорусские продукты в магазине.</p>
 @endru
-IMG_7969.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7969.jpg'])
 
 @ru
   <p>На платформе в ожидании обратного поезда в Санкт-Петербург.</p>
 @endru
-IMG_7970.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7970.jpg'])
 @endsection
