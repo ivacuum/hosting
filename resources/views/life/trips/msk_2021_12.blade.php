0fbb8c5a55bed4750fe79801c68b9de43c9fd0ac
Sergey Pankov <vacuum@ivacuum.ru>
2023-01-28 17:55:44 +0100

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2021_12.blade.php
@@ -0,0 +1,62 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Новогодний вагон метро.</p>
+@endru
+IMG_8903.jpeg
+
+@ru
+  <p>Украшенные улицы.</p>
+@endru
+IMG_8904.jpeg
+IMG_8929.jpeg
+IMG_8930.jpeg
+IMG_8932.jpeg
+
+@ru
+  <p>Внутри ГУМа.</p>
+@endru
+IMG_8908.jpeg
+
+@ru
+  <p>На каждом входе в ГУМ рамки металлодетекторов.</p>
+@endru
+IMG_8910.jpeg
+
+@ru
+  <p>Красная площадь украшена.</p>
+@endru
+IMG_8912.jpeg
+IMG_8924.jpeg
+
+@ru
+  <p>На ярмарке продают елки.</p>
+@endru
+IMG_8925.jpeg
+
+@ru
+  <p>Парк Зарядье.</p>
+@endru
+IMG_8926.jpeg
+
+@ru
+  <p>Березы.</p>
+@endru
+IMG_8927.jpeg
+
+@ru
+  <p>В каждом торговом центре использованы километры гирлянд.</p>
+@endru
+IMG_8931.jpeg
+
+@ru
+  <p>Подарочный ЦУМ.</p>
+@endru
+IMG_8933.jpeg
+
+@ru
+  <p>Внутри ЦУМа.</p>
+@endru
+IMG_8934.jpeg
+@endsection
