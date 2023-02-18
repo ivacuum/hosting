62042f2464804a626293647a3636034f1313ee0d
Sergey Pankov <vacuum@ivacuum.ru>
2023-02-18 22:59:34 +0300

    Нижний Новгород в 2019

--- resources/views/life/trips/nizhny-novgorod_2021.blade.php
+++ resources/views/life/trips/nizhny-novgorod_2021.blade.php
@@ -4,423 +4,457 @@
 @ru
   <p>В поезде Ласточка места напротив весьма близко друг к другу, как в электричке.</p>
 @endru
-IMG_7997.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7997.jpg'])
 
 @ru
   <p>Танки на железной дороге в городе Ковров.</p>
 @endru
-IMG_7999.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_7999.jpg'])
 
 @ru
   <p>Багажная полка наклонена, чтобы вещи не выпрыгивали с нее.</p>
 @endru
-IMG_8001.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8001.jpg'])
 
 @ru
-  <p>Поезд прибыл на московский вокзал.</p>
+  <p>Поезд прибыл на Московский вокзал.</p>
 @endru
-IMG_8002.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8002.jpg'])
 
 @ru
-  <p>Путь с центрального вокзала на остановку трамвая вдоль заборов.</p>
+  <p>Путь с центрального вокзала на остановку трамвая вдоль заборов стройки.</p>
 @endru
-IMG_8003.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8003.jpg'])
 
 @ru
-  <p>Поездка в трамвае натолкнула на мысль, что вокруг полно низкооплачиваемой работы, которая не требует длительной подготовки. Кондуктор, курьер, кассир, дворник. Но без всех этих профессий города бы не существовали в привычном нам виде.</p>
+  <p>Поездка в трамвае натолкнула на мысль, что вокруг полно низкооплачиваемой работы, которая не требует длительной подготовки. Кондуктор, курьер, кассир, дворник и т.п. Но без всех этих профессий города бы не существовали в привычном нам виде.</p>
 @endru
-IMG_8005.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8005.jpg'])
 
 @ru
   <p>Путь к арендованной квартире.</p>
 @endru
-IMG_8006.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8006.jpg'])
 
 @ru
   <p>Получение ключей от арендованной квартиры от хоста, который живет неподалеку.</p>
 @endru
-IMG_8007.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8007.jpg'])
 
 @ru
   <p>Жилой район.</p>
 @endru
-IMG_8008.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8008.jpg'])
 
 @ru
   <p>Раскопки для прокладки труб.</p>
 @endru
-IMG_8009.jpeg
-IMG_8104.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8009.jpg',
+  'IMG_8104.jpg',
+]])
 
 @ru
-  <p>На остановке перечислено какой транспорт делает здесь остановки.</p>
+  <p>На остановке перечислено какой транспорт здесь можно дождаться.</p>
 @endru
-IMG_8010.jpeg
-IMG_8013.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8010.jpg',
+  'IMG_8013.jpg',
+]])
 
 @ru
   <p>Среди информации в автобусе можно найти имена водителей и кондукторов. Проезд стоит 28 рублей.</p>
 @endru
-IMG_8011.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8011.jpg'])
 
 @ru
   <p>Табло на остановке куда-то подключается.</p>
 @endru
-IMG_8012.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8012.jpg'])
 
 @ru
   <p>1221 — год основания Нижнего Новгорода.</p>
 @endru
-IMG_8014.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8014.jpg'])
 
 @ru
   <p>При оплате банковской картой проезд дешевле на два рубля.</p>
 @endru
-IMG_8017.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8017.jpg'])
 
 @ru
   <p>Одинокий прокатный велосипед на площади.</p>
 @endru
-IMG_8018.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8018.jpg'])
 
 @ru
-  <p>Улицы обновляют к 800-летию.</p>
+  <p>Очень много улиц обновляют к 800-летию.</p>
 @endru
-IMG_8019.jpeg
-IMG_8022.jpeg
-IMG_8026.jpeg
-IMG_8027.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8019.jpg',
+  'IMG_8022.jpg',
+  'IMG_8026.jpg',
+  'IMG_8027.jpg',
+]])
 
 @ru
   <p>Подходы к набережной тоже закрыты.</p>
 @endru
-IMG_8020.jpeg
-IMG_8021.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8020.jpg',
+  'IMG_8021.jpg',
+]])
 
 @ru
   <p>Скамейка.</p>
 @endru
-IMG_8023.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8023.jpg'])
 
 @ru
   <p>Ворота, калитка и кодовая панель между ними. К калитке приделана ручка.</p>
 @endru
-IMG_8024.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8024.jpg'])
 
 @ru
-  <p>Упоминание 800-летия.</p>
+  <p>Упоминание 800-летия на фонарном столбе.</p>
 @endru
-IMG_8025.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8025.jpg'])
 
 @ru
-  <p>В парке книжные полки, лавочка и урна.</p>
+  <p>В парке книжные полки под навесом, лавочка и урна.</p>
 @endru
-IMG_8028.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8028.jpg'])
 
 @ru
   <p>Крохотная сцена.</p>
 @endru
-IMG_8029.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8029.jpg'])
 
 @ru
   <p>Пекарня занимает весь первый этаж.</p>
 @endru
-IMG_8030.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8030.jpg'])
 
 @ru
   <p>Прям на месяц попозже приехать — все было бы отремонтировано и установлено.</p>
 @endru
-IMG_8031.jpeg
-IMG_8032.jpeg
-IMG_8033.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8031.jpg',
+  'IMG_8032.jpg',
+  'IMG_8033.jpg',
+]])
 
 @ru
   <p>Огромная закругленная скамейка.</p>
 @endru
-IMG_8034.jpeg
-IMG_8036.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8034.jpg',
+  'IMG_8036.jpg',
+]])
 
 @ru
   <p>Фонтан.</p>
 @endru
-IMG_8035.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8035.jpg'])
 
 @ru
   <p>Длинная прямая скамейка и напольная карта местности перед ней.</p>
 @endru
-IMG_8037.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8037.jpg'])
 
 @ru
   <p>Длинная пешеходная Большая Покровская улица.</p>
 @endru
-IMG_8038.jpeg
-IMG_8042.jpeg
-IMG_8094.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8038.jpg',
+  'IMG_8042.jpg',
+  'IMG_8094.jpg',
+]])
 
 @ru
   <p>Люк.</p>
 @endru
-IMG_8039.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8039.jpg'])
 
 @ru
   <p>Скамейка вокруг дерева.</p>
 @endru
-IMG_8040.jpeg
-IMG_8041.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8040.jpg',
+  'IMG_8041.jpg',
+]])
 
 @ru
   <p>Еще скамейки.</p>
 @endru
-IMG_8043.jpeg
-IMG_8044.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8043.jpg',
+  'IMG_8044.jpg',
+]])
 
 @ru
   <p>Старый дом соседствует с новой многоэтажкой.</p>
 @endru
-IMG_8045.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8045.jpg'])
 
 @ru
   <p>Уличная табличка Холодного переулка.</p>
 @endru
-IMG_8046.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8046.jpg'])
 
 @ru
   <p>Слив сооружают.</p>
 @endru
-IMG_8047.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8047.jpg'])
 
 @ru
   <p>Кинотеатр для взрослых и детей.</p>
 @endru
-IMG_8048.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8048.jpg'])
 
 @ru
   <p>Фасад магазина подарков и сувениров.</p>
 @endru
-IMG_8049.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8049.jpg'])
 
 @ru
   <p>Магнит в цокольном этаже. Респект за наличие туалета.</p>
 @endru
-IMG_8051.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8051.jpg'])
 
 @ru
   <p>Интерьер магнита. Внутри есть столики, чтобы поесть на месте.</p>
 @endru
-IMG_8050.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8050.jpg'])
 
 @ru
   <p>Транспорт напрокат в сквере.</p>
 @endru
-IMG_8052.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8052.jpg'])
 
 @ru
   <p>Чистые скамейки в сквере.</p>
 @endru
-IMG_8053.jpeg
-IMG_8054.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8053.jpg',
+  'IMG_8054.jpg',
+]])
 
 @ru
   <p>Обосранная скамейка.</p>
 @endru
-IMG_8055.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8055.jpg'])
 
 @ru
-  <p>Лампа уличного освещения.</p>
+  <p>Светодиодная лампа уличного освещения.</p>
 @endru
-IMG_8057.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8057.jpg'])
 
 @ru
   <p>Стеклянные здания.</p>
 @endru
-IMG_8058.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8058.jpg'])
 
 @ru
-  <p>Сломанная или недоделанная часть тротуара. Без должной осторожности во время пути можно переломать ноги.</p>
+  <p>Сломанная или недоделанная часть тротуара. Без должной внимательности во время пути легко травмироваться.</p>
 @endru
-IMG_8059.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8059.jpg'])
 
 @ru
   <p>Скамейка со статуями.</p>
 @endru
-IMG_8060.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8060.jpg'])
 
 @ru
   <p>Станция метро. Реклама Сбер-маркета — через него хорошо заказывать дюжину ведер мороженого, чтобы всю морозилку надолго забить.</p>
 @endru
-IMG_8061.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8061.jpg'])
 
 @ru
   <p>Прежнее место пересечения проезжей части пешеходами теперь закрыто забором.</p>
 @endru
-IMG_8062.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8062.jpg'])
 
 @ru
   <p>Энергосберегающие лампы в уличных фонарях.</p>
 @endru
-IMG_8063.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8063.jpg'])
 
 @ru
   <p>Деревянный дом со старыми оконными рамами с форточкой.</p>
 @endru
-IMG_8064.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8064.jpg'])
 
 @ru
   <p>Схема газа и предостережение.</p>
 @endru
-IMG_8065.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8065.jpg'])
 
 @ru
   <p>Заросший дом.</p>
 @endru
-IMG_8066.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8066.jpg'])
 
 @ru
   <p>Советские рисунки мелкой плиткой.</p>
 @endru
-IMG_8067.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8067.jpg'])
 
 @ru
   <p>Пешеходный мост через дорогу.</p>
 @endru
-IMG_8068.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8068.jpg'])
 
 @ru
   <p>Дорога.</p>
 @endru
-IMG_8069.jpeg
-IMG_8070.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8069.jpg',
+  'IMG_8070.jpg',
+]])
 
 @ru
   <p>Тротуар еще не готов.</p>
 @endru
-IMG_8071.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8071.jpg'])
 
 @ru
   <p>А ближе к реке уже готов.</p>
 @endru
-IMG_8072.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8072.jpg'])
 
 @ru
   <p>Панорамы.</p>
 @endru
-IMG_8073.jpeg
-IMG_8074.jpeg
-IMG_8075.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8073.jpg',
+  'IMG_8074.jpg',
+  'IMG_8075.jpg',
+]])
 
 @ru
   <p>Спуск.</p>
 @endru
-IMG_8076.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8076.jpg'])
 
 @ru
-  <p>Знак со схемой движения. Однако, проезд слева перекрыт.</p>
+  <p>Знак со схемой движения. Однако, проезд слева сейчас перекрыт.</p>
 @endru
-IMG_8077.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8077.jpg'])
 
 @ru
   <p>Нижне-Волжская набережная.</p>
 @endru
-IMG_8078.jpeg
-IMG_8079.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8078.jpg',
+  'IMG_8079.jpg',
+]])
 
 @ru
   <p>Освежают фасады.</p>
 @endru
-IMG_8080.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8080.jpg'])
 
 @ru
   <p>Здание речного вокзала.</p>
 @endru
-IMG_8082.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8082.jpg'])
 
 @ru
   <p>Пространство перед вокзалом.</p>
 @endru
-IMG_8083.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8083.jpg'])
 
 @ru
   <p>Улица в перекрытом под фестиваль «Столицы закатов» районе пустует.</p>
 @endru
-IMG_8084.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8084.jpg'])
 
 @ru
   <p>Переулки.</p>
 @endru
-IMG_8085.jpeg
-IMG_8086.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8085.jpg',
+  'IMG_8086.jpg',
+]])
 
 @ru
   <p>Поверхность разрисована.</p>
 @endru
-IMG_8087.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8087.jpg'])
 
 @ru
   <p>Вот как это называется — центр города на капитальном ремонте. Закончат как раз через месяц.</p>
 @endru
-IMG_8088.jpeg
-IMG_8089.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8088.jpg',
+  'IMG_8089.jpg',
+]])
 
 @ru
   <p>Проход к Кремлю тоже закрыт со всех сторон.</p>
 @endru
-IMG_8090.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8090.jpg'])
 
 @ru
   <p>П-образная скамейка.</p>
 @endru
-IMG_8091.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8091.jpg'])
 
 @ru
   <p>Периметр Кремля.</p>
 @endru
-IMG_8092.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8092.jpg'])
 
 @ru
   <p>Ступеньки наверх.</p>
 @endru
-IMG_8093.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8093.jpg'])
 
 @ru
   <p>Широкая скамейка.</p>
 @endru
-IMG_8097.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8097.jpg'])
 
 @ru
   <p>Вход на станцию метро в пустынном районе без точек притяжения.</p>
 @endru
-IMG_8098.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8098.jpg'])
 
 @ru
   <p>Автобусная остановка без павильона.</p>
 @endru
-IMG_8099.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8099.jpg'])
 
 @ru
   <p>Пляж пользуется популярностью.</p>
 @endru
-IMG_8100.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8100.jpg'])
 
 @ru
   <p>Надвигается дождь.</p>
 @endru
-IMG_8102.jpeg
-IMG_8101.jpeg
-IMG_8103.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8102.jpg',
+  'IMG_8101.jpg',
+  'IMG_8103.jpg',
+]])
 
 @ru
   <p>Табло прибытия и отправления поездов на Московском вокзале.</p>
 @endru
-IMG_8105.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8105.jpg'])
 
 @ru
   <p>Автоматические камеры хранения.</p>
 @endru
-IMG_8106.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8106.jpg'])
 
 @ru
   <p>Трамвай.</p>
 @endru
-IMG_8170.jpeg
-IMG_8168.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8170.jpg',
+  'IMG_8168.jpg',
+]])
 @endsection
