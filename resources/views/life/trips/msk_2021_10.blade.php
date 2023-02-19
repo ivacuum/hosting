68196e1c42a4aa041edd0f4161ee0192188466cc
Sergey Pankov <vacuum@ivacuum.ru>
2023-02-19 22:06:21 +0300

    Москва в октябре 2021

--- resources/views/life/trips/msk_2021_10.blade.php
+++ resources/views/life/trips/msk_2021_10.blade.php
@@ -4,47 +4,49 @@
 @ru
   <p>В первом классе брянского поезда Иван Паристый дают воду и немного еды.</p>
 @endru
-IMG_8750.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8750.jpg'])
 
 @ru
   <p>Компоновка сидений 2+1.</p>
 @endru
-IMG_8751.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8751.jpg'])
 
 @ru
   <p>Электробусы заряжаются на парковке возле Киевского вокзала.</p>
 @endru
-IMG_8753.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8753.jpg'])
 
 @ru
   <p>Здание гостиницы Славянская.</p>
 @endru
-IMG_8754.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8754.jpg'])
 
 @ru
   <p>Лестница на мосту в отсутствие работающего эскалатора.</p>
 @endru
-IMG_8755.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8755.jpg'])
 
 @ru
-  <p>Оформление моста.</p>
+  <p>Внутреннее оформление моста.</p>
 @endru
-IMG_8756.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8756.jpg'])
 
 @ru
   <p>Осенние краски.</p>
 @endru
-IMG_8758.jpeg
-IMG_8759.jpeg
-IMG_8760.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8758.jpg',
+  'IMG_8759.jpg',
+  'IMG_8760.jpg',
+]])
 
 @ru
   <p>Напомнило карту организаций в <a class="link" href="onomichi.2019#2gis">Японии</a>.</p>
 @endru
-IMG_8761.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8761.jpg'])
 
 @ru
   <p>Хотя это были энциклопедии на полке книжного магазина.</p>
 @endru
-IMG_8762.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8762.jpg'])
 @endsection
