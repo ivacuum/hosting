d96f5465f8381668b5792f2f60c6c269b4ebdaa4
Sergey Pankov <vacuum@ivacuum.ru>
2023-02-19 11:52:45 +0300

    Москва в июле 2021

--- resources/views/life/trips/msk_2021_07_19.blade.php
+++ resources/views/life/trips/msk_2021_07_19.blade.php
@@ -4,118 +4,120 @@
 @ru
   <p>Восточный вокзал. На него перенесли большую часть поездов с Курского вокзала.</p>
 @endru
-IMG_8172.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8172.jpg'])
 
 @ru
   <p>Станция метро Черкизовская.</p>
 @endru
-IMG_8173.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8173.jpg'])
 
 @ru
   <p>Самый дорогой деловой обед — 780 ₽.</p>
 @endru
-IMG_8174.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8174.jpg'])
 
 @ru
   <p>По меню за ту же стоимость только один суп.</p>
 @endru
-IMG_8175.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8175.jpg'])
 
 @ru
   <p>У РЖД в районе дня рождения именинник имеет возможность покупать билеты по спецтарифу, дающему десятипроцентную скидку.</p>
 @endru
-IMG_8176.png
+@include('tpl.pic-arbitrary-2x', ['pic' => 'IMG_8176.jpg', 'w' => 414, 'h' => 896])
 
 @ru
   <p>В городском автобусе есть второе водительское место. Для обучения новичков?</p>
 @endru
-IMG_8178.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8178.jpg'])
 
 @ru
   <p>Сезон заборов преследует от самого Нижнего Новгорода. Парк закрыт на год.</p>
 @endru
-IMG_8179.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8179.jpg'])
 
 @ru
   <p>Схематичное расположение газа. Телефон газовой службы — 104. Телефоны 01, 02, 03 и 04 уже давненько не встречались.</p>
 @endru
-IMG_8180.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8180.jpg'])
 
 @ru
-  <p>Дом напротив закрытого парка словно намок из-за выбора цветов плитки.</p>
+  <p>Дом напротив закрытого парка словно намок от дождя из-за выбора цветов плитки.</p>
 @endru
-IMG_8181.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8181.jpg'])
 
 @ru
-  <p>Зелень.</p>
+  <p>Зеленая улица.</p>
 @endru
-IMG_8182.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8182.jpg'])
 
 @ru
-  <p>Прозрачный подъезд.</p>
+  <p>Прозрачная дверь в подъезд дома.</p>
 @endru
-IMG_8184.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8184.jpg'])
 
 @ru
   <p>Частично окрашенный дом.</p>
 @endru
-IMG_8185.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8185.jpg'])
 
 @ru
   <p>Парк Красная Пресня.</p>
 @endru
-IMG_8187.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8187.jpg'])
 
 @ru
   <p>Навигация в парке.</p>
 @endru
-IMG_8188.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8188.jpg'])
 
 @ru
   <p>Хорошо откинутая спинка скамейки.</p>
 @endru
-IMG_8190.jpeg
-IMG_8189.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_8190.jpg',
+  'IMG_8189.jpg',
+]])
 
 @ru
   <p>Первые этажи небоскребов не спешат завершать.</p>
 @endru
-IMG_8192.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8192.jpg'])
 
 @ru
   <p>Набережная.</p>
 @endru
-IMG_8193.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8193.jpg'])
 
 @ru
   <p>Лежаки на набережной.</p>
 @endru
-IMG_8194.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8194.jpg'])
 
 @ru
   <p>Множество лестниц.</p>
 @endru
-IMG_8195.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8195.jpg'])
 
 @ru
-  <p>А мог бы быть и эскалатор, как в Барселоне.</p>
+  <p>А мог бы быть и эскалатор, как в <a class="link" href="barcelona.2016#escalator">Барселоне</a>.</p>
 @endru
-IMG_8197.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8197.jpg'])
 
 @ru
   <p>Облачное небо за закрученным небоскребом.</p>
 @endru
-IMG_8198.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8198.jpg'])
 
 @ru
   <p>Москва-сити.</p>
 @endru
-IMG_8199.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8199.jpg'])
 
 @ru
   <p>Хорошо тем, у кого окна на запад — наблюдать закат.</p>
 @endru
-IMG_8201.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_8201.jpg'])
 
 @ru
   <p>С уменьшением температуры чувствовалось как легче становится и рюкзак.</p>
