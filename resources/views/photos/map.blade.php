dfcc70dac66be7855945f5cc39fc6a98b06086e6
Sergey Pankov <vacuum@ivacuum.ru>
2023-03-05 13:52:56 +0300

    Поправлен показ фото на карте по прямо ссылке на фото

--- resources/views/photos/map.blade.php
+++ resources/views/photos/map.blade.php
@@ -1,3 +1,5 @@
+<?php /** @var \App\Photo $photo */ ?>
+
 @extends('photos.base')
 
 @section('content')
@@ -5,8 +7,8 @@
   id="photos_map"
   class="photo-map-container -mx-4 sm:mx-0 -mb-6"
   data-action="{{ fullUrl() }}"
-  data-lat="{{ $photo->lat ?? Request::input('lat', 52) }}"
-  data-lon="{{ $photo->lon ?? Request::input('lon', 30) }}"
+  data-lat="{{ $photo->point->lat ?? Request::input('lat', 52) }}"
+  data-lon="{{ $photo->point->lon ?? Request::input('lon', 30) }}"
   data-zoom="{{ $photo !== null ? 17 : Request::input('zoom', 4) }}"
   data-clusterize="{{ Request::input('clusterize', 1) }}"
   data-cluster-size="{{ Request::input('cluster-size', 64) }}"
