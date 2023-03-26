a840659a78120b218e4690214cf514e26ca376fe
Sergey Pankov <vacuum@ivacuum.ru>
2023-03-26 09:50:18 +0300

    Пенза в 2022

--- resources/views/life/trips/penza_2022.blade.php
+++ resources/views/life/trips/penza_2022.blade.php
@@ -4,313 +4,327 @@
 @ru
   <p>В Домодедово теперь цивилизованный проход от аэроэкспресса в аэропорт.</p>
 @endru
-IMG_9388.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9388.jpg'])
 
 @ru
   <p>По бортовому номеру самолета RA-73027 через сервис <a href="https://www.flightradar24.com/data/aircraft/ra-73027" class="link">flightradar24</a> можно пробить чей он и куда летает. Оказалось, что этот самолет отправился в Сабетту — вахтовый поселок на крайнем севере России, где сжижают природный газ.</p>
 @endru
-IMG_9389.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9389.jpg'])
 
 @ru
-  <p>Самолеты стоят в поле без дела. Отправлены на запчасти?</p>
+  <p>Самолеты стоят в поле без дела. Отправлены на запчасти в качестве борьбы с санкциями?</p>
 @endru
-IMG_9390.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9390.jpg'])
 
 @ru
   <p>Пензенский аэропорт.</p>
 @endru
-IMG_9391.jpeg
-IMG_9392.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9391.jpg',
+  'IMG_9392.jpg',
+]])
 
 @ru
   <p>Фонтанная площадь.</p>
 @endru
-IMG_9393.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9393.jpg'])
 
 @ru
   <p>Сами фонтаны.</p>
 @endru
-IMG_9394.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9394.jpg'])
 
 @ru
   <p>Улица Московская.</p>
 @endru
-IMG_9395.jpeg
-IMG_9413.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9395.jpg',
+  'IMG_9413.jpg',
+]])
 
 @ru
-  <p>Люк подсказывает примерный возраст Пензы — более 350 лет.</p>
+  <p>Люк подсказывает примерный возраст Пензы — не менее 350 лет.</p>
 @endru
-IMG_9397.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9397.jpg'])
 
 @ru
   <p>Река Сура.</p>
 @endru
-IMG_9398.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9398.jpg'])
 
 @ru
   <p>Мост через реку.</p>
 @endru
-IMG_9399.jpeg
-IMG_9401.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9399.jpg',
+  'IMG_9401.jpg',
+]])
 
 @ru
-  <p>На таком узком пандусе вдвоем с колясками не разойтись.</p>
+  <p>На таком узком пандусе справа вдвоем с колясками не разойтись.</p>
 @endru
-IMG_9400.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9400.jpg'])
 
 @ru
   <p>Доступные для аренды самокаты.</p>
 @endru
-IMG_9402.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9402.jpg'])
 
 @ru
   <p>Скамейка и урна.</p>
 @endru
-IMG_9403.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9403.jpg'])
 
 @ru
   <p>Часы показывают правильное время.</p>
 @endru
-IMG_9404.jpeg
-IMG_9441.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9404.jpg',
+  'IMG_9441.jpg',
+]])
 
 @ru
   <p>Часы показывают неправильное время.</p>
 @endru
-IMG_9415.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9415.jpg'])
 
 @ru
   <p>Скамейки на площади Ленина.</p>
 @endru
-IMG_9405.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9405.jpg'])
 
 @ru
-  <p>Здание правильтества напомнило таковое в Великом Новгороде.</p>
+  <p>Здание правильтества напомнило таковое в <a class="link" href="veliky-novgorod.2021#government">Великом Новгороде</a>.</p>
 @endru
-IMG_9406.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9406.jpg'])
 
 @ru
-  <p>Перед многими пешеходными переходами на плитке размещена памятка.</p>
+  <p>Перед многими пешеходными переходами на плитке размещена памятка безопасности.</p>
 @endru
-IMG_9407.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9407.jpg'])
 
 @ru
   <p>Кулинарные рецепты от магазина Вкусвилл.</p>
 @endru
-IMG_9408.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9408.jpg'])
 
 @ru
   <p>Уличная табличка.</p>
 @endru
-IMG_9412.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9412.jpg'])
 
 @ru
   <p>Одиночное место в тени.</p>
 @endru
-IMG_9414.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9414.jpg'])
 
 @ru
   <p>Уличная навигация.</p>
 @endru
-IMG_9416.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9416.jpg'])
 
 @ru
   <p>Люк.</p>
 @endru
-IMG_9417.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9417.jpg'])
 
 @ru
   <p>Собор.</p>
 @endru
-IMG_9418.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9418.jpg'])
 
 @ru
   <p>Сквер у собора.</p>
 @endru
-IMG_9419.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9419.jpg'])
 
 @ru
   <p>Взгляд зацепился за борца по имени Попа Ж. в третьей колонке.</p>
 @endru
-IMG_9420.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9420.jpg'])
 
 @ru
   <p>Церковь.</p>
 @endru
-IMG_9421.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9421.jpg'])
 
 @ru
   <p>Губернаторский дом.</p>
 @endru
-IMG_9422.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9422.jpg'])
 
 @ru
   <p>Фонтан.</p>
 @endru
-IMG_9423.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9423.jpg'])
 
 @ru
   <p>На скамейках подписано в честь кого сквер.</p>
 @endru
-IMG_9424.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9424.jpg'])
 
 @ru
   <p>Водосточная труба закрывает уличную табличку.</p>
 @endru
-IMG_9425.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9425.jpg'])
 
 @ru
   <p>Березки.</p>
 @endru
-IMG_9426.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9426.jpg'])
 
 @ru
   <p>Корпус пензенского государственного университета.</p>
 @endru
-IMG_9428.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9428.jpg'])
 
 @ru
   <p>Центральный парк.</p>
 @endru
-IMG_9429.jpeg
-IMG_9430.jpeg
-IMG_9432.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9429.jpg',
+  'IMG_9430.jpg',
+  'IMG_9432.jpg',
+]])
 
 @ru
   <p>Батут со страховкой для детей.</p>
 @endru
-IMG_9431.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9431.jpg'])
 
 @ru
   <p>Местами Пенза весьма рельефный город.</p>
 @endru
-IMG_9434.jpeg
-IMG_9435.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9434.jpg',
+  'IMG_9435.jpg',
+]])
 
 @ru
   <p>Памятник афганские ворота.</p>
 @endru
-IMG_9437.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9437.jpg'])
 
 @ru
   <p>Один воробей кормит второго.</p>
 @endru
-IMG_9438.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9438.jpg'])
 
 @ru
   <p>Высотки на широкой улице Пушкина.</p>
 @endru
-IMG_9440.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9440.jpg'])
 
 @ru
-  <p>Маршруты на автовокзале. Непростой выбор поехать в Саранск или в Саратов. Неочевидно, что в Саратов местное время плюс один час, поэтому время в пути указывают на час больше. Возможно, это первый раз, когда внутри страны на автобусе пересекаю часовые пояса.</p>
+  <p>Маршруты на автовокзале. Непростой выбор поехать в Саранск или в Саратов. Неочевидно, что в Саратов местное время плюс один час, поэтому время в пути указывают на час больше. Это второй раз, когда внутри страны на автобусе пересекаю часовые пояса. Первый был в автобусном туре из Москвы в Самарскую область на фестиваль <a class="link" href="rammstein.2013">Рок над Волгой</a>.</p>
 @endru
-IMG_9443.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9443.jpg'])
 
 @ru
   <p>Кассы автовокзала. Под кассами реклама сайта и приложений вокзала.</p>
 @endru
-IMG_9444.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9444.jpg'])
 
 @ru
   <p>Зал ожидания.</p>
 @endru
-IMG_9445.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9445.jpg'])
 
 @ru
   <p>Табло рейсов.</p>
 @endru
-IMG_9446.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9446.jpg'])
 
 @ru
   <p>Уличные таблички.</p>
 @endru
-IMG_9447.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9447.jpg'])
 
 @ru
   <p>Скоро будет уложена плитка, а пока просьба перейти на противоположную сторону.</p>
 @endru
-IMG_9448.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9448.jpg'])
 
 @ru
   <p>Сегодня +29&deg;C. Припекаемся.</p>
 @endru
-IMG_9449.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9449.jpg'])
 
 @ru
   <p>Ресторан грузинской кухни.</p>
 @endru
-IMG_9451.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9451.jpg'])
 
 @ru
   <p>Здание арбитражного суда.</p>
 @endru
-IMG_9452.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9452.jpg'])
 
 @ru
   <p>Пляж терновского затона. Затон — речной залив.</p>
 @endru
-IMG_9453.jpeg
-IMG_9454.jpeg
+@include('tpl.fotorama-2x', ['pics' => [
+  'IMG_9453.jpg',
+  'IMG_9454.jpg',
+]])
 
 @ru
   <p>Панорама кольцевой набережной.</p>
 @endru
-IMG_9456.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9456.jpg'])
 
 @ru
   <p>Человек рыбачит.</p>
 @endru
-IMG_9457.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9457.jpg'])
 
 @ru
   <p>Паровозик везет пассажиров.</p>
 @endru
-IMG_9459.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9459.jpg'])
 
 @ru
   <p>Солнце заходит.</p>
 @endru
-IMG_9463.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9463.jpg'])
 
 @ru
   <p>Снег тает. Фонтан стреляет.</p>
 @endru
-IMG_9464.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9464.jpg'])
 
 @ru
   <p>Гигантская скамейка.</p>
 @endru
-IMG_9465.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9465.jpg'])
 
 @ru
   <p>Бассейн Дельфин. В <a href="kaluga" class="link">Калуге</a> тоже одноименный есть.</p>
 @endru
-IMG_9466.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9466.jpg'])
 
 @ru
   <p>Еще закат.</p>
 @endru
-IMG_9467.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9467.jpg'])
 
 @ru
   <p>Салют в честь дня молодежи.</p>
 @endru
-IMG_9468.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9468.jpg'])
 
 @ru
   <p>Какими тонкими стали светофоры.</p>
 @endru
-IMG_9607.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9607.jpg'])
 
 @ru
-  <p>История с краном.</p>
+  <p>Первая проба арендовать квартиру через Островок. В квартире кран в ванной комнате удивительным образом находился снаружи раковины. Если его не закрыть до конца, то он лил под себя, а так как раковина не могла его спасти, то вода лилась на пол и отправлялась к соседям снизу.</p>
 @endru
-IMG_9608.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9608.jpg'])
 
 @ru
   <p>Зал ожидающих посадки в самолет.</p>
 @endru
-IMG_9611.jpeg
+@include('tpl.pic-2x', ['pic' => 'IMG_9611.jpg'])
 @endsection
