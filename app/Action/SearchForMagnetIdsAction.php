5fa3b63b79f88dc203586cd64b665a4606ecbc88
Sergey Pankov <vacuum@ivacuum.ru>
2023-04-09 19:01:39 +0300

    Учет отсутствия категории

--- app/Action/SearchForMagnetIdsAction.php
+++ app/Action/SearchForMagnetIdsAction.php
@@ -9,7 +9,7 @@ use Laravel\Scout\Builder;
 
 class SearchForMagnetIdsAction
 {
-    public function execute(string $q, int $categoryId, bool $fulltext): array
+    public function execute(string $q, int|null $categoryId, bool $fulltext): array
     {
         return match (config('scout.driver')) {
             'sphinx' => $this->sphinx($q, $categoryId, $fulltext),
@@ -17,14 +17,14 @@ class SearchForMagnetIdsAction
         };
     }
 
-    private function meilisearch(string $q, int $categoryId)
+    private function meilisearch(string $q, int|null $categoryId)
     {
         return Magnet::search($q)
             ->when($categoryId, fn (Builder $query) => $query->whereIn('category_id', \TorrentCategoryHelper::selfAndDescendantsIds($categoryId)))
             ->raw()['hits'];
     }
 
-    private function sphinx(string $q, int $categoryId, bool $fulltext)
+    private function sphinx(string $q, int|null $categoryId, bool $fulltext)
     {
         return Magnet::search($q, function (SphinxQL $builder) use ($categoryId, $fulltext, $q) {
             $builder = $builder->match(
