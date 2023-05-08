9676466dd2e96947b565f5b1009bf4531749d7a4
Sergei Pankov <vacuum@ivacuum.ru>
2023-05-08 10:17:14 +0300

    Заготовка о поездке в Дубай

---
+++ resources/views/life/trips/dubai_2023_02_09.blade.php
@@ -0,0 +1,18 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Вход в отель.</p>
+@endru
+IMG_2260.jpeg
+
+@ru
+  <p>Новый коворкинг, новая встреча с коллегами.</p>
+@endru
+IMG_2261.jpeg
+
+@ru
+  <p>Справа стоим, слева идем.</p>
+@endru
+IMG_2263.jpeg
+@endsection
