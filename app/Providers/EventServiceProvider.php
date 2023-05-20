2afaddbe93981d157b1326197ccf001242ed8ac1
Sergei Pankov <vacuum@ivacuum.ru>
2023-05-20 14:48:07 +0300

    Автоматическое обнаружение обработчиков событий

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,38 +1,21 @@
 <?php namespace App\Providers;
 
-use App\Events;
-use App\Listeners;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-        Events\ChatMessageCreated::class => [Listeners\TelegramChatMessage::class],
-        Events\CommentPublished::class => [Listeners\NotifyUsersAboutComment::class],
-        Events\DomainWhoisUpdated::class => [Listeners\EmailWhoisChanges::class],
-        Events\IssueCreated::class => [Listeners\TelegramIssue::class],
-        Events\MagnetAddedAnonymously::class => [Listeners\TelegramAnonymousMagnet::class],
-        Events\TypoReceived::class => [Listeners\TelegramTypo::class],
-
-        'Illuminate\Auth\Events\Login' => [
-            Listeners\UserEmptySalt::class,
-            Listeners\LogUserLogin::class,
-        ],
-
         'Illuminate\Foundation\Events\LocaleUpdated' => [
             'Ivacuum\Generic\Listeners\SetLocale',
-        ],
-
-        'Illuminate\Http\Client\Events\ConnectionFailed' => [
-            Listeners\LogHttpConnectionFailed::class,
-        ],
-
-        'Illuminate\Http\Client\Events\ResponseReceived' => [
-            Listeners\LogExternalHttpRequest::class,
         ],
 
         'Ivacuum\Generic\Events\MailReported' => [
             'Ivacuum\Generic\Listeners\TelegramMailReport',
         ],
     ];
+
+    public function shouldDiscoverEvents()
+    {
+        return true;
+    }
 }
