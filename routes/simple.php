93deb08277b090a00f4e5a235d446085dcfe0fcb
Sergei Pankov <vacuum@ivacuum.ru>
2024-04-22 17:04:55 +0300

    Соответствующие коды ошибок

--- routes/simple.php
+++ routes/simple.php
@@ -2,11 +2,11 @@
 
 use App\Http\Controllers;
 
-Route::view('401', 'errors.401');
-Route::view('403', 'errors.403');
-Route::view('404', 'errors.404');
-Route::view('500', 'errors.500');
-Route::view('503', 'errors.503');
+Route::view('401', 'errors.401', status: 401);
+Route::view('403', 'errors.403', status: 403);
+Route::view('404', 'errors.404', status: 404);
+Route::view('500', 'errors.500', status: 500);
+Route::view('503', 'errors.503', status: 503);
 
 Route::view('cv', 'cv');
 Route::get('ip', Controllers\PrintIpController::class);
