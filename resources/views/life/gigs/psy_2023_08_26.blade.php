dea01cb1cd41afd6938c7dfc2eee56aaf32c24f8
Sergei Pankov <vacuum@ivacuum.ru>
2024-04-25 19:52:24 +0200

    Черновики

---
+++ resources/views/life/gigs/psy_2023_08_26.blade.php
@@ -0,0 +1,30 @@
+@extends('life.gigs.base')
+
+{{-- Busan Asiad Auxiliary Stadium --}}
+
+@section('content')
+@ru
+  <p></p>
+@endru
+IMG_3364.jpeg
+IMG_3365.jpeg
+IMG_3367.jpeg
+IMG_3369.jpeg
+IMG_3371.jpeg
+IMG_3376.jpeg
+IMG_3377.jpeg
+IMG_3378.jpeg
+IMG_3379.jpeg
+IMG_3381.jpeg
+IMG_3382.jpeg
+IMG_3383.jpeg
+IMG_3384.jpeg
+IMG_3385.jpeg
+IMG_3386.jpeg
+IMG_3387.jpeg
+IMG_3388.jpeg
+IMG_3389.jpeg
+IMG_3390.jpeg
+IMG_3391.jpeg
+IMG_3392.jpeg
+@endsection
