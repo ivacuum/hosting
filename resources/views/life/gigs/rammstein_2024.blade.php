decb0c94489fe9fb91ea7a94db3a17001e815044
Sergei Pankov <vacuum@ivacuum.ru>
2024-06-22 00:48:33 +0300

    Заготовка о поездке на Раммштайн

---
+++ resources/views/life/gigs/rammstein_2024.blade.php
@@ -0,0 +1,191 @@
+@extends('life.gigs.base')
+
+{{-- Belgrade Ušće Park --}}
+
+@section('content')
+@ru
+  <p>Единственный концерт тура в безвизовой стране — Сербии. По ощущениям, половина посетителей прилетела сюда из РФ. Билеты купили в первый час продаж 18 октября. На предпродаже для членов фан-клуба купить билеты не удалось — их слишком быстро разобрали. Билеты на 24 мая разобрали в первый же день, поэтому вскоре анонсировали второй концерт 25 мая в том же месте.</p>
+@endru
+
+Так как больше половины компании попали на Раммштайн впервые, то встал вопрос что непередаваемое они испытают вживую. Их живой звук не передать словами и наушниками.
+
+@ru
+  <p>Место проведения — парк Ушче.</p>
+@endru
+IMG_9340.jpeg
+
+@ru
+  <p>По углам огороженной забором территории расположены камеры хранения.</p>
+@endru
+IMG_9341.jpeg
+
+@ru
+  <p>К сожалению, утром прошел дождь. Земля не успела высохнуть.</p>
+@endru
+IMG_9342.jpeg
+
+@ru
+  <p>Заходящим справа раздали фиолетовые браслеты, как в отелях с ол инклюзивом.</p>
+@endru
+IMG_9343.jpeg
+
+@ru
+  <p>В продаже мерч. Цены удивительно высокие — 35 евро на концерте против 25 евро на сайте или 21 евро для членов фан-клуба.</p>
+@endru
+IMG_9344.jpeg
+
+@ru
+  <p>Мякотка.</p>
+@endru
+IMG_9345.jpeg
+IMG_9346.jpeg
+
+@ru
+  <p>Правый проход в фан-зону.</p>
+@endru
+IMG_9347.jpeg
+
+@ru
+  <p>Сцена скраю.</p>
+@endru
+IMG_9349.jpeg
+
+@ru
+  <p>Металлический настил спас почти всю фан-зону от грязевой ванны. Танцполу не так повезло.</p>
+@endru
+IMG_9350.jpeg
+
+@ru
+  <p>Плотный слой опилок на траве.</p>
+@endru
+IMG_9351.jpeg
+
+@ru
+  <p>Центральное расположение, чтобы насладиться световым шоу. Остается только концерту начаться концерт после захода солнца. А то в Дрездене из-за периода тишины с 22:00 половину концерта провели при солнечном свете, что сильно подпортило визуальную составляющую шоу.</p>
+@endru
+IMG_9353.jpeg
+
+@ru
+  <p>В 19:13 трибуны только начинают заполняться. Если судить по первому концерту тура в Праге, то разогрев ожидается в 19:30 и Раммштайн в 20:15.</p>
+@endru
+IMG_9352.jpeg
+
+@ru
+  <p>Обувь дошедших до фан-зоны.</p>
+@endru
+IMG_9355.jpeg
+
+@ru
+  <p>Шоу началось в 21:00, когда уже стало совсем темно. Здорово, что световое шоу раскрылось на все сто. Плохо, что ради последнего поезда до Нови Сада людям пришлось покидать концерт за несколько песен до конца.</p>
+@endru
+
+@ru
+  <p>Исполнение песни Engel на малой сцене.</p>
+@endru
+IMG_9358.jpeg
+
+@ru
+  <p>На трибунах загорелись фонарики.</p>
+@endru
+IMG_9357.jpeg
+
+@ru
+  <p>Выход на бис состоялся после 23:00. Кадры песни Rammstein.</p>
+@endru
+IMG_9360.jpeg
+IMG_9362.jpeg
+IMG_9363.jpeg
+IMG_9364.jpeg
+IMG_9365.jpeg
+IMG_9366.jpeg
+IMG_9367.jpeg
+IMG_9368.jpeg
+IMG_9369.jpeg
+
+@ru
+  <p>Последняя песня концерта — Adieu.</p>
+@endru
+IMG_9373.jpeg
+
+@ru
+  <p>Группа на лифте поднимается наверх после исполнения всех песен.</p>
+@endru
+IMG_9376.jpeg
+IMG_9375.jpeg
+
+@ru
+  <p>На этом концерт заканчивается и начинается преодоления засасывающего болота.</p>
+@endru
+IMG_9380.jpeg
+
+@ru
+  <p>Болото оставило свои следы на память.</p>
+@endru
+IMG_9382.jpeg
+
+@ru
+  <p>По грязной обуви затем все выходные в Белграде можно было опознать людей, которые ходили на концерт в пятницу или субботу.</p>
+@endru
+IMG_9383.jpeg
+
+@ru
+  <p>Так как все жилье по адекватным ценам в Белграде закончилось еще за месяцы до концерта, то был подобран вариант в Нови Саде. На последний поезд попасть без ущерба концерту не удалось, поэтому первый утренний поезд доставил до жилья.</p>
+@endru
+IMG_9386.jpeg
+
+@ru
+  <p>Браслет фан-зоны.</p>
+@endru
+IMG_9387.jpeg
+
+@component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/rammstein.2024.05.24.jpg'])
+  <ol>
+    <li>Ramm 4</li>
+    <li>Links 2-3-4</li>
+    <li>Keine Lust</li>
+    <li>Sehnsucht</li>
+    <li>Asche zu Asche</li>
+    <li>Mein Herz brennt</li>
+    <li>Puppe</li>
+    <li>Wiener Blut</li>
+    <li>Zeit</li>
+    <li>Deutschland <span class="text-sm text-muted">Remix by Richard Z. Kruspe</span></li>
+    <li>Deutschland</li>
+    <li>Radio</li>
+    <li>Mein Teil</li>
+    <li>Du hast</li>
+    <li>Sonne</li>
+    <li>Engel <span class="text-sm text-muted">Piano version with Abélard performed on B-Stage</span></li>
+    <li>Ausländer</li>
+    <li>Du riechst so gut</li>
+    <li>Pussy</li>
+    <li>Ich will</li>
+    <li>Rammstein</li>
+    <li>Adieu</li>
+  </ol>
+@endcomponent
+
+Du hast from drone
+https://www.youtube.com/watch?v=CFcbnLX-O7Y
+
+Sonne from drone
+https://www.youtube.com/watch?v=ySyM-mCqtsU
+
+Месиво тяжелая техника сделала.
+https://www.youtube.com/watch?v=y1CxmKRBHxo
+
+@ru
+  <p>Видеозапись концерта.</p>
+@en
+  <p>Video of the show.</p>
+@endru
+{{-- Фан-зона --}}
+<livewire:youtube title="Rammstein 2024, Ušće Park, Belgrade, Serbia" v="xmx69LOrCGk"/>
+{{-- Трибуны --}}
+<livewire:youtube title="Rammstein 2024, Ušće Park, Belgrade, Serbia" v="pLQzlRpUhlI"/>
+
+<h3 class="font-medium text-2xl mb-2 mt-12">@ru Бонусные материалы @en Bonus materials @endru</h3>
+<ul>
+  <li><a class="link" href="https://www.instagram.com/p/B0jg1buAmi2/">@ru Видео из инстаграма Тилля @en Video from Till's Instagram @endru</a></li>
+</ul>
+@endsection
