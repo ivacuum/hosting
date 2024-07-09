ade202136199755c758e2ced2eee74a38e43c414
Sergei Pankov <vacuum@ivacuum.ru>
2024-07-09 21:06:00 +0900

    Заготовки о концертах

---
+++ resources/views/life/gigs/psy_2024_07_06.blade.php
@@ -0,0 +1,98 @@
+@extends('life.gigs.base')
+
+{{-- Gwangju World Cup Stadium --}}
+
+@section('content')
+@ru
+  <p></p>
+@endru
+IMG_9855.jpeg
+IMG_9860.jpeg
+IMG_9861.jpeg
+IMG_9862.jpeg
+IMG_9863.jpeg
+IMG_9864.jpeg
+IMG_9865.jpeg
+IMG_9866.jpeg
+IMG_9867.jpeg
+IMG_9868.jpeg
+IMG_9869.jpeg
+IMG_9870.jpeg
+IMG_9871.jpeg
+IMG_9872.jpeg
+IMG_9873.jpeg
+IMG_9875.jpeg
+IMG_9876.jpeg
+IMG_9877.jpeg
+IMG_9878.jpeg
+IMG_9879.jpeg
+IMG_9880.jpeg
+IMG_9882.jpeg
+IMG_9883.jpeg
+
+@component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/psy.2024.07.06.jpg'])
+  <h5 class="font-medium text-lg mb-1">@ru Сет @en Set @endru 1</h5>
+  <ol class="list-inside pl-0">
+    <li>새 <span class="text-sm text-muted">Bird</span></li>
+    <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
+    <li>감동이야 <span class="text-sm text-muted">You Move Me</span></li>
+    <li>New Face</li>
+    <li>오늘밤 새 <span class="text-sm text-muted">All Night Long</span></li>
+    <li>어땠을까 <span class="text-sm text-muted">What Would Have Been</span></li>
+    <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
+
+    <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">@ru Сет @en Set @endru 2</h5>
+  <ol class="list-inside pl-0" start="8">
+    <li>GENTLEMAN</li>
+    <li>Right Now</li>
+    <li>낙원 <span class="text-sm text-muted">Paradise</span></li>
+    <li>DADDY</li>
+    <li>밤이 깊었네 <span class="text-sm text-muted">Sleepless</span></li>
+    <li>I LUV IT</li>
+
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">@ru Сет @en Set @endru 3</h5>
+  <ol class="list-inside pl-0" start="14">
+    <li>We are the One</li>
+    <li>흔들어 주세요 <span class="text-sm text-muted">Shake It</span></li>
+    <li>Dream</li>
+    <li>나팔바지 <span class="text-sm text-muted">Napal Baji</span></li>
+    <li>아버지 <span class="text-sm text-muted">Father</span></li>
+    <li>강남스타일 <span class="text-sm text-muted">Gangnam Style</span></li>
+    <li>That That</li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">Encore 1</h5>
+  <ol class="list-inside pl-0" start="20">
+    <li>바람났어</li>
+    <li>쏘리쏘리</li>
+    <li>Nobody</li>
+    <li>내가 제일 잘나가</li>
+    <li>Tears</li>
+    <li>FANTASTIC BABY</li>
+    <li>뱅뱅뱅</li>
+    <li>뜨거운 안녕 MR <span class="text-sm text-muted">Passionate goodbye</span></li>
+    <li>기댈곳 <span class="text-sm text-muted">Refuge</span></li>
+    <li>나는 나비</li>
+    <li>낭만고양이</li>
+    <li>아파트</li>
+    <li>그대에게</li>
+    <li>여행을 떠나요</li>
+    <li>승리를 위하여</li>
+    <li>강남스타일 by audience <span class="text-sm text-muted">Gangnam Style</span></li>
+    <li>마지막 장면 <span class="text-sm text-muted">Last Scene</span></li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">Encore 2</h5>
+  <ol class="list-inside pl-0" start="37">
+    <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
+    <li>걱정말아요 그대 <span class="text-sm text-muted">Don't Worry</span></li>
+    <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
+    <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
+  </ol>
+@endcomponent
+@endsection
---
+++ resources/views/life/gigs/psy_2024_07_07.blade.php
@@ -0,0 +1,146 @@
+@extends('life.gigs.base')
+
+{{-- Gwangju World Cup Stadium --}}
+
+@section('content')
+@ru
+  <p>Все в этот день пыталось помешать запланированному стечению обстоятельств. Сначала ежедневно работающая пекарня оказалась закрыта. Затем впервые нашедшееся через Убер такси вселило надежду, что на него можно полагаться. Затем такси домой не вызвалось. Затем время поджимало на обед. Затем первая раменная закрылась на перерыв, во второй была полная посадка и ожидание 15 минут, которых не было. Затем лоттерия не нашлась вживую, которая была на картах (не стоит верить объектам с единичными отзывами). Спас GS25 около дома, который в упор раньше не замечался. Затем несколько безуспешных вызовов такси и, наконец-то, кто-то принимает заказ. Ожидание 10 минут. Проходит пять минут, таксист словно не приближается к нам, а едет вдоль нас. Проходит еще пять минут — ситуация не меняется, он по-прежнему в десяти минутах от нас. Проходит еще пять минут — таксист начинает удаляться от нас. Отмена заказа. Последующие заказы такси тоже не увенчаются успехом. Поездка на автобусе вынуждает опоздать на вход по номеру на билете, но он хотя бы гарантированно довезет до стадиона. Приехали в итоге в пять (за час до начала концерта). Затем получение билетов и пулей на вход. Удалось пробраться в ту же яму за ограждением сбоку, как и 6 июля. То есть, можно пару тысяч входных номеров наверстать, если пойти вглубь танцпола.</p>
+@endru
+IMG_9885.jpeg
+IMG_9886.jpeg
+IMG_9902.jpeg
+IMG_9903.jpeg
+IMG_9904.jpeg
+IMG_9905.jpeg
+IMG_9906.jpeg
+IMG_9907.jpeg
+IMG_9908.jpeg
+IMG_9909.jpeg
+IMG_9910.jpeg
+IMG_9911.jpeg
+IMG_9912.jpeg
+IMG_9913.jpeg
+IMG_9914.jpeg
+IMG_9915.jpeg
+IMG_9916.jpeg
+IMG_9917.jpeg
+IMG_9918.jpeg
+IMG_9919.jpeg
+IMG_9920.jpeg
+IMG_9921.jpeg
+IMG_9922.jpeg
+IMG_9923.jpeg
+IMG_9924.jpeg
+IMG_9925.jpeg
+IMG_9926.jpeg
+IMG_9927.jpeg
+IMG_9928.jpeg
+IMG_9929.jpeg
+IMG_9930.jpeg
+IMG_9931.jpeg
+IMG_9932.jpeg
+IMG_9933.jpeg
+IMG_9934.jpeg
+IMG_9935.jpeg
+IMG_9936.jpeg
+IMG_9937.jpeg
+IMG_9938.jpeg
+IMG_9939.jpeg
+IMG_9940.jpeg
+IMG_9942.jpeg
+IMG_9943.jpeg
+IMG_9944.jpeg
+IMG_9945.jpeg
+IMG_9946.jpeg
+IMG_9947.jpeg
+IMG_9948.jpeg
+IMG_9950.jpeg
+IMG_9951.jpeg
+IMG_9952.jpeg
+IMG_9953.jpeg
+IMG_9954.jpeg
+IMG_9956.jpeg
+IMG_9957.jpeg
+IMG_9958.jpeg
+IMG_9960.jpeg
+IMG_9961.jpeg
+IMG_9962.JPG
+IMG_9963.JPG
+IMG_9964.JPG
+IMG_9965.JPG
+IMG_9966.JPG
+IMG_9967.JPG
+IMG_9968.JPG
+IMG_9970.jpeg
+IMG_9971.jpeg
+IMG_9972.jpeg
+IMG_9973.jpeg
+IMG_9974.jpeg
+IMG_9975.jpeg
+
+@component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/psy.2024.07.06.jpg'])
+  <h5 class="font-medium text-lg mb-1">@ru Сет @en Set @endru 1</h5>
+  <ol class="list-inside pl-0">
+    <li>새 <span class="text-sm text-muted">Bird</span></li>
+    <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
+    <li>감동이야 <span class="text-sm text-muted">You Move Me</span></li>
+    <li>New Face</li>
+    <li>오늘밤 새 <span class="text-sm text-muted">All Night Long</span></li>
+    <li>어땠을까 <span class="text-sm text-muted">What Would Have Been</span></li>
+    <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
+
+    <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">@ru Сет @en Set @endru 2</h5>
+  <ol class="list-inside pl-0" start="8">
+    <li>GENTLEMAN</li>
+    <li>Right Now</li>
+    <li>낙원 <span class="text-sm text-muted">Paradise</span></li>
+    <li>DADDY</li>
+    <li>밤이 깊었네 <span class="text-sm text-muted">Sleepless</span></li>
+    <li>I LUV IT</li>
+
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">@ru Сет @en Set @endru 3</h5>
+  <ol class="list-inside pl-0" start="14">
+    <li>We are the One</li>
+    <li>흔들어 주세요 <span class="text-sm text-muted">Shake It</span></li>
+    <li>Dream</li>
+    <li>나팔바지 <span class="text-sm text-muted">Napal Baji</span></li>
+    <li>아버지 <span class="text-sm text-muted">Father</span></li>
+    <li>강남스타일 <span class="text-sm text-muted">Gangnam Style</span></li>
+    <li>That That</li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">Encore 1</h5>
+  <ol class="list-inside pl-0" start="20">
+    <li>바람났어</li>
+    <li>쏘리쏘리</li>
+    <li>Nobody</li>
+    <li>내가 제일 잘나가</li>
+    <li>Tears</li>
+    <li>FANTASTIC BABY</li>
+    <li>뱅뱅뱅</li>
+    <li>뜨거운 안녕 MR <span class="text-sm text-muted">Passionate goodbye</span></li>
+    <li>기댈곳 <span class="text-sm text-muted">Refuge</span></li>
+    <li>나는 나비</li>
+    <li>낭만고양이</li>
+    <li>아파트</li>
+    <li>그대에게</li>
+    <li>여행을 떠나요</li>
+    <li>승리를 위하여</li>
+    <li>강남스타일 by audience <span class="text-sm text-muted">Gangnam Style</span></li>
+    <li>마지막 장면 <span class="text-sm text-muted">Last Scene</span></li>
+  </ol>
+
+  <h5 class="font-medium text-lg mt-4 mb-1">Encore 2</h5>
+  <ol class="list-inside pl-0" start="37">
+    <li>에술이야 <span class="text-sm text-muted">It's Art</span></li>
+    <li>챔피언 <span class="text-sm text-muted">Champion</span></li>
+    <li>걱정말아요 그대 <span class="text-sm text-muted">Don't Worry</span></li>
+    <li>연예인 <span class="text-sm text-muted">Entertainer</span></li>
+  </ol>
+@endcomponent
+@endsection
