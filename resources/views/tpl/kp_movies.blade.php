7db23a58632585a95d24b67c1aa39add18cca426
Sergei Pankov <vacuum@ivacuum.ru>
2024-08-02 21:12:15 +0300

    Новое листание фильмов

--- resources/views/tpl/kp_movies.blade.php
+++ resources/views/tpl/kp_movies.blade.php
@@ -1,17 +1,17 @@
 <?php /** @var \App\FavoriteMovie $movie */ ?>
 
 <div
-  class="grid gap-1 -mx-4 sm:mx-0 overflow-x-scroll"
-  style="grid-template-columns: repeat({{ count($movies) }}, max-content); grid-template-rows: 350px;"
+  class="grid gap-x-4 snap-x sm:mx-0 overflow-x-scroll"
+  style="grid-template-columns: repeat({{ count($movies) }}, max-content); grid-template-rows: 400px;"
 >
   @foreach ($movies as $movie)
     <a
-      class="screenshot-link tooltipped tooltipped-n"
+      class="screenshot-link snap-start tooltipped tooltipped-n"
       aria-label="{{ $movie->title_ru }} / {{ $movie->title_en }}"
       href="{{ $movie->externalLink() }}"
     >
       <img
-        class="screenshot h-[350px]"
+        class="screenshot h-[400px]"
         src="{{ $movie->cover() }}"
         loading="lazy"
         alt=""
