7f7e72d16f0c91c3a847ea3080e71872138d5b59
Sergei Pankov <vacuum@ivacuum.ru>
2024-09-22 13:13:57 +0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -20,7 +20,7 @@ return [
     'telegram' => [
         'admin_id' => (int) env('TELEGRAM_ADMIN_ID', 0),
         'bot_token' => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
-        'bot_username' => env('TELEGRAM_BOT_USERNAME'),
+        'bot_username' => env('TELEGRAM_BOT_USERNAME', 'ExampleBot'),
         'webhook_secret_token' => env('TELEGRAM_WEBHOOK_SECRET_TOKEN'),
     ],
 
