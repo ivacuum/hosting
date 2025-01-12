72d2cb9f591d725c1feccf4a7cad03e9621b7742
Sergei Pankov <vacuum@ivacuum.ru>
2025-01-12 15:06:41 +0300

    Оформление кода

--- tests/Job/FetchTorrentBodyJobTest.php
+++ tests/Job/FetchTorrentBodyJobTest.php
@@ -4,7 +4,6 @@ namespace Tests\Job;
 
 use App\Factory\MagnetFactory;
 use App\Jobs\FetchTorrentBodyJob;
-use App\Services\Rto;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -18,14 +17,12 @@ class FetchTorrentBodyJobTest extends TestCase
         $announcer = 'announcer';
         $magnet = MagnetFactory::new()->withRtoId(911)->create();
 
-        $http = \Http::fake([
+        \Http::fake([
             'rto.vacuum.name/forum/viewtopic.php?t=911' => \Http::response('<div class="post_body">' . $body . '<fieldset class="attach"><span class="attach_link"><a class="magnet-link" href="magnet:?xt=urn:btih:info_hash&tr=' . urlencode($announcer) . '"></a></span></fieldset></div>'),
         ]);
 
-        $rto = new Rto($http);
-
         $job = new FetchTorrentBodyJob(911);
-        $job->handle($rto);
+        $this->app->call($job->handle(...));
 
         $magnet->refresh();
 
