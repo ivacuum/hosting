118d7ee5205914432757e85f625c3b09dea15372
Sergei Pankov <vacuum@ivacuum.ru>
2025-01-12 15:50:14 +0300

    Оформление кода

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Console\Commands;
-use App\Domain\Config;
 use App\Exceptions\RenderTokenMismatch;
 use App\Exceptions\SendToSentry;
 use App\Exceptions\SkipDatabaseOffline;
@@ -11,6 +9,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 use Symfony\Component\VarDumper\Cloner\AbstractCloner;
 
 AbstractCloner::$defaultCasters[Carbon\CarbonInterface::class] = App\Caster\CarbonCaster::prune(...);
@@ -19,12 +18,13 @@ AbstractCloner::$defaultCasters[Illuminate\Database\Eloquent\Model::class] = App
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/health-up',
         then: function () {
-            \Route::prefix('')
+            Route::prefix('')
                 ->group(__DIR__ . '/../routes/simple.php');
 
-            \Route::middleware(['web', 'auth', Admin::class, AcpNavigation::class])
+            Route::middleware(['web', 'auth', Admin::class, AcpNavigation::class])
                 ->prefix('acp')
                 ->name('acp.')
                 ->group(__DIR__ . '/../routes/acp.php');
@@ -57,45 +57,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->throttle(fn (\Throwable $e) => Limit::perDay(50));
 
         $exceptions->dontTruncateRequestExceptions();
-    })
-    ->withSchedule(function (\Illuminate\Console\Scheduling\Schedule $schedule) {
-        $cronOutput = Config::CronOutput->get();
-
-        $schedule
-            ->command(Commands\ProcessMetrics::class)
-            ->appendOutputTo($cronOutput);
-
-        $schedule
-            ->command(Commands\TrimMetricsStream::class)
-            ->cron('38 2,14 * * *')
-            ->appendOutputTo($cronOutput);
-
-        $schedule
-            ->command('model:prune', [
-                '--model' => [
-                    \App\ExternalHttpRequest::class,
-                    \App\PasswordReset::class,
-                ],
-            ])
-            ->cron('35 2,14 * * *')
-            ->appendOutputTo($cronOutput);
-
-        $schedule->command(Commands\SitemapBuild::class)
-            ->cron('30 2 * * *')
-            ->appendOutputTo($cronOutput);
-
-        // $schedule->command(Commands\WarmUpPhotoCache::class)
-        //     ->cron('40 5 * * *')
-        //     ->appendOutputTo($cronOutput);
-        //
-        // $schedule->command(Commands\WhoisUpdate::class)->cron('0 */4 * * *');
-
-        $schedule->command(Commands\RtoUpdate::class)
-            ->cron('20 */3 * * *')
-            ->appendOutputTo($cronOutput);
-
-        $schedule->command(Commands\PingDcppHubs::class)
-            ->cron('25 15 * * *')
-            ->appendOutputTo($cronOutput);
     })
     ->create();
---
+++ routes/console.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Console\Commands;
+use App\Domain\Config;
+use Illuminate\Support\Facades\Schedule;
+
+$cronOutput = Config::CronOutput->get();
+
+// cron('* * * * *')
+// 1. minute 0-59
+// 2. hour 0-23
+// 3. day of month 1-31
+// 4. month 1-12
+// 5. day of week 0-7 (0 and 7 is Sunday)
+Schedule::command(Commands\ProcessMetrics::class)
+    ->appendOutputTo($cronOutput);
+
+Schedule::command(Commands\SitemapBuild::class)
+    ->dailyAt('2:30')
+    ->appendOutputTo($cronOutput);
+
+Schedule::command('model:prune', [
+    '--model' => [
+        App\ExternalHttpRequest::class,
+        App\PasswordReset::class,
+    ],
+])
+    ->twiceDailyAt(first: 2, second: 14, offset: 35)
+    ->appendOutputTo($cronOutput);
+
+Schedule::command(Commands\TrimMetricsStream::class)
+    ->cron('38 2,14 * * *')
+    ->twiceDailyAt(first: 2, second: 14, offset: 38)
+    ->appendOutputTo($cronOutput);
+
+Schedule::command(Commands\RtoUpdate::class)
+    ->everyThreeHours(offset: 20)
+    ->appendOutputTo($cronOutput);
+
+Schedule::command(Commands\PingDcppHubs::class)
+    ->dailyAt('15:25')
+    ->appendOutputTo($cronOutput);
