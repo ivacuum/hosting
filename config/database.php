4d8bb6f6f09ba1dea0e75c7d50289e2ac506655f
Sergei Pankov <vacuum@ivacuum.ru>
2025-02-01 21:15:02 +0300

    Достаточно настроек .env

--- config/database.php
+++ config/database.php
@@ -22,15 +22,4 @@ return [
             ],
         ],
     ],
-
-    'redis' => [
-        'client' => env('REDIS_CLIENT', 'predis'),
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'port' => env('REDIS_PORT', 6379),
-            'password' => env('REDIS_PASSWORD'),
-            'database' => 0,
-        ],
-    ],
 ];
