763a3bd18d16264e0f253dda45d7492dc6255ca5
Sergei Pankov <vacuum@ivacuum.ru>
2025-04-07 22:42:19 +0400

    Заготовка о поездке в Афины

--- resources/views/life/trips/athens_2024.blade.php
+++ resources/views/life/trips/athens_2024.blade.php
@@ -2,314 +2,1079 @@
 
 @section('content')
 @ru
-  <p></p>
+  <p>Пропущен первоначальный рейс. Взята замена. За сколько до вылета взят рейс? Сразу пройдена регистрация.</p>
+@endru
+
+@ru
+  <p>Автобус подвез к центральной площади (Синтагма?). Оттуда путь пешком до жилья. Забастовка работников метрополитена.</p>
 @endru
 IMG_6196.jpeg
+
+@ru
+  <p>Никакой транспорт уже в час ночи не ходит. Во втором часу ночи удалось найти квартиру и заселиться.</p>
+@endru
 IMG_6197.jpeg
-IMG_6198.jpeg
+
+@ru
+  <p>Таксофоны.</p>
+@endru
 IMG_6199.jpeg
-IMG_6200.jpeg
-IMG_6201.jpeg
-IMG_6202.jpeg
-IMG_6203.jpeg
-IMG_6204.jpeg
+
+@ru
+  <p>Газовые счетчики в подъезде.</p>
+@endru
 IMG_6205.jpeg
+
+@ru
+  <p>У входной двери в квартиру два выключателя. Над звонком написана фамилия жильца. На двери есть ручка.</p>
+@endru
 IMG_6206.jpeg
+
+@ru
+  <p>Лифтом просят пользоваться по одному. Странно, что просят только на английском.</p>
+@endru
 IMG_6207.jpeg
+
+@ru
+  <p>Столик для писем в подъезде. По дизайну стола легко узнать страну.</p>
+@endru
 IMG_6208.jpeg
+
+@ru
+  <p>Туристический автобус привез людей к Акрополю.</p>
+@endru
 IMG_6209.jpeg
+
+@ru
+  <p>Из центра Афин видны горы.</p>
+@endru
 IMG_6210.jpeg
+
+@ru
+  <p>Железнодорожные пути не используются.</p>
+@endru
 IMG_6211.jpeg
+
+@ru
+  <p>Указатель прохода к Акрополю.</p>
+@endru
 IMG_6212.jpeg
-IMG_6213.jpeg
+
+@ru
+  <p>Узкие улицы.</p>
+@endru
 IMG_6214.jpeg
+
+@ru
+  <p>Сувениры. На верхних знакомые имена.</p>
+@endru
 IMG_6215.jpeg
+
+@ru
+  <p>Район Плака прекрасен.</p>
+@endru
 IMG_6234.jpeg
-IMG_6236.jpeg
-IMG_6237.jpeg
-IMG_6238.jpeg
-IMG_6239.jpeg
-IMG_6240.jpeg
 IMG_6241.jpeg
-IMG_6242.jpeg
-IMG_6244.jpeg
-IMG_6245.jpeg
-IMG_6246.jpeg
 IMG_6247.jpeg
 IMG_6248.jpeg
 IMG_6249.jpeg
-IMG_6250.jpeg
-IMG_6251.jpeg
 IMG_6252.jpeg
+
+@ru
+  <p>Цветок.</p>
+@endru
+IMG_6236.jpeg
+
+@ru
+  <p>Веранда общепита и цветочные клумбы.</p>
+@endru
+IMG_6237.jpeg
+
+@ru
+  <p>Надписи на греческом.</p>
+@endru
+IMG_6238.jpeg
+
+@ru
+  <p>Ресторан Диоген — у нас в компании так звали тестовые площадки.</p>
+@endru
+IMG_6239.jpeg
+
+@ru
+  <p>Уличная табличка.</p>
+@endru
+IMG_6242.jpeg
+
+@ru
+  <p>Как называется игра?</p>
+@endru
+IMG_6244.jpeg
+
+@ru
+  <p>Красивое здание.</p>
+@endru
+IMG_6246.jpeg
+
+@ru
+  <p>Входная дверь в квартиру и почтовый ящик.</p>
+@endru
+IMG_6250.jpeg
+
+@ru
+  <p>Снова видно горы.</p>
+@endru
+IMG_6251.jpeg
+
+@ru
+  <p>Граффити на узкой улице.</p>
+@endru
 IMG_6253.jpeg
-IMG_6254.jpeg
-IMG_6255.jpeg
+
+@ru
+  <p>Почтовый ящик.</p>
+@endru
 IMG_6256.jpeg
+
+@ru
+  <p>Кактусы растут на улице.</p>
+@endru
 IMG_6257.jpeg
-IMG_6269.jpeg
+
+@ru
+  <p>Мусор на крыше.</p>
+@endru
 IMG_6270.jpeg
+
+@ru
+  <p>Зелень на заборах.</p>
+@endru
 IMG_6271.jpeg
+
+@ru
+  <p>Желтые дома.</p>
+@endru
 IMG_6272.jpeg
+
+@ru
+  <p>Уголок старины.</p>
+@endru
 IMG_6282.jpeg
 IMG_6283.jpeg
-IMG_6284.jpeg
 IMG_6285.jpeg
-IMG_6286.jpeg
 IMG_6287.jpeg
+
+@ru
+  <p>Узкая улица.</p>
+@endru
+IMG_6284.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_6286.jpeg
+
+@ru
+  <p>Стволы деревьев снизу красят белым, как в России.</p>
+@endru
 IMG_6288.jpeg
+
+@ru
+  <p>Еще уголок старины.</p>
+@endru
 IMG_6289.jpeg
+
+@ru
+  <p>Дорожные знаки наезжают друг на друга.</p>
+@endru
 IMG_6290.jpeg
+
+@ru
+  <p>Манекен сидит на подоконнике.</p>
+@endru
 IMG_6291.jpeg
+
+@ru
+  <p>Самый старый дом в Афинах?</p>
+@endru
 IMG_6292.jpeg
 IMG_6303.jpeg
-IMG_6304.jpeg
-IMG_6305.jpeg
 IMG_6324.jpeg
+
+@ru
+  <p>На столе готовое поле для настольных игр.</p>
+@endru
+IMG_6305.jpeg
+
+@ru
+  <p>Много где идет ремонт.</p>
+@endru
 IMG_6336.jpeg
-IMG_6337.jpeg
+
+@ru
+  <p>Церковь среди зелени.</p>
+@endru
 IMG_6339.jpeg
 IMG_6340.jpeg
+
+@ru
+  <p>Собор?</p>
+@endru
 IMG_6341.jpeg
+
+@ru
+  <p>Сыры в супермаркете.</p>
+@endru
 IMG_6342.jpeg
 IMG_6343.jpeg
+
+@ru
+  <p>Надписи на греческом на верхнем бортике дома желтого цвета.</p>
+@endru
 IMG_6344.jpeg
+
+@ru
+  <p>Улицы.</p>
+@endru
 IMG_6345.jpeg
 IMG_6346.jpeg
+
+@ru
+  <p>В Старбаксе.</p>
+@endru
 IMG_6347.jpeg
+
+@ru
+  <p>Мороженое.</p>
+@endru
 IMG_6351.jpeg
+
+@ru
+  <p>Улица.</p>
+@endru
 IMG_6352.jpeg
+
+@ru
+  <p>Торговая галерея.</p>
+@endru
 IMG_6354.jpeg
+
+@ru
+  <p>С заходом солнца многие бизнесы закрываются.</p>
+@endru
 IMG_6355.jpeg
+
+@ru
+  <p>Улицы пустеют.</p>
+@endru
 IMG_6356.jpeg
+
+@ru
+  <p>Несмотря на закрытый магазин, весь ассортимент обуви по-прежнему можно посмотреть через стекло.</p>
+@endru
 IMG_6357.jpeg
+
+@ru
+  <p>Церковь.</p>
+@endru
 IMG_6358.jpeg
+
+@ru
+  <p>Фонтаны на площади с неисчислимым количеством голубей.</p>
+@endru
 IMG_6360.jpeg
+
+@ru
+  <p>Метро все еще не работает.</p>
+@endru
 IMG_6361.jpeg
+
+@ru
+  <p>Сначала подвернулось недорогое жилье в этом районе, но район Плака оказался куда приятнее.</p>
+@endru
 IMG_6363.jpeg
-IMG_6364.jpeg
+
+@ru
+  <p>В аптеке?</p>
+@endru
 IMG_6365.jpeg
+
+@ru
+  <p>Вечерние улицы.</p>
+@endru
 IMG_6366.jpeg
 IMG_6368.jpeg
 IMG_6369.jpeg
 IMG_6370.jpeg
+
+@ru
+  <p>На мой взгляд, эталон прозрачного входа в подъезд.</p>
+@endru
 IMG_6371.jpeg
+
+@ru
+  <p>Афины — рельефный город.</p>
+@endru
 IMG_6372.jpeg
+
+@ru
+  <p>Контейнер для сдачи ненужной одежды.</p>
+@endru
 IMG_6373.jpeg
+
+@ru
+  <p>18 апреля внезапно объявлен бесплатный вход в Акрополь. Очередь на вход растянулась минут на сорок.</p>
+@endru
 IMG_6375.jpeg
+
+@ru
+  <p>Съемка репортажа.</p>
+@endru
 IMG_6377.jpeg
+
+@ru
+  <p>Все должны пройти через кассы.</p>
+@endru
 IMG_6378.jpeg
+
+@ru
+  <p>И получить бесплатный билет.</p>
+@endru
 IMG_6379.jpeg
+
+@ru
+  <p>Территория позади турникетов.</p>
+@endru
 IMG_6380.jpeg
-IMG_6381.jpeg
+
+@ru
+  <p>Старина.</p>
+@endru
 IMG_6382.jpeg
+
+@ru
+  <p>Амфитеатр?</p>
+@endru
 IMG_6383.jpeg
+
+@ru
+  <p>Трон.</p>
+@endru
 IMG_6384.jpeg
-IMG_6385.jpeg
-IMG_6388.jpeg
+
+@ru
+  <p>По мере подъема открывается панорама города.</p>
+@endru
 IMG_6389.jpeg
+
+@ru
+  <p>Надписи на камне на греческом.</p>
+@endru
 IMG_6390.jpeg
+
+@ru
+  <p>Два камня подписаны: NK2383 и NK36.</p>
+@endru
 IMG_6391.jpeg
+
+@ru
+  <p>Еще тяжело различимые надписи на греческом.</p>
+@endru
 IMG_6392.jpeg
+
+@ru
+  <p>Раковина.</p>
+@endru
 IMG_6393.jpeg
+
+@ru
+  <p>Амфитеатр?</p>
+@endru
 IMG_6394.jpeg
-IMG_6395.jpeg
-IMG_6414.jpeg
-IMG_6434.jpeg
+
+@ru
+  <p>Панорама столицы.</p>
+@endru
 IMG_6435.jpeg
+IMG_6465.jpeg
+
+@ru
+  <p>Еще старину среди зелени в районе неподалеку видно.</p>
+@endru
 IMG_6442.jpeg
-IMG_6446.jpeg
+
+@ru
+  <p>Финальная часть подъема к Акрополю.</p>
+@endru
 IMG_6447.jpeg
+
+@ru
+  <p>Потоки на вход и выход разделены.</p>
+@endru
 IMG_6448.jpeg
-IMG_6449.jpeg
+
+@ru
+  <p>Старина на высоте.</p>
+@endru
 IMG_6450.jpeg
 IMG_6451.jpeg
 IMG_6452.jpeg
 IMG_6453.jpeg
 IMG_6454.jpeg
-IMG_6455.jpeg
-IMG_6456.jpeg
-IMG_6457.jpeg
-IMG_6459.jpeg
-IMG_6460.jpeg
-IMG_6461.jpeg
-IMG_6462.jpeg
-IMG_6463.jpeg
-IMG_6464.jpeg
-IMG_6465.jpeg
 IMG_6466.jpeg
 IMG_6470.jpeg
-IMG_6471.jpeg
-IMG_6472.jpeg
 IMG_6478.jpeg
 IMG_6480.jpeg
 IMG_6481.jpeg
+
+@ru
+  <p>Скамейка.</p>
+@endru
+IMG_6455.jpeg
+
+@ru
+  <p>Амфитеатр сверху.</p>
+@endru
+IMG_6456.jpeg
+
+@ru
+  <p>Здание музея сверху.</p>
+@endru
+IMG_6457.jpeg
+
+@ru
+  <p>Акрополь на реконструкции.</p>
+@endru
+IMG_6459.jpeg
+IMG_6463.jpeg
+IMG_6472.jpeg
+
+@ru
+  <p>Греческий флаг.</p>
+@endru
+IMG_6460.jpeg
+
+@ru
+  <p>Отдых в тени.</p>
+@endru
+IMG_6461.jpeg
+
+@ru
+  <p>Питьевые фонтанчики.</p>
+@endru
+IMG_6462.jpeg
+
+@ru
+  <p>Спуск.</p>
+@endru
 IMG_6482.jpeg
+
+@ru
+  <p>Футболки о Греции.</p>
+@endru
 IMG_6483.jpeg
+
+@ru
+  <p>Все новые и новые уголки старины попадаются во время прогулки.</p>
+@endru
 IMG_6484.jpeg
+
+@ru
+  <p>Улица.</p>
+@endru
 IMG_6485.jpeg
+
+@ru
+  <p>Котика гладят.</p>
+@endru
 IMG_6486.jpeg
+
+@ru
+  <p>Зелень на несколько метров свисает с балкона.</p>
+@endru
 IMG_6509.jpeg
+
+@ru
+  <p>Улицы.</p>
+@endru
 IMG_6510.jpeg
 IMG_6512.jpeg
 IMG_6513.jpeg
+
+@ru
+  <p>Улица Диогена.</p>
+@endru
 IMG_6514.jpeg
+
+@ru
+  <p>Торговая улица.</p>
+@endru
 IMG_6515.jpeg
+
+@ru
+  <p>Тихий переулок.</p>
+@endru
 IMG_6516.jpeg
+
+@ru
+  <p>Дорога.</p>
+@endru
 IMG_6521.jpeg
+
+@ru
+  <p>Протискивайся как хочешь.</p>
+@endru
 IMG_6522.jpeg
+
+@ru
+  <p>Троллейбус.</p>
+@endru
 IMG_6523.jpeg
+
+@ru
+  <p>Беговая дорожка на стадионе.</p>
+@endru
 IMG_6524.jpeg
+
+@ru
+  <p>Скромные зрительские трибуны.</p>
+@endru
 IMG_6525.jpeg
+
+@ru
+  <p>В парке.</p>
+@endru
 IMG_6526.jpeg
+
+@ru
+  <p>Мандарины растут.</p>
+@endru
 IMG_6528.jpeg
+
+@ru
+  <p>Мандарины падают. На вкус еще неспелые.</p>
+@endru
 IMG_6530.jpeg
-IMG_6532.jpeg
+
+@ru
+  <p>Что за здание?</p>
+@endru
 IMG_6533.jpeg
 IMG_6534.jpeg
+
+@ru
+  <p>Аллея.</p>
+@endru
 IMG_6535.jpeg
+
+@ru
+  <p>Статуя.</p>
+@endru
 IMG_6536.jpeg
+
+@ru
+  <p>Скамейка без ножек.</p>
+@endru
 IMG_6537.jpeg
+
+@ru
+  <p>Трамвай.</p>
+@endru
 IMG_6538.jpeg
-IMG_6539.jpeg
-IMG_6540.jpeg
-IMG_6541.jpeg
+
+@ru
+  <p>Зеленый пешеходный тоннель.</p>
+@endru
 IMG_6542.jpeg
+
+@ru
+  <p>Круговая скамейка.</p>
+@endru
+IMG_6540.jpeg
+
+@ru
+  <p>Пальмы в парке.</p>
+@endru
 IMG_6543.jpeg
-IMG_6544.jpeg
-IMG_6545.jpeg
 IMG_6546.jpeg
+
+@ru
+  <p>Цветы на островке.</p>
+@endru
 IMG_6547.jpeg
+
+@ru
+  <p>Навигация по парку.</p>
+@endru
 IMG_6548.jpeg
+
+@ru
+  <p>Беседка.</p>
+@endru
 IMG_6566.jpeg
-IMG_6567.jpeg
+
+@ru
+  <p>Утки и голуби у воды.</p>
+@endru
 IMG_6568.jpeg
-IMG_6569.jpeg
-IMG_6570.jpeg
+
+@ru
+  <p>Вода цвета зеленой листвы.</p>
+@endru
 IMG_6571.jpeg
-IMG_6572.jpeg
+
+@ru
+  <p>Утки притаились.</p>
+@endru
 IMG_6573.jpeg
-IMG_6574.jpeg
+
+@ru
+  <p>Люди отдыхают у воды.</p>
+@endru
 IMG_6576.jpeg
+
+@ru
+  <p>Утята плывут за уткой.</p>
+@endru
 IMG_6577.jpeg
+
+@ru
+  <p>Общественный туалет.</p>
+@endru
 IMG_6578.jpeg
+
+@ru
+  <p>Ручей в парке.</p>
+@endru
 IMG_6579.jpeg
+
+@ru
+  <p>Цветок.</p>
+@endru
 IMG_6580.jpeg
+
+@ru
+  <p>Рыба плавает в пруду.</p>
+@endru
 IMG_6581.jpeg
+
+@ru
+  <p>Греческие скамейки.</p>
+@endru
 IMG_6583.jpeg
-IMG_6584.jpeg
+
+@ru
+  <p>Наклон дороги.</p>
+@endru
 IMG_6585.jpeg
+
+@ru
+  <p>Продажа футбольных мячей.</p>
+@endru
 IMG_6586.jpeg
+
+@ru
+  <p>Автобус наклонен на остановке для уменьшения высоты порога.</p>
+@endru
 IMG_6587.jpeg
+
+@ru
+  <p>Много скутеристов.</p>
+@endru
 IMG_6588.jpeg
+
+@ru
+  <p>Широкие балконы.</p>
+@endru
 IMG_6589.jpeg
+IMG_6595.jpeg
+
+@ru
+  <p>Улицы района Колонаки.</p>
+@endru
 IMG_6590.jpeg
 IMG_6591.jpeg
 IMG_6592.jpeg
-IMG_6593.jpeg
-IMG_6594.jpeg
-IMG_6595.jpeg
-IMG_6596.jpeg
-IMG_6597.jpeg
-IMG_6598.jpeg
-IMG_6599.jpeg
-IMG_6600.jpeg
-IMG_6601.jpeg
-IMG_6602.jpeg
-IMG_6603.jpeg
-IMG_6604.jpeg
-IMG_6605.jpeg
-IMG_6608.jpeg
-IMG_6609.jpeg
-IMG_6610.jpeg
-IMG_6611.jpeg
-IMG_6612.jpeg
-IMG_6627.jpeg
-IMG_6628.jpeg
-IMG_6629.jpeg
-IMG_6630.jpeg
-IMG_6631.jpeg
-IMG_6632.jpeg
-IMG_6633.jpeg
-IMG_6634.jpeg
-IMG_6635.jpeg
-IMG_6636.jpeg
-IMG_6640.jpeg
-IMG_6641.jpeg
-IMG_6642.jpeg
-IMG_6643.jpeg
-IMG_6644.jpeg
-IMG_6645.jpeg
-IMG_6646.jpeg
-IMG_6647.jpeg
-IMG_6648.jpeg
-IMG_6649.jpeg
-IMG_6650.jpeg
-IMG_6651.jpeg
-IMG_6652.jpeg
-IMG_6653.jpeg
 IMG_6654.jpeg
 IMG_6655.jpeg
-IMG_6656.jpeg
-IMG_6657.jpeg
-IMG_6658.jpeg
+
+@ru
+  <p>Столик на улице.</p>
+@endru
+IMG_6594.jpeg
+
+@ru
+  <p>Дорога.</p>
+@endru
+IMG_6596.jpeg
+
+@ru
+  <p>Фрукты в супермаркете.</p>
+@endru
+IMG_6597.jpeg
+
+@ru
+  <p>Лестница среди жилых домов.</p>
+@endru
+IMG_6599.jpeg
+
+@ru
+  <p>Канатная дорога.</p>
+@endru
+IMG_6600.jpeg
+
+@ru
+  <p>Большинство балконов в растениях.</p>
+@endru
+IMG_6601.jpeg
+
+@ru
+  <p>Горшки с цветами на внешнем блоке кондиционера.</p>
+@endru
+IMG_6603.jpeg
+
+@ru
+  <p>Самостоятельный подъем вместо канатной дороги. Панорама города открывается постепенно. Закатное солнце приятно греет.</p>
+@endru
+IMG_6609.jpeg
+
+@ru
+  <p>Общепиты с панорамным видом.</p>
+@endru
+IMG_6612.jpeg
+IMG_6627.jpeg
+
+@ru
+  <p>Мотоцикл и скутер припаркованы.</p>
+@endru
+IMG_6628.jpeg
+
+@ru
+  <p>Акрополь виднеется.</p>
+@endru
+IMG_6629.jpeg
+
+@ru
+  <p>Скамейки с панорамным видом.</p>
+@endru
+IMG_6630.jpeg
+IMG_6632.jpeg
+
+@ru
+  <p>Подъем продолжается.</p>
+@endru
+IMG_6634.jpeg
+IMG_6641.jpeg
+
+@ru
+  <p>Кактусы захватывают скалу.</p>
+@endru
+IMG_6635.jpeg
+
+@ru
+  <p>Впечатляет оглядываться назад.</p>
+@endru
+IMG_6642.jpeg
+
+@ru
+  <p>Уже видна верхняя точка.</p>
+@endru
+IMG_6643.jpeg
+
+@ru
+  <p>Листья кактусов исписаны людьми.</p>
+@endru
+IMG_6644.jpeg
+
+@ru
+  <p>Кактусы цветут.</p>
+@endru
+IMG_6645.jpeg
+
+@ru
+  <p>Панорама города с вершины.</p>
+@endru
+IMG_6646.jpeg
+
+@ru
+  <p>Люди наверху.</p>
+@endru
+IMG_6647.jpeg
+IMG_6650.jpeg
+
+@ru
+  <p>Общепит наверху.</p>
+@endru
+IMG_6651.jpeg
+
+@ru
+  <p>Спуск вниз.</p>
+@endru
+IMG_6652.jpeg
+
+@ru
+  <p>Кривое дерево.</p>
+@endru
+IMG_6653.jpeg
+
+@ru
+  <p>Гладкая плитка под ногами.</p>
+@endru
 IMG_6664.jpeg
+
+@ru
+  <p>Здание парламента.</p>
+@endru
+IMG_6658.jpeg
+
+@ru
+  <p>Трамвай на остановке.</p>
+@endru
 IMG_6666.jpeg
+
+@ru
+  <p>Маленький автобус на остановке.</p>
+@endru
 IMG_6667.jpeg
+
+@ru
+  <p>Терминал по покупке билетов.</p>
+@endru
 IMG_6668.jpeg
+
+@ru
+  <p>Скамейка на остановке.</p>
+@endru
 IMG_6669.jpeg
+
+@ru
+  <p>Около шести недель до концерта Раммштайна в Афинах. У меня планах посетить их концерт в Белграде.</p>
+@endru
 IMG_6671.jpeg
+
+@ru
+  <p>Древняя арка.</p>
+@endru
 IMG_6673.jpeg
+
+@ru
+  <p>Котики на капоте автомобиля.</p>
+@endru
 IMG_6674.jpeg
+
+@ru
+  <p>Таверна Панос.</p>
+@endru
 IMG_6675.jpeg
+
+@ru
+  <p>Парковочное место для колясок.</p>
+@endru
 IMG_6676.jpeg
+
+@ru
+  <p>Комар-пчела.</p>
+@endru
 IMG_6680.jpeg
+
+@ru
+  <p>Домофон и сейфы для ключей.</p>
+@endru
 IMG_6681.jpeg
+
+@ru
+  <p>Котел на балконе.</p>
+@endru
 IMG_6682.jpeg
+
+@ru
+  <p>Ко второму этажу подведена желтая труба для сброса мусора прямо в контейнер.</p>
+@endru
 IMG_6683.jpeg
+
+@ru
+  <p>Чипсы в ассортименте в супермаркете.</p>
+@endru
 IMG_6684.jpeg
+
+@ru
+  <p>Собака смотрит в открытое водительское окно.</p>
+@endru
 IMG_6685.jpeg
+
+@ru
+  <p>В приоткрытую дверь подъезда видно газовый счетчик.</p>
+@endru
 IMG_6686.jpeg
+
+@ru
+  <p>Каждая буква логотипа в своем дизайне. За буквами тень от солнечного света, что добавляет объема.</p>
+@endru
 IMG_6687.jpeg
+
+@ru
+  <p>Граффити.</p>
+@endru
 IMG_6688.jpeg
 IMG_6690.jpeg
+
+@ru
+  <p>Снос стен дома.</p>
+@endru
 IMG_6691.jpeg
+
+@ru
+  <p>Внезапно банковская карта перестала работать, пришлось пополнить транспортную карту наличными.</p>
+@endru
 IMG_6692.jpeg
+
+@ru
+  <p>Остановка трамвая.</p>
+@endru
 IMG_6693.jpeg
+
+@ru
+  <p>В вагоне трамвая.</p>
+@endru
 IMG_6694.jpeg
+
+@ru
+  <p>На трамвае можно доехать прямо до моря.</p>
+@endru
 IMG_6695.jpeg
-IMG_6696.jpeg
-IMG_6697.jpeg
+
+@ru
+  <p>Станция Эдем. Сад?</p>
+@endru
 IMG_6698.jpeg
+
+@ru
+  <p>Пляжный песок темного цвета.</p>
+@endru
 IMG_6699.jpeg
-IMG_6700.jpeg
+
+@ru
+  <p>Парусное судно в море.</p>
+@endru
 IMG_6701.jpeg
+
+@ru
+  <p>Берег.</p>
+@endru
 IMG_6702.jpeg
-IMG_6703.jpeg
-IMG_6704.jpeg
+
+@ru
+  <p>Кто-то потерял наушник.</p>
+@endru
 IMG_6705.jpeg
+
+@ru
+  <p>Общепит с видом на море.</p>
+@endru
 IMG_6706.jpeg
+
+@ru
+  <p>Канал.</p>
+@endru
 IMG_6707.jpeg
+
+@ru
+  <p>Парковка.</p>
+@endru
 IMG_6708.jpeg
-IMG_6709.jpeg
-IMG_6710.jpeg
 IMG_6711.jpeg
+
+@ru
+  <p>Общественный туалет.</p>
+@endru
+IMG_6709.jpeg
+
+@ru
+  <p>Яхты на пирсе.</p>
+@endru
+IMG_6710.jpeg
+
+@ru
+  <p>В продаже в супермаркете разноцветные яйца.</p>
+@endru
 IMG_6712.jpeg
+
+@ru
+  <p>Полупрозрачная упаковка напитка — вместо воды окрасили этикетку.</p>
+@endru
 IMG_6713.jpeg
+
+@ru
+  <p>Ассортимент мороженого.</p>
+@endru
 IMG_6714.jpeg
+
+@ru
+  <p>Ассортимент сыров.</p>
+@endru
 IMG_6715.jpeg
+
+@ru
+  <p>Фонтан.</p>
+@endru
 IMG_6716.jpeg
+
+@ru
+  <p>Собака смотрит в пассажирское окно.</p>
+@endru
 IMG_6717.jpeg
-IMG_6718.jpeg
+
+@ru
+  <p>Улица во время дождя.</p>
+@endru
 IMG_6719.jpeg
+
+@ru
+  <p>Наконец-то метрополитен заработал. Поезд готов отправиться в темный тоннель со станции метро.</p>
+@endru
 IMG_6726.jpeg
+
+@ru
+  <p>Платформа станции метро.</p>
+@endru
 IMG_6727.jpeg
+
+@ru
+  <p>Вагон метро.</p>
+@endru
 IMG_6728.jpeg
-IMG_6729.jpeg
+
+@ru
+  <p>В аэропорт ходит каждый состав, как в Москве до делового центра.</p>
+@endru
 IMG_6730.jpeg
+
+@ru
+  <p>Станция метро аэропорта.</p>
+@endru
 IMG_6731.jpeg
+
+@ru
+  <p>Терминалы обилечивания у входа на станцию метро аэропорта.</p>
+@endru
 IMG_6732.jpeg
+
+@ru
+  <p>В день вылета тоже пасмурно.</p>
+@endru
 IMG_6733.jpeg
+
+@ru
+  <p>Лабиринт на регистрацию на рейс.</p>
+@endru
 IMG_6734.jpeg
+
+@ru
+  <p>Табло вылетов.</p>
+@endru
 IMG_6735.jpeg
+
+@ru
+  <p>Южанин в футболке и северянин в куртке.</p>
+@endru
 IMG_6736.jpeg
 @endsection
