324a5e50f2174e7b1d80f7aa79bf0f382530dee1
Sergei Pankov <vacuum@ivacuum.ru>
2025-05-04 14:47:49 +0300

    Все Early Hints в коде, а не в nginx

--- app/Http/Middleware/EarlyHints.php
+++ app/Http/Middleware/EarlyHints.php
@@ -17,6 +17,10 @@ class EarlyHints
         $manifest = json_decode(file_get_contents(public_path('assets/manifest.json')), associative: true);
 
         $links = [
+            '<https://life.ivacuum.org>; rel=preconnect',
+            '<https://ivacuum.org>; rel=preconnect',
+            '<https://mc.yandex.ru>; rel=preconnect',
+            '<https://mc.yandex.com>; rel=preconnect',
             "</assets/{$manifest['resources/css/app.css']['file']}>; rel=preload; as=style",
             "</assets/{$manifest['resources/js/app.js']['file']}>; rel=preload; as=script",
             "</assets/{$manifest['resources/css/InterVariable.woff2']['file']}>; rel=preload; as=font",
