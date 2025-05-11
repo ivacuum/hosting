3e49d3db3b7f40fd140a29087218dc1717c88ac2
Sergei Pankov <vacuum@ivacuum.ru>
2025-05-11 11:38:26 +0300

    Уменьшен размер блоков с кодом

--- resources/views/components/terminal-pre.blade.php
+++ resources/views/components/terminal-pre.blade.php
@@ -1,3 +1,3 @@
 <div class="js-shortcuts-item overflow-x-scroll">
-  <div class="font-mono bg-slate-50 dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-sm px-4 py-2 break-words whitespace-pre-wrap">{{ $slot }}</div>
+  <div class="font-mono text-sm bg-slate-50 dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-sm px-4 py-2 break-words whitespace-pre-wrap">{{ $slot }}</div>
 </div>
