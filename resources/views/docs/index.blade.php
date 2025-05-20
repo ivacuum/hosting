172e3d2de9bfb196466141e4a4e7bba024942c4d
Sergei Pankov <vacuum@ivacuum.ru>
2025-05-20 19:18:04 +0300

    Мобильная навигация

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -9,6 +9,18 @@
 <p>В разделе справка рассказано как пользоваться различными разделами данного сайта.</p>
 <p>В разделе хостинг представлена подборка различных инструкций и заготовок, чтобы экономить время при решении повторяющихся задач и проблем.</p>
 
+<div class="sm:hidden">
+  <h2>@lang('Справка')</h2>
+  <div><a class="link" href="@lng/docs/trips">@lang('Поездки')</a></div>
+</div>
+
+<div class="sm:hidden">
+  <h2>@lang('Хостинг')</h2>
+  <div><a class="link" href="@lng/docs/amazon-s3">@lang('Amazon S3')</a></div>
+  <div><a class="link" href="@lng/docs/freebsd">@lang('FreeBSD')</a></div>
+  <div><a class="link" href="@lng/docs/nginx">@lang('Nginx')</a></div>
+</div>
+
 <h2>Веб-технологии</h2>
 <div>
   <a class="link" href="https://developers.whatwg.org/" rel="nofollow">
