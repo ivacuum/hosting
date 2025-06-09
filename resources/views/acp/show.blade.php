4a9686482ee12f0a9f2191efabc0f258d272da8e
Sergei Pankov <vacuum@ivacuum.ru>
2025-06-09 20:40:12 +0300

    Оформление аккордеона

--- resources/views/acp/show.blade.php
+++ resources/views/acp/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 @if (Auth::user()->isRoot())
   <details class="mt-4">
-    <summary class="outline-none">JSON</summary>
-    <div class="bg-light dark:bg-slate-800 border border-gray-200 dark:border-slate-700 mt-1 py-1 px-2 text-xs dark:text-slate-300 rounded-sm">
+    <summary class="outline-none pseudo">JSON</summary>
+    <div class="bg-light dark:bg-slate-800 border border-gray-200 dark:border-slate-700 mt-1 py-1 px-2 text-sm dark:text-slate-300 rounded-sm">
       <pre class="inline-block break-words mb-0 whitespace-pre-wrap max-w-full">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
     </div>
   </details>
