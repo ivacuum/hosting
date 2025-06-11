d3b1728de2332ecda5b6a6a193c8b52c3610c79e
Sergei Pankov <vacuum@ivacuum.ru>
2025-06-11 21:34:57 +0300

    Оформление страницы с тренажерами

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -1,15 +1,15 @@
 @extends('life.base')
 
 @section('content')
-<h1 class="text-4xl mb-2">@lang('Тренажеры')</h1>
+<h1 class="font-medium text-4xl tracking-tight mb-2">@lang('Тренажеры')</h1>
 <p>@lang('Интерактивные инструменты для практики иностранных языков.')</p>
 
 <div class="grid gap-12 md:grid-cols-2 mt-12">
   <div>
     <div class="flex flex-col gap-2">
       <a class="flex flex-col gap-2 link-parent group" href="{{ to('japanese/hiragana-katakana') }}">
-        <div class="flex items-center justify-center border border-amber-200 dark:border-amber-700/25 rounded-sm bg-amber-100 dark:bg-amber-800/40 dark:group-hover:bg-amber-800/60 w-24 h-12 text-2xl text-amber-900 dark:text-amber-200">五十音</div>
-        <h2 class="text-2xl text-balance link">
+        <div class="flex items-center justify-center border border-amber-200 group-hover:border-amber-400 dark:border-amber-700/25 dark:group-hover:border-amber-700/25 rounded-sm bg-amber-100 dark:bg-amber-800/40 dark:group-hover:bg-amber-800/60 w-24 h-12 text-2xl text-amber-900 dark:text-amber-200">五十音</div>
+        <h2 class="font-medium text-2xl text-balance link">
           @lang('japanese.hiragana-katakana-trainer')
         </h2>
       </a>
@@ -19,8 +19,8 @@
   <div>
     <div class="flex flex-col gap-2">
       <a class="flex flex-col gap-2 link-parent group" href="{{ to('japanese/words-trainer') }}">
-        <div class="flex items-center justify-center border border-cyan-200 dark:border-cyan-700/25 rounded-sm bg-cyan-100 dark:bg-cyan-700/40 dark:group-hover:bg-cyan-700/60 w-24 h-12 text-2xl text-cyan-900 dark:text-cyan-200">単語</div>
-        <h2 class="text-2xl text-balance link">
+        <div class="flex items-center justify-center border border-cyan-200 group-hover:border-cyan-400 dark:border-cyan-700/25 dark:group-hover:border-cyan-700/25 rounded-sm bg-cyan-100 dark:bg-cyan-700/40 dark:group-hover:bg-cyan-700/60  w-24 h-12 text-2xl text-cyan-900 dark:text-cyan-200">単語</div>
+        <h2 class="font-medium text-2xl text-balance link">
           @lang('Тренажер по набору слов хираганой и катаканой')
         </h2>
       </a>
@@ -30,8 +30,8 @@
   <div>
     <div class="flex flex-col gap-2">
       <a class="flex flex-col gap-2 link-parent group" href="{{ to('korean/hangul') }}">
-        <div class="flex items-center justify-center border border-indigo-200 dark:border-indigo-700/25 rounded-sm bg-indigo-100 dark:bg-indigo-700/40 dark:group-hover:bg-indigo-700/60 w-24 h-12 text-2xl text-indigo-900 dark:text-indigo-200">한글</div>
-        <h2 class="text-2xl text-balance link">
+        <div class="flex items-center justify-center border border-indigo-200 group-hover:border-indigo-400 dark:border-indigo-700/25 dark:group-hover:border-indigo-700/25 rounded-sm bg-indigo-100 dark:bg-indigo-700/40 dark:group-hover:bg-indigo-700/60 w-24 h-12 text-2xl text-indigo-900 dark:text-indigo-200">한글</div>
+        <h2 class="font-medium text-2xl text-balance link">
           @lang('Тренажер хангыля')
         </h2>
       </a>
@@ -41,8 +41,8 @@
   <div>
     <div class="flex flex-col gap-2">
       <a class="flex flex-col gap-2 link-parent group" href="{{ to('trainers/numbers') }}">
-        <div class="flex items-center justify-center border border-lime-200 dark:border-lime-700/25 rounded-sm bg-lime-100 dark:bg-lime-800/40 dark:group-hover:bg-lime-800/60 w-24 h-12 text-2xl text-lime-900 dark:text-lime-200">123</div>
-        <h2 class="text-2xl text-balance link">
+        <div class="flex items-center justify-center border border-lime-200 group-hover:border-lime-400 dark:border-lime-700/25 dark:group-hover:border-lime-700/25 rounded-sm bg-lime-100 dark:bg-lime-800/40 dark:group-hover:bg-lime-800/60 w-24 h-12 text-2xl text-lime-900 dark:text-lime-200">123</div>
+        <h2 class="font-medium text-2xl text-balance link">
           @lang('Тренажер чисел')
         </h2>
       </a>
