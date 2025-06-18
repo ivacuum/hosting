8d248ee9d8233972e76458e3ac63ffd21da4aa5b
Sergei Pankov <vacuum@ivacuum.ru>
2025-06-19 01:43:00 +0300

    Интересны только атрибуты модели — не связи

--- resources/views/acp/show.blade.php
+++ resources/views/acp/show.blade.php
@@ -1,3 +1,8 @@
+<?php
+/**
+ * @var \Illuminate\Database\Eloquent\Model $model
+ */
+?>
 @extends(view()->exists("$tpl.base") ? "$tpl.base" : 'acp.layout')
 
 @section('content')
@@ -5,7 +10,7 @@
   <details class="mt-4">
     <summary class="outline-none pseudo">JSON</summary>
     <div class="bg-light dark:bg-slate-800 border border-gray-200 dark:border-slate-700 mt-1 py-1 px-2 text-sm dark:text-slate-300 rounded-sm">
-      <pre class="inline-block break-words mb-0 whitespace-pre-wrap max-w-full">{{ $model->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
+      <pre class="inline-block break-words mb-0 whitespace-pre-wrap max-w-full">{{ json_encode($model->attributesToArray(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) }}</pre>
     </div>
   </details>
 @endif
