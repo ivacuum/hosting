0071508edcdb635c4c3f24eaf4bb675af1a7d175
Sergei Pankov <vacuum@ivacuum.ru>
2025-07-26 17:23:05 +0300

    Заготовка для одноразовых скриптов

---
+++ app/Console/Commands/Chore.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Contracts\Console\Isolatable;
+use Ivacuum\Generic\Commands\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand('app:chore')]
+class Chore extends Command implements Isolatable
+{
+    protected $signature = 'app:chore';
+    protected $description = 'Do some chores';
+
+    public function handle()
+    {
+        return self::SUCCESS;
+    }
+}
