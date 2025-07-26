c1ac24b6f0a1a3654e2beac9577ab208985134d2
Sergei Pankov <vacuum@ivacuum.ru>
2025-07-26 13:14:53 +0300

    Внешний диск для галереи

--- config/filesystems.php
+++ config/filesystems.php
@@ -95,6 +95,18 @@ return [
             'throw' => true,
         ],
 
+        'r2-gallery' => [
+            'driver' => 's3',
+            'key' => env('R2_ACCESS_KEY_ID'),
+            'secret' => env('R2_SECRET_ACCESS_KEY'),
+            'region' => env('R2_DEFAULT_REGION', 'auto'),
+            'bucket' => env('R2_GALLERY_BUCKET'),
+            'url' => env('R2_URL'),
+            'endpoint' => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => true,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
