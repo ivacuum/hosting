40eaec0557dec50e4e313df6c9432a8ade329f56
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-02 16:18:50 +0300

    Попытка уменьшить количество вызовов функций

--- resources/views/tpl/pic-2x.blade.php
+++ resources/views/tpl/pic-2x.blade.php
@@ -1,18 +1,24 @@
-<?php /** @var \App\Trip $trip */ ?>
+<?php
+/**
+ * @var \App\Trip $trip
+ */
+$pic1x = ViewHelper::pic($slug ?? $trip->slug, $pic);
+$pic2x = ViewHelper::pic2x($slug ?? $trip->slug, $pic);
+?>
 
 <div class="-mt-2 mb-6 -mx-4 sm:mx-0 js-shortcuts-item" id="{{ $pic }}">
   <div class="max-w-[1000px] mx-auto text-center">
     <div class="pb-[75%] relative">
       @if ($isCrawler)
         <img
-          src="{{ ViewHelper::pic2x($slug ?? $trip->slug, $pic) }}"
-          alt="{{ isset($trip) ? $trip->imgAltText() : '' }}"
+          src="{{ $pic2x }}"
+          alt="{{ $trip?->imgAltText() ?? '' }}"
         >
       @else
         <img
           class="absolute left-0 w-full h-full object-cover sm:rounded-sm"
-          src="{{ ViewHelper::pic($slug ?? $trip->slug, $pic) }}"
-          srcset="{{ ViewHelper::pic($slug ?? $trip->slug, $pic) }} 1000w, {{ ViewHelper::pic2x($slug ?? $trip->slug, $pic) }} 2000w"
+          src="{{ $pic1x }}"
+          srcset="{{ $pic1x }} 1000w, {{ $pic2x }} 2000w"
           alt=""
           loading="lazy"
         >
@@ -27,7 +33,7 @@
           @endif
           <a
             class="flex items-center justify-center p-2 text-white hover:text-gray-200 drop-shadow-xs/90 hover:drop-shadow-sm/90"
-            href="{{ ViewHelper::pic2x($slug ?? $trip->slug, $pic) }}"
+            href="{{ $pic2x }}"
           >
             @svg (link)
           </a>
