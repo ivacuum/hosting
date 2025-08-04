1a82bd762758e34cbbba822e964097231db95f9c
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-04 14:14:05 +0300

    Настройка доступа к хранилищу фотографий

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,36 +51,18 @@ return [
             'throw' => true,
         ],
 
-        'temp' => [
-            'driver' => 'local',
-            'root' => public_path('uploads/temp'),
-            'url' => '/uploads/temp',
-            'visibility' => 'public',
-            'throw' => true,
-        ],
-
         'photos' => [
-            'driver' => env('PHOTOS_DRIVER', 'sftp'),
-            'host' => env('PHOTOS_HOST'),
-            'port' => (int) env('PHOTOS_PORT', 22),
+            'driver' => env('PHOTOS_DRIVER', 'local'),
             'root' => env('PHOTOS_ROOT', public_path('uploads/photos')),
-            'username' => env('PHOTOS_USERNAME'),
-            'password' => env('PHOTOS_PASSWORD'),
             'url' => env('PHOTOS_URL', '/uploads/photos'),
-            'privateKey' => env('PHOTOS_PRIVATE_KEY'),
-            'passphrase' => env('PHOTOS_PASSPHRASE'),
             'throw' => true,
-            'timeout' => 10,
-            'permissions' => [
-                'dir' => [
-                    'public' => 0755,
-                    'private' => 0755,
-                ],
-                'file' => [
-                    'public' => 0644,
-                    'private' => 0644,
-                ],
-            ],
+
+            'key' => env('R2_ACCESS_KEY_ID'),
+            'secret' => env('R2_SECRET_ACCESS_KEY'),
+            'region' => env('R2_DEFAULT_REGION', 'auto'),
+            'bucket' => env('PHOTOS_R2_BUCKET'),
+            'endpoint' => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
         'r2' => [
@@ -118,5 +100,37 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
         ],
+
+        'temp' => [
+            'driver' => 'local',
+            'root' => public_path('uploads/temp'),
+            'url' => '/uploads/temp',
+            'visibility' => 'public',
+            'throw' => true,
+        ],
+
+        // 'sftp-example' => [
+        //     'driver' => env('PHOTOS_DRIVER', 'sftp'),
+        //     'host' => env('PHOTOS_HOST'),
+        //     'port' => (int) env('PHOTOS_PORT', 22),
+        //     'root' => env('PHOTOS_ROOT', public_path('uploads/photos')),
+        //     'username' => env('PHOTOS_USERNAME'),
+        //     'password' => env('PHOTOS_PASSWORD'),
+        //     'url' => env('PHOTOS_URL', '/uploads/photos'),
+        //     'privateKey' => env('PHOTOS_PRIVATE_KEY'),
+        //     'passphrase' => env('PHOTOS_PASSPHRASE'),
+        //     'throw' => true,
+        //     'timeout' => 10,
+        //     'permissions' => [
+        //         'dir' => [
+        //             'public' => 0755,
+        //             'private' => 0755,
+        //         ],
+        //         'file' => [
+        //             'public' => 0644,
+        //             'private' => 0644,
+        //         ],
+        //     ],
+        // ],
     ],
 ];
