1add4e6ae4b20e359038d3fc777c8ee6f9c1a913
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-13 15:03:38 +0300

    Консистентность данных модели Country

--- app/Observers/CountryObserver.php
+++ app/Observers/CountryObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\Country;
 use App\Utilities\CacheHelper;
+use Illuminate\Support\Str;
 
 class CountryObserver
 {
@@ -20,5 +22,19 @@ class CountryObserver
         $this->cache->forgetCities();
         $this->cache->forgetCountries();
         $this->cache->forgetTrips();
+    }
+
+    public function saving(Country $country)
+    {
+        $this->maintainConsistency($country);
+    }
+
+    private function maintainConsistency(Country $country): void
+    {
+        $country->slug = Str::trim($country->slug);
+        $country->emoji = Str::trim($country->emoji);
+        $country->title_en = Str::trim($country->title_en);
+        $country->title_ru = Str::trim($country->title_ru);
+        $country->hashtags = Str::trim($country->hashtags);
     }
 }
