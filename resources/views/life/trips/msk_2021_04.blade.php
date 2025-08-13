3d01f0d527cba7ffd2350a31ad0b0b37542865cb
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-13 15:44:05 +0300

    Перевод заметки

--- resources/views/life/trips/msk_2021_04.blade.php
+++ resources/views/life/trips/msk_2021_04.blade.php
@@ -3,51 +3,71 @@
 @section('content')
 @ru
   <p>Новая Третьяковская галерея.</p>
+@en
+  <p>New Tretyakov Gallery.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7527.jpg'])
 
 @ru
   <p>Отверстия для глаз для просмотра экспозиции.</p>
+@en
+  <p>Eye holes for viewing the exhibition.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7528.jpg'])
 
 @ru
   <p>Свободное падение брусков со столкновениями с кольями.</p>
+@en
+  <p>Blocks are free falling and colliding with stakes.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7529.jpg'])
 
 @ru
   <p>Афиши семидесятых.</p>
+@en
+  <p>Posters from the seventies.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7531.jpg'])
 
 @ru
   <p>Проект по борьбе с пластиковыми отходами.</p>
+@en
+  <p>Project to reduce plastic waste.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7533.jpg'])
 
 @ru
   <p>USB-зарядки в вагоне метро.</p>
+@en
+  <p>USB charging ports in the metro car.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7534.jpg'])
 
 @ru
   <p>Улица Бутырский Вал.</p>
+@en
+  <p>Butyrsky Val street.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7535.jpg'])
 
 @ru
   <p>Флакон. Рынок.</p>
+@en
+  <p>Flacon. Market.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7536.jpg'])
 
 @ru
   <p>Подсказка местоположения под схемой.</p>
+@en
+  <p>Location hint under the map.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7537.jpg'])
 
 @ru
-  <p>Территория флакона.</p>
+  <p>Территория Флакона.</p>
+@en
+  <p>Flacon territory.</p>
 @endru
 @include('tpl.pic-2x', ['pic' => 'IMG_7538.jpg'])
 @endsection
