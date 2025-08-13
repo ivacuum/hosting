567972e88b034141240a1c7050c93326bcabbff7
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-13 15:04:38 +0300

    Консистентность данных модели Gig

--- app/Observers/GigObserver.php
+++ app/Observers/GigObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\Gig;
 use App\Utilities\CacheHelper;
+use Illuminate\Support\Str;
 
 class GigObserver
 {
@@ -16,5 +18,20 @@ class GigObserver
     public function saved()
     {
         $this->cache->forgetGigs();
+    }
+
+    public function saving(Gig $gig)
+    {
+        $this->maintainConsistency($gig);
+    }
+
+    private function maintainConsistency(Gig $gig): void
+    {
+        $gig->slug = Str::trim($gig->slug);
+        $gig->title_en = Str::trim($gig->title_en);
+        $gig->title_ru = Str::trim($gig->title_ru);
+        $gig->meta_image = Str::trim($gig->meta_image);
+        $gig->meta_description_en = Str::trim($gig->meta_description_en);
+        $gig->meta_description_ru = Str::trim($gig->meta_description_ru);
     }
 }
