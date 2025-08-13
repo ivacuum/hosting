840df4933721384a52c519c78c10f29ecbd61492
Sergei Pankov <vacuum@ivacuum.ru>
2025-08-13 15:03:14 +0300

    Консистентность данных модели City

--- app/Observers/CityObserver.php
+++ app/Observers/CityObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\City;
 use App\Utilities\CacheHelper;
+use Illuminate\Support\Str;
 
 class CityObserver
 {
@@ -20,5 +22,23 @@ class CityObserver
         $this->cache->forgetCities();
         $this->cache->forgetCountries();
         $this->cache->forgetTrips();
+    }
+
+    public function saving(City $city)
+    {
+        $this->maintainConsistency($city);
+    }
+
+    private function maintainConsistency(City $city): void
+    {
+        $city->iata = Str::trim($city->iata);
+        $city->slug = Str::trim($city->slug);
+        $city->hashtags = Str::trim($city->hashtags);
+        $city->title_en = Str::trim($city->title_en);
+        $city->title_ru = Str::trim($city->title_ru);
+
+        if ($city->iata && mb_strlen($city->iata) !== 3) {
+            throw new \DomainException('IATA code can only be three characters long.');
+        }
     }
 }
