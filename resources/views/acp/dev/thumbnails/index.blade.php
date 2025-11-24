41e9e6fdae06bb852379c5231d6d851f4833e3b4
Sergei Pankov <vacuum@ivacuum.ru>
2025-11-24 11:23:40 +0300

    Поправлена ссылка

--- resources/views/acp/dev/thumbnails/index.blade.php
+++ resources/views/acp/dev/thumbnails/index.blade.php
@@ -5,5 +5,5 @@
 <h2 class="font-medium text-3xl mb-2">Создание миниатюр</h2>
 @livewire(App\Livewire\ThumbnailMaker::class)
 
-<a class="btn btn-default mt-6" href="@lng/dev/thumbnails/clean">Почистить папку с загруженными файлами</a>
+<a class="btn btn-default mt-6" href="@lng/acp/dev/thumbnails/clean">Почистить папку с загруженными файлами</a>
 @endsection
