080a8bfcd1f9f680efa7b867c04108eaaf3377a0
Sergei Pankov <vacuum@ivacuum.ru>
2026-01-04 16:08:08 +0300

    Локализация

--- resources/views/japanese/words-trainer.blade.php
+++ resources/views/japanese/words-trainer.blade.php
@@ -16,26 +16,42 @@
           <li><span class="font-medium text-gray-500">катаканой</span> — <span class="font-bold" lang="ja">スシ</span></li>
           <li>оригинальным написанием с <span class="font-medium text-gray-500">кандзи</span> — <span class="font-bold" lang="ja">寿司</span></li>
         </ul>
+      @en
+        Once you have <a class="link" href="@lng/japanese/hiragana-katakana">mastered the syllabaries</a>, you are ready to start typing full words. Using 'sushi' (寿司) as an example, answers may be submitted in any of the following formats:
+        <ul>
+          <li><span class="font-medium text-gray-500">Romaji</span>: <span class="font-bold" lang="en">sushi</span></li>
+          <li><span class="font-medium text-gray-500">Hiragana</span>: <span class="font-bold" lang="ja">すし</span></li>
+          <li><span class="font-medium text-gray-500">Katakana</span>: <span class="font-bold" lang="ja">スシ</span></li>
+          <li><span class="font-medium text-gray-500">Kanji</span> (original spelling): <span class="font-bold" lang="ja">寿司</span></li>
+        </ul>
       @endru
     </div>
     <div>
       @ru
         Для проверки ответа необходимо нажать Ввод или клавишу Энтер. В случае правильного ответа следующее слово будет предложено автоматически.
+      @en
+        To validate your answer, click Submit or press the Enter key. The next word appears automatically if your answer is correct.
       @endru
     </div>
     <div>
       @ru
         Прием ответов хираганой и катаканой предусмотрен для случая, когда вы добавили себе на устройство японскую раскладку клавиатуры и только осваиваете ее.
+      @en
+        Support for direct Hiragana and Katakana input is intended for users who have configured a Japanese keyboard layout and wish to practice native typing.
       @endru
     </div>
     <div>
       @ru
         На иероглифы на фиолетовом фоне можно нажать, чтобы перейти на страницу с дополнительной информацией. Там же можно послушать произношение слов и посмотреть примеры предложений с ними.
+      @en
+        Click a word on a purple background to view more details. The detailed page includes native pronunciation and examples of the word used in context.
       @endru
     </div>
     <div>
       @ru
         База слов из прекрасного сервиса <a class="link" href="https://wanikani.com/" rel="noreferrer">Wanikani</a>, посвященного разучиванию ключей, кандзи и словарных слов. <a class="link" href="/news/263">Мой отзыв о Wanikani</a>.
+      @en
+        The vocabulary database is sourced from <a class="link" href="https://wanikani.com/" rel="noreferrer">Wanikani</a>, a wonderful platform dedicated to learning radicals, kanji, and vocabulary.
       @endru
     </div>
   </div>
