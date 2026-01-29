ea6051c8f67e2ac920426e4c2f4e1068dcd938f5
Sergei Pankov <vacuum@ivacuum.ru>
2026-01-29 14:36:56 +0300

    Черновик заметки

--- resources/views/life/trips/belgrade_2024_05_26.blade.php
+++ resources/views/life/trips/belgrade_2024_05_26.blade.php
@@ -3,16 +3,22 @@
 @section('content')
 @ru
   <p>Воскресный просмотр MotoGP. Гран-при Каталонии.</p>
+@en
+  <p>Watching MotoGP on Sunday. Catalan Grand Prix.</p>
 @endru
 IMG_9423.jpeg
 
 @ru
   <p>Кофейня Rocket Coffee.</p>
+@en
+  <p>Rocket Coffee.</p>
 @endru
 IMG_9427.jpeg
 
 @ru
   <p>Старый Савский мост.</p>
+@en
+  <p>Old Sava Bridge.</p>
 @endru
 IMG_9429.jpeg
 @endsection
