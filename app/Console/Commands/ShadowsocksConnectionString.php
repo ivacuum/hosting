8062a727b0f4ed7ce700e73cdbec8a28aec1b293
Sergei Pankov <vacuum@ivacuum.ru>
2026-01-31 18:45:14 +0300

    Учет пробелов

--- app/Console/Commands/ShadowsocksConnectionString.php
+++ app/Console/Commands/ShadowsocksConnectionString.php
@@ -20,7 +20,7 @@ class ShadowsocksConnectionString extends Command
         $server = text('What is the server address and port?', 'example.com:12345');
         $cipher = suggest('What is the cipher?', ['aes-256-gcm'], required: true);
         $secret = password('What is the password?', required: true);
-        $label = text('What is the server label?', 'test-msk');
+        $label = rawurlencode(text('What is the server label?', 'test-msk'));
 
         note('ss://' . base64_encode("{$cipher}:{$secret}") . "@{$server}#{$label}");
     }
