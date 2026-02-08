0a363398b771db2ad3f5855a95c8d303e05d983c
Sergei Pankov <vacuum@ivacuum.ru>
2026-02-09 02:12:20 +0300

    Оформление кода

--- app/Jobs/AbstractJob.php
+++ app/Jobs/AbstractJob.php
@@ -2,18 +2,12 @@
 
 namespace App\Jobs;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Queue\Queueable;
 
 abstract class AbstractJob implements ShouldQueue
 {
-    use Dispatchable;
-    use InteractsWithQueue;
     use Queueable;
-    use SerializesModels;
 
     public $tries;
     public $backoff;
