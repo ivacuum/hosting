4f6f17767bb986c005bfd73103792fc46a969b5d
Sergei Pankov <vacuum@ivacuum.ru>
2026-03-09 10:45:31 +0300

    Заготовки о концертах

---
+++ resources/views/life/gigs/dreamtheater_2026_02_20.blade.php
@@ -0,0 +1,26 @@
+@extends('life.gigs.base')
+
+{{-- Blue Square SOL Travel Hall --}}
+
+@section('content')
+@component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/dreamtheater.2026.jpg'])
+  <ol>
+    <li>Metropolis Pt. 1: The Miracle and the Sleeper</li>
+    <li>Overture 1928</li>
+    <li>Strange Déjà Vu</li>
+    <li>The Mirror</li>
+    <li>Panic Attack</li>
+    <li>The Enemy Inside</li>
+    <li>Peruvian Skies</li>
+    <li>As I Am</li>
+    <li>In the Arms of Morpheus</li>
+    <li>Night Terror</li>
+    <li>Midnight Messiah</li>
+    <li>Are We Dreaming?</li>
+    <li>Bend the Clock</li>
+    <li>The Shadow Man Incident</li>
+    <li>The Spirit Carries On</li>
+    <li>Pull Me Under</li>
+  </ol>
+@endcomponent
+@endsection
---
+++ resources/views/life/gigs/dreamtheater_2026_02_22.blade.php
@@ -0,0 +1,26 @@
+@extends('life.gigs.base')
+
+{{-- Blue Square SOL Travel Hall --}}
+
+@section('content')
+@component('tpl.setlist', ['cover' => 'https://life.ivacuum.org/gigs/dreamtheater.2026.jpg'])
+  <ol>
+    <li>In the Arms of Morpheus</li>
+    <li>Night Terror</li>
+    <li>A Broken Man</li>
+    <li>Dead Asleep</li>
+    <li>Midnight Messiah</li>
+    <li>Are We Dreaming?</li>
+    <li>Bend the Clock</li>
+    <li>The Shadow Man Incident</li>
+    <li>As I Am</li>
+    <li>The Enemy Inside</li>
+    <li>Panic Attack</li>
+    <li>Through My Words</li>
+    <li>Fatal Tragedy</li>
+    <li>Peruvian Skies</li>
+    <li>Take the Time</li>
+    <li>A Change of Seasons</li>
+  </ol>
+@endcomponent
+@endsection
