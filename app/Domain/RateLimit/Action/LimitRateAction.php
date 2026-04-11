cd9d37a6e14533381fd02e4d476faef9fb6a9b83
Sergei Pankov <vacuum@ivacuum.ru>
2026-04-11 19:40:51 +0300

    Оформление кода

--- app/Domain/RateLimit/Action/LimitRateAction.php
+++ app/Domain/RateLimit/Action/LimitRateAction.php
@@ -3,20 +3,23 @@
 namespace App\Domain\RateLimit\Action;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Container\Attributes\Config;
 
 class LimitRateAction
 {
     public function __construct(
+        #[Config('cache.default')]
+        private string $cacheStore,
         private LimitRateWithCacheAction $limitRateWithCache,
         private LimitRateWithRedisAction $limitRateWithRedis,
     ) {}
 
     public function execute(Limit $limit): bool
     {
-        if (app()->runningUnitTests()) {
-            return $this->limitRateWithCache->execute($limit);
+        if ($this->cacheStore === 'redis') {
+            return $this->limitRateWithRedis->execute($limit);
         }
 
-        return $this->limitRateWithRedis->execute($limit);
+        return $this->limitRateWithCache->execute($limit);
     }
 }
