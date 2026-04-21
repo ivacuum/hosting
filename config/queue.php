1336353c6cc3eb9e5941ad4441a38cdc79bf25a5
Sergei Pankov <vacuum@ivacuum.ru>
2026-04-21 16:19:00 +0300

    Ускорена обработка задач из очередей

--- config/queue.php
+++ config/queue.php
@@ -7,7 +7,10 @@ return [
             'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
-            'block_for' => null,
+            // 3 секунды ожидания задачи эффективнее 3 секунд сна
+            // Если block_for=null, sleep=3 и задача поступила через 0,05 сек после начала сна, то она будет обработана через 2,95 сек
+            // Если block_for=3 и задача поступила через 0,05 сек после блокировки, то она будет обработана сразу
+            'block_for' => 3,
             'after_commit' => true,
         ],
     ],
