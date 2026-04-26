7e7af8e08a05ba84048766407a51421329fe0d35
Sergei Pankov <vacuum@ivacuum.ru>
2026-04-26 18:25:19 +0300

    Заготовка о поездке в Питер

---
+++ resources/views/life/trips/spb_2026.blade.php
@@ -0,0 +1,703 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Общее табло отправления поездов Казанского и Ленинградского вокзалов. Ленинградский вокзал с августа 2024 года до сих пор закрыт на реконструкцию. На Казанский вокзал занесло желание попробовать лаунж перед отправлением в Питер. Бесплатные проходы в лаунжи перепадают от банка каждый месяц и сгорают в случае неиспользования. В приложении было замечено, что лаунжи на вокзалах можно посещать не только до отправления, как в аэропортах, но и в течение часа по прибытии. В данном случае интересовало можно ли посещать лаунж не на том вокзале, с которого отправляешься.</p>
+@endru
+IMG_5460.jpeg
+
+@ru
+  <p>Да, можно. Плюс оказалось, если едешь на поезде Аврора бизнес-классом (класс 1Р), то проход в лаунж до отправления уже включен в стоимость билета. В лаунж можно попасть только с билетом на поезд дальнего следования — обладателей билетов на региональные экспрессы разворачивали.</p>
+@endru
+
+@ru
+  <p>Зал богато украшен, особенно потолок.</p>
+@endru
+IMG_5461.jpeg
+
+@ru
+  <p>Детали потолка и люстра.</p>
+@endru
+IMG_5462.jpeg
+
+@ru
+  <p>Двухэтажный поезд Аврора снаружи. Эконом стоил 3300 ₽, бизнес-класс 4800 ₽. Было решено попробовать бизнес-класс.</p>
+@endru
+IMG_5467.jpeg
+
+@ru
+  <p>Интерьер вагона. Можно сидеть отдельно, чем и было решено воспользоваться.</p>
+@endru
+IMG_5468.jpeg
+
+@ru
+  <p>Места для ног предостаточно. Большинство пассажиров в тапочках, потому что они полагаются в дорожном наборе. Очередной раз радует выбор компактного 14-дюймового ноутбука, потому что с ним удобно в поездках.</p>
+@endru
+IMG_5470.jpeg
+
+@ru
+  <p>Параллельно куда-то неспешно едет пустой Сапсан.</p>
+@endru
+IMG_5474.jpeg
+
+@ru
+  <p>При покупке билета было видно, что включено питание. Не обратил внимания, что питание можно будет выбрать по меню.</p>
+@endru
+IMG_5476.jpeg
+
+@ru
+  <p>Меню сразу дают открытым на нужной странице, потому что питание в утреннем и дневном поездах отличается. Нужно выбрать между первой колонкой блюд, второй и третьей. Оказалось, что существует некий способ сделать предварительный заказ на питание — он позволяет комбинировать любую закуску с любым основным блюдом.</p>
+@endru
+IMG_5477.jpeg
+
+@ru
+  <p>Закатный пейзаж за окном поезда.</p>
+@en
+  <p>Sunset landscape outside the train window.</p>
+@endru
+IMG_5482.jpeg
+
+@ru
+  <p>Прибыли. На стойке регистрации правила бесплатного доступа в лаунж Московского вокзала в Санкт-Петербурге.</p>
+@endru
+IMG_5485.jpeg
+
+@ru
+  <p>Гладильная доска и кулер с водой в коридоре каждого этажа отеля.</p>
+@endru
+IMG_5486.jpeg
+
+@ru
+  <p>Сбоку от двери табличка с указанием квартир на каждом этаже. А над дверью номера каждой квартиры подъезда. У десятки что-то с нулем случилось.</p>
+@endru
+IMG_5487.jpeg
+
+@ru
+  <p>Большая Подьяческая улица.</p>
+@endru
+IMG_5488.jpeg
+
+@ru
+  <p>Улица Никольская площадь.</p>
+@endru
+IMG_5490.jpeg
+
+@ru
+  <p>Люк Петербургской телефонной сети.</p>
+@en
+  <p>St. Petersburg telephone network manhole cover.</p>
+@endru
+IMG_5491.jpeg
+
+@ru
+  <p>Два сквера по бокам консерватории.</p>
+@endru
+IMG_5492.jpeg
+IMG_5493.jpeg
+
+@ru
+  <p>Здание консерватории сквозь голые ветви деревьев на Театральной площади. С зеленой листвой такого вида уже бы не было.</p>
+@endru
+IMG_5494.jpeg
+
+@ru
+  <p>Компактная автомобильная заправка рядом со сквером.</p>
+@endru
+IMG_5495.jpeg
+
+@ru
+  <p>Общественный туалет со своим адресом.</p>
+@endru
+IMG_5500.jpeg
+
+@ru
+  <p>Набережная канала Грибоедова.</p>
+@endru
+IMG_5501.jpeg
+
+@ru
+  <p>Львиный мост через канал Грибоедова.</p>
+  <p>Lion Bridge over the Griboyedov Canal.</p>
+@endru
+IMG_5502.jpeg
+IMG_5503.jpeg
+
+@ru
+  <p>Канал Грибоедова.</p>
+@en
+  <p>The Griboyedov Canal.</p>
+@endru
+IMG_5504.jpeg
+
+@ru
+  <p>Фасад.</p>
+@endru
+IMG_5505.jpeg
+
+@ru
+  <p>Деревья у Гостиного двора высажены без использования приствольных решеток — ничего не препятствует распространению земли по тротуару.</p>
+@endru
+IMG_5506.jpeg
+
+@ru
+  <p>Армянская апостольская церковь Святой Екатерины видна между домов на Невском проспекте.</p>
+@endru
+IMG_5507.jpeg
+
+@ru
+  <p>Поляна цветущих пролесок у стен Михайловского замка.</p>
+@en
+  <p>A glade of blooming squills near the walls of St. Michael's Castle.</p>
+@endru
+IMG_5508.jpeg
+
+@ru
+  <p>Набережная реки Фонтанки.</p>
+@en
+  <p>Fontanka River Embankment.</p>
+@endru
+IMG_5509.jpeg
+
+@ru
+  <p>Статуи на фасаде дома украсили яркими цветами.</p>
+@en
+  <p>The statues on the building's facade are decorated with bright flowers.</p>
+@endru
+IMG_5510.jpeg
+
+@ru
+  <p>Фасад, арка и ворота дома на улице Правды, 13.</p>
+@endru
+IMG_5511.jpeg
+
+@ru
+  <p>Свод арки украшен дореволюционной мозаикой.</p>
+@en
+  <p>The vault of the archway is decorated with a pre-revolutionary mosaic.</p>
+@endru
+IMG_5512.jpeg
+
+@ru
+  <p>Световая проекция портрета Достоевского на торце дома.</p>
+@endru
+IMG_5514.jpeg
+
+@ru
+  <p>Вечерняя подсветка Владимирского собора.</p>
+@en
+  <p>The evening illumination of the Vladimirsky Cathedral.</p>
+@endru
+IMG_5515.jpeg
+
+@ru
+  <p>Аллея парка Екатерингоф. Деревья еще без листьев, но газон уже зеленеет.</p>
+@en
+  <p>An alley in Ekaterinhof Park. The trees are still leafless, but the lawn is already turning green.</p>
+@endru
+IMG_5517.jpeg
+
+@ru
+  <p>На берегу реки Таракановки.</p>
+@endru
+IMG_5518.jpeg
+IMG_5519.jpeg
+
+@ru
+  <p>Вид на противоположный берег.</p>
+@endru
+IMG_5522.jpeg
+IMG_5524.jpeg
+
+@ru
+  <p>Появляются первые листья. Скоро здание перестанет быть видно.</p>
+@endru
+IMG_5523.jpeg
+
+@ru
+  <p>Деревья у берега растут почти под 45 градусов.</p>
+@endru
+IMG_5525.jpeg
+
+@ru
+  <p>Аллея.</p>
+@en
+  <p>Alley.</p>
+@endru
+IMG_5526.jpeg
+
+@ru
+  <p>Деревянные кормушки для птиц.</p>
+@en
+  <p>Wooden bird feeders.</p>
+@endru
+IMG_5527.jpeg
+
+@ru
+  <p>Небольшой островок посреди пруда.</p>
+@en
+  <p>A small island in the middle of the pond.</p>
+@endru
+IMG_5528.jpeg
+
+@ru
+  <p>Нарвские триумфальные ворота с тротуара.</p>
+@endru
+IMG_5531.jpeg
+
+@ru
+  <p>Кассы и камеры хранения стадиона Кировец.</p>
+@endru
+IMG_5532.jpeg
+
+@ru
+  <p>Ограничители парковки у стадиона раскрашены под футбольные мячи.</p>
+@en
+  <p>Parking bollards near the stadium are painted like soccer balls.</p>
+@endru
+IMG_5533.jpeg
+
+@ru
+  <p>Вид на памятник героям Краснодона в парке Екатерингоф.</p>
+@endru
+IMG_5534.jpeg
+
+@ru
+  <p>Еще один пруд.</p>
+@endru
+IMG_5535.jpeg
+
+@ru
+  <p>Табличка с информацией о парке. Объявление гласит, что до мая он закрыт на весеннюю просушку. На самом деле в него можно попасть с одной стороны, хотя все остальные калитки закрыты.</p>
+@endru
+IMG_5536.jpeg
+
+@ru
+  <p>Свежевыкрашенная урна с бумажным предупреждением.</p>
+@en
+  <p>A freshly painted trash can with a paper warning.</p>
+@endru
+IMG_5537.jpeg
+
+@ru
+  <p>Утки гуляют по зеленой траве парка.</p>
+@endru
+IMG_5538.jpeg
+
+@ru
+  <p>Жилые комплексы и пришвартованные суда на противоположном берегу.</p>
+@endru
+IMG_5539.jpeg
+
+@ru
+  <p>Портовые краны и Богоявленская церковь.</p>
+@en
+  <p>Port cranes and the Epiphany Church.</p>
+@endru
+IMG_5540.jpeg
+
+@ru
+  <p>Песчаный берег Екатерингофки.</p>
+@en
+  <p>The sandy bank of the Ekaterinhofka River.</p>
+@endru
+IMG_5541.jpeg
+
+@ru
+  <p>Пруд.</p>
+@en
+  <p>Pond.</p>
+@endru
+IMG_5542.jpeg
+
+@ru
+  <p>Скамейка. Изогнутый мостик перекинут через небольшой канал.</p>
+@endru
+IMG_5543.jpeg
+
+@ru
+  <p>Мостик закрыт из-за аварийного состояния. Настил местами обвалился.</p>
+@endru
+IMG_5544.jpeg
+
+@ru
+  <p>Приятный пейзаж.</p>
+@endru
+IMG_5545.jpeg
+
+@ru
+  <p>Проход к Нарвским триумфальным воротам через подземный переход.</p>
+@endru
+IMG_5546.jpeg
+
+@ru
+  <p>Павильон станции метро Нарвская с зелеными дверьми.</p>
+@en
+  <p>The pavilion of the Narvskaya metro station with green doors.</p>
+@endru
+IMG_5547.jpeg
+
+@ru
+  <p>Торговый комплекс Галерея 1814 через дорогу от метро.</p>
+@en
+  <p>The Gallery 1814 shopping complex across the road from the metro.</p>
+@endru
+IMG_5548.jpeg
+
+@ru
+  <p>Памятник маршалу Говорову на площади Стачек.</p>
+@endru
+IMG_5549.jpeg
+
+@ru
+  <p>Нарвские триумфальные ворота вблизи.</p>
+@en
+  <p>The Narva Triumphal Arch up close.</p>
+@endru
+IMG_5550.jpeg
+
+@ru
+  <p>Трамвай в ретро стиле.</p>
+@endru
+IMG_5552.jpeg
+
+@ru
+  <p>Кировский универмаг в качестве примера конструктивизма. Однако, наполнение у него теперь современное.</p>
+@endru
+IMG_5554.jpeg
+
+@ru
+  <p>Советское панно на глухом торце здания призывает пролетариев объединяться.</p>
+@en
+  <p>A Soviet panel on the blank end wall of a building calls for the workers of the world to unite.</p>
+@endru
+IMG_5555.jpeg
+
+@ru
+  <p>Арка между домами.</p>
+@endru
+IMG_5556.jpeg
+
+{{--@ru--}}
+{{--  <p>Памятник Кирову на фоне здания администрации Кировского района с серпом и молотом.</p>--}}
+{{--@en--}}
+{{--  <p>Monument to Kirov against the backdrop of the Kirovsky District Administration building with a hammer and sickle.</p>--}}
+{{--@endru--}}
+{{--IMG_5557.jpeg--}}
+
+@ru
+  <p>Палеты с товаром выгружены прямо на тротуар у магазина.</p>
+@endru
+IMG_5560.jpeg
+
+@ru
+  <p>Здание администрации Кировского района. В голове играет композиция Hell March из игры C&C Red Alert.</p>
+@endru
+IMG_5561.jpeg
+
+@ru
+  <p>Чайки кружат над прудом в парке имени 9 Января.</p>
+@endru
+IMG_5564.jpeg
+
+@ru
+  <p>Яркая дверь спортзала.</p>
+@en
+  <p>The bright door of the sports hall.</p>
+@endru
+IMG_5565.jpeg
+
+@ru
+  <p>Цены на топливо весной 2026 года.</p>
+@en
+  <p>Fuel prices in the spring of 2026.</p>
+@endru
+IMG_5566.jpeg
+
+@ru
+  <p>Мойка дорог в процессе.</p>
+@endru
+IMG_5567.jpeg
+
+@ru
+  <p>Старые железнодорожные пути проходят между жилым комплексом и промзоной.</p>
+@endru
+IMG_5568.jpeg
+
+@ru
+  <p>Фасадная сетка на заброшенном здании выглядит как порванные шторы и развевается на ветру. Натянута она странным образом.</p>
+@endru
+IMG_5569.jpeg
+
+@ru
+  <p>Фасад Балтийского вокзала.</p>
+@en
+  <p>Baltiysky railway terminal facade.</p>
+@endru
+IMG_5571.jpeg
+
+@ru
+  <p>Павильон станции метро Балтийская примыкает прямо к зданию одноименного вокзала.</p>
+@endru
+IMG_5572.jpeg
+
+@ru
+  <p>Проход к поездам через зал Балтийского вокзала.</p>
+@endru
+IMG_5573.jpeg
+
+@ru
+  <p>Автоматы по продаже билетов на пригородные электрички.</p>
+@en
+  <p>Ticket vending machines for suburban trains.</p>
+@endru
+IMG_5574.jpeg
+
+@ru
+  <p>Линия турникетов для прохода на платформы.</p>
+@en
+  <p>A line of turnstiles for access to the platforms.</p>
+@endru
+IMG_5575.jpeg
+
+@ru
+  <p>Вход в музей железных дорог России рядом с Балтийским вокзалом.</p>
+@en
+  <p>Entrance to the Russian Railway Museum next to the Baltiysky railway station.</p>
+@endru
+IMG_5576.jpeg
+
+@ru
+  <p>Юсуповский сад.</p>
+@en
+  <p>Yusupov Garden.</p>
+@endru
+IMG_5578.jpeg
+
+@ru
+  <p>Фасад здания.</p>
+@endru
+IMG_5580.jpeg
+
+@ru
+  <p>Памятник Дмитрию Менделееву в сквере у Института метрологии.</p>
+@en
+  <p>Monument to Dmitry Mendeleev in the square near the Institute of Metrology.</p>
+@endru
+IMG_5582.jpeg
+
+@ru
+  <p>Рядом на стене здания таблица Менделеева по состоянию на начало прошлого века.</p>
+@endru
+IMG_5584.jpeg
+
+@ru
+  <p>Фонтан в Введенском саду на фоне военно-медицинского музея.</p>
+@endru
+IMG_5586.jpeg
+
+@ru
+  <p>Фасад Витебского вокзала с часовой башней.</p>
+@endru
+IMG_5587.jpeg
+
+@ru
+  <p>Часы показывают время в <a class="link" href="kaliningrad">Калининграде</a> и <a class="link" href="yakutsk">Якутске</a>. Точность вызывает сомнения.</p>
+@endru
+IMG_5588.jpeg
+
+@ru
+  <p>Стоило отойти от Витебского вокзала, тротуар закончился — пошла грунтовая дорожка.</p>
+@endru
+IMG_5589.jpeg
+
+@ru
+  <p>Проход к планетарию №1.</p>
+@endru
+IMG_5590.jpeg
+
+@ru
+  <p>Учебная площадка при мотошколе.</p>
+@endru
+IMG_5591.jpeg
+
+@ru
+  <p>В магазине шоколад собственного производства.</p>
+@endru
+IMG_5592.jpeg
+
+@ru
+  <p>Пискаревский проспект. Широкий тротуар вымощен плиткой.</p>
+@en
+  <p>Piskarevsky Prospekt. The wide sidewalk is paved with tiles.</p>
+@endru
+IMG_5595.jpeg
+
+@ru
+  <p>Жилой комплекс Пять звезд, пруд, сквер Безбородко.</p>
+@endru
+IMG_5596.jpeg
+
+@ru
+  <p>Скамейка с видом на пруд.</p>
+@endru
+IMG_5597.jpeg
+
+@ru
+  <p>Современное офисное здание. Фасад облицован переплетенной кортеновской сталью.</p>
+@endru
+IMG_5599.jpeg
+
+@ru
+  <p>Ленрезерв — выставочный центр на тему патриотизма.</p>
+@endru
+IMG_5600.jpeg
+
+@ru
+  <p>Ограда дачи Кушелева-Безбородко со скульптурами львов, держащих в зубах цепь.</p>
+@endru
+IMG_5601.jpeg
+IMG_5602.jpeg
+
+@ru
+  <p>Бизнес-центр Бенуа. Задний фасад здания украшен увеличенным до пикселей фрагментом рисунка художника.</p>
+@endru
+IMG_5604.jpeg
+
+@ru
+  <p>Шуточная мемориальная доска: «В этом здании в мае 1917 года скрывался В. И. Ленин от Н. К. Крупской».</p>
+@en
+  <p>A comic memorial plaque: "In this building in May 1917, V. I. Lenin hid from N. K. Krupskaya."</p>
+@endru
+IMG_5605.jpeg
+
+@ru
+  <p>Гравицапа в бизнес-парке Полюстрово отсылает к советскому фильму Кин-дза-дза.</p>
+@endru
+IMG_5607.jpeg
+
+@ru
+  <p>В парке много деревьев.</p>
+@endru
+IMG_5608.jpeg
+IMG_5609.jpeg
+
+@ru
+  <p>Бизнес-центр Зима.</p>
+@endru
+IMG_5610.jpeg
+
+@ru
+  <p>Бизнес-центр «Феррум».</p>
+@endru
+IMG_5611.jpeg
+
+@ru
+  <p>Фактура кортеновской стали вблизи.</p>
+@en
+  <p>The texture of Corten steel up close.</p>
+@endru
+IMG_5612.jpeg
+
+@ru
+  <p>Широкая Свердловская набережная.</p>
+@endru
+IMG_5613.jpeg
+
+@ru
+  <p>Робот Яндекса снимает панорамы улиц.</p>
+@endru
+IMG_5614.jpeg
+
+@ru
+  <p>Благоустроенный сквер Кнорозова.</p>
+@endru
+IMG_5615.jpeg
+IMG_5617.jpeg
+
+@ru
+  <p>Мемориальная доска Юрию Кнорозову, расшифровавшему письменность майя, с милой деталью — фигурой его любимой кошки.</p>
+@en
+  <p>A memorial plaque to Yuri Knorozov, who deciphered the Maya script, featuring a cute detail — the figure of his beloved cat.</p>
+@endru
+IMG_5620.jpeg
+
+@ru
+  <p>Информационный стенд.</p>
+@en
+  <p>An information board.</p>
+@endru
+IMG_5621.jpeg
+
+@ru
+  <p>Вид снизу на Финляндский железнодорожный мост.</p>
+@endru
+IMG_5625.jpeg
+
+@ru
+  <p>Яркий закат над Невой.</p>
+@en
+  <p>A bright sunset over the Neva River.</p>
+@endru
+IMG_5627.jpeg
+IMG_5628.jpeg
+
+@ru
+  <p>Речной теплоход на фоне вечерних огней и силуэтов труб ТЭЦ.</p>
+@en
+  <p>A riverboat against the backdrop of evening lights and silhouettes of thermal power plant chimneys.</p>
+@endru
+IMG_5645.jpeg
+
+@ru
+  <p>Большеохтинский мост в ночной подсветке.</p>
+@endru
+IMG_5647.jpeg
+
+@ru
+  <p>Сквозь пролеты виднеется Смольный собор.</p>
+@endru
+IMG_5646.jpeg
+
+@ru
+  <p>Длинный крытый пешеходный переход вдоль строительной площадки.</p>
+@en
+  <p>A long covered pedestrian walkway along a construction site.</p>
+@endru
+IMG_5648.jpeg
+
+@ru
+  <p>Было приятно, что в отеле включен завтрак. Меньше четырех тарелок набирать не получалось.</p>
+@endru
+IMG_5652.jpeg
+
+@ru
+  <p>Зона ресепшен в отеле.</p>
+@en
+  <p>The hotel reception area.</p>
+@endru
+IMG_5654.jpeg
+
+@ru
+  <p>Панно на потолке Московского вокзала.</p>
+@endru
+IMG_5655.jpeg
+
+@ru
+  <p>Геометрия бетонного навеса над платформой.</p>
+@en
+  <p>The geometry of the concrete canopy over the platform.</p>
+@endru
+IMG_5656.jpeg
+
+@ru
+  <p>Информационное табло под потолком в салоне Сапсана.</p>
+@en
+  <p>An information display under the ceiling in the Sapsan carriage.</p>
+@endru
+IMG_5657.jpeg
+
+@ru
+  <p>Был взят билет с питанием. Дали бутерброд в ланч-боксе с вот таким оформлением.</p>
+@endru
+IMG_5659.jpeg
+@endsection
