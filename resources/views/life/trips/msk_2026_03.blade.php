901b096500ab19a9ee0641919955e9233e19f654
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-09 05:56:49 +0700

    Черновик

---
+++ resources/views/life/trips/msk_2026_03.blade.php
@@ -0,0 +1,38 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Электровоз.</p>
+@en
+  <p>Electric locomotive.</p>
+@endru
+IMG_5357.jpeg
+
+@ru
+  <p>Двери в поезде метро на Филевской линии.</p>
+@en
+  <p>Doors in a metro train on the Filyovskaya line.</p>
+@endru
+IMG_5358.jpeg
+
+@ru
+  <p>Поезд метро на изогнутой платформе станции «Александровский сад».</p>
+@en
+  <p>A metro train on the curved platform of the Aleksandrovsky Sad station.</p>
+@endru
+IMG_5359.jpeg
+
+@ru
+  <p>Троицкая башня Кремля и остатки снега в Александровском саду.</p>
+@en
+  <p>The Troitskaya Tower of the Kremlin and the remnants of snow in the Alexander Garden.</p>
+@endru
+IMG_5360.jpeg
+
+@ru
+  <p>Юбилейный люк: 1823–2013.</p>
+@en
+  <p>An anniversary manhole cover: 1823–2013.</p>
+@endru
+IMG_5361.jpeg
+@endsection
