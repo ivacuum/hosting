f234778c26eb065be7458a2f4f0fcfdaad17b4f1
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-09 08:24:32 +0700

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2025_01.blade.php
@@ -0,0 +1,154 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Прибытие на Киевский вокзал.</p>
+@en
+  <p>Arrival at the Kiyevsky railway station.</p>
+@endru
+IMG_1148.jpeg
+
+@ru
+  <p>Знакомый по Калуге мясной ресторан Бык есть в Москве.</p>
+@endru
+IMG_1150.jpeg
+
+@ru
+  <p>Уличная навигация у Киевского вокзала.</p>
+@en
+  <p>Street navigation near the Kiyevsky railway station.</p>
+@endru
+IMG_1151.jpeg
+
+@ru
+  <p>Елка Московского транспорта.</p>
+@en
+  <p>Moscow Transport's Christmas tree.</p>
+@endru
+IMG_1152.jpeg
+
+@ru
+  <p>Прибытие поезда метро.</p>
+@en
+  <p>Metro train arrival.</p>
+@endru
+IMG_1153.jpeg
+
+@ru
+  <p>Окрестности станции метро Юго-Западная.</p>
+@endru
+IMG_1154.jpeg
+
+@ru
+  <p>Тротуар чистят реагентами, судя по лежащей белой крошке.</p>
+@endru
+IMG_1155.jpeg
+
+@ru
+  <p>Голуби облепили провода. Называю такое их скопление артиллерией, потому что обосрать могут в любой момент.</p>
+@endru
+IMG_1157.jpeg
+IMG_1156.jpeg
+
+@ru
+  <p>Заснеженный двор.</p>
+@en
+  <p>Snow-covered courtyard.</p>
+@endru
+IMG_1158.jpeg
+
+@ru
+  <p>Детская площадка в снегу.</p>
+@en
+  <p>Snow-covered playground.</p>
+@endru
+IMG_1159.jpeg
+
+@ru
+  <p>Спортивная площадка.</p>
+@en
+  <p>Street workout area.</p>
+@endru
+IMG_1160.jpeg
+
+@ru
+  <p>Частично прозрачная дверь подъезда жилого дома. Скамейка под козырьком.</p>
+@endru
+IMG_1161.jpeg
+
+@ru
+  <p>Более прозрачная дверь и пандус у подъезда номер два.</p>
+@endru
+IMG_1163.jpeg
+
+@ru
+  <p>Антипарковочные столбики вдоль тротуара.</p>
+@endru
+IMG_1165.jpeg
+
+@ru
+  <p>Уборка снега роторным снегоочистителем.</p>
+@en
+  <p>Snow removal with a rotary snow blower.</p>
+@endru
+IMG_1167.jpeg
+
+@ru
+  <p>Меню китайского ресторана Золотая рыбка.</p>
+@endru
+IMG_1168.jpeg
+
+@ru
+  <p>Интерьер ресторана.</p>
+@endru
+IMG_1169.jpeg
+
+@ru
+  <p>Купон на скидку.</p>
+@en
+  <p>A discount coupon.</p>
+@endru
+IMG_1170.jpeg
+
+@ru
+  <p>Прилавок китайского магазина. Можно оплатить покупки через WeChat или Alipay.</p>
+@en
+  <p>Counter at a Chinese store. You can pay with WeChat or Alipay.</p>
+@endru
+IMG_1171.jpeg
+
+@ru
+  <p>Оплата переводом китайскому продавцу.</p>
+@en
+  <p>Payment by transfer to a Chinese seller.</p>
+@endru
+IMG_1172.PNG
+
+@ru
+  <p>Кафе с бабл-ти.</p>
+@en
+  <p>Bubble tea cafe.</p>
+@endru
+IMG_1173.jpeg
+
+@ru
+  <p>Экспресс до Калуги на Киевском вокзале.</p>
+@en
+  <p>Express train to Kaluga at the Kiyevsky railway station.</p>
+@endru
+IMG_1174.jpeg
+
+@ru
+  <p>Заснеженная платформа.</p>
+@en
+  <p>Snow-covered platform.</p>
+@endru
+IMG_1175.jpeg
+
+@ru
+  <p>Интерьер вагона экспресса.</p>
+@en
+  <p>Interior of the express train car.</p>
+@endru
+IMG_1176.jpeg
+@endsection
