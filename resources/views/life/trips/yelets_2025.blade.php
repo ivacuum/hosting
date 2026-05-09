3c51afa6e053b6e46492b33f06a60a58e5b4a486
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-09 07:23:46 +0700

    Черновик

---
+++ resources/views/life/trips/yelets_2025.blade.php
@@ -0,0 +1,123 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Поезд из Липецка прибыл на станцию Елец.</p>
+@endru
+IMG_3127.jpeg
+
+@ru
+  <p>Крохотное здание вокзала. Напоминает Калугу-2.</p>
+@endru
+IMG_3128.jpeg
+
+@ru
+  <p>Железнодорожные платформы.</p>
+@endru
+IMG_3130.jpeg
+
+@ru
+  <p>Автобусная остановка напротив вокзала.</p>
+@endru
+IMG_3131.jpeg
+
+@ru
+  <p>Стоимость проезда в автобусе: 28 ₽ по транспортной или банковской карте и 32 ₽ наличными.</p>
+@en
+  <p>Bus fare: 28 ₽ with a transport or bank card and 32 ₽ with cash.</p>
+@endru
+IMG_3132.jpeg
+
+@ru
+  <p>В дождливый день было малоприятно долго ждать автобус. Такси помогло быстро и сухо добраться до жилья. Этим же вечером предстоял ночной поезд до Москвы, поэтому заселение было лишь для возможности припарковать вещи и отдохнуть.</p>
+@endru
+IMG_3133.jpeg
+
+@ru
+  <p>Деревянный дом с резными белыми наличниками.</p>
+@en
+  <p>A wooden house with carved white window frames.</p>
+@endru
+IMG_3134.jpeg
+
+@ru
+  <p>Деревянный стенд с туристической картой города.</p>
+@en
+  <p>A wooden stand featuring a tourist map of the city.</p>
+@endru
+IMG_3135.jpeg
+
+@ru
+  <p>Таблички с историческими и современными названиями улиц на углу дома 102. Профсоюзная (бывшая Введенская) и Ленина (бывшая Манежная).</p>
+@en
+  <p>Street signs with historical and modern names on the corner of building 102. Profsoyuznaya (formerly Vvedenskaya) and Lenina (formerly Manezhnaya).</p>
+@endru
+IMG_3136.jpeg
+
+@ru
+  <p>Яркий фиолетовый магазин на углу у пешеходного перехода.</p>
+@en
+  <p>A bright purple shop on the corner next to a pedestrian crossing.</p>
+@endru
+IMG_3137.jpeg
+
+@ru
+  <p>Дорога. Справа возвышается красная пожарная каланча.</p>
+@en
+  <p>Road. Red fire tower rising on the right.</p>
+@endru
+IMG_3138.jpeg
+
+@ru
+  <p>Вознесенский собор с синими куполами в начале улицы.</p>
+@endru
+IMG_3140.jpeg
+
+@ru
+  <p>Стеклянная автобусная остановка «Детский парк».</p>
+@en
+  <p>Glass bus stop shelter "Detsky Park".</p>
+@endru
+IMG_3141.jpeg
+
+@ru
+  <p>Гостиница Елец — массивное советское здание в малоэтажном городе.</p>
+@endru
+IMG_3142.jpeg
+
+@ru
+  <p>Деревянный указатель направлений к местным достопримечательностям.</p>
+@en
+  <p>A wooden signpost pointing to local landmarks.</p>
+@endru
+IMG_3143.jpeg
+
+@ru
+  <p>Карусель в виде чайного сервиза с гжельской росписью.</p>
+@en
+  <p>A carousel shaped like a tea set painted in the Gzhel style.</p>
+@endru
+IMG_3144.jpeg
+
+@ru
+  <p>Кованая стела.</p>
+@en
+  <p>A wrought-iron stele.</p>
+@endru
+IMG_3145.jpeg
+
+@ru
+  <p>Кованые гербы Ельца и других городов, участвующих в фестивале малых городов.</p>
+@endru
+IMG_3146.jpeg
+IMG_3147.jpeg
+IMG_3148.jpeg
+IMG_3149.jpeg
+
+@ru
+  <p>Ярко-желтая форзиция и таблички с названиями растений в сквере.</p>
+@en
+  <p>A bright yellow forsythia bush and plant nameplates in the park.</p>
+@endru
+IMG_3150.jpeg
+@endsection
