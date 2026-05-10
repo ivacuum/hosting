a00eb834264a878ea59ccb3b2d9f2389e77ead9a
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-11 06:25:37 +0700

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2025_07.blade.php
@@ -0,0 +1,66 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Лет девять уже прошло с первой пробы вегетарианской котлеты в Джаганнате.</p>
+@endru
+IMG_3769.jpeg
+
+@ru
+  <p>Перекрытая улица Земляной Вал.</p>
+@endru
+IMG_3770.jpeg
+
+@ru
+  <p>Масштабный стрит-арт на фасаде здания напротив Курского вокзала.</p>
+@endru
+IMG_3772.jpeg
+
+@ru
+  <p>Павильон станции метро Курская.</p>
+@en
+  <p>Kurskaya metro station pavilion.</p>
+@endru
+IMG_3773.jpeg
+
+@ru
+  <p>Трамваи разных поколений.</p>
+@en
+  <p>Trams of different generations.</p>
+@endru
+IMG_3774.jpeg
+
+@ru
+  <p>В центре Москвы активно глушат и подменяют GPS. Карты показывают, что ты находишься где-то где-то под Симферополем.</p>
+@endru
+IMG_3775.PNG
+
+@ru
+  <p>Поезд Ласточка на платформе.</p>
+@en
+  <p>Lastochka train at the platform.</p>
+@endru
+IMG_3777.jpeg
+
+@ru
+  <p>Оформление станции метро Киевская.</p>
+@en
+  <p>Decoration of the Kievskaya metro station.</p>
+@endru
+IMG_3778.jpeg
+
+@ru
+  <p>Вид с платформы Киевского вокзала.</p>
+@endru
+IMG_3780.jpeg
+
+@ru
+  <p>Мурал на торце жилого дома в Калуге.</p>
+@endru
+IMG_3781.jpeg
+
+@ru
+  <p>Еще один мурал — с картой города и достопримечательностями Калуги. Нравится как переиспользовали цвет фасада.</p>
+@endru
+IMG_3782.jpeg
+@endsection
