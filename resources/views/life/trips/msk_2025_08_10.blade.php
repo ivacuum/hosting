b57bfc59f66fa80295b4c79106ded010c12c5f9d
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-10 08:30:24 +0700

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2025_08_10.blade.php
@@ -0,0 +1,95 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Дизель-поезд РА2 на станции Калуга-1.</p>
+@endru
+IMG_3891.jpeg
+
+@ru
+  <p>Электрички на станции Калуга-1 и поезд дальнего следования.</p>
+@endru
+IMG_3892.jpeg
+
+@ru
+  <p>Турникеты на выход из метро.</p>
+@en
+  <p>Metro exit turnstiles.</p>
+@endru
+IMG_3894.jpeg
+
+@ru
+  <p>Цветочное оформление у пруда в Парке Горького.</p>
+@en
+  <p>Floral decoration by the pond in Gorky Park.</p>
+@endru
+IMG_3896.jpeg
+
+@ru
+  <p>Фонтаны в Парке Горького.</p>
+@en
+  <p>Fountains in Gorky Park.</p>
+@endru
+IMG_3897.jpeg
+
+@ru
+  <p>Катамараны на Голицынском пруду.</p>
+@en
+  <p>Pedal boats on Golitsyn Pond.</p>
+@endru
+IMG_3898.jpeg
+
+@ru
+  <p>Тенистая аллея.</p>
+@en
+  <p>Shady alley.</p>
+@endru
+IMG_3899.jpeg
+
+@ru
+  <p>Белка в Нескучном саду.</p>
+@en
+  <p>Squirrel in Neskuchny Garden.</p>
+@endru
+IMG_3902.jpeg
+
+@ru
+  <p>Гравийные дорожки и минималистичный фонарь.</p>
+@en
+  <p>Gravel paths and minimalist streetlights.</p>
+@endru
+IMG_3903.jpeg
+
+@ru
+  <p>Вид на Москву-реку и главное здание МГУ на закате.</p>
+@en
+  <p>View of the Moscow River and the main MSU building at sunset.</p>
+@endru
+IMG_3908.jpeg
+
+@ru
+  <p>Крыша Лужников возвышается над деревьями.</p>
+@en
+  <p>The roof of the Luzhniki rising above the trees.</p>
+@endru
+IMG_3909.jpeg
+
+@ru
+  <p>Освещенный навес у Киевского вокзала.</p>
+@endru
+IMG_3910.jpeg
+
+@ru
+  <p>Кассы метро на станции «Киевская».</p>
+@en
+  <p>Metro ticket offices at Kiyevskaya station.</p>
+@endru
+IMG_3911.jpeg
+
+@ru
+  <p>Поезд дальнего следования у платформы Киевского вокзала.</p>
+@en
+  <p>A long-distance train at the platform of Kiyevsky Railway Station.</p>
+@endru
+IMG_3912.jpeg
+@endsection
