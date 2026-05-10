a34a6eec39e30338a4de894fd12c7727a0ea8eca
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-11 06:12:54 +0700

    Заготовка о поездке в Пусан

---
+++ resources/views/life/trips/busan_2025.blade.php
@@ -0,0 +1,105 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Почему-то у соседних кресел экспресса до Москвы спинка разной высоты.</p>
+@endru
+IMG_3918.jpeg
+
+@ru
+  <p>Площадь Тверская Застава. Бизнес-центр Белая площадь.</p>
+@en
+  <p>Tverskaya Zastava Square and White Square business center.</p>
+@endru
+IMG_3919.jpeg
+
+@ru
+  <p>Аэроэкспресс до аэропорта Шереметьево.</p>
+@endru
+IMG_3920.jpeg
+
+@ru
+  <p>Платформа Аэроэкспресса в Шереметьево.</p>
+@en
+  <p>Aeroexpress platform at Sheremetyevo airport.</p>
+@endru
+IMG_3923.jpeg
+
+@ru
+  <p>Стенд авиакомпании China Eastern. Впервые узнал о возможности рерутинга — смене города, в который летишь.</p>
+@endru
+IMG_3924.jpeg
+
+@ru
+  <p>Самолет China Eastern Airlines у телетрапа.</p>
+@en
+  <p>China Eastern Airlines plane at the jet bridge.</p>
+@endru
+IMG_3926.jpeg
+
+@ru
+  <p>Внутри самолета до Шанхая.</p>
+@endru
+IMG_3932.jpeg
+
+@ru
+  <p>Терминал аэропорта Пудун в Шанхае.</p>
+@en
+  <p>Terminal of Pudong Airport in Shanghai.</p>
+@endru
+IMG_3933.jpeg
+
+@ru
+  <p>Как только появились бесплатные проходы в лаунжи от банка, перестали волновать многочасовые пересадки и прибытие в аэропорт заранее — можно поделать что-нибудь полезное и есть по мере нападения голода.</p>
+@endru
+IMG_3934.jpeg
+
+@ru
+  <p>Очередь на посадку у выхода 216.</p>
+@en
+  <p>Boarding line at gate 216.</p>
+@endru
+IMG_3936.jpeg
+
+@ru
+  <p>Посадка в самолет.</p>
+@en
+  <p>Boarding the plane.</p>
+@endru
+IMG_3937.jpeg
+
+@ru
+  <p>Аэродромный тягач.</p>
+@en
+  <p>Pushback tractor.</p>
+@endru
+IMG_3938.jpeg
+
+@ru
+  <p>Бортовое питание на коротком рейсе.</p>
+@en
+  <p>In-flight meal on a short flight.</p>
+@endru
+IMG_3939.jpeg
+
+@ru
+  <p>Задержка рейса MU5033. Прибытие в Сеул в 17:31 вместо 15:20.</p>
+@en
+  <p>MU5033 flight delay. Arrived in Seoul at 17:31 instead of 15:20.</p>
+@endru
+IMG_3940.PNG
+
+@ru
+  <p>Ближайшие автобусы из аэропорта до Пусана только завтра утром.</p>
+@en
+  <p>The next buses from the airport to Busan are only available tomorrow morning.</p>
+@endru
+IMG_3941.jpeg
+
+@ru
+  <p>Расписание автобусов на завтра. Время в пути неизвестно. Самый ранний автобус в 06:20 утра. Некий улучшенный премиум.</p>
+@en
+  <p>Tomorrow's bus schedule.</p>
+@endru
+IMG_3942.jpeg
+@endsection
