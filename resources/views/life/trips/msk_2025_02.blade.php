3be0b437a6f3dbca84a1f2a2a3a3982680dbafbe
Sergei Pankov <vacuum@ivacuum.ru>
2026-05-10 07:59:57 +0700

    Заготовка о поездке в Москву

---
+++ resources/views/life/trips/msk_2025_02.blade.php
@@ -0,0 +1,175 @@
+@extends('life.trips.base')
+
+@section('content')
+@ru
+  <p>Второй железнодорожный вокзал Калуги.</p>
+@endru
+IMG_1223.jpeg
+
+@ru
+  <p>Никольская улица в новогоднем убранстве.</p>
+@en
+  <p>Nikolskaya street in New Year decorations.</p>
+@endru
+IMG_1224.jpeg
+
+@ru
+  <p>Ресторан Мандарин. В витрине висят утки. По картам думал, что его назвали в честь фрукта, а вживую очевидно, что в честь Китая.</p>
+@endru
+IMG_1225.jpeg
+
+@ru
+  <p>Праздничный тоннель из шаров и гирлянд.</p>
+@endru
+IMG_1226.jpeg
+
+@ru
+  <p>Широкая скамейка. Стволы деревьев укутаны.</p>
+@endru
+IMG_1227.jpeg
+
+@ru
+  <p>Здание политехнического музея.</p>
+@endru
+IMG_1228.jpeg
+IMG_1229.jpeg
+
+@ru
+  <p>В Ильинском сквере часовня-памятник гренадерам, павшим под Плевной.</p>
+@endru
+IMG_1230.jpeg
+
+@ru
+  <p>Историческая застройка Старой площади сквозь голые зимние деревья.</p>
+@endru
+IMG_1231.jpeg
+
+@ru
+  <p>Ящик с противогололедным материалом, мешки с реагентами и инвентарь дворника.</p>
+@en
+  <p>A box with anti-icing material, bags of reagents, and a street sweeper's tools.</p>
+@endru
+IMG_1232.jpeg
+
+@ru
+  <p>Тротуар шире проезжей части.</p>
+@endru
+IMG_1233.jpeg
+
+@ru
+  <p>Дом по улице Забелина.</p>
+@endru
+IMG_1234.jpeg
+
+@ru
+  <p>Японская кухня на обед. При посадке сообщили сколько времени можно провести за столом (около полутора часов).</p>
+@endru
+IMG_1235.jpeg
+
+@ru
+  <p>Фасад Московского архитектурного института с майоликовыми панно.</p>
+@en
+  <p>The facade of the Moscow Architectural Institute with majolica panels.</p>
+@endru
+IMG_1237.jpeg
+
+@ru
+  <p>Станция метро Красносельская.</p>
+@en
+  <p>Krasnoselskaya metro station.</p>
+@endru
+IMG_1238.jpeg
+
+@ru
+  <p>Вид на пути Казанского вокзала. Ряды поездов РЖД в фирменной раскраске.</p>
+@en
+  <p>View of the Kazansky railway station tracks. Rows of RZD trains in their corporate livery.</p>
+@endru
+IMG_1239.jpeg
+
+@ru
+  <p>Шоу-рум в магазине 4k-monitor. Интересовало посмотреть как олед-мониторы выглядят вживую и появилось ли что-нибудь лучше монитора LG 5K UltraFine. Спойлер: пока не появилось.</p>
+@endru
+IMG_1240.jpeg
+
+@ru
+  <p>Черный цвет на оледе действительно сливается с рамками. 240 герц приятны глазу. Разрешение 2560×1440 — маловато, надо минимум на 4К замахиваться ради четкости текста, но такого образца в демо-зоне не было. Да и ПК гораздо мощнее нужен для 4К.</p>
+@endru
+IMG_1241.jpeg
+
+@ru
+  <p>Улица Краснопрудная.</p>
+@endru
+IMG_1243.jpeg
+
+@ru
+  <p>Третьяковский проезд и фрагмент Китайгородской стены.</p>
+@en
+  <p>Tretyakovsky Proyezd and a fragment of the Kitay-gorod wall.</p>
+@endru
+IMG_1244.jpeg
+
+@ru
+  <p>Здание отеля Метрополь с майоликовыми панно.</p>
+@endru
+IMG_1245.jpeg
+
+@ru
+  <p>Большой театр с новогодними декорациями перед ним.</p>
+@endru
+IMG_1246.jpeg
+
+@ru
+  <p>Попытка сфотографировать Большой театр вблизи.</p>
+@endru
+IMG_1247.jpeg
+
+@ru
+  <p>Световые арки на Театральной площади.</p>
+@en
+  <p>Illuminated arches on Teatralnaya Square.</p>
+@endru
+IMG_1248.jpeg
+
+@ru
+  <p>Гостиница Метрополь и новогодние украшения.</p>
+@endru
+IMG_1249.jpeg
+
+@ru
+  <p>Многополосная дорога.</p>
+@endru
+IMG_1250.jpeg
+IMG_1251.jpeg
+
+@ru
+  <p>У Киевского вокзала появился навес до моста Богдана Хмельницкого.</p>
+@endru
+IMG_1252.jpeg
+
+@ru
+  <p>Московский электробус.</p>
+@en
+  <p>A Moscow electric bus.</p>
+@endru
+IMG_1253.jpeg
+
+@ru
+  <p>Автоматические камеры хранения на Киевском вокзале. Можно оплатить и открыть ячейку по куар-коду со телефона.</p>
+@en
+  <p>Automated luggage lockers at the Kiyevsky railway station. You can pay and open a locker via QR code from your smartphone.</p>
+@endru
+IMG_1254.jpeg
+
+@ru
+  <p>Лепнина с советскими сюжетами на Киевском вокзале.</p>
+@endru
+IMG_1255.jpeg
+
+@ru
+  <p>Зал ожидания Киевского вокзала.</p>
+@en
+  <p>The waiting room of the Kiyevsky railway station.</p>
+@endru
+IMG_1256.jpeg
+@endsection
