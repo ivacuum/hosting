51e401dadc643e43b1fc96826bbfb0b30ceabec6
Sergei Pankov <vacuum@ivacuum.ru>
2026-06-20 18:26:04 +0300

    Отзыв об игре Yakuza Like a Dragon

--- resources/views/life/games/yakuza-like-a-dragon.blade.php
+++ resources/views/life/games/yakuza-like-a-dragon.blade.php
@@ -164,46 +164,198 @@
   <p>Сонный киносеанс.</p>
 @endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 08.44.57.55.webp'])
+
+@ru
+  <p>Завязка очередного побочного задания — кто-то застрял вверх ногами в куче мусора.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 11.04.53.86.webp'])
+
+@ru
+  <p>Вскоре в мусоре прибывает — теперь там торчит уже две пары ног, а помогать им никто не спешит.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 13.18.03.62.webp'])
+
+@ru
+  <p>Мистер Мазохист. Обычные удары его не берут, а лишь приносят удовольствие. К такому нужно найти особый подход.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 14.27.18.19.webp'])
+
+@ru
+  <p>Бейджик управляющего круглосуточным магазином Poppo. Нравится, когда четкое детальное окружение.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 14.32.11.85.webp'])
+
+@ru
+  <p>Мазохизм бывает разным. Например, кулинарным — Касуга чуть не сгорел от острейшего сычуаньского блюда, но сразу же захотел добавки.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 14.53.54.52.webp'])
+
+@ru
+  <p>Призыв рака Нэнси против босса. Сначала она мирно взбирается на плечо жертвы.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 15.55.35.83.webp'])
+
+@ru
+  <p>А затем переходит в атаку.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 15.55.56.89.webp'])
+
+@ru
+  <p>Касуга несет горшок с легендарным кимчи из побочного квеста «Чудо кимчи». Это блюдо настолько острое и целебное, что буквально ставит людей на ноги.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.18 - 16.29.23.54.webp'])
+
+@ru
+  <p>Офис организации «Герой по вызову» и его основатель Хиро, носящий супергеройскую маску. Первый спасенный кот возвращен владельцу.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.19 - 14.18.31.66.webp'])
+
+@ru
+  <p>Результат нахождения всех девяти кошек по квестам Хиро — теперь его офис превратился в настоящий кошачий приют.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.19 - 14.19.14.02.webp'])
+
+@ru
+  <p>Успешный разгон митинга.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.20 - 20.47.23.51.webp'])
+
+@ru
+  <p>Экран результатов боя. Качаются персонажи, качаются профессии, повышается уровень доверия друг между другом. Самое неприятное в игре было, что перед одним боссом пришлось всех серьезно подкачать, чтобы босс стал посильным.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.23 - 12.44.27.20.webp'])
+
+@ru
+  <p>Сражение с гигантским взбесившимся роботом-пылесосом.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.23 - 13.49.00.95.webp'])
+
+@ru
+  <p>Снова в школе профподготовки. Вопрос из теста на диетолога: какое из блюд является турецким?</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 11.16.34.47.webp'])
+
+@ru
+  <p>Следующий вопрос по диетологии — о холодном супе из картофельного пюре, сливок и бульона.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 11.17.28.53.webp'])
+
+@ru
+  <p>Вопрос о Гринвичской обсерватории в Англии, определяющей время по Гринвичу (GMT).</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 12.30.01.03.webp'])
+
+@ru
+  <p>Какая шахматная фигура может превращаться в любую другую, кроме короля?</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 12.31.50.03.webp'])
+
+@ru
+  <p>Персонажи вне текущей активной команды тоже качаются, но медленнее.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 18.40.16.97.webp'])
+
+@ru
+  <p>Шимпанзе Клара угоняет экскаватор.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 19.02.55.76.webp'])
+
+@ru
+  <p>Экскаватору не поздоровилось.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.26 - 19.15.44.89.webp'])
+
+@ru
+  <p>Забавный квест на подсчет людей, проходящих через улицу. Кто по этой улице только не ходил!</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.28 - 20.36.26.70.webp'])
+
+@ru
+  <p>Дружеский ужин и забавный диалог за столом в корейском ресторанчике. Вкусы Касуги весьма специфичны.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.28 - 22.04.31.66.webp'])
+
+@ru
+  <p>Испытание ловкости в мини-игре UFO Catcher (игровой автомат с клешнёй) в клубе SEGA.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.29 - 12.55.57.35.webp'])
-@include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.29 - 12.58.37.100.webp'])
+
+@ru
+  <p>Из грязи в князи. В начале игры ищешь копейки, а уже в середине можешь ворочать миллионы.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.29 - 16.54.02.79.webp'])
+
+@ru
+  <p>Классно сделаны разговоры персонажей о тех или иных местах города. Усиливают чувство сплоченности.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.31 - 19.24.23.17.webp'])
+
+@ru
+  <p>Воспроизведен канал в Осаке.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.31 - 19.27.36.25.webp'])
-@include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.31 - 22.55.12.03.webp'])
+
+@ru
+  <p>Сюжетный звонок.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.05.31 - 22.55.13.95.webp'])
+
+@ru
+  <p>Стены бара Survive (убежища команды) обклеили оскорбительными листовками. Оппоненты пытаются очернить главного героя и его друзей.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.01 - 14.49.03.11.webp'])
+
+@ru
+  <p>Сюжетный агитационный фургон.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.01 - 14.50.53.48.webp'])
+
+@ru
+  <p>Предвыборный штаб конкурентов празднует промежуточные успехи. Снова нравятся детали и надписи.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.03 - 15.29.52.28.webp'])
+
+@ru
+  <p>След от поцелуя на щеке Касуги после успешного завершения одной из романтических линий.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.03 - 23.02.00.90.webp'])
+
+@ru
+  <p>Не могу не порекомендовать завершить все романтические линии, чтобы увидеть кульминацию. Миякоши из школы профподготовки угрожает Касуге нагинатой.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.48.53.14.webp'])
+
+@ru
+  <p>Сумирэ из мастерской тоже пришла на разборки не с пустыми руками: она хочет опробовать на неверном Касуге новый арбалет.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.49.06.59.webp'])
+
+@ru
+  <p>Рилика из Hello Work разминает кулаки. У нее явно есть для Касуги особенное «задание».</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.49.17.60.webp'])
+
+@ru
+  <p>Ироха из бара Survive вооружена бейсбольной битой с гвоздями. Похоже, приятные беседы у барной стойки подошли к концу.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.49.31.99.webp'])
-@include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.49.58.35.webp'])
+
+@ru
+  <p>Касуга пытается убедить разъяренных девушек, что это всё недоразумение. Получается не очень.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.50.00.01.webp'])
+
+@ru
+  <p>Кульминация.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 19.50.09.35.webp'])
+
+@ru
+  <p>Местные жители знают толк в протагонистах: «Не у каждого найдутся кудрявые волосы, выносливость к взрывам и куча свободного времени!»</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 20.01.57.43.webp'])
+
+@ru
+  <p>Касуга в костюме маскота Оно Мичио разбирается с фальшивыми подделками. Посчастливилось побывать в этом регионе и попробовать их местный рамен, что можно видеть на голове маскота.</p>
+@endru
 @include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 20.22.13.27.webp'])
-@include('tpl.game-screenshot', ['pic' => 'Yakuza  Like a Dragon Screenshot 2021.06.04 - 20.22.20.75.webp'])
