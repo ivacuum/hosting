eb32b5d48a58d48e45e6404dbc6f84901ef3a4e5
Sergei Pankov <vacuum@ivacuum.ru>
2026-08-01 19:08:29 +0300

    До 20 МБ можно

--- config/livewire.php
+++ config/livewire.php
@@ -10,4 +10,18 @@ return [
             'test' => false,
         ],
     ],
+
+    'temporary_file_upload' => [
+        'disk' => env('LIVEWIRE_TEMPORARY_FILE_UPLOAD_DISK'),
+        'rules' => ['required', 'file', 'max:20480'],
+        'directory' => null,
+        'middleware' => null,
+        'preview_mimes' => [
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5,
+        'cleanup' => true,
+    ],
 ];
