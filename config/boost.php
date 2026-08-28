1db508f18fe7eba5ae8a534c089e6d412f22cbd0
Sergei Pankov <vacuum@ivacuum.ru>
2026-08-28 18:39:22 +0300

    Обновление зависимостей

---
+++ config/boost.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'guidelines' => [
+        'exclude' => [
+            // 'phpunit/core',
+            // 'tests',
+        ],
+    ],
+    'skills' => [
+        'exclude' => [
+            'laravel-best-practices',
+            'testing-best-practices',
+        ],
+    ],
+];
